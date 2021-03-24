f61169c1da34da72875ffeca0ecb60a63cf9164a
megrogan <megrogan@gmail.com>
2021-03-24 16:53:43 +0000

    Expand test calls

--- js-agent/src/testCalls.ts
+++ js-agent/src/testCalls.ts
@@ -2,38 +2,64 @@ import WalletApi from "./WalletApi";
 import { AnonymousIdentity, Principal } from "@dfinity/agent";
 import BigNumber from "bignumber.js";
 import { ICPTs } from "./canisters/ledger/service";
+import LedgerService from "./canisters/ledger/service";
+import GovernanceService, { ProposalInfo } from "./canisters/governance/service";
 
-const TRANSACTION_FEE : ICPTs = { doms: new BigNumber(137) };
+const TRANSACTION_FEE : ICPTs = fromDoms(137);
 
-export default function(walletApi: WalletApi) {
+export default async function(walletApi: WalletApi) {
     console.log("Enter testCalls");
 
     const identity = new AnonymousIdentity();
     const icEndpoint = "http://10.12.31.5:8080/";
     const myPrincipal = Principal.fromText("347of-sq6dc-h53df-dtzkw-eama6-hfaxk-a7ghn-oumsd-jf2qy-tqvqc-wqe");
 
-    const governanceService = walletApi.buildGovernanceService(
-        icEndpoint,
-        identity
-    );
+    const governanceService = walletApi.buildGovernanceService(icEndpoint, identity);
+    const ledgerService = walletApi.buildLedgerService(icEndpoint,identity);
+    const ledgerViewService = walletApi.buildLedgerViewService(icEndpoint, identity);
 
-    const ledgerService = walletApi.buildLedgerService(
-        icEndpoint,
-        identity);
+    const pendingProposals = await governanceService.get_pending_proposals();
+    console.log(pendingProposals);
 
-    ledgerService.send({
-        to: myPrincipal,
+    // Get initial anon balance
+    let balance = await getBalance(ledgerService, identity.getPrincipal());
+    console.log(balance.toString());
+    
+    // Send ICPs from anon to Hamish
+    await sendTokens(ledgerService, myPrincipal, 100_000_000);
+    
+    // Get new anon balance
+    balance = await getBalance(ledgerService, identity.getPrincipal());
+    console.log(balance.toString());    
+
+    console.log("Leave testCalls");
+}
+
+async function sendTokens(ledgerService: LedgerService, to: Principal, amount: number): Promise<BigNumber> {
+    let result =  await ledgerService.send({
+        to: to,
         fee: TRANSACTION_FEE,
         memo: new BigNumber(0),
-        amount: { doms: new BigNumber(100_000_000) },
+        amount: fromDoms(amount),
         block_height: [],
         from_subaccount: [],
         to_subaccount: []
-    })
+    });   
 
-    const ledgerViewService = walletApi.buildLedgerViewService(
-        icEndpoint,
-        identity);
+    console.log(result);
 
-    console.log("Leave testCalls");
+    return result;
+}
+
+async function getBalance(ledgerService: LedgerService, principal: Principal): Promise<BigNumber> {
+    let result = await ledgerService.account_balance({
+        sub_account: [],
+        account: principal
+    });
+
+    return result.doms;
+}
+
+function fromDoms(amount: number) : ICPTs {
+    return { doms: new BigNumber(amount) }
 }
