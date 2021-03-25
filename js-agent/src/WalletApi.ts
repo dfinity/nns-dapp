025d7eef8ae8cfa8fed436a20851d54c63dd0528
Hamish Peebles <hamishpeebles@gmail.com>
2021-03-25 12:25:42 +0000

    Add governance service to wallet API

--- js-agent/src/WalletApi.ts
+++ js-agent/src/WalletApi.ts
@@ -1,12 +1,16 @@
 import { Identity } from "@dfinity/agent";
+import governanceBuilder from "./canisters/governance/builder";
+import GovernanceService from "./canisters/governance/Service";
 import ledgerBuilder from "./canisters/ledger/builder";
 import LedgerService from "./canisters/ledger/Service";
-import LedgerViewService from "./canisters/ledgerView/Service";
 import ledgerViewBuilder from "./canisters/ledgerView/builder";
+import LedgerViewService from "./canisters/ledgerView/Service";
 import { GetTransactionsRequest, GetTransactionsResponse } from "./canisters/ledgerView/model";
 import { BlockHeight, GetBalanceRequest, ICPTs, SendICPTsRequest } from "./canisters/ledger/model";
+import { ProposalInfo } from "./canisters/governance/model";
 
-export default class LedgerApi {
+export default class WalletApi {
+    private _governanceService: GovernanceService;
     private _ledgerService: LedgerService;
     private _ledgerViewService: LedgerViewService;
 
@@ -17,6 +21,10 @@ export default class LedgerApi {
 
     public async getBalance(request: GetBalanceRequest): Promise<ICPTs> {
         return this._ledgerService.getBalance(request);
+    }
+
+    public async getPendingProposals(): Promise<Array<ProposalInfo>> {
+        return this._governanceService.getPendingProposals();
     }
 
     public async getTransactions(request: GetTransactionsRequest): Promise<GetTransactionsResponse> {
