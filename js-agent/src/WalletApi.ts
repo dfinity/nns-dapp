d05c8b7754d6efbfd9f8b2d42e3789dfd711de88
megrogan <megrogan@gmail.com>
2021-03-26 17:48:35 +0000

    Updated WalletApi with latest methods

--- js-agent/src/WalletApi.ts
+++ js-agent/src/WalletApi.ts
@@ -1,6 +1,7 @@
+import { Option } from "./canisters/option";
 import { AnonymousIdentity, Identity } from "@dfinity/agent";
 import governanceBuilder from "./canisters/governance/builder";
-import GovernanceService, { ManageNeuron, ManageNeuronResponse } from "./canisters/governance/model";
+import GovernanceService, { GetFullNeuronResponse, GetNeuronInfoResponse, ManageNeuron, ManageNeuronResponse } from "./canisters/governance/model";
 import ledgerBuilder from "./canisters/ledger/builder";
 import LedgerService from "./canisters/ledger/model";
 import ledgerViewBuilder from "./canisters/ledgerView/builder";
@@ -39,8 +40,28 @@ export default class WalletApi {
         return this.ledgerService.getBalance(request);
     }
 
+    public async getFullNeuron(neuronId: bigint) : Promise<GetFullNeuronResponse> {
+        return this.governanceService.getFullNeuron(neuronId);
+    }
+
+    public async getNeuronInfo(neuronId: bigint) : Promise<GetNeuronInfoResponse> {
+        return this.governanceService.getNeuronInfo(neuronId);
+    }
+
+    public async getFinalizedProposals() : Promise<Array<ProposalInfo>> {
+        return this.governanceService.getFinalizedProposals();
+    }
+
+    public async getNeuronIds() : Promise<Array<bigint>> {
+        return this.governanceService.getNeuronIds();
+    }
+
     public async getPendingProposals(): Promise<Array<ProposalInfo>> {
         return this.governanceService.getPendingProposals();
+    }
+
+    public async getProposalInfo(proposalId: bigint) : Promise<Option<ProposalInfo>> {
+        return this.governanceService.getProposalInfo(proposalId);
     }
 
     public async getTransactions(request: GetTransactionsRequest): Promise<GetTransactionsResponse> {
