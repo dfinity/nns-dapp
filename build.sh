38a03e177283e837ba8af5a3760db6225ab1bf40
Joachim Breitner <mail@joachim-breitner.de>
2021-05-05 10:47:30 +0200

    Serve assets from a canister that statically includes the assets
    This includes all assets aggressively compressed (and tar’ed) in the
    `.wasm`, and unpacks them on `canister_init`.
    
    Libraries copied from Identity canister.
    
    The spinner is loaded from a different URL. This should be changed to
    point to mainnet once the Identity is live.

--- build.sh
+++ build.sh
@@ -2,10 +2,3 @@
 
 # TODO: add what this does other than "generate wasm"
 ./generate-wasm.sh nns_ui
-
-# build JavaScript agent
-(cd js-agent && ./build.sh)
-
-# build the flutter app
-cd dfinity_wallet
-flutter build web --web-renderer canvaskit --release --no-sound-null-safety
--- dfinity_wallet/lib/loading_overlay.dart
+++ dfinity_wallet/lib/loading_overlay.dart
@@ -107,7 +107,7 @@ class _NodeState extends State<NodeOverlay> with TickerProviderStateMixin {
                     child: SizedBox.expand(
                       child: GifImage(
                         controller: controller,
-                        image: AssetImage("assets/dfinity-glitch.gif"),
+                        image: NetworkImage("https://identity.messaging.dfinity.network/glitch-loop.webp"),
                       ),
                     )))),
         // child: IgnorePointer(child: NodeWorld(oscillationMultiplier: 2,))),
