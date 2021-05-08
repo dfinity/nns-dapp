8247dae208b9a41a1a38b262a69390f82c243ed7
Paul H. Liu <paul@thev.net>
2021-05-08 08:41:56 -0700

    Add nix-shell support and make it determinstic

--- build.sh
+++ build.sh
@@ -5,11 +5,12 @@
 
 # build the flutter app
 cd dfinity_wallet || exit
-flutter build web --web-renderer canvaskit --release --no-sound-null-safety
+flutter build web --web-renderer canvaskit --release --no-sound-null-safety --pwa-strategy=none
+sed -i -e 's/flutter_service_worker.js?v=[0-9]*/flutter_service_worker.js/' build/web/index.html
 
 # Bundle into a tight tarball
 cd build/web/ || exit
-tar cJvf ../../../assets.tar.xz .
+tar cJv --mtime='2021-05-07 17:00+00' --sort=name --exclude .last_build_id -f ../../../assets.tar.xz .
 cd ../../.. || exit
 ls -sh assets.tar.xz
 
