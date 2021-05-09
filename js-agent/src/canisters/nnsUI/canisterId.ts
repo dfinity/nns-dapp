92d446122555f663f10558206672cf56b34050af
Hamish Peebles <hamishpeebles@gmail.com>
2021-05-09 23:01:02 +0100

    Update canisterId on testnet

--- js-agent/src/canisters/nnsUI/canisterId.ts
+++ js-agent/src/canisters/nnsUI/canisterId.ts
@@ -2,6 +2,6 @@ import { Principal } from "@dfinity/agent";
 
 // TODO get this from build arg
 const CANISTER_ID = Principal.fromText("qoctq-giaaa-aaaaa-aaaea-cai");
-// const CANISTER_ID = Principal.fromText("qhbym-qaaaa-aaaaa-aaafq-cai"); // TEST CONFIG
+// const CANISTER_ID = Principal.fromText("qaa6y-5yaaa-aaaaa-aaafa-cai"); // TEST CONFIG
 
 export default CANISTER_ID;
