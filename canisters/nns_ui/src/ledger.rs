a32c19b97ea381654754d8c5cbc44c74afe86916
Hamish Peebles <hamishpeebles@gmail.com>
2021-05-11 14:57:09 +0100

    Implement transaction syncing from archive canisters

--- canisters/nns_ui/src/ledger.rs
+++ canisters/nns_ui/src/ledger.rs
@@ -1,7 +1,8 @@
+use dfn_core::CanisterId;
 use dfn_protobuf::protobuf;
 use ic_nns_constants::LEDGER_CANISTER_ID;
 use ledger_canister::{BlockHeight, EncodedBlock, GetBlocksArgs, GetBlocksRes, TipOfChainRes};
-use ledger_canister::protobuf::TipOfChainRequest;
+use ledger_canister::protobuf::{TipOfChainRequest, ArchiveIndexResponse};
 
 pub async fn tip_of_chain() -> Result<BlockHeight, String> {
     let response: TipOfChainRes = dfn_core::call(
@@ -13,9 +14,19 @@ pub async fn tip_of_chain() -> Result<BlockHeight, String> {
     Ok(response.tip_index)
 }
 
-pub async fn get_blocks(from: BlockHeight, length: u32) -> Result<Vec<EncodedBlock>, String> {
-    let response: GetBlocksRes = dfn_core::call(
+pub async fn get_archive_index() -> Result<ArchiveIndexResponse, String> {
+    let response: ArchiveIndexResponse = dfn_core::call(
         LEDGER_CANISTER_ID,
+        "get_archive_index_pb",
+        protobuf,
+        ()).await.map_err(|e| e.1)?;
+
+    Ok(response)
+}
+
+pub async fn get_blocks(canister_id: CanisterId, from: BlockHeight, length: u32) -> Result<Vec<EncodedBlock>, String> {
+    let response: GetBlocksRes = dfn_core::call(
+        canister_id,
         "get_blocks_pb",
         protobuf,
         GetBlocksArgs::new(from, length as usize)).await.map_err(|e| e.1)?;
