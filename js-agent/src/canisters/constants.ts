f5e1afcdd555e8fad35ba76ed542ede6c5fbd5df
Hamish Peebles <hamishpeebles@gmail.com>
2021-05-17 18:21:58 +0100

    Revert typescript files

--- js-agent/src/canisters/constants.ts
+++ js-agent/src/canisters/constants.ts
@@ -3,5 +3,5 @@ export const CREATE_CANISTER_MEMO = BigInt(0x41455243); // CREA,
 export const TOP_UP_CANISTER_MEMO = BigInt(0x50555054); // TPUP
 
 // TODO get this from build arg
-// export const HOST: string = undefined;
-export const HOST = "https://cdtesting.dfinity.network/"; // TEST CONFIG
+export const HOST: string = undefined;
+// export const HOST = "https://cdtesting.dfinity.network/"; // TEST CONFIG
