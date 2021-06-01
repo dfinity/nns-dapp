4d84e51f92d84c2e75953bd70ff63264d8ed16a5
Islam El-Ashi <ielashi@users.noreply.github.com>
2021-06-01 17:15:31 +0200

    Improve deployment process (#56)
    * Improve deployment process
    
    * Dockerfile
    
    * flutter test

---
+++ update_config.sh
@@ -0,0 +1,29 @@
+set -e
+
+
+if ! [[ $DEPLOY_ENV = "xsmallh" ]] && ! [[ $DEPLOY_ENV = "mainnet" ]]; then
+  echo "Which deployment environment? Set DEPLOY_ENV to 'xsmallh' or 'mainnet'"
+	exit 1
+fi
+
+if [[ $DEPLOY_ENV = "xsmallh" ]]; then
+    # xsmallh config
+    cat > js-agent/src/config.json << EOF
+{
+    "IDENTITY_SERVICE_URL": "https://qaa6y-5yaaa-aaaaa-aaafa-cai.xsmallh.dfinity.network/",
+    "HOST": "https://xsmallh.dfinity.network/",
+    "OWN_CANISTER_ID": "$(dfx canister --no-wallet --network xsmallh id nns_ui)",
+    "FETCH_ROOT_KEY": true
+}
+EOF
+
+else
+    # mainnet config
+    cat > js-agent/src/config.json << EOF
+{
+    "IDENTITY_SERVICE_URL": "https://identity.ic0.app/",
+    "OWN_CANISTER_ID": "qoctq-giaaa-aaaaa-aaaea-cai",
+    "FETCH_ROOT_KEY": false
+}
+EOF
+fi
