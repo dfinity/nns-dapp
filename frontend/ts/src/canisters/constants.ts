925727cf7936fd4a69d4221775c5237c67bc497b
Islam El-Ashi <islam.elashi@dfinity.org>
2021-07-02 10:15:22 +0200

    Fix config

--- frontend/ts/src/canisters/constants.ts
+++ frontend/ts/src/canisters/constants.ts
@@ -3,4 +3,4 @@ export const CREATE_CANISTER_MEMO = BigInt(0x41455243); // CREA,
 export const TOP_UP_CANISTER_MEMO = BigInt(0x50555054); // TPUP
 import config from "../config.json";
 
-export const HOST = config["HOST"];
+export const HOST = ("HOST" in config) ? config["HOST"] : undefined;
