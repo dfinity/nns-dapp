58e0398a085ba270c2e48fcb5d554302a1e308a6
Hamish Peebles <hamishpeebles@gmail.com>
2021-09-01 16:42:21 +0100

    Clean up

--- frontend/ts/src/canisters/getAndRefreshNeurons.ts
+++ frontend/ts/src/canisters/getAndRefreshNeurons.ts
@@ -2,25 +2,27 @@ import GovernanceService, { NeuronInfo } from "./governance/model";
 import LedgerService, { GetBalancesRequest } from "./ledger/model";
 import { NeuronId } from "./common/types";
 import { Option } from "./option";
+import { TRANSACTION_FEE } from "./constants";
 
 export default async function (
     governanceService: GovernanceService,
     ledgerService: LedgerService) : Promise<Array<NeuronInfo>> {
 
-    const neurons = await governanceService.getNeurons();
+    let neurons = await governanceService.getNeurons();
 
     const neuronsToRefresh = await findNeuronsWhichNeedRefresh(neurons, ledgerService);
-    if (!neuronsToRefresh.length) {
-        return neurons;
+
+    if (neuronsToRefresh.length) {
+        const promises: Promise<Option<NeuronId>>[] = neuronsToRefresh.map(n => governanceService.claimOrRefreshNeuron({
+            neuronId: n,
+            by: {NeuronIdOrSubaccount: {}}
+        }));
+        await Promise.all(promises);
+
+        neurons = await governanceService.getNeurons();
     }
 
-    const promises: Promise<Option<NeuronId>>[] = neuronsToRefresh.map(n => governanceService.claimOrRefreshNeuron({
-        neuronId: n,
-        by: { NeuronIdOrSubaccount: {} }
-    }));
-    await Promise.all(promises);
-
-    return await governanceService.getNeurons();
+    return neurons.filter(n => n.fullNeuron && n.fullNeuron.cachedNeuronStake > TRANSACTION_FEE);
 }
 
 const findNeuronsWhichNeedRefresh = async (neurons: Array<NeuronInfo>, ledgerService: LedgerService) : Promise<NeuronId[]> => {
