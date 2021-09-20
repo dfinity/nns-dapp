2141f87c334186dd9fcc29c86cb53203265974c0
nico <10734840+nzoghb@users.noreply.github.com>
2021-09-20 07:50:43 -0700

    NNS1-642-p3: Change temporary logic in post-upgrade hook (#202)
    This is motivated by: https://dfinity.atlassian.net/browse/NNS1-636. It is also a followup to #178 and is meant to be released immediately after.

--- rs/src/accounts_store.rs
+++ rs/src/accounts_store.rs
@@ -4,7 +4,7 @@ use crate::multi_part_transactions_processor::{
     MultiPartTransactionsProcessor,
 };
 use crate::state::StableState;
-use candid::{CandidType, Encode};
+use candid::{CandidType, Decode, Encode};
 use dfn_candid::Candid;
 use ic_base_types::{CanisterId, PrincipalId};
 use ic_certified_map::{leaf_hash, AsHashTree, Hash, HashTree, RbTree};
@@ -1431,16 +1431,16 @@ impl StableState for AccountsStore {
     fn decode(bytes: Vec<u8>) -> Result<Self, String> {
         #[allow(clippy::type_complexity)]
         let (
+            accounts_vec,
             transactions,
-            accounts_old,
             neuron_accounts,
             block_height_synced_up_to,
             multi_part_transactions_processor,
             last_ledger_sync_timestamp_nanos,
             neurons_topped_up_count,
         ): (
+            Vec<(Vec<u8>, Vec<u8>)>,
             VecDeque<Transaction>,
-            Vec<Option<Account>>,
             HashMap<AccountIdentifier, NeuronDetails>,
             Option<BlockHeight>,
             MultiPartTransactionsProcessor,
@@ -1457,38 +1457,46 @@ impl StableState for AccountsStore {
         let mut accounts = RbTree::new();
         let mut hardware_wallets_and_sub_accounts = HashMap::new();
 
-        for a in accounts_old.into_iter().flatten() {
-            a.sub_accounts
-                .iter()
-                .for_each(|(sub_account_identifier, sub_account)| {
-                    hardware_wallets_and_sub_accounts.insert(
-                        sub_account.account_identifier,
-                        AccountWrapper::SubAccount(a.account_identifier, *sub_account_identifier),
-                    );
-                });
-            sub_accounts_count += a.sub_accounts.len() as u64;
+        accounts_vec
+            .iter()
+            .for_each(|(account_identifier, account_bytes)| {
+                let account = Decode!(account_bytes, Account).unwrap();
+                account
+                    .sub_accounts
+                    .iter()
+                    .for_each(|(sub_account_identifier, sub_account)| {
+                        hardware_wallets_and_sub_accounts.insert(
+                            sub_account.account_identifier,
+                            AccountWrapper::SubAccount(
+                                account.account_identifier,
+                                *sub_account_identifier,
+                            ),
+                        );
+                    });
+                sub_accounts_count += account.sub_accounts.len() as u64;
 
-            a.hardware_wallet_accounts
-                .iter()
-                .for_each(|hardware_wallet_account| {
-                    hardware_wallets_and_sub_accounts
-                        .entry(AccountIdentifier::from(hardware_wallet_account.principal))
-                        .and_modify(|account_wrapper| {
-                            if let AccountWrapper::HardwareWallet(account_identifiers) =
-                                account_wrapper
-                            {
-                                account_identifiers.push(a.account_identifier);
-                            }
-                        })
-                        .or_insert_with(|| {
-                            AccountWrapper::HardwareWallet(vec![a.account_identifier])
-                        });
-                });
-            hardware_wallet_accounts_count += a.hardware_wallet_accounts.len() as u64;
+                account
+                    .hardware_wallet_accounts
+                    .iter()
+                    .for_each(|hardware_wallet_account| {
+                        hardware_wallets_and_sub_accounts
+                            .entry(AccountIdentifier::from(hardware_wallet_account.principal))
+                            .and_modify(|account_wrapper| {
+                                if let AccountWrapper::HardwareWallet(account_identifiers) =
+                                    account_wrapper
+                                {
+                                    account_identifiers.push(account.account_identifier);
+                                }
+                            })
+                            .or_insert_with(|| {
+                                AccountWrapper::HardwareWallet(vec![account.account_identifier])
+                            });
+                    });
+                hardware_wallet_accounts_count += account.hardware_wallet_accounts.len() as u64;
 
-            accounts.insert(a.account_identifier.to_vec(), a);
-            accounts_count += 1;
-        }
+                accounts.insert(account_identifier.to_vec(), account);
+                accounts_count += 1;
+            });
 
         Ok(AccountsStore {
             accounts,
