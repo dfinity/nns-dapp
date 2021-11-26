3472a959e69e809339edc40cff757c5d2d8f6027
Islam El-Ashi <islam.elashi@dfinity.org>
2021-11-26 09:52:45 +0200

    EXC-704: Switch to HTML renderer.

--- build.sh
+++ build.sh
@@ -15,10 +15,10 @@ set -x
 # build the flutter app
 cd frontend/dart || exit
 if [[ $DEPLOY_ENV = "mainnet" ]]; then
-  flutter build web --web-renderer auto --release --no-sound-null-safety --pwa-strategy=none --dart-define=FLUTTER_WEB_CANVASKIT_URL=/assets/canvaskit/
+  flutter build web --web-renderer html --release --no-sound-null-safety --pwa-strategy=none --dart-define=FLUTTER_WEB_CANVASKIT_URL=/assets/canvaskit/
 else
   # For all networks that are not main net, build with the staging config
-  flutter build web --web-renderer auto --release --no-sound-null-safety --pwa-strategy=none --dart-define=DEPLOY_ENV=staging
+  flutter build web --web-renderer html --release --no-sound-null-safety --pwa-strategy=none --dart-define=DEPLOY_ENV=staging
 fi
 sed -i -e 's/flutter_service_worker.js?v=[0-9]*/flutter_service_worker.js/' build/web/index.html
 
