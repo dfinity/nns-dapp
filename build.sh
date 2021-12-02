a000601934413fbe683e5621c819f96dc42421cc
Max <max@dfinity.org>
2021-12-02 15:21:09 +0100

    Add the svelte app to the canister

--- build.sh
+++ build.sh
@@ -9,32 +9,47 @@ fi
 
 set -x
 
+# Prepare a clean new home for the assets
+rm -fr web-assets assets.tar.xz
+
 # build typescript code
 (cd frontend/ts && ./build.sh)
 
 # build the flutter app
-cd frontend/dart || exit
-if [[ $DEPLOY_ENV = "mainnet" ]]; then
-  flutter build web --web-renderer html --release --no-sound-null-safety --pwa-strategy=none --dart-define=FLUTTER_WEB_CANVASKIT_URL=/assets/canvaskit/
-else
-  # For all networks that are not main net, build with the staging config
-  flutter build web --web-renderer html --release --no-sound-null-safety --pwa-strategy=none --dart-define=DEPLOY_ENV=staging
-fi
-sed -i -e 's/flutter_service_worker.js?v=[0-9]*/flutter_service_worker.js/' build/web/index.html
+pushd frontend/dart || exit
+  if [[ $DEPLOY_ENV = "mainnet" ]]; then
+    flutter build web --web-renderer html --release --no-sound-null-safety --pwa-strategy=none --dart-define=FLUTTER_WEB_CANVASKIT_URL=/assets/canvaskit/
+  else
+    # For all networks that are not main net, build with the staging config
+    flutter build web --web-renderer html --release --no-sound-null-safety --pwa-strategy=none --dart-define=DEPLOY_ENV=staging
+  fi
+  sed -i -e 's/flutter_service_worker.js?v=[0-9]*/flutter_service_worker.js/' build/web/index.html
+popd || exit
+cp -R frontend/dart/build/web/ web-assets
+
+# Build the svelte app
+SVELTE_APP_DIR=frontend/svelte
+pushd "$SVELTE_APP_DIR" || exit
+  npm run "build:${DEPLOY_ENV}"
+popd || exit
+
+rm -fr web-assets/v2 # There should not be anything here but just in case...
+cp -R "$SVELTE_APP_DIR"/dist web-assets/v2
 
 # Bundle into a tight tarball
 # On macOS you need to install gtar + xz
 # brew install gnu-tar
 # brew install xz
 
-cd build/web/ || exit
-# Remove the assets/NOTICES file, as it's large in size and not used.
-rm assets/NOTICES
-tar cJv --mtime='2021-05-07 17:00+00' --sort=name --exclude .last_build_id -f ../../../../assets.tar.xz . || \
-gtar cJv --mtime='2021-05-07 17:00+00' --sort=name --exclude .last_build_id -f ../../../../assets.tar.xz .
-cd ../../../.. || exit
-ls -sh assets.tar.xz
-sha256sum assets.tar.xz
+pushd web-assets || exit
+  # Remove the assets/NOTICES file, as it's large in size and not used.
+  rm assets/NOTICES
+  tar cJv --mtime='2021-05-07 17:00+00' --sort=name --exclude .last_build_id -f ../assets.tar.xz . || \
+  gtar cJv --mtime='2021-05-07 17:00+00' --sort=name --exclude .last_build_id -f ../assets.tar.xz .
+  cd ../../../.. || exit
+  ls -sh assets.tar.xz
+  sha256sum assets.tar.xz
+popd || exit
 
 echo Compiling rust package
 if [[ $DEPLOY_ENV = "mainnet" ]]; then
