b4a9dfabe220ebb4e6fd255f85ecd8988be0a21b
Max <bitdivine@users.noreply.github.com>
2021-12-02 14:26:52 +0100

    Svelte nns (#274)
    # Motivation
    The svelte app will require the nns-js library.  Making use of it requires some wider changes, so those are best made early.
    
    # Changes
    - Add nns-js library
    - Make the formatter ignore build artefacts
    
    # Tests
    That the nns-js library loads correctly can be demonstrated by:
    ```
    npm ci
    npm run dev
    ```
    then point a browser at the port advertised by npm (typically `http://localhost:3000`) and look in the console.  You should see no errors and:
    ```
    Account Identifier
    main.ts:19 a2a794c66495083317e4be5197eb655b1e63015469d769e2338af3d3e3f3aa86
    ```

--- frontend/svelte/src/main.ts
+++ frontend/svelte/src/main.ts
@@ -1,4 +1,5 @@
 import App from "./App.svelte";
+import { AccountIdentifier } from "@dfinity/nns";
 
 const app = new App({
   target: document.body,
@@ -8,3 +9,11 @@ const app = new App({
 });
 
 export default app;
+
+// Test code to demonstrate nns-js:
+const accountIdentifier = AccountIdentifier.fromHex(
+  "a2a794c66495083317e4be5197eb655b1e63015469d769e2338af3d3e3f3aa86"
+);
+
+console.log("Account Identifier");
+console.log(accountIdentifier.toHex());
