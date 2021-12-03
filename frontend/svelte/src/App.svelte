50c4d1a7772fa83b203b00fdaab82e30da33d730
Max <max@dfinity.org>
2021-12-03 19:18:56 +0100

    Remove the default svelte content

--- frontend/svelte/src/App.svelte
+++ frontend/svelte/src/App.svelte
@@ -1,28 +1,31 @@
 <script lang="ts">
-  export let name: string;
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
 </main>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
+  /* Common parameters used throughout the app */
+  :root {
+    --widget-border: 25px;
+    --widget-border-radius: 25px;
+    --widget-border-radius-small: 10px;
+    --widget-grey: #282a2d;
+    --text-grey: #aeb7b7;
+    --background-grey: #383c3c;
+    --button-blue: #005fb7;
   }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    background-color: black;
+  }
+
+  main {
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(var(--background-grey) 80%, black);
   }
 
   @media (min-width: 640px) {
