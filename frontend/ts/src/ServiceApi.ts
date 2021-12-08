4364dbefea11831724195b5fdc2fe006e52b2ad3
Formatting Committer <nobody@example.com>
2021-12-08 17:03:48 +0000

    Updating svelte frontend formatting

--- frontend/ts/src/ServiceApi.ts
+++ frontend/ts/src/ServiceApi.ts
@@ -490,10 +490,12 @@ export default class ServiceApi {
     // The identity's principal is jg6qm-uw64t-m6ppo-oluwn-ogr5j-dc5pm-lgy2p-eh6px-hebcd-5v73i-nqe
     // The identity's default ledger address is 5b315d2f6702cb3a27d826161797d7b2c2e131cd312aece51d4d5574d1247087
     const publicKey = "Uu8wv55BKmk9ZErr6OIt5XR1kpEGXcOSOC1OYzrAwuk=";
-    const privateKey =  "N3HB8Hh2PrWqhWH2Qqgr1vbU9T3gb1zgdBD8ZOdlQnVS7zC/nkEqaT1kSuvo4i3ldHWSkQZdw5I4LU5jOsDC6Q==";
+    const privateKey =
+      "N3HB8Hh2PrWqhWH2Qqgr1vbU9T3gb1zgdBD8ZOdlQnVS7zC/nkEqaT1kSuvo4i3ldHWSkQZdw5I4LU5jOsDC6Q==";
     const identity = Ed25519KeyIdentity.fromKeyPair(
-        blobFromUint8Array(base64ToUInt8Array(publicKey)),
-        blobFromUint8Array(base64ToUInt8Array(privateKey)));
+      blobFromUint8Array(base64ToUInt8Array(publicKey)),
+      blobFromUint8Array(base64ToUInt8Array(privateKey))
+    );
 
     const agent = new HttpAgent({
       host: HOST,
--- frontend/ts/src/utils.ts
+++ frontend/ts/src/utils.ts
@@ -8,5 +8,5 @@ export class UnsupportedValueError extends Error {
 }
 
 export function base64ToUInt8Array(base64String: string): Uint8Array {
-  return Uint8Array.from(atob(base64String), c => c.charCodeAt(0));
+  return Uint8Array.from(atob(base64String), (c) => c.charCodeAt(0));
 }
