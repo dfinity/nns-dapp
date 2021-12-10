8fdbaa1592c522e17287aa4a5ecd7de75a99bd0c
Max <max@dfinity.org>
2021-12-10 15:23:06 +0100

    One interval only

--- frontend/svelte/src/AuthSync.ts
+++ frontend/svelte/src/AuthSync.ts
@@ -5,16 +5,14 @@ export class AuthSync {
   broadcastChannel;
   static SIGN_OUT = "signOut";
   static SIGN_IN = "signIn";
-  static FAST_INTERVAL = 10000;
-  static SLOW_INTERVAL = 60000;
+  static INTERVAL = 10000;
 
   constructor(callback: Function) {
     if (typeof BroadcastChannel === "undefined") {
       // Safari, IE.  (Safari is expected to have BroadcastChannels soon.)
-      setInterval(callback, AuthSync.FAST_INTERVAL);
+      setInterval(callback, AuthSync.INTERVAL);
     } else {
       // Chrome, Firefox, Edge.
-      setInterval(callback, AuthSync.SLOW_INTERVAL);
       this.broadcastChannel = new BroadcastChannel("nns-auth");
       this.broadcastChannel.onmessage = function (event) {
         callback();
