7580692375cd36aae8b46abb41ad6894754d18f3
Max <max@dfinity.org>
2021-12-10 11:12:21 +0100

    Add svelte to canister

--- build.sh
+++ build.sh
@@ -13,26 +13,40 @@ set -x
 (cd frontend/ts && ./build.sh)
 
 # build the flutter app
-cd frontend/dart || exit
-if [[ $DEPLOY_ENV = "mainnet" ]]; then
-  flutter build web --web-renderer html --release --no-sound-null-safety --pwa-strategy=none --dart-define=FLUTTER_WEB_CANVASKIT_URL=/assets/canvaskit/
-else
-  # For all networks that are not main net, build with the staging config
-  flutter build web --web-renderer html --release --no-sound-null-safety --pwa-strategy=none --dart-define=DEPLOY_ENV=staging
-fi
-sed -i -e 's/flutter_service_worker.js?v=[0-9]*/flutter_service_worker.js/' build/web/index.html
+pushd frontend/dart || exit
+  if [[ $DEPLOY_ENV = "mainnet" ]]; then
+    flutter build web --web-renderer html --release --no-sound-null-safety --pwa-strategy=none --dart-define=FLUTTER_WEB_CANVASKIT_URL=/assets/canvaskit/
+  else
+    # For all networks that are not main net, build with the staging config
+    flutter build web --web-renderer html --release --no-sound-null-safety --pwa-strategy=none --dart-define=DEPLOY_ENV=staging
+  fi
+  sed -i -e 's/flutter_service_worker.js?v=[0-9]*/flutter_service_worker.js/' build/web/index.html
+popd || exit
+
+rm -f assets.tar.xz
+rm -fr web-assets
+cp -R frontend/dart/build/web/ web-assets
+
+# Build the svelte app
+pushd frontend/svelte || exit
+  npm ci
+  npm run build
+popd || exit
+
+rm -fr web-assets/v2
+cp -R frontend/svelte/public web-assets/v2
 
 # Bundle into a tight tarball
 # On macOS you need to install gtar + xz
 # brew install gnu-tar
 # brew install xz
+pushd web-assets || exit
+  # Remove the assets/NOTICES file, as it's large in size and not used.
+  rm assets/NOTICES
+  tar cJv --mtime='2021-05-07 17:00+00' --sort=name --exclude .last_build_id -f ../assets.tar.xz . || \
+  gtar cJv --mtime='2021-05-07 17:00+00' --sort=name --exclude .last_build_id -f ../assets.tar.xz .
+popd || exit
 
-cd build/web/ || exit
-# Remove the assets/NOTICES file, as it's large in size and not used.
-rm assets/NOTICES
-tar cJv --mtime='2021-05-07 17:00+00' --sort=name --exclude .last_build_id -f ../../../../assets.tar.xz . ||
-  gtar cJv --mtime='2021-05-07 17:00+00' --sort=name --exclude .last_build_id -f ../../../../assets.tar.xz .
-cd ../../../.. || exit
 ls -sh assets.tar.xz
 sha256sum assets.tar.xz
 
