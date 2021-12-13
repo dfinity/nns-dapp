be4691879cc69a6e9a7e3ccb4768dc7cd30d4752
Max <max@dfinity.org>
2021-12-13 12:33:21 +0100

    Drop broadcast

--- frontend/svelte/src/AuthSync.ts
+++ frontend/svelte/src/AuthSync.ts
@@ -7,7 +7,6 @@ export class AuthSync {
   static NAME = "AuthSync";
   static SIGN_OUT = "signOut";
   static SIGN_IN = "signIn";
-  static INTERVAL = 10000;
 
   /**
    * Creates a login sync.
@@ -15,44 +14,39 @@ export class AuthSync {
    * @param {Function} callback - invoked whenever login status may have changed in another tab.
    */
   constructor(callback: Function) {
-    if (typeof BroadcastChannel === "undefined") {
-      // Safari, IE.  (Safari is expected to have BroadcastChannels soon.)
-      window.addEventListener('storage', () => {
-        let currentStoredState = window.localStorage.getItem(AuthSync.NAME);
-        if (currentStoredState !== this.lastStoredState) {
-            this.lastStoredState = currentStoredState;
-            callback();
-        }
-      });
-      setInterval(callback, AuthSync.INTERVAL);
-    } else {
-      // Chrome, Firefox, Edge.
-      this.broadcastChannel = new BroadcastChannel(AuthSync.NAME);
-      this.broadcastChannel.onmessage = function (event) {
+    // Use localStorage rather than BroadcastChannel because of Safari and IE.  (Safari is expected to have BroadcastChannels soon.)
+    window.addEventListener("storage", () => {
+      let currentStoredState = window.localStorage.getItem(AuthSync.NAME);
+      if (currentStoredState !== this.lastStoredState) {
+        this.lastStoredState = currentStoredState;
         callback();
-      };
-    }
+      }
+    });
   }
 
   /**
    * Communicates to other tabs that we have signed in.
    */
   onSignIn() {
-    if (undefined === this.broadcastChannel) {
-      localStorage.setItem(AuthSync.NAME, AuthSync.SIGN_IN);
-    } else {
-      this.broadcastChannel.postMessage(AuthSync.SIGN_IN);
-    }
+    this.#onChange(AuthSync.SIGN_IN);
   }
 
   /**
    * Communicates to other tabs that we have signed out.
    */
   onSignOut() {
-    if (undefined === this.broadcastChannel) {
-      localStorage.setItem(AuthSync.NAME, AuthSync.SIGN_OUT);
-    } else {
-      this.broadcastChannel.postMessage(AuthSync.SIGN_OUT);
-    }
+    this.#onChange(AuthSync.SIGN_OUT);
+  }
+
+  /**
+   * Communicates to other tabs that the login state has changed.
+   *
+   * @param {string} action - login or logout action.
+   */
+  #onChange(action) {
+    localStorage.setItem(AuthSync.NAME, JSON.stringify({
+      action,
+      timestamp: Date.now(),
+    }));
   }
 }
