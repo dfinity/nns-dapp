5c8092a2f663114be24efbb060fc499edca0af98
anishh2003 <58399007+anishh2003@users.noreply.github.com>
2021-12-14 22:48:16 +0000

    basic home page navigation works now (#299)

--- frontend/dart/lib/main.dart
+++ frontend/dart/lib/main.dart
@@ -51,25 +51,21 @@ class NnsDapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HiveBoxesWidget(
-        boxes: hiveBoxes,
-        child: ICApiManager(
-          child: Builder(builder: (context) {
-            return MaterialApp.router(
-              builder: (context, child) {
-                return Theme(
-                  data: getTheme(Responsive.isMobile(context)),
-                  child: child!
-                );
-              },
-              debugShowCheckedModeBanner: false,
-              routerDelegate: router,
-              routeInformationParser: WalletRouteParser(hiveBoxes, context),
-              title: 'Network Nervous System',
-            );
-          }),
-        ),
+    return HiveBoxesWidget(
+      boxes: hiveBoxes,
+      child: ICApiManager(
+        child: Builder(builder: (context) {
+          return MaterialApp.router(
+            builder: (context, child) {
+              return Theme(
+                  data: getTheme(Responsive.isMobile(context)), child: child!);
+            },
+            debugShowCheckedModeBanner: false,
+            routerDelegate: router,
+            routeInformationParser: WalletRouteParser(hiveBoxes, context),
+            title: 'Network Nervous System',
+          );
+        }),
       ),
     );
   }
@@ -91,31 +87,30 @@ class NnsDapp extends StatelessWidget {
       }),
       textTheme: nnsDappTextTheme(isMobile),
       elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ButtonStyle(
-          shape: MaterialStateProperty.all(RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10))),
-          backgroundColor: MaterialStateProperty.resolveWith((states) {
-            if (states.contains(MaterialState.disabled)) {
-              return AppColors.gray400;
-            } else {
-              return AppColors.blue600;
-            }
-          }),
-        )),
+          style: ButtonStyle(
+        shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+        backgroundColor: MaterialStateProperty.resolveWith((states) {
+          if (states.contains(MaterialState.disabled)) {
+            return AppColors.gray400;
+          } else {
+            return AppColors.blue600;
+          }
+        }),
+      )),
       textButtonTheme: TextButtonThemeData(
-        style: ButtonStyle(
-          textStyle: MaterialStateProperty.all(
-            TextStyle(color: Colors.white)),
-          overlayColor: MaterialStateProperty.all(
-            AppColors.gray400.withOpacity(0.04)))),
+          style: ButtonStyle(
+              textStyle:
+                  MaterialStateProperty.all(TextStyle(color: Colors.white)),
+              overlayColor: MaterialStateProperty.all(
+                  AppColors.gray400.withOpacity(0.04)))),
       cardTheme: CardTheme(
-        color: AppColors.background,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20)),
-        shadowColor: Colors.white.withOpacity(0.3),
-        elevation: 7,
-        margin: EdgeInsets.only(
-          left: 20, right: 20, top: 20, bottom: 0)),
+          color: AppColors.background,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          shadowColor: Colors.white.withOpacity(0.3),
+          elevation: 7,
+          margin: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 0)),
     );
   }
 }
