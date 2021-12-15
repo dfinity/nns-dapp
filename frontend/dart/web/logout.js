4df459c90b0dcdbadad8d856fe8583157a29b81d
Max <max@dfinity.org>
2021-12-15 03:35:14 +0100

    rm console log

--- frontend/dart/web/logout.js
+++ frontend/dart/web/logout.js
@@ -2,19 +2,16 @@
 (() => {
     let loggingOut = false; // Debounce to protect against multiple simultaneous reload calls interfering with each other.
     let checkLoginStatus = () => {
-      console.log("Checking login status");
       let currentStoredState = window.localStorage.getItem("AuthSync");
       try {
          const {action} = JSON.parse(currentStoredState);
          if ((action !== "signIn") && (!loggingOut)) {
              loggingOut = true;
              window.location.assign('/v2/');
-         } else {
-             console.log("Is logged in");
          }
       } catch(e) {
-         console.info(e);
          // Login status was not present and well formed.
+         console.info(e);
          window.location.assign('/v2/');
       }
     }
