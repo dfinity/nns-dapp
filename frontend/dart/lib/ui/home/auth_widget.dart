0e9b6e08ae0b2c583d118c31341226cd6d1d8f94
Max <max@dfinity.org>
2021-12-23 13:11:00 +0100

    Revert "Revert "Revert "Svelte login cross link (#298)" (#304)" (#312)"
    This reverts commit d6e2e7f64f197045c46dbc83988e7f92439940b5.

--- frontend/dart/lib/ui/home/auth_widget.dart
+++ frontend/dart/lib/ui/home/auth_widget.dart
@@ -1,16 +1,59 @@
+import 'package:nns_dapp/ui/_components/custom_auto_size.dart';
+
 import '../../nns_dapp.dart';
 
 class AuthWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // If we were about to render the flutter login page, call logout() which navigates to /v2 when finished.
-    // Note: Navigating directly to v2 here is unreliable as logout may not have completed yet.
-    context.icApi.logout();
     return Container(
       constraints: BoxConstraints.expand(),
       decoration: BoxDecoration(
         image: DecorationImage(
             image: AssetImage('assets/nns_background.jpeg'), fit: BoxFit.cover),
+      ),
+      child: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.1),
+            child: Center(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 500),
+                child: Column(
+                  children: [
+                    AutoSizeText(
+                      'The Internet Computer',
+                      style: context.textTheme.headline1,
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 20),
+                    AutoSizeText(
+                      'Network Nervous System',
+                      style: context.textTheme.headline2,
+                      textAlign: TextAlign.center,
+                    ),
+                    Expanded(child: Container()),
+                    ElevatedButton(
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Text(
+                            "LOGIN",
+                            style: context.textTheme.headline3,
+                          ),
+                        ),
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(AppColors.blue800)),
+                        onPressed: () {
+                          context.icApi.authenticate(() {
+                            context.nav.push(accountsTabPage);
+                          });
+                        }),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
