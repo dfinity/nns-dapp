3c9665c23f347681b5c68800deb7f9041bc79e24
Anish <anishh2003@gmail.com>
2021-12-23 15:27:54 +0000

    new design implemented for login page

--- frontend/dart/lib/ui/home/auth_widget.dart
+++ frontend/dart/lib/ui/home/auth_widget.dart
@@ -1,16 +1,101 @@
+import 'package:nns_dapp/ui/_components/custom_auto_size.dart';
+
 import '../../nns_dapp.dart';
 
 class AuthWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // If we were about to render the flutter login page, call logout() which navigates to /v2 when finished.
-    // Note: Navigating directly to v2 here is unreliable as logout may not have completed yet.
-    context.icApi.logout();
     return Container(
       constraints: BoxConstraints.expand(),
       decoration: BoxDecoration(
         image: DecorationImage(
             image: AssetImage('assets/nns_background.jpeg'), fit: BoxFit.cover),
+      ),
+      child: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.1),
+            child: Center(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 500),
+                child: Column(
+                  children: [
+                    AutoSizeText(
+                      'The Internet Computer',
+                      style: context.textTheme.headline1,
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'NETWORK',
+                          style: context.textTheme.headline2!.copyWith(
+                            color: Color(0x2ca8df),
+                          ),
+                        ),
+                        Text(' . '),
+                        Text(
+                          'NERVOUS',
+                          style: context.textTheme.headline2!.copyWith(
+                            color: Color(0xd81c6f),
+                          ),
+                        ),
+                        Text(' . '),
+                        Text(
+                          'SYSTEM',
+                          style: context.textTheme.headline2!.copyWith(
+                            color: Color(0x859d44),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    SvgPicture.asset("assets/ic_colour_logo.svg"),
+                    Expanded(child: Container()),
+                    Row(
+                      children: [
+                        Text(
+                          'ICP',
+                          style: TextStyle(
+                            color: Color(0x859d44),
+                          ),
+                        ),
+                        Text(
+                          'and',
+                        ),
+                        Text(
+                          'governance',
+                          style: TextStyle(
+                            color: Color(0x2ca8df),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Text(
+                            "LOGIN",
+                            style: context.textTheme.headline3,
+                          ),
+                        ),
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(AppColors.blue800)),
+                        onPressed: () {
+                          context.icApi.authenticate(() {
+                            context.nav.push(accountsTabPage);
+                          });
+                        }),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
