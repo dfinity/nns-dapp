a7e92f388bc183803be942a10866f3d95399e4f0
Daniel.Bloom <daniel.bloom@dfinity.org>
2022-01-03 14:20:04 -0800

    use `secp256k1.ts` from `agent-js`

--- frontend/ts/src/ledger/secp256k1.ts
+++ frontend/ts/src/ledger/secp256k1.ts
@@ -1,4 +1,19 @@
-import { PublicKey, DerEncodedPublicKey } from "@dfinity/agent";
+import { DerEncodedPublicKey, PublicKey } from '@dfinity/agent';
+
+function equals(b1: ArrayBuffer, b2: ArrayBuffer): boolean {
+  if (b1.byteLength !== b2.byteLength) {
+    return false;
+  }
+
+  const u1 = new Uint8Array(b1);
+  const u2 = new Uint8Array(b2);
+  for (let i = 0; i < u1.length; i++) {
+    if (u1[i] !== u2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 // This implementation is adjusted from the Ed25519PublicKey.
 // The RAW_KEY_LENGTH and DER_PREFIX are modified accordingly
@@ -29,7 +44,7 @@ export class Secp256k1PublicKey implements PublicKey {
     if (publicKey.byteLength !== Secp256k1PublicKey.RAW_KEY_LENGTH) {
       const bl = publicKey.byteLength;
       throw new TypeError(
-        `secp256k1 public key must be ${Secp256k1PublicKey.RAW_KEY_LENGTH} bytes long (is ${bl})`
+        `secp256k1 public key must be ${Secp256k1PublicKey.RAW_KEY_LENGTH} bytes long (is ${bl})`,
       );
     }
 
@@ -42,25 +57,19 @@ export class Secp256k1PublicKey implements PublicKey {
   }
 
   private static derDecode(key: DerEncodedPublicKey): ArrayBuffer {
-    const expectedLength =
-      Secp256k1PublicKey.DER_PREFIX.length + Secp256k1PublicKey.RAW_KEY_LENGTH;
+    const expectedLength = Secp256k1PublicKey.DER_PREFIX.length + Secp256k1PublicKey.RAW_KEY_LENGTH;
     if (key.byteLength !== expectedLength) {
       const bl = key.byteLength;
       throw new TypeError(
-        `secp256k1 DER-encoded public key must be ${expectedLength} bytes long (is ${bl})`
+        `secp256k1 DER-encoded public key must be ${expectedLength} bytes long (is ${bl})`,
       );
     }
 
-    ArrayBuffer
-
-    const rawKey = key.slice(Secp256k1PublicKey.DER_PREFIX.length);
-
-    const areEqual = (first: Uint8Array, second: Uint8Array) =>
-      first.length === second.length && first.every((value, index) => value === second[index])
-    if (!areEqual(new Uint8Array(this.derEncode(rawKey)), new Uint8Array(key))) {
+    const rawKey = key.slice(0, Secp256k1PublicKey.DER_PREFIX.length);
+    if (!equals(this.derEncode(rawKey), key)) {
       throw new TypeError(
-        "secp256k1 DER-encoded public key is invalid. A valid secp256k1 DER-encoded public key " +
-          `must have the following prefix: ${Secp256k1PublicKey.DER_PREFIX}`
+        'secp256k1 DER-encoded public key is invalid. A valid secp256k1 DER-encoded public key ' +
+          `must have the following prefix: ${Secp256k1PublicKey.DER_PREFIX}`,
       );
     }
 
