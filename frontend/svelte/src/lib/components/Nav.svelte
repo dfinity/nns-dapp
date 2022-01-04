3aea92acad2a4648e97a3b9b4738d6d382209284
peterpeterparker <david.dalbusco@outlook.com>
2022-01-04 13:26:24 +0100

    feat: active route display

--- frontend/svelte/src/lib/components/Nav.svelte
+++ frontend/svelte/src/lib/components/Nav.svelte
@@ -1,10 +1,20 @@
+<script lang="ts">
+    import {routePath} from '../utils/route.utils';
 
+    let currentPath: string = routePath();
+
+    const routes: {path: string, label: string}[] = [
+        {path: '/#/icp', label: 'ICP'},
+        {path: '/#/neurons', label: 'NEURONS'},
+        {path: '/#/voting', label: 'VOTING'},
+        {path: '/#/canisters', label: '</a>'}
+    ];
+</script>
 
 <nav>
-    <a href="/#/icp">ICP</a>
-    <a href="/#/neurons">NEURONS</a>
-    <a href="/#/voting">VOTING</a>
-    <a href="/#/canisters">CANISTERS</a>
+    {#each routes as { path, label }}
+        <a href={path} class:active={currentPath === path}>{label}</a>
+    {/each}
 </nav>
 
 <style lang="scss">
@@ -23,5 +33,10 @@
 
     a {
       height: calc(100% - 2 * var(--padding));
+    }
+
+    .active {
+      background: var(--blue);
+      color: var(--blue-contrast);
     }
 </style>
