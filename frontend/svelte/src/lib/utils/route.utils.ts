f34037dd2ee220bd969c4f943d6f1038d4cb44ac
peterpeterparker <david.dalbusco@outlook.com>
2022-01-04 11:51:34 +0100

    fix: lower case hash

--- frontend/svelte/src/lib/utils/route.utils.ts
+++ frontend/svelte/src/lib/utils/route.utils.ts
@@ -1,3 +1,3 @@
 // TODO: do we want to use a hash based routing?
 // Get the current route from the location hash to lower case prefixed with root. Example: #/ICP => /icp
-export const routePath = (): string => `/${window.location.hash}`;
+export const routePath = (): string => `/${window.location.hash.toLowerCase()}`;
