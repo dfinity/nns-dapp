0da001d97d8542087ed7b456b7a95106ad295a4a
David Dal Busco <david.dalbusco@dfinity.org>
2022-01-04 14:12:54 +0100

    feat: sync auth on storage change

--- frontend/svelte/src/lib/components/Guard.svelte
+++ frontend/svelte/src/lib/components/Guard.svelte
@@ -2,6 +2,8 @@
     import {authStore} from "../stores/auth.store";
 </script>
 
+<svelte:window on:storage={async () => await authStore.init()}/>
+
 {#await authStore.init()}
     <!-- TODO: display a spinner or other animation while loading the auth? -->
 {:then value}
