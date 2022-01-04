40694059fc11a2c1b7b42411ba8ad67352edc0a2
Formatting Committer <nobody@example.com>
2022-01-04 06:39:17 +0000

    Updating svelte frontend formatting

--- frontend/svelte/.eslintrc.cjs
+++ frontend/svelte/.eslintrc.cjs
@@ -15,7 +15,7 @@ module.exports = {
   parserOptions: {
     sourceType: "module",
     ecmaVersion: 2020,
-    project: ["./tsconfig.json"]
+    project: ["./tsconfig.json"],
   },
   env: {
     browser: true,
