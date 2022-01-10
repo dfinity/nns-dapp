6b04d6241d735ee0bd23bbf3bf932f8e83217801
David Dal Busco <david.dalbusco@outlook.com>
2022-01-10 17:44:45 +0100

    docs: add todo references (#339)
    # Motivation
    
    Keep track of recent TODOs
    
    # Changes
    
    - Jira references added

--- frontend/svelte/src/routes/Auth.svelte
+++ frontend/svelte/src/routes/Auth.svelte
@@ -11,7 +11,7 @@
     try {
       await authStore.signIn();
     } catch (err) {
-      // TODO: we display the errors?
+      // TODO(L2-176): display the errors
       console.error(err);
     }
   };
