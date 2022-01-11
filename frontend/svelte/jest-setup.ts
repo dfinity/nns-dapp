4b85237a4e55668e0b3053edada239fb3f01ebd1
David Dal Busco <david.dalbusco@dfinity.org>
2022-01-11 15:30:11 +0100

    docs: polyfill text encoder

--- frontend/svelte/jest-setup.ts
+++ frontend/svelte/jest-setup.ts
@@ -1,5 +1,7 @@
 import "@testing-library/jest-dom";
 
+// jsdom does not implement TextEncoder
+// Polyfill the encoders with node
 import { TextEncoder, TextDecoder } from "util";
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
