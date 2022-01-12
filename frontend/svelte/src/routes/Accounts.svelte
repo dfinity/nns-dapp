154041d34e06333aa083b4088b71f18def892d34
David Dal Busco <david.dalbusco@dfinity.org>
2022-01-12 12:15:51 +0100

    feat: account balance

--- frontend/svelte/src/routes/Accounts.svelte
+++ frontend/svelte/src/routes/Accounts.svelte
@@ -1,12 +1,41 @@
 <script lang="ts">
   import Layout from "../lib/components/Layout.svelte";
-  import { onMount } from "svelte";
+  import { onDestroy, onMount } from "svelte";
+  import type { Unsubscriber } from "svelte/types/runtime/store";
+  import { AuthStore, authStore } from "../lib/stores/auth.store";
+  import { AccountIdentifier, ICP, LedgerCanister } from "@dfinity/nns";
+  import { E8S_PER_ICP } from "../lib/contants/icp.constants";
 
   onMount(() => {
     if (process.env.REDIRECT_TO_LEGACY) {
       window.location.replace("/#/accounts");
     }
   });
+
+  const unsubscribe: Unsubscriber = authStore.subscribe(
+    async ({ principal }: AuthStore) => {
+      const ledger: LedgerCanister = LedgerCanister.create();
+
+      const balance: ICP = await ledger.accountBalance(
+        AccountIdentifier.fromPrincipal(principal),
+        false
+      );
+
+      // TODO:
+      // - extract ledger code
+      // - extract number format
+      // - add local state
+
+      console.log(
+        `Balance: ${new Intl.NumberFormat("fr-FR", {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 8,
+        }).format(Number(balance.toE8s()) / E8S_PER_ICP).replace(',', '.')}`
+      );
+    }
+  );
+
+  onDestroy(unsubscribe);
 </script>
 
 {#if !process.env.REDIRECT_TO_LEGACY}
