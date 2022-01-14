6279dae05136b931655b42dc057da6716c49f8f3
Formatting Committer <nobody@example.com>
2022-01-14 10:50:51 +0000

    Updating svelte frontend formatting

--- rs/src/assets.rs
+++ rs/src/assets.rs
@@ -148,7 +148,7 @@ pub fn http_request(req: HttpRequest) -> HttpResponse {
 
 fn content_type_of(request_path: &str) -> Option<&'static str> {
     if request_path.ends_with('/') {
-        return  Some("text/html");
+        return Some("text/html");
     }
     request_path
         .split('.')
