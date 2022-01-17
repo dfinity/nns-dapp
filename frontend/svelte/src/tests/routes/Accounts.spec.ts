0cb31fe435e242a12991090c1666eef0dd7d781f
David Dal Busco <david.dalbusco@dfinity.org>
2022-01-17 15:22:41 +0100

    test: accounts

--- frontend/svelte/src/tests/routes/Accounts.spec.ts
+++ frontend/svelte/src/tests/routes/Accounts.spec.ts
@@ -4,10 +4,14 @@
 
 import { authStore } from "../../lib/stores/auth.store";
 import { mockAuthStoreSubscribe } from "../mocks/auth.store.mock";
-import { mockAccountsStoreSubscribe } from "../mocks/accounts.store.mock";
+import {
+  mockAccountsStoreSubscribe,
+  mockMainAccount,
+} from "../mocks/accounts.store.mock";
 import { accountsStore } from "../../lib/stores/accounts.store";
-import {render} from '@testing-library/svelte';
-import Accounts from '../../routes/Accounts.svelte';
+import { render } from "@testing-library/svelte";
+import Accounts from "../../routes/Accounts.svelte";
+import { formatICP } from "../../lib/utils/icp.utils";
 
 describe("Accounts", () => {
   let authStoreMock, accountsStoreMock;
@@ -29,5 +33,37 @@ describe("Accounts", () => {
     expect(title).not.toBeUndefined();
     expect(title).toBeVisible();
     expect(title).toHaveTextContent("Accounts");
+  });
+
+  it("should render title and account icp", () => {
+    const { container } = render(Accounts);
+
+    const titleRow = container.querySelector("section > div");
+
+    expect(titleRow.textContent).toEqual(
+      `Accounts ${formatICP(mockMainAccount.balance.toE8s())} ICP`
+    );
+  });
+
+  it("should render a main card", () => {
+    const { container } = render(Accounts);
+
+    const article = container.querySelector("article");
+    expect(article).not.toBeUndefined();
+  });
+
+  it("should render account icp in card too", () => {
+    const { container } = render(Accounts);
+
+    const cardTitleRow = container.querySelector("article > div > div");
+
+    expect(cardTitleRow.textContent).toEqual(
+      `${formatICP(mockMainAccount.balance.toE8s())} ICP`
+    );
+  });
+
+  it("should render account identifier", () => {
+    const { getByText } = render(Accounts);
+    getByText(mockMainAccount.identifier);
   });
 });
