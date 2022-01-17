cba3fff643512482a1ceb68ecafd538527fed5e0
David Dal Busco <david.dalbusco@dfinity.org>
2022-01-17 09:08:19 +0100

    chore: revert redirect guard as in main

--- frontend/svelte/src/routes/Voting.svelte
+++ frontend/svelte/src/routes/Voting.svelte
@@ -1,23 +1,19 @@
 <script lang="ts">
   import Layout from "../lib/components/Layout.svelte";
-  import type { Unsubscriber } from "svelte/types/runtime/store";
-  import { AuthStore, authStore } from "../lib/stores/auth.store";
-  import { onDestroy } from "svelte";
+  import { onMount } from "svelte";
 
   // TODO: To be removed once this page has been implemented
-  const unsubscribe: Unsubscriber = authStore.subscribe(
-    ({ signedIn }: AuthStore) => {
-      if (signedIn && process.env.REDIRECT_TO_LEGACY) {
-        window.location.replace("/#/accounts");
-      }
+  onMount(() => {
+    if (process.env.REDIRECT_TO_LEGACY) {
+      window.location.replace("/#/accounts");
     }
-  );
-
-  onDestroy(unsubscribe);
+  });
 </script>
 
-<Layout>
-  <section>
-    <h1>Voting page</h1>
-  </section>
-</Layout>
+{#if !process.env.REDIRECT_TO_LEGACY}
+  <Layout>
+    <section>
+      <h1>Voting page</h1>
+    </section>
+  </Layout>
+{/if}
