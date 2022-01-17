1117039ee3e346c0006859ea6f78579dbfd62950
David Dal Busco <david.dalbusco@dfinity.org>
2022-01-17 10:10:34 +0100

    docs: script purpose

--- frontend/svelte/scripts/copy.index.mjs
+++ frontend/svelte/scripts/copy.index.mjs
@@ -2,6 +2,11 @@
 
 import { readFileSync, writeFileSync } from "fs";
 
+/**
+ * Rollup takes care of the JS and CSS bundles. Here we copy the index.html from the source to the output folder.
+ * At the same time, we also update the <base /> reference (see comment below).
+ * By pre-rendering this information, we make it static and guarantee it will be set when the app - the JS code - access it at runtime.
+ */
 const copyIndex = () => {
   const buffer = readFileSync("./src/index.html");
   const content = buffer.toString("utf-8");
