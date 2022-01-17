99f1494821ffb7c91fab86f1ed8470bb50bea256
Nicolas Mattia <nicolas@nmattia.com>
2022-01-17 19:25:08 +0100

    Create build script for dart code (#360)
    This makes the top-level `build.sh` more readable and is more in line
    with e.g. `frontend/ts`'s `build.sh`.

---
+++ frontend/dart/build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# Build the flutter app
+
+set -euo pipefail
+
+MYPATH="$(
+  cd -- "$(dirname "$0")" >/dev/null 2>&1
+  pwd -P
+)"
+
+echo "Assuming dart codebase path is $MYPATH"
+cd "$MYPATH"
+
+if [[ $DEPLOY_ENV = "mainnet" ]]; then
+  flutter build web --web-renderer html --release --no-sound-null-safety --pwa-strategy=none --dart-define=FLUTTER_WEB_CANVASKIT_URL=/assets/canvaskit/
+else
+  # For all networks that are not mainnet, build with the staging config
+  flutter build web --web-renderer html --release --no-sound-null-safety --pwa-strategy=none --dart-define=DEPLOY_ENV=staging
+fi
+
+# Remove the random hash from flutter output
+sed -i -e 's/flutter_service_worker.js?v=[0-9]*/flutter_service_worker.js/' build/web/index.html
