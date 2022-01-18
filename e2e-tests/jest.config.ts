8e0984d7528eb3f88be693f1e9bff58af85cbdef
Nicolas Mattia <nicolas@dfinity.org>
2022-01-18 11:55:59 +0100

    wip

--- e2e-tests/jest.config.ts
+++
@@ -1,6 +0,0 @@
-// A simple jest config
-
-export default {
-  preset: "ts-jest", // make sure tests are transpiled
-  testEnvironment: "jsdom", // otherwise jest complains that the test hasn't returned (TODO: investigate)
-};
