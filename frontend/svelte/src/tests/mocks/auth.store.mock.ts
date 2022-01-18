e3589f3e814edadc8ce00cbabcb18dc1974570a6
David Dal Busco <david.dalbusco@dfinity.org>
2022-01-18 10:46:07 +0100

    test: load account balance

--- frontend/svelte/src/tests/mocks/auth.store.mock.ts
+++ frontend/svelte/src/tests/mocks/auth.store.mock.ts
@@ -1,14 +1,10 @@
 import type { Subscriber } from "svelte/store";
 import type { AuthStore } from "../../lib/stores/auth.store";
-import type { Principal } from "@dfinity/principal";
+import { Principal } from "@dfinity/principal";
 
-class MockPrincipal {
-  toText() {
-    return "xlmdg-vkosz-ceopx-7wtgu-g3xmd-koiyc-awqaq-7modz-zf6r6-364rh-oqe";
-  }
-}
-
-export const mockPrincipal = new MockPrincipal();
+export const mockPrincipal = Principal.fromText(
+  "xlmdg-vkosz-ceopx-7wtgu-g3xmd-koiyc-awqaq-7modz-zf6r6-364rh-oqe"
+);
 
 /**
  * A static mock of the auth store. The component that uses it will be rendered for test with a value that is already defined on mount.
@@ -16,7 +12,7 @@ export const mockPrincipal = new MockPrincipal();
 export const mockAuthStoreSubscribe = (
   run: Subscriber<AuthStore>
 ): (() => void) => {
-  run({ signedIn: true, principal: mockPrincipal as Principal });
+  run({ signedIn: true, principal: mockPrincipal });
 
   return () => {};
 };
