febd4a98c27c21200174563e6e17c6d32fd9a20f
Nicolas Mattia <nicolas@dfinity.org>
2022-01-18 17:51:33 +0100

    Specify proxy values

--- e2e-tests/specs/example.e2e.ts
+++ e2e-tests/specs/example.e2e.ts
@@ -1,6 +1,8 @@
 
 describe('Test foo', () => {
     it('Bar', async () => {
+
+        // TODO: source the values from somewhere
         await browser.url('http://localhost:8086/v2/');
 
         await browser.$('h1').waitForExist();
