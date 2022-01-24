0a617891f42c6d719f12d4c57cb6dc778c614f93
David Dal Busco <david.dalbusco@dfinity.org>
2022-01-24 11:36:42 +0100

    test: checkbox

---
+++ frontend/svelte/src/tests/routes/Proposals.spec.ts
@@ -0,0 +1,34 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render } from "@testing-library/svelte";
+import { authStore } from "../../lib/stores/auth.store";
+import { mockAuthStoreSubscribe } from "../mocks/auth.store.mock";
+import Proposals from "../../routes/Proposals.svelte";
+
+describe("Proposals", () => {
+  let authStoreMock;
+
+  beforeEach(() => {
+    authStoreMock = jest
+      .spyOn(authStore, "subscribe")
+      .mockImplementation(mockAuthStoreSubscribe);
+  });
+  it("should render a title", () => {
+    const { getByText } = render(Proposals);
+
+    expect(getByText("Voting")).toBeInTheDocument();
+  });
+
+  it("should render a description", () => {
+    const { getByText } = render(Proposals);
+
+    expect(
+      getByText(
+        "The Internet Computer network runs under the control of the Network Nervous System",
+        { exact: false }
+      )
+    ).toBeInTheDocument();
+  });
+});
