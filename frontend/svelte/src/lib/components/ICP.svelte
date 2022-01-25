9a600c238bf1a4edf61fd80792d4dd4ff000cbbd
David Dal Busco <david.dalbusco@outlook.com>
2022-01-25 10:42:51 +0100

    feat: main account summary (#361)
    # Motivation
    
    Display a summary of the "main" account on the "ICP" page.
    
    # Changes
    
    - use `nns-js` for the first time and query the account balance with the ledger
    - create a store for the information of the account(s)
    - sync new store with existing auth store i.e. load account information on sign in and reset on sign out
    - display "main" account on "ICP" page
    - create reusable components for the miscellaneous information (cards, identifier and icp)
    - add a function to format ICP with 8 decimals and thousand separators

---
+++ frontend/svelte/src/lib/components/ICP.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import type { ICP } from "@dfinity/nns";
+  import { formatICP } from "../utils/icp.utils";
+
+  export let icp: ICP;
+</script>
+
+{#if icp}
+  <div>
+    <span>{`${formatICP(icp.toE8s())}`}</span>
+    <span>ICP</span>
+  </div>
+{/if}
+
+<style lang="scss">
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+
+    span:first-of-type {
+      font-weight: 700;
+      font-size: var(--icp-font-size, var(--font-size-h3));
+      color: var(--gray-50);
+    }
+
+    @media (max-width: 768px) {
+      align-items: baseline;
+      display: inline-grid;
+      grid-template-columns: repeat(2, auto);
+      grid-gap: 5px;
+      justify-content: flex-end;
+    }
+  }
+</style>
