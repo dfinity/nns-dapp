23be9b5ecebd738b98c252259a28f4e10f758f45
David Dal Busco <david.dalbusco@dfinity.org>
2022-01-26 08:15:22 +0100

    test: basehref v2

--- frontend/svelte/src/tests/lib/utils/route.utils.spec.ts
+++ frontend/svelte/src/tests/lib/utils/route.utils.spec.ts
@@ -7,19 +7,14 @@ import {
   replaceHistory,
   routeContext,
   routePath,
-} from '../../../lib/utils/route.utils';
+} from "../../../lib/utils/route.utils";
+import * as routeUtils from "../../../lib/utils/route.utils";
 
 describe("route-utils", () => {
-  const baseHref = "https://nns.ic0.app/";
+  const resetTestURL = (baseUrl) =>
+    window.history.replaceState({}, "Test Title", baseUrl);
 
-  const resetTestURL = () =>
-    window.history.replaceState({}, "Test Title", baseHref);
-
-  afterEach(() => resetTestURL());
-
-  // TODO: base href /v2/
-
-  it("should return the pathname and the hash without base href", () => {
+  const testRoutePath = () => {
     expect(routePath()).toEqual("/");
 
     window.history.replaceState({}, undefined, "/?redirect=accounts");
@@ -37,9 +32,9 @@ describe("route-utils", () => {
 
     window.history.replaceState({}, undefined, "/accounts?param=test");
     expect(routePath()).toEqual("/accounts");
-  });
+  };
 
-  it("should find context for the route", () => {
+  const testRouteContext = () => {
     expect(routeContext()).toEqual("");
 
     window.history.replaceState({}, undefined, "/?redirect=accounts");
@@ -57,57 +52,113 @@ describe("route-utils", () => {
 
     window.history.replaceState({}, undefined, "/accounts?param=test");
     expect(routeContext()).toEqual("accounts");
-  });
+  };
 
-  it("should replace location", () => {
+  const testReplaceHistory = ({
+    baseUrl,
+    historyIndex,
+  }: {
+    baseUrl: string;
+    historyIndex: number;
+  }) => {
     replaceHistory({ path: "/" });
-    expect(window.location.href).toEqual(baseHref);
-    expect(window.history.length).toEqual(1);
+    expect(window.location.href).toEqual(baseUrl);
+    expect(window.history.length).toEqual(historyIndex + 1);
 
-    replaceHistory({ path: "/", query: "redirect=accounts"});
-    expect(window.location.href).toEqual(`${baseHref}?redirect=accounts`);
-    expect(window.history.length).toEqual(1);
+    replaceHistory({ path: "/", query: "redirect=accounts" });
+    expect(window.location.href).toEqual(`${baseUrl}?redirect=accounts`);
+    expect(window.history.length).toEqual(historyIndex + 1);
 
-    replaceHistory({ path: "/#/accounts"});
-    expect(window.location.href).toEqual(`${baseHref}#/accounts`);
-    expect(window.history.length).toEqual(1);
+    replaceHistory({ path: "/#/accounts" });
+    expect(window.location.href).toEqual(`${baseUrl}#/accounts`);
+    expect(window.history.length).toEqual(historyIndex + 1);
 
-    replaceHistory({ path: "/accounts"});
-    expect(window.location.href).toEqual(`${baseHref}accounts`);
-    expect(window.history.length).toEqual(1);
+    replaceHistory({ path: "/accounts" });
+    expect(window.location.href).toEqual(`${baseUrl}accounts`);
+    expect(window.history.length).toEqual(historyIndex + 1);
 
-    replaceHistory({ path: "/#/accounts?param=test"});
-    expect(window.location.href).toEqual(`${baseHref}#/accounts?param=test`);
-    expect(window.history.length).toEqual(1);
+    replaceHistory({ path: "/#/accounts?param=test" });
+    expect(window.location.href).toEqual(`${baseUrl}#/accounts?param=test`);
+    expect(window.history.length).toEqual(historyIndex + 1);
 
-    replaceHistory({ path: "/accounts?param=test"});
-    expect(window.location.href).toEqual(`${baseHref}accounts?param=test`);
-    expect(window.history.length).toEqual(1);
+    replaceHistory({ path: "/accounts?param=test" });
+    expect(window.location.href).toEqual(`${baseUrl}accounts?param=test`);
+    expect(window.history.length).toEqual(historyIndex + 1);
+  };
+
+  const testPushHistory = ({
+    baseUrl,
+    historyIndex,
+  }: {
+    baseUrl: string;
+    historyIndex: number;
+  }) => {
+    pushHistory({ path: "/" });
+    expect(window.location.href).toEqual(baseUrl);
+    expect(window.history.length).toEqual(historyIndex + 2);
+
+    pushHistory({ path: "/", query: "redirect=accounts" });
+    expect(window.location.href).toEqual(`${baseUrl}?redirect=accounts`);
+    expect(window.history.length).toEqual(historyIndex + 3);
+
+    pushHistory({ path: "/#/accounts" });
+    expect(window.location.href).toEqual(`${baseUrl}#/accounts`);
+    expect(window.history.length).toEqual(historyIndex + 4);
+
+    pushHistory({ path: "/accounts" });
+    expect(window.location.href).toEqual(`${baseUrl}accounts`);
+    expect(window.history.length).toEqual(historyIndex + 5);
+
+    pushHistory({ path: "/#/accounts?param=test" });
+    expect(window.location.href).toEqual(`${baseUrl}#/accounts?param=test`);
+    expect(window.history.length).toEqual(historyIndex + 6);
+
+    pushHistory({ path: "/accounts?param=test" });
+    expect(window.location.href).toEqual(`${baseUrl}accounts?param=test`);
+    expect(window.history.length).toEqual(historyIndex + 7);
+  };
+
+  describe("base href is /", () => {
+    const baseUrl = "https://nns.ic0.app/";
+
+    beforeAll(() =>
+      jest.spyOn(routeUtils, "baseHref").mockImplementation(() => "/")
+    );
+
+    afterEach(() => resetTestURL(baseUrl));
+
+    it("should return the pathname and the hash without base href", () =>
+      testRoutePath());
+
+    it("should find context for the route", () => testRouteContext());
+
+    it("should replace location", () =>
+      testReplaceHistory({ baseUrl, historyIndex: window.history.length - 1 }));
+
+    it("should push location", () =>
+      testPushHistory({ baseUrl, historyIndex: window.history.length - 1 }));
   });
 
-  it("should push location", () => {
-    pushHistory({ path: "/" });
-    expect(window.location.href).toEqual(baseHref);
-    expect(window.history.length).toEqual(2);
+  describe("base href is /v2/", () => {
+    const baseUrl = "https://nns.ic0.app/v2/";
 
-    pushHistory({ path: "/", query: "redirect=accounts"});
-    expect(window.location.href).toEqual(`${baseHref}?redirect=accounts`);
-    expect(window.history.length).toEqual(3);
+    beforeAll(() =>
+      jest.spyOn(routeUtils, "baseHref").mockImplementation(() => "/v2/")
+    );
 
-    pushHistory({ path: "/#/accounts"});
-    expect(window.location.href).toEqual(`${baseHref}#/accounts`);
-    expect(window.history.length).toEqual(4);
+    afterEach(() => resetTestURL(baseUrl));
 
-    pushHistory({ path: "/accounts"});
-    expect(window.location.href).toEqual(`${baseHref}accounts`);
-    expect(window.history.length).toEqual(5);
+    it("should return the pathname and the hash without base href", () =>
+      testRoutePath());
 
-    pushHistory({ path: "/#/accounts?param=test"});
-    expect(window.location.href).toEqual(`${baseHref}#/accounts?param=test`);
-    expect(window.history.length).toEqual(6);
+    it("should find context for the route", () => testRouteContext());
 
-    pushHistory({ path: "/accounts?param=test"});
-    expect(window.location.href).toEqual(`${baseHref}accounts?param=test`);
-    expect(window.history.length).toEqual(7);
+    // We already ran the test for baseHref / that's why there are already pushHistory
+
+    it("should replace location", () =>
+      testReplaceHistory({ baseUrl, historyIndex: window.history.length - 1 }));
+
+    it("should push location", () =>
+      testPushHistory({ baseUrl, historyIndex: window.history.length - 1 }));
   });
 });
