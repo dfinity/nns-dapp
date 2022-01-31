b59e5a15f67c6dd1ff537b63ea264368bcabc4ce
David Dal Busco <david.dalbusco@dfinity.org>
2022-01-31 09:57:56 +0100

    feat: return label key if not found

--- frontend/svelte/src/tests/lib/utils/i18n.utils.spec.ts
+++ frontend/svelte/src/tests/lib/utils/i18n.utils.spec.ts
@@ -6,10 +6,10 @@ describe("i18n-utils", () => {
   });
 
   it("should not match translations", () => {
-    expect(translate({ labelKey: "core" })).toEqual("");
-    expect(translate({ labelKey: "test" })).toEqual("");
+    expect(translate({ labelKey: "core" })).toEqual("core");
+    expect(translate({ labelKey: "test" })).toEqual("test");
     expect(translate({ labelKey: "" })).toEqual("");
-    expect(translate({ labelKey: "core.test" })).toEqual("");
-    expect(translate({ labelKey: "core.test.test" })).toEqual("");
+    expect(translate({ labelKey: "core.test" })).toEqual("core.test");
+    expect(translate({ labelKey: "core.test.test" })).toEqual("core.test.test");
   });
 });
