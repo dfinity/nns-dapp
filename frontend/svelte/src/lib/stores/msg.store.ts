714f1397eb639db0adb4834a93877c25fd950384
David Dal Busco <david.dalbusco@dfinity.org>
2022-01-31 09:26:47 +0100

    chore: format

--- frontend/svelte/src/lib/stores/msg.store.ts
+++ frontend/svelte/src/lib/stores/msg.store.ts
@@ -1,8 +1,8 @@
-import { writable } from 'svelte/store';
+import { writable } from "svelte/store";
 
 export interface Msg {
-    labelKey: string;
-    type: "info" | "error";
+  labelKey: string;
+  type: "info" | "error";
 }
 
 export const msg = writable<Msg | undefined>(undefined);
