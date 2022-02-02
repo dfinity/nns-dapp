079c350ef496a7a3f3cd33b94deb17ced5dff4a7
mstrasinskis <maksims.strasinskis@dfinity.org>
2022-02-02 16:51:38 +0100

    Add placeholder for correct scrolling

--- frontend/svelte/src/lib/components/Footer.svelte
+++ frontend/svelte/src/lib/components/Footer.svelte
@@ -8,12 +8,17 @@
 <footer>
   <slot></slot>
 </footer>
+<div class="place-holder"></div>
 
 <style lang="scss">
   @use "../themes/mixins/img";
 
+  .place-holder {
+    height: var(--section-footer-height);
+  }
+
   footer {
-    position: absolute;
+    position: fixed;
     bottom: 0;
     left: 0;
     right: 0;
@@ -26,9 +31,14 @@
 
     background: var(--background);
     background: linear-gradient(0deg, rgba(11,12,12,1) 10%, rgba(11,12,12,0) 100%);
+
+    // don't block parent scroll
+    pointer-events: none;
   }
 
   footer :global(button) {
+    pointer-events: all;
+
     // TODO: remove <tmp_styles>
     background: var(--blue-400);
     padding: 10px;
@@ -38,7 +48,7 @@
 
     margin: 25px 10px;
     width: 186px;
-    
+
     @media (min-width: 768px) {
       margin: 30px 40px;
       width: 436px;
