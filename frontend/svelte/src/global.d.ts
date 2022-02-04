b674c541a20c173080d4dc0a3e933296be2e7217
David Dal Busco <david.dalbusco@dfinity.org>
2022-02-04 11:47:55 +0100

    docs: comment

--- frontend/svelte/src/global.d.ts
+++ frontend/svelte/src/global.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="svelte" />
 
+// The input event is neither defined in node types no in svelte.
+// This extends the event with the currentTarget that is provided by the browser and that can be used to retrieve the input value.
 type InputEventHandler = Event & {
   currentTarget: EventTarget & HTMLInputElement;
 };
