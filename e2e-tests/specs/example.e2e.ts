a8d014c4b28d6361c250bdf34b3181a53dfbfaf2
Nicolas Mattia <nicolas@dfinity.org>
2022-02-07 10:25:46 +0100

    Wait for all img tags to be complete
    This makes sure that we wait for the background to be loaded before we
    take the screenshots.

--- e2e-tests/specs/example.e2e.ts
+++ e2e-tests/specs/example.e2e.ts
@@ -4,6 +4,24 @@ describe("landing page", () => {
 
     await browser.$("h1").waitForExist();
 
+    browser.waitUntil(
+      function () {
+        return this.execute(function () {
+          let imgs = document.getElementsByTagName("img");
+          if (imgs.length > 0) {
+            return (
+              Array.prototype.every.call(imgs, (img) => {
+                return img.complete;
+              }) && document.readyState === "complete"
+            );
+          } else {
+            return true;
+          }
+        });
+      },
+      { timeoutMsg: `image wasn't loaded` }
+    );
+
     await browser["screenshot"]("landing-page");
   });
 });
