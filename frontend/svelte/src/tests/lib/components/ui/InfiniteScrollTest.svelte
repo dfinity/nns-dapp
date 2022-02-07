5e0549c606378b87249aad24dd99dc31f299a72c
David Dal Busco <david.dalbusco@dfinity.org>
2022-02-07 10:17:57 +0100

    test: infinite scroll

---
+++ frontend/svelte/src/tests/lib/components/ui/InfiniteScrollTest.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import InfiniteScroll from "../../../../lib/components/ui/InfiniteScroll.svelte";
+
+    export let elements!: number[];
+    export let spy!: () => void;
+
+    const intersect = () => spy();
+</script>
+
+<InfiniteScroll on:nnsIntersect={intersect}>
+    {#each elements as element, i}
+        <div>Test {i}</div>
+    {/each}
+</InfiniteScroll>
