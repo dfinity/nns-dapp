28387c0241688c7a0032b62a4b2dbf89aecac8ef
David Dal Busco <david.dalbusco@dfinity.org>
2022-02-08 10:17:33 +0100

    feat: badge component (#414)
    # Motivation
    
    A new inline block element `<Badge />` to display notification or status.
    
    # Changes
    
    - new `<Badge />` element

---
+++ frontend/svelte/src/lib/components/ui/Badge.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  export let color: "warning" | "success" | undefined = undefined;
+</script>
+
+<div class={color}>
+  <slot />
+</div>
+
+<style lang="scss">
+  div {
+    display: inline-block;
+
+    width: fit-content;
+
+    margin: var(--padding) 0;
+    padding: var(--padding);
+    border-radius: var(--border-radius);
+
+    border: 2px solid var(--badge-color, currentColor);
+    color: var(--badge-color);
+
+    font-size: var(--font-size-h3);
+
+    &.warning {
+      --badge-color: var(--yellow-400);
+    }
+
+    &.success {
+      --badge-color: var(--green-500);
+    }
+
+    @media (min-width: 768px) {
+      margin: 0;
+    }
+  }
+</style>
