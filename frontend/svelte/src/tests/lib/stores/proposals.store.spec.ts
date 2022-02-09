74dd928887491bbb7b32e8218af5dcbe1c8ee0f8
David Dal Busco <david.dalbusco@dfinity.org>
2022-02-09 12:42:43 +0100

    test: proposals store filter

--- frontend/svelte/src/tests/lib/stores/proposals.store.spec.ts
+++ frontend/svelte/src/tests/lib/stores/proposals.store.spec.ts
@@ -1,4 +1,5 @@
 import type { ProposalInfo } from "@dfinity/nns";
+import { ProposalRewardStatus, ProposalStatus, Topic } from "@dfinity/nns";
 import { get } from "svelte/store";
 import { DEFAULT_PROPOSALS_FILTERS } from "../../../lib/constants/proposals.constants";
 import {
@@ -41,9 +42,54 @@ describe("proposals-store", () => {
   });
 
   describe("filter", () => {
+    beforeEach(() => {
+      proposalsFiltersStore.filterTopics(DEFAULT_PROPOSALS_FILTERS.topics);
+      proposalsFiltersStore.filterRewards(DEFAULT_PROPOSALS_FILTERS.rewards);
+      proposalsFiltersStore.filterStatus(DEFAULT_PROPOSALS_FILTERS.status);
+    });
+
     it("should be initialized with default filters", () => {
       const filters = get(proposalsFiltersStore);
       expect(filters).toEqual(DEFAULT_PROPOSALS_FILTERS);
+    });
+
+    it("should update topic filters", () => {
+      const filter = [Topic.NetworkEconomics, Topic.SubnetManagement];
+      proposalsFiltersStore.filterTopics(filter);
+
+      const filters = get(proposalsFiltersStore);
+      expect(filters).toEqual({
+        ...DEFAULT_PROPOSALS_FILTERS,
+        topics: filter,
+      });
+    });
+
+    it("should update topic rewards", () => {
+      const filter = [
+        ProposalRewardStatus.PROPOSAL_REWARD_STATUS_ACCEPT_VOTES,
+        ProposalRewardStatus.PROPOSAL_REWARD_STATUS_READY_TO_SETTLE,
+      ];
+      proposalsFiltersStore.filterRewards(filter);
+
+      const filters = get(proposalsFiltersStore);
+      expect(filters).toEqual({
+        ...DEFAULT_PROPOSALS_FILTERS,
+        rewards: filter,
+      });
+    });
+
+    it("should update topic status", () => {
+      const filter = [
+        ProposalStatus.PROPOSAL_STATUS_OPEN,
+        ProposalStatus.PROPOSAL_STATUS_REJECTED,
+      ];
+      proposalsFiltersStore.filterStatus(filter);
+
+      const filters = get(proposalsFiltersStore);
+      expect(filters).toEqual({
+        ...DEFAULT_PROPOSALS_FILTERS,
+        status: filter,
+      });
     });
   });
 });
