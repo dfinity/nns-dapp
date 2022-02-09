034d13a113e6e4f818996ae05c5539bd19d81c46
mstrasinskis <maksims.strasinskis@dfinity.org>
2022-02-09 15:11:57 +0100

    Add IsolatedPageLayout

---
+++ frontend/svelte/src/lib/components/common/IsolatedPageLayout.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import IconBackIosNew from "../../icons/IconBackIosNew.svelte";
+  import { i18n } from "../../stores/i18n";
+  import Toasts from "../ui/Toasts.svelte";
+  import Footer from "./Footer.svelte";
+
+  const dispatch = createEventDispatcher();
+  const back = () => dispatch("back");
+</script>
+
+<header>
+  <button
+    class="back"
+    on:click|stopPropagation={back}
+    aria-label={$i18n.modals.back}><IconBackIosNew /></button
+  >
+  <h2><slot name="header" /></h2>
+</header>
+<main>
+  <slot />
+</main>
+
+<Footer>
+  <slot name="footer" />
+</Footer>
+
+<Toasts />
+
+<style lang="scss">
+  header {
+    display: grid;
+    grid-template-columns: var(--isolated-page-header-height) 1fr var(
+        --isolated-page-header-height
+      );
+    align-items: center;
+    height: var(--isolated-page-header-height);
+
+    // to make the shadow visible
+    position: relative;
+    z-index: 1;
+
+    background-color: var(--background);
+    box-shadow: 0 4px 4px 1px rgba(var(--background-rgb), 0.7);
+
+    button {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+    }
+
+    h2 {
+      text-align: center;
+    }
+  }
+
+  main {
+    position: absolute;
+    inset: calc(var(--isolated-page-header-height)) 0 0;
+    padding-top: 50px;
+
+    overflow: auto;
+
+    background-color: var(--gray-50-background);
+  }
+</style>
---
+++ frontend/svelte/src/lib/icons/IconBackIosNew.svelte
@@ -0,0 +1,12 @@
+<!-- source: https://material.io/icons/ -->
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  height="24px"
+  width="24px"
+  enable-background="new 0 0 24 24"
+  viewBox="0 0 24 24"
+  fill="currentColor"
+  ><rect fill="none" height="24" width="24" /><g
+    ><polygon points="17.77,3.77 16,2 6,12 16,22 17.77,20.23 9.54,12" /></g
+  ></svg
+>
