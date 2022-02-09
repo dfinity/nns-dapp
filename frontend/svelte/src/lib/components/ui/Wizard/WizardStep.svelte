b74f6766ec47a47ac3066c12fd4470babcebf0a8
Llorenç <llorenc.muntaner@dfinity.org>
2022-02-09 08:00:00 +0100

    Use container width

--- frontend/svelte/src/lib/components/ui/Wizard/WizardStep.svelte
+++ frontend/svelte/src/lib/components/ui/Wizard/WizardStep.svelte
@@ -1,27 +1,31 @@
 <script lang="ts">
-  import { onDestroy } from "svelte";
+  import { onDestroy, onMount } from "svelte";
 
   import { fly } from "svelte/transition";
   import { selectedStepStore } from "./wizardStore";
 
   export let index: number;
+  const DEFAULT_OFFSET = 200;
+  const ANIMATION_DURATION = 200;
   let currentIndex: number | null = null;
   let isCurrentIndex: boolean = false;
-  const X_SLIDE_OFFSET = 200;
+  let container: HTMLDivElement | undefined;
+  let absolutOffset = 200;
   let slideOffset: number | undefined;
 
   const unsubscribe = selectedStepStore.subscribe((index) => {
-    slideOffset = index > currentIndex ? X_SLIDE_OFFSET : -X_SLIDE_OFFSET;
+    slideOffset = index > currentIndex ? absolutOffset : -absolutOffset;
     currentIndex = index;
   });
 
   onDestroy(unsubscribe);
 
+  $: absolutOffset = container ? container.clientWidth : DEFAULT_OFFSET;
   $: isCurrentIndex = index === currentIndex;
 </script>
 
 {#if isCurrentIndex}
-  <div in:fly={{ x: slideOffset, duration: 200 }}>
+  <div bind:this={container} in:fly={{ x: slideOffset, duration: ANIMATION_DURATION }}>
     <slot />
   </div>
 {/if}
