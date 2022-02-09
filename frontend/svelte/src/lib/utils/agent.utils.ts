d528aac0d7275ab627767f336c60adf33deda92f
Llorenç <llorenc.muntaner@dfinity.org>
2022-02-09 15:26:38 +0100

    Revert renaming

--- frontend/svelte/src/lib/utils/agent.utils.ts
+++ frontend/svelte/src/lib/utils/agent.utils.ts
@@ -1,6 +1,6 @@
 import { HttpAgent } from "@dfinity/agent";
 
-const host = process.env.SERVICE_URL;
+const host = process.env.IDENTITY_SERVICE_URL;
 
 // To avoid being executed in tests that only import it
 export const createAgent = () => new HttpAgent({ host });
