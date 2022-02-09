5014a662af770eb1f5b3ae61b15d62288e7557fe
David Dal Busco <david.dalbusco@dfinity.org>
2022-02-09 09:06:09 +0100

    docs: comment

--- frontend/svelte/src/lib/stores/proposals.store.ts
+++ frontend/svelte/src/lib/stores/proposals.store.ts
@@ -12,6 +12,12 @@ export interface ProposalsFiltersStore {
   status: ProposalStatus[];
 }
 
+/**
+ * A store that contains the proposals
+ *
+ * - setProposals: replace the current list of proposals with a new list
+ * - pushProposals: append proposals to the current list of proposals. Notably useful when the proposals are fetched in a page that implements an infinite scrolling.
+ */
 const initProposalsStore = () => {
   const { subscribe, update, set } = writable<ProposalInfo[]>([]);
 
@@ -31,6 +37,20 @@ const initProposalsStore = () => {
   };
 };
 
+/**
+ * A store that contains the filters that are used to fetch the proposals.
+ *
+ * The filters are handled in a specific store - i.e. not within the proposals store - because there is no clean way using svelte store to guess what has changed when a value is updated.
+ * For example: if a store contains apples and bananas, when a change is applied and the subscribe functions hits, it is not possible to guess if there were new apples or bananas.
+ *
+ * In the "Proposals" page we have the need to query new proposals when filters changes i.e. trigger a search when the filter changes not when the proposals changes.
+ * That's why above limitation leads to a separate store.
+ *
+ * - filterTopics: set the filter topics (enum Topic)
+ * - filterRewards: set the filter for the status of the rewards (enum ProposalRewardStatus)
+ * - filterStatus: set the filter for the status of the proposals (enum ProposalStatus)
+ *
+ */
 const initProposalsFiltersStore = () => {
   const { subscribe, update } = writable<ProposalsFiltersStore>({
     topics: [
