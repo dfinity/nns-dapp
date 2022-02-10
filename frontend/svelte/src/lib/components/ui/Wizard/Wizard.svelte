a4f91518433f99b0c127fe037376d06190d6e79e
Llorenç <98735375+lmuntanerDfinity@users.noreply.github.com>
2022-02-10 13:18:07 +0100

    L2-215: Wizard Component (#415)
    # Motivation
    
    Create a new component that enables forms and functionality to be displayed by steps.
    
    NOTE: It doesn't work with two Wizard rendered in the same page.
    
    # Changes
    
    * New components: Wizard, WizardPanel
    * New components share a store: wizardStore.
    * New folder to group the different files working together.
    
    # Tests
    
    New tests at Wizard.spec.ts.

---
+++ frontend/svelte/src/lib/components/ui/Wizard/Wizard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { onDestroy } from "svelte";
+
+  import { selectedStepStore } from "./wizardStore";
+
+  export function next() {
+    selectedStepStore.update((index) => index + 1);
+  }
+
+  export function back() {
+    selectedStepStore.update((index) => (index > 0 ? index - 1 : index));
+  }
+
+  onDestroy(() => {
+    // Clean up, all instances of Wizard use the same store.
+    selectedStepStore.set(0);
+  });
+</script>
+
+<section>
+  <slot />
+</section>
