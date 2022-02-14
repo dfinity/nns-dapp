0dc7d6fbc0174373cd36be118bbff9080bb5b7aa
mstrasinskis <98811342+mstrasinskis@users.noreply.github.com>
2022-02-14 18:32:49 +0100

    Input with button (#427)
    * Add input-button
    
    * Update input styling
    
    * Add input with button test

---
+++ frontend/svelte/src/tests/lib/components/ui/InputTest.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Input from "../../../../lib/components/ui/Input.svelte";
+
+  export let props = {};
+</script>
+
+<Input {...props}>
+  <svelte:fragment slot="button">
+    <button>Test Button</button>
+  </svelte:fragment>
+</Input>
