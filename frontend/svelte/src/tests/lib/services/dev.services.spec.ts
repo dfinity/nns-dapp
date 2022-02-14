d6ca4122b5c70b61abd24a23f78fe7f8952ce81c
David Dal Busco <david.dalbusco@dfinity.org>
2022-02-14 15:49:00 +0100

    test: asset get icps works only on testnet

---
+++ frontend/svelte/src/tests/lib/services/dev.services.spec.ts
@@ -0,0 +1,17 @@
+import { getIPCs } from "../../../lib/services/dev.services";
+import { accountsStore } from "../../../lib/stores/accounts.store";
+import { mockAccountsStoreSubscribe } from "../../mocks/accounts.store.mock";
+
+describe("dev-services", () => {
+  beforeEach(() => {
+    jest
+      .spyOn(accountsStore, "subscribe")
+      .mockImplementation(mockAccountsStoreSubscribe);
+  });
+
+  it("should throw an error if the environment is not testnet", async () => {
+    await expect(getIPCs(0.00000002)).rejects.toThrowError(
+      'The environment is not "testnet"'
+    );
+  });
+});
