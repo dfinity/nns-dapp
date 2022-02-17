1c6ad0d899b482dbb73c30eef025e4c906e78c9d
Llorenç <llorenc.muntaner@dfinity.org>
2022-02-17 17:23:15 +0100

    Feat: Add Eslint

---
+++ frontend/svelte/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  extends: [
+    // "plugin:@typescript-eslint/recommended",
+    // "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+    extraFileExtensions: [".svelte"],
+    types: ["jest"]
+  },
+  env: {
+    es6: true,
+    browser: true,
+  },
+  overrides: [
+    {
+      files: ["*.svelte"],
+      processor: "svelte3/svelte3",
+    },
+    {
+      files: ['*.spec.ts'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+  settings: {
+    "svelte3/typescript": () => require("typescript"),
+  },
+  plugins: ["svelte3", "@typescript-eslint"],
+  ignorePatterns: ["node_modules", ".eslintrc.js"],
+  rules: {}
+};
