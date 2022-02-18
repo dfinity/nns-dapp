af2a3fd58dbf26a513ae9865d2e162c8a5adf52c
Hamish Peebles <hamishpeebles@gmail.com>
2022-02-18 17:17:13 +0000

    Added test

--- rs/src/proposals/payloads.rs
+++ rs/src/proposals/payloads.rs
@@ -1,6 +1,7 @@
 use crate::proposals::payloads::nns_function_02::payload_45339;
 use crate::proposals::payloads::nns_function_04::payload_44955;
 use crate::proposals::payloads::nns_function_05::payload_45091;
+use crate::proposals::payloads::nns_function_07::payload_45543;
 use crate::proposals::payloads::nns_function_10::payload_45372;
 use crate::proposals::payloads::nns_function_11::payload_45343;
 use crate::proposals::payloads::nns_function_13::payload_43630;
@@ -12,6 +13,7 @@ use crate::proposals::payloads::nns_function_23::payload_44892;
 mod nns_function_02;
 mod nns_function_04;
 mod nns_function_05;
+mod nns_function_07;
 mod nns_function_10;
 mod nns_function_11;
 mod nns_function_13;
@@ -28,7 +30,7 @@ pub fn get_payloads() -> Vec<(i32, Vec<u8>)> {
         (4, payload_44955()),
         (5, payload_45091()),
         // (6, ???),
-        // (7, ???),
+        (7, payload_45543()),
         // (8, ???),
         // (9, ???),
         (10, payload_45372()),
