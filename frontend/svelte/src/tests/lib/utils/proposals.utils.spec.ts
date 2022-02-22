5c94bedd3c63f8cdc10cf931d043dbbb178e54e0
mstrasinskis <maksims.strasinskis@dfinity.org>
2022-02-22 17:07:18 +0100

    test: actionFields + proposal.mock

--- frontend/svelte/src/tests/lib/utils/proposals.utils.spec.ts
+++ frontend/svelte/src/tests/lib/utils/proposals.utils.spec.ts
@@ -2,7 +2,10 @@ import type { ProposalInfo } from "@dfinity/nns";
 import {
   emptyProposals,
   lastProposalId,
+  proposalActionFields,
+  proposalFirstActionKey,
 } from "../../../lib/utils/proposals.utils";
+import { mockProposalInfo } from "../../mocks/proposal.mock";
 
 describe("proposals-utils", () => {
   const mockProposals: ProposalInfo[] = [
@@ -21,8 +24,26 @@ describe("proposals-utils", () => {
   it("should find no last proposal id", () =>
     expect(lastProposalId([])).toBeUndefined());
 
-  it("should find a last proposal id", () =>
-    expect(lastProposalId(mockProposals)).toEqual(
-      mockProposals[mockProposals.length - 1].id
+  it("should find fist action key", () =>
+    expect(proposalFirstActionKey(mockProposalInfo.proposal)).toEqual(
+      "ExecuteNnsFunction"
     ));
+
+  describe("proposalActionFields", () => {
+    it("should filter action fields", () => {
+      const fields = proposalActionFields(mockProposalInfo.proposal);
+
+      expect(fields.map(([key]) => key).join()).toEqual(
+        "nnsFunctionId,nnsFunctionName,payload"
+      );
+    });
+
+    it("should mock payload formatting", () => {
+      const fields = proposalActionFields(mockProposalInfo.proposal);
+      const payload = fields.find(([key]) => key === "payload")[1];
+      expect(payload).toEqual(
+        '{data_source: {"icp":["Binance"],"sdr":"fixer.io"}, timestamp_seconds: 200}'
+      );
+    });
+  });
 });
