6a66eef590d767add4acb5854d7e1515b3a93365
Llorenç <llorenc.muntaner@dfinity.org>
2022-02-23 08:36:57 +0100

    CR Fixes

--- frontend/svelte/src/lib/canisters/nns-dapp/nns-dapp.errors.ts
+++ frontend/svelte/src/lib/canisters/nns-dapp/nns-dapp.errors.ts
@@ -1,36 +1,17 @@
-type AccountNotFound = "AccountNotFound";
-const accountNotFoundKind: AccountNotFound = "AccountNotFound";
-
 export class AccountNotFoundError extends Error {
-  static kind: AccountNotFound = accountNotFoundKind;
-  public kind: AccountNotFound;
   constructor(message: string) {
     super(message);
-    this.kind = accountNotFoundKind;
   }
 }
-
-type SubAccountLimitExceeded = "SubAccountLimitExceeded";
-const subAccountLimitExceededKind: SubAccountLimitExceeded =
-  "SubAccountLimitExceeded";
 
 export class SubAccountLimitExceededError extends Error {
-  static kind: SubAccountLimitExceeded = subAccountLimitExceededKind;
-  public kind: SubAccountLimitExceeded;
   constructor(message: string) {
     super(message);
-    this.kind = subAccountLimitExceededKind;
   }
 }
 
-type NameTooLong = "NameTooLong";
-const nameTooLongKind: NameTooLong = "NameTooLong";
-
 export class NameTooLongError extends Error {
-  static kind: NameTooLong = nameTooLongKind;
-  public kind: NameTooLong;
   constructor(message: string) {
     super(message);
-    this.kind = nameTooLongKind;
   }
 }
