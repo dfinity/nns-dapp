5d343820ae5bcb2c4c3f7f981ad29198cc74555b
Llorenç <llorenc.muntaner@dfinity.org>
2022-02-24 11:33:04 +0100

    Skip test

--- frontend/svelte/src/tests/lib/modals/AddAccountModal.spec.ts
+++ frontend/svelte/src/tests/lib/modals/AddAccountModal.spec.ts
@@ -5,6 +5,7 @@
 import { fireEvent } from "@testing-library/dom";
 import { render } from "@testing-library/svelte";
 import AddAccountModal from "../../../lib/modals/AddAccountModal/AddAccountModal.svelte";
+import * as accountsServices from "../../../lib/services/accounts.services";
 
 const en = require("../../../lib/i18n/en.json");
 
@@ -33,7 +34,7 @@ describe("AddAccountModal", () => {
     expect(queryByText(en.accounts.new_linked_account_title)).not.toBeNull();
   });
 
-  it("should have disabled Create neuron button", async () => {
+  it("should have disabled Add Account button", async () => {
     const { container, queryByText } = render(AddAccountModal);
 
     const accountCard = queryByText(en.accounts.new_linked_title);
@@ -46,7 +47,7 @@ describe("AddAccountModal", () => {
     expect(createButton.getAttribute("disabled")).not.toBeNull();
   });
 
-  it("should have enabled Create neuron button when entering amount", async () => {
+  it("should have enabled Add Account button when entering name", async () => {
     const { container, queryByText } = render(AddAccountModal);
 
     const accountCard = queryByText(en.accounts.new_linked_title);
@@ -62,5 +63,31 @@ describe("AddAccountModal", () => {
     const createButton = container.querySelector('button[type="submit"]');
     expect(createButton).not.toBeNull();
     expect(createButton.getAttribute("disabled")).toBeNull();
+  });
+
+  // TODO: Cannot mock `createSubAccount` function from account.services.ts
+  // Is it because it's importing it in another component?
+  xit("should have enabled Add Account button when entering name", async () => {
+    const createSubAccountMock = jest.fn().mockResolvedValue(undefined);
+    jest
+      .spyOn(accountsServices, "createSubAccount")
+      .mockImplementation(createSubAccountMock);
+    const { container, queryByText } = render(AddAccountModal);
+
+    const accountCard = queryByText(en.accounts.new_linked_title);
+    expect(accountCard).not.toBeNull();
+
+    await fireEvent.click(accountCard.parentElement);
+
+    const input = container.querySelector('input[name="newAccount"]');
+    // Svelte generates code for listening to the `input` event
+    // https://github.com/testing-library/svelte-testing-library/issues/29#issuecomment-498055823
+    await fireEvent.input(input, { target: { value: "test name" } });
+
+    const createButton = container.querySelector('button[type="submit"]');
+
+    await fireEvent.click(createButton);
+
+    expect(createSubAccountMock).toBeCalled();
   });
 });
