2fe864f67d236f1b1a178a85c1d9ee16623fb97b
David Dal Busco <david.dalbusco@dfinity.org>
2022-02-28 09:05:17 +0100

    feat: infinite scroll observes 20% of page

--- frontend/svelte/src/lib/constants/constants.ts
+++ frontend/svelte/src/lib/constants/constants.ts
@@ -1,1 +1,5 @@
 export const LIST_PAGINATION_LIMIT: number = 10;
+/**
+ * The infinite scroll observe an element that finds place after x % of last page.
+ */
+export const INFINITE_SCROLL_OFFSET: number = 0.2;
