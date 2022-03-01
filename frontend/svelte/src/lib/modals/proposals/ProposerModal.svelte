444cf26936d888e3b73070aa940a5aa67db13939
Formatting Committer <nobody@example.com>
2022-03-01 09:58:11 +0000

    Updating svelte frontend formatting

--- frontend/svelte/src/lib/modals/proposals/ProposerModal.svelte
+++ frontend/svelte/src/lib/modals/proposals/ProposerModal.svelte
@@ -1,34 +1,36 @@
 <script lang="ts">
-    import Modal from "../Modal.svelte";
-    import type {ProposalInfo} from '@dfinity/nns';
-    import {NeuronId} from '@dfinity/nns';
-    import {i18n} from "../../stores/i18n";
-    import Card from "../../components/ui/Card.svelte";
+  import Modal from "../Modal.svelte";
+  import type { ProposalInfo } from "@dfinity/nns";
+  import { NeuronId } from "@dfinity/nns";
+  import { i18n } from "../../stores/i18n";
+  import Card from "../../components/ui/Card.svelte";
 
-    export let proposalInfo: ProposalInfo;
+  export let proposalInfo: ProposalInfo;
 
-    let proposer: NeuronId | undefined;
-    let visible: boolean = false;
+  let proposer: NeuronId | undefined;
+  let visible: boolean = false;
 
-    $: ({ proposer } = proposalInfo);
+  $: ({ proposer } = proposalInfo);
 </script>
 
-<button class="text" on:click|stopPropagation={() => visible = true}><small>Proposer: {proposer || ""}</small></button>
+<button class="text" on:click|stopPropagation={() => (visible = true)}
+  ><small>Proposer: {proposer || ""}</small></button
+>
 
-<Modal {visible} on:nnsClose={() => visible = false} theme="dark">
-    <span slot="title">{$i18n.neuron.title}</span>
+<Modal {visible} on:nnsClose={() => (visible = false)} theme="dark">
+  <span slot="title">{$i18n.neuron.title}</span>
 
-    <!-- TODO: Both neuron details card and fetching neuron itself are implemented in L2-313 -->
-    <!-- Above task needs to be solved first before being able to implement following TODOs -->
+  <!-- TODO: Both neuron details card and fetching neuron itself are implemented in L2-313 -->
+  <!-- Above task needs to be solved first before being able to implement following TODOs -->
 
-    <Card><h4>TODO: Neuron details</h4></Card>
+  <Card><h4>TODO: Neuron details</h4></Card>
 
-    <Card><h4>TODO: Voting history</h4></Card>
+  <Card><h4>TODO: Voting history</h4></Card>
 </Modal>
 
 <style lang="scss">
-    button {
-      padding: 0;
-      margin: 0;
-    }
+  button {
+    padding: 0;
+    margin: 0;
+  }
 </style>
