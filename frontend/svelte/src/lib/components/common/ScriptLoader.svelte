fe0653aa1b9fe2dbd9344f33df34391da6e579fa
mstrasinskis <maksims.strasinskis@dfinity.org>
2022-03-01 09:32:44 +0100

    test: markdown test

--- frontend/svelte/src/lib/components/common/ScriptLoader.svelte
+++ frontend/svelte/src/lib/components/common/ScriptLoader.svelte
@@ -7,13 +7,24 @@
   let script: HTMLScriptElement;
 
   onMount(async () => {
-    script.addEventListener("load", () => dispatch("nnsLoad"));
+    script.addEventListener("load", () => {
+      dispatch("nnsLoad");
+    });
 
     script.addEventListener("error", (event) => {
       console.error("script load error", event);
       dispatch("nnsError");
     });
   });
+
+  /**
+   * There is no script loading in unit-test environment.
+   * To test ScriptLoader user component the test should mock the outcome of script loading.
+   */
+  if (typeof jest !== "undefined") {
+    // doesn't work w/o the timeout because of child first initialization.
+    setTimeout(() => dispatch("nnsLoad"));
+  }
 </script>
 
 <svelte:head>
