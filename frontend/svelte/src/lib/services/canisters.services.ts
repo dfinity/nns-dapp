59e3e8b15a619f5eff246cf53fc2daa69838faf6
David Dal Busco <david.dalbusco@dfinity.org>
2022-03-02 16:37:14 +0100

    feat: load users' canisters (#516)
    # Motivation
    
    Get the list of canisters of the users with the NNSDapp canister.
    
    # Changes
    
    - copy `getCanisters` from `ts` to svelte app `NNSDappCanister`
    - add a store that contains the canisters
    - load the canisters `onMount` in the "Canisters" page

---
+++ frontend/svelte/src/lib/services/canisters.services.ts
@@ -0,0 +1,46 @@
+import type { Identity } from "@dfinity/agent";
+import { get } from "svelte/store";
+import { NNSDappCanister } from "../canisters/nns-dapp/nns-dapp.canister";
+import type { CanisterDetails } from "../canisters/nns-dapp/nns-dapp.types";
+import { OWN_CANISTER_ID } from "../constants/canister-ids.constants";
+import { identityServiceURL } from "../constants/identity.constants";
+import { canistersStore } from "../stores/canisters.store";
+import { i18n } from "../stores/i18n";
+import { createAgent } from "../utils/agent.utils";
+
+export const listCanisters = async ({
+  clearBeforeQuery,
+  identity,
+}: {
+  clearBeforeQuery?: boolean;
+  identity: Identity | null | undefined;
+}) => {
+  if (clearBeforeQuery) {
+    canistersStore.setCanisters([]);
+  }
+
+  const canisters: CanisterDetails[] = await queryCanisters({
+    identity,
+  });
+
+  canistersStore.setCanisters(canisters);
+};
+
+const queryCanisters = async ({
+  identity,
+}: {
+  identity: Identity | null | undefined;
+}): Promise<CanisterDetails[]> => {
+  if (!identity) {
+    throw new Error(get(i18n).error.missing_identity);
+  }
+
+  const agent = await createAgent({ identity, host: identityServiceURL });
+  // ACCOUNTS
+  const nnsDapp: NNSDappCanister = NNSDappCanister.create({
+    agent,
+    canisterId: OWN_CANISTER_ID,
+  });
+
+  return nnsDapp.getCanisters({ certified: true });
+};
