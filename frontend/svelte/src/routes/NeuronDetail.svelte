12052837ee8c59fd3a255cbd10d116a2991c2037
Max <bitdivine@users.noreply.github.com>
2022-03-05 15:08:57 +0100

    Redirect sometimes (#518)
    # Motivation
    We plan to deploy the voting tab first.  For this we will need to be able to switch between svelte and flutter, depending on which tab the user is on.
    
    # Changes
    
    REDIRECT_TO_LEGACY was a binary value, true or false.  We need a third value that lets us see tab changes before they are deployed.  We now have:
    
    Existing string values: "true" "1" "false" "0"
    New string values:  "prod", "staging"

--- frontend/svelte/src/routes/NeuronDetail.svelte
+++ frontend/svelte/src/routes/NeuronDetail.svelte
@@ -3,7 +3,7 @@
   import { i18n } from "../lib/stores/i18n";
 </script>
 
-{#if !process.env.REDIRECT_TO_LEGACY}
+{#if process.env.REDIRECT_TO_LEGACY === "never"}
   <HeadlessLayout>
     <svelte:fragment slot="header">{$i18n.neuron_detail.title}</svelte:fragment>
   </HeadlessLayout>
