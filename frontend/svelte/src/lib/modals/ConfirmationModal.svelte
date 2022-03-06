92b7b31a03468cd9251c5723c6a57b28eff13315
mstrasinskis <maksims.strasinskis@dfinity.org>
2022-03-06 14:52:04 +0100

    feat: add ConfirmationModal component

---
+++ frontend/svelte/src/lib/modals/ConfirmationModal.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import { i18n } from "../stores/i18n";
+  import Modal from "./Modal.svelte";
+
+  const dispatch = createEventDispatcher();
+</script>
+
+<Modal theme="dark" size="small" on:nnsClose>
+  <article>
+    <slot />
+    <div role="toolbar">
+      <button on:click={() => dispatch("nnsClose")} class="secondary full-width"
+        >{$i18n.core.confirm_no}</button
+      >
+      <button class="primary full-width" on:click={() => dispatch("nnsConfirm")}
+        >{$i18n.core.confirm_yes}</button
+      >
+    </div>
+  </article>
+</Modal>
+
+<style lang="scss">
+  article {
+    padding: calc(2 * var(--padding));
+  }
+
+  [role="toolbar"] {
+    display: flex;
+    gap: var(--padding);
+  }
+</style>
