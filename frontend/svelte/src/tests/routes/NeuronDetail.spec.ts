8fa4b2407911795dac3bcd1fac4cffe2811fd628
Max <max@dfinity.org>
2022-03-07 15:13:43 +0100

    Rm unused

--- frontend/svelte/src/tests/routes/NeuronDetail.spec.ts
+++ frontend/svelte/src/tests/routes/NeuronDetail.spec.ts
@@ -3,19 +3,9 @@
  */
 
 import { render } from "@testing-library/svelte";
-import { authStore } from "../../lib/stores/auth.store";
 import NeuronDetail from "../../routes/NeuronDetail.svelte";
-import { mockAuthStoreSubscribe } from "../mocks/auth.store.mock";
 
 describe("Canisters", () => {
-  let _authStoreMock: jest.MockedFunction<any>;
-
-  beforeEach(() => {
-    _authStoreMock = jest
-      .spyOn(authStore, "subscribe")
-      .mockImplementation(mockAuthStoreSubscribe);
-  });
-
   it("should render title", () => {
     const { getByText } = render(NeuronDetail);
 
