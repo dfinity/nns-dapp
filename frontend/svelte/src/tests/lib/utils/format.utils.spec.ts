fbed52389d9901e9d419e8868b6b6f45808ede87
mstrasinskis <98811342+mstrasinskis@users.noreply.github.com>
2022-03-08 15:46:03 +0100

    L2 281 cast vote card (#527)
    * feat: add button.danger
    
    * feat: load neurons on proposal detail page
    
    * feat: add Checkbox/inputFirst param
    
    * feat: add formatVotingPower
    
    * feat: add CastVoteCard neurons selection
    
    * style: warning button update
    
    * style: add CastVoteCard checkbox list styles
    
    * feat: add uniqObjects util
    
    * feat: add replacePlaceholders util
    
    * fix: secondary button mobile
    
    * feat: add castVote function
    
    * feat: add ConfirmationModal component
    
    * feat: make Modal.title optional
    
    * fix: add confirm dialog labels
    
    * feat: add vote confirmation mechanism
    
    * feat: add busy-screen
    
    * feat: add BusyScreen to app
    
    * test: add BusyScreen test
    
    * refactoring: PR update
    
    * test: fix Modal test
    
    * test: add formatVotingPower test
    
    * test: add uniqObjects test
    
    * test: add replacePlaceholders test
    
    * test: castVote
    
    * test: ConfirmationModal
    
    * test: castVote
    
    * test: VoteConfirmationModal
    
    * test: VoteConfirmationModal
    
    * chore: update types
    
    * chore: update comments
    
    * chore: fix register-vote related ESLint errors
    
    * style: apply same font-size for all the buttons
    
    * chore: PR changes (minor refactoring, comments and typos)
    
    * fix: test attributes

--- frontend/svelte/src/tests/lib/utils/format.utils.spec.ts
+++ frontend/svelte/src/tests/lib/utils/format.utils.spec.ts
@@ -2,15 +2,13 @@ import { formatNumber } from "../../../lib/utils/format.utils";
 
 describe("format.utils", () => {
   it("should format number", () => {
-    expect(formatNumber(BigInt(0), { minFraction: 0, maxFraction: 0 })).toEqual(
-      "0"
+    expect(formatNumber(0, { minFraction: 0, maxFraction: 0 })).toEqual("0");
+
+    expect(formatNumber(0)).toEqual("0.00");
+
+    expect(formatNumber(123456789, { minFraction: 0, maxFraction: 0 })).toEqual(
+      "123’456’789"
     );
-
-    expect(formatNumber(BigInt(0))).toEqual("0.00");
-
-    expect(
-      formatNumber(BigInt(123456789), { minFraction: 0, maxFraction: 0 })
-    ).toEqual("123’456’789");
 
     expect(formatNumber(0.123456789)).toEqual("0.12");
 
