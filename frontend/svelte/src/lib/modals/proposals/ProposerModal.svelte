73d4e697fe9d8e0331cf1048cc52cfcfaea006af
Llorenç <llorenc.muntaner@dfinity.org>
2022-03-08 16:18:35 +0100

    Fix: Green CI

--- frontend/svelte/src/lib/modals/proposals/ProposerModal.svelte
+++ frontend/svelte/src/lib/modals/proposals/ProposerModal.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
   import Modal from "../Modal.svelte";
-  import type { ProposalInfo } from "@dfinity/nns";
   import type { NeuronId } from "@dfinity/nns";
   import { i18n } from "../../stores/i18n";
   import Card from "../../components/ui/Card.svelte";
-  import { NeuronInfo } from "@dfinity/nns";
+  import type { NeuronInfo } from "@dfinity/nns";
   import { getNeuron } from "../../services/neurons.services";
   import Spinner from "../../components/ui/Spinner.svelte";
   import NeuronCard from "../../components/neurons/NeuronCard.svelte";
