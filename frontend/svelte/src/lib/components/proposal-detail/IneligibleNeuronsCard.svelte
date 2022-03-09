a9145e1fe72558ef5697055aaeadcd0c9d7a6766
mstrasinskis <maksims.strasinskis@dfinity.org>
2022-03-09 11:45:06 +0100

    style: add IneligibleNeuronsCard styles

--- frontend/svelte/src/lib/components/proposal-detail/IneligibleNeuronsCard.svelte
+++ frontend/svelte/src/lib/components/proposal-detail/IneligibleNeuronsCard.svelte
@@ -41,4 +41,40 @@
 {/if}
 
 <style lang="scss">
+  @use "../../themes/mixins/media";
+
+  p {
+    margin: 0 0 calc(2 * var(--padding));
+    font-size: var(--font-size-h5);
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+
+  li {
+    margin: var(--padding) 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    font-size: var(--font-size-h5);
+
+    @include media.min-width(small) {
+      margin: calc(0.5 * var(--padding)) 0;
+      flex-direction: row;
+      align-items: center;
+    }
+
+    @include media.min-width(medium) {
+      font-size: var(--font-size-h4);
+    }
+    small {
+      font-size: var(--font-size-ultra-small);
+      @include media.min-width(medium) {
+        font-size: var(--font-size-small);
+      }
+    }
+  }
 </style>
