7cd01cb3a65c6fadcb2e0bfd223adfae724d3bc4
David Dal Busco <david.dalbusco@dfinity.org>
2022-03-15 09:56:34 +0100

    test: adapt card block and ballot

--- frontend/svelte/src/tests/lib/components/proposals/BallotSummary.spec.ts
+++ frontend/svelte/src/tests/lib/components/proposals/BallotSummary.spec.ts
@@ -2,18 +2,23 @@
  * @jest-environment jsdom
  */
 
-import { GovernanceCanister } from "@dfinity/nns";
+import { BallotInfo, GovernanceCanister, Vote } from "@dfinity/nns";
 import type { Proposal } from "@dfinity/nns/dist/types/types/governance_converters";
 import { render, waitFor } from "@testing-library/svelte";
-import ProposalShortSummary from "../../../../lib/components/proposals/BallotSummary.svelte";
+import BallotSummary from "../../../../lib/components/proposals/BallotSummary.svelte";
 import { authStore } from "../../../../lib/stores/auth.store";
 import { mockAuthStoreSubscribe } from "../../../mocks/auth.store.mock";
 import { MockGovernanceCanister } from "../../../mocks/governance.canister.mock";
 import { mockProposals } from "../../../mocks/proposals.store.mock";
 
-describe("ProposalShortSummary", () => {
-  const props = {
+describe("BallotSummary", () => {
+  const mockBallot: BallotInfo = {
+    vote: Vote.YES,
     proposalId: mockProposals[0].id,
+  };
+
+  const props = {
+    ballot: mockBallot,
   };
 
   const mockGovernanceCanister: MockGovernanceCanister =
@@ -30,11 +35,11 @@ describe("ProposalShortSummary", () => {
   });
 
   it("should render proposal summary", async () => {
-    const { container, getByText } = render(ProposalShortSummary, {
+    const { container, getByText } = render(BallotSummary, {
       props,
     });
 
-    await waitFor(() => expect(container.querySelector("p")).not.toBeNull());
+    await waitFor(() => expect(container.querySelector("div.markdown p")).not.toBeNull());
 
     expect(
       getByText((mockProposals[0].proposal as Proposal).summary)
