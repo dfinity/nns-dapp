c6255971e7b6c532535650de4599e35bebdc033d
David Dal Busco <david.dalbusco@dfinity.org>
2022-03-15 09:58:46 +0100

    test: should render vote

--- frontend/svelte/src/tests/lib/components/proposals/BallotSummary.spec.ts
+++ frontend/svelte/src/tests/lib/components/proposals/BallotSummary.spec.ts
@@ -6,6 +6,7 @@ import { BallotInfo, GovernanceCanister, Vote } from "@dfinity/nns";
 import type { Proposal } from "@dfinity/nns/dist/types/types/governance_converters";
 import { render, waitFor } from "@testing-library/svelte";
 import BallotSummary from "../../../../lib/components/proposals/BallotSummary.svelte";
+import * as en from "../../../../lib/i18n/en.json";
 import { authStore } from "../../../../lib/stores/auth.store";
 import { mockAuthStoreSubscribe } from "../../../mocks/auth.store.mock";
 import { MockGovernanceCanister } from "../../../mocks/governance.canister.mock";
@@ -39,10 +40,26 @@ describe("BallotSummary", () => {
       props,
     });
 
-    await waitFor(() => expect(container.querySelector("div.markdown p")).not.toBeNull());
+    await waitFor(() =>
+      expect(container.querySelector("div.markdown p")).not.toBeNull()
+    );
 
     expect(
       getByText((mockProposals[0].proposal as Proposal).summary)
+    ).toBeInTheDocument();
+  });
+
+  it("should render ballot vote", async () => {
+    const { container, getByText } = render(BallotSummary, {
+      props,
+    });
+
+    await waitFor(() =>
+      expect(container.querySelector("p.vote")).not.toBeNull()
+    );
+
+    expect(
+      getByText(en.core[Vote[mockBallot.vote].toLowerCase()])
     ).toBeInTheDocument();
   });
 });
