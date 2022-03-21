eaad535d22a8a78e81ad7076fa4c414676fe6b7d
David Dal Busco <david.dalbusco@dfinity.org>
2022-03-21 12:08:08 +0100

    feat: aria-busy

--- frontend/svelte/src/lib/components/ui/SkeletonText.svelte
+++ frontend/svelte/src/lib/components/ui/SkeletonText.svelte
@@ -3,7 +3,7 @@
   export let animated: boolean = true;
 </script>
 
-<div class:animated data-tid="skeleton-text">
+<div class:animated data-tid="skeleton-text" aria-busy={true}>
   <span>&nbsp;</span>
 </div>
 
