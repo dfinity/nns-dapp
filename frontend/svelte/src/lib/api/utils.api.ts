10dba460a333885f2eec3609885a7c4dc0a6f046
Llorenç <llorenc.muntaner@dfinity.org>
2022-03-22 11:34:39 +0100

    L2-322

--- frontend/svelte/src/lib/api/utils.api.ts
+++ frontend/svelte/src/lib/api/utils.api.ts
@@ -1,18 +1,5 @@
 import type { SubAccountArray } from "../canisters/nns-dapp/nns-dapp.types";
 
-// Source: as in ts/src/canisters/converters.ts
-export const arrayOfNumberToUint8Array = (
-  numbers: Array<number>
-): Uint8Array => {
-  return new Uint8Array(numbers);
-};
-
-export const arrayOfNumberToArrayBuffer = (
-  numbers: Array<number>
-): ArrayBuffer => {
-  return arrayOfNumberToUint8Array(numbers).buffer;
-};
-
 export const arrayBufferToNumber = (buffer: ArrayBuffer): number => {
   const view = new DataView(buffer);
   return view.getUint32(view.byteLength - 4);
@@ -20,11 +7,12 @@ export const arrayBufferToNumber = (buffer: ArrayBuffer): number => {
 
 /**
  * Converts a subAccount from NNS Dapp canister to the type that nns-js needs.
+ * as in ts/src/canisters/converters.ts
  *
  * @param subAccount Array<number> from SubAccountArray type in nns-dapp canister
  * @returns subAccountId that `ledgerCanister` from nns-js requires.
  */
 export const toSubAccountId = (subAccount: SubAccountArray): number => {
-  const bytes = arrayOfNumberToArrayBuffer(subAccount);
+  const bytes = new Uint8Array(subAccount).buffer;
   return arrayBufferToNumber(bytes);
 };
