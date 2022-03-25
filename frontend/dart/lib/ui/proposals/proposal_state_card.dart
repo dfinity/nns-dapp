cafaf65d11df22b4719436f0a439cb545fb9790a
Anish <anishh2003@gmail.com>
2022-03-25 11:29:42 +0000

    removed space formatting

--- frontend/dart/lib/ui/proposals/proposal_state_card.dart
+++ frontend/dart/lib/ui/proposals/proposal_state_card.dart
@@ -6,6 +6,7 @@ import 'package:nns_dapp/ui/_components/form_utils.dart';
 import 'package:nns_dapp/ui/_components/responsive.dart';
 import 'package:nns_dapp/ui/neuron_info/neuron_info_widget.dart';
 import 'package:nns_dapp/ui/widgets/flutter_json_viewer.dart';
+
 import '../../nns_dapp.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -13,7 +14,9 @@ class ProposalStateCard extends StatelessWidget {
   final Proposal proposal;
   final List<Neuron> neurons;
 
-  const ProposalStateCard({Key? key, required this.proposal, required this.neurons}) : super(key: key);
+  const ProposalStateCard(
+      {Key? key, required this.proposal, required this.neurons})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +42,8 @@ class ProposalStateCard extends StatelessWidget {
                   decoration: ShapeDecoration(
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(8),
-                          side: BorderSide(width: 2, color: proposal.status.color))),
+                          side: BorderSide(
+                              width: 2, color: proposal.status.color))),
                   child: Padding(
                     padding: EdgeInsets.all(8.0),
                     child: Text(
@@ -87,18 +91,19 @@ class ProposalStateCard extends StatelessWidget {
                         radius: Radius.circular(20),
                         thickness: 5,
                         child: Container(
-                          height: proposal.summary.length.toDouble() < kProposalSummaryBoxMaxHeight
-                              ? proposal.summary.length.toDouble() + kProposalSummaryBoxMinHeight
+                          height: proposal.summary.length.toDouble() <
+                                  kProposalSummaryBoxMaxHeight
+                              ? proposal.summary.length.toDouble() +
+                                  kProposalSummaryBoxMinHeight
                               : kProposalSummaryBoxMaxHeight,
                           width: MediaQuery.of(context).size.width,
                           child: Markdown(
                               data: proposal.summary,
-                              onTapLink: (text, url, title) {
-                                if (url != null) launch(url);
-                              },
-                              styleSheet: MarkdownStyleSheet.fromTheme(ThemeData(
-                                  cardColor: AppColors.black,
-                                  textTheme: nnsDappTextTheme(Responsive.isMobile(context))))),
+                              styleSheet: MarkdownStyleSheet.fromTheme(
+                                  ThemeData(
+                                      cardColor: AppColors.black,
+                                      textTheme: nnsDappTextTheme(
+                                          Responsive.isMobile(context))))),
                         ),
                       ),
                     ),
@@ -111,12 +116,14 @@ class ProposalStateCard extends StatelessWidget {
               onPressed: () => launch(proposal.url),
               child: Text(
                 proposal.url,
-                style: context.textTheme.subtitle2?.copyWith(color: Colors.blue),
+                style:
+                    context.textTheme.subtitle2?.copyWith(color: Colors.blue),
               ),
             ),
             TextButton(
                 onPressed: () {
-                  OverlayBaseWidget.show(context, NeuronInfoWidget(proposal.proposer));
+                  OverlayBaseWidget.show(
+                      context, NeuronInfoWidget(proposal.proposer));
                 },
                 child: Text(
                   "Proposer: ${proposal.proposer}",
@@ -145,7 +152,8 @@ class ProposalStateCard extends StatelessWidget {
 class ActionDetailsWidget extends StatelessWidget {
   final Proposal proposal;
 
-  const ActionDetailsWidget({Key? key, required this.proposal}) : super(key: key);
+  const ActionDetailsWidget({Key? key, required this.proposal})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -155,7 +163,9 @@ class ActionDetailsWidget extends StatelessWidget {
 
     return Container(
       width: double.infinity,
-      decoration: BoxDecoration(color: AppColors.mediumBackground, borderRadius: BorderRadius.circular(10)),
+      decoration: BoxDecoration(
+          color: AppColors.mediumBackground,
+          borderRadius: BorderRadius.circular(10)),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -165,7 +175,9 @@ class ActionDetailsWidget extends StatelessWidget {
               child: Text(actionKey, style: context.textTheme.headline4),
             ),
           ),
-          ...fields.entries.filter((entry) => entry.key != 'payloadBytes').map((entry) {
+          ...fields.entries
+              .filter((entry) => entry.key != 'payloadBytes')
+              .map((entry) {
             return Padding(
               padding: const EdgeInsets.all(8.0),
               child: Column(
@@ -173,15 +185,14 @@ class ActionDetailsWidget extends StatelessWidget {
                 children: [
                   Text(
                     entry.key,
-                    style: context.textTheme.bodyText1?.copyWith(fontSize: 14, color: AppColors.gray50),
+                    style: context.textTheme.bodyText1
+                        ?.copyWith(fontSize: 14, color: AppColors.gray50),
                   ),
-                  SizedBox(height: 5.0),
                   entry.key != 'payload'
                       ? SelectableText(
-                          entry.value.toString().toString(),
-                          style: context.textTheme.bodyText2,
-                        )
-                      : JsonViewer(entry.value)
+                    entry.value.toString().toString(),
+                    style: context.textTheme.bodyText2,
+                  ): JsonViewer(entry.value)
                 ],
               ),
             );
