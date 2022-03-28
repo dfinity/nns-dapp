006dbfd1859f705df4cff139f7b603823ff70dbe
Max <max@dfinity.org>
2022-03-28 11:41:53 +0200

    Test voting

--- e2e-tests/specs/vote.e2e.ts
+++ e2e-tests/specs/vote.e2e.ts
@@ -28,7 +28,7 @@ describe("vote", () => {
 	    await numIcp.setValue("10");
 	    const submitButton = await browser.$('[data-tid="get-icp-submit"]');
 	    await submitButton.waitForExist();
-	    await browser.waitUntil(() => submitButton.isClickable(), {timeoutMsg: "Timeout waiting for submitButton to be clickable."});
+	    await browser.waitUntil(async () => submitButton.isClickable(), {timeoutMsg: "Timeout waiting for submitButton to be clickable."});
 	    await browser["screenshot"]("get-icp-form");
 	    await submitButton.click();
 	    //browser.pause(10000);
@@ -42,7 +42,7 @@ describe("vote", () => {
   it("goToNeuronTab", async() => {
     const tabButton = await getNeuronTabButton(browser);
     await tabButton.waitForExist();
-    await browser.waitUntil(() => tabButton.isClickable(), {timeoutMsg: "Timeout waiting for the neuron tab button."});
+    await browser.waitUntil(async () => tabButton.isClickable(), {timeoutMsg: "Timeout waiting for the neuron tab button."});
     await tabButton.click();
     const neuronsTab = await getNeuronsBody(browser);
     await neuronsTab.waitForExist();
@@ -56,7 +56,7 @@ describe("vote", () => {
   it("createNeuronIfNone", async () => {
     await browser["screenshot"]("looking-for-neuron");
     const neuron = await browser.$('[data-tid="neurons-body"] [data-tid="card"]');
-    if (false && neuron.isExisting()) {
+    if (neuron.isExisting()) {
         console.log("We already have a neuron");
     } else {
         const stakingButton = await getStakingButton(browser);
@@ -81,15 +81,15 @@ describe("vote", () => {
         const maxDissolveDelay = await dissolveDelaySlider.getAttribute("max");
 	await dissolveDelaySlider.setValue(maxDissolveDelay);
         const updateDissolveDelayButton = await browser.$('#modalContent button.primary');
-	await browser.waitUntil(() => updateDissolveDelayButton.isClickable(), {timeoutMsg: "Timeout waiting for updateDissolveDelayButton to be clickable."});
+	await browser.waitUntil(async () => updateDissolveDelayButton.isClickable(), {timeoutMsg: "Timeout waiting for updateDissolveDelayButton to be clickable."});
 	updateDissolveDelayButton.click();
 	const confirmUpdateButton = await browser.$('#modalContent [data-tid="confirm-delay-button"]');
-	await browser.waitUntil(() => confirmUpdateButton.isClickable(), {timeoutMsg: "Timeout waiting for confirmUpdateButton to be clickable."});
+	await browser.waitUntil(async () => confirmUpdateButton.isClickable(), {timeoutMsg: "Timeout waiting for confirmUpdateButton to be clickable."});
 	confirmUpdateButton.click();
 	await browser.waitUntil(async () => !await browser.$('#modalContent [data-tid="confirm-delay-button"]').isExisting(), {timeout: 60000, timeoutMsg: "Timeout waiting for confirmUpdateButton to disappear."});
 	const closeFollowingModal = await browser.$('[data-tid="close-modal"]');
 	closeFollowingModal.waitForExist();
-	await browser.waitUntil(() => closeFollowingModal.isClickable(), {timeoutMsg: "Timeout waiting for the closeFollowingModal button to be clickable."});
+	await browser.waitUntil(async () => closeFollowingModal.isClickable(), {timeoutMsg: "Timeout waiting for the closeFollowingModal button to be clickable."});
 	await closeFollowingModal.click();
 	await browser.waitUntil(async () => !await browser.$('[data-tid="close-modal"]').isExisting(), {timeout: 60000, timeoutMsg: "Timeout waiting for the modal to disappear."});
     }
@@ -103,21 +103,32 @@ describe("vote", () => {
     await votingTabButton.click();
     await browser["screenshot"]("voting-page");
   });
-
-  /*
-  it("navigateToVotingTab", async () => {
-    const votingTabButton = await getVotingTabButton(browser);
-    await votingTabButton.waitForExist();
-    await votingTabButton.click();
-    await browser["screenshot"]("voting-page");
-  }, 10000);
-
-  it("canShowAllProposals", async () => {
-    const result = await browser.execute(() =>
-      document.querySelectorAll('.filters button')
-    );
-
-    console.log(result);
+  it('hide-unavailable-proposals', async() => {
+    // TODO: Only click if the checkbox is not checked.
+    const hideUnavailableCheckbox = await browser.$('label[for="hide-unavailable-proposals"]');
+    await hideUnavailableCheckbox.waitForExist();
+    await hideUnavailableCheckbox.click();
+    // TODO: How to check whether the reload has already taken place?
+    await browser.pause(3000);
+    await browser["screenshot"]("hide-unavailable");
   });
-*/
+  it('votes', async() => {
+    const proposalCard = await browser.$('[data-tid="card"]');
+    await proposalCard.waitForExist();
+    await proposalCard.click();
+    // TODO: data-tid for the whole page, so we know when it has loaded.
+    const yesMan = await browser.$('[data-tid="vote-yes"]');
+    await yesMan.waitForExist();
+    await browser.waitUntil(async () => yesMan.isClickable(), {timeoutMsg: "Timeout waiting for the yes vote button to be clickable."});
+    await browser["screenshot"]("ready-to-vote");
+    await yesMan.click();
+
+    const confirmButton = await browser.$('[data-tid="confirm-yes"]');
+    await confirmButton.waitForExist();
+    await browser.waitUntil(async () => confirmButton.isClickable(), {timeoutMsg: "Timeout waiting for the confirm-yes button to be clickable."});
+    await browser["screenshot"]("confirming-yes-vote");
+    await confirmButton.click();
+    await browser.pause(6000);
+    await browser["screenshot"]("voted");
+  });
 });
