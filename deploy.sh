e4a848237765657bba523889bb01cc5948dae15d
Max <max@dfinity.org>
2022-03-29 12:53:23 +0200

    ++

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$(realpath "$0")")" || exit
 NETWORK=$1
 
 echo "Deploying to $NETWORK..."
-DEPLOY_ENV=$NETWORK dfx deploy --no-wallet --network "$NETWORK"
+DEPLOY_ENV=$NETWORK dfx deploy --no-wallet --network "$NETWORK" nns-dapp
 echo "Deployed to: https://$(jq -jc '.["nns-dapp"].testnet' canister_ids.json).nnsdapp.dfinity.network"
