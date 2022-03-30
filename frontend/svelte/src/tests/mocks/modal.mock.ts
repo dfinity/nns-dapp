ac76ddcb613d34e6aebf9a637edc552f25fe6665
David Dal Busco <david.dalbusco@dfinity.org>
2022-03-30 15:16:39 +0200

    test: fake introend event

---
+++ frontend/svelte/src/tests/mocks/modal.mock.ts
@@ -0,0 +1,9 @@
+import {waitFor} from '@testing-library/svelte';
+
+export const waitModalIntroEnd = async ({container, selector}: {container: HTMLElement, selector: string}) => {
+  // Testing library does not implement on:introend that's why we fake the event
+  const event = new CustomEvent("introend");
+  container.querySelector('div[role="dialog"]')?.dispatchEvent(event);
+
+  await waitFor(() => expect(container.querySelector(selector)).not.toBeNull());
+};
