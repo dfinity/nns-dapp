ff2940acb47f6cd1a23ac18c529f2c9237a3e9c9
Llorenç <llorenc.muntaner@dfinity.org>
2022-03-30 15:14:24 +0200

    L2-353: Add todos

--- frontend/svelte/src/lib/components/neuron-detail/actions/MergeMaturityButton.svelte
+++ frontend/svelte/src/lib/components/neuron-detail/actions/MergeMaturityButton.svelte
@@ -2,6 +2,7 @@
   import { i18n } from "../../../stores/i18n";
 </script>
 
+<!-- TODO: https://dfinity.atlassian.net/browse/L2-372 -->
 <button disabled class="primary small"
   >{$i18n.neuron_detail.merge_maturity}</button
 >
--- frontend/svelte/src/lib/components/neuron-detail/actions/SpawnNeuronButton.svelte
+++ frontend/svelte/src/lib/components/neuron-detail/actions/SpawnNeuronButton.svelte
@@ -2,6 +2,7 @@
   import { i18n } from "../../../stores/i18n";
 </script>
 
+<!-- TODO: https://dfinity.atlassian.net/browse/L2-373 -->
 <button disabled class="primary small"
   >{$i18n.neuron_detail.spawn_neuron}</button
 >
