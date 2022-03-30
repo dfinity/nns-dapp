2f3ccc3d5fba706f91393772f3dd830b9af11d6a
Llorenç <llorenc.muntaner@dfinity.org>
2022-03-30 10:50:06 +0200

    L2-353: MaturityCard UI

---
+++ frontend/svelte/src/lib/components/neuron-detail/actions/MergeMaturityButton.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { i18n } from "../../../stores/i18n";
+</script>
+
+<button disabled class="primary small"
+  >{$i18n.neuron_detail.merge_maturity}</button
+>
---
+++ frontend/svelte/src/lib/components/neuron-detail/actions/SpawnNeuronButton.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { i18n } from "../../../stores/i18n";
+</script>
+
+<button disabled class="primary small"
+  >{$i18n.neuron_detail.spawn_neuron}</button
+>
