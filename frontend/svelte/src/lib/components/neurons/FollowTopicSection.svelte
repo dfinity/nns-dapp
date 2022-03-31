66a142b2dd8b5ac2ee8cf7ad6d44f4812e4a6698
mstrasinskis <maksims.strasinskis@dfinity.org>
2022-03-31 15:25:36 +0200

    feat: add Collapsible to FollowTopicSection

--- frontend/svelte/src/lib/components/neurons/FollowTopicSection.svelte
+++ frontend/svelte/src/lib/components/neurons/FollowTopicSection.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   // Tested in EditFollowNeurons.spec.ts
-  import type { NeuronId, Topic, NeuronInfo } from "@dfinity/nns";
-  import IconExpandMore from "../../icons/IconExpandMore.svelte";
+  import { type NeuronId, Topic, type NeuronInfo } from "@dfinity/nns";
   import NewFolloweeModal from "../../modals/neurons/NewFolloweeModal.svelte";
   import { removeFollowee } from "../../services/neurons.services";
   import { i18n } from "../../stores/i18n";
   import { toastsStore } from "../../stores/toasts.store";
+  import Collapsible from "../ui/Collapsible.svelte";
 
   export let topic: Topic;
   export let neuron: NeuronInfo;
@@ -14,16 +14,13 @@
   $: title = $i18n.follow_neurons[`topic_${topic}_title`];
   let subtitle: string;
   $: subtitle = $i18n.follow_neurons[`topic_${topic}_subtitle`];
+  let id: string | undefined;
+  $: id = Topic[topic];
 
-  let isExpanded: boolean = false;
   let showNewFolloweeModal: boolean = false;
   let followees: NeuronId[] = [];
 
-  const toggleContent = () => {
-    // TODO: Fetch followees and render them - https://dfinity.atlassian.net/browse/L2-365
-    isExpanded = !isExpanded;
-  };
-
+  // TODO: Fetch followees and render them - https://dfinity.atlassian.net/browse/L2-365
   const openNewFolloweeModal = () => (showNewFolloweeModal = true);
   const closeNewFolloweeModal = () => (showNewFolloweeModal = false);
 
@@ -42,41 +39,39 @@
 </script>
 
 <article data-tid="follow-topic-section">
-  <div class="wrapper">
-    <div>
-      <h3>{title}</h3>
-      <p class="subtitle">{subtitle}</p>
+  <Collapsible {id} iconSize={"medium"}>
+    <svelte:fragment slot="header">
+      <div class="wrapper">
+        <div>
+          <h3>{title}</h3>
+          <p class="subtitle">{subtitle}</p>
+        </div>
+        <div class="toolbar">
+          <!-- TODO: Set total followees - https://dfinity.atlassian.net/browse/L2-365 -->
+          <h3 class="badge">0</h3>
+        </div>
+      </div>
+    </svelte:fragment>
+    <div class="content" data-tid="follow-topic-section-current">
+      <h5>{$i18n.follow_neurons.current_followees}</h5>
+      <ul>
+        {#each followees as followee}
+          <li data-tid="current-followee-item">
+            <!-- TODO: Use followee details - https://dfinity.atlassian.net/browse/L2-365 -->
+            <p>DFINITY Foundation</p>
+            <button on:click={() => removeCurrentFollowee(followee)}>x</button>
+          </li>
+        {/each}
+      </ul>
+      <div class="button-wrapper">
+        <button
+          class="secondary small"
+          data-tid="open-new-followee-modal"
+          on:click={openNewFolloweeModal}>{$i18n.follow_neurons.add}</button
+        >
+      </div>
     </div>
-    <div class="toolbar">
-      <!-- TODO: Set total followees - https://dfinity.atlassian.net/browse/L2-365 -->
-      <h3 class="badge">0</h3>
-      <!-- TODO: Use expandable component -->
-      <span
-        class:isExpanded
-        on:click={toggleContent}
-        data-tid="expand-topic-followees"><IconExpandMore /></span
-      >
-    </div>
-  </div>
-  <div class="content" class:isExpanded data-tid="follow-topic-section-current">
-    <h5>{$i18n.follow_neurons.current_followees}</h5>
-    <ul>
-      {#each followees as followee}
-        <li data-tid="current-followee-item">
-          <!-- TODO: Use followee details - https://dfinity.atlassian.net/browse/L2-365 -->
-          <p>DFINITY Foundation</p>
-          <button on:click={() => removeCurrentFollowee(followee)}>x</button>
-        </li>
-      {/each}
-    </ul>
-    <div class="button-wrapper">
-      <button
-        class="secondary small"
-        data-tid="open-new-followee-modal"
-        on:click={openNewFolloweeModal}>{$i18n.follow_neurons.add}</button
-      >
-    </div>
-  </div>
+  </Collapsible>
   {#if showNewFolloweeModal}
     <NewFolloweeModal {neuron} {topic} on:nnsClose={closeNewFolloweeModal} />
   {/if}
@@ -84,6 +79,15 @@
 
 <style lang="scss">
   @use "../../themes/mixins/interaction";
+
+  article {
+    :global(.collapsible-expand-icon) {
+      align-items: start;
+      padding-top: 30px;
+      color: var(--background-contrast);
+    }
+  }
+
   .wrapper {
     display: flex;
     align-items: start;
@@ -101,6 +105,7 @@
     justify-content: space-around;
     gap: calc(2 * var(--padding));
     margin-top: calc(2 * var(--padding));
+    margin-right: calc(2 * var(--padding));
   }
 
   .badge {
@@ -115,32 +120,7 @@
     justify-content: center;
   }
 
-  .toolbar span {
-    @include interaction.tappable;
-
-    color: var(--background-contrast);
-
-    transition: transform 0.3s;
-
-    &.isExpanded {
-      // We need to translateY to keep the center in the same place
-      transform: rotate(180deg) translateY(4px);
-    }
-  }
-
   .content {
-    visibility: hidden;
-    max-height: 0;
-    height: fit-content;
-    overflow: hidden;
-    transition: all 0.3s;
-
-    &.isExpanded {
-      visibility: visible;
-      max-height: calc(300px);
-      height: fit-content;
-    }
-
     .button-wrapper {
       display: flex;
       align-items: center;
