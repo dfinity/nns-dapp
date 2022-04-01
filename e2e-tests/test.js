3f95b19a04897bac5eb0201098b38424b5ac3a3c
Max <max@dfinity.org>
2022-04-01 13:53:40 +0200

    fmt

--- e2e-tests/test.js
+++ e2e-tests/test.js
@@ -4,10 +4,10 @@
  * are run.
  */
 
-'use strict';
+"use strict";
 
-import * as fs from 'fs';
-import * as child_process from 'child_process';
+import * as fs from "fs";
+import * as child_process from "child_process";
 
 /*
  * Read the values from dfx.json and canister_ids.json
@@ -16,12 +16,14 @@ const CANISTER_IDS_PATH = `${__dirname}/../.dfx/local/canister_ids.json`;
 let nns_canister_id;
 let ii_canister_id;
 try {
-	const canister_ids = JSON.parse(fs.readFileSync(CANISTER_IDS_PATH, 'utf8'));
-	nns_canister_id = canister_ids['nns-dapp'].local;
-	ii_canister_id = canister_ids['internet_identity'].local;
+  const canister_ids = JSON.parse(fs.readFileSync(CANISTER_IDS_PATH, "utf8"));
+  nns_canister_id = canister_ids["nns-dapp"].local;
+  ii_canister_id = canister_ids["internet_identity"].local;
 } catch (e) {
-	console.log(`Could not read 'nns-dapp' local canister ID from ${CANISTER_IDS_PATH}`);
-	throw e;
+  console.log(
+    `Could not read 'nns-dapp' local canister ID from ${CANISTER_IDS_PATH}`
+  );
+  throw e;
 }
 
 console.log(`NNS Dapp using canister ID: ${nns_canister_id}`);
@@ -30,14 +32,14 @@ console.log(`II using canister id: ${ii_canister_id}`);
 const DFX_JSON_PATH = `${__dirname}/../dfx.json`;
 let replica_host;
 try {
-	const dfx_json = JSON.parse(fs.readFileSync(DFX_JSON_PATH, 'utf8'));
-	replica_host = dfx_json.networks.local.bind;
-	if (replica_host.startsWith('http://') !== true) {
-		replica_host = `http://${replica_host}`;
-	}
+  const dfx_json = JSON.parse(fs.readFileSync(DFX_JSON_PATH, "utf8"));
+  replica_host = dfx_json.networks.local.bind;
+  if (replica_host.startsWith("http://") !== true) {
+    replica_host = `http://${replica_host}`;
+  }
 } catch (e) {
-	console.log(`Could not read replica host from ${DFX_JSON_PATH}`);
-	throw e;
+  console.log(`Could not read replica host from ${DFX_JSON_PATH}`);
+  throw e;
 }
 
 console.log(`Using replica host: ${replica_host}`);
@@ -54,48 +56,48 @@ const II_URL = `http://localhost:${II_PORT}`;
 const NNS_DAPP_PORT = 8086;
 const NNS_DAPP_URL = `http://localhost:${NNS_DAPP_PORT}`;
 
-const proxy = child_process.spawn('proxy', [
-	'--replica-host',
-	replica_host,
-	`${nns_canister_id}:${NNS_DAPP_PORT}`,
-	`${ii_canister_id}:${II_PORT}`
+const proxy = child_process.spawn("proxy", [
+  "--replica-host",
+  replica_host,
+  `${nns_canister_id}:${NNS_DAPP_PORT}`,
+  `${ii_canister_id}:${II_PORT}`,
 ]);
 
-proxy.stdout.on('data', (data) => {
-	console.log(`proxy: ${data}`);
+proxy.stdout.on("data", (data) => {
+  console.log(`proxy: ${data}`);
 });
 
-proxy.stdout.on('close', (code) => {
-	console.log(`proxy returned with ${code}`);
+proxy.stdout.on("close", (code) => {
+  console.log(`proxy returned with ${code}`);
 });
 
 /*
  * Start the tests
  */
-const wdio = child_process.spawn('npm', ['run', 'wdio'], {
-	env: { ...process.env, NNS_DAPP_URL, II_URL }
+const wdio = child_process.spawn("npm", ["run", "wdio"], {
+  env: { ...process.env, NNS_DAPP_URL, II_URL },
 });
 
-wdio.stdout.on('data', (data) => {
-	console.log(`wdio: ${data}`);
+wdio.stdout.on("data", (data) => {
+  console.log(`wdio: ${data}`);
 });
 
-wdio.stderr.on('data', (data) => {
-	console.log(`wdio error: ${data}`);
+wdio.stderr.on("data", (data) => {
+  console.log(`wdio error: ${data}`);
 });
 
-wdio.stdout.on('close', (code) => {
-	console.log(`wdio returned with ${code}`);
-	proxy.kill();
+wdio.stdout.on("close", (code) => {
+  console.log(`wdio returned with ${code}`);
+  proxy.kill();
 });
 
-wdio.on('exit', (code: number, signal?: number) => {
-	if (code > 0) {
-		// bubble up error from wdio tests
-		throw new Error(`End-to-end tests returned with ${code}`);
-	} else if (undefined !== signal) {
-		console.error('Child was killed with signal', signal);
-	} else {
-		console.log('Child exited okay');
-	}
+wdio.on("exit", (code: number, signal?: number) => {
+  if (code > 0) {
+    // bubble up error from wdio tests
+    throw new Error(`End-to-end tests returned with ${code}`);
+  } else if (undefined !== signal) {
+    console.error("Child was killed with signal", signal);
+  } else {
+    console.log("Child exited okay");
+  }
 });
--- e2e-tests/wdio.conf.ts
+++ e2e-tests/wdio.conf.ts
@@ -1,73 +1,77 @@
-import { existsSync, mkdirSync } from 'fs';
+import { existsSync, mkdirSync } from "fs";
 
 const NNS_DAPP_URL: string = (process.env.NNS_DAPP_URL as boolean)
-	? String(process.env.NNS_DAPP_URL)
-	: 'http://localhost:8080';
+  ? String(process.env.NNS_DAPP_URL)
+  : "http://localhost:8080";
 
 export const config: WebdriverIO.Config = {
-	baseUrl: NNS_DAPP_URL,
+  baseUrl: NNS_DAPP_URL,
 
-	before: (_capabilities, _spec) => {
-		browser['screenshot-count'] = 0;
-		browser['screenshots-taken'] = new Set();
+  before: (_capabilities, _spec) => {
+    browser["screenshot-count"] = 0;
+    browser["screenshots-taken"] = new Set();
 
-		browser.addCommand('screenshot', async (name) => {
-			const countStr: string = browser['screenshots-taken'].size.toFixed().padStart(2, '0');
-			if (true === browser['screenshots-taken'].has(name)) {
-				throw Error(`A screenshot with this name was already taken: '${name}'`);
-			}
-			browser['screenshots-taken'].add(name);
+    browser.addCommand("screenshot", async (name) => {
+      const countStr: string = browser["screenshots-taken"].size
+        .toFixed()
+        .padStart(2, "0");
+      if (true === browser["screenshots-taken"].has(name)) {
+        throw Error(`A screenshot with this name was already taken: '${name}'`);
+      }
+      browser["screenshots-taken"].add(name);
 
-			const SCREENSHOTS_DIR = 'screenshots';
-			if (!existsSync(SCREENSHOTS_DIR)) {
-				mkdirSync(SCREENSHOTS_DIR);
-			}
+      const SCREENSHOTS_DIR = "screenshots";
+      if (!existsSync(SCREENSHOTS_DIR)) {
+        mkdirSync(SCREENSHOTS_DIR);
+      }
 
-			await browser.saveScreenshot(`${SCREENSHOTS_DIR}/${countStr}-${name}.png`);
-		});
-	},
+      await browser.saveScreenshot(
+        `${SCREENSHOTS_DIR}/${countStr}-${name}.png`
+      );
+    });
+  },
 
-	afterTest: function (test, context, { error }) {
-		// Take a screenshot anytime a test fails and throws an error.
-		// Note: We could also use `result !== 0` or `passed === true`.
-		//       The reason for conditioning on an error is that if
-		//       no error is thrown, the code follows its normal flow
-		//       so it is possible to take a screenshot in the test itself.
-		//       This hook here captures "sudden" death that may be hard
-		//       or tedious to capture otherwise.
-		if (undefined !== error) {
-			// Filenames containing spaces are painful to work with on the command line,
-			// so we replace any spaces in the test name when we create the screenshot.
-			browser['screenshot'](`test-fail-${test.title.replace(/ /g, '-')}`);
-		}
-	},
+  afterTest: function (test, context, { error }) {
+    // Take a screenshot anytime a test fails and throws an error.
+    // Note: We could also use `result !== 0` or `passed === true`.
+    //       The reason for conditioning on an error is that if
+    //       no error is thrown, the code follows its normal flow
+    //       so it is possible to take a screenshot in the test itself.
+    //       This hook here captures "sudden" death that may be hard
+    //       or tedious to capture otherwise.
+    if (undefined !== error) {
+      // Filenames containing spaces are painful to work with on the command line,
+      // so we replace any spaces in the test name when we create the screenshot.
+      browser["screenshot"](`test-fail-${test.title.replace(/ /g, "-")}`);
+    }
+  },
 
-	autoCompileOpts: {
-		autoCompile: true,
-		tsNodeOpts: {
-			transpileOnly: true,
-			project: 'tsconfig.json'
-		}
-	},
-	specs: ['./specs/**/*.ts'],
-	exclude: [],
-	capabilities: [
-		{
-			browserName: 'chrome',
-			'goog:chromeOptions': {
-				args: ['headless', 'disable-gpu']
-			},
-			acceptInsecureCerts: true
-		}
-	],
-	logLevel: 'info',
-	services: ['chromedriver'],
+  autoCompileOpts: {
+    autoCompile: true,
+    tsNodeOpts: {
+      transpileOnly: true,
+      project: "tsconfig.json",
+    },
+  },
+  specs: ["./specs/**/*.ts"],
+  exclude: [],
+  capabilities: [
+    {
+      browserName: "chrome",
+      "goog:chromeOptions": {
+        args: ["headless", "disable-gpu"],
+      },
+      acceptInsecureCerts: true,
+    },
+  ],
+  logLevel: "info",
+  services: ["chromedriver"],
 
-	framework: 'mocha',
-	reporters: ['spec'],
+  framework: "mocha",
+  reporters: ["spec"],
 
-	mochaOpts: {
-		ui: 'bdd',
-		timeout: 60000
-	}
+  mochaOpts: {
+    ui: "bdd",
+    timeout: 60000,
+  },
 };
