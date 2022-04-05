c441d378b824a0d2ec579da967b508a25bbb0bda
David Dal Busco <david.dalbusco@dfinity.org>
2022-04-05 08:22:18 +0200

    test: modal newtransaction nav

---
+++ frontend/svelte/src/tests/lib/modals/accounts/NewTransactionModal.spec.ts
@@ -0,0 +1,68 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { fireEvent } from "@testing-library/dom";
+import type { RenderResult } from "@testing-library/svelte";
+import { render, waitFor } from "@testing-library/svelte";
+import NewTransactionModal from "../../../../lib/modals/accounts/NewTransactionModal.svelte";
+import { accountsStore } from "../../../../lib/stores/accounts.store";
+import {
+  mockAccountsStoreSubscribe,
+  mockSubAccount,
+} from "../../../mocks/accounts.store.mock";
+import en from "../../../mocks/i18n.mock";
+import { waitModalIntroEnd } from "../../../mocks/modal.mock";
+
+describe("NewTransactionModal", () => {
+  jest
+    .spyOn(accountsStore, "subscribe")
+    .mockImplementation(mockAccountsStoreSubscribe([mockSubAccount]));
+
+  const modalSelector = "div.toolbar";
+
+  it("should display modal", () => {
+    const { container } = render(NewTransactionModal);
+
+    expect(container.querySelector("div.modal")).not.toBeNull();
+  });
+
+  const renderModal = async (): Promise<RenderResult> => {
+    const modal = render(NewTransactionModal, {
+      props: { canSelectAccount: true },
+    });
+
+    const { container } = modal;
+    await waitModalIntroEnd({ container, selector: modalSelector });
+
+    return modal;
+  };
+
+  it("should navigate back and forth between step SelectAccount and SelectDestination", async () => {
+    const { container, getByText } = await renderModal();
+
+    expect(
+      getByText(en.accounts.select_source, { exact: false })
+    ).toBeInTheDocument();
+
+    const mainAccount = container.querySelector(
+      'article[role="button"]:first-of-type'
+    ) as HTMLButtonElement;
+    fireEvent.click(mainAccount);
+
+    await waitFor(() =>
+      expect(
+        getByText(en.accounts.select_destination, { exact: false })
+      ).toBeInTheDocument()
+    );
+
+    const back = container.querySelector("button.back") as HTMLButtonElement;
+    fireEvent.click(back);
+
+    await waitFor(() =>
+      expect(
+        getByText(en.accounts.select_source, { exact: false })
+      ).toBeInTheDocument()
+    );
+  });
+});
