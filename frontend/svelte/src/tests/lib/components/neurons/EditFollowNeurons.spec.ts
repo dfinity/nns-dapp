2860ed601b19402a602689049e66cb36ecba2713
Llorenç <llorenc.muntaner@dfinity.org>
2022-04-06 13:51:22 +0200

    Chore: Clean Test Output and Warnings

--- frontend/svelte/src/tests/lib/components/neurons/EditFollowNeurons.spec.ts
+++ frontend/svelte/src/tests/lib/components/neurons/EditFollowNeurons.spec.ts
@@ -5,6 +5,12 @@ import { render } from "@testing-library/svelte";
 import EditFollowNeurons from "../../../../lib/components/neurons/EditFollowNeurons.svelte";
 import { mockNeuron } from "../../../mocks/neurons.mock";
 
+jest.mock("../../../../lib/services/knownNeurons.services", () => {
+  return {
+    listKnownNeurons: jest.fn(),
+  };
+});
+
 describe("EditFollowNeurons", () => {
   // Tested in FollowNeuronsModal.spec.ts
   it("is not tested in isolation", () => {
