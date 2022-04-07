3bbe3d13ba1e89d648506dca89325fb62d64b61c
Formatting Committer <nobody@example.com>
2022-04-07 14:49:56 +0000

    Updating svelte frontend formatting

--- frontend/svelte/src/routes/Auth.svelte
+++ frontend/svelte/src/routes/Auth.svelte
@@ -66,7 +66,8 @@
     <h1>{$i18n.auth.nns}</h1>
     <h2>{$i18n.auth.ic}</h2>
     <p>{$i18n.auth.icp_governance}</p>
-    <button on:click={signIn} data-tid="login-button">{$i18n.auth.login}</button>
+    <button on:click={signIn} data-tid="login-button">{$i18n.auth.login}</button
+    >
   </main>
 
   <img
