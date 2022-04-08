fcc4728c524f0a09ea915a58900fd6d778df8653
mstrasinskis <maksims.strasinskis@dfinity.org>
2022-04-08 12:51:19 +0200

    feat: render followee neurons and topics

---
+++ frontend/svelte/src/lib/components/neuron-detail/NeuronFollowingCard/Followee.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import { Topic } from "@dfinity/nns";
+  import type { FolloweesNeuron } from "../../../utils/neuron.utils";
+  import { i18n } from "../../../stores/i18n";
+
+  export let followee: FolloweesNeuron;
+  let modalOpen = false;
+</script>
+
+<button class="text" on:click|stopPropagation={() => (modalOpen = true)}>
+  {followee.neuronId}
+</button>
+
+<ul>
+  {#each followee.topics as topic}
+    <li>{$i18n.topics[Topic[topic]]}</li>
+  {/each}
+</ul>
+
+{#if modalOpen}
+  Neuron Info
+{/if}
+
+<style lang="scss">
+  button {
+    margin: calc(0.5 * var(--padding)) 0;
+  }
+
+  ul {
+    display: flex;
+    gap: var(--padding);
+    flex-wrap: wrap;
+
+    list-style: none;
+
+    margin-bottom: var(--padding);
+    padding: 0 0 calc(2 * var(--padding));
+    border-bottom: 1px solid var(--gray-600);
+
+    &:last-of-type {
+      margin-bottom: 0;
+      padding-bottom: 0;
+      border-bottom: 0;
+    }
+  }
+
+  li {
+    display: inline-block;
+    padding: calc(0.5 * var(--padding)) var(--padding);
+
+    color: var(--brand-sea-buckthorn);
+    border: 1px solid var(--brand-sea-buckthorn);
+    border-radius: var(--border-radius);
+  }
+</style>
