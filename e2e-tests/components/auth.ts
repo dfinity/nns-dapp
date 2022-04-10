3c7870401d05c05ec000ac9d38b9c327889d56de
Max <max@dfinity.org>
2022-04-10 07:00:16 +0200

    Remove incomplete tag definitions

--- e2e-tests/components/auth.ts
+++
@@ -1,8 +0,0 @@
-export const getLoginButton = async (browser: WebdriverIO.Browser) => {
-  return await browser.$("main button", { timeout: 10_000 });
-};
-
-export const isAuthPage = async (browser: WebdriverIO.Browser) => {
-  const loginButton = await getLoginButton(browser);
-  return await loginButton.isExisting();
-};
