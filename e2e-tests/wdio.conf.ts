36c32cb11a6c9a30e6447711837ae92d4ec09c6b
Max <max@dfinity.org>
2022-04-11 22:17:56 +0200

    Sanitise screenshot filenames+count separately for each suite

--- e2e-tests/wdio.conf.ts
+++ e2e-tests/wdio.conf.ts
@@ -5,17 +5,23 @@ export const config: WebdriverIO.Config = {
   baseUrl: NNS_DAPP_URL,
 
   before: (_capabilities, _spec) => {
-    browser["screenshot-count"] = 0;
     browser["screenshots-taken"] = new Set();
+    browser["screenshot-prefix"] = "before";
+    browser["screenshot-count"] = 0;
 
     browser.addCommand("screenshot", async (name) => {
-      const countStr: string = browser["screenshots-taken"].size
+      const countStr: string = (browser["screenshot-count"]++)
         .toFixed()
         .padStart(2, "0");
-      if (true === browser["screenshots-taken"].has(name)) {
-        throw Error(`A screenshot with this name was already taken: '${name}'`);
+      const unsafeFilename = `${browser["screenshot-prefix"]}_${countStr}_${name}`;
+      // Filesystem-safe characters: ASCII alnum and hyphen.
+      // Underscore may be used to separate the components of the path.
+      const filename = unsafeFilename.replace(/[^a-zA-Z0-9_]+/g, "-");
+
+      if (true === browser["screenshots-taken"].has(filename)) {
+        throw Error(`A screenshot with this name was already taken: '${filename}'`);
       }
-      browser["screenshots-taken"].add(name);
+      browser["screenshots-taken"].add(filename);
 
       const SCREENSHOTS_DIR = "screenshots";
       if (!(existsSync(SCREENSHOTS_DIR) as boolean)) {
@@ -23,12 +29,17 @@ export const config: WebdriverIO.Config = {
       }
 
       await browser.saveScreenshot(
-        `${SCREENSHOTS_DIR}/${countStr}-${name}.png`
+        `${SCREENSHOTS_DIR}/${filename}.png`
       );
     });
   },
 
-  afterTest: function (test, context, { error }) {
+  beforeSuite: (suite) => {
+    browser["screenshot-prefix"] = suite.fullTitle;
+    browser["screenshot-count"] = 0;
+  },
+
+  afterTest: function (test, context, {error}) {
     // Take a screenshot anytime a test fails and throws an error.
     // Note: We could also use `result !== 0` or `passed === true`.
     //       The reason for conditioning on an error is that if
@@ -39,7 +50,7 @@ export const config: WebdriverIO.Config = {
     if (undefined !== error) {
       // Filenames containing spaces are painful to work with on the command line,
       // so we replace any spaces in the test name when we create the screenshot.
-      browser["screenshot"](`test-fail-${test.title.replace(/ /g, "-")}`);
+      browser["screenshot"](`test-fail_${test.title}`);
     }
   },
 
