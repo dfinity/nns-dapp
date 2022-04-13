acc2bbcd0583b16bded762110f0a2a9f1c884ad3
Formatting Committer <nobody@example.com>
2022-04-13 09:41:39 +0000

    Updating svelte frontend formatting

--- frontend/svelte/src/lib/components/ui/RouteSpinner.svelte
+++ frontend/svelte/src/lib/components/ui/RouteSpinner.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-    import Spinner from "./Spinner.svelte";
+  import Spinner from "./Spinner.svelte";
 </script>
 
 <div class="spinner" role="presentation">
-    <Spinner />
+  <Spinner />
 </div>
 
 <style lang="scss">
-    .spinner {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(0, calc(-1 * (var(--footer-height) / 5)));
-    }
+  .spinner {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(0, calc(-1 * (var(--footer-height) / 5)));
+  }
 </style>
