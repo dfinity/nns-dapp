cd44a6d103a1e22d8fd8e9d329688b556df59b7c
Llorenç <llorenc.muntaner@dfinity.org>
2022-04-14 07:19:27 +0200

    Feat: Clean up code

--- frontend/svelte/src/lib/types/errors.ts
+++ frontend/svelte/src/lib/types/errors.ts
@@ -1,33 +1,7 @@
-export class NotFoundError extends Error {
-  readonly message: string;
-  constructor(message?: string) {
-    super(message);
-    this.message = message ?? "";
-    // https://www.typescriptlang.org/docs/handbook/2/classes.html#inheriting-built-in-types
-    Object.setPrototypeOf(this, NotFoundError.prototype);
-  }
-}
+export class NotFoundError extends Error {}
 
-export class NotAuthorizedError extends Error {
-  message: string;
-  constructor(message?: string) {
-    super(message);
-    Object.setPrototypeOf(this, NotAuthorizedError.prototype);
-  }
-}
+export class NotAuthorizedError extends Error {}
 
-export class InvalidAmountError extends Error {
-  message: string;
-  constructor(message?: string) {
-    super(message);
-    Object.setPrototypeOf(this, InvalidAmountError.prototype);
-  }
-}
+export class InvalidAmountError extends Error {}
 
-export class InsufficientAmountError extends Error {
-  message: string;
-  constructor(message?: string) {
-    super(message);
-    Object.setPrototypeOf(this, InsufficientAmountError.prototype);
-  }
-}
+export class InsufficientAmountError extends Error {}
