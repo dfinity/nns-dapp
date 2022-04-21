020b927129475fa7524c43f9d8d5ac9a388aa1e7
Max <max@dfinity.org>
2022-04-21 14:03:04 +0200

    Include less in the PR

--- e2e-tests/specs/multi-tab-auth.e2e.ts
+++ e2e-tests/specs/multi-tab-auth.e2e.ts
@@ -59,36 +59,4 @@ describe("multi-tab-auth", () => {
       await navigator.getElement(AuthPage.SELECTOR);
     });
   });
-
-  // Now with login rather than registration:
-  it("loginTabOne", async () => {
-    if (identityAnchor === undefined) {
-      throw new Error(`Registration failed to provide an identity anchor.`);
-    }
-    await loginWithIdentity(browser, identityAnchor);
-    await waitForImages(browser);
-  });
-
-  it("allTabsLogInAgain", async () => {
-    await Promise.all(
-      nnsTabs.map(async (tabId, index) => {
-        await browser.switchToWindow(tabId);
-        await navigator.getElement(Header.LOGOUT_BUTTON_SELECTOR);
-        await browser["screenshot"](`register-logged-in-tab-${index}`);
-      })
-    );
-  });
-
-  it("oneTabLogsOutAgain", async () => {
-    await browser.switchToWindow(nnsTabs[0]);
-    await navigator.click(Header.LOGOUT_BUTTON_SELECTOR, "logout");
-    await navigator.getElement(AuthPage.SELECTOR);
-  });
-
-  it("allTabsLogOutAgain", async () => {
-    nnsTabs.forEach(async (tabId) => {
-      await browser.switchToWindow(tabId);
-      await navigator.getElement(AuthPage.SELECTOR);
-    });
-  });
 });
