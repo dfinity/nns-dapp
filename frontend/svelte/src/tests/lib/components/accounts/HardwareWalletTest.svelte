afe9eaef9ad20ef54f0f77fc06d9ac662225e961
David Dal Busco <david.dalbusco@dfinity.org>
2022-04-26 12:41:30 +0200

    test: hadware wallet name step

---
+++ frontend/svelte/src/tests/lib/components/accounts/HardwareWalletTest.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { setContext, SvelteComponent } from "svelte";
+  import {
+    TransactionContext,
+    transactionStore,
+  } from "../../../../lib/stores/transaction.store";
+  import { ADD_ACCOUNT_CONTEXT_KEY } from "../../../../lib/stores/add-account.store";
+
+  export let testComponent: typeof SvelteComponent;
+  export let nextCallback: () => void | undefined = undefined;
+
+  setContext<TransactionContext>(ADD_ACCOUNT_CONTEXT_KEY, {
+    store: transactionStore,
+    next: () => nextCallback?.(),
+  });
+</script>
+
+<svelte:component this={testComponent} />
