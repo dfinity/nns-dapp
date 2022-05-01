9fcf0ab559276374d886ce3751762f70f95ae573
mstrasinskis <maksims.strasinskis@dfinity.org>
2022-05-01 21:28:15 +0200

    feat: add svelte-json-tree

--- frontend/svelte/src/lib/components/proposal-detail/ProposalDetailCard/ProposalActions.svelte
+++ frontend/svelte/src/lib/components/proposal-detail/ProposalDetailCard/ProposalActions.svelte
@@ -5,11 +5,13 @@
     proposalFirstActionKey,
     proposalActionFields,
   } from "../../../../lib/utils/proposals.utils";
+  import JSONTree from "svelte-json-tree";
+  import { stringifyJson } from "../../../utils/utils";
 
   export let proposal: Proposal | undefined;
 
   let actionKey: string | undefined;
-  let actionFields: [string, string][] = [];
+  let actionFields: [string, unknown][] = [];
   $: actionKey =
     proposal !== undefined ? proposalFirstActionKey(proposal) : undefined;
   $: actionFields =
@@ -22,7 +24,18 @@
     {#each actionFields as [key, value]}
       <li>
         <h4>{key}</h4>
-        <p>{value}</p>
+        {#if typeof value === "object"}
+          <p class="json">
+            <JSONTree
+              defaultExpandedLevel={0}
+              value={JSON.parse(stringifyJson(value))}
+            />
+          </p>
+        {:else}
+          <p>
+            {value}
+          </p>
+        {/if}
       </li>
     {/each}
   </ul>
@@ -30,6 +43,29 @@
 
 <style lang="scss">
   @use "../../../themes/mixins/media";
+
+  :global(.json) {
+    --json-tree-font-size: var(--font-size-small);
+    // --json-tree-font-family: "Circular", sans-serif;
+    --json-tree-li-indentation: var(--padding);
+    --json-tree-li-line-height: var(--line-height-standard);
+    // colors
+    --json-tree-property-color: var(--gray-200);
+    --json-tree-string-color: var(--yellow-400);
+    --json-tree-symbol-color: var(--gray-200);
+    --json-tree-boolean-color: var(--blue-200);
+    --json-tree-function-color: var(--blue-200);
+    --json-tree-number-color: var(--blue-200);
+    --json-tree-label-color: var(--gray-200);
+    --json-tree-property-color: var(--background-contrast);
+    --json-tree-arrow-color: var(--blue-500);
+    --json-tree-operator-color: var(--gray-200);
+    --json-tree-null-color: var(--gray-100);
+    --json-tree-undefined-color: var(--gray-100);
+    --json-tree-date-color: var(--gray-100);
+    --json-tree-internal-color: var(--gray-100);
+    --json-tree-regex-color: var(--yellow-400);
+  }
 
   ul {
     margin: 0;
