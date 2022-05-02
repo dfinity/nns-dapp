3dff0c9519de4e8ea5389483277fcf0ab17dd742
David Dal Busco <david.dalbusco@dfinity.org>
2022-05-02 16:26:57 +0200

    test: update ndash

--- frontend/svelte/src/tests/lib/components/proposal-detail/VotingCard/VotingConfirmationToolbar.spec.ts
+++ frontend/svelte/src/tests/lib/components/proposal-detail/VotingCard/VotingConfirmationToolbar.spec.ts
@@ -140,7 +140,8 @@ describe("VotingConfirmationToolbar", () => {
       }
     )
       .replace(/<strong>/g, "")
-      .replace(/<\/strong>/g, "");
+      .replace(/<\/strong>/g, "")
+      .replace("&ndash;", "-");
 
     const { textContent }: HTMLParagraphElement = container.querySelector(
       ".question"
