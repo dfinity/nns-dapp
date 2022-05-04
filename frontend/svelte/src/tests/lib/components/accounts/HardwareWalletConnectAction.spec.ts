e6f508259dcc0853e5cbf1ffe20fae6103ae01f8
David Dal Busco <david.dalbusco@dfinity.org>
2022-05-04 11:36:35 +0200

    test: connected

--- frontend/svelte/src/tests/lib/components/accounts/HardwareWalletConnectAction.spec.ts
+++ frontend/svelte/src/tests/lib/components/accounts/HardwareWalletConnectAction.spec.ts
@@ -5,20 +5,15 @@
 import { fireEvent } from "@testing-library/dom";
 import { render, waitFor } from "@testing-library/svelte";
 import HardwareWalletConnectAction from "../../../../lib/components/accounts/HardwareWalletConnectAction.svelte";
+import { LedgerConnectionState } from "../../../../lib/constants/ledger.constants";
 import { connectToHardwareWalletProxy } from "../../../../lib/proxy/ledger.services.proxy";
+import { mockIdentity } from "../../../mocks/auth.store.mock";
 import en from "../../../mocks/i18n.mock";
-import {LedgerConnectionState} from '../../../../lib/constants/ledger.constants';
 
 jest.mock("../../../../lib/proxy/ledger.services.proxy");
 
 describe("HardwareWalletConnectAction", () => {
-  beforeAll(() => {
-    (connectToHardwareWalletProxy as jest.Mock).mockImplementation(async (callback) =>
-        callback({ connectionState: LedgerConnectionState.CONNECTING })
-    );
-  });
-
-  afterAll(() => {
+  afterEach(() => {
     jest.clearAllMocks();
     jest.restoreAllMocks();
   });
@@ -31,13 +26,49 @@ describe("HardwareWalletConnectAction", () => {
     ).toBeInTheDocument();
   });
 
-  it("should display a spinner while connecting", async () => {
-    const { getByRole, getByTestId } = render(HardwareWalletConnectAction);
+  describe("connecting", () => {
+    beforeAll(() => {
+      (connectToHardwareWalletProxy as jest.Mock).mockImplementation(
+        async (callback) =>
+          callback({ connectionState: LedgerConnectionState.CONNECTING })
+      );
+    });
 
-    fireEvent.click(getByRole("button"));
+    it("should display a spinner while connecting", async () => {
+      const { getByRole, getByTestId } = render(HardwareWalletConnectAction);
 
-    await waitFor(() => expect(getByTestId("spinner")).not.toBeNull());
+      fireEvent.click(getByRole("button"));
 
-    // expect(spy).toHaveBeenCalled();
+      await waitFor(() => expect(getByTestId("spinner")).not.toBeNull());
+    });
+  });
+
+  describe("connected", () => {
+    beforeAll(() => {
+      (connectToHardwareWalletProxy as jest.Mock).mockImplementation(
+        async (callback) =>
+          callback({
+            connectionState: LedgerConnectionState.CONNECTED,
+            ledgerIdentity: mockIdentity,
+          })
+      );
+    });
+
+    it("should display a connected innforation", async () => {
+      const { getByRole, getByText } = render(HardwareWalletConnectAction);
+
+      fireEvent.click(getByRole("button"));
+
+      await waitFor(() =>
+        expect(
+          getByText(en.accounts.hardware_wallet_connected)
+        ).toBeInTheDocument()
+      );
+
+      expect(getByText(en.core.principal)).toBeInTheDocument();
+      expect(
+        getByText(mockIdentity.getPrincipal().toString())
+      ).toBeInTheDocument();
+    });
   });
 });
