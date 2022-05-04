cac795e50b3e74a73719e99425177c0273d03925
David Dal Busco <david.dalbusco@dfinity.org>
2022-05-04 09:49:39 +0200

    test: ledger services

--- frontend/svelte/src/tests/lib/keys/secp256k1.spec.ts
+++ frontend/svelte/src/tests/lib/keys/secp256k1.spec.ts
@@ -1,24 +1,12 @@
 import type { DerEncodedPublicKey } from "@dfinity/agent";
 import { Secp256k1PublicKey } from "../../../lib/keys/secp256k1";
+import {fromHexString, testSecp256k1Vectors} from '../../mocks/ledger.identity.mock';
 
 // Copied from @dfinity/identity-ledgerhq
 
 describe("secp256k1", () => {
-  const fromHexString = (hexString: string): ArrayBuffer => {
-    return new Uint8Array(
-      (hexString.match(/.{1,2}/g) ?? []).map((byte) => parseInt(byte, 16))
-    ).buffer;
-  };
-
-  const testVectors: Array<[string, string]> = [
-    [
-      "0410d34980a51af89d3331ad5fa80fe30d8868ad87526460b3b3e15596ee58e812422987d8589ba61098264df5bb9c2d3ff6fe061746b4b31a44ec26636632b835",
-      "3056301006072A8648CE3D020106052B8104000A0342000410d34980a51af89d3331ad5fa80fe30d8868ad87526460b3b3e15596ee58e812422987d8589ba61098264df5bb9c2d3ff6fe061746b4b31a44ec26636632b835",
-    ],
-  ];
-
   test("DER encoding of secp256k1 keys", async () => {
-    testVectors.forEach(([rawPublicKeyHex, derEncodedPublicKeyHex]) => {
+    testSecp256k1Vectors.forEach(([rawPublicKeyHex, derEncodedPublicKeyHex]) => {
       const publicKey = Secp256k1PublicKey.fromRaw(
         fromHexString(rawPublicKeyHex)
       );
@@ -31,7 +19,7 @@ describe("secp256k1", () => {
   // TypeError: secp256k1 public key must be 65 bytes long (is 23)
 
   test.skip("DER decoding of ED25519 keys", async () => {
-    testVectors.forEach(([rawPublicKeyHex, derEncodedPublicKeyHex]) => {
+    testSecp256k1Vectors.forEach(([rawPublicKeyHex, derEncodedPublicKeyHex]) => {
       const derPublicKey = fromHexString(
         derEncodedPublicKeyHex
       ) as DerEncodedPublicKey;
