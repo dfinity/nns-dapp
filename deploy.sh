387f3a4adc1697b36dbe007b949780dc3ecb4e50
Max <max@dfinity.org>
2022-05-11 15:52:15 +0200

    Add helpful comment

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,10 @@ help_text() {
 
 	Usage:
 
+	Preflight: Make sure that you can run build.sh before using deploy.sh.  The build
+	  script requires a number of dependencies.  If you cannot run build.sh then
+	  "dfx deploy" will fail.
+
 	./deploy.sh
 	  Creates a local network with the nns-dapp and supporting NNS and II canisters.
 	  This is the same as: ./deploy.sh local
