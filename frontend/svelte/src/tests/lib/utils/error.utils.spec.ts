d219e7af352029ab045f22098da7b9252f9692ca
David Dal Busco <david.dalbusco@dfinity.org>
2022-05-12 10:36:15 +0200

    test: toast error parser

--- frontend/svelte/src/tests/lib/utils/error.utils.spec.ts
+++ frontend/svelte/src/tests/lib/utils/error.utils.spec.ts
@@ -1,4 +1,6 @@
-import { errorToString } from "../../../lib/utils/error.utils";
+import { HardwareWalletAttachError } from "../../../lib/canisters/nns-dapp/nns-dapp.errors";
+import { LedgerErrorMessage } from "../../../lib/errors/ledger.errors";
+import { errorToString, toToastError } from "../../../lib/utils/error.utils";
 
 class TestError extends Error {
   constructor(msg: string) {
@@ -7,17 +9,59 @@ class TestError extends Error {
 }
 
 describe("error-utils", () => {
-  it("should parse error", () => {
-    expect(errorToString("test")).toEqual("test");
+  describe("to string", () => {
+    it("should parse error", () => {
+      expect(errorToString("test")).toEqual("test");
 
-    const error = new Error("test");
-    expect(errorToString(error)).toEqual("test");
+      const error = new Error("test");
+      expect(errorToString(error)).toEqual("test");
 
-    const error2 = new TestError("test");
-    expect(errorToString(error2)).toEqual("test");
+      const error2 = new TestError("test");
+      expect(errorToString(error2)).toEqual("test");
+    });
+
+    it("should not parse error", () => {
+      expect(errorToString(undefined)).toBeUndefined();
+    });
   });
 
-  it("should not parse error", () => {
-    expect(errorToString(undefined)).toBeUndefined();
+  describe("to toast", () => {
+    it("should use fallback message", () => {
+      expect(
+        toToastError({
+          fallbackErrorLabelKey: "test.test",
+          err: undefined,
+        })
+      ).toEqual({ labelKey: "test.test" });
+
+      const err = new HardwareWalletAttachError("test");
+
+      expect(
+        toToastError({
+          fallbackErrorLabelKey: "test.test",
+          err,
+        })
+      ).toEqual({ labelKey: "test.test", err });
+
+      expect(
+        toToastError({
+          fallbackErrorLabelKey: "test.test",
+          errorsWithMessage: [LedgerErrorMessage],
+          err,
+        })
+      ).toEqual({ labelKey: "test.test", err });
+    });
+
+    it("should use error message key", () => {
+      const err = new HardwareWalletAttachError("test.error");
+
+      expect(
+        toToastError({
+          fallbackErrorLabelKey: "test.test",
+          errorsWithMessage: [HardwareWalletAttachError],
+          err,
+        })
+      ).toEqual({ labelKey: "test.error" });
+    });
   });
 });
