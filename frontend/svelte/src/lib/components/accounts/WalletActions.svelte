735cd83aabb8f9f22dbc6233e8eedde3edc6b854
mstrasinskis <maksims.strasinskis@dfinity.org>
2022-05-16 09:26:27 +0200

    refactor: WalletActions to context store

--- frontend/svelte/src/lib/components/accounts/WalletActions.svelte
+++ frontend/svelte/src/lib/components/accounts/WalletActions.svelte
@@ -2,8 +2,17 @@
   import type { Account, AccountType } from "../../types/account";
   import RenameSubAccount from "./RenameSubAccount.svelte";
   import HardwareWalletShowAction from "./HardwareWalletShowAction.svelte";
+  import { getContext } from "svelte";
+  import {
+    SELECTED_ACCOUNT_CONTEXT_KEY,
+    type SelectedAccountContext,
+  } from "../../stores/selectedAccount.store";
 
-  export let selectedAccount: Account | undefined;
+  const selectedAccountStore = getContext<SelectedAccountContext>(
+    SELECTED_ACCOUNT_CONTEXT_KEY
+  );
+  let selectedAccount: Account | undefined;
+  $: selectedAccount = $selectedAccountStore.account;
 
   let type: AccountType | undefined;
   $: type = selectedAccount?.type;
