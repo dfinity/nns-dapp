6ca140dd0fa0467af06f223e6d16245c2ec879ad
Hamish Peebles <hamishpeebles@gmail.com>
2022-05-16 13:53:28 +0100

    Call directly into CMC to create or top up a canister

---
+++ rs/src/canisters/cmc.rs
@@ -0,0 +1,17 @@
+use cycles_minting_canister::{NotifyCreateCanister, NotifyError, NotifyTopUp};
+use dfn_core::CanisterId;
+use dfn_candid::candid;
+use ic_nns_constants::CYCLES_MINTING_CANISTER_ID;
+use crate::Cycles;
+
+pub async fn notify_create_canister(request: NotifyCreateCanister) -> Result<Result<CanisterId, NotifyError>, String> {
+    dfn_core::call(CYCLES_MINTING_CANISTER_ID, "notify_create_canister", candid, (request,))
+        .await
+        .map_err(|e| e.1)
+}
+
+pub async fn notify_top_up_canister(request: NotifyTopUp) -> Result<Result<Cycles, NotifyError>, String> {
+    dfn_core::call(CYCLES_MINTING_CANISTER_ID, "notify_top_up", candid, (request,))
+        .await
+        .map_err(|e| e.1)
+}
