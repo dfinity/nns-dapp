40525ee1f243fe4c6aa3f8b14ca5ce3ae0f40e98
Max <max@dfinity.org>
2022-05-18 06:49:58 +0200

    var to let

--- config.ts
+++ config.ts
@@ -13,7 +13,7 @@ import * as configFromFile from "./frontend/ts/src/config.json";
  * Returns the given environment variable, if defined and non-empty, else throws an error.
  */
 export function getRequiredEnvVar(key) {
-  var value = process.env[key];
+  let value = process.env[key];
   if (undefined === value || value === "") {
     value = configFromFile[key];
   }
