10498507a22643b7a4a57b16ab8a97a0f0da8539
mstrasinskis <98811342+mstrasinskis@users.noreply.github.com>
2022-05-23 16:46:42 +0200

    Debug stores (#882)
    * feat: collect all stores
    
    * feat: digestText
    
    * feat: debug store anonymisation
    
    * feat: hidden trigger and fixes
    
    * fix: anonymizeBallot
    
    * feat: cutAndAnonymize
    
    * refactor: anonymize.utils
    
    * feat: switch to saveToJSONFile (showSaveFilePicker)
    
    * refactor: make debugLog dynamically imported

--- frontend/svelte/src/lib/utils/dev.utils.ts
+++ frontend/svelte/src/lib/utils/dev.utils.ts
@@ -1,3 +1,5 @@
+import { generateDebugLogProxy } from "../proxy/debug.services.proxy";
+
 export const isNode = (): boolean =>
   typeof process !== "undefined" &&
   process.versions != null &&
@@ -26,3 +28,39 @@ export const hashCode = (value: string | bigint | number): string =>
   )
     .toString(36)
     .toUpperCase();
+
+// https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
+export const digestText = async (text: string): Promise<string> => {
+  const msgUint8 = new TextEncoder().encode(text);
+  const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8);
+  // convert buffer to byte array
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  // convert bytes to hex string
+  const hashHex = hashArray
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+  return hashHex;
+};
+
+/**
+ * To not have "nns-state" in the code
+ * @returns "nns-state"
+ */
+const TRIGGER_PHRASE = [101, 116, 97, 116, 115, 45, 115, 110, 110]
+  .reverse()
+  .map((c) => String.fromCharCode(c))
+  .join("");
+
+/**
+ * Add console.log with a version that logs the stores on TRIGGER_PHRASE
+ */
+export const bindDebugGenerator = () => {
+  const originalLog = console.log;
+  console.log = function (...args) {
+    if (args.length === 1 && args[0] === TRIGGER_PHRASE) {
+      generateDebugLogProxy().then();
+    } else {
+      originalLog.apply(this, args);
+    }
+  };
+};
