e3b75fa134eb321c4815ef4a61de9c990536352a
Llorenç <llorenc.muntaner@dfinity.org>
2022-05-25 07:39:51 +0200

    L2-585: CR fixes

--- frontend/svelte/src/lib/canisters/cmc/cmc.canister.ts
+++ frontend/svelte/src/lib/canisters/cmc/cmc.canister.ts
@@ -36,12 +36,10 @@ export class CMCCanister {
    * @returns Promise<BigInt>
    */
   public getIcpToCyclesConversionRate = async (): Promise<bigint> => {
-    const response = await this.service.get_icp_xdr_conversion_rate();
+    const { data } = await this.service.get_icp_xdr_conversion_rate();
 
     // TODO: validate the certificate in the response - https://dfinity.atlassian.net/browse/FOLLOW-223
-    return (
-      (response.data.xdr_permyriad_per_icp * CYCLES_PER_XDR) / BigInt(10_000)
-    );
+    return (data.xdr_permyriad_per_icp * CYCLES_PER_XDR) / BigInt(10_000);
   };
 
   /**
