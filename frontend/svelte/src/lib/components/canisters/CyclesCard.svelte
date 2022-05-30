e82a5115a01306e3ab277a4a2517bc30ec361ae4
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2022-05-30 08:51:20 +0200

    L2-334: Setup Canister Detail Page (#910)
    # Motivation
    
    User can see the canister title in the canister detail page.
    
    # Changes
    
    * New canister service "loadCanisterDetails" that fetches canister details and adds them to the store.
    * New store "canisterDetailsStore" which stores the data of a canister. It's a different type than from nns dapp.
    * New canister service "routePathCanisterId" to read the canister id from the url.
    * Setup of "ControllersCard" and "CyclesCard" to help parallel development.
    * CanisterDetails route component triggers fetching canisters and details and read them from the stores.
    
    # Tests
    
    * New test for new two canister services.
    * Extend CanisterDetails route tests.

---
+++ frontend/svelte/src/lib/components/canisters/CyclesCard.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { i18n } from "../../stores/i18n";
+  import Card from "../ui/Card.svelte";
+
+  // TODO: Add Cycles - https://dfinity.atlassian.net/browse/L2-598
+  // TODO: UI - https://dfinity.atlassian.net/browse/L2-599
+</script>
+
+<Card testId="canister-cycles-card">
+  <h4 slot="start">{$i18n.canister_detail.cycles}</h4>
+</Card>
