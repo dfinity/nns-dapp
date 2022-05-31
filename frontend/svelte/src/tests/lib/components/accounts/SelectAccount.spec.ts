a0de1f1713c9019f7ba7c5e8c03eb8237b4d7174
Formatting Committer <nobody@example.com>
2022-05-31 12:40:58 +0000

    Updating svelte frontend formatting

--- frontend/svelte/src/tests/lib/components/accounts/SelectAccount.spec.ts
+++ frontend/svelte/src/tests/lib/components/accounts/SelectAccount.spec.ts
@@ -76,7 +76,7 @@ describe("SelectAccount", () => {
     });
 
     await waitFor(() =>
-        expect(queryByText(en.accounts.my_accounts)).toBeInTheDocument()
+      expect(queryByText(en.accounts.my_accounts)).toBeInTheDocument()
     );
 
     accountsStore.reset();
