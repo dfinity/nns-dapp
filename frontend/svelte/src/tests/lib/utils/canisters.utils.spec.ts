09251abe9323eafe011bfc64d03388124f430779
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2022-05-31 08:25:52 +0200

    L2-599: Canister Detail Cards UI (#919)
    # Motivation
    
    User can see canister cycles and controllers.
    
    # Changes
    
    * Implement the UI in the CyclesCard and ControllersCard.
    * Add a canister util "formatCyclesToTCycles"
    * Reuse style from HotkeysCard.
    
    # Tests
    
    * Test UI rendering in CyclesCard and ControllersCard.
    * Test for new canister util "formatCyclesToTCycles".

--- frontend/svelte/src/tests/lib/utils/canisters.utils.spec.ts
+++ frontend/svelte/src/tests/lib/utils/canisters.utils.spec.ts
@@ -1,5 +1,8 @@
 import { Principal } from "@dfinity/principal";
-import { getCanisterInfoById } from "../../../lib/utils/canisters.utils";
+import {
+  formatCyclesToTCycles,
+  getCanisterInfoById,
+} from "../../../lib/utils/canisters.utils";
 import { mockCanisters } from "../../mocks/canisters.mock";
 
 describe("canister-utils", () => {
@@ -37,6 +40,15 @@ describe("canister-utils", () => {
         canistersStore: store,
       });
       expect(canister).toBeUndefined();
+    });
+  });
+
+  describe("formatCyclesToTCycles", () => {
+    it("formats cycles into T Cycles with three decimals of accuracy", () => {
+      expect(formatCyclesToTCycles(BigInt(1_000_000_000_000))).toBe("1.000");
+      expect(formatCyclesToTCycles(BigInt(876_500_000_000))).toBe("0.877");
+      expect(formatCyclesToTCycles(BigInt(876_400_000_000))).toBe("0.876");
+      expect(formatCyclesToTCycles(BigInt(10_120_000_000_000))).toBe("10.120");
     });
   });
 });
