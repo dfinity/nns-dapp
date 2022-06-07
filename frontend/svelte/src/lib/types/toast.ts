a7e6275834db6321588b95790444e96870840afa
David Dal Busco <david.dalbusco@dfinity.org>
2022-06-07 10:02:57 +0200

    fix: declaration substitutions in toast

--- frontend/svelte/src/lib/types/toast.ts
+++ frontend/svelte/src/lib/types/toast.ts
@@ -4,7 +4,6 @@ export type ToastLevel = "success" | "warn" | "error";
 
 export interface ToastMsg {
   labelKey: string;
-  substitutions?: { [from: string]: string };
   level: ToastLevel;
   detail?: string;
   duration?: number;
