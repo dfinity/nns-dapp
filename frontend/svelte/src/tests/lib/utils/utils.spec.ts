4cd9fb48c673992480e6020b2f8d896515160ac3
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2022-06-13 19:14:38 +0200

    L2-679: Improve merge maturity error with HW (#983)
    # Motivation
    
    User sees proper error when merging maturity of a HW neuron with old version.
    
    # Changes
    
    * Add ledger service "assertLedgerVersion" that throws if Ledger version is less than minVersion.
    * Use ledger service in "mergeMaturity" service.
    * New util "smallerVersion" that returns boolean comparing two versions.
    * Manage LedgerErrorMessage type errors separately in error mapping.
    
    # Tests
    
    * Test for new util.
    * Test for new ledger service.

--- frontend/svelte/src/tests/lib/utils/utils.spec.ts
+++ frontend/svelte/src/tests/lib/utils/utils.spec.ts
@@ -6,6 +6,7 @@ import {
   isHash,
   isNullable,
   nonNullable,
+  smallerVersion,
   stringifyJson,
   uniqueObjects,
 } from "../../../lib/utils/utils";
@@ -214,6 +215,105 @@ describe("utils", () => {
       expect(isHash(bytes(""))).toBe(false);
       expect(isHash(bytes(NaN))).toBe(false);
       expect(isHash(bytes(Infinity))).toBe(false);
+    });
+  });
+
+  describe("smallerVersion", () => {
+    it("returns true if current version is smaller than min version", () => {
+      expect(
+        smallerVersion({
+          minVersion: "1.0",
+          currentVersion: "0.0.9",
+        })
+      ).toBe(true);
+      expect(
+        smallerVersion({
+          minVersion: "2.0.0",
+          currentVersion: "1.9.9",
+        })
+      ).toBe(true);
+      expect(
+        smallerVersion({
+          minVersion: "2.1.5",
+          currentVersion: "2.1.4",
+        })
+      ).toBe(true);
+      expect(
+        smallerVersion({
+          minVersion: "2.1.5",
+          currentVersion: "1.8.9",
+        })
+      ).toBe(true);
+      expect(
+        smallerVersion({
+          minVersion: "2",
+          currentVersion: "1",
+        })
+      ).toBe(true);
+    });
+    it("returns false if current version is bigger than min version", () => {
+      expect(
+        smallerVersion({
+          minVersion: "0.0.9",
+          currentVersion: "1.0",
+        })
+      ).toBe(false);
+      expect(
+        smallerVersion({
+          minVersion: "1.9.9",
+          currentVersion: "2.0.0",
+        })
+      ).toBe(false);
+      expect(
+        smallerVersion({
+          minVersion: "2.1.4",
+          currentVersion: "2.1.5",
+        })
+      ).toBe(false);
+      expect(
+        smallerVersion({
+          minVersion: "1.8.9",
+          currentVersion: "2.1.5",
+        })
+      ).toBe(false);
+      expect(
+        smallerVersion({
+          minVersion: "1",
+          currentVersion: "2",
+        })
+      ).toBe(false);
+    });
+    it("returns false if current version is same as min version", () => {
+      expect(
+        smallerVersion({
+          minVersion: "1",
+          currentVersion: "1.0",
+        })
+      ).toBe(false);
+      expect(
+        smallerVersion({
+          minVersion: "2",
+          currentVersion: "2.0.0",
+        })
+      ).toBe(false);
+      expect(
+        smallerVersion({
+          minVersion: "2.1.4",
+          currentVersion: "2.1.4",
+        })
+      ).toBe(false);
+      expect(
+        smallerVersion({
+          minVersion: "1.0.0",
+          currentVersion: "1",
+        })
+      ).toBe(false);
+      expect(
+        smallerVersion({
+          minVersion: "13.4.5",
+          currentVersion: "13.4.5",
+        })
+      ).toBe(false);
     });
   });
 });
