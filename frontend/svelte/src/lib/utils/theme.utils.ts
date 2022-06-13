6fd6e63242018fce72af57cfbb94e468809ed155
David Dal Busco <david.dalbusco@dfinity.org>
2022-06-13 16:38:19 +0200

    docs: theme-color effect

--- frontend/svelte/src/lib/utils/theme.utils.ts
+++ frontend/svelte/src/lib/utils/theme.utils.ts
@@ -8,6 +8,9 @@ export const applyTheme = (theme: Theme) => {
   const color: string = getComputedStyle(documentElement).getPropertyValue(
     "--gray-50-background"
   );
+
+  // Update theme-color for mobile devices to customize the display of the page or of the surrounding user interface
+  // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/meta/name/theme-color
   head?.children
     ?.namedItem("theme-color")
     ?.setAttribute("content", color.trim());
