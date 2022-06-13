b3fdc714873032c6f589d39b7bd1fe9f14182cdd
David Dal Busco <david.dalbusco@dfinity.org>
2022-06-13 10:31:38 +0200

    test: select theme with store

---
+++ frontend/svelte/src/tests/lib/stores/theme.store.spec.ts
@@ -0,0 +1,25 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { get } from "svelte/store";
+import { theme } from "../../../lib/stores/theme.store";
+import * as themeUtils from "../../../lib/utils/theme.utils";
+
+enum TestTheme {
+  DARK = "dark",
+  LIGHT = "light",
+}
+
+describe("theme-store", () => {
+  it("should select theme", () => {
+    const applyTheme = jest.spyOn(themeUtils, "applyTheme");
+
+    // @ts-ignore we have just one theme at the moment therefore we use pseudo themes for test purpose
+    theme.select(TestTheme.LIGHT);
+
+    const themeValue = get(theme);
+    expect(themeValue).toEqual(`${TestTheme.LIGHT}`);
+    expect(applyTheme).toHaveBeenCalled();
+  });
+});
