8b147a867e8e50ab035e7b1573740f0c30fa9bc6
Formatting Committer <nobody@example.com>
2022-06-15 07:09:25 +0000

    Updating svelte frontend formatting

--- frontend/svelte/src/tests/lib/components/common/Nav.spec.ts
+++ frontend/svelte/src/tests/lib/components/common/Nav.spec.ts
@@ -3,9 +3,8 @@
  */
 
 import { render } from "@testing-library/svelte";
-import Menu from "../../../../lib/components/common/Menu.svelte";
-import Nav from '../../../../lib/components/common/Nav.svelte';
-import en from '../../../mocks/i18n.mock';
+import Nav from "../../../../lib/components/common/Nav.svelte";
+import en from "../../../mocks/i18n.mock";
 
 describe("Nav", () => {
   const shouldRenderMenuItem = (context: string) => {
@@ -14,10 +13,12 @@ describe("Nav", () => {
     expect(link).not.toBeNull();
     expect(link).toBeVisible();
     expect(link.textContent?.trim()).toEqual(en.navigation[context]);
-  }
+  };
 
-  it("should render accounts menu item", () => shouldRenderMenuItem('accounts'));
-  it("should render neurons menu item", () => shouldRenderMenuItem('accounts'));
-  it("should render voting menu item", () => shouldRenderMenuItem('accounts'));
-  it("should render canisters menu item", () => shouldRenderMenuItem('accounts'));
+  it("should render accounts menu item", () =>
+    shouldRenderMenuItem("accounts"));
+  it("should render neurons menu item", () => shouldRenderMenuItem("accounts"));
+  it("should render voting menu item", () => shouldRenderMenuItem("accounts"));
+  it("should render canisters menu item", () =>
+    shouldRenderMenuItem("accounts"));
 });
