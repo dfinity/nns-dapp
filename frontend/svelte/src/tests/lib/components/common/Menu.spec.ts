ec32c17f30b59fd853a7d208f6756b4d7e1013e5
David Dal Busco <david.dalbusco@dfinity.org>
2022-06-15 14:35:47 +0200

    test: open menu

--- frontend/svelte/src/tests/lib/components/common/Menu.spec.ts
+++ frontend/svelte/src/tests/lib/components/common/Menu.spec.ts
@@ -2,12 +2,45 @@
  * @jest-environment jsdom
  */
 
-import { render } from "@testing-library/svelte";
+import { fireEvent, render, waitFor } from "@testing-library/svelte";
 import Menu from "../../../../lib/components/common/Menu.svelte";
 
 describe("Menu", () => {
-  it("should not render a get icps feature", () => {
-    const { getByTestId } = render(Menu);
+  it("menu should be closed per default", () => {
+    const { getByRole } = render(Menu);
+    expect(() => getByRole("menu")).toThrow();
+  });
+
+  const openMenu = async ({ getByTestId, getByRole }) => {
+    const button = getByTestId("menu") as HTMLButtonElement;
+    await fireEvent.click(button);
+
+    await waitFor(() => expect(getByRole("menu")).not.toBeNull());
+  };
+
+  it("should open the menu", async () => {
+    const renderResult = render(Menu);
+
+    await openMenu(renderResult);
+  });
+
+  it("should not render a get icps feature", async () => {
+    const renderResult = render(Menu);
+
+    await openMenu(renderResult);
+
+    const { getByTestId } = renderResult;
+
     expect(() => getByTestId("get-icp-button")).toThrow();
+  });
+
+  it("should render navigation", async () => {
+    const renderResult = render(Menu);
+
+    await openMenu(renderResult);
+
+    const { getAllByRole } = renderResult;
+
+    expect(getAllByRole("menuitem").length > 0).toBeTruthy();
   });
 });
