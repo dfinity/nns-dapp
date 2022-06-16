97c26089927677ca153ba1bd8cd1b91a9e1cc4c5
Max <max@dfinity.org>
2022-06-16 11:47:20 +0200

    Make a rollback proposal text as well

--- PROPOSAL_TEMPLATE.sh
+++ PROPOSAL_TEMPLATE.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 cd "$(dirname "$0")"
-mkdir -p release
+mkdir -p release/ci
+PR="$(git log -n1 tags/release-candidate --oneline | awk '{print $(NF)}' | tr -cd 0-9)"
+CI="https://github.com/dfinity/nns-dapp/pull/${CI}/checks"
+WASM="release/ci/nns-dapp.wasm"
+if test -f "$WASM"
+then SHA="$(sha256sum "$WASM" | awk '{print $1}')"
 cat <<EOF >release/PROPOSAL.md
 # Upgrade frontend NNS Dapp canister to commit \`$(git rev-parse tags/release-candidate)\`
-Wasm sha256 hash: \`<WASM_HASH>\` (\`<LINK_TO_GITHUB_ACTION>\`)
+Wasm sha256 hash: \`${SHA}\` (\`${CI}\`)
 
 ## Change Log:
 
@@ -18,6 +23,22 @@ Wasm sha256 hash: \`<WASM_HASH>\` (\`<LINK_TO_GITHUB_ACTION>\`)
 + bash -xc "git log --format='%C(auto) %h %s' $(git rev-parse --short tags/prod)..$(git rev-parse --short tags/release-candidate)"
 $(bash -xc "git log --format='%C(auto) %h %s' $(git rev-parse --short tags/prod)..$(git rev-parse --short tags/release-candidate)")
 \`\`\`
+
+## Wasm Verification
+
+To build the wasm module yourself and verify its hash, run the following commands from the root of the nns-dapp repo:
+
+git pull  # to ensure you have the latest changes.
+git checkout \`$(git rev-parse tags/release-candidate)\`
+./scripts/docker-build
+sha256sum nns-dapp.wasm
+EOF
+
+
+
+cat <<EOF >release/ROLLBACK.md
+# Downgrade frontend NNS Dapp canister to commit \`$(git rev-parse tags/release-candidate)\`
+Wasm sha256 hash: \`${SHA}\` (\`${WASM}\`)
 
 ## Wasm Verification
 
