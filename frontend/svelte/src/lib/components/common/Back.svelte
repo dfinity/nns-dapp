10273092daf009e07d98ca93a1d05447fa6b39f4
Llorenç <llorenc.muntaner@dfinity.org>
2022-06-21 16:52:24 +0200

    Fix: Update chrome driver

--- frontend/svelte/src/lib/components/common/Back.svelte
+++ frontend/svelte/src/lib/components/common/Back.svelte
@@ -2,15 +2,18 @@
   import IconBackIosNew from "../../icons/IconBackIosNew.svelte";
   import { i18n } from "../../stores/i18n";
   import { createEventDispatcher } from "svelte";
+  import Tooltip from "../ui/Tooltip.svelte";
 
   const dispatch = createEventDispatcher();
 </script>
 
-<button
-  class="back icon-only"
-  on:click|stopPropagation={() => dispatch("nnsBack")}
-  aria-label={$i18n.core.back}><IconBackIosNew /></button
->
+<Tooltip id="back" text={$i18n.core.back} slot="start">
+  <button
+    class="back icon-only"
+    on:click|stopPropagation={() => dispatch("nnsBack")}
+    aria-label={$i18n.core.back}><IconBackIosNew /></button
+  >
+</Tooltip>
 
 <style lang="scss">
   @use "../../themes/mixins/header";
