69d9e2e2941bc44bb90b49021d456dabe3300c09
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2022-06-21 08:17:48 +0200

    L2-721: i18n Canister api errors (#1008)
    # Motivation
    
    Errors from polling service in canisters api are translatable.
    
    # Changes
    
    * New Error type `ApiErrorKey` thrown when polling is exceeded.
    
    # Tests
    
    No extra tests

--- frontend/svelte/src/lib/types/i18n.d.ts
+++ frontend/svelte/src/lib/types/i18n.d.ts
@@ -90,6 +90,8 @@ interface I18nError {
   canister_top_up_unknown: string;
   canister_update_settings: string;
   not_canister_controller_to_update: string;
+  limit_exceeded_topping_up_canister: string;
+  limit_exceeded_creating_canister: string;
   canister_invalid_transaction: string;
 }
 
