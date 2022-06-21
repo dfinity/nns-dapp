41f4f0560ff42dc80a3faaa2a9ce28a2a205ad22
David Dal Busco <david.dalbusco@dfinity.org>
2022-06-21 16:48:29 +0200

    test: use data-tid to select back

--- e2e-tests/components/proposals-tab.ts
+++ e2e-tests/components/proposals-tab.ts
@@ -6,7 +6,7 @@ export class ProposalsTab extends MyNavigator {
   static readonly SELECTOR: string = `[data-tid="proposals-tab"]`;
   static readonly PROPOSAL_TALLY_SELECTOR: string = ".latest-tally";
   static readonly PROPOSAL_FILTER_APPLY_SELECTOR: string = `[data-tid="apply-proposals-filter"]`;
-  static readonly BACK_TO_PROPOSAL_LIST_SELECTOR: string = "button.back";
+  static readonly HEADER_BACK_SELECTOR: string = '[data-tid="back"]';
 
   public static proposalIdSelector(proposalId: number): string {
     return `[data-tid="proposal-id"][data-proposal-id="${proposalId}"]`;
