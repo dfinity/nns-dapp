10c81b20be2e7ec7eae97a2900ce92cfcf81eaf2
David Dal Busco <david.dalbusco@dfinity.org>
2022-06-24 07:26:15 +0200

    fix: glitchy signout

--- frontend/svelte/src/lib/services/auth.services.ts
+++ frontend/svelte/src/lib/services/auth.services.ts
@@ -22,8 +22,7 @@ export const logout = async ({
   window.localStorage.clear();
 
   // We reload the page to make sure all the states are cleared
-  // Trigger this reload after a brief timeout to allow the rendering of the sign-in page first otherwise the ui might be displayed as glitchy (login screen rendered over current view)
-  setTimeout(() => window.location.reload(), 50);
+  window.location.reload();
 };
 
 /**
