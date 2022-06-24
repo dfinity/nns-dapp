4bff988819a96305c2606fcd7f6fd80f96ef33ca
mstrasinskis <maksims.strasinskis@dfinity.org>
2022-06-24 14:26:09 +0200

    chore: remove comments

--- frontend/svelte/src/lib/services/sns.services.ts
+++ frontend/svelte/src/lib/services/sns.services.ts
@@ -12,13 +12,5 @@ export const loadSnsSwapState = async (
 ): Promise<SnsSwapState> =>
   mockWaiting(() => SNS_SWAP_STATES_MAP[rootCanisterId.toText()]);
 
-// 0. separate data that rarely changed from another
-// 1. prepare mock + PR
-// 2. notion with a field table whre it comes from
-
-// the current light version ok
-// 3. drop down mobile
-// 4. modal?
-
 export const listSnsSummary = async (): Promise<SnsSummary[]> =>
   mockWaiting(() => SNS_SUMMARY_LIST);
