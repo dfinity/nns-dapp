90eb1d0b131069503d3e1ec13a8e325d011dc601
mstrasinskis <98811342+mstrasinskis@users.noreply.github.com>
2022-06-27 21:08:09 +0200

    L2 747 launches block UI (#1043)
    * feat: SnsProjects.store
    
    * chore: remove logs
    
    * fix: sns mock types and values
    
    * feat: projects meta info
    
    * feat: display Deadline value as duration
    
    * feat: ICP inheritSize prop
    
    * refactor: move mocks under tests
    
    * test: SnsLaunchpad related tests
    
    * chore: review changes
    
    * chore: remove redundant field

--- frontend/svelte/src/lib/services/sns.services.ts
+++ frontend/svelte/src/lib/services/sns.services.ts
@@ -1,16 +1,40 @@
 import type { Principal } from "@dfinity/principal";
 import {
-  mockWaiting,
-  SNS_SUMMARY_LIST,
-  SNS_SWAP_STATES_MAP,
-  type SnsSummary,
-  type SnsSwapState,
-} from "./sns.services.mock";
+  mockSnsSummaryList,
+  mockSnsSwapState,
+} from "../../tests/mocks/sns-projects.mock";
+import { mockAbout5SecondsWaiting } from "../../tests/mocks/utils.mock";
+import {
+  snsSummariesStore,
+  snsSwapStatesStore,
+} from "../stores/snsProjects.store";
+import type { SnsSummary, SnsSwapState } from "./sns.mock";
+
+/**
+ * Loads summaries with swapStates
+ */
+export const loadSnsFullProjects = async () => {
+  const summaries = await listSnsSummary();
+
+  snsSummariesStore.setSummaries({
+    summaries,
+    certified: true,
+  });
+
+  for (const { rootCanisterId } of summaries) {
+    loadSnsSwapState(rootCanisterId).then((swapState) =>
+      snsSwapStatesStore.setSwapState({
+        swapState,
+        certified: true,
+      })
+    );
+  }
+};
 
 export const loadSnsSwapState = async (
   rootCanisterId: Principal
 ): Promise<SnsSwapState> =>
-  mockWaiting(() => SNS_SWAP_STATES_MAP[rootCanisterId.toText()]);
+  mockAbout5SecondsWaiting(() => mockSnsSwapState(rootCanisterId));
 
 export const listSnsSummary = async (): Promise<SnsSummary[]> =>
-  mockWaiting(() => SNS_SUMMARY_LIST);
+  mockAbout5SecondsWaiting(() => mockSnsSummaryList);
--- frontend/svelte/src/lib/types/i18n.d.ts
+++ frontend/svelte/src/lib/types/i18n.d.ts
@@ -522,9 +522,16 @@ interface I18nNeuron_detail {
 
 interface I18nSns_launchpad {
   header: string;
-  current_launches: string;
+  projects: string;
+  no_projects: string;
   proposals: string;
+  no_proposals: string;
+}
+
+interface I18nSns_project {
   project: string;
+  deadline: string;
+  your_commitment: string;
 }
 
 interface I18nSns_project_detail {
@@ -633,6 +640,7 @@ interface I18n {
   nns_function_names: I18nNns_function_names;
   neuron_detail: I18nNeuron_detail;
   sns_launchpad: I18nSns_launchpad;
+  sns_project: I18nSns_project;
   sns_project_detail: I18nSns_project_detail;
   time: I18nTime;
   error__ledger: I18nError__ledger;
