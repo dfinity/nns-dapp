0bdd71529b21039d38f2584662c33c5d4fb09fc4
mstrasinskis <maksims.strasinskis@dfinity.org>
2022-06-29 22:06:02 +0200

    feat: New component

---
+++ frontend/svelte/src/lib/components/ui/New.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import IconFiberNew from "../../icons/IconFiberNew.svelte";
+</script>
+
+<span>
+  <IconFiberNew />
+</span>
+
+<style lang="scss">
+  span {
+    width: var(--padding-4x);
+    height: auto;
+    line-height: 0;
+    color: var(--primary);
+
+    :global(svg) {
+      width: 100%;
+      height: auto;
+    }
+  }
+</style>
