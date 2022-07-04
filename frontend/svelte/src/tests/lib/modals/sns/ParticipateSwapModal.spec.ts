9b6eb6491f687cf6a943db0d050089f41868515d
Llorenç <llorenc.muntaner@dfinity.org>
2022-07-04 17:51:17 +0200

    L2-795: CR changes

--- frontend/svelte/src/tests/lib/modals/sns/ParticipateSwapModal.spec.ts
+++ frontend/svelte/src/tests/lib/modals/sns/ParticipateSwapModal.spec.ts
@@ -18,70 +18,95 @@ describe("ParticipateSwapModal", () => {
     });
   };
 
-  beforeEach(() => {
-    jest
-      .spyOn(accountsStore, "subscribe")
-      .mockImplementation(mockAccountsStoreSubscribe());
+  describe("accountsStore is populated", () => {
+    beforeEach(() => {
+      jest
+        .spyOn(accountsStore, "subscribe")
+        .mockImplementation(mockAccountsStoreSubscribe());
+    });
+
+    it("should display modal", async () => {
+      const { container } = await renderSwapModal();
+
+      expect(container.querySelector("div.modal")).not.toBeNull();
+    });
+
+    it("should display dropdown to select account and input to add amount", async () => {
+      const { queryByTestId, container } = await renderSwapModal();
+
+      expect(queryByTestId("select-account-dropdown")).toBeInTheDocument();
+      expect(
+        container.querySelector("input[name='amount']")
+      ).toBeInTheDocument();
+    });
+
+    it("should trigger close on cancel", async () => {
+      const { queryByTestId, component } = await renderSwapModal();
+
+      const onClose = jest.fn();
+      component.$on("nnsClose", onClose);
+
+      await clickByTestId(queryByTestId, "sns-swap-participate-button-cancel");
+      await waitFor(() => expect(onClose).toBeCalled());
+    });
+
+    it("should have disabled button by default", async () => {
+      const { queryByTestId } = await renderSwapModal();
+
+      const participateButton = queryByTestId(
+        "sns-swap-participate-button-next"
+      );
+      expect(participateButton?.hasAttribute("disabled")).toBeTruthy();
+    });
+
+    it("should enable button when input value changes", async () => {
+      const { queryByTestId, container } = await renderSwapModal();
+
+      const participateButton = queryByTestId(
+        "sns-swap-participate-button-next"
+      );
+      expect(participateButton?.hasAttribute("disabled")).toBeTruthy();
+
+      const input = container.querySelector("input[name='amount']");
+      input && fireEvent.input(input, { target: { value: "10" } });
+      await waitFor(() =>
+        expect(participateButton?.hasAttribute("disabled")).toBeFalsy()
+      );
+    });
+
+    it("should move to the last step", async () => {
+      const { queryByTestId, container } = await renderSwapModal();
+
+      const participateButton = queryByTestId(
+        "sns-swap-participate-button-next"
+      );
+      expect(participateButton?.hasAttribute("disabled")).toBeTruthy();
+
+      const input = container.querySelector("input[name='amount']");
+      input && fireEvent.input(input, { target: { value: "10" } });
+      await waitFor(() =>
+        expect(participateButton?.hasAttribute("disabled")).toBeFalsy()
+      );
+
+      participateButton && fireEvent.click(participateButton);
+      await waitFor(() =>
+        expect(queryByTestId("sns-swap-participate-step-2")).toBeFalsy()
+      );
+    });
   });
 
-  it("should display modal", async () => {
-    const { container } = await renderSwapModal();
+  describe("accountsStore is empty", () => {
+    it("should have disabled button if no account is selected", async () => {
+      const { queryByTestId, container } = await renderSwapModal();
 
-    expect(container.querySelector("div.modal")).not.toBeNull();
-  });
+      const participateButton = queryByTestId(
+        "sns-swap-participate-button-next"
+      );
 
-  it("should display dropdown to select account and input to add amount", async () => {
-    const { queryByTestId, container } = await renderSwapModal();
+      const input = container.querySelector("input[name='amount']");
+      input && (await fireEvent.input(input, { target: { value: "10" } }));
 
-    expect(queryByTestId("select-account-dropdown")).toBeInTheDocument();
-    expect(container.querySelector("input[name='amount']")).toBeInTheDocument();
-  });
-
-  it("should trigger close on cancel", async () => {
-    const { queryByTestId, component } = await renderSwapModal();
-
-    const onClose = jest.fn();
-    component.$on("nnsClose", onClose);
-
-    await clickByTestId(queryByTestId, "sns-swap-participate-button-cancel");
-    await waitFor(() => expect(onClose).toBeCalled());
-  });
-
-  it("should have disabled button by default", async () => {
-    const { queryByTestId } = await renderSwapModal();
-
-    const participateButton = queryByTestId("sns-swap-participate-button-next");
-    expect(participateButton?.hasAttribute("disabled")).toBeTruthy();
-  });
-
-  it("should enable button when input value changes", async () => {
-    const { queryByTestId, container } = await renderSwapModal();
-
-    const participateButton = queryByTestId("sns-swap-participate-button-next");
-    expect(participateButton?.hasAttribute("disabled")).toBeTruthy();
-
-    const input = container.querySelector("input[name='amount']");
-    input && fireEvent.input(input, { target: { value: "10" } });
-    await waitFor(() =>
-      expect(participateButton?.hasAttribute("disabled")).toBeFalsy()
-    );
-  });
-
-  it("should move to the last step", async () => {
-    const { queryByTestId, container } = await renderSwapModal();
-
-    const participateButton = queryByTestId("sns-swap-participate-button-next");
-    expect(participateButton?.hasAttribute("disabled")).toBeTruthy();
-
-    const input = container.querySelector("input[name='amount']");
-    input && fireEvent.input(input, { target: { value: "10" } });
-    await waitFor(() =>
-      expect(participateButton?.hasAttribute("disabled")).toBeFalsy()
-    );
-
-    participateButton && fireEvent.click(participateButton);
-    await waitFor(() =>
-      expect(queryByTestId("sns-swap-participate-step-2")).toBeFalsy()
-    );
+      expect(participateButton?.hasAttribute("disabled")).toBeFalsy();
+    });
   });
 });
