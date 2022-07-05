9dc1ccc799fa2eedd20e96f41ed28c48327753a0
Max <max@dfinity.org>
2022-07-05 20:46:28 +0200

    fmt

--- deploy.sh
+++ deploy.sh
@@ -231,7 +231,7 @@ fi
 if [[ "$DEPLOY_SNS" == "true" ]]; then
   # If the wasm canister has not been installed already, install it.
   # TODO: Maybe put this behind a flag?
-echo Checking whether sns wasm is installed
+  echo Checking whether sns wasm is installed
   SNS_WASM_CANISTER_ID="$(dfx canister --network "$DFX_NETWORK" id wasm_canister 2>/dev/null || echo NOPE)"
   if [[ "${SNS_WASM_CANISTER_ID:-}" != "NOPE" ]]
   then
