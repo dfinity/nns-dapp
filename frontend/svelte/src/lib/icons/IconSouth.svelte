cedbd46c1cce3bba30bf69b1be874c85bb7b929a
Llorenç <llorenc.muntaner@dfinity.org>
2022-07-11 11:05:57 +0200

    L2-814: Icon Review Participation

---
+++ frontend/svelte/src/lib/icons/IconSouth.svelte
@@ -0,0 +1,8 @@
+<!-- Source: https://fonts.google.com/icons?selected=Material%20Symbols%20Outlined%3Asouth%3AFILL%400%3Bwght%40400%3BGRAD%400%3Bopsz%4048 -->
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  height="48"
+  width="48"
+  fill="currentColor"
+  ><path d="M24 44 10 30l2.1-2.1 10.4 10.4V4h3v34.3l10.4-10.4L38 30Z" /></svg
+>
