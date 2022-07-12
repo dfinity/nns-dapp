92d2bfa4e4474684e63c0a1dfc4cf4c74e5e6932
David Dal Busco <david.dalbusco@dfinity.org>
2022-07-12 07:09:19 +0200

    chore: last testnet sns deployment

--- frontend/svelte/src/lib/api/sns.api.ts
+++ frontend/svelte/src/lib/api/sns.api.ts
@@ -50,7 +50,7 @@ const listSnses = async ({
   // TODO(L2-828): extract property for wasm canister id
 
   const { listSnses }: SnsWasmCanister = SnsWasmCanister.create({
-    canisterId: Principal.fromText("nsrxe-iiaaa-aaaaa-aacaa-cai"),
+    canisterId: Principal.fromText("p2lib-2iaaa-aaaaa-aacma-cai"),
     agent,
   });
 
