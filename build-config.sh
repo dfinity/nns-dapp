f37bbaf135d2a6d4ce4f20469e7ecc8d01143426
Max <max@dfinity.org>
2022-07-12 10:11:38 +0200

    ++

--- build-config.sh
+++ build-config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Prints build configuration as KEY=VALUE lines, suitable for bash and github actions.
-set -euxo pipefail
+set -euo pipefail
 cd "$(dirname "$(realpath "$0")")"
 jq -r '.defaults.build.config | to_entries | .[] | .key+"="+(.value|tostring)' dfx.json
 jq -r '"DFX_VERSION="+.dfx' dfx.json
