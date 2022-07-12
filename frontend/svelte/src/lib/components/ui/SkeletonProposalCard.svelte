771f1c9367d726f047ccded6b9ee6bd9bbeed421
mstrasinskis <maksims.strasinskis@dfinity.org>
2022-07-12 18:56:35 +0300

    feat: SkeletonProposalCard

---
+++ frontend/svelte/src/lib/components/ui/SkeletonProposalCard.svelte
@@ -0,0 +1,36 @@
+<!-- adapted source: https://github.com/ionic-team/ionic-framework/tree/main/core/src/components/skeleton-text -->
+<script lang="ts">
+  import Card from "./Card.svelte";
+  import SkeletonParagraph from "./SkeletonParagraph.svelte";
+</script>
+
+<Card testId="skeleton-card">
+  <div class="title" slot="start">
+    <SkeletonParagraph />
+  </div>
+  <div class="content">
+    <SkeletonParagraph />
+    <SkeletonParagraph />
+    <SkeletonParagraph />
+  </div>
+</Card>
+
+<style lang="scss">
+  .title {
+    width: 100%;
+    height: var(--padding-3x);
+  }
+
+  .content {
+    display: grid;
+    grid-template-columns: 15% auto 15%;
+    gap: var(--padding-3x);
+    align-items: center;
+    height: var(--padding-4x);
+
+    :global(:nth-child(2)) {
+      height: var(--padding-1_5x);
+      border-radius: var(--border-radius);
+    }
+  }
+</style>
