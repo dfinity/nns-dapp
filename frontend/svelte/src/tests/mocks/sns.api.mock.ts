a96e96f6d87464539eacae96196d862b33f819df
David Dal Busco <david.dalbusco@dfinity.org>
2022-07-13 09:03:08 +0200

    test: sns api

---
+++ frontend/svelte/src/tests/mocks/sns.api.mock.ts
@@ -0,0 +1,39 @@
+import type { DeployedSns } from "@dfinity/nns";
+import { Principal } from "@dfinity/principal";
+import type {CanisterStatusResultV2} from '@dfinity/sns';
+
+export const deployedSnsMock: DeployedSns[] = [
+  {
+    root_canister_id: [Principal.fromText("pin7y-wyaaa-aaaaa-aacpa-cai")],
+  },
+  {
+    root_canister_id: [Principal.fromText("zdlco-vyaaa-aaaaa-aabva-cai")],
+  },
+];
+
+export const rootCanisterIdMock: Principal = Principal.fromText(
+    "pin7y-wyaaa-aaaaa-aacpa-cai"
+);
+
+export const ledgerCanisterIdMock: Principal = Principal.fromText(
+    "ktxdj-qiaaa-aaaaa-aacqa-cai"
+);
+
+export const governanceCanisterIdMock: Principal = Principal.fromText(
+    "ppmzm-3aaaa-aaaaa-aacpq-cai"
+);
+
+export const swapCanisterIdMock: Principal = Principal.fromText(
+    "kuwf5-5qaaa-aaaaa-aacqq-cai"
+);
+
+export const snsMock: [string, Principal, CanisterStatusResultV2][] = [
+  ["root", rootCanisterIdMock, {} as unknown as CanisterStatusResultV2],
+  ["ledger", ledgerCanisterIdMock, {} as unknown as CanisterStatusResultV2],
+  [
+    "governance",
+    governanceCanisterIdMock,
+    {} as unknown as CanisterStatusResultV2,
+  ],
+  ["swap", swapCanisterIdMock, {} as unknown as CanisterStatusResultV2],
+];
