4ebf6e8078bee9e33c4289066e02af5f9fd53f63
mstrasinskis <maksims.strasinskis@dfinity.org>
2022-07-15 09:27:16 +0300

    chore: get rid of mock shuffle

--- frontend/svelte/src/lib/utils/dev.utils.ts
+++ frontend/svelte/src/lib/utils/dev.utils.ts
@@ -94,9 +94,3 @@ export function triggerDebugReport(node: HTMLElement) {
     },
   };
 }
-
-export const shuffle = <T>(items: T[]): T[] =>
-  items
-    .map((value) => ({ value, sort: Math.random() }))
-    .sort((a, b) => a.sort - b.sort)
-    .map(({ value }) => value);
