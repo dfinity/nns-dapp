e5dbb08ecf5830cc8456080220069659d20b2ead
David Dal Busco <david.dalbusco@dfinity.org>
2022-07-19 16:58:24 +0200

    feat: display data from swap

---
+++ frontend/svelte/src/lib/components/project-detail/ProjectSwapDetails.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import { ICP } from "@dfinity/nns";
+  import type { SnsSummary } from "../../types/sns";
+  import { getContext } from "svelte";
+  import {
+    PROJECT_DETAIL_CONTEXT_KEY,
+    type ProjectDetailContext,
+  } from "../../types/project-detail.context";
+  import KeyValuePair from "../ui/KeyValuePair.svelte";
+  import Icp from "../ic/ICP.svelte";
+  import InfoContextKey from "../ui/InfoContextKey.svelte";
+  import { i18n } from "../../stores/i18n";
+  import type { Init } from "@dfinity/sns";
+
+  const { store: projectDetailStore } = getContext<ProjectDetailContext>(
+    PROJECT_DETAIL_CONTEXT_KEY
+  );
+
+  let summary: SnsSummary;
+  // type safety validation is done in ProjectDetail component
+  $: summary = $projectDetailStore.summary as SnsSummary;
+
+  let details: Init;
+  $: ({ details } = summary.swap);
+
+  let minCommitmentIcp: ICP;
+  $: minCommitmentIcp = ICP.fromE8s(details.min_participant_icp_e8s);
+  let maxCommitmentIcp: ICP;
+  $: maxCommitmentIcp = ICP.fromE8s(details.max_participant_icp_e8s);
+</script>
+
+<KeyValuePair>
+  <InfoContextKey slot="key"
+    ><svelte:fragment slot="header"
+      >{$i18n.sns_project_detail.min_commitment}</svelte:fragment
+    >
+    <p class="small">
+      This is the text that is hidden and should appear on click
+    </p>
+  </InfoContextKey>
+  <Icp slot="value" icp={minCommitmentIcp} singleLine />
+</KeyValuePair>
+<KeyValuePair>
+  <InfoContextKey slot="key"
+    ><svelte:fragment slot="header"
+      >{$i18n.sns_project_detail.max_commitment}</svelte:fragment
+    >
+    <p class="small">
+      This should be an explanation of what does maximum commitment means
+    </p>
+  </InfoContextKey>
+  <Icp slot="value" icp={maxCommitmentIcp} singleLine />
+</KeyValuePair>
