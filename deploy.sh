4300eb5e76d247c6c29532290c2eeec660790cdf
Max <max@dfinity.org>
2022-07-19 16:27:12 +0200

    Print the sns creation response

--- deploy.sh
+++ deploy.sh
@@ -249,7 +249,8 @@ if [[ "$DEPLOY_SNS" == "true" ]]; then
     done
   fi
   echo "Creating SNS"
-  ./target/ic/sns deploy --network "$DFX_NETWORK" --override-sns-wasm-canister-id-for-tests "${SNS_WASM_CANISTER_ID}" --init-config-file sns_init.yml >sns_creation.idl
+  ./target/ic/sns deploy --network "$DFX_NETWORK" --override-sns-wasm-canister-id-for-tests "${SNS_WASM_CANISTER_ID}" --init-config-file sns_init.yml |
+    tee /dev/stderr >sns_creation.idl
 
   echo "Populate canister_ids.json"
   if test -e canister_ids.json; then
