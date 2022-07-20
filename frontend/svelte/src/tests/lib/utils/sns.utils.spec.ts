54ac64f2c5b36f0db90cbf18c604594858bdc963
David Dal Busco <david.dalbusco@dfinity.org>
2022-07-20 07:45:30 +0200

    test: concat summaries

--- frontend/svelte/src/tests/lib/utils/sns.utils.spec.ts
+++ frontend/svelte/src/tests/lib/utils/sns.utils.spec.ts
@@ -1,4 +1,7 @@
-import { concatSnsSummary } from "../../../lib/utils/sns.utils";
+import {
+  concatSnsSummaries,
+  concatSnsSummary,
+} from "../../../lib/utils/sns.utils";
 import {
   mockSummary,
   mockSwap,
@@ -7,6 +10,96 @@ import {
 } from "../../mocks/sns-projects.mock";
 
 describe("sns-utils", () => {
+  describe("concat sns summaries", () => {
+    it("should return empty for undefined summary", () => {
+      const summaries = concatSnsSummaries([[], []]);
+
+      expect(summaries.length).toEqual(0);
+    });
+
+    it("should return empty for undefined swap query", () => {
+      const summaries = concatSnsSummaries([[mockSummary], []]);
+
+      expect(summaries.length).toEqual(0);
+    });
+
+    it("should return empty for undefined swap init", () => {
+      const summaries = concatSnsSummaries([
+        [mockSummary],
+        [
+          {
+            rootCanisterId: "1234",
+            swap: [
+              {
+                init: [],
+                state: [],
+              },
+            ],
+          },
+        ],
+      ]);
+
+      expect(summaries.length).toEqual(0);
+    });
+
+    it("should return empty for undefined swap state", () => {
+      const summaries = concatSnsSummaries([
+        [mockSummary],
+        [
+          {
+            rootCanisterId: "1234",
+            swap: [
+              {
+                init: [mockSwapInit],
+                state: [],
+              },
+            ],
+          },
+        ],
+      ]);
+
+      expect(summaries.length).toEqual(0);
+    });
+
+    it("should return empty if not root id are matching between summaries and swaps", () => {
+      const summaries = concatSnsSummaries([
+        [mockSummary],
+        [
+          {
+            rootCanisterId: "1234",
+            swap: [
+              {
+                init: [mockSwapInit],
+                state: [mockSwapState],
+              },
+            ],
+          },
+        ],
+      ]);
+
+      expect(summaries.length).toEqual(0);
+    });
+
+    it("should concat summaries and swaps", () => {
+      const summaries = concatSnsSummaries([
+        [mockSummary],
+        [
+          {
+            rootCanisterId: mockSummary.rootCanisterId.toText(),
+            swap: [
+              {
+                init: [mockSwapInit],
+                state: [mockSwapState],
+              },
+            ],
+          },
+        ],
+      ]);
+
+      expect(summaries.length).toEqual(1);
+    });
+  });
+
   describe("concat a sns summary", () => {
     it("should throw error for undefined summary", () => {
       const call = () => concatSnsSummary([undefined, undefined]);
