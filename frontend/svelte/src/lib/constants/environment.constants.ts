752012f1f2a1c80b140428a372f11d2e33469e8c
Max <max@dfinity.org>
2022-07-20 12:36:56 +0200

    ++

--- frontend/svelte/src/lib/constants/environment.constants.ts
+++ frontend/svelte/src/lib/constants/environment.constants.ts
@@ -4,7 +4,7 @@ export const ROLLUP_WATCH: boolean = process.env.ROLLUP_WATCH === "true";
 export const FETCH_ROOT_KEY: boolean = process.env.FETCH_ROOT_KEY === "true";
 export const ENABLE_NEW_SPAWN_FEATURE: boolean =
   process.env.ENABLE_NEW_SPAWN_FEATURE === "true";
-export const WASM_CANISTER_ID: string = process.env.WASM_CANISTER_ID;
+export const WASM_CANISTER_ID: string = String(process.env.WASM_CANISTER_ID);
 export const IS_TESTNET: boolean =
   DFX_NETWORK !== "mainnet" &&
   FETCH_ROOT_KEY === true &&
