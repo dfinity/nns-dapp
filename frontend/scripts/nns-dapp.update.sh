be1d526612a4da66efdeae338b49ed86b6ee9af4
Formatting Committer <nobody@example.com>
2022-07-22 09:49:05 +0000

    Updating frontend formatting

--- frontend/scripts/nns-dapp.update.sh
+++ frontend/scripts/nns-dapp.update.sh
@@ -10,4 +10,4 @@ cd "$(dirname "$(realpath "$0")")/.."
 {
   echo "// Generated from nns-dapp repo commit ${NNS_DAPP_COMMIT} by $(basename "${0}")"
   cat "$NNS_DAPP_DIR/$NNS_DAPP_SUBDIR"
-} > src/lib/canisters/nns-dapp/nns_dapp.did
+} >src/lib/canisters/nns-dapp/nns_dapp.did
