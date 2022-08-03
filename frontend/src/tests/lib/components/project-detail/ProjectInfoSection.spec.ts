6b4aaeae3ebe7bf4c2ab2deda867d250ad98c5bf
David Dal Busco <david.dalbusco@dfinity.org>
2022-08-03 14:59:43 +0200

    test: not render content if undefined

--- frontend/src/tests/lib/components/project-detail/ProjectInfoSection.spec.ts
+++ frontend/src/tests/lib/components/project-detail/ProjectInfoSection.spec.ts
@@ -10,17 +10,18 @@ import {
   type ProjectDetailContext,
   type ProjectDetailStore,
 } from "../../../../lib/types/project-detail.context";
+import type { SnsSummary } from "../../../../lib/types/sns";
 import { mockSnsFullProject } from "../../../mocks/sns-projects.mock";
 import ContextWrapperTest from "../ContextWrapperTest.svelte";
 
 describe("ProjectInfoSection", () => {
-  const renderProjectInfoSection = () =>
+  const renderProjectInfoSection = (summary: SnsSummary | undefined) =>
     render(ContextWrapperTest, {
       props: {
         contextKey: PROJECT_DETAIL_CONTEXT_KEY,
         contextValue: {
           store: writable<ProjectDetailStore>({
-            summary: mockSnsFullProject.summary,
+            summary,
             swapCommitment: mockSnsFullProject.swapCommitment,
           }),
         } as ProjectDetailContext,
@@ -29,12 +30,17 @@ describe("ProjectInfoSection", () => {
     });
 
   it("should render title", async () => {
-    const { container } = renderProjectInfoSection();
+    const { container } = renderProjectInfoSection(mockSnsFullProject.summary);
     expect(container.querySelector("h1")).toBeInTheDocument();
   });
 
   it("should render project link", async () => {
-    const { container } = renderProjectInfoSection();
+    const { container } = renderProjectInfoSection(mockSnsFullProject.summary);
     expect(container.querySelector("a")).toBeInTheDocument();
+  });
+
+  it("should not render content if the summary is not yet defined", async () => {
+    const { container } = renderProjectInfoSection(undefined);
+    expect(container.querySelector("h1")).not.toBeInTheDocument();
   });
 });
