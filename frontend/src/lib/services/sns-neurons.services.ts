f6feb38ad969d524c5c8edb8caf50b82cb6dc5fa
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2022-08-04 09:46:27 +0200

    L2-865 and L2-868: SNS Neuron detail page setup (#1204)
    # Motivation
    
    Setup the SNS Neuron Detail page and connect it to the SNS governance canister.
    
    # Changes
    
    * New route SnsNeuronDetail
    * New AppPath SnsNeuronDetail which means adding it to a few helpers.
    * New utils to interact with the new path: routePathSnsNeuronId, routePathSnsNeuronCanisterId and getThirdLastPathDetail
    * New sns neuron service to get neuron details.
    * New sns api to get the neuron data.
    
    # Tests
    
    * Test the new route.
    * Test the new utils.
    * Test the new sns api function.
    * Test the new sns neuron service.

--- frontend/src/lib/services/sns-neurons.services.ts
+++ frontend/src/lib/services/sns-neurons.services.ts
@@ -1,9 +1,15 @@
 import type { Principal } from "@dfinity/principal";
 import type { SnsNeuron } from "@dfinity/sns";
-import { querySnsNeurons } from "../api/sns.api";
-import { snsNeuronsStore } from "../stores/sns-neurons.store";
+import { get } from "svelte/store";
+import { querySnsNeuron, querySnsNeurons } from "../api/sns.api";
+import {
+  snsNeuronsStore,
+  type ProjectNeuronStore,
+} from "../stores/sns-neurons.store";
 import { toastsStore } from "../stores/toasts.store";
 import { toToastError } from "../utils/error.utils";
+import { getSnsNeuronByHexId } from "../utils/sns-neuron.utils";
+import { hexStringToBytes } from "../utils/utils";
 import { queryAndUpdate } from "./utils.services";
 
 export const loadSnsNeurons = async (
@@ -41,5 +47,69 @@ export const loadSnsNeurons = async (
       );
     },
     logMessage: "Syncing Sns Neurons",
+  });
+};
+
+const getSnsNeuronsFromStoreByProject = (
+  rootCanisterId: Principal
+): ProjectNeuronStore | undefined =>
+  get(snsNeuronsStore)[rootCanisterId.toText()];
+
+const getNeuronFromStoreByIdHex = ({
+  neuronIdHex,
+  rootCanisterId,
+}: {
+  neuronIdHex: string;
+  rootCanisterId: Principal;
+}): { neuron?: SnsNeuron; certified?: boolean } => {
+  const projectData = getSnsNeuronsFromStoreByProject(rootCanisterId);
+  const neuron = getSnsNeuronByHexId({
+    neuronIdHex,
+    neurons: projectData?.neurons,
+  });
+  return {
+    neuron,
+    certified: projectData?.certified ?? false,
+  };
+};
+
+export const getSnsNeuron = async ({
+  neuronIdHex,
+  rootCanisterId,
+  onLoad,
+  onError,
+}: {
+  neuronIdHex: string;
+  rootCanisterId: Principal;
+  onLoad: ({ certified: boolean, neuron: SnsNeuron }) => void;
+  onError?: ({ certified, error }) => void;
+}): Promise<void> => {
+  const { neuron, certified } = getNeuronFromStoreByIdHex({
+    neuronIdHex,
+    rootCanisterId,
+  });
+  if (neuron !== undefined) {
+    onLoad({
+      neuron,
+      certified,
+    });
+    return;
+  }
+  const neuronId = hexStringToBytes(neuronIdHex);
+  return queryAndUpdate<SnsNeuron, Error>({
+    request: ({ certified, identity }) =>
+      querySnsNeuron({
+        rootCanisterId,
+        identity,
+        certified,
+        neuronId: { id: neuronId },
+      }),
+    onLoad: ({ response: neuron, certified }) => {
+      onLoad({ neuron, certified });
+    },
+    onError: ({ certified, error }) => {
+      onError?.({ certified, error });
+    },
+    logMessage: `Getting Sns Neuron ${neuronIdHex}`,
   });
 };
