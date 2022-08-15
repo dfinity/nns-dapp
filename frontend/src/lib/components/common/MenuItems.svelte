66d398b638010a103007ae6fc4565dc6367b716c
David Dal Busco <david.dalbusco@dfinity.org>
2022-08-15 08:15:45 +0200

    fix: merge import
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- frontend/src/lib/components/common/MenuItems.svelte
+++ frontend/src/lib/components/common/MenuItems.svelte
@@ -11,7 +11,7 @@
   import { AppPath } from "../../constants/routes.constants";
   import { routeStore } from "../../stores/route.store";
   import IconRocketLaunch from "../../icons/IconRocketLaunch.svelte";
-  import { ENABLE_SNS_NEURONS } from "../../constants/environment.constants";
+  import {ENABLE_SNS_NEURONS, IS_TESTNET} from "../../constants/environment.constants";
   import BadgeNew from "../ui/BadgeNew.svelte";
   import GetICPs from "../ic/GetICPs.svelte";
 
