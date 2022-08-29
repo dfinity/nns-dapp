fdec5de31d3e66ecfedb87c7b0f0a5143312673e
David Dal Busco <david.dalbusco@dfinity.org>
2022-08-29 07:15:56 +0200

    test: merge labels for tests
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- frontend/src/tests/mocks/i18n.mock.ts
+++ frontend/src/tests/mocks/i18n.mock.ts
@@ -1,4 +1,11 @@
 // @ts-ignore - workaround error "Consider using '--resolveJsonModule' to import module with '.json' extension." displayed in Webstorm and VS Code when the project is opened form root
-import * as en from "../../lib/i18n/en.json";
+import * as enCore from "../../lib/i18n/en.json";
+// @ts-ignore - workaround error "Consider using '--resolveJsonModule' to import module with '.json' extension." displayed in Webstorm and VS Code when the project is opened form root
+import * as enGovernance from "../../lib/i18n/en.governance.json";
+
+const en = {
+    ...enCore,
+    ...enGovernance
+};
 
 export default en;
