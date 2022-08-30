51a1ff4fca00a5605612310ec5d4a3314306099d
David Dal Busco <david.dalbusco@dfinity.org>
2022-08-30 10:33:22 +0200

    test: content of vote section
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

---
+++ frontend/src/tests/lib/components/proposal-detail/ProposalVotingSectionTest.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { setContext } from "svelte";
+  import { ProposalInfo } from "@dfinity/nns";
+  import ProposalVotingSection from "../../../../lib/components/proposal-detail/ProposalVotingSection.svelte";
+  import {
+    SELECTED_PROPOSAL_CONTEXT_KEY,
+    type SelectedProposalStore,
+  } from "../../../../lib/types/selected-proposal.context";
+  import { writable } from "svelte/store";
+
+  export let proposalInfo: ProposalInfo;
+
+  const store = writable<SelectedProposalStore>({
+    proposalId: proposalInfo.id,
+    proposal: proposalInfo,
+  });
+
+  setContext(SELECTED_PROPOSAL_CONTEXT_KEY, { store });
+</script>
+
+<ProposalVotingSection {proposalInfo} />
