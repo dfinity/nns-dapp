35a3060a9bbece47cd16bffa7d682f3445b7906b
David Dal Busco <david.dalbusco@dfinity.org>
2022-09-12 12:25:09 +0200

    test: navigation
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- frontend/src/tests/lib/components/proposal-detail/ProposalNavigation.spec.ts
+++ frontend/src/tests/lib/components/proposal-detail/ProposalNavigation.spec.ts
@@ -2,9 +2,13 @@
  * @jest-environment jsdom
  */
 
+import { fireEvent } from "@testing-library/dom";
 import { render } from "@testing-library/svelte";
+import { get } from "svelte/store";
 import ProposalNavigation from "../../../../lib/components/proposal-detail/ProposalNavigation.svelte";
+import { AppPath } from "../../../../lib/constants/routes.constants";
 import { proposalsStore } from "../../../../lib/stores/proposals.store";
+import { routeStore } from "../../../../lib/stores/route.store";
 import { mockProposals } from "../../../mocks/proposals.store.mock";
 
 describe("ProposalNavigation", () => {
@@ -98,6 +102,46 @@ describe("ProposalNavigation", () => {
       expect(
         getByTestId("proposal-nav-next")?.classList.contains("hidden")
       ).toBeTruthy();
+    });
+  });
+
+  describe("action", () => {
+    const props = { proposalInfo: mockProposals[1] };
+    const proposalId = BigInt(202);
+
+    beforeAll(() =>
+      proposalsStore.setProposals({
+        proposals: [...mockProposals, { ...mockProposals[0], id: proposalId }],
+        certified: true,
+      })
+    );
+
+    afterAll(() =>
+      proposalsStore.setProposals({ proposals: [], certified: undefined })
+    );
+
+    it("should go to next", () => {
+      const { getByTestId } = render(ProposalNavigation, {
+        props,
+      });
+
+      const btn = getByTestId("proposal-nav-next") as HTMLButtonElement;
+      fireEvent.click(btn);
+
+      const { path } = get(routeStore);
+      expect(path).toEqual(`${AppPath.ProposalDetail}/${proposalId}`);
+    });
+
+    it("should go to previous", () => {
+      const { getByTestId } = render(ProposalNavigation, {
+        props,
+      });
+
+      const btn = getByTestId("proposal-nav-previous") as HTMLButtonElement;
+      fireEvent.click(btn);
+
+      const { path } = get(routeStore);
+      expect(path).toEqual(`${AppPath.ProposalDetail}/${mockProposals[0].id}`);
     });
   });
 });
