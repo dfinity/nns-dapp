e038eaab0f34e04e1a0cae960de057f4cb40415a
David Dal Busco <david.dalbusco@dfinity.org>
2022-09-13 15:17:15 +0200

    feat: use alert modal
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- frontend/src/lib/modals/ConfirmationModal.svelte
+++ frontend/src/lib/modals/ConfirmationModal.svelte
@@ -1,39 +1,30 @@
 <script lang="ts">
   import { createEventDispatcher } from "svelte";
   import { i18n } from "../stores/i18n";
-  import LegacyModal from "./LegacyModal.svelte";
+  import { Modal } from "@dfinity/gix-components";
   import { busy } from "../stores/busy.store";
 
   const dispatch = createEventDispatcher();
 </script>
 
-<LegacyModal on:nnsClose>
+<Modal role="alert" on:nnsClose>
   <article>
     <slot />
-    <div role="toolbar">
-      <button
-        data-tid="confirm-no"
-        disabled={$busy}
-        on:click={() => dispatch("nnsClose")}
-        class="secondary full-width">{$i18n.core.confirm_no}</button
-      >
-      <button
-        data-tid="confirm-yes"
-        disabled={$busy}
-        class="primary full-width"
-        on:click={() => dispatch("nnsConfirm")}>{$i18n.core.confirm_yes}</button
-      >
-    </div>
   </article>
-</LegacyModal>
 
-<style lang="scss">
-  article {
-    padding: var(--padding-2x);
-  }
+  <svelte:fragment slot="toolbar">
+    <button
+      data-tid="confirm-no"
+      disabled={$busy}
+      on:click={() => dispatch("nnsClose")}
+      class="secondary">{$i18n.core.confirm_no}</button
+    >
+    <button
+      data-tid="confirm-yes"
+      disabled={$busy}
+      class="primary"
+      on:click={() => dispatch("nnsConfirm")}>{$i18n.core.confirm_yes}</button
+    >
+  </svelte:fragment>
+</Modal>
 
-  [role="toolbar"] {
-    display: flex;
-    gap: var(--padding);
-  }
-</style>
