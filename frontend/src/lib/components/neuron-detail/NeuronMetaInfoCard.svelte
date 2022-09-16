7df06e6f1901d7deb4ccae917fb0b7e99aebea2d
mstrasinskis <maksims.strasinskis@dfinity.org>
2022-09-16 16:26:01 +0200

    reset: svelte changes

--- frontend/src/lib/components/neuron-detail/NeuronMetaInfoCard.svelte
+++ frontend/src/lib/components/neuron-detail/NeuronMetaInfoCard.svelte
@@ -24,7 +24,6 @@
   } from "../../utils/neuron.utils";
   import { accountsStore } from "../../stores/accounts.store";
   import Value from "../ui/Value.svelte";
-  import DisburseNnsNeuronModal from "../../modals/neurons/DisburseNnsNeuronModal.svelte";
 
   export let neuron: NeuronInfo;
 
@@ -90,7 +89,7 @@
       {#if isControllable}
         <IncreaseDissolveDelayButton {neuron} />
         {#if neuron.state === NeuronState.Dissolved}
-          <DisburseButton {neuron} modal={DisburseNnsNeuronModal} />
+          <DisburseButton {neuron} />
         {:else if neuron.state === NeuronState.Dissolving || neuron.state === NeuronState.Locked}
           <DissolveActionButton
             neuronState={neuron.state}
