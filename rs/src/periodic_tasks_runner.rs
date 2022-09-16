d2a79a7118e300ba7e42b658b5cf2b7dab48eb54
Llorenç <llorenc.muntaner@dfinity.org>
2022-09-16 10:37:04 +0200

    L2-1002: Revert line break

--- rs/src/periodic_tasks_runner.rs
+++ rs/src/periodic_tasks_runner.rs
@@ -434,6 +434,7 @@ fn should_prune_transactions() -> bool {
         let memory_usage_bytes = (core::arch::wasm32::memory_size(0) * 65536) as u32;
         memory_usage_bytes > MEMORY_LIMIT_BYTES
     }
+
     #[cfg(not(target_arch = "wasm32"))]
     {
         const TRANSACTIONS_COUNT_LIMIT: u32 = 1_000_000;
