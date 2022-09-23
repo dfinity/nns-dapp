51fd759662b9c4dab628169b9e39c3d65d0481c2
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2022-09-23 15:52:47 +0200

    Feat/update sns did files (#1364)
    # Motivation
    
    Update Sns Project types to adatp to breaking changes in SNS Swap Canister.
    
    # Changes
    
    * Main one is the SnsSwap. `SnsSummary.swap` has changed completely. There is now an `SnsParams` with most of the needed data that before was in Init.
    
    # Tests
    
    * Fixed all broken tests.

--- frontend/src/tests/lib/components/launchpad/ProjectCardSwapInfo.spec.ts
+++ frontend/src/tests/lib/components/launchpad/ProjectCardSwapInfo.spec.ts
@@ -5,16 +5,13 @@
 import { SnsSwapLifecycle } from "@dfinity/sns";
 import { render } from "@testing-library/svelte";
 import ProjectCardSwapInfo from "../../../../lib/components/launchpad/ProjectCardSwapInfo.svelte";
+import type { SnsSwapCommitment } from "../../../../lib/types/sns";
 import { secondsToDuration } from "../../../../lib/utils/date.utils";
 import { formatToken } from "../../../../lib/utils/icp.utils";
+import { getCommitmentE8s } from "../../../../lib/utils/sns.utils";
 import en from "../../../mocks/i18n.mock";
 import {
   mockSnsFullProject,
-  mockSummary,
-  mockSwap,
-  mockSwapState,
-  mockSwapTimeWindow,
-  mockSwapTimeWindowText,
   summaryForLifecycle,
 } from "../../../mocks/sns-projects.mock";
 
@@ -35,42 +32,12 @@ describe("ProjectCardSwapInfo", () => {
     });
 
     const durationTillDeadline =
-      mockSwapTimeWindow.end_timestamp_seconds -
+      mockSnsFullProject.summary.swap.params.swap_due_timestamp_seconds -
       BigInt(Math.round(Date.now() / 1000));
 
     expect(
       getByText(secondsToDuration(durationTillDeadline))
     ).toBeInTheDocument();
-  });
-
-  it("should render no deadline if no deadline", () => {
-    const { getByText } = render(ProjectCardSwapInfo, {
-      props: {
-        project: {
-          ...mockSnsFullProject,
-          summary: {
-            ...mockSummary,
-            swap: {
-              ...mockSwap,
-              state: {
-                ...mockSwapState,
-                open_time_window: [],
-              },
-            },
-          },
-        },
-      },
-    });
-
-    const durationTillDeadline =
-      mockSwapTimeWindow.end_timestamp_seconds -
-      BigInt(Math.round(Date.now() / 1000));
-
-    const call = () => getByText(secondsToDuration(durationTillDeadline));
-    expect(call).toThrow();
-
-    const call2 = () => getByText(en.sns_project_detail.deadline);
-    expect(call2).toThrow();
   });
 
   it("should render my commitment", () => {
@@ -81,30 +48,13 @@ describe("ProjectCardSwapInfo", () => {
     });
 
     const icpValue = formatToken({
-      value: mockSnsFullProject.swapCommitment?.myCommitment
-        ?.amount_icp_e8s as bigint,
+      value:
+        getCommitmentE8s(
+          mockSnsFullProject.swapCommitment as SnsSwapCommitment
+        ) ?? BigInt(0),
     });
 
     expect(getByText(icpValue, { exact: false })).toBeInTheDocument();
-  });
-
-  it("should render sale start", () => {
-    const { container } = render(ProjectCardSwapInfo, {
-      props: {
-        project: {
-          ...mockSnsFullProject,
-          summary: summaryForLifecycle(SnsSwapLifecycle.Pending),
-        },
-      },
-    });
-
-    const element = container.querySelector(
-      '[data-tid="date-seconds"]'
-    ) as HTMLElement;
-
-    expect(element.innerHTML).toEqual(
-      mockSwapTimeWindowText.start_timestamp_seconds
-    );
   });
 
   it("should render completed", () => {
--- frontend/src/tests/lib/components/project-detail/ParticipateButton.spec.ts
+++ frontend/src/tests/lib/components/project-detail/ParticipateButton.spec.ts
@@ -2,14 +2,15 @@
  * @jest-environment jsdom
  */
 
-import { SnsSwapLifecycle, type SnsSwapBuyerState } from "@dfinity/sns";
+import { SnsSwapLifecycle } from "@dfinity/sns";
 import { waitFor } from "@testing-library/svelte";
 import ParticipateButton from "../../../../lib/components/project-detail/ParticipateButton.svelte";
 import type { SnsSwapCommitment } from "../../../../lib/types/sns";
 import en from "../../../mocks/i18n.mock";
 import {
-  buildMockSwapInit,
+  createTransferableAmount,
   mockSnsFullProject,
+  mockSnsParams,
   mockSnsSwapCommitment,
   principal,
   summaryForLifecycle,
@@ -94,9 +95,9 @@ describe("ParticipateButton", () => {
       swapCommitment: {
         rootCanisterId: mock.rootCanisterId,
         myCommitment: {
-          ...(mock.myCommitment as SnsSwapBuyerState),
-          amount_icp_e8s: buildMockSwapInit(mock.rootCanisterId.toText())
-            .max_participant_icp_e8s,
+          icp: [
+            createTransferableAmount(mockSnsParams.max_participant_icp_e8s),
+          ],
         },
       },
       Component: ParticipateButton,
