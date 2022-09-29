d6bcbc821672d1a7f556aaf04c2858b3974677ea
Llorenç <llorenc.muntaner@dfinity.org>
2022-09-29 09:10:41 +0200

    GIX-1031: Project timeline

--- frontend/src/lib/components/project-detail/ProjectTimeline.svelte
+++ frontend/src/lib/components/project-detail/ProjectTimeline.svelte
@@ -1,65 +1,46 @@
 <script lang="ts">
-  // import { getContext } from "svelte";
-  // import {
-  //   PROJECT_DETAIL_CONTEXT_KEY,
-  //   type ProjectDetailContext,
-  // } from "../../types/project-detail.context";
-  // import type { SnsSummary, SnsSummarySwap } from "../../types/sns";
-  // import ProgressBar from "../ui/ProgressBar.svelte";
-  // import { i18n } from "../../stores/i18n";
-  // import {
-  //   durationTillSwapDeadline,
-  //   durationTillSwapStart,
-  //   swapDuration,
-  // } from "../../utils/projects.utils";
-  // import { secondsToDuration } from "../../utils/date.utils";
-  // import Value from "../ui/Value.svelte";
+  import { getContext } from "svelte";
+  import {
+    PROJECT_DETAIL_CONTEXT_KEY,
+    type ProjectDetailContext,
+  } from "../../types/project-detail.context";
+  import type { SnsSummary, SnsSummarySwap } from "../../types/sns";
+  import { i18n } from "../../stores/i18n";
+  import { durationTillSwapDeadline } from "../../utils/projects.utils";
+  import { secondsToDuration } from "../../utils/date.utils";
+  import Value from "../ui/Value.svelte";
 
-  // const { store: projectDetailStore } = getContext<ProjectDetailContext>(
-  //   PROJECT_DETAIL_CONTEXT_KEY
-  // );
+  const { store: projectDetailStore } = getContext<ProjectDetailContext>(
+    PROJECT_DETAIL_CONTEXT_KEY
+  );
 
-  // let swap: SnsSummarySwap;
+  let swap: SnsSummarySwap;
   // type safety validation is done in ProjectStatusSection component
-  // $: ({ swap } = $projectDetailStore.summary as SnsSummary);
+  $: ({ swap } = $projectDetailStore.summary as SnsSummary);
 
-  // let durationTillStart: bigint | undefined;
-  // $: durationTillStart = durationTillSwapStart(swap);
-  // let durationSeconds: bigint | undefined;
-  // $: durationSeconds = swapDuration(swap);
-  // let durationTillDeadline: bigint | undefined;
-  // $: durationTillDeadline = durationTillSwapDeadline(swap);
+  let durationTillDeadline: bigint | undefined;
+  $: durationTillDeadline = durationTillSwapDeadline(swap);
 </script>
 
-<!-- TODO: https://dfinity.atlassian.net/browse/GIX-1031 Change UX because now we don't have a start date -->
-
-<!-- {#if durationTillDeadline !== undefined && durationSeconds !== undefined && durationTillStart !== undefined}
-  <div>
-    <ProgressBar
-      value={Number(durationTillStart)}
-      max={Number(durationSeconds)}
-      color="blue"
-    >
-      <p slot="top" class="push-apart">
-        <span>
-          {$i18n.sns_project_detail.deadline}
-        </span>
-        <Value>
-          {secondsToDuration(durationTillDeadline)}
-        </Value>
-      </p>
-    </ProgressBar>
-  </div>
-{/if} -->
+{#if durationTillDeadline !== undefined}
+  <p class="push-apart">
+    <span>
+      {$i18n.sns_project_detail.deadline}
+    </span>
+    <Value>
+      {secondsToDuration(durationTillDeadline)}
+    </Value>
+  </p>
+{/if}
 <div />
 
 <style lang="scss">
-  // p {
-  //   margin: 0;
-  // }
+  p {
+    margin: 0;
+  }
 
-  // .push-apart {
-  //   display: flex;
-  //   justify-content: space-between;
-  // }
+  .push-apart {
+    display: flex;
+    justify-content: space-between;
+  }
 </style>
