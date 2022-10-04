30e13716c0d60a26bc2982b88108a38754b9a624
David Dal Busco <david.dalbusco@dfinity.org>
2022-10-04 10:21:03 +0200

    feat: merge maturity description for hw

--- frontend/src/lib/types/i18n.d.ts
+++ frontend/src/lib/types/i18n.d.ts
@@ -482,7 +482,8 @@ interface I18nNeuron_detail {
   merge: string;
   spawn_neuron: string;
   spawn: string;
-  maturity_tooltip: string;
+  stake_maturity_tooltip: string;
+  merge_maturity_tooltip: string;
   start_dissolve_description: string;
   stop_dissolve_description: string;
   join_community_fund_success: string;
