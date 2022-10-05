8901b2cff212878dd6a03fe518308b6a8921fe6f
David Dal Busco <david.dalbusco@dfinity.org>
2022-10-05 08:05:03 +0200

    test: fix mock type

--- frontend/__mocks__/$lib/constants/canister-ids.constants.ts
+++ frontend/__mocks__/$lib/constants/canister-ids.constants.ts
@@ -1,4 +1,6 @@
-export const OWN_CANISTER_ID="qhbym-qaaaa-aaaaa-aaafq-cai";
-export const LEDGER_CANISTER_ID="ryjl3-tyaaa-aaaaa-aaaba-cai";
-export const GOVERNANCE_CANISTER_ID="rrkah-fqaaa-aaaaa-aaaaq-cai";
-export const CYCLES_MINTING_CANISTER_ID="rkp4c-7iaaa-aaaaa-aaaca-cai";
+import {Principal} from "@dfinity/principal";
+
+export const OWN_CANISTER_ID=Principal.fromText("qhbym-qaaaa-aaaaa-aaafq-cai");
+export const LEDGER_CANISTER_ID=Principal.fromText("ryjl3-tyaaa-aaaaa-aaaba-cai");
+export const GOVERNANCE_CANISTER_ID=Principal.fromText("rrkah-fqaaa-aaaaa-aaaaq-cai");
+export const CYCLES_MINTING_CANISTER_ID=Principal.fromText("rkp4c-7iaaa-aaaaa-aaaca-cai");
