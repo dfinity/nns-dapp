bffe0afe6636af27f249c1683d643e89f2051dca
Llorenç <llorenc.muntaner@dfinity.org>
2022-10-07 14:02:05 +0200

    GIX-994: SNS Transaction

--- frontend/src/lib/derived/accounts-list.derived.ts
+++ frontend/src/lib/derived/accounts-list.derived.ts
@@ -2,13 +2,19 @@
  * A derived store that returns the accounts as an array of accounts.
  */
 
+import { isNnsProjectStore } from "$lib/selected-project.derived";
+import { snsProjectAccountsStore } from "$lib/sns/sns-project-accounts.derived";
 import { accountsStore } from "$lib/stores/accounts.store";
 import { derived } from "svelte/store";
 
 export const accountsListStore = derived(
-  accountsStore,
-  ({ main, subAccounts, hardwareWallets }) =>
-    main === undefined
-      ? []
-      : [main, ...(subAccounts ?? []), ...(hardwareWallets ?? [])]
+  [accountsStore, isNnsProjectStore, snsProjectAccountsStore],
+  ([{ main, subAccounts, hardwareWallets }, isNns, projectAccounts]) => {
+    if (isNns) {
+      return main === undefined
+        ? []
+        : [main, ...(subAccounts ?? []), ...(hardwareWallets ?? [])];
+    }
+    return projectAccounts ?? [];
+  }
 );
---
+++ frontend/src/lib/modals/accounts/SnsTransactionModal.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import { ICPToken, TokenAmount } from "@dfinity/nns";
+  import { createEventDispatcher } from "svelte";
+  import { startBusy, stopBusy } from "../../stores/busy.store";
+  import { i18n } from "../../stores/i18n";
+  import type { Step } from "../../stores/steps.state";
+  import { toastsSuccess } from "../../stores/toasts.store";
+  import type { NewTransaction } from "../../types/transaction.context";
+  import TransactionModal from "./NewTransaction/TransactionModal.svelte";
+
+  let currentStep: Step;
+
+  $: title =
+    currentStep?.name === "Form"
+      ? $i18n.accounts.new_transaction
+      : $i18n.accounts.review_transaction;
+
+  const dispatcher = createEventDispatcher();
+  const transfer = async ({
+    detail: { sourceAccount, amount, destinationAddress },
+  }: CustomEvent<NewTransaction>) => {
+    startBusy({
+      initiator: "accounts",
+    });
+
+    // Errors in amount already handled by TransactionModal
+    const tokenAmount = TokenAmount.fromNumber({
+      amount,
+      token: ICPToken,
+    });
+
+    console.log("transfer", sourceAccount, destinationAddress, tokenAmount);
+
+    // if (success) {
+    //   toastsSuccess({ labelKey: "accounts.transaction_success" });
+    // }
+
+    stopBusy("accounts");
+
+    // // We close the modal in case of success or error if the selected source is not a hardware wallet.
+    // // In case of hardware wallet, the error messages might contain interesting information for the user such as "your device is idle"
+    // if (success || !isAccountHardwareWallet(sourceAccount)) {
+    //   dispatcher("nnsClose");
+    // }
+  };
+</script>
+
+<TransactionModal on:nnsSubmit={transfer} on:nnsClose bind:currentStep>
+  <svelte:fragment slot="title"
+    >{title ?? $i18n.accounts.new_transaction}</svelte:fragment
+  >
+  <p slot="description">
+    {$i18n.accounts.icp_transaction_description}
+  </p>
+</TransactionModal>
