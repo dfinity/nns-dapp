2cd89cc40cd9985c4b40bef13ad3f3ac7d12041a
Formatting Committer <nobody@example.com>
2022-10-07 11:59:50 +0000

    Updating frontend formatting

--- frontend/scripts/build.robots.mjs
+++ frontend/scripts/build.robots.mjs
@@ -2,31 +2,30 @@
 
 import * as dotenv from "dotenv";
 import { readFileSync, writeFileSync } from "fs";
-import {join} from "path";
+import { join } from "path";
 
 dotenv.config();
 
 const buildRobots = () => {
-    const updatedIndexHTML = updateRobots();
-    writeFileSync("./public/index.html", updatedIndexHTML);
+  const updatedIndexHTML = updateRobots();
+  writeFileSync("./public/index.html", updatedIndexHTML);
 };
-
 
 /**
  * No search engines indexation locally or testnet otherwise remove comments.
  */
 const updateRobots = () => {
-    const content = readFileSync(
-        join(process.cwd(), "public", "index.html"),
-        "utf-8"
-    );
+  const content = readFileSync(
+    join(process.cwd(), "public", "index.html"),
+    "utf-8"
+  );
 
-    return content.replace(
-        "<!-- ROBOTS -->",
-        ["local", "testnet"].includes(process.env.VITE_DFX_NETWORK)
-            ? '<meta name="robots" content="noindex, nofollow" />'
-            : ""
-    );
-}
+  return content.replace(
+    "<!-- ROBOTS -->",
+    ["local", "testnet"].includes(process.env.VITE_DFX_NETWORK)
+      ? '<meta name="robots" content="noindex, nofollow" />'
+      : ""
+  );
+};
 
 buildRobots();
