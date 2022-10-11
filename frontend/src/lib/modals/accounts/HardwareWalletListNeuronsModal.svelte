63a5bc7beec210a9f43c4746e1096c1b1ee5cdee
David Dal Busco <david.dalbusco@dfinity.org>
2022-10-11 08:03:44 +0200

    feat: use new modal

--- frontend/src/lib/modals/accounts/HardwareWalletListNeuronsModal.svelte
+++ frontend/src/lib/modals/accounts/HardwareWalletListNeuronsModal.svelte
@@ -1,19 +1,14 @@
 <script lang="ts">
-  import LegacyModal from "$lib/modals/LegacyModal.svelte";
+  import { Modal } from "@dfinity/gix-components";
   import { i18n } from "$lib/stores/i18n";
   import HardwareWalletNeurons from "$lib/components/accounts/HardwareWalletNeurons.svelte";
 </script>
 
-<LegacyModal on:nnsClose size="big">
+<Modal on:nnsClose>
   <span slot="title">{$i18n.neurons.title}</span>
 
   <div>
     <HardwareWalletNeurons />
   </div>
-</LegacyModal>
+</Modal>
 
-<style lang="scss">
-  div {
-    padding: var(--padding-2x);
-  }
-</style>
