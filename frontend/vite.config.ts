52a8f9ad0b31cafb056cbe75fbcdda83d2e01801
David Dal Busco <david.dalbusco@dfinity.org>
2022-10-13 13:24:37 +0200

    build: scope polyfill

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,7 +14,10 @@ const config: UserConfig = {
     target: "es2020",
     rollupOptions: {
       // Polyfill Buffer for production build. The hardware wallet needs Buffer.
-      plugins: [inject({ Buffer: ["buffer", "Buffer"] })],
+      plugins: [inject({
+        include: ['node_modules/@ledgerhq/**'],
+        modules: { Buffer: ['buffer', 'Buffer'], }
+      })],
     },
   },
   define: {
