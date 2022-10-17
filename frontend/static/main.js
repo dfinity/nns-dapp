127c66e6f0eb14c7a3c5957f8472481295854ebb
David Dal Busco <david.dalbusco@dfinity.org>
2022-10-17 06:56:57 +0200

    build: dummy main.js for local development

---
+++ frontend/static/main.js
@@ -0,0 +1,2 @@
+// Empty script for local resolution when evaluating index.html while developing. It resolves "Not found: /main.js".
+// This file is overwritten on production build. See scripts/build.csp.mjs.
