327ec1e42c3bf2b1b678030aef2a6ff957bcf752
David Dal Busco <david.dalbusco@dfinity.org>
2022-10-17 10:16:40 +0200

    chore: remove stake maturity feature flag (#1456)
    # Motivation
    
    Feature flag "stake maturity" is not needed anymore, feature is enabled on mainnet.

--- frontend/src/lib/types/i18n.d.ts
+++ frontend/src/lib/types/i18n.d.ts
@@ -475,7 +475,6 @@ interface I18nNeuron_detail {
   disburse: string;
   increase_stake: string;
   split_neuron: string;
-  voting_power_tooltip_without_stake: string;
   voting_power_tooltip_with_stake: string;
   join_community_fund_description: string;
   leave_community_fund_description: string;
