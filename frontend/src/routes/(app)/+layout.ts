4da0a8bb4e9979f40c92a3f0828023e2dbb3a4a0
David Dal Busco <david.dalbusco@dfinity.org>
2022-10-26 12:02:12 +0200

    feat: buffer needed only for app

--- frontend/src/routes/(app)/+layout.ts
+++ frontend/src/routes/(app)/+layout.ts
@@ -4,6 +4,11 @@ import type { Page } from "$lib/derived/page.derived";
 import type { LoadEvent } from "@sveltejs/kit";
 import type { LayoutLoad } from "./$types";
 
+// Polyfill Buffer for development purpose. node_modules/@ledgerhq needs buffer.
+// ⚠️ For production build the polyfill needs to be injected with Rollup (see vite.config.ts) because the page might be loaded before the _layout.js which will contains this polyfill
+import { Buffer } from "buffer";
+globalThis.Buffer = Buffer;
+
 export const load: LayoutLoad = ($event: LoadEvent): Partial<Page> => {
   if (!browser) {
     return {
