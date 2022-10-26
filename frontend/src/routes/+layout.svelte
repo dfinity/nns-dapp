fcbfea7a1b646c8835f20c8c3b62ac8a5dd05e95
David Dal Busco <david.dalbusco@dfinity.org>
2022-10-26 11:16:10 +0200

    feat: lazy load app initializer

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,19 +1,14 @@
 <script lang="ts">
-  import type { Unsubscriber } from "svelte/types/runtime/store";
   import { onDestroy, onMount } from "svelte";
   import { authStore } from "$lib/stores/auth.store";
   import type { AuthStore } from "$lib/stores/auth.store";
   import { initWorker } from "$lib/services/worker.services";
-  import { initApp } from "$lib/services/app.services";
-  import { syncBeforeUnload } from "$lib/utils/before-unload.utils";
-  import { voteRegistrationActive } from "$lib/utils/proposals.utils";
-  import { voteRegistrationStore } from "$lib/stores/vote-registration.store";
 
   let worker: { syncAuthIdle: (auth: AuthStore) => void } | undefined;
 
   onMount(async () => (worker = await initWorker()));
 
-  const unsubscribeAuth: Unsubscriber = authStore.subscribe(
+  const unsubscribeAuth = authStore.subscribe(
     async (auth: AuthStore) => {
       worker?.syncAuthIdle(auth);
 
@@ -21,19 +16,12 @@
         return;
       }
 
+      const {initApp} = await import("../lib/services/app.services");
       await initApp();
     }
   );
 
-  const unsubscribeVoteInProgress: Unsubscriber =
-    voteRegistrationStore.subscribe(({ registrations }) =>
-      syncBeforeUnload(voteRegistrationActive(registrations))
-    );
-
-  onDestroy(() => {
-    unsubscribeAuth();
-    unsubscribeVoteInProgress();
-  });
+  onDestroy(() => unsubscribeAuth());
 </script>
 
 <slot />
