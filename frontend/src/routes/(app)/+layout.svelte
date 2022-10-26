3d63a406a95fd3672ecaae474c0ce46268bc43f8
David Dal Busco <david.dalbusco@dfinity.org>
2022-10-26 10:59:54 +0200

    feat: toast and busy screen not needed on login screen

--- frontend/src/routes/(app)/+layout.svelte
+++ frontend/src/routes/(app)/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
   import Layout from "$lib/components/common/Layout.svelte";
+  import { Toasts } from "@dfinity/gix-components";
+  import BusyScreen from "$lib/components/ui/BusyScreen.svelte";
 </script>
 
 <Layout>
   <slot />
 </Layout>
+
+<Toasts />
+<BusyScreen />
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,8 +3,6 @@
   import { onDestroy, onMount } from "svelte";
   import { authStore } from "$lib/stores/auth.store";
   import type { AuthStore } from "$lib/stores/auth.store";
-  import { Toasts } from "@dfinity/gix-components";
-  import BusyScreen from "$lib/components/ui/BusyScreen.svelte";
   import { initWorker } from "$lib/services/worker.services";
   import { initApp } from "$lib/services/app.services";
   import { syncBeforeUnload } from "$lib/utils/before-unload.utils";
@@ -39,9 +37,6 @@
 </script>
 
 <slot />
-
-<Toasts />
-<BusyScreen />
 
 <style lang="scss" global>
   @import "../../node_modules/@dfinity/gix-components/styles/global.scss";
