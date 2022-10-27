c1a302813bb9c6daccba507ebfdf80f6cb2fe050
David Dal Busco <david.dalbusco@dfinity.org>
2022-10-27 09:58:09 +0200

    test: nav util

--- frontend/src/lib/utils/page.utils.ts
+++ frontend/src/lib/utils/page.utils.ts
@@ -1,4 +1,4 @@
-import {AppPath, ROUTE_ID_GROUP_APP} from "$lib/constants/routes.constants";
+import { AppPath, ROUTE_ID_GROUP_APP } from "$lib/constants/routes.constants";
 
 export const pathForRouteId = (routeId: string | null | undefined): AppPath => {
   if (routeId === null || routeId === undefined) {
@@ -6,7 +6,9 @@ export const pathForRouteId = (routeId: string | null | undefined): AppPath => {
   }
 
   const index = Object.values(AppPath).indexOf(
-    routeId.replace(ROUTE_ID_GROUP_APP, "").replace(/\/$/, "") as unknown as AppPath
+    routeId
+      .replace(ROUTE_ID_GROUP_APP, "")
+      .replace(/\/$/, "") as unknown as AppPath
   );
   const key = Object.keys(AppPath)[index];
 
--- frontend/src/tests/lib/utils/navigation.utils.spec.ts
+++ frontend/src/tests/lib/utils/navigation.utils.spec.ts
@@ -1,66 +1,92 @@
-import { AppPath } from "$lib/constants/routes.constants";
-import { reloadRouteData } from "$lib/utils/navigation.utils";
+import { OWN_CANISTER_ID_TEXT } from "$lib/constants/canister-ids.constants";
+import { AppPath, UNIVERSE_PARAM } from "$lib/constants/routes.constants";
+import { buildUrl, reloadRouteData } from "$lib/utils/navigation.utils";
 
 describe("navigation-utils", () => {
-  it("should not reload on back", () => {
-    expect(
-      reloadRouteData({
-        expectedPreviousPath: AppPath.Canister,
-        effectivePreviousPath: AppPath.Canister,
-        currentData: ["test"],
-      })
-    ).toBeFalsy();
+  describe("reload", () => {
+    it("should not reload on back", () => {
+      expect(
+        reloadRouteData({
+          expectedPreviousPath: AppPath.Canister,
+          effectivePreviousPath: AppPath.Canister,
+          currentData: ["test"],
+        })
+      ).toBeFalsy();
+    });
+
+    it("should reload if not expected route referrer", () => {
+      expect(
+        reloadRouteData({
+          expectedPreviousPath: AppPath.Canister,
+          effectivePreviousPath: AppPath.Neuron,
+          currentData: ["test"],
+        })
+      ).toBeTruthy();
+
+      expect(
+        reloadRouteData({
+          expectedPreviousPath: AppPath.Canister,
+          effectivePreviousPath: AppPath.Neuron,
+          currentData: ["test"],
+        })
+      ).toBeTruthy();
+
+      expect(
+        reloadRouteData({
+          expectedPreviousPath: AppPath.Canister,
+          effectivePreviousPath: undefined,
+          currentData: ["test"],
+        })
+      ).toBeTruthy();
+
+      expect(
+        reloadRouteData({
+          expectedPreviousPath: AppPath.Canister,
+          effectivePreviousPath: AppPath.Authentication,
+          currentData: ["test"],
+        })
+      ).toBeTruthy();
+    });
+
+    it("should reload if data empty", () => {
+      expect(
+        reloadRouteData({
+          expectedPreviousPath: AppPath.Canister,
+          effectivePreviousPath: AppPath.Neuron,
+          currentData: [],
+        })
+      ).toBeTruthy();
+
+      expect(
+        reloadRouteData({
+          expectedPreviousPath: AppPath.Canister,
+          effectivePreviousPath: AppPath.Neuron,
+          currentData: undefined,
+        })
+      ).toBeTruthy();
+    });
   });
 
-  it("should reload if not expected route referrer", () => {
-    expect(
-      reloadRouteData({
-        expectedPreviousPath: AppPath.Canister,
-        effectivePreviousPath: AppPath.Neuron,
-        currentData: ["test"],
-      })
-    ).toBeTruthy();
+  describe("url", () => {
+    it("should build url", () => {
+      expect(
+        buildUrl({
+          path: AppPath.Proposal,
+          universe: OWN_CANISTER_ID_TEXT,
+          params: { proposal: `123` },
+        })
+      ).toEqual(
+        `${AppPath.Proposal}/?${UNIVERSE_PARAM}=${OWN_CANISTER_ID_TEXT}&proposal=123`
+      );
 
-    expect(
-      reloadRouteData({
-        expectedPreviousPath: AppPath.Canister,
-        effectivePreviousPath: AppPath.Neuron,
-        currentData: ["test"],
-      })
-    ).toBeTruthy();
-
-    expect(
-      reloadRouteData({
-        expectedPreviousPath: AppPath.Canister,
-        effectivePreviousPath: undefined,
-        currentData: ["test"],
-      })
-    ).toBeTruthy();
-
-    expect(
-      reloadRouteData({
-        expectedPreviousPath: AppPath.Canister,
-        effectivePreviousPath: AppPath.Authentication,
-        currentData: ["test"],
-      })
-    ).toBeTruthy();
-  });
-
-  it("should reload if data empty", () => {
-    expect(
-      reloadRouteData({
-        expectedPreviousPath: AppPath.Canister,
-        effectivePreviousPath: AppPath.Neuron,
-        currentData: [],
-      })
-    ).toBeTruthy();
-
-    expect(
-      reloadRouteData({
-        expectedPreviousPath: AppPath.Canister,
-        effectivePreviousPath: AppPath.Neuron,
-        currentData: undefined,
-      })
-    ).toBeTruthy();
+      expect(
+        buildUrl({
+          path: AppPath.Proposal,
+          universe: OWN_CANISTER_ID_TEXT,
+        })
+      ).toEqual(
+        `${AppPath.Proposal}/?${UNIVERSE_PARAM}=${OWN_CANISTER_ID_TEXT}`
+      );
+    });
   });
 });
