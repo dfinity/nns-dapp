cd8171f19af9dc5d9c613c377639f1cec51da0d5
David Dal Busco <david.dalbusco@dfinity.org>
2022-11-01 16:40:29 +0100

    test: label was simplified

--- frontend/src/tests/lib/components/canisters/CanisterCard.spec.ts
+++ frontend/src/tests/lib/components/canisters/CanisterCard.spec.ts
@@ -82,7 +82,7 @@ describe("CanisterCard", () => {
     const button = queryByRole("button");
 
     expect(button?.getAttribute("aria-label")).toEqual(
-      `Copy "${mockCanister.canister_id.toText()}" to clipboard`
+      `Copy to clipboard`
     );
   });
 });
