5792b454bfebf798e3406133debd838a08721d16
mstrasinskis <maksims.strasinskis@dfinity.org>
2022-11-02 15:28:32 +0100

    test: SnsMeruonMaturityCard

---
+++ frontend/src/tests/lib/components/neuron-detail/SnsNeuronMaturityCard.spec.ts
@@ -0,0 +1,59 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import SnsNeuronMaturityCard from "$lib/components/neuron-detail/SnsNeuronMaturityCard.svelte";
+import { authStore } from "$lib/stores/auth.store";
+import {
+  SELECTED_SNS_NEURON_CONTEXT_KEY,
+  type SelectedSnsNeuronContext,
+  type SelectedSnsNeuronStore,
+} from "$lib/types/sns-neuron-detail.context";
+import { formattedSnsMaturity } from "$lib/utils/sns-neuron.utils";
+import type { SnsNeuron } from "@dfinity/sns";
+import { render } from "@testing-library/svelte";
+import { writable } from "svelte/store";
+import { mockAuthStoreSubscribe } from "../../../mocks/auth.store.mock";
+import en from "../../../mocks/i18n.mock";
+import { mockSnsNeuron } from "../../../mocks/sns-neurons.mock";
+import ContextWrapperTest from "../ContextWrapperTest.svelte";
+
+describe("SnsNeuronMaturityCard", () => {
+  const renderSnsNeuronMaturityCard = (neuron: SnsNeuron) =>
+    render(ContextWrapperTest, {
+      props: {
+        contextKey: SELECTED_SNS_NEURON_CONTEXT_KEY,
+        contextValue: {
+          store: writable<SelectedSnsNeuronStore>({
+            selected: {
+              neuronIdHex: neuron.id.toString(),
+              rootCanisterId: null,
+            },
+            neuron,
+          }),
+        } as SelectedSnsNeuronContext,
+        Component: SnsNeuronMaturityCard,
+      },
+    });
+
+  beforeAll(() =>
+    jest
+      .spyOn(authStore, "subscribe")
+      .mockImplementation(mockAuthStoreSubscribe)
+  );
+
+  it("renders maturity title", () => {
+    const { queryByText } = renderSnsNeuronMaturityCard({ ...mockSnsNeuron });
+
+    expect(queryByText(en.neuron_detail.maturity_title)).toBeInTheDocument();
+  });
+
+  it("renders formatted total maturity", () => {
+    const { queryByText } = renderSnsNeuronMaturityCard({ ...mockSnsNeuron });
+    expect(queryByText(en.neuron_detail.maturity_title)).toBeInTheDocument();
+
+    const formatted = formattedSnsMaturity({ ...mockSnsNeuron });
+
+    expect(queryByText(formatted)).toBeInTheDocument();
+  });
+});
