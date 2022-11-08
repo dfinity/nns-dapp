278d2fb97c4b20d8f7bac18d44de1ebb4bd22902
David Dal Busco <david.dalbusco@dfinity.org>
2022-11-08 14:37:52 +0100

    style: dissolve delay as secondary button

--- frontend/src/lib/components/sns-neuron-detail/actions/DissolveSnsNeuronButton.svelte
+++ frontend/src/lib/components/sns-neuron-detail/actions/DissolveSnsNeuronButton.svelte
@@ -48,7 +48,7 @@
   };
 </script>
 
-<button on:click={showModal} class="warning"
+<button on:click={showModal} class="secondary"
   >{keyOf({ obj: $i18n.neuron_detail, key: buttonKey })}</button
 >
 {#if isOpen}
