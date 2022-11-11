0d19d888d01947dff4b49def75a6e21ebf697f17
Llorenç <llorenc.muntaner@dfinity.org>
2022-11-11 14:05:30 +0100

    Chore: Integration Tests

--- frontend/src/tests/lib/routes/Accounts.spec.ts
+++ frontend/src/tests/lib/routes/Accounts.spec.ts
@@ -2,12 +2,14 @@
  * @jest-environment jsdom
  */
 
+import { createSubAccount, loadAccounts } from "$lib/api/accounts.api";
 import {
   OWN_CANISTER_ID,
   OWN_CANISTER_ID_TEXT,
 } from "$lib/constants/canister-ids.constants";
 import { snsSelectedTransactionFeeStore } from "$lib/derived/sns/sns-selected-transaction-fee.store";
 import Accounts from "$lib/routes/Accounts.svelte";
+import { accountsStore } from "$lib/stores/accounts.store";
 import { authStore } from "$lib/stores/auth.store";
 import { committedProjectsStore } from "$lib/stores/projects.store";
 import { snsAccountsStore } from "$lib/stores/sns-accounts.store";
@@ -15,6 +17,10 @@ import { transactionsFeesStore } from "$lib/stores/transaction-fees.store";
 import { page } from "$mocks/$app/stores";
 import { fireEvent, waitFor } from "@testing-library/dom";
 import { render } from "@testing-library/svelte";
+import {
+  mockMainAccount,
+  mockSubAccount,
+} from "../../mocks/accounts.store.mock";
 import { mockAuthStoreSubscribe } from "../../mocks/auth.store.mock";
 import en from "../../mocks/i18n.mock";
 import { mockSnsMainAccount } from "../../mocks/sns-accounts.mock";
@@ -23,19 +29,28 @@ import {
   mockSnsFullProject,
 } from "../../mocks/sns-projects.mock";
 import { mockSnsSelectedTransactionFeeStoreSubscribe } from "../../mocks/transaction-fee.mock";
+import { clickByTestId } from "../../utils/utils.test-utils";
 
-jest.mock("$lib/services/sns-accounts.services", () => {
+jest.mock("$lib/api/accounts.api", () => {
   return {
-    syncSnsAccounts: jest.fn().mockResolvedValue(undefined),
+    createSubAccount: jest.fn().mockResolvedValue(undefined),
+    loadAccounts: jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        main: mockMainAccount,
+        subAccounts: [mockSubAccount],
+        hardwareWallets: [],
+      })
+    ),
   };
 });
 
 describe("Accounts", () => {
-  beforeAll(() =>
+  beforeAll(() => {
     jest
       .spyOn(authStore, "subscribe")
-      .mockImplementation(mockAuthStoreSubscribe)
-  );
+      .mockImplementation(mockAuthStoreSubscribe);
+    jest.spyOn(console, "error").mockImplementation(jest.fn);
+  });
 
   jest
     .spyOn(committedProjectsStore, "subscribe")
@@ -170,5 +185,43 @@ describe("Accounts", () => {
     await waitFor(() => {
       expect(getByTestId("transaction-step-1")).toBeInTheDocument();
     });
+  });
+
+  it("should create a subaccount", async () => {
+    accountsStore.set({
+      main: mockMainAccount,
+      subAccounts: [],
+      hardwareWallets: [],
+    });
+    const { queryByTestId, container } = render(Accounts);
+
+    await waitFor(() =>
+      expect(queryByTestId("open-add-account-modal")).toBeInTheDocument()
+    );
+
+    await clickByTestId(queryByTestId, "open-add-account-modal");
+
+    await waitFor(() =>
+      expect(queryByTestId("choose-linked-as-account-type")).toBeInTheDocument()
+    );
+
+    await clickByTestId(queryByTestId, "choose-linked-as-account-type");
+
+    await waitFor(() =>
+      expect(
+        container.querySelector('input[name="newAccount"]')
+      ).toBeInTheDocument()
+    );
+    const input = container.querySelector('input[name="newAccount"]');
+    // Svelte generates code for listening to the `input` event
+    // https://github.com/testing-library/svelte-testing-library/issues/29#issuecomment-498055823
+    input && (await fireEvent.input(input, { target: { value: "test name" } }));
+
+    const createButton = container.querySelector('button[type="submit"]');
+
+    createButton && (await fireEvent.click(createButton));
+
+    await waitFor(() => expect(createSubAccount).toHaveBeenCalled());
+    await waitFor(() => expect(loadAccounts).toBeCalled());
   });
 });
