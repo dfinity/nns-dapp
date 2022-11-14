a42534ad587ed765d5934a617510197312ecff2e
David Dal Busco <david.dalbusco@dfinity.org>
2022-11-14 15:12:09 +0100

    feat: move sign in sync in layout

--- frontend/src/routes/(app)/+layout.svelte
+++ frontend/src/routes/(app)/+layout.svelte
@@ -3,8 +3,33 @@
   import { voteRegistrationStore } from "$lib/stores/vote-registration.store";
   import { syncBeforeUnload } from "$lib/utils/before-unload.utils";
   import { voteRegistrationActive } from "$lib/utils/proposals.utils";
-  import { onDestroy } from "svelte";
+  import {onDestroy, onMount} from "svelte";
   import { Toasts, BusyScreen } from "@dfinity/gix-components";
+  import {authStore} from "$lib/stores/auth.store";
+  import {toastsError} from "$lib/stores/toasts.store";
+  import {browser, prerendering} from "$app/environment";
+  import {displayAndCleanLogoutMsg} from "$lib/services/auth.services";
+  import {layoutAuthReady} from "$lib/stores/layout.store";
+
+  const syncAuthStore = async () => {
+    try {
+      await authStore.sync();
+    } catch (err) {
+      toastsError({ labelKey: "error.auth_sync", err });
+    }
+  };
+
+  onMount(async () => {
+    if (prerendering || !browser) {
+      return;
+    }
+
+    await syncAuthStore();
+
+    displayAndCleanLogoutMsg();
+
+    layoutAuthReady.set(true);
+  });
 
   const unsubscribeVoteInProgress = voteRegistrationStore.subscribe(
     ({ registrations }) =>
