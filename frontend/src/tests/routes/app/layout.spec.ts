1963c336542ae0f96b893a2003046f3d893e6a70
David Dal Busco <david.dalbusco@dfinity.org>
2022-11-15 09:48:26 +0100

    test: init auth

---
+++ frontend/src/tests/routes/app/layout.spec.ts
@@ -0,0 +1,31 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { initAppAuth, initAppData } from "$lib/services/$public/app.services";
+import App from "$routes/(app)/+layout.svelte";
+import { render, waitFor } from "@testing-library/svelte";
+
+jest.mock("$lib/services/$public/app.services", () => ({
+  initAppAuth: jest.fn(() => Promise.resolve()),
+  initAppData: jest.fn(() => Promise.resolve()),
+}));
+
+describe("Layout", () => {
+  afterAll(() => {
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
+  });
+
+  it("should init the auth on mount", async () => {
+    render(App);
+
+    await waitFor(() => expect(initAppAuth).toHaveBeenCalled());
+  });
+
+  it("should init the public data on mount", async () => {
+    render(App);
+
+    await waitFor(() => expect(initAppData).toHaveBeenCalled());
+  });
+});
