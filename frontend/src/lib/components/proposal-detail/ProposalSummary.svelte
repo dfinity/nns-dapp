41802974302467447efb68cc392ecfb6711a5214
David Dal Busco <david.dalbusco@dfinity.org>
2022-11-16 09:53:49 +0100

    style: proposal summary colors

--- frontend/src/lib/components/proposal-detail/ProposalSummary.svelte
+++ frontend/src/lib/components/proposal-detail/ProposalSummary.svelte
@@ -11,7 +11,7 @@
   $: showTitle = $$slots.title !== undefined;
 </script>
 
-<div class="markdown value">
+<div class="markdown">
   {#if showTitle}
     <div class="title"><slot name="title" /></div>
   {/if}
@@ -46,6 +46,7 @@
     :global(h6) {
       line-height: var(--line-height-standard);
       font-weight: normal;
+      color: inherit;
     }
 
     :global(h1) {
