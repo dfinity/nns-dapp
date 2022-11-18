10a6ba3fc3b1154134b8038c99828baca6890ee2
David Dal Busco <david.dalbusco@dfinity.org>
2022-11-18 10:10:00 +0100

    test: adapt for copy a11yn

--- frontend/src/tests/lib/components/ui/Identifier.spec.ts
+++ frontend/src/tests/lib/components/ui/Identifier.spec.ts
@@ -26,6 +26,8 @@ describe("Identifier", () => {
 
     const button = queryByRole("button");
 
-    expect(button?.getAttribute("aria-label")).toEqual(`Copy to clipboard`);
+    expect(button?.getAttribute("aria-label")).toEqual(
+      `Copy to clipboard: ${identifier}`
+    );
   });
 });
