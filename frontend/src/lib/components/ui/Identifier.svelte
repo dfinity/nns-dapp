a7c639097f1cf9c0f0a240f41e868e57380818b8
David Dal Busco <david.dalbusco@dfinity.org>
2022-11-18 06:48:51 +0100

    refactor: extract cmp

--- frontend/src/lib/components/ui/Identifier.svelte
+++ frontend/src/lib/components/ui/Identifier.svelte
@@ -1,37 +1,16 @@
 <script lang="ts">
-  import { Value, Copy } from "@dfinity/gix-components";
   import Hash from "$lib/components/ui/Hash.svelte";
+  import IdentifierPlain from "$lib/components/ui/IdentifierPlain.svelte";
 
   export let identifier: string;
   export let label: string | undefined = undefined;
   export let showCopy = false;
   export let size: "small" | "medium" = "small";
-
-  let labelText: string;
-  $: labelText = label === undefined ? "" : `${label} `;
 </script>
 
 {#if size === "small"}
   <Hash id="identifier" tagName="p" testId="identifier" text={identifier} />
 {:else}
-  <p>
-    <span data-tid="identifier" class:text_small={size === "small"}
-      >{labelText}<Value>{identifier}</Value></span
-    >
-    {#if showCopy}
-      <Copy value={identifier} />
-    {/if}
-  </p>
+  <IdentifierPlain {size} {label} {identifier} {showCopy} />
 {/if}
 
-<style lang="scss">
-  span {
-    word-break: break-all;
-  }
-
-  p {
-    margin: 0;
-    display: flex;
-    align-items: center;
-  }
-</style>
