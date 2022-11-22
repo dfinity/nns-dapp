454b65aec1f589d7f4e968302c56dcc075a1a173
David Dal Busco <david.dalbusco@dfinity.org>
2022-11-22 19:18:51 +0100

    feat: reset toast on signin

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,6 +4,7 @@
   import type { AuthStore } from "$lib/stores/auth.store";
   import { initWorker } from "$lib/services/worker.services";
   import { initAppDataProxy } from "$lib/proxy/app.services.proxy";
+  import {toastsReset} from "$lib/stores/toasts.store";
 
   let ready = false;
 
@@ -24,6 +25,11 @@
     }
 
     ready = true;
+
+    // We reset the toast to clear any previous messages.
+    // This is notably user-friendly in case of the user would have been sign-out automatically - session duration expired.
+    // That way user does not have to manually close previous message.
+    toastsReset();
 
     // Load app global stores data
     await initAppDataProxy();
