863a3f65410c92ea145cc856d13a6c2e51424767
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2022-11-23 13:19:44 +0100

    GIX-1111: Render SNS Topics (#1552)
    # Motivation
    
    List the topics that the user can follow for an SNS project.
    
    # Changes
    
    * Rename FollowTopicSection to FollowNnsTopicSection
    * New component FollowTopicSection agnostic to NNS or SNS that renders
    the UI of a topic.
    * Implement FollowSnsNeuronsModal. Access snsFunctions and render a
    FollowTopicSection for each followable section.
    * New sns neuron util "functionsToFollow".
    
    # Tests
    
    * Add test cases for FollowSnsNeuronsModal.
    * Test new component FollowTopicSection

--- frontend/src/lib/constants/sns-neurons.constants.ts
+++ frontend/src/lib/constants/sns-neurons.constants.ts
@@ -7,3 +7,5 @@ export const HOTKEY_PERMISSIONS = [
   SnsNeuronPermissionType.NEURON_PERMISSION_TYPE_VOTE,
   SnsNeuronPermissionType.NEURON_PERMISSION_TYPE_SUBMIT_PROPOSAL,
 ];
+
+export const UNSPECIFIED_FUNCTION_ID = BigInt(0);
