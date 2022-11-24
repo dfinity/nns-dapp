fafebcb81709b8427ab55611418f17f5f1ba9a8f
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2022-11-24 18:07:14 +0100

    Fix: Enable launchpad (#1571)
    # Motivation
    
    Enable launchpad
    
    # Changes
    
    * Disable redirect for prod in launchpad and project page.

--- frontend/src/routes/(app)/project/+page.svelte
+++ frontend/src/routes/(app)/project/+page.svelte
@@ -1,16 +1,6 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { IS_TESTNET } from "$lib/constants/environment.constants";
   import RouteModule from "$lib/components/common/RouteModule.svelte";
   import { AppPath } from "$lib/constants/routes.constants";
-  import { goto } from "$app/navigation";
-
-  onMount(async () => {
-    if (!IS_TESTNET) {
-      await goto(AppPath.Accounts, { replaceState: true });
-      return;
-    }
-  });
 
   // Preloaded by +page.ts
   export let data: { project: string | null | undefined };
