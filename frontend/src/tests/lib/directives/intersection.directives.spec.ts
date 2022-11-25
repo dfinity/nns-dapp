752960fdd83b8b66709fd177fc0afcadb03982c7
David Dal Busco <david.dalbusco@dfinity.org>
2022-11-25 10:20:13 +0100

    test: intersection directive

---
+++ frontend/src/tests/lib/directives/intersection.directives.spec.ts
@@ -0,0 +1,48 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import * as directives from "$lib/directives/intersection.directives";
+import { render } from "@testing-library/svelte";
+import {
+  IntersectionObserverActive,
+  IntersectionObserverPassive,
+} from "../../mocks/infinitescroll.mock";
+import IntersectionTest from "./IntersectionTest.svelte";
+
+describe("IntersectionDirectives", () => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore: test file
+  beforeAll(() => (global.IntersectionObserver = IntersectionObserverActive));
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore: test file
+  afterAll(() => (global.IntersectionObserver = IntersectionObserverPassive));
+
+  let spy;
+  let testIntersecting: boolean;
+
+  beforeEach(
+    () =>
+      (spy = jest
+        .spyOn(directives, "dispatchIntersecting")
+        .mockImplementation(
+          ({ intersecting }) => (testIntersecting = intersecting)
+        ))
+  );
+  afterEach(() => jest.clearAllMocks());
+
+  it("should trigger an intersect event", () => {
+    render(IntersectionTest);
+
+    expect(spy).toHaveBeenCalled();
+    expect(testIntersecting).toBeTruthy();
+  });
+
+  it("should not trigger an intersect event", () => {
+    render(IntersectionTest);
+
+    expect(spy).toHaveBeenCalled();
+    expect(testIntersecting).toBeFalsy();
+  });
+});
