0f3d0dd6a5c55945293a01943a871426995ae150
David Dal Busco <david.dalbusco@dfinity.org>
2022-11-28 17:14:07 +0100

    fix: followee modal overflow

--- frontend/src/lib/types/nns-neuron-detail.context.ts
+++ frontend/src/lib/types/nns-neuron-detail.context.ts
@@ -1,3 +1,4 @@
+import type { FolloweesNeuron } from "$lib/utils/neuron.utils";
 import type { NeuronInfo } from "@dfinity/nns";
 import type { Writable } from "svelte/store";
 
@@ -13,11 +14,14 @@ export type NnsNeuronModal =
   | "stake-maturity"
   | "merge-maturity"
   | "spawn"
-  | "join-community-fund";
+  | "join-community-fund"
+  | "voting-history";
 
 export interface NnsNeuronStore {
   neuron: NeuronInfo | undefined;
   modal: NnsNeuronModal | undefined;
+  // TODO: find a better pattern than including the selected followee within the neuron context and thus just to open the related modal
+  selectedFollowee: FolloweesNeuron | undefined;
 }
 
 export interface NnsNeuronContext {
