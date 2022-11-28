c44263c6487abb08af3a0443807da6599f47d1b0
David Dal Busco <david.dalbusco@dfinity.org>
2022-11-28 15:23:58 +0100

    feat: canister detail island and modals

---
+++ frontend/src/lib/modals/canisters/CanisterDetailModals.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import AddCyclesModal from "$lib/modals/canisters/AddCyclesModal.svelte";
+    import type {CanisterDetailsContext, CanisterDetailsModal} from "$lib/types/canister-detail.context";
+    import {CANISTER_DETAILS_CONTEXT_KEY} from "$lib/types/canister-detail.context";
+    import {getContext} from "svelte";
+
+    const context: CanisterDetailsContext = getContext<CanisterDetailsContext>(
+        CANISTER_DETAILS_CONTEXT_KEY
+    );
+    const { store }: CanisterDetailsContext = context;
+
+    let modal: CanisterDetailsModal | undefined;
+    $: ({ modal } = $store);
+
+    const close = () => store.update((data) => ({ ...data, modal: undefined }));
+</script>
+
+
+{#if modal === "add-cycles"}
+    <AddCyclesModal on:nnsClose={close} />
+{/if}
--- frontend/src/lib/types/canister-detail.context.ts
+++ frontend/src/lib/types/canister-detail.context.ts
@@ -3,6 +3,8 @@ import type { CanisterDetails as CanisterInfo } from "$lib/canisters/nns-dapp/nn
 import type { Principal } from "@dfinity/principal";
 import type { Writable } from "svelte/store";
 
+export type CanisterDetailsModal = "add-cycles"
+
 /**
  * A store that contains canister info and detail.
  */
@@ -10,11 +12,13 @@ export interface SelectCanisterDetailsStore {
   info: CanisterInfo | undefined;
   details: CanisterDetails | undefined;
   controller: boolean | undefined;
+  modal: CanisterDetailsModal | undefined;
 }
 
 export interface CanisterDetailsContext {
   store: Writable<SelectCanisterDetailsStore>;
   reloadDetails: (canisterId: Principal) => Promise<void>;
+  toggleModal: (modal: CanisterDetailsModal | undefined) => void;
 }
 
 export const CANISTER_DETAILS_CONTEXT_KEY = Symbol("canister-details");
