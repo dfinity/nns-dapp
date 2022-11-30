39040dcb6707933f646f876bc90075b4369a262b
Llorenç <llorenc.muntaner@dfinity.org>
2022-11-30 07:11:13 +0100

    GIX-1114: New component TagsList

--- frontend/src/lib/components/sns-neuron-detail/SnsFollowee.svelte
+++ frontend/src/lib/components/sns-neuron-detail/SnsFollowee.svelte
@@ -2,36 +2,21 @@
   import { Tag } from "@dfinity/gix-components";
   import type { SnsFolloweesByNeuron } from "$lib/utils/sns-neuron.utils";
   import Hash from "../ui/Hash.svelte";
+  import TagsList from "../ui/TagsList.svelte";
 
   export let followee: SnsFolloweesByNeuron;
 </script>
 
 <!-- TODO: Open Voting history modal https://dfinity.atlassian.net/browse/GIX-1156 -->
-<button id={followee.neuronIdHex} class="text">
-  <Hash text={followee.neuronIdHex} id={followee.neuronIdHex} tagName="span" />
-</button>
+<TagsList id={followee.neuronIdHex}>
+  <Hash
+    slot="title"
+    text={followee.neuronIdHex}
+    id={followee.neuronIdHex}
+    tagName="span"
+  />
 
-<ul aria-labelledby={followee.neuronIdHex}>
   {#each followee.nsFunctions as nsFunction}
     <Tag tagName="li">{nsFunction.name}</Tag>
   {/each}
-</ul>
-
-<style lang="scss">
-  button {
-    margin: 0 0 var(--padding-0_5x);
-    font-size: var(--font-size-h5);
-  }
-
-  ul {
-    display: flex;
-    gap: var(--padding-0_5x);
-    flex-wrap: wrap;
-
-    list-style: none;
-
-    margin-bottom: var(--padding);
-    padding: 0 0 var(--padding-2x);
-    border-bottom: 1px solid currentColor;
-  }
-</style>
+</TagsList>
