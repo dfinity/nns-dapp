de6f8e34b114f656cb3cbc0737fa192e5f904d43
Max <max@dfinity.org>
2022-12-07 10:41:33 +0100

    Spurious rormatting change - to be fixed by CI

--- rs/src/constants.rs
+++ rs/src/constants.rs
@@ -1,4 +1,4 @@
 use ledger_canister::Memo;
 
-pub const MEMO_CREATE_CANISTER: Memo = Memo(0x41455243); // == 'CREA'
+  pub const MEMO_CREATE_CANISTER: Memo = Memo(0x41455243); // == 'CREA'
 pub const MEMO_TOP_UP_CANISTER: Memo = Memo(0x50555054); // == 'TPUP'
