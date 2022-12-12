48384da84af9f4d746c0ce55cd2614c6e65c8f50
David Dal Busco <david.dalbusco@dfinity.org>
2022-12-12 11:25:45 +0100

    refactor: extract login links

--- frontend/src/lib/pages/Login.svelte
+++ frontend/src/lib/pages/Login.svelte
@@ -5,12 +5,6 @@
   import type { AuthStore } from "$lib/stores/auth.store";
   import { isSignedIn } from "$lib/utils/auth.utils";
   import { i18n } from "$lib/stores/i18n";
-  import {
-    IconWallet,
-    IconPassword,
-    IconUsers,
-    IconRocketLaunch,
-  } from "@dfinity/gix-components";
   import { OWN_CANISTER_ID_TEXT } from "$lib/constants/canister-ids.constants";
   import SignIn from "$lib/components/common/SignIn.svelte";
   import {
@@ -18,7 +12,7 @@
     buildProposalUrl,
   } from "$lib/utils/navigation.utils";
   import { goto } from "$app/navigation";
-  import { AppPath } from "$lib/constants/routes.constants";
+  import LoginLinks from "$lib/components/login/LoginLinks.svelte";
 
   let signedIn = false;
 
@@ -62,32 +56,7 @@
   <SignIn />
 </div>
 
-<ul>
-  <li>
-    <a href={AppPath.Accounts} data-tid="auth-link-accounts"
-      ><IconWallet size="32px" />
-      {$i18n.auth.wallet}</a
-    >
-  </li>
-  <li>
-    <a href={AppPath.Neurons} data-tid="auth-link-neurons"
-      ><IconPassword size="32px" />
-      {$i18n.auth.stake}</a
-    >
-  </li>
-  <li>
-    <a href={AppPath.Proposals} data-tid="auth-link-proposals"
-      ><IconUsers size="32px" />
-      {$i18n.auth.earn}</a
-    >
-  </li>
-  <li>
-    <a href={AppPath.Launchpad} data-tid="auth-link-launchpad"
-      ><IconRocketLaunch size="32px" />
-      {$i18n.auth.launchpad}</a
-    >
-  </li>
-</ul>
+<LoginLinks />
 
 <style lang="scss">
   @use "@dfinity/gix-components/styles/mixins/media";
@@ -149,44 +118,6 @@
     }
   }
 
-  ul {
-    list-style-type: none;
-
-    padding: 0;
-    margin: var(--padding) 0 0;
-
-    grid-template-columns: repeat(2, calc((100% - var(--padding-2x)) / 2));
-    grid-column-gap: var(--padding-2x);
-    grid-row-gap: var(--padding-2x);
-
-    width: 100%;
-
-    display: none;
-
-    @media (min-height: 620px) {
-      display: grid;
-    }
-
-    @include login.min-size(medium) {
-      display: none;
-    }
-
-    @include login.min-size(large) {
-      display: flex;
-      flex-grow: inherit;
-      justify-content: center;
-      gap: var(--padding-2x);
-      padding: var(--padding-4x) 0;
-    }
-  }
-
-  li {
-    @include login.min-size(medium) {
-      padding: var(--padding-1_5x) 0;
-      width: 206px;
-    }
-  }
-
   .sign-in {
     width: 100%;
     align-self: center;
@@ -201,46 +132,6 @@
     @include login.min-size(medium) {
       margin: var(--padding-6x) 0 var(--padding);
       width: auto;
-    }
-  }
-
-  a {
-    text-decoration: none;
-    text-align: center;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    box-sizing: border-box;
-    height: 100%;
-
-    padding: var(--padding-3x) var(--padding);
-
-    background: linear-gradient(
-      113.27deg,
-      #24133c 0%,
-      #291641 49.25%,
-      #29103c 100%
-    );
-    color: var(--value-color);
-    border: 1px solid #4b3870;
-
-    border-radius: var(--border-radius);
-
-    @include fonts.small;
-
-    &:hover,
-    &:active,
-    &:focus {
-      text-decoration: underline;
-    }
-
-    :global(svg) {
-      vertical-align: bottom;
-      margin: 0 0 var(--padding);
-      color: var(--secondary);
     }
   }
 
