b6305ad41e4646432bdbff1322723eeb94a5805f
David Dal Busco <david.dalbusco@dfinity.org>
2022-12-23 18:19:48 +0100

    style: extract global style to ensure build reproducibility (#1666)
    # Motivation
    
    We have build reproducibility with last releases.
    
    CI: hash a2c0186f49a3d3201575ade95bc6610e4a30ebe6 for
    https://github.com/dfinity/nns-dapp/actions/runs/3764587278
    Local: hash
    187a34a440503da62c161e679baa02b431455c17c2b6e51bc433d63dbcae2d8f
    
    Root cause of the issue is the build of the NNS-dapp frontend that
    generates different entries ("keys") in the ouputed
    `vite-manifest.json`.
    
    # Solution / Workaround
    
    The real solution still needs to be found but to fix the
    reproducibitility of current state we can avoid the use of CSS mixin in
    favor of a global style.
    
    If a global CSS style is used => a unique  CSS is generated
    If mixins are used => Svelte or Vite tries to optimize the CSS and
    generate a single outputed CSS for the common code. In this process it
    generates a file and the algorithm uses the name of the first match.
    Therefore we ended up with a `vite-manifest.json` containing sometimes
    `SignInCanisters.css` or `SignInNeurons.css`
    
    # Changes
    
    - extract common CSS to global style
    
    # Screenshots
    
    
    ![image](https://user-images.githubusercontent.com/16886711/209373803-86f85300-6120-4730-9c59-a9e2f887ca25.png)

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -50,5 +50,6 @@
 <style lang="scss" global>
   @import "@dfinity/gix-components/styles/global.scss";
   @import "../lib/themes/legacy";
+  @import "../lib/themes/global";
   @import "../lib/themes/variables";
 </style>
