629135dc4862724f550e08b88fa2e21687998909
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2022-12-29 07:04:42 +0100

    Fix: Rounding error in sns transactions (#1669)
    # Motivation
    
    Fix two bugs:
    
    * Rounding issue in SNS transactions.
    * Account balance was not updated after staking or topping up sns
    neurons.
    
    # Changes
    
    * Change implementation of `numberToE8s` to use
    `TokenAmount.fromNumber`.
    * Call `loadSnsAccounts` service after staking or topping up sns neuron.
    
    # Tests
    
    * Fix test.
    * Add test for new test cases of loadSnsAccounts call.
    * Add tests for token utils `numberToE8s`.

--- frontend/src/lib/services/sns-neurons.services.ts
+++ frontend/src/lib/services/sns-neurons.services.ts
@@ -37,6 +37,7 @@ import {
   hasAutoStakeMaturityOn,
   subaccountToHexString,
 } from "$lib/utils/sns-neuron.utils";
+import { numberToE8s } from "$lib/utils/token.utils";
 import { hexStringToBytes } from "$lib/utils/utils";
 import type { Identity } from "@dfinity/agent";
 import { Principal } from "@dfinity/principal";
@@ -53,6 +54,7 @@ import {
 } from "@dfinity/utils";
 import { get } from "svelte/store";
 import { getAuthenticatedIdentity } from "./auth.services";
+import { loadSnsAccounts } from "./sns-accounts.services";
 import {
   checkSnsNeuronBalances,
   neuronNeedsRefresh,
@@ -412,7 +414,7 @@ export const increaseStakeNeuron = async ({
   neuronId,
 }: {
   rootCanisterId: Principal;
-  amount: bigint;
+  amount: number;
   account: Account;
   neuronId: SnsNeuronId;
 }): Promise<{ success: boolean }> => {
@@ -420,14 +422,16 @@ export const increaseStakeNeuron = async ({
     // TODO: Get identity depending on account to support HW accounts
     const identity = await getAuthenticatedIdentity();
 
+    const stakeE8s = numberToE8s(amount);
     await increaseStakeNeuronApi({
       // We can cast it because we already checked that the neuron id is not undefined
       neuronId,
       rootCanisterId,
-      stakeE8s: amount,
+      stakeE8s,
       identity,
       source: decodeSnsAccount(account.identifier),
     });
+    await loadSnsAccounts({ rootCanisterId });
 
     return { success: true };
   } catch (err) {
@@ -448,20 +452,24 @@ export const stakeNeuron = async ({
   account,
 }: {
   rootCanisterId: Principal;
-  amount: bigint;
+  amount: number;
   account: Account;
 }): Promise<{ success: boolean }> => {
   try {
     // TODO: Get identity depending on account to support HW accounts
     const identity = await getAuthenticatedIdentity();
+    const stakeE8s = numberToE8s(amount);
     await stakeNeuronApi({
       controller: identity.getPrincipal(),
       rootCanisterId,
-      stakeE8s: amount,
+      stakeE8s,
       identity,
       source: decodeSnsAccount(account.identifier),
     });
-    await loadNeurons({ rootCanisterId, certified: true });
+    await Promise.all([
+      loadSnsAccounts({ rootCanisterId }),
+      loadNeurons({ rootCanisterId, certified: true }),
+    ]);
     return { success: true };
   } catch (err) {
     toastsError(
