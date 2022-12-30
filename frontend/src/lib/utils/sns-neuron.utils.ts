1b208f36b283c3f3a3707b59b8981deef231481a
Llorenç <llorenc.muntaner@dfinity.org>
2022-12-30 12:11:35 +0100

    Fix: Use helper

--- frontend/src/lib/utils/sns-neuron.utils.ts
+++ frontend/src/lib/utils/sns-neuron.utils.ts
@@ -38,7 +38,9 @@ export const getSnsNeuronState = ({
       : NeuronState.Locked;
   }
   if ("WhenDissolvedTimestampSeconds" in dissolveState) {
-    return dissolveState.WhenDissolvedTimestampSeconds < Date.now() / 1000
+    // In case `nowInSeconds` ever changes and doesn't return an integer we use Math.floor
+    return dissolveState.WhenDissolvedTimestampSeconds <
+      BigInt(Math.floor(nowInSeconds()))
       ? NeuronState.Dissolved
       : NeuronState.Dissolving;
   }
