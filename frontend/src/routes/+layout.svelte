8f086d34eafc6ab4df0ee23c90614038e3345f58
David Dal Busco <david.dalbusco@dfinity.org>
2022-12-30 10:18:45 +0100

    style: redo scoped styles

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -50,6 +50,5 @@
 <style lang="scss" global>
   @import "@dfinity/gix-components/styles/global.scss";
   @import "../lib/themes/legacy";
-  @import "../lib/themes/global";
   @import "../lib/themes/variables";
 </style>
