57e90923f6bbb8921c58dd02011a7e20e3b63462
David Dal Busco <david.dalbusco@dfinity.org>
2023-01-03 10:13:07 +0100

    feat: display balance on large screen

--- frontend/src/lib/components/accounts/AccountsTitle.svelte
+++ frontend/src/lib/components/accounts/AccountsTitle.svelte
@@ -1,37 +1,30 @@
 <script lang="ts">
   import type { TokenAmount } from "@dfinity/nns";
-  import { i18n } from "$lib/stores/i18n";
-  import { replacePlaceholders } from "$lib/utils/i18n.utils";
-  import { formatToken } from "$lib/utils/token.utils";
-  import AmountDisplay from "$lib/components/ic/AmountDisplay.svelte";
-  import Tooltip from "$lib/components/ui/Tooltip.svelte";
+  import Summary from "$lib/components/summary/Summary.svelte";
+  import AccountsBalance from "$lib/components/accounts/AccountsBalance.svelte";
 
   export let balance: TokenAmount | undefined;
 
-  let totalTokens: string;
-  $: totalTokens =
-    balance !== undefined
-      ? formatToken({
-          value: balance.toE8s(),
-          detailed: true,
-        })
-      : "";
+  let innerWidth = 0;
+  let summary = true;
+
+  // TODO: workaround to display the summary with title or balance only. It should be removed once we integrate the balance within the universe selector cards.
+  $: summary = innerWidth > 1024;
 </script>
 
-<div class="details">
-  {#if balance !== undefined}
-    <Tooltip
-      id="wallet-total-icp"
-      text={replacePlaceholders($i18n.accounts.current_balance_total, {
-          $amount: totalTokens,
-        })}
-    >
-      <AmountDisplay copy amount={balance}>
-        <span>{$i18n.accounts.total}</span>
-      </AmountDisplay>
-    </Tooltip>
+<svelte:window bind:innerWidth />
+
+{#if summary}
+  <Summary projects="display">
+    <div class="details" slot="details">
+      <AccountsBalance {balance} />
+    </div>
+  </Summary>
+  {:else}
+    <div class="amount">
+      <AccountsBalance {balance} />
+    </div>
   {/if}
-</div>
 
 <style lang="scss">
   @use "@dfinity/gix-components/styles/mixins/fonts";
@@ -39,6 +32,10 @@
   .details {
     height: var(--padding-4x);
     color: var(--description-color);
+    @include fonts.small;
+  }
+
+  .amount {
     padding: 0 0 var(--padding-2x);
   }
 </style>
