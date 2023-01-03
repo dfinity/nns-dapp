0cd7693f2cb2557862a288f37c60bbc6d9c93ee2
David Dal Busco <david.dalbusco@dfinity.org>
2023-01-03 07:59:42 +0100

    feat: use derived store

--- frontend/src/lib/components/universe/SelectProjectDropdown.svelte
+++ frontend/src/lib/components/universe/SelectProjectDropdown.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-  import { onDestroy, onMount } from "svelte";
-  import { OWN_CANISTER_ID } from "$lib/constants/canister-ids.constants";
+  import { onMount } from "svelte";
   import { snsProjectIdSelectedStore } from "$lib/derived/selected-project.derived";
-  import { i18n } from "$lib/stores/i18n";
-  import { committedProjectsStore } from "$lib/stores/projects.store";
   import { Dropdown, DropdownItem, Spinner } from "@dfinity/gix-components";
   import { goto } from "$app/navigation";
   import SummaryLogo from "$lib/components/summary/SummaryLogo.svelte";
   import { buildSwitchUniverseUrl } from "$lib/utils/navigation.utils";
+  import { selectableProjects } from "$lib/derived/selectable-projects.derived";
+  import { i18n } from "$lib/stores/i18n";
 
   let selectedCanisterId: string | undefined;
 
@@ -19,7 +18,7 @@
   // Setting the selected value before it's available in `selectableProjects` was not setting it as selected in the dropdown.
   const updateSelectedCanisterId = () => {
     if (
-      selectableProjects.find(
+      $selectableProjects.find(
         ({ canisterId }) => $snsProjectIdSelectedStore.toText() === canisterId
       ) !== undefined
     ) {
@@ -34,37 +33,7 @@
   })();
 
   // Update the selected canister id when we selectableProjects are loaded
-  $: selectableProjects, updateSelectedCanisterId();
-
-  type SelectableProject = {
-    name: string;
-    canisterId: string;
-  };
-
-  const nnsProject = {
-    name: $i18n.core.nns,
-    canisterId: OWN_CANISTER_ID.toText(),
-  };
-
-  let selectableProjects: SelectableProject[] = [nnsProject];
-  const unsubscribe = committedProjectsStore.subscribe((projects) => {
-    selectableProjects = [
-      nnsProject,
-      ...(projects?.map(
-        ({
-          rootCanisterId,
-          summary: {
-            metadata: { name },
-          },
-        }) => ({
-          name,
-          canisterId: rootCanisterId.toText(),
-        })
-      ) ?? []),
-    ];
-  });
-
-  onDestroy(unsubscribe);
+  $: $selectableProjects, updateSelectedCanisterId();
 </script>
 
 <!-- We don't want to render the dropdown until we have a selected canister id. -->
@@ -77,8 +46,8 @@
   >
     <SummaryLogo slot="start" />
 
-    {#each selectableProjects as { canisterId, name } (canisterId)}
-      <DropdownItem value={canisterId}>{name}</DropdownItem>
+    {#each $selectableProjects as { canisterId, summary } (canisterId)}
+      <DropdownItem value={canisterId}>{summary?.metadata.name ?? $i18n.core.nns}</DropdownItem>
     {/each}
   </Dropdown>
 {:else}
