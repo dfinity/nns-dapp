147ab6373b0d8e6913ebf8c5524751987ab6f816
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-01-03 06:02:31 +0100

    feat: DayInput component

---
+++ frontend/src/lib/components/common/MinButton.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { IconSubdirectory } from "@dfinity/gix-components";
+  import { i18n } from "$lib/stores/i18n";
+</script>
+
+<button on:click|preventDefault class="ghost" type="button">
+  <span class="icon">
+    <IconSubdirectory />
+  </span>
+  {$i18n.core.min}
+</button>
+
+<style lang="scss">
+  button {
+    display: flex;
+    align-items: center;
+    gap: var(--padding-0_5x);
+  }
+  .icon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transform: scaleX(-1);
+  }
+</style>
---
+++ frontend/src/lib/components/ui/DayInput.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import MaxButton from "$lib/components/common/MaxButton.svelte";
+  import InputWithError from "./InputWithError.svelte";
+  import MinButton from "$lib/components/common/MinButton.svelte";
+
+  export let days: number | undefined = undefined;
+  export let min: number | undefined = undefined;
+  export let max: number | undefined = undefined;
+  export let label: string | undefined = undefined;
+  export let errorMessage: string | undefined = undefined;
+
+  const dispatch = createEventDispatcher();
+  const setMin = () => dispatch("nnsMin");
+  const setMax = () => dispatch("nnsMax");
+</script>
+
+<InputWithError
+  placeholderLabelKey="core.amount"
+  name="amount"
+  bind:value={days}
+  {max}
+  inputType="number"
+  {errorMessage}
+  on:input
+  on:blur
+>
+  <MinButton on:click={setMin} slot="left" />
+  <MaxButton on:click={setMax} slot="right" />
+</InputWithError>
