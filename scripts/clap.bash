f757f8845b4061cba556fcaffc08009ef325341a
Max <max@dfinity.org>
2023-01-04 10:48:00 +0100

    ++

--- scripts/clap.bash
+++ scripts/clap.bash
@@ -1,45 +1,22 @@
-# Optparse - a BASH argument parser
-# Heavily modified from an original by:
-# Optparse - a BASH wrapper for getopts < doesn't use optparse any more.  Just bash.
-# https://github.com/nk412/optparse
-# Copyright (c) 2015 Nagarjuna Kumarappan
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
-# Author: Nagarjuna Kumarappan <nagarjuna.412@gmail.com>
+# clap - a BASH argument parser
+# This parser aims to have similar parsing semantics as Rust's clap parser; if it doesn't look anything like clap it's not just you.
 
-optparse_usage=""
-optparse_flags=""
-optparse_defaults=""
-optparse_arguments_string=""
+clap_usage=""
+clap_flags=""
+clap_defaults=""
+clap_arguments_string=""
 
 # -----------------------------------------------------------------------------------------------------------------------------
-function optparse.throw_error() {
+function clap.throw_error() {
 	local message="$1"
 	echo "OPTPARSE: ERROR: $message"
 	exit 1
 }
 
 # -----------------------------------------------------------------------------------------------------------------------------
-function optparse.define() {
+function clap.define() {
 	if [ $# -lt 3 ]; then
-		optparse.throw_error "optparse.define <short> <long> <variable> [<desc>] [<default>] [<value>] [<nargs>]"
+		clap.throw_error "clap.define <short> <long> <variable> [<desc>] [<default>] [<value>] [<nargs>]"
 	fi
 	local nargs=""
 	for option_id in $(seq 1 $#); do
@@ -54,13 +31,13 @@ function optparse.define() {
 		if [ "$key" = "short" ]; then
 			local shortname="$value"
 			if [ ${#shortname} -ne 1 ]; then
-				optparse.throw_error "short name expected to be one character long"
+				clap.throw_error "short name expected to be one character long"
 			fi
 			local short="-${shortname}"
 		elif [ "$key" = "long" ]; then
 			local longname="$value"
 			if [ ${#longname} -lt 2 ]; then
-				optparse.throw_error "long name expected to be atleast one character long"
+				clap.throw_error "long name expected to be at least one character long"
 			fi
 			local long="--${longname}"
 		elif [ "$key" = "desc" ]; then
@@ -77,7 +54,7 @@ function optparse.define() {
 	done
 
 	if [ "$variable" = "" ]; then
-		optparse.throw_error "You must give a variable for option: ($short/$long)"
+		clap.throw_error "You must give a variable for option: ($short/$long)"
 	fi
 
 	if [ "$val" = "" ]; then
@@ -85,30 +62,30 @@ function optparse.define() {
 	fi
 
 	# build OPTIONS and help
-	optparse_usage="${optparse_usage}#NL#TB${short} $(printf "%-25s %s" "${long}:" "${desc}")"
+	clap_usage="${clap_usage}#NL#TB${short} $(printf "%-25s %s" "${long}:" "${desc}")"
 	if [ "$default" != "" ] && [ "${nargs:-}" != "0" ]; then
-		optparse_usage="${optparse_usage} [default:$default]"
+		clap_usage="${clap_usage} [default:$default]"
 	fi
 	if [ "${nargs:-}" == "" ]; then
-		optparse_flags="${optparse_flags}#NL#TB#TB${long}${short:+|${short}})#NL#TB#TB#TB${variable}=\"\$1\"; shift 1;;"
+		clap_flags="${clap_flags}#NL#TB#TB${long}${short:+|${short}})#NL#TB#TB#TB${variable}=\"\$1\"; shift 1;;"
 	elif [ "${nargs:-}" == "0" ]; then
-		optparse_flags="${optparse_flags}#NL#TB#TB${long}${short:+|${short}})#NL#TB#TB#TB${variable}=\"true\";;"
+		clap_flags="${clap_flags}#NL#TB#TB${long}${short:+|${short}})#NL#TB#TB#TB${variable}=\"true\";;"
 	else
-		optparse_flags="${optparse_flags}#NL#TB#TB${long}${short:+|${short}})#NL#TB#TB#TB${variable}=(); for ((i=0; i<nargs; i++)); do ${variable}+=( \"\$1\" ); shift 1; done;;"
+		clap_flags="${clap_flags}#NL#TB#TB${long}${short:+|${short}})#NL#TB#TB#TB${variable}=(); for ((i=0; i<nargs; i++)); do ${variable}+=( \"\$1\" ); shift 1; done;;"
 	fi
 	if [ "$default" != "" ]; then
-		optparse_defaults="${optparse_defaults}#NL${variable}=${default}"
+		clap_defaults="${clap_defaults}#NL${variable}=${default}"
 	fi
-	optparse_arguments_string="${optparse_arguments_string}${shortname}"
+	clap_arguments_string="${clap_arguments_string}${shortname}"
 	if [ "$val" = "\$OPTARG" ]; then
-		optparse_arguments_string="${optparse_arguments_string}:"
+		clap_arguments_string="${clap_arguments_string}:"
 	fi
 }
 
 # -----------------------------------------------------------------------------------------------------------------------------
-function optparse.build() {
+function clap.build() {
 	local build_file
-	build_file="$(mktemp -t "optparse-XXXXXX.tmp")"
+	build_file="$(mktemp -t "clap-XXXXXX.tmp")"
 
 	# Function usage
 	cat <<EOF >"$build_file"
@@ -117,7 +94,7 @@ cat << XXX
 usage: \$(basename "\$0") [OPTIONS]
 
 OPTIONS:
-        $optparse_usage
+        $clap_usage
 
         -? --help  :  usage
 
@@ -127,7 +104,7 @@ XXX
 }
 
 # Set default variable values
-$optparse_defaults
+$clap_defaults
 
 # Contract long options into short options
 while [ \$# -ne 0 ]; do
@@ -135,7 +112,7 @@ while [ \$# -ne 0 ]; do
         shift 1
 
         case "\$param" in
-                $optparse_flags
+                $clap_flags
                 "-?"|--help)
 			print_help
 			echo
@@ -166,10 +143,10 @@ EOF
 		cat <<<"$(sed "s/#TB/\t/g" "$build_file")" > "$build_file"
 
 	# Unset global variables
-	unset optparse_usage
-	unset optparse_arguments_string
-	unset optparse_defaults
-	unset optparse_flags
+	unset clap_usage
+	unset clap_arguments_string
+	unset clap_defaults
+	unset clap_flags
 
 	# Return file name to parent
 	echo "$build_file"
