6d488d0c4fec35f4748cd0a160610f46004b8fda
Llorenç <llorenc.muntaner@dfinity.org>
2023-01-07 08:38:29 +0100

    GIX-1209: Safety redirection

--- frontend/src/lib/pages/SnsProposals.svelte
+++ frontend/src/lib/pages/SnsProposals.svelte
@@ -1,1 +1,16 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { OWN_CANISTER_ID } from "$lib/constants/canister-ids.constants";
+  import { ENABLE_SNS_VOTING } from "$lib/constants/environment.constants";
+  import { buildProposalsUrl } from "$lib/utils/navigation.utils";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    // We don't render this page if not enabled, but to be safe we redirect to the NNS proposals page as well.
+    if (!ENABLE_SNS_VOTING) {
+      goto(buildProposalsUrl({ universe: OWN_CANISTER_ID.toText() }));
+    }
+  });
+</script>
+
 <div data-tid="sns-proposals-page">To be developed</div>
