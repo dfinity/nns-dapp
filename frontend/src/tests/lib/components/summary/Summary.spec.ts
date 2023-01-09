a8b22555ae21ab1ccab550714aeac7ea721db6f9
David Dal Busco <david.dalbusco@dfinity.org>
2023-01-09 12:59:07 +0100

    test: summary

---
+++ frontend/src/tests/lib/components/summary/Summary.spec.ts
@@ -0,0 +1,71 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import Summary from "$lib/components/summary/Summary.svelte";
+import { snsProjectSelectedStore } from "$lib/derived/selected-project.derived";
+import { render } from "@testing-library/svelte";
+import { mockStoreSubscribe } from "../../../mocks/commont.mock";
+import en from "../../../mocks/i18n.mock";
+import { mockSnsFullProject } from "../../../mocks/sns-projects.mock";
+
+describe("Summary", () => {
+  it("should render a property size", () => {
+    const { container } = render(Summary, { props: { size: "small" } });
+    expect(container.querySelector(".small")).not.toBeNull();
+  });
+
+  it("should render a logo", () => {
+    const { getByTestId } = render(Summary);
+    expect(getByTestId("summary-logo")).not.toBeNull();
+  });
+
+  it("should render a logo", () => {
+    const { getByTestId } = render(Summary);
+    expect(getByTestId("summary-logo")).not.toBeNull();
+  });
+
+  it("should render internet computer if none", () => {
+    const { container } = render(Summary, { props: { projects: "none" } });
+    expect(
+      container?.querySelector("h1")?.textContent?.includes(en.core.ic)
+    ).toBeTruthy();
+  });
+
+  describe("nns", () => {
+    beforeAll(() =>
+      jest
+        .spyOn(snsProjectSelectedStore, "subscribe")
+        .mockImplementation(mockStoreSubscribe(undefined))
+    );
+
+    afterAll(() => jest.clearAllMocks());
+
+    it("should render internet computer", () => {
+      const { container } = render(Summary, { props: { projects: "display" } });
+
+      expect(
+        container?.querySelector("h1")?.textContent?.includes(en.core.ic)
+      ).toBeTruthy();
+    });
+  });
+
+  describe("sns", () => {
+    beforeAll(() =>
+      jest
+        .spyOn(snsProjectSelectedStore, "subscribe")
+        .mockImplementation(mockStoreSubscribe(mockSnsFullProject))
+    );
+
+    afterAll(() => jest.clearAllMocks());
+
+    it("should render project", () => {
+      const { container } = render(Summary, { props: { projects: "display" } });
+      expect(
+        container
+          ?.querySelector("h1")
+          ?.textContent?.includes(mockSnsFullProject.summary.metadata.name)
+      ).toBeTruthy();
+    });
+  });
+});
