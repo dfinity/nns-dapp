fb9d516dc3291bd481862acc1bf3a3e7362c257f
David Dal Busco <david.dalbusco@dfinity.org>
2023-01-09 14:19:31 +0100

    test: skeleton

---
+++ frontend/src/tests/lib/components/ui/SkeletonCard.spec.ts
@@ -0,0 +1,23 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import SkeletonCard from "$lib/components/ui/SkeletonCard.svelte";
+import { render } from "@testing-library/svelte";
+
+describe("SkeletonCard", () => {
+  it("should render small", () => {
+    const { container } = render(SkeletonCard);
+    expect(container.querySelectorAll("span")?.length).toEqual(4);
+  });
+
+  it("should render medium", () => {
+    const { container } = render(SkeletonCard, { props: { size: "medium" } });
+    expect(container.querySelectorAll("span")?.length).toEqual(6);
+  });
+
+  it("should render large", () => {
+    const { container } = render(SkeletonCard, { props: { size: "large" } });
+    expect(container.querySelectorAll("span")?.length).toEqual(8);
+  });
+});
