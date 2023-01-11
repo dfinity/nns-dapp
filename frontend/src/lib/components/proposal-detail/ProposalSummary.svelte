bc149c2819ffe4a280207929495a131c2b8bc915
David Dal Busco <david.dalbusco@dfinity.org>
2023-01-11 13:54:55 +0100

    style: proposal markdown display improvements (#1697)
    # Motivation
    
    Hard to style with `:global` and without that a huge variety of test content but following style rules should improve a bit the display of the markdown of the proposals. Let me know if you have better ideas!
    
    # Changes
    
    - tweak `:global` of the `.markdown`

--- frontend/src/lib/components/proposal-detail/ProposalSummary.svelte
+++ frontend/src/lib/components/proposal-detail/ProposalSummary.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Markdown from "$lib/components/ui/Markdown.svelte";
+  import { nonNullish } from "$lib/utils/utils";
 
   export let summary: string | undefined;
 
@@ -10,6 +11,10 @@
 <div class="markdown">
   {#if showTitle}
     <div class="title"><slot name="title" /></div>
+
+    {#if nonNullish(summary) && summary !== ""}
+      <hr />
+    {/if}
   {/if}
 
   <Markdown text={summary} />
@@ -17,21 +22,45 @@
 
 <style lang="scss">
   @use "@dfinity/gix-components/styles/mixins/media";
-
-  .title {
-    margin-bottom: var(--padding-4x);
-  }
+  @use "@dfinity/gix-components/styles/mixins/fonts";
 
   .markdown {
     overflow-wrap: break-word;
 
-    :global(a) {
-      font-size: inherit;
+    :global(*) {
+      color: var(--label-color);
     }
 
-    :global(pre) {
-      // make the <code> scrollable
-      overflow-x: auto;
+    :global(strong) {
+      font-weight: var(--font-weight-bold);
+    }
+
+    :global(a) {
+      @include fonts.standard;
+      color: var(--tertiary);
+
+      &:hover,
+      &:active {
+        color: var(--primary);
+      }
+    }
+
+    :global(h1) {
+      @include fonts.h4;
+      margin-bottom: var(--padding-2x);
+    }
+
+    :global(h2) {
+      @include fonts.h5;
+      margin-bottom: var(--padding-2x);
+    }
+
+    :global(h3),
+    :global(h4),
+    :global(h5),
+    :global(h6) {
+      @include fonts.standard(true);
+      margin-bottom: var(--padding);
     }
 
     :global(h1),
@@ -40,31 +69,72 @@
     :global(h4),
     :global(h5),
     :global(h6) {
-      line-height: var(--line-height-standard);
-      font-weight: normal;
-      color: inherit;
+      color: var(--value-color);
+      font-weight: 400;
     }
 
-    :global(h1) {
-      font-size: var(--font-size-h4);
+    :global(table),
+    :global(pre) {
+      background: var(--input-background);
+      color: var(--input-background-contrast);
+      border-radius: var(--border-radius);
+
+      font-family: ui-monospace, SFMono-Regular, SF Mono, Menlo, Consolas,
+        Liberation Mono, monospace;
     }
 
-    :global(h2) {
-      font-size: var(--font-size-h5);
-    }
-
-    :global(h3),
-    :global(h4),
-    :global(h5),
-    :global(h6) {
-      zoom: 0.8;
+    :global(table *),
+    :global(pre) {
+      @include fonts.small;
     }
 
     :global(table) {
       display: block;
       overflow: auto;
-      padding: var(--padding) 0 var(--padding);
       margin: var(--padding) 0 var(--padding-2x);
+      border-spacing: var(--padding-2x) 0;
+      padding: var(--padding-2x) 0;
+    }
+
+    :global(th) {
+      font-weight: 400;
+    }
+
+    :global(pre) {
+      // make the <code> scrollable
+      overflow-x: auto;
+
+      margin: var(--padding-3x) 0 var(--padding-4x);
+      padding: var(--padding-2x);
+    }
+
+    :global(p),
+    :global(ul),
+    :global(ol),
+    :global(dl) {
+      margin: 0 0 var(--padding-3x);
+    }
+
+    :global(hr) {
+      color: var(--line);
+      margin: var(--padding-3x) 0;
+    }
+  }
+
+  @include media.dark-theme {
+    .markdown {
+      :global(a) {
+        &:hover,
+        &:active {
+          color: var(--primary-contrast);
+        }
+      }
+
+      :global(table),
+      :global(pre) {
+        background: var(--card-background);
+        color: var(--card-background-contrast);
+      }
     }
   }
 </style>
