96337258f17d2730a90539181bf6c60fe088c621
David Dal Busco <david.dalbusco@dfinity.org>
2023-01-11 07:31:17 +0100

    feat: shorten ellipsis to avoid overflow

--- frontend/src/lib/utils/format.utils.ts
+++ frontend/src/lib/utils/format.utils.ts
@@ -30,7 +30,16 @@ export const formatPercentage = (
 /**
  * Shortens the text from the middle. Ex: "12345678901234567890" -> "1234567...5678901"
  * @param text
+ * @param splitLength An optional length for the split. e.g. 12345678 becomes, if splitLength = 2, 12...78
  * @returns text
  */
-export const shortenWithMiddleEllipsis = (text: string): string =>
-  text.length > 16 ? `${text.slice(0, 7)}...${text.slice(-7)}` : text;
+export const shortenWithMiddleEllipsis = (
+  text: string,
+  splitLength = 7
+): string => {
+  // Original min length was 16 to extract 7 split
+  const minLength = splitLength * 2 + 2;
+  return text.length > minLength
+    ? `${text.slice(0, splitLength)}...${text.slice(-1 * splitLength)}`
+    : text;
+};
