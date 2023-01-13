45c9e07d27dcd23de07bc232a40653533f64c404
Llorenç <llorenc.muntaner@dfinity.org>
2023-01-13 13:28:04 +0100

    GIX-1211: CR changes

--- frontend/src/lib/components/sns-proposals/SnsProposalsList.svelte
+++ frontend/src/lib/components/sns-proposals/SnsProposalsList.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import type { SnsProposalData } from "@dfinity/sns";
-  import { i18n } from "$lib/stores/i18n";
   import SnsProposalCard from "$lib/components/sns-proposals/SnsProposalCard.svelte";
-  import { InfiniteScroll, Spinner } from "@dfinity/gix-components";
+  import { InfiniteScroll } from "@dfinity/gix-components";
   import type { SnsNervousSystemFunction } from "@dfinity/sns";
-  import SkeletonCard from "$lib/components/ui/SkeletonCard.svelte";
   import { fromNullable } from "@dfinity/utils";
+  import NoProposals from "../proposals/NoProposals.svelte";
+  import LoadingProposals from "../proposals/LoadingProposals.svelte";
+  import ListLoader from "../proposals/ListLoader.svelte";
 
   export let proposals: SnsProposalData[] | undefined;
   export let nsFunctions: SnsNervousSystemFunction[] | undefined;
@@ -16,35 +17,20 @@
 <!-- TODO: Remove when implementing filters https://dfinity.atlassian.net/browse/GIX-1212 -->
 <div data-tid="sns-proposals-page">
   {#if proposals === undefined}
-    <div class="card-grid" data-tid="proposals-loading">
-      <SkeletonCard />
-      <SkeletonCard />
-      <SkeletonCard />
-    </div>
+    <LoadingProposals />
   {:else if proposals.length === 0}
-    <p class="description" data-tid="no-proposals-msg">
-      {$i18n.voting.nothing_found}
-    </p>
+    <NoProposals />
   {:else}
-    <InfiniteScroll
-      layout="grid"
-      on:nnsIntersect
-      disabled={disableInfiniteScroll}
-    >
-      {#each proposals as proposalData (fromNullable(proposalData.id)?.id)}
-        <SnsProposalCard {proposalData} {nsFunctions} />
-      {/each}
-    </InfiniteScroll>
-    {#if loadingNextPage}
-      <div class="spinner" data-tid="next-page-sns-proposals-spinner">
-        <Spinner inline />
-      </div>
-    {/if}
+    <ListLoader loading={loadingNextPage}>
+      <InfiniteScroll
+        layout="grid"
+        on:nnsIntersect
+        disabled={disableInfiniteScroll}
+      >
+        {#each proposals as proposalData (fromNullable(proposalData.id)?.id)}
+          <SnsProposalCard {proposalData} {nsFunctions} />
+        {/each}
+      </InfiniteScroll>
+    </ListLoader>
   {/if}
 </div>
-
-<style lang="scss">
-  .spinner {
-    margin: var(--padding-4x) 0 0;
-  }
-</style>
