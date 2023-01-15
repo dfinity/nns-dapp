c922cf4d3ab919d56f0b74c43e5fcd56f88f85dc
David Dal Busco <david.dalbusco@dfinity.org>
2023-01-15 13:29:04 +0100

    feat: query sns balance with an update call on selection

--- frontend/src/lib/routes/Accounts.svelte
+++ frontend/src/lib/routes/Accounts.svelte
@@ -9,9 +9,49 @@
   import SnsAccountsFooter from "$lib/components/accounts/SnsAccountsFooter.svelte";
   import { uncertifiedLoadSnsBalances } from "$lib/services/projects.services";
   import { snsSummariesStore } from "$lib/stores/sns.store";
+  import type { Principal } from "@dfinity/principal";
+  import { projectsAccountsBalance } from "$lib/derived/projects-accounts-balance.derived";
+  import { isNullish } from "$lib/utils/utils";
+  import { loadSnsBalance } from "$lib/services/sns-accounts-balance.services";
+  import { isNnsProject } from "$lib/utils/projects.utils";
 
   $: (async () =>
     await uncertifiedLoadSnsBalances({ summaries: $snsSummariesStore }))();
+
+  let projectIdSelected = $snsProjectIdSelectedStore;
+
+  const certifySnsBalance = async (snsProjectId: Principal) => {
+    // Update Sns balance only when the selected project changes
+    if (projectIdSelected.toText() === snsProjectId.toText()) {
+      return;
+    }
+
+    projectIdSelected = snsProjectId;
+
+    if (isNnsProject(projectIdSelected)) {
+      return;
+    }
+
+    const projectAccountsBalance = $projectsAccountsBalance[projectIdSelected];
+
+    // e.g. user has entered an unknown project ID in the browser url bar
+    // Should not happen because user is redirect to Nns but never too sure
+    if (isNullish(projectAccountsBalance)) {
+      return;
+    }
+
+    // If data are already certified we do not have to refresh these to avoid to stress the network #performance
+    if (projectAccountsBalance.certified) {
+      return;
+    }
+
+    await loadSnsBalance({
+      rootCanisterId: projectIdSelected,
+      strategy: "update",
+    });
+  };
+
+  $: (async () => await certifySnsBalance($snsProjectIdSelectedStore))();
 </script>
 
 <main>
