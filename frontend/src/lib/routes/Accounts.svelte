3fe985eb11211c4210cb75d8dd8e90f00dec28f9
David Dal Busco <david.dalbusco@dfinity.org>
2023-01-15 14:49:13 +0100

    feat: refresh Sns projects balance on changes with query and update calls

--- frontend/src/lib/routes/Accounts.svelte
+++ frontend/src/lib/routes/Accounts.svelte
@@ -7,52 +7,17 @@
   } from "$lib/derived/selected-project.derived";
   import SnsAccounts from "$lib/pages/SnsAccounts.svelte";
   import SnsAccountsFooter from "$lib/components/accounts/SnsAccountsFooter.svelte";
-  import { uncertifiedLoadSnsBalances } from "$lib/services/projects.services";
   import { snsSummariesStore } from "$lib/stores/sns.store";
-  import type { Principal } from "@dfinity/principal";
-  import { projectsAccountsBalance } from "$lib/derived/projects-accounts-balance.derived";
-  import { isNullish } from "$lib/utils/utils";
-  import { loadSnsBalance } from "$lib/services/sns-accounts-balance.services";
-  import { isNnsProject } from "$lib/utils/projects.utils";
+  import { uncertifiedLoadSnsAccountsBalances } from "$lib/services/sns-accounts-balance.services";
 
-  $: (async () =>
-    await uncertifiedLoadSnsBalances({ summaries: $snsSummariesStore }))();
-
+  // Selected project ID on mount
   let projectIdSelected = $snsProjectIdSelectedStore;
 
-  const certifySnsBalance = async (snsProjectId: Principal) => {
-    // Update Sns balance only when the selected project changes
-    if (projectIdSelected.toText() === snsProjectId.toText()) {
-      return;
-    }
-
-    projectIdSelected = snsProjectId;
-
-    if (isNnsProject(projectIdSelected)) {
-      return;
-    }
-
-    const projectAccountsBalance =
-      $projectsAccountsBalance[projectIdSelected.toText()];
-
-    // e.g. user has entered an unknown project ID in the browser url bar
-    // Should not happen because user is redirect to Nns but never too sure
-    if (isNullish(projectAccountsBalance)) {
-      return;
-    }
-
-    // If data are already certified we do not have to refresh these to avoid to stress the network #performance
-    if (projectAccountsBalance.certified) {
-      return;
-    }
-
-    await loadSnsBalance({
-      rootCanisterId: projectIdSelected,
-      strategy: "update",
-    });
-  };
-
-  $: (async () => await certifySnsBalance($snsProjectIdSelectedStore))();
+  $: (async () =>
+    await uncertifiedLoadSnsAccountsBalances({
+      summaries: $snsSummariesStore,
+      excludeRootCanisterIds: [projectIdSelected.toText()],
+    }))();
 </script>
 
 <main>
--- frontend/src/lib/services/sns-accounts-balance.services.ts
+++ frontend/src/lib/services/sns-accounts-balance.services.ts
@@ -1,21 +1,20 @@
 import { getSnsAccounts } from "$lib/api/sns-ledger.api";
-import {
-  queryAndUpdate,
-  type QueryAndUpdateStrategy,
-} from "$lib/services/utils.services";
+import { queryAndUpdate } from "$lib/services/utils.services";
 import { snsAccountsBalanceStore } from "$lib/stores/sns-accounts-balance.store";
 import { toastsError } from "$lib/stores/toasts.store";
 import type { Account } from "$lib/types/account";
-import type { RootCanisterId } from "$lib/types/sns";
+import type {
+  RootCanisterId,
+  RootCanisterIdText,
+  SnsSummary,
+} from "$lib/types/sns";
 import { toToastError } from "$lib/utils/error.utils";
 import { sumAccounts } from "$lib/utils/sns-accounts.utils";
 
-export const loadSnsBalance = ({
+const uncertifiedLoadSnsAccountsBalance = ({
   rootCanisterId,
-  strategy,
 }: {
   rootCanisterId: RootCanisterId;
-  strategy: QueryAndUpdateStrategy;
 }): Promise<void> => {
   return queryAndUpdate<Account[], unknown>({
     request: ({ certified, identity }) =>
@@ -44,6 +43,35 @@ export const loadSnsBalance = ({
       );
     },
     logMessage: "Syncing Sns Balance",
-    strategy,
+    strategy: "query",
   });
 };
+
+/**
+ * Load Sns projects accounts balances
+ *
+ * ⚠️ WARNING: this feature only performs "query" calls. Effective "update" is performed when a Sns project is manually selected either through the token navigation switcher or accessed directly via the browser url.
+ *
+ * @param summaries The list of summaries - Sns projects - for which the balance of the accounts should be fetched.
+ * @param excludeRootCanisterIds As the balance is also loaded by loadSnsAccounts() - to perform query and UPDATE call - this variable can be used to avoid to perform unnecessary query and per extension to override data in the balance store.
+ */
+export const uncertifiedLoadSnsAccountsBalances = ({
+  summaries,
+  excludeRootCanisterIds = [],
+}: {
+  summaries: SnsSummary[];
+  excludeRootCanisterIds?: RootCanisterIdText[];
+}): Promise<void[]> =>
+  Promise.all(
+    (
+      summaries.filter(
+        ({ metadataCertified, rootCanisterId }) =>
+          metadataCertified === false &&
+          !excludeRootCanisterIds.includes(rootCanisterId.toText())
+      ) ?? []
+    ).map(({ rootCanisterId }) =>
+      uncertifiedLoadSnsAccountsBalance({
+        rootCanisterId,
+      })
+    )
+  );
