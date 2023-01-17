767ece88728bfd9508713d2a14ffd668d8159afa
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-01-17 08:12:36 +0100

    fix: remove Summary from proposal detail page

--- frontend/src/lib/routes/ProposalDetail.svelte
+++ frontend/src/lib/routes/ProposalDetail.svelte
@@ -6,7 +6,6 @@
     snsProjectIdSelectedStore,
   } from "$lib/derived/selected-project.derived";
   import { ENABLE_SNS_VOTING } from "$lib/constants/environment.constants";
-  import Summary from "$lib/components/summary/Summary.svelte";
   import type { AppPath } from "$lib/constants/routes.constants";
 
   export let referrerPath: AppPath | undefined = undefined;
@@ -14,9 +13,6 @@
 </script>
 
 <main>
-  {#if ENABLE_SNS_VOTING}
-    <Summary />
-  {/if}
   {#if $isNnsProjectStore || !ENABLE_SNS_VOTING}
     <NnsProposalDetail {referrerPath} {proposalIdText} />
   {:else if $snsProjectIdSelectedStore !== undefined && ENABLE_SNS_VOTING}
