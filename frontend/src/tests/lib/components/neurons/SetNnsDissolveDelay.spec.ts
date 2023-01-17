692990f9402c195f2086906f8b3d394c9f376c16
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-01-17 15:50:20 +0100

    refactor: rename SetNnsDissolveDelay

--- frontend/src/tests/lib/components/neurons/SetNnsDissolveDelay.spec.ts
+++ frontend/src/tests/lib/components/neurons/SetNnsDissolveDelay.spec.ts
@@ -1,14 +1,14 @@
 /**
  * @jest-environment jsdom
  */
-import SetDissolveDelay from "$lib/components/neurons/SetDissolveDelay.svelte";
+import SetNnsDissolveDelay from "$lib/components/neurons/SetNnsDissolveDelay.svelte";
 import { render } from "@testing-library/svelte";
 import { mockNeuron } from "../../../mocks/neurons.mock";
 
-describe("SetDissolveDelay", () => {
+describe("SetNnsDissolveDelay", () => {
   // Tested in CreateNeuronModal.spec.ts
   it("is not tested in isolation", () => {
-    render(SetDissolveDelay, {
+    render(SetNnsDissolveDelay, {
       props: {
         neuron: mockNeuron,
         cancelButtonText: "Cancel",
