b6c47e61455051ffc6df50ec8c79d9394e4b9398
David Dal Busco <david.dalbusco@dfinity.org>
2023-01-18 16:58:43 +0100

    feat: use constant

--- frontend/src/lib/constants/metrics.constants.ts
+++ frontend/src/lib/constants/metrics.constants.ts
@@ -1,2 +1,4 @@
+import { SECONDS_IN_MINUTE } from "$lib/constants/constants";
+
 // Workers
-export const SYNC_METRICS_TIMER_INTERVAL = 60000; // 1 minute
+export const SYNC_METRICS_TIMER_INTERVAL = SECONDS_IN_MINUTE * 1000; // 1 minute
