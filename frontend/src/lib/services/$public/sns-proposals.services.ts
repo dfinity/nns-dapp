865282d39fdffa59810eac2e16aaf05cb0099b3b
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-01-18 11:57:24 +0100

    feat: sns loadProposal

--- frontend/src/lib/services/$public/sns-proposals.services.ts
+++ frontend/src/lib/services/$public/sns-proposals.services.ts
@@ -1,10 +1,16 @@
-import { queryProposals } from "$lib/api/sns-governance.api";
+import { queryProposal, queryProposals } from "$lib/api/sns-governance.api";
 import { DEFAULT_SNS_PROPOSALS_PAGE_SIZE } from "$lib/constants/sns-proposals.constants";
 import { snsProposalsStore } from "$lib/stores/sns-proposals.store";
-import { toastsError } from "$lib/stores/toasts.store";
+import { toastsError, toastsShow } from "$lib/stores/toasts.store";
+import { errorToString } from "$lib/utils/error.utils";
 import type { Principal } from "@dfinity/principal";
 import type { SnsProposalData, SnsProposalId } from "@dfinity/sns";
-import { queryAndUpdate } from "../utils.services";
+import {
+  queryAndUpdate,
+  type QueryAndUpdateOnError,
+  type QueryAndUpdateOnResponse,
+  type QueryAndUpdateStrategy,
+} from "../utils.services";
 
 export const loadSnsProposals = async ({
   rootCanisterId,
@@ -39,5 +45,100 @@ export const loadSnsProposals = async ({
         err,
       });
     },
+  });
+};
+
+/**
+ * Get from store or query a proposal and apply the result to the callback (`setProposal`).
+ * The function propagate error to the toast and call an optional callback in case of error.
+ */
+export const loadProposal = async ({
+  rootCanisterId,
+  proposalId,
+  setProposal,
+  handleError,
+  callback,
+  silentErrorMessages,
+  silentUpdateErrorMessages,
+  strategy = "query_and_update",
+}: {
+  rootCanisterId: Principal;
+  proposalId: bigint;
+  setProposal: (proposal: SnsProposalData) => void;
+  handleError?: (certified: boolean) => void;
+  callback?: (certified: boolean) => void;
+  silentErrorMessages?: boolean;
+  silentUpdateErrorMessages?: boolean;
+  strategy?: QueryAndUpdateStrategy;
+}): Promise<void> => {
+  const catchError: QueryAndUpdateOnError<Error | unknown> = (
+    erroneusResponse
+  ) => {
+    console.error(erroneusResponse);
+
+    const skipUpdateErrorHandling =
+      silentUpdateErrorMessages === true && erroneusResponse.certified === true;
+
+    if (silentErrorMessages !== true && !skipUpdateErrorHandling) {
+      const details = errorToString(erroneusResponse?.error);
+      toastsShow({
+        labelKey: "error.proposal_not_found",
+        level: "error",
+        detail: `id: "${proposalId}"${
+          details === undefined ? "" : `. ${details}`
+        }`,
+      });
+    }
+
+    handleError?.(erroneusResponse.certified);
+  };
+
+  try {
+    return await getProposal({
+      rootCanisterId,
+      proposalId,
+      onLoad: ({ response: proposal, certified }) => {
+        if (!proposal) {
+          catchError({ certified, error: undefined });
+          return;
+        }
+
+        setProposal(proposal);
+
+        callback?.(certified);
+      },
+      onError: catchError,
+      strategy,
+    });
+  } catch (error: unknown) {
+    catchError({ certified: true, error });
+  }
+};
+
+const getProposal = async ({
+  rootCanisterId,
+  proposalId,
+  onLoad,
+  onError,
+  strategy = "query_and_update",
+}: {
+  rootCanisterId: Principal;
+  proposalId: bigint;
+  onLoad: QueryAndUpdateOnResponse<SnsProposalData | undefined>;
+  onError: QueryAndUpdateOnError<Error | unknown | undefined>;
+  strategy?: QueryAndUpdateStrategy;
+}): Promise<void> => {
+  return queryAndUpdate<SnsProposalData, unknown>({
+    identityType: "current",
+    request: ({ certified, identity }) =>
+      queryProposal({
+        identity,
+        certified,
+        rootCanisterId,
+        proposalId,
+      }),
+    onLoad,
+    onError,
+    strategy,
   });
 };
