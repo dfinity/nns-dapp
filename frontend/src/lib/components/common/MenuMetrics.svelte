4a453f9562b2544aeae08b31a9745b60ced77688
David Dal Busco <david.dalbusco@dfinity.org>
2023-01-19 14:01:03 +0100

    fix: tvl appearance in menu as block (#1729)
    # Motivation
    
    Particularly noticable on android, the TVL block makes its appearance in the menu without a fixed width which feels like a glitch.
    
    # Changes
    
    - use layout store from gix-component
    - mitigate glitch with some animation

--- frontend/src/lib/components/common/MenuMetrics.svelte
+++ frontend/src/lib/components/common/MenuMetrics.svelte
@@ -1,21 +1,46 @@
 <script lang="ts">
   import TotalValueLocked from "$lib/components/metrics/TotalValueLocked.svelte";
+  import { layoutMenuOpen } from "@dfinity/gix-components";
+
+  export let sticky = true;
 </script>
 
-<div>
+<div class:open={$layoutMenuOpen} class:sticky>
   <TotalValueLocked layout="stacked" />
 </div>
 
 <style lang="scss">
+  @use "@dfinity/gix-components/styles/mixins/media";
+
   div {
     display: flex;
     justify-content: center;
 
     padding: var(--padding-8x) var(--padding-2x) var(--padding-2x);
 
-    :global(div) {
-      max-width: calc(var(--menu-width) - var(--padding-8x));
-      word-break: break-all;
+    transition: transform linear var(--animation-time-normal),
+      opacity linear calc(var(--animation-time-short) / 2);
+    transform: translate(-150%, 0);
+    opacity: 0;
+
+    @mixin open {
+      transform: translate(0, 0);
+      opacity: 1;
+
+      :global(div) {
+        word-break: break-all;
+      }
+    }
+
+    &.open {
+      @include open;
+    }
+
+    &.sticky {
+      // On xlarge screen the menu can be always open
+      @include media.min-width(xlarge) {
+        @include open;
+      }
     }
   }
 </style>
