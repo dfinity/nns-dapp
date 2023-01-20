06391003ebeeaa0185ae3d133ae78188c7dac27a
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-01-20 14:21:15 +0100

    feat: fetch proposal

--- frontend/src/lib/pages/SnsProposalDetail.svelte
+++ frontend/src/lib/pages/SnsProposalDetail.svelte
@@ -4,29 +4,25 @@
   import { OWN_CANISTER_ID } from "$lib/constants/canister-ids.constants";
   import { ENABLE_SNS_VOTING } from "$lib/constants/environment.constants";
   import { buildProposalsUrl } from "$lib/utils/navigation.utils";
-  import {snsOnlyProjectStore} from "$lib/derived/selected-project.derived";
+  import { snsOnlyProjectStore } from "$lib/derived/selected-project.derived";
   import { AppPath } from "$lib/constants/routes.constants";
-  import {
-    debugSelectedSnsProposalStore,
-  } from "$lib/derived/debug.derived";
+  import { debugSelectedSnsProposalStore } from "$lib/derived/debug.derived";
   import { authStore } from "$lib/stores/auth.store";
   import type {
     SelectedSnsProposalContext,
     SelectedSnsProposalStore,
   } from "$lib/types/sns-selected-proposal.context";
   import { SELECTED_SNS_PROPOSAL_CONTEXT_KEY } from "$lib/types/sns-selected-proposal.context";
-  import {
-    loadProposal,
-  } from "$lib/services/$public/sns-proposals.services";
+  import { loadProposal } from "$lib/services/$public/sns-proposals.services";
   import type { SnsProposalData } from "@dfinity/sns";
   import { fromDefinedNullable } from "@dfinity/utils";
   import { stringifyJson } from "$lib/utils/utils.js";
-  import {writable} from "svelte/store";
-  import {layoutTitleStore} from "$lib/stores/layout.store";
-  import {i18n} from "$lib/stores/i18n";
+  import { writable } from "svelte/store";
+  import { layoutTitleStore } from "$lib/stores/layout.store";
+  import { i18n } from "$lib/stores/i18n";
+  import type { Principal } from "@dfinity/principal";
 
   export let proposalIdText: string | undefined | null = undefined;
-
 
   onMount(() => {
     // We don't render this page if not enabled, but to be safe we redirect to the NNS proposals page as well.
@@ -64,58 +60,50 @@
   // BEGIN: loading and navigation
 
   const goBack = (): Promise<void> =>
-    // TODO: do we need to replace state?
-    goto(
-      AppPath.Proposals
-    );
+    goto(AppPath.Proposals, { replaceState: true });
 
-  const findProposal = async () => {
+  const findProposal = async ({
+    proposalId,
+    rootCanisterId,
+  }: {
+    proposalId: bigint;
+    rootCanisterId: Principal;
+  }) => {
     const onError = (certified: boolean) => {
-      // Ignore "application payload size (X) cannot be larger than Y" error thrown by update calls
       if (certified) {
         return;
       }
       goBack();
     };
 
-    // Technically should not happen has the proposalId is checked in the auto subscriber
-    if (proposalId === undefined) {
-      return;
-    }
-
-    // TODO: check if needed
-    if ($snsOnlyProjectStore === undefined) {
-      return;
-    }
+    console.log("lol", proposalId, rootCanisterId);
 
     await loadProposal({
-      rootCanisterId: $snsOnlyProjectStore,
+      rootCanisterId,
       proposalId,
       setProposal: (proposalData: SnsProposalData) => {
         // User might navigate quickly between proposals - previous / next.
         // e.g. the update call of previous proposal id 3n might be fetched after user has navigated to next proposal id 2n
-        if (fromDefinedNullable(proposalData.id).id !== proposalId) {
+        const proposalDataId = fromDefinedNullable(proposalData.id).id;
+        if (proposalDataId !== proposalId) {
           return;
         }
 
-        // TODO: add to A store
-        tmpProposal = proposalData;
-        // selectedProposalStore.update(({ proposalId }) => ({
-        //   proposalId,
-        //   proposal: proposalData,
-        // }));
+        selectedProposalStore.update(({ proposalId }) => ({
+          proposalId,
+          proposal: proposalData,
+        }));
       },
       handleError: onError,
       silentUpdateErrorMessages: true,
     });
   };
 
-  let tmpProposal: SnsProposalData | undefined;
-
   $: $authStore.identity,
     $snsOnlyProjectStore,
     proposalId,
     (async () => {
+      // TODO: check what to do when there is no proposalId in URL
       if (proposalId === undefined) {
         await goBack();
         return;
@@ -127,22 +115,30 @@
         proposal: undefined,
       });
 
-      await findProposal();
+      if ($snsOnlyProjectStore === undefined) {
+        return;
+      }
+
+      await findProposal({
+        proposalId,
+        rootCanisterId: $snsOnlyProjectStore,
+      });
     })();
 
   // END: loading and navigation
 
   $: layoutTitleStore.set(
-          `${$i18n.proposal_detail.title}${
-                  $selectedProposalStore.proposalId !== undefined
-                          ? ` ${$selectedProposalStore.proposalId}`
-                          : ""
-          }`
+    `${$i18n.proposal_detail.title}${
+      $selectedProposalStore.proposalId !== undefined
+        ? ` ${$selectedProposalStore.proposalId}`
+        : ""
+    }`
   );
-
 </script>
 
 <div class="content-grid" data-tid="sns-proposal-details-grid">
-  <h1>SnsProposalDetail: {proposalIdText}</h1>
-  <pre>{stringifyJson(tmpProposal, { indentation: 2 })}</pre>
+  <h1>{$selectedProposalStore?.proposalId}</h1>
+  <pre>{stringifyJson($selectedProposalStore?.proposal, {
+      indentation: 2,
+    })}</pre>
 </div>
