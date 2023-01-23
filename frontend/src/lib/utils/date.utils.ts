77daa989f5bf40168aef2685ac1d4b9719dadaef
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-01-23 15:26:15 +0100

    Chore: Add created at parameter in transactions (#1737)
    # Motivation
    
    Add createdAt param to all transactions to avoid that a retry from
    agent-js or the browser triggers a duplicated transaction.
    
    # Changes
    
    * Use createdAt param from nns-js and sns-js in all transactions.
    
    # Tests
    
    * Add test for new param.

--- frontend/src/lib/utils/date.utils.ts
+++ frontend/src/lib/utils/date.utils.ts
@@ -109,3 +109,5 @@ export const secondsToTime = (seconds: number): string => {
 };
 
 export const nowInSeconds = (): number => Math.round(Date.now() / 1000);
+export const nowInBigIntNanoSeconds = (): bigint =>
+  BigInt(Date.now()) * BigInt(1e6);
