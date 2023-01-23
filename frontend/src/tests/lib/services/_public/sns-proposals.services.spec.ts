a8c9b3d589b393ee3f3a939ec96b69ba2a5f670c
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-01-23 10:01:55 +0100

    test: loadSnsProposal

--- frontend/src/tests/lib/services/_public/sns-proposals.services.spec.ts
+++ frontend/src/tests/lib/services/_public/sns-proposals.services.spec.ts
@@ -4,11 +4,17 @@
 
 import * as api from "$lib/api/sns-governance.api";
 import { DEFAULT_SNS_PROPOSALS_PAGE_SIZE } from "$lib/constants/sns-proposals.constants";
-import { loadSnsProposals } from "$lib/services/$public/sns-proposals.services";
+import {
+  loadSnsProposal,
+  loadSnsProposals,
+} from "$lib/services/$public/sns-proposals.services";
 import { authStore } from "$lib/stores/auth.store";
 import { snsProposalsStore } from "$lib/stores/sns-proposals.store";
+import * as toastsFunctions from "$lib/stores/toasts.store";
 import { AnonymousIdentity } from "@dfinity/agent";
 import type { SnsProposalData } from "@dfinity/sns";
+import { SnsGovernanceError } from "@dfinity/sns";
+import { fromDefinedNullable } from "@dfinity/utils";
 import { waitFor } from "@testing-library/svelte";
 import { get } from "svelte/store";
 import {
@@ -38,24 +44,36 @@ describe("sns-proposals services", () => {
       .spyOn(api, "queryProposals")
       .mockResolvedValue(proposals);
 
+    jest
+      .spyOn(authStore, "subscribe")
+      .mockImplementation(mutableMockAuthStoreSubscribe);
+
     describe("not logged in", () => {
       afterEach(() => {
         snsProposalsStore.reset();
         jest.clearAllMocks();
       });
+
+      beforeAll(() => {
+        authStoreMock.next({
+          identity: undefined,
+        });
+      });
+
       it("should call queryProposals with the default params", async () => {
         await loadSnsProposals({
           rootCanisterId: mockPrincipal,
         });
-        expect(queryProposalsSpy).toHaveBeenCalledWith({
-          params: {
-            limit: DEFAULT_SNS_PROPOSALS_PAGE_SIZE,
-            beforeProposal: undefined,
-          },
-          identity: new AnonymousIdentity(),
-          certified: false,
-          rootCanisterId: mockPrincipal,
-        });
+        expect(queryProposalsSpy).toHaveBeenCalledWith(
+          expect.objectContaining({
+            params: {
+              limit: DEFAULT_SNS_PROPOSALS_PAGE_SIZE,
+              beforeProposal: undefined,
+            },
+            identity: new AnonymousIdentity(),
+            rootCanisterId: mockPrincipal,
+          })
+        );
       });
 
       it("should call queryProposals with the last proposal id params", async () => {
@@ -101,10 +119,6 @@ describe("sns-proposals services", () => {
     });
 
     describe("logged in", () => {
-      jest
-        .spyOn(authStore, "subscribe")
-        .mockImplementation(mutableMockAuthStoreSubscribe);
-
       it("should call queryProposals with user's identity", async () => {
         authStoreMock.next({
           identity: mockIdentity,
@@ -121,6 +135,114 @@ describe("sns-proposals services", () => {
           certified: false,
           rootCanisterId: mockPrincipal,
         });
+      });
+    });
+  });
+
+  describe("loadSnsProposal", () => {
+    const proposalId = fromDefinedNullable(mockSnsProposal.id).id;
+
+    describe("not logged in", () => {
+      beforeEach(() => {
+        authStoreMock.next({
+          identity: undefined,
+        });
+      });
+
+      afterEach(() => {
+        jest.clearAllMocks();
+      });
+
+      it("should call queryProposal", async () => {
+        const queryProposalSpy = jest
+          .spyOn(api, "queryProposal")
+          .mockResolvedValue(mockSnsProposal);
+
+        await loadSnsProposal({
+          rootCanisterId: mockPrincipal,
+          proposalId,
+          setProposal: jest.fn(),
+        });
+
+        await waitFor(() =>
+          expect(queryProposalSpy).toHaveBeenCalledWith(
+            expect.objectContaining({
+              proposalId,
+              rootCanisterId: mockPrincipal,
+              identity: new AnonymousIdentity(),
+            })
+          )
+        );
+      });
+
+      it("should call setProposal", async () => {
+        const setProposalSpy = jest.fn();
+        await loadSnsProposal({
+          rootCanisterId: mockPrincipal,
+          proposalId,
+          setProposal: setProposalSpy,
+        });
+        await waitFor(() =>
+          expect(setProposalSpy).toHaveBeenCalledWith(mockSnsProposal)
+        );
+      });
+    });
+
+    describe("logged in", () => {
+      authStoreMock.next({
+        identity: mockIdentity,
+      });
+
+      it("should call queryProposals with user's identity", async () => {
+        const queryProposalSpy = jest
+          .spyOn(api, "queryProposal")
+          .mockResolvedValue(mockSnsProposal);
+
+        authStoreMock.next({
+          identity: mockIdentity,
+        });
+        await loadSnsProposal({
+          rootCanisterId: mockPrincipal,
+          proposalId,
+          setProposal: jest.fn(),
+        });
+        expect(queryProposalSpy).toHaveBeenCalledWith(
+          expect.objectContaining({
+            proposalId,
+            rootCanisterId: mockPrincipal,
+            identity: mockIdentity,
+            certified: false,
+          })
+        );
+      });
+    });
+
+    describe("errors", () => {
+      beforeEach(() => {
+        jest.spyOn(api, "queryProposal").mockImplementation(() => {
+          throw new SnsGovernanceError("test-message");
+        });
+        jest.spyOn(console, "error").mockImplementation(jest.fn);
+      });
+      afterEach(() => jest.clearAllMocks());
+
+      it("should show error message in toast details", async () => {
+        const toastsShow = jest.spyOn(toastsFunctions, "toastsShow");
+        authStoreMock.next({
+          identity: mockIdentity,
+        });
+        await loadSnsProposal({
+          rootCanisterId: mockPrincipal,
+          proposalId,
+          setProposal: jest.fn(),
+        });
+        expect(toastsShow).toBeCalled();
+        expect(toastsShow).toBeCalledWith(
+          expect.objectContaining({
+            labelKey: "error.proposal_not_found",
+            detail: expect.stringContaining("test-message"),
+          })
+        );
       });
     });
   });
