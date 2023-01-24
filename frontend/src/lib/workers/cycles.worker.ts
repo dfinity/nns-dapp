a88c6a455789c61705bbfdd7056bf58ee84157ee
David Dal Busco <david.dalbusco@dfinity.org>
2023-01-24 12:37:28 +0100

    feat: poll and display canister status (cycles, memory and status) (#1699)
    # Motivation
    
    Provide more information for the developers about their canisters by polling (every minute) and display the status (cycles, memory and status) of their canisters on the "Canisters" view / grid.
    
    # Changes
    
    - create a `canisters.api.cjs` to query the canister status
    - in this api, explicitely uses agent-js `CJS` version to be able to run it in a web worker
    - implement a web worker that checks every minutes for the canister status
    - intercept the post message on the window side and disply remaining cycles, memory and status
    
    # Notes
    
    - because only controllers can inquiry status and because we do not want to polute the ui, the errors throw if sync does not work are explicitely not displayed

---
+++ frontend/src/lib/workers/cycles.worker.ts
@@ -0,0 +1,137 @@
+import { queryCanisterDetails } from "$lib/api/canisters.api.cjs";
+import type { CanisterDetails } from "$lib/canisters/ic-management/ic-management.canister.types";
+import { SYNC_CYCLES_TIMER_INTERVAL } from "$lib/constants/canisters.constants";
+import type { CanisterSync } from "$lib/types/canister";
+import type {
+  PostMessage,
+  PostMessageDataRequest,
+} from "$lib/types/post-messages";
+import { createAuthClient } from "$lib/utils/auth.utils";
+import type { Identity } from "@dfinity/agent";
+
+onmessage = async ({
+  data: dataMsg,
+}: MessageEvent<PostMessage<PostMessageDataRequest>>) => {
+  const { msg, data } = dataMsg;
+
+  switch (msg) {
+    case "nnsStopCyclesTimer":
+      await stopCyclesTimer();
+      return;
+    case "nnsStartCyclesTimer":
+      await startCyclesTimer({ data });
+      return;
+  }
+};
+
+let timer: NodeJS.Timeout | undefined = undefined;
+
+const loadIdentity = async (): Promise<Identity | undefined> => {
+  const authClient = await createAuthClient();
+  const authenticated = await authClient.isAuthenticated();
+
+  // Not authenticated therefore no identity to fetch the cycles
+  if (!authenticated) {
+    return undefined;
+  }
+
+  return authClient.getIdentity();
+};
+
+const startCyclesTimer = async ({
+  data: { canisterId },
+}: {
+  data: PostMessageDataRequest;
+}) => {
+  // This worker has already been started
+  if (timer !== undefined) {
+    return;
+  }
+
+  const identity: Identity | undefined = await loadIdentity();
+
+  if (!identity) {
+    // We do nothing if no identity
+    return;
+  }
+
+  const sync = async () => await syncCanister({ identity, canisterId });
+
+  // We sync the cycles now but also schedule the update afterwards
+  await sync();
+
+  timer = setInterval(sync, SYNC_CYCLES_TIMER_INTERVAL);
+};
+
+const stopCyclesTimer = async () => {
+  if (!timer) {
+    return;
+  }
+
+  clearInterval(timer);
+  timer = undefined;
+};
+
+let syncInProgress = false;
+
+const syncCanister = async ({
+  identity,
+  canisterId,
+}: {
+  identity: Identity;
+  canisterId: string;
+}) => {
+  // Avoid to duplicate the sync if already in progress and not yet finished
+  if (syncInProgress) {
+    return;
+  }
+
+  syncInProgress = true;
+
+  try {
+    const canisterInfo: CanisterDetails = await queryCanisterDetails({
+      canisterId,
+      identity,
+    });
+
+    syncCanisterData({
+      canisterInfo,
+      canisterId,
+    });
+  } catch (err: unknown) {
+    console.error(err);
+
+    emitCanister({
+      id: canisterId,
+      sync: "error",
+    });
+  }
+
+  syncInProgress = false;
+};
+
+// Update ui with one canister information
+const emitCanister = (canister: CanisterSync) =>
+  postMessage({
+    msg: "nnsSyncCanister",
+    data: {
+      canister,
+    },
+  });
+
+const syncCanisterData = ({
+  canisterId,
+  canisterInfo,
+}: {
+  canisterId: string;
+  canisterInfo: CanisterDetails;
+}) => {
+  const canister: CanisterSync = {
+    id: canisterId,
+    sync: "synced",
+    cyclesStatus: canisterInfo.cycles > 0 ? "ok" : "empty",
+    data: canisterInfo,
+  };
+
+  emitCanister(canister);
+};
