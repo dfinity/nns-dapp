250a6497d3fa870ce36df424bf7da5b70e3b1db5
Max Murphy-Skvorzov <max@winning.black>
2023-01-27 15:15:41 +0100

    Factor out rust build steps

--- rs/sns_cache/build.sh
+++ rs/sns_cache/build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-cargo build --target wasm32-unknown-unknown --release -p sns_cache --color always 2>&1 #|& head -n 100
+cd "$(dirname "$0")/../.."
+canister_name=sns_cache
+cargo build --target wasm32-unknown-unknown --release -p "$canister_name"
+ic-cdk-optimizer "./target/wasm32-unknown-unknown/release/${canister_name}.wasm" -o "./${canister_name}.wasm"
+gzip -f -n "${canister_name}.wasm"
+mv "${canister_name}.wasm.gz" "${canister_name}.wasm"
+ls -sh "./${canister_name}.wasm"
+sha256sum "./${canister_name}.wasm"
--- rs/sns_cache/src/types/ic_sns_root.sh
+++ rs/sns_cache/src/types/ic_sns_root.sh
@@ -2,11 +2,11 @@
 
 cp ~/dfn/ic/rs/sns/root/canister/root.did ic_sns_root.did
 {
-	cat <<-EOF
+  cat <<-EOF
 	#![allow(non_camel_case_types)]
 
 	use crate::types::{CandidType, Deserialize, Serialize, EmptyRecord};
 	use ic_cdk::api::call::CallResult;
 	EOF
-	didc bind ic_sns_root.did --target rs | sed -E 's/^(struct|enum) /pub &/g;s/^use .*/\/\/ &/g;s/\<Deserialize\>/&, Serialize, Clone, Debug/g;s/^  [a-z].*:/  pub&/g;s/^( *pub ) *pub /\1/g'
-} > ic_sns_root.rs
+  didc bind ic_sns_root.did --target rs | sed -E 's/^(struct|enum) /pub &/g;s/^use .*/\/\/ &/g;s/\<Deserialize\>/&, Serialize, Clone, Debug/g;s/^  [a-z].*:/  pub&/g;s/^( *pub ) *pub /\1/g'
+} >ic_sns_root.rs
