d25f20a2665e2f4bcd0af8319392ebf491e66ac0
Max Murphy-Skvorzov <max@winning.black>
2023-01-27 17:37:01 +0100

    ++

--- rs/sns_cache/src/types.rs
+++ rs/sns_cache/src/types.rs
@@ -3,3 +3,26 @@ pub mod ic_sns_ledger;
 pub mod ic_sns_root;
 pub mod ic_sns_swap;
 pub mod ic_sns_wasm;
+
+use ic_cdk::{
+    export::{
+        candid::{CandidType, Deserialize},
+    },
+};
+pub use ic_sns_ledger::{Tokens as SnsTokens, Value as Icrc1Value};
+pub use ic_sns_governance::{
+    GetMetadataResponse, ListNervousSystemFunctionsResponse,
+};
+pub use ic_sns_root::ListSnsCanistersResponse;
+pub use ic_sns_swap::GetStateResponse;
+pub use ic_sns_wasm::{
+    DeployedSns, ListDeployedSnsesResponse, SnsCanisterIds,
+};
+use serde::Serialize;
+
+/// A named empty record.
+///
+/// Many candid interfaces take an empty record as their argument.
+/// Anonymous empty records are not handles correctly by didc, so we name them 'EmptyRecord'.
+#[derive(CandidType, Deserialize, Serialize, Clone, Debug)]
+pub struct EmptyRecord{}
