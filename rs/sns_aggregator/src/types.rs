d223d619e141eb9e483815ea17cb9d1687bba7a4
Max <bitdivine@users.noreply.github.com>
2023-01-30 15:45:03 +0100

    Nns types (#1772)
    # Motivation
    We want to call the sns canisters, but to depend only on the provided
    wasm types, not import the types from the IC repo.
    
    - The IC repo is large, and increases compilation time substantially.
    - The traits applied to the Rust types in the ic repo are not the ones
    we care about. We need parsing and serialization to JSON. Upstream is
    more concerned with protobuf, that we do not need.
    
    # Changes
    - Import sns canister did files
    - Use didc to generate rust types.  But:
      - did makes all fields private, so in accessible.
      - has problems with anonymous empty records
      - has no mechanism for communicating additional traits
    - Use sed and patch files to give the Rust types the traits we need and
    to fix bugs in the conversion.
    
    # Tests
    - These types are used in the current prototypical sns cache.
    
    ---------
    
    Co-authored-by: Max Murphy-Skvorzov <max@winning.black>
    Co-authored-by: mstrasinskis <98811342+mstrasinskis@users.noreply.github.com>

---
+++ rs/sns_aggregator/src/types.rs
@@ -0,0 +1,20 @@
+pub mod ic_sns_governance;
+pub mod ic_sns_ledger;
+pub mod ic_sns_root;
+pub mod ic_sns_swap;
+pub mod ic_sns_wasm;
+
+use ic_cdk::export::candid::{CandidType, Deserialize};
+pub use ic_sns_governance::{GetMetadataResponse, ListNervousSystemFunctionsResponse};
+pub use ic_sns_ledger::{Tokens as SnsTokens, Value as Icrc1Value};
+pub use ic_sns_root::ListSnsCanistersResponse;
+pub use ic_sns_swap::GetStateResponse;
+pub use ic_sns_wasm::{DeployedSns, ListDeployedSnsesResponse, SnsCanisterIds};
+use serde::Serialize;
+
+/// A named empty record.
+///
+/// Many candid interfaces take an empty record as their argument.
+/// Anonymous empty records are not handled correctly by didc, so we name them 'EmptyRecord'.
+#[derive(CandidType, Deserialize, Serialize, Clone, Debug)]
+pub struct EmptyRecord {}
