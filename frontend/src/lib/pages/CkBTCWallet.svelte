dcb16c933807d9386b2f9d138c310a70daf117f0
David Dal Busco <david.dalbusco@dfinity.org>
2023-01-31 13:06:36 +0100

    feat: load ckBTC accounts if not loaded

--- frontend/src/lib/pages/CkBTCWallet.svelte
+++ frontend/src/lib/pages/CkBTCWallet.svelte
@@ -9,6 +9,13 @@
   import { setContext } from "svelte/internal";
   import { findAccount } from "$lib/utils/accounts.utils";
   import { ckBTCAccountsStore } from "$lib/stores/ckbtc-accounts.store";
+  import { isNullish, nonNullish } from "$lib/utils/utils";
+  import { loadCkBTCAccounts } from "$lib/services/ckbtc-accounts.services";
+  import { toastsError } from "$lib/stores/toasts.store";
+  import { replacePlaceholders } from "$lib/utils/i18n.utils";
+  import { i18n } from "$lib/stores/i18n";
+  import { goto } from "$app/navigation";
+  import { AppPath } from "$lib/constants/routes.constants";
 
   export let accountIdentifier: string | undefined | null = undefined;
 
@@ -23,13 +30,43 @@
     store: selectedAccountStore,
   });
 
-  $: selectedAccountStore.set({
-    account: findAccount({
-      identifier: accountIdentifier,
-      accounts: $ckBTCAccountsStore.accounts,
-    }),
-    neurons: [],
-  });
+  const goBack = (): Promise<void> => goto(AppPath.Accounts);
+
+  let requestLoadAccounts = false;
+
+  const loadAccount = async () => {
+    selectedAccountStore.set({
+      account: findAccount({
+        identifier: accountIdentifier,
+        accounts: $ckBTCAccountsStore.accounts,
+      }),
+      neurons: [],
+    });
+
+    // We found an account in store for the provided account identifier, all data are set
+    if (nonNullish($selectedAccountStore.account)) {
+      return;
+    }
+
+    // Accounts are loaded in store but no account identifier is matching
+    // or
+    // We loaded once the accounts and, it loaded no accounts. In any case, we do not want to load again
+    if ($ckBTCAccountsStore.accounts.length > 0 || requestLoadAccounts) {
+      toastsError({
+        labelKey: replacePlaceholders($i18n.error.account_not_found, {
+          $account_identifier: accountIdentifier ?? "",
+        }),
+      });
+
+      await goBack();
+      return;
+    }
+
+    // Maybe the accounts are just not loaded yet, so we try to load
+    await loadCkBTCAccounts({});
+  }
+
+  $: accountIdentifier, $ckBTCAccountsStore.accounts, (async () => loadAccount())();
 </script>
 
 <Island>
