1edebf069351c8fc7d3957a56fe8c89597c233ea
David Dal Busco <david.dalbusco@dfinity.org>
2023-02-01 10:22:26 +0100

    feat: load transactions

--- frontend/src/lib/components/accounts/CkBTCTransactionsList.svelte
+++ frontend/src/lib/components/accounts/CkBTCTransactionsList.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
   import type { Account } from "$lib/types/account";
   import { onMount } from "svelte";
+  import { loadCkBTCAccountNextTransactions } from "$lib/services/ckbtc-transactions.services";
+  import { IcrcTransactionData } from "$lib/types/transaction";
+  import { sortTransactions } from "$lib/utils/icrc-transactions.utils";
+  import { icrcTransactionsStore } from "$lib/stores/icrc-transactions.store";
+  import { CKBTC_INDEX_CANISTER_ID } from "$lib/constants/canister-ids.constants";
 
   export let account: Account;
 
@@ -8,9 +13,17 @@
 
   const loadTransactions = async () => {
     loading = true;
-    // load
+    await loadCkBTCAccountNextTransactions({
+      account
+    });
     loading = false;
   };
 
   onMount(async () => await loadTransactions());
+
+  let transactions: IcrcTransactionData[];
+  $: transactions = sortTransactions(
+    $icrcTransactionsStore[CKBTC_INDEX_CANISTER_ID.toText()]?.[account.identifier]
+      ?.transactions
+  );
 </script>
