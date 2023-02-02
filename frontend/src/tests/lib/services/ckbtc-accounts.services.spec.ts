b65c7a69e3378718caf7628a6b139b51d0132324
David Dal Busco <david.dalbusco@dfinity.org>
2023-02-02 09:42:40 +0100

    test: ckBTC sync

--- frontend/src/tests/lib/services/ckbtc-accounts.services.spec.ts
+++ frontend/src/tests/lib/services/ckbtc-accounts.services.spec.ts
@@ -1,15 +1,20 @@
 import * as ledgerApi from "$lib/api/ckbtc-ledger.api";
 import { CKBTC_UNIVERSE_CANISTER_ID } from "$lib/constants/canister-ids.constants";
+import { ckBTCTokenStore } from "$lib/derived/universes-tokens.derived";
+import * as services from "$lib/services/ckbtc-accounts.services";
 import { loadCkBTCAccounts } from "$lib/services/ckbtc-accounts.services";
 import { ckBTCAccountsStore } from "$lib/stores/ckbtc-accounts.store";
 import { icrcTransactionsStore } from "$lib/stores/icrc-transactions.store";
 import { tick } from "svelte";
 import { get } from "svelte/store";
-import { mockCkBTCMainAccount } from "../../mocks/ckbtc-accounts.mock";
+import {
+  mockCkBTCMainAccount,
+  mockCkBTCToken,
+} from "../../mocks/ckbtc-accounts.mock";
 import { mockIcrcTransactionWithId } from "../../mocks/icrc-transactions.mock";
 
 describe("ckbtc-accounts-services", () => {
-  describe("loadSnsAccounts", () => {
+  describe("loadCkBTCAccounts", () => {
     beforeEach(() => {
       jest.clearAllMocks();
       ckBTCAccountsStore.reset();
@@ -75,6 +80,39 @@ describe("ckbtc-accounts-services", () => {
       expect(
         transactionsStore[CKBTC_UNIVERSE_CANISTER_ID.toText()]
       ).toBeUndefined();
+    });
+  });
+
+  describe("syncCkBTCAccounts", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+      ckBTCAccountsStore.reset();
+    });
+
+    it("should call ckBTC accounts and token and load them in store", async () => {
+      const spyAccountsQuery = jest
+        .spyOn(ledgerApi, "getCkBTCAccounts")
+        .mockImplementation(() => Promise.resolve([mockCkBTCMainAccount]));
+
+      const spyTokenQuery = jest
+        .spyOn(ledgerApi, "getCkBTCToken")
+        .mockImplementation(() => Promise.resolve(mockCkBTCToken));
+
+      await services.syncCkBTCAccounts({});
+
+      await tick();
+
+      expect(spyAccountsQuery).toBeCalled();
+      expect(spyTokenQuery).toBeCalled();
+
+      const accountsStore = get(ckBTCAccountsStore);
+      expect(accountsStore.accounts).toHaveLength(1);
+
+      const tokenStore = get(ckBTCTokenStore);
+      expect(tokenStore).toEqual({
+        token: mockCkBTCToken,
+        certified: true,
+      });
     });
   });
 });
