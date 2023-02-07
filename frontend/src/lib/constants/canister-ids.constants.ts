61cc097cc6509308cc468a23d11556d865975e00
David Dal Busco <david.dalbusco@dfinity.org>
2023-02-07 07:02:19 +0100

    docs: comment

--- frontend/src/lib/constants/canister-ids.constants.ts
+++ frontend/src/lib/constants/canister-ids.constants.ts
@@ -20,8 +20,8 @@ export const CKBTC_MINTER_CANISTER_ID = Principal.fromText(
   "q3fc5-haaaa-aaaaa-aaahq-cai"
 );
 
-// We fallback to hardcoded canister IDs because ckBTC is not deployed on every environment at the moment and we do not want to introduce constants that can be undefined
-// Feature flag should be set accordingly. Feature flag is active on mainnet, the fallback are the one to use on mainnet.
+// We fallback to hardcoded canister IDs because ckBTC is not deployed on every environment at the moment and, we do not want to introduce constants that can be undefined.
+// The feature flags should be set accordingly. The feature is active on mainnet, therefore the fallback values are the one to use on mainnet.
 const MAINNET_CKBTC_LEDGER_CANISTER_ID = "mxzaz-hqaaa-aaaar-qaada-cai";
 const MAINNET_CKBTC_INDEX_CANISTER_ID = "n5wcd-faaaa-aaaar-qaaea-cai";
 
