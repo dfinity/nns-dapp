e46c3e727a8a79033f62e9704ce5696215e12f16
David Dal Busco <david.dalbusco@dfinity.org>
2023-02-09 08:35:51 +0100

    test: modal receive

--- frontend/src/tests/lib/modals/accounts/CkBTCReceiveModal.spec.ts
+++ frontend/src/tests/lib/modals/accounts/CkBTCReceiveModal.spec.ts
@@ -3,10 +3,12 @@
  */
 
 import CkBTCReceiveModal from "$lib/modals/accounts/CkBTCReceiveModal.svelte";
+import { fireEvent, waitFor } from "@testing-library/svelte";
 import {
   mockCkBTCAddress,
   mockCkBTCMainAccount,
 } from "../../../mocks/ckbtc-accounts.mock";
+import en from "../../../mocks/i18n.mock";
 import { renderModal } from "../../../mocks/modal.mock";
 
 describe("CkBTCReceiveModal", () => {
@@ -28,8 +30,44 @@ describe("CkBTCReceiveModal", () => {
   });
 
   it("should render account identifier (without being shortened)", async () => {
+    const { getByText, container } = await renderTransactionModal();
+
+    const button = container.querySelector(
+      "div.segment-button:nth-of-type(3) button"
+    ) as HTMLButtonElement;
+    expect(button).not.toBeNull();
+
+    await fireEvent.click(button);
+
+    await waitFor(() =>
+      expect(getByText(mockCkBTCMainAccount.identifier)).toBeInTheDocument()
+    );
+  });
+
+  it("should render a QR code", async () => {
+    const { getByTestId } = await renderTransactionModal();
+
+    expect(getByTestId("qr-code")).toBeInTheDocument();
+  });
+
+  it("should render a bitcoin description", async () => {
     const { getByText } = await renderTransactionModal();
 
-    expect(getByText(mockCkBTCMainAccount.identifier)).toBeInTheDocument();
+    expect(getByText(en.ckbtc.btc_receive_note)).toBeInTheDocument();
+  });
+
+  it("should render account identifier (without being shortened)", async () => {
+    const { getByText, container } = await renderTransactionModal();
+
+    const button = container.querySelector(
+      "div.segment-button:nth-of-type(3) button"
+    ) as HTMLButtonElement;
+    expect(button).not.toBeNull();
+
+    await fireEvent.click(button);
+
+    await waitFor(() =>
+      expect(getByText(en.ckbtc.ckBTC_receive_note)).toBeInTheDocument()
+    );
   });
 });
