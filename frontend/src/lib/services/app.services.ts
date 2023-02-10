c0b8c39db9851eee101b2f41d9166b20fdcae9e3
Llorenç <llorenc.muntaner@dfinity.org>
2023-02-10 11:37:43 +0100

    Chore: remove loadMainTransactionFee

--- frontend/src/lib/services/app.services.ts
+++ frontend/src/lib/services/app.services.ts
@@ -1,15 +1,10 @@
-import { loadMainTransactionFee } from "$lib/services/transaction-fees.services";
 import { syncAccounts } from "./accounts.services";
 import { listNeurons } from "./neurons.services";
 
 export const initAppPrivateData = (): Promise<
   [PromiseSettledResult<void[]>]
 > => {
-  const initNns: Promise<void>[] = [
-    syncAccounts(),
-    listNeurons(),
-    loadMainTransactionFee(),
-  ];
+  const initNns: Promise<void>[] = [syncAccounts(), listNeurons()];
 
   /**
    * If Nns load but Sns load fails it is "fine" to go on because Nns are core features.
