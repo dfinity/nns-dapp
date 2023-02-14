ba1ac94ca4f42500ba895b3c7dbe1b0d884c4557
David Dal Busco <david.dalbusco@dfinity.org>
2023-02-14 14:04:07 +0100

    refactor: ckBTC transation modal move and call

--- frontend/src/tests/lib/components/accounts/CkBTCWalletFooter.spec.ts
+++ frontend/src/tests/lib/components/accounts/CkBTCWalletFooter.spec.ts
@@ -6,10 +6,15 @@ import CkBTCWalletFooter from "$lib/components/accounts/CkBTCWalletFooter.svelte
 import type { Account } from "$lib/types/account";
 import { fireEvent } from "@testing-library/dom";
 import { render, waitFor } from "@testing-library/svelte";
+import { tokensStore } from "../../../../lib/stores/tokens.store";
 import {
   mockCkBTCAddress,
   mockCkBTCMainAccount,
 } from "../../../mocks/ckbtc-accounts.mock";
+import {
+  mockTokensSubscribe,
+  mockUniversesTokens,
+} from "../../../mocks/tokens.mock";
 import CkBTCWalletContextTest from "./CkBTCWalletContextTest.svelte";
 
 jest.mock("$lib/services/ckbtc-minter.services", () => {
@@ -19,6 +24,14 @@ jest.mock("$lib/services/ckbtc-minter.services", () => {
 });
 
 describe("CkBTCWalletFooter", () => {
+  beforeAll(() => {
+    jest
+      .spyOn(tokensStore, "subscribe")
+      .mockImplementation(mockTokensSubscribe(mockUniversesTokens));
+  });
+
+  afterAll(() => jest.clearAllMocks());
+
   const renderWalletActions = (account?: Account) =>
     render(CkBTCWalletContextTest, {
       props: {
@@ -47,6 +60,19 @@ describe("CkBTCWalletFooter", () => {
 
     fireEvent.click(
       getByTestId("receive-ckbtc-transaction") as HTMLButtonElement
+    );
+
+    await waitFor(() =>
+      expect(container.querySelector("div.modal")).not.toBeNull()
+    );
+  });
+
+  it("should open transaction modal", async () => {
+    const { getByTestId, container } =
+      renderWalletActions(mockCkBTCMainAccount);
+
+    fireEvent.click(
+      getByTestId("open-new-ckbtc-transaction") as HTMLButtonElement
     );
 
     await waitFor(() =>
