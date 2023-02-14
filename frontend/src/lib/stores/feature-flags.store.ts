ca6be28589c7d60277d7d6fbcb59d3aab6a6183c
Llorenç <llorenc.muntaner@dfinity.org>
2023-02-14 09:14:36 +0100

    GIX-1322: Use overrideFeatureFlagsStore

--- frontend/src/lib/stores/feature-flags.store.ts
+++ frontend/src/lib/stores/feature-flags.store.ts
@@ -1,28 +1,36 @@
 import { FEATURE_FLAGS } from "$lib/constants/environment.constants";
 import { storeLocalStorageKey } from "$lib/constants/stores.constants";
-import type { Readable } from "svelte/store";
+import { derived, type Readable } from "svelte/store";
 import { writableStored } from "./writable-stored";
 
-export interface FeatureFlagsStore extends Readable<FEATURE_FLAGS> {
+type OverrideFeatureFlagsData = Partial<FEATURE_FLAGS>;
+export interface OverrideFeatureFlagsStore
+  extends Readable<OverrideFeatureFlagsData> {
   reset: () => void;
 }
 
 /**
- * A store that contains the feature flags.
+ * A store that contains the feature flags that have been overridden by the user.
  */
-export const initFeatureFlagsStore = (): FeatureFlagsStore => {
-  const defaultFlags: FEATURE_FLAGS = FEATURE_FLAGS;
-
-  const { subscribe, set } = writableStored<FEATURE_FLAGS>({
+const initOverrideFeatureFlagsStore = (): OverrideFeatureFlagsStore => {
+  const { subscribe, set } = writableStored<OverrideFeatureFlagsData>({
     key: storeLocalStorageKey.FeatureFlags,
-    defaultValue: defaultFlags,
+    defaultValue: {},
   });
 
   return {
     subscribe,
     // TODO: Expose a method to change one feature flag that raises error if feature flag does not exist
-    reset: () => set(defaultFlags),
+    reset: () => set({}),
   };
 };
 
-export const featureFlagsStore = initFeatureFlagsStore();
+const overrideFeatureFlagsStore = initOverrideFeatureFlagsStore();
+
+export const featureFlagsStore = derived(
+  overrideFeatureFlagsStore,
+  ($overrideFeatureFlagsStore) => ({
+    ...FEATURE_FLAGS,
+    ...$overrideFeatureFlagsStore,
+  })
+);
