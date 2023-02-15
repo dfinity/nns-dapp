b8d5d87748cc0726429bc25c12de4874813ae155
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-02-15 16:08:57 +0100

    Chore: Restrict feature flags functionality (#1885)
    # Motivation
    
    Do not allow to set any feature flags. Ideally, we would only allow the
    ones approved by security.
    
    # Changes
    
    * Rename `assertFeatureFlag` to `assertValidFeatureFlag` in
    `features-flags.store.ts`.
    * Add list of EDITABLE_FEATURE_FLAGS.
    * Check that flag is included in EDITABLE_FEATURE_FLAGS in
    `assertValidFeatureFlag`.
    
    # Tests
    
    * Test new functionality.

--- frontend/src/tests/lib/stores/feature-flags.store.spec.ts
+++ frontend/src/tests/lib/stores/feature-flags.store.spec.ts
@@ -8,7 +8,10 @@ import { get } from "svelte/store";
 
 describe("featureFlags store", () => {
   const noKey = "NO_KEY" as FeatureKey;
-  const error = new Error(`Unknown feature flag: ${noKey}`);
+  const noKeyError = `Unknown feature flag: ${noKey}`;
+  const notEditable: FeatureKey = "TEST_FLAG_NOT_EDITABLE";
+  const notEditableError = `Feature flag is not editable: ${notEditable}`;
+  const editableFlag: FeatureKey = "TEST_FLAG_EDITABLE";
   beforeEach(() => {
     overrideFeatureFlagsStore.reset();
   });
@@ -23,14 +26,13 @@ describe("featureFlags store", () => {
   });
 
   it("should change value when overrideFeatureFlagsStore is updated", () => {
-    const featureKey = "ENABLE_SNS_2";
-    const featureFlag = featureFlagsModule[featureKey];
+    const featureFlag = featureFlagsModule[editableFlag];
 
-    overrideFeatureFlagsStore.setFlag(featureKey, true);
+    overrideFeatureFlagsStore.setFlag(editableFlag, true);
     const enabledBefore = get(featureFlag);
     expect(enabledBefore).toEqual(true);
 
-    overrideFeatureFlagsStore.setFlag(featureKey, false);
+    overrideFeatureFlagsStore.setFlag(editableFlag, false);
 
     const enabledAfter = get(featureFlag);
     expect(enabledAfter).toEqual(false);
@@ -38,26 +40,39 @@ describe("featureFlags store", () => {
 
   it("should throw if a non feature flag is set", () => {
     expect(() => overrideFeatureFlagsStore.setFlag(noKey, false)).toThrowError(
-      error
+      noKeyError
     );
   });
 
+  it("should throw if a non editable feature flag is set", () => {
+    expect(() =>
+      overrideFeatureFlagsStore.setFlag(notEditable, false)
+    ).toThrowError(notEditableError);
+  });
+
   it("should remove feature flags", () => {
-    const featureKey = "ENABLE_SNS_2";
-    const featureFlag = featureFlagsModule[featureKey];
+    const featureFlag = featureFlagsModule[editableFlag];
     const initialValue = get(featureFlag);
 
-    overrideFeatureFlagsStore.setFlag(featureKey, !initialValue);
+    overrideFeatureFlagsStore.setFlag(editableFlag, !initialValue);
 
     const enabledMid = get(featureFlag);
     expect(enabledMid).toEqual(!initialValue);
 
-    overrideFeatureFlagsStore.removeFlag(featureKey);
+    overrideFeatureFlagsStore.removeFlag(editableFlag);
     const enabledAfter = get(featureFlag);
     expect(enabledAfter).toEqual(initialValue);
   });
 
   it("should throw if a non feature flag is removed", () => {
-    expect(() => overrideFeatureFlagsStore.removeFlag(noKey)).toThrow(error);
+    expect(() => overrideFeatureFlagsStore.removeFlag(noKey)).toThrow(
+      noKeyError
+    );
+  });
+
+  it("should throw if a non editable feature flag is removed", () => {
+    expect(() => overrideFeatureFlagsStore.removeFlag(notEditable)).toThrow(
+      notEditableError
+    );
   });
 });
