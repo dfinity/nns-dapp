0345491c84879a05b9b4a65cbbb411b13ed3a41a
David Dal Busco <david.dalbusco@dfinity.org>
2023-02-20 16:44:39 +0100

    test: select network dropdown

---
+++ frontend/src/tests/lib/components/accounts/SelectNetworkDropdown.spec.ts
@@ -0,0 +1,68 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import SelectNetworkDropdown from "$lib/components/accounts/SelectNetworkDropdown.svelte";
+import { fireEvent, render, waitFor } from "@testing-library/svelte";
+import { TransactionNetwork } from "../../../../lib/types/transaction";
+import en from "../../../mocks/i18n.mock";
+import SelectNetworkDropdownTest from "./SelectNetworkDropdownTest.svelte";
+
+describe("SelectNetworkDropdown", () => {
+  it("should display a network title", () => {
+    const { getByText } = render(SelectNetworkDropdown);
+
+    expect(getByText(en.accounts.network)).toBeInTheDocument();
+  });
+
+  it("should render a select component", () => {
+    const { getByTestId } = render(SelectNetworkDropdown);
+
+    expect(getByTestId("select-network-dropdown")).not.toBeNull();
+  });
+
+  it("should display a disable placeholder", () => {
+    const { container } = render(SelectNetworkDropdown);
+
+    const option = container.querySelector("option[disabled]");
+
+    expect(option).not.toBeNull();
+    expect(option?.getAttribute("value")).toEqual("undefined");
+  });
+
+  it("should display an option to select ICP", () => {
+    const { container } = render(SelectNetworkDropdown);
+
+    const option = container.querySelector("option[value='network_icp_ckbtc']");
+
+    expect(option).not.toBeNull();
+    expect(option.innerHTML).toEqual(en.accounts.network_icp_ckbtc);
+  });
+
+  it("should display an option to select bitcoin", () => {
+    const { container } = render(SelectNetworkDropdown);
+
+    const option = container.querySelector("option[value='network_bitcoin']");
+
+    expect(option).not.toBeNull();
+    expect(option.innerHTML).toEqual(en.accounts.network_bitcoin);
+  });
+
+  it("should bind select to selected network", async () => {
+    const { getByTestId, container } = render(SelectNetworkDropdownTest);
+
+    const selectElement = getByTestId(
+      "select-network-dropdown"
+    ) as HTMLSelectElement | null;
+    selectElement &&
+      fireEvent.change(selectElement, {
+        target: { value: TransactionNetwork.ICP_CKBTC },
+      });
+
+    const div = container.querySelector(".container");
+
+    await waitFor(() =>
+      expect(div.classList).toContain(TransactionNetwork.ICP_CKBTC)
+    );
+  });
+});
---
+++ frontend/src/tests/lib/components/accounts/SelectNetworkDropdownTest.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import SelectNetworkDropdown from "$lib/components/accounts/SelectNetworkDropdown.svelte";
+
+  let selectedNetwork = undefined;
+</script>
+
+<div class={`container ${selectedNetwork}`}>
+  <SelectNetworkDropdown bind:selectedNetwork />
+</div>
