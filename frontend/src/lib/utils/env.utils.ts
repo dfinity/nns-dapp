f474d2054810d7c73199140080427bca8b53dbb9
David Dal Busco <david.dalbusco@dfinity.org>
2023-02-23 17:43:07 +0100

    chore: pre-rendering guard not needed

--- frontend/src/lib/utils/env.utils.ts
+++ frontend/src/lib/utils/env.utils.ts
@@ -1,11 +1,6 @@
-import { building } from "$app/environment";
 import { NNS_IC_ORG_ALTERNATIVE_ORIGIN } from "$lib/constants/origin.constants";
 
 export const isNnsAlternativeOrigin = (): boolean => {
-  if (building) {
-    return false;
-  }
-
   const {
     location: { origin },
   } = window;
