8d6a183218cd6ea5f9ef415cefdd78fcf30a9270
David Dal Busco <david.dalbusco@dfinity.org>
2023-02-27 17:45:40 +0100

    feat: warning icon

--- frontend/src/lib/components/sale/SaleInProgress.svelte
+++ frontend/src/lib/components/sale/SaleInProgress.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import { ProgressSteps, type ProgressStep } from "@dfinity/gix-components";
   import type { SaleStep } from "$lib/types/sale";
-  import {i18n} from "$lib/stores/i18n";
+  import { ICON_SIZE_LARGE } from "$lib/constants/layout.constants";
+  import { IconWarning } from "@dfinity/gix-components";
+  import { i18n } from "$lib/stores/i18n";
 
   export let progressStep: SaleStep;
 
@@ -51,6 +53,9 @@
 </script>
 
 <div class="warning">
+  <div class="icon">
+    <IconWarning size={ICON_SIZE_LARGE} />
+  </div>
   <p class="value">{$i18n.sns_sale.this_may_take_a_few_minutes}</p>
   <p class="description">{$i18n.sns_sale.do_not_close}</p>
 </div>
@@ -59,11 +64,30 @@
 
 <style lang="scss">
   .warning {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    column-gap: var(--padding-2x);
+    align-items: center;
+
     background: var(--overlay-background);
     color: var(--overlay-background-contrast);
     padding: var(--padding) var(--padding-3x);
     border-radius: var(--border-radius);
 
     margin: 0 0 var(--padding-4x);
+  }
+
+  .icon {
+    color: var(--warning-emphasis);
+    grid-row: 1/3;
+  }
+
+  .value {
+    margin-bottom: 0;
+  }
+
+  .description {
+    grid-column: 2/3;
+    margin-top: var(--padding-0_5x);
   }
 </style>
