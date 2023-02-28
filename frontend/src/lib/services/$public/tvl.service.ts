15b39606dbbc3bf929b8bd450ceb407f93e3920d
David Dal Busco <david.dalbusco@dfinity.org>
2023-02-28 10:14:01 +0100

    docs: improve comment

--- frontend/src/lib/services/$public/tvl.service.ts
+++ frontend/src/lib/services/$public/tvl.service.ts
@@ -5,7 +5,8 @@ import { AnonymousIdentity } from "@dfinity/agent";
 export const queryTVL = (): Promise<TvlResult> =>
   queryTVLApi({
     // Because we use the service in a web worker.
-    // Do not use utils to generate anonymous identity to avoid build issue: Unexpected token (Note that you need plugins to import files that are not JavaScript)
+    // Do not use utils to generate anonymous identity to avoid Vite/Rollup build issue:
+    // "Unexpected token (Note that you need plugins to import files that are not JavaScript)"
     identity: new AnonymousIdentity(),
     certified: false,
   });
