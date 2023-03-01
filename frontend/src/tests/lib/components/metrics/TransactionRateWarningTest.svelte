8a8b7a0cfb0b492dcfbe28177fd124fee2ff30c8
David Dal Busco <david.dalbusco@dfinity.org>
2023-03-01 15:52:15 +0100

    test: transaction warning display

---
+++ frontend/src/tests/lib/components/metrics/TransactionRateWarningTest.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Metrics from "$lib/components/metrics/Metrics.svelte";
+  import TransactionRateWarning from "$lib/components/metrics/TransactionRateWarning.svelte";
+  import { Toasts } from "@dfinity/gix-components";
+</script>
+
+<TransactionRateWarning />
+<Metrics />
+<Toasts position="top" />
