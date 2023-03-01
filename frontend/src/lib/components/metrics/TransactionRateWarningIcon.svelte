e0f96f3fc82b61a40628ce8ff93d478395b64d3d
David Dal Busco <david.dalbusco@dfinity.org>
2023-03-01 15:05:02 +0100

    refactor: rename components

---
+++ frontend/src/lib/components/metrics/TransactionRateWarningIcon.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { IconMeter } from "@dfinity/gix-components";
+</script>
+
+<div>
+  <IconMeter />
+</div>
+
+<style lang="scss">
+  .high {
+    --meter-rotate: 70deg;
+  }
+</style>
