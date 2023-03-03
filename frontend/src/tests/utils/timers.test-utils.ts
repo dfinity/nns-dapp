979341b56537ec860ac9fc5e5985efe2bfcad02e
David de Kloet <david.de.kloet@dfinity.org>
2023-03-03 09:19:38 +0100

    comment

--- frontend/src/tests/utils/timers.test-utils.ts
+++ frontend/src/tests/utils/timers.test-utils.ts
@@ -2,6 +2,11 @@ import { nonNullish } from "@dfinity/utils";
 
 const originalTimeout = setTimeout;
 
+// When awaiting a resolved promise, the code after it doesn't happen
+// immediately. This can be confusing in tests if you have expectations based
+// on that code. Calling `await runResolvedPromises()` after some async code
+// will make sure everything that doesn't depend on future events has already
+// executed.
 export const runResolvedPromises = () =>
   new Promise((resolve) => originalTimeout(resolve, 0));
 
