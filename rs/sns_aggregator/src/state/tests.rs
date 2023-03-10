cdee8ee1f9c702fcde039fe270c71a66d86fe817
Max Murphy-Skvorzov <max@winning.black>
2023-03-10 17:19:17 +0100

    ++

--- rs/sns_aggregator/src/state/tests.rs
+++ rs/sns_aggregator/src/state/tests.rs
@@ -1,6 +1,10 @@
 //! Tests for the aggregator state
 
-use super::StableState; // Is this the EU?
+use std::cell::RefCell;
+
+use crate::{types::upstream::SnsCache, assets::Assets};
+
+use super::{Config, StableState}; // Is this the EU?
 
 struct StateSerdeTestVector {
     name: &'static str,
@@ -9,12 +13,38 @@ struct StateSerdeTestVector {
 
 #[test]
 fn serializing_and_deserializing_returns_original() {
-    let test_vectors = vec![StateSerdeTestVector{name: "default", state: StableState::default()}];
-    for StateSerdeTestVector{name, state} in test_vectors.iter() {
-        let serialized = state.to_bytes().expect("Failed to serialize");
-        let deserialized = StableState::from_bytes(&serialized).expect("Failed to seserialize");
-        for fun in [ |state: &StableState| state.sns_cache.borrow().all_sns.len() ] {
-          assert_eq!(fun(state), fun(&deserialized));
+    let test_vectors = vec![
+        StateSerdeTestVector {
+            name: "default",
+            state: StableState::default(),
+        },
+        StateSerdeTestVector {
+            name: "explicit_default",
+            state: StableState {
+                config: RefCell::new(Config::default()),
+                sns_cache: RefCell::new(SnsCache::default()),
+                assets: RefCell::new(Assets::default()),
+            },
+        },
+        StateSerdeTestVector {
+            name: "explicit_default",
+            state: StableState {
+                config: RefCell::new(Config::default()),
+                sns_cache: RefCell::new(SnsCache::default()),
+                assets: RefCell::new(Assets::default()),
+            },
+        },
+    ];
+    for StateSerdeTestVector { name, state } in test_vectors.iter() {
+        let serialized = state
+            .to_bytes()
+            .map_err(|err| format!("Failed to serialize {name}: {err:?}"))
+            .unwrap();
+        let deserialized = StableState::from_bytes(&serialized)
+            .map_err(|err| format!("Failed to deserialize {name}: {err:?}"))
+            .unwrap();
+        for fun in [|state: &StableState| state.sns_cache.borrow().all_sns.len()] {
+            assert_eq!(fun(state), fun(&deserialized));
         }
     }
 }
