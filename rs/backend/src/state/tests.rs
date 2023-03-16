f4ad88d1ad8d50b218b53089d9a50827fdb7e931
Max Murphy-Skvorzov <max@winning.black>
2023-03-16 03:27:42 +0100

    More tests

--- rs/backend/src/state/tests.rs
+++ rs/backend/src/state/tests.rs
@@ -1,7 +1,9 @@
 use super::{AssetHashes, Assets, PerformanceCounts, StableState, State};
 use dfn_candid::Candid;
-use on_wire::IntoWire;
+use on_wire::{FromWire, IntoWire};
 use std::cell::RefCell;
+use crate::state::AccountsStore;
+
 
 /// Creates a populated test state for testing.
 pub fn setup_test_state() -> State {
@@ -14,14 +16,40 @@ pub fn setup_test_state() -> State {
 }
 
 #[test]
-fn adding_perf_should_succeed() {
+fn state_can_be_restored_from_stable_memory() {
     let toy_state = setup_test_state();
-    let bytes_without_perf: Vec<u8> = Candid((
-        toy_state.accounts_store.borrow().encode(),
-        toy_state.assets.borrow().encode(),
-    ))
-    .into_bytes()
-    .unwrap();
+    let bytes: Vec<u8> = toy_state.encode();
+    let parsed = State::decode(bytes).expect("Failed to parse");
+    assert_eq!(toy_state, parsed, "State has changed");
+}
+
+// Test upgrading and downgrading from the previous schema.
+impl State {
+    fn encode_old_001(&self) -> Vec<u8> {
+        Candid((self.accounts_store.borrow().encode(), self.assets.borrow().encode()))
+            .into_bytes()
+            .unwrap()
+    }
+
+    fn decode_old_001(bytes: Vec<u8>) -> Result<Self, String> {
+        let (account_store_bytes, assets_bytes) = Candid::from_bytes(bytes).map(|c| c.0)?;
+
+        let assets = Assets::decode(assets_bytes)?;
+        let asset_hashes = AssetHashes::from(&assets);
+
+        Ok(State {
+            accounts_store: RefCell::new(AccountsStore::decode(account_store_bytes)?),
+            assets: RefCell::new(assets),
+            asset_hashes: RefCell::new(asset_hashes),
+            // Not included in the old stable memory:
+            performance: RefCell::new(PerformanceCounts::default()),
+        })
+    }
+}
+#[test]
+fn upgrade_from_state_001_should_succeed() {
+    let toy_state = setup_test_state();
+    let bytes_without_perf: Vec<u8> = toy_state.encode_old_001();
     let parsed_with_perf = State::decode(bytes_without_perf).expect("Failed to parse bytes without perf");
     // This is the highly valuable state:
     assert_eq!(
@@ -34,4 +62,23 @@ fn adding_perf_should_succeed() {
         toy_state.asset_hashes, parsed_with_perf.asset_hashes,
         "Asset hashes have changed"
     );
+    // Perf is expected to change.
+}
+#[test]
+fn downgrade_to_state_001_should_succeed() {
+    let toy_state = setup_test_state();
+    let bytes: Vec<u8> = toy_state.encode();
+    let parsed_with_perf = State::decode_old_001(bytes).expect("Failed to parse bytes");
+    // This is the highly valuable state:
+    assert_eq!(
+        toy_state.accounts_store, parsed_with_perf.accounts_store,
+        "Accounts store has changed"
+    );
+    // It's nice if we keep these:
+    assert_eq!(toy_state.assets, parsed_with_perf.assets, "Assets have changed");
+    assert_eq!(
+        toy_state.asset_hashes, parsed_with_perf.asset_hashes,
+        "Asset hashes have changed"
+    );
+    // Perf is expected to change.
 }
