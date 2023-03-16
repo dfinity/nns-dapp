5f9726406fcf37e93fed07294a8c2c81624a244f
Max Murphy-Skvorzov <max@winning.black>
2023-03-16 03:42:33 +0100

    Rename

--- rs/backend/src/perf/tests.rs
+++ rs/backend/src/perf/tests.rs
@@ -1,8 +1,9 @@
 use super::PerformanceCounts;
 use crate::state::StableState;
 
+/// Given valid performance data, it should serialize and be parsed back to the original.
 #[test]
-fn stats_stable_memory_serde_works() {
+fn perf_stable_memory_serde_works() {
     let test_vectors = vec![PerformanceCounts::test_data()];
     for data in test_vectors {
         let serialized: Vec<u8> = data.encode();
@@ -12,10 +13,10 @@ fn stats_stable_memory_serde_works() {
 }
 
 /// Decoding performance counters should never fail, even if the data is empty or junk.
-/// The performance counters are not critical to the operation of tghe canister and failing
+/// The performance counters are not critical to the operation of the canister and failing
 /// to parse them should never ever cause a canister upgrade to fail.
 #[test]
-fn stats_stable_memory_never_fails() {
+fn perf_stable_memory_never_fails() {
     let junk_vectors: Vec<Vec<u8>> = vec!["", "finestkeyboardrandom"]
         .iter()
         .map(|x| x.as_bytes().to_vec())
