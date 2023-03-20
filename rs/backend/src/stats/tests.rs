dbcbf9a90a37992161582d9f9b3b32c898652681
Max Murphy-Skvorzov <max@winning.black>
2023-03-20 23:13:47 +0100

    lint

--- rs/backend/src/stats/tests.rs
+++ rs/backend/src/stats/tests.rs
@@ -9,7 +9,7 @@ fn populated_state_should_have_populated_stats() {
     let stats = get_stats(&state);
     crate::accounts_store::tests::assert_initial_test_store_stats_are_correct(&stats);
     assert!(
-        stats.performance_counts.len() > 0,
+        !stats.performance_counts.is_empty(),
         "Stats should include performance counts"
     );
 }
