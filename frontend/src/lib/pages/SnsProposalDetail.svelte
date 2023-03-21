5d82cc342c7758ca4108aa7bc860cfb7cfabccb0
Llorenç <llorenc.muntaner@dfinity.org>
2023-03-21 15:53:49 +0100

    GIX-1219: Fetch SNS proposal and load it in component

--- frontend/src/lib/pages/SnsProposalDetail.svelte
+++ frontend/src/lib/pages/SnsProposalDetail.svelte
@@ -8,6 +8,7 @@
   import { getSnsProposalById } from "$lib/services/$public/sns-proposals.services";
   import { snsOnlyProjectStore } from "$lib/derived/sns/sns-selected-project.derived";
   import type { SnsProposalData, SnsProposalId } from "@dfinity/sns";
+  import { toastsError } from "$lib/stores/toasts.store";
 
   export let proposalIdText: string | undefined | null = undefined;
 
@@ -32,16 +33,42 @@
         getSnsProposalById({
           rootCanisterId: $snsOnlyProjectStore,
           proposalId,
+  // By setting a local variable, we avoid calling the below text when the whole store is changes.
+  let rootCanisterId = $snsOnlyProjectStore;
+
+  // TODO: Fix race condition in case the user changes the proposal before the first one hasn't loaded yet.
+  $: {
+    if (nonNullish(proposalIdText) && nonNullish(rootCanisterId)) {
+      // We can't be sure that `snsOnlyProjectStore` is the same when `handleError` is called.
+      const rootCanisterIdText = rootCanisterId.toText();
+      try {
+        const proposalId = BigInt(proposalIdText);
+        proposal = "loading";
+        getSnsProposalById({
+          rootCanisterId,
+          proposalId: { id: proposalId },
           setProposal: ({ proposal: proposalData }) => {
             proposal = proposalData;
           },
           handleError: () => {
             // TODO: redirect
+            goto(buildProposalsUrl({ universe: rootCanisterIdText }), {
+              replaceState: true,
+            });
           },
         });
       } catch (error) {
         proposal = "error";
         // TODO: Add a toast for this error and redirect
+        toastsError({
+          labelKey: "error.wrong_proposal_id",
+          substitutions: {
+            $proposalId: proposalIdText,
+          },
+        });
+        goto(buildProposalsUrl({ universe: rootCanisterIdText }), {
+          replaceState: true,
+        });
       }
     }
   }
