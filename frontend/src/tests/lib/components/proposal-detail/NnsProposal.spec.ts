ac78b1c87cc2b126e8b1ad912f3eda5cb9c96ce9
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-03-22 18:35:45 +0100

    Chore: Refactor NNS Proposal cards (#2156)
    # Motivation
    
    Make the cards of the Proposal page agnostic of the parent.
    
    # Changes
    
    * Remove margin-top from ProposalProposerActionsEntry.svelte
    * Remove margin-top from ProposalProposerPayloadEntry.svelte
    * Remove the component NnsProposalProposeDataSection.svelte that was
    just rendering two components.
    * Move those two components to NnsProposal.svelte and manage the spacing
    there with a display flex and gap.
    
    # Tests
    
    * Mostly UI changes, but I added a similar test that was in the removed
    component to the NnsProposal.spec.

--- frontend/src/tests/lib/components/proposal-detail/NnsProposal.spec.ts
+++ frontend/src/tests/lib/components/proposal-detail/NnsProposal.spec.ts
@@ -2,7 +2,12 @@
  * @jest-environment jsdom
  */
 
-import { mockProposalInfo } from "$tests/mocks/proposal.mock";
+import * as proposalsApi from "$lib/api/proposals.api";
+import {
+  mockProposalInfo,
+  proposalActionNnsFunction21,
+} from "$tests/mocks/proposal.mock";
+import { blockAllCallsTo } from "$tests/utils/module.test-utils";
 import { render, waitFor } from "@testing-library/svelte";
 import NnsProposalTest from "./NnsProposalTest.svelte";
 
@@ -10,13 +15,27 @@ jest.mock("$lib/utils/html.utils", () => ({
   markdownToHTML: (value) => Promise.resolve(value),
 }));
 
+jest.mock("$lib/api/nns-dapp.api");
+
 describe("Proposal", () => {
+  blockAllCallsTo(["$lib/api/nns-dapp.api"]);
+
   const renderProposalModern = () =>
     render(NnsProposalTest, {
       props: {
-        proposalInfo: mockProposalInfo,
+        proposalInfo: {
+          ...mockProposalInfo,
+          proposal: {
+            ...mockProposalInfo.proposal,
+            action: proposalActionNnsFunction21,
+          },
+        },
       },
     });
+
+  beforeEach(() => {
+    jest.spyOn(proposalsApi, "queryProposalPayload").mockResolvedValue({});
+  });
 
   it("should render a detail grid", async () => {
     const { queryByTestId } = renderProposalModern();
@@ -39,11 +58,20 @@ describe("Proposal", () => {
     );
   });
 
-  it("should render proposer proposal data", async () => {
+  it("should render proposer proposal actions entry", async () => {
     const { queryByTestId } = renderProposalModern();
     await waitFor(() =>
       expect(
         queryByTestId("proposal-proposer-actions-entry-title")
+      ).toBeInTheDocument()
+    );
+  });
+
+  it("should render proposer proposal payload entry", async () => {
+    const { queryByTestId } = renderProposalModern();
+    await waitFor(() =>
+      expect(
+        queryByTestId("proposal-proposer-payload-entry-title")
       ).toBeInTheDocument()
     );
   });
