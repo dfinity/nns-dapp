80a6bf273cfb021b25b91f3897c6d98187740739
David de Kloet <david.de.kloet@dfinity.org>
2023-03-27 11:02:21 +0200

    allByTestId

--- frontend/src/tests/page-objects/playwright.page-object.ts
+++ frontend/src/tests/page-objects/playwright.page-object.ts
@@ -16,8 +16,24 @@ export class PlaywrightPageObjectElement implements PageObjectElement {
     return new PlaywrightPageObjectElement(this.locator.locator(selector));
   }
 
+  async querySelectorAll(
+    selector: string
+  ): Promise<PlaywrightPageObjectElement[]> {
+    return (await this.locator.locator(selector).all()).map(
+      (locator) => new PlaywrightPageObjectElement(locator)
+    );
+  }
+
+  querySelector(selector: string): PlaywrightPageObjectElement {
+    return new PlaywrightPageObjectElement(this.locator.locator(selector));
+  }
+
   byTestId(tid: string): PlaywrightPageObjectElement {
     return this.querySelector(`[data-tid=${tid}]`);
+  }
+
+  allByTestId(tid: string): Promise<PlaywrightPageObjectElement[]> {
+    return this.querySelectorAll(`[data-tid=${tid}]`);
   }
 
   getText(): Promise<string> {
