b72a29a43b0483a36ff65d50fc21567f70116ccd
Max Murphy-Skvorzov <max@winning.black>
2023-03-29 15:57:46 +0200

    Simple utility to check args

--- rs/backend/src/bin/nns-dapp-check-args.rs
+++ rs/backend/src/bin/nns-dapp-check-args.rs
@@ -1,5 +1,14 @@
+//! Code for testing arguments
+use candid::Decode;
 use nns_dapp::arguments::CanisterArguments;
+use std::env::args;
+use std::fs;
 
+/// Simple utiliyty to check that Rust can parse the binary arguments.
 fn main() {
-    println!("Hello world");
+    let path = args().skip(1).next().expect("No path provided");
+    println!("Checking binary arguments at: {path}");
+    let bytes = fs::read(path).expect("Failed to read path");
+    let arg = Decode!(&bytes, Option<CanisterArguments>).expect("Binary is not valid candid");
+    println!("Parsed as:\n{arg:#?}");
 }
