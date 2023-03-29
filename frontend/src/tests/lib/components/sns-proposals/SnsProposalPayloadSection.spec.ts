6ec0be58bf5dc82c164d3957383b9ec9e4491ea8
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-03-29 14:20:16 +0200

    GIX-1220: SNS Proposal payload card (#2197)
    # Motivation
    
    User can see the payload of an SNS Proposal.
    
    # Changes
    
    * Implement SnsProposalPayloadSection.svelte using
    ProposalSummary.svelte.
    
    # Tests
    
    * New page object SnsProposalPayloadSectionPo
    * Test SnsProposalPayloadSection component with new PO.

---
+++ frontend/src/tests/lib/components/sns-proposals/SnsProposalPayloadSection.spec.ts
@@ -0,0 +1,63 @@
+/**
+ * @jest-environment jsdom
+ */
+import SnsProposalPayloadSection from "$lib/components/sns-proposals/SnsProposalPayloadSection.svelte";
+import en from "$tests/mocks/i18n.mock";
+import { mockSnsProposal } from "$tests/mocks/sns-proposals.mock";
+import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
+import { SnsProposalPayloadSectionPo } from "$tests/page-objects/SnsProposalPayloadSection.page-object";
+import { runResolvedPromises } from "$tests/utils/timers.test-utils";
+import type { SnsProposalData } from "@dfinity/sns";
+import { render } from "@testing-library/svelte";
+
+jest.mock("$lib/utils/html.utils", () => ({
+  markdownToHTML: (value) => Promise.resolve(value),
+}));
+
+describe("SnsProposalPayloadSection", () => {
+  const renderComponent = async (props) => {
+    const { container } = render(SnsProposalPayloadSection, {
+      props,
+    });
+
+    await runResolvedPromises();
+
+    return SnsProposalPayloadSectionPo.under(
+      new JestPageObjectElement(container)
+    );
+  };
+
+  describe("when payload is defined", () => {
+    const payload = "# Some Summary";
+    const proposal: SnsProposalData = {
+      ...mockSnsProposal,
+      payload_text_rendering: [payload],
+    };
+    const props = { proposal };
+
+    it("should render title", async () => {
+      const po = await renderComponent(props);
+
+      expect(await po.getCardTitle()).toBe(en.proposal_detail.payload);
+    });
+
+    it("should contain summary", async () => {
+      const po = await renderComponent(props);
+
+      expect(await (await po.getPayloadText()).trim()).toBe(payload);
+    });
+  });
+
+  describe("when payload is not defined", () => {
+    const proposal: SnsProposalData = {
+      ...mockSnsProposal,
+      payload_text_rendering: [],
+    };
+    const props = { proposal };
+
+    it("should not render content", async () => {
+      const po = await renderComponent(props);
+      expect(await po.isPresent()).toBe(false);
+    });
+  });
+});
