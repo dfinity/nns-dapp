3711dc7e213de93c590f3635a75328fc2465fb87
Max <bitdivine@users.noreply.github.com>
2023-03-29 12:58:47 +0200

    Args v2 (#2132)
    # Motivation
    We wish to stop baking canister IDs and URLs into the nns-dapp at build
    time.
    
    # Changes
    - Add a `<meta>` tag to the head of every index.html with:
      - A data attribute providing the own canister ID
    
    # Tests
    - TODO: How would this be tested using the shiny new e2e framework?
    
    Tested manually:
    
    * Download state: `curl -fL --retry 5
    https://github.com/dfinity/snsdemo/releases/download/tip/state.zip >
    state.zip`
    * Start dfx with the state:
    ```
    $ dfx-state-install --state "$(dfx cache show)/state.zip"
    replace .config/dfx/networks.json? [y]es, [n]o, [A]ll, [N]one, [r]ename: n
    replace .config/dfx/identity/snsdemo8/identity.pem? [y]es, [n]o, [A]ll, [N]one, [r]ename: y
    replace .config/dfx/identity/snsdemo8/neurons/local? [y]es, [n]o, [A]ll, [N]one, [r]ename: y
    replace .config/dfx/identity/snsdemo8/identity.json? [y]es, [n]o, [A]ll, [N]one, [r]ename: y
    replace .config/dfx/identity/ident-1/identity.pem? [y]es, [n]o, [A]ll, [N]one, [r]ename: y
    replace .config/dfx/identity/ident-1/identity.json? [y]es, [n]o, [A]ll, [N]one, [r]ename: y
    Running dfx start for version 0.13.1
    [SNIP]
    ```
    * Use docker builds for the nns-dapp: `jq
    '.canisters["nns-dapp"].build="./scripts/docker-build"' dfx.json |
    sponge dfx.json`
    * Install the nns-dapp: `dfx deploy nns-dapp`
    * Look at the source code of the nns-dapp in the browser:
    ```
    ...
    <link rel="modulepreload" ...>
      <meta name="nns-dapp-vars" data-own-canister-id="qsgjb-riaaa-aaaaa-aaaga-cai"></head>
      <body>
        <div>
    ...
    ```
    
    ---------
    
    Co-authored-by: Max Murphy-Skvorzov <max@winning.black>

---
+++ rs/backend/src/arguments.rs
@@ -0,0 +1,45 @@
+//! Code for customizinga  particular installation
+use core::cell::RefCell;
+
+/// Init and post_upgrade arguments
+#[derive(Debug, Default, Eq, PartialEq)]
+pub struct CanisterArguments {
+    /// Values that are to be set in the web front end, by injecting them into Javascript.
+    args: Vec<(String, String)>,
+}
+
+thread_local! {
+  pub static CANISTER_ARGUMENTS: RefCell<CanisterArguments> = RefCell::new(CanisterArguments::default().with_own_canister_id());
+}
+
+impl CanisterArguments {
+    /// HTML to be appended onto _every_ index.html
+    pub fn to_html(&self) -> String {
+        let mut ans = r#"<meta name="nns-dapp-vars""#.to_string();
+        for (key, value) in &self.args {
+            ans.push(' ');
+            ans.push_str(&configname2attributename(key));
+            ans.push('=');
+            ans.push_str(&configvalue2attributevalue(value));
+        }
+        ans.push('>');
+        ans
+    }
+
+    pub fn with_own_canister_id(mut self) -> Self {
+        self.args
+            .push(("OWN_CANISTER_ID".to_string(), ic_cdk::api::id().to_string()));
+        self
+    }
+}
+
+/// Converts an upper-snake-case configuration variable to a lower-kebab-case name prefixed with data-.
+/// This, in turn, will appear in JavaScript & family as camel case.
+pub fn configname2attributename(name: &str) -> String {
+    "data-".to_owned() + &name.replace('_', "-").to_lowercase()
+}
+
+/// Escapes a configuration value
+pub fn configvalue2attributevalue(value: &str) -> String {
+    serde_json::Value::String(value.to_string()).to_string()
+}
