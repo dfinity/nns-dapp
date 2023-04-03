45733ad2f7d8514ef3d83c1f781a89294491660a
David Dal Busco <david.dalbusco@dfinity.org>
2023-04-03 16:50:08 +0200

    test: load

--- frontend/src/tests/lib/components/accounts/BitcoinAddress.spec.ts
+++ frontend/src/tests/lib/components/accounts/BitcoinAddress.spec.ts
@@ -10,6 +10,7 @@ import {
 } from "$lib/constants/ckbtc-canister-ids.constants";
 import { AppPath } from "$lib/constants/routes.constants";
 import { bitcoinAddressStore } from "$lib/stores/bitcoin.store";
+import { mockMainAccount } from "$tests/mocks/accounts.store.mock";
 import { mockIdentity } from "$tests/mocks/auth.store.mock";
 import {
   mockBTCAddressTestnet,
@@ -32,37 +33,68 @@ describe("BitcoinAddress", () => {
     universeId: CKTESTBTC_UNIVERSE_CANISTER_ID,
   };
 
-  beforeEach(() => jest.clearAllMocks());
-
-  it("should load bitcoin address on mount", async () => {
-    const spyGetAddress = jest
-      .spyOn(minterApi, "getBTCAddress")
-      .mockResolvedValue(mockBTCAddressTestnet);
-
-    render(BitcoinAddress, { props });
-
-    await waitFor(() =>
-      expect(spyGetAddress).toBeCalledWith({
-        identity: mockIdentity,
-        canisterId: CKTESTBTC_MINTER_CANISTER_ID,
-      })
-    );
+  beforeEach(() => {
+      jest.clearAllMocks();
+      bitcoinAddressStore.reset();
   });
 
-  it("should not load bitcoin address on mount if already loaded", async () => {
-    const spyGetAddress = jest
-      .spyOn(minterApi, "getBTCAddress")
-      .mockResolvedValue(mockBTCAddressTestnet);
+  describe("not matching bitcoin address store", () => {
+    let spyGetAddress;
 
-    const data = {
-      identifier: mockCkBTCMainAccount.identifier,
-      btcAddress: mockBTCAddressTestnet,
-    };
+    beforeEach(() => {
+      spyGetAddress = jest
+        .spyOn(minterApi, "getBTCAddress")
+        .mockResolvedValue(mockBTCAddressTestnet);
+    });
 
-    bitcoinAddressStore.set(data);
+    it("should load bitcoin address on mount", async () => {
+      render(BitcoinAddress, { props });
 
-    render(BitcoinAddress, { props });
+      await waitFor(() =>
+        expect(spyGetAddress).toBeCalledWith({
+          identity: mockIdentity,
+          canisterId: CKTESTBTC_MINTER_CANISTER_ID,
+        })
+      );
+    });
 
-    await waitFor(() => expect(spyGetAddress).not.toHaveBeenCalled());
+    it("should also load bitcoin address on mount if no match", async () => {
+      const data = {
+        identifier: mockMainAccount.identifier,
+        btcAddress: mockBTCAddressTestnet,
+      };
+
+      bitcoinAddressStore.set(data);
+
+      render(BitcoinAddress, { props });
+
+      await waitFor(() =>
+        expect(spyGetAddress).toBeCalledWith({
+          identity: mockIdentity,
+          canisterId: CKTESTBTC_MINTER_CANISTER_ID,
+        })
+      );
+    });
+  });
+
+  describe("existing bitcoin address store", () => {
+    beforeEach(() => {
+      const data = {
+        identifier: mockCkBTCMainAccount.identifier,
+        btcAddress: mockBTCAddressTestnet,
+      };
+
+      bitcoinAddressStore.set(data);
+    });
+
+    it("should not load bitcoin address on mount if already loaded", async () => {
+      const spyGetAddress = jest
+        .spyOn(minterApi, "getBTCAddress")
+        .mockResolvedValue(mockBTCAddressTestnet);
+
+      render(BitcoinAddress, { props });
+
+      await waitFor(() => expect(spyGetAddress).not.toHaveBeenCalled());
+    });
   });
 });
