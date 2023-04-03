4c564a565b6692509aa899f691021902ca74f1b8
David de Kloet <david.de.kloet@dfinity.org>
2023-04-03 10:51:14 +0200

    Use Google Chrome and don't install

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -44,8 +44,8 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      name: "Google Chrome",
+      use: { ...devices["Desktop Chrome"], channel: "chrome" },
     },
     // {
     //   name: 'firefox',
