57fd09a9e577f794ad49be840ae9cfd03d78e495
David de Kloet <david.de.kloet@dfinity.org>
2023-04-04 11:39:26 +0200

    Remove deliberate test failure

--- frontend/src/tests/e2e/accounts.spec.ts
+++ frontend/src/tests/e2e/accounts.spec.ts
@@ -12,7 +12,7 @@ test("Test accounts requirements", async ({ page, context }) => {
   const appPo = new AppPo(pageElement);
 
   // The user has a main account
-  const mainAccountName = "Mainx";
+  const mainAccountName = "Main";
   const nnsAccountsPo = appPo.getAccountsPo().getNnsAccountsPo();
   expect(
     await nnsAccountsPo.getMainAccountCardPo().getAccountName(),
