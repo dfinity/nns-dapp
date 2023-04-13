c1a7c94b0583ea7ddf903a3a73dcac3e5c1fbcae
David Dal Busco <david.dalbusco@dfinity.org>
2023-04-13 17:37:44 +0200

    test: use testid

--- frontend/src/tests/lib/components/accounts/BitcoinFeeDisplayTest.svelte
+++ frontend/src/tests/lib/components/accounts/BitcoinFeeDisplayTest.svelte
@@ -2,9 +2,10 @@
   import BitcoinFeeDisplay from "$lib/components/accounts/BitcoinFeeDisplay.svelte";
   import { i18n } from "$lib/stores/i18n";
 
+  export let testId: string | undefined = undefined;
   export let fee: bigint | undefined | null = undefined;
 </script>
 
-<BitcoinFeeDisplay {fee}>
+<BitcoinFeeDisplay {fee} {testId}>
   {$i18n.accounts.estimated_bitcoin_transaction_fee}
 </BitcoinFeeDisplay>
