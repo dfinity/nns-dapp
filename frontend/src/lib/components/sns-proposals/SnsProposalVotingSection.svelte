9ee434cdaa82a29eb638fa9a99624b0a4352923b
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-04-18 14:36:47 +0200

    feat: hide voting control for settled proposals

--- frontend/src/lib/components/sns-proposals/SnsProposalVotingSection.svelte
+++ frontend/src/lib/components/sns-proposals/SnsProposalVotingSection.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
-  import type { SnsProposalData, SnsTally } from "@dfinity/sns";
+  import type {SnsProposalData, SnsTally} from "@dfinity/sns";
+  import {SnsProposalRewardStatus} from "@dfinity/sns";
   import SnsVotingCard from "$lib/components/sns-proposals/SnsVotingCard.svelte";
   import VotesResults from "$lib/components/proposal-detail/VotesResults.svelte";
-  import { fromDefinedNullable } from "@dfinity/utils";
-  import { E8S_PER_ICP } from "$lib/constants/icp.constants";
+  import {fromDefinedNullable} from "@dfinity/utils";
+  import {E8S_PER_ICP} from "$lib/constants/icp.constants";
+  import {snsRewardStatus} from "$lib/utils/sns-proposals.utils";
 
   export let proposal: SnsProposalData;
   export let reloadProposal: () => Promise<void>;
+
+  let settled = false;
+  $: settled = snsRewardStatus(proposal) === SnsProposalRewardStatus.PROPOSAL_REWARD_STATUS_SETTLED;
 
   let tally: SnsTally;
   $: tally = fromDefinedNullable(proposal.latest_tally);
@@ -24,6 +29,7 @@
 >
   <VotesResults {yes} {no} {total} />
 
-  <!--  TODO(sns-voting): check {#if !settled} logic for sns-->
-  <SnsVotingCard {proposal} {reloadProposal} />
+  {#if !settled}
+    <SnsVotingCard {proposal} {reloadProposal} />
+  {/if}
 </div>
