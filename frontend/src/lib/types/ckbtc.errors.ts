c4f6e4fbe41c08a3c53e992fc02c81903bbcc109
David Dal Busco <david.dalbusco@dfinity.org>
2023-04-26 10:25:10 +0200

    feat: ckBTC display error as info

--- frontend/src/lib/types/ckbtc.errors.ts
+++ frontend/src/lib/types/ckbtc.errors.ts
@@ -1,3 +1,15 @@
 export class CkBTCErrorKey extends Error {}
 
+export class CkBTCInfoKey {
+  private readonly labelKey: string;
+
+  constructor(labelKey: string) {
+    this.labelKey = labelKey;
+  }
+
+  get key() {
+    return this.labelKey;
+  }
+}
+
 export class CkBTCErrorRetrieveBtcMinAmount extends Error {}
