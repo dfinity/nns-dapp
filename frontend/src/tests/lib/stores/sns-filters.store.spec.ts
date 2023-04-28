067bcbc55d857f8fddeae8f28bb7c9f72f930e23
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-04-28 14:09:51 +0200

    GIX-1212: Add sns proposal filters reward status (#2394)
    # Motivation
    
    Users want to filter SNS proposal by reward status.
    
    # Changes
    
    * New filters modal `SnsFilterRewardsModal`.
    * Add rewards filters button in SnsProposalsFilters component.
    * Open `SnsFilterRewardsModal` when rewards filter button is clicked.
    * Load reward status filters in `loadSnsFilters` service.
    * Add new methods to `snsFiltersStore` to manage reward status.
    
    # Tests
    
    * Test new modal `SnsFilterRewardsModal`.
    * Add test case in `SnsProposalsFilters` for new button.
    * Add test case in `loadSnsFilters` service.
    * Add test case for new functionality in `snsFiltersStore`.

--- frontend/src/tests/lib/stores/sns-filters.store.spec.ts
+++ frontend/src/tests/lib/stores/sns-filters.store.spec.ts
@@ -4,32 +4,58 @@ import {
 } from "$lib/stores/sns-filters.store";
 import { mockPrincipal } from "$tests/mocks/auth.store.mock";
 import { Principal } from "@dfinity/principal";
-import { SnsProposalDecisionStatus } from "@dfinity/sns";
+import {
+  SnsProposalDecisionStatus,
+  SnsProposalRewardStatus,
+} from "@dfinity/sns";
 import { get } from "svelte/store";
 
 describe("sns-filters store", () => {
+  const rootCanisterId = mockPrincipal;
+  const rootCanisterId2 = Principal.fromText("pin7y-wyaaa-aaaaa-aacpa-cai");
+  const decisionStatus = [
+    {
+      id: "1",
+      name: "status-1",
+      value: SnsProposalDecisionStatus.PROPOSAL_DECISION_STATUS_OPEN,
+      checked: true,
+    },
+    {
+      id: "2",
+      name: "status-2",
+      value: SnsProposalDecisionStatus.PROPOSAL_DECISION_STATUS_ADOPTED,
+      checked: true,
+    },
+  ];
+  const unCheckedDecisionStatus = decisionStatus.map((status) => ({
+    ...status,
+    checked: false,
+  }));
+  const rewardStatus = [
+    {
+      id: "1",
+      name: "status-1",
+      value: SnsProposalRewardStatus.PROPOSAL_REWARD_STATUS_ACCEPT_VOTES,
+      checked: true,
+    },
+    {
+      id: "2",
+      name: "status-2",
+      value: SnsProposalRewardStatus.PROPOSAL_REWARD_STATUS_READY_TO_SETTLE,
+      checked: true,
+    },
+  ];
+  const unCheckedRewardStatus = rewardStatus.map((status) => ({
+    ...status,
+    checked: false,
+  }));
+
   describe("snsFiltersStore", () => {
     beforeEach(() => {
       snsFiltersStore.reset();
     });
 
     it("should setDecisionStatus in different projects", () => {
-      const rootCanisterId = mockPrincipal;
-      const rootCanisterId2 = Principal.fromText("pin7y-wyaaa-aaaaa-aacpa-cai");
-      const decisionStatus = [
-        {
-          id: "1",
-          name: "status-1",
-          value: SnsProposalDecisionStatus.PROPOSAL_DECISION_STATUS_OPEN,
-          checked: true,
-        },
-        {
-          id: "2",
-          name: "status-2",
-          value: SnsProposalDecisionStatus.PROPOSAL_DECISION_STATUS_ADOPTED,
-          checked: true,
-        },
-      ];
       snsFiltersStore.setDecisionStatus({ rootCanisterId, decisionStatus });
 
       const projectStore = get(snsFiltersStore)[rootCanisterId.toText()];
@@ -43,26 +69,26 @@ describe("sns-filters store", () => {
       expect(projectStore2.decisionStatus).toEqual(decisionStatus);
     });
 
-    it("setCheckDecisionStatus should check filters in different projects", () => {
-      const rootCanisterId = mockPrincipal;
-      const rootCanisterId2 = Principal.fromText("pin7y-wyaaa-aaaaa-aacpa-cai");
-      const decisionStatus = [
-        {
-          id: "1",
-          name: "status-1",
-          value: SnsProposalDecisionStatus.PROPOSAL_DECISION_STATUS_OPEN,
-          checked: false,
-        },
-        {
-          id: "2",
-          name: "status-2",
-          value: SnsProposalDecisionStatus.PROPOSAL_DECISION_STATUS_ADOPTED,
-          checked: false,
-        },
-      ];
+    it("should setRewardStatus in different projects", () => {
+      snsFiltersStore.setRewardStatus({ rootCanisterId, rewardStatus });
 
+      const projectStore = get(snsFiltersStore)[rootCanisterId.toText()];
+      expect(projectStore.rewardStatus).toEqual(rewardStatus);
+
+      snsFiltersStore.setRewardStatus({
+        rootCanisterId: rootCanisterId2,
+        rewardStatus,
+      });
+      const projectStore2 = get(snsFiltersStore)[rootCanisterId2.toText()];
+      expect(projectStore2.rewardStatus).toEqual(rewardStatus);
+    });
+
+    it("setCheckDecisionStatus should check filters in different projects", () => {
       // Project rootCanisterId
-      snsFiltersStore.setDecisionStatus({ rootCanisterId, decisionStatus });
+      snsFiltersStore.setDecisionStatus({
+        rootCanisterId,
+        decisionStatus: unCheckedDecisionStatus,
+      });
       const projectStore = get(snsFiltersStore)[rootCanisterId.toText()];
       expect(
         projectStore.decisionStatus.filter(({ checked }) => checked).length
@@ -81,7 +107,7 @@ describe("sns-filters store", () => {
       // Project rootCanisterId2
       snsFiltersStore.setDecisionStatus({
         rootCanisterId: rootCanisterId2,
-        decisionStatus,
+        decisionStatus: unCheckedDecisionStatus,
       });
       const projectStore3 = get(snsFiltersStore)[rootCanisterId2.toText()];
       expect(
@@ -117,31 +143,80 @@ describe("sns-filters store", () => {
         projectStore6.decisionStatus.filter(({ checked }) => checked).length
       ).toEqual(1);
     });
+
+    it("setCheckRewardStatus should check filters in different projects", () => {
+      // Project rootCanisterId
+      snsFiltersStore.setRewardStatus({
+        rootCanisterId,
+        rewardStatus: unCheckedRewardStatus,
+      });
+      const projectStore = get(snsFiltersStore)[rootCanisterId.toText()];
+      expect(
+        projectStore.rewardStatus.filter(({ checked }) => checked).length
+      ).toEqual(0);
+
+      const statuses = rewardStatus.map(({ value }) => value);
+      snsFiltersStore.setCheckRewardStatus({
+        rootCanisterId,
+        checkedRewardStatus: statuses,
+      });
+      const projectStore2 = get(snsFiltersStore)[rootCanisterId.toText()];
+      expect(
+        projectStore2.rewardStatus.filter(({ checked }) => checked).length
+      ).toEqual(statuses.length);
+
+      // Project rootCanisterId2
+      snsFiltersStore.setRewardStatus({
+        rootCanisterId: rootCanisterId2,
+        rewardStatus: unCheckedRewardStatus,
+      });
+      const projectStore3 = get(snsFiltersStore)[rootCanisterId2.toText()];
+      expect(
+        projectStore3.rewardStatus.filter(({ checked }) => checked).length
+      ).toEqual(0);
+
+      snsFiltersStore.setCheckRewardStatus({
+        rootCanisterId: rootCanisterId2,
+        checkedRewardStatus: [
+          SnsProposalRewardStatus.PROPOSAL_REWARD_STATUS_ACCEPT_VOTES,
+        ],
+      });
+      const projectStore4 = get(snsFiltersStore)[rootCanisterId2.toText()];
+      expect(
+        projectStore4.rewardStatus.filter(({ checked }) => checked).length
+      ).toEqual(1);
+
+      // Project 1 has not changed
+      const projectStore5 = get(snsFiltersStore)[rootCanisterId.toText()];
+      expect(
+        projectStore5.rewardStatus.filter(({ checked }) => checked).length
+      ).toEqual(statuses.length);
+
+      // Uncheck from Project 2
+      snsFiltersStore.setCheckRewardStatus({
+        rootCanisterId,
+        checkedRewardStatus: [
+          SnsProposalRewardStatus.PROPOSAL_REWARD_STATUS_ACCEPT_VOTES,
+        ],
+      });
+      const projectStore6 = get(snsFiltersStore)[rootCanisterId.toText()];
+      expect(
+        projectStore6.rewardStatus.filter(({ checked }) => checked).length
+      ).toEqual(1);
+    });
   });
 
   describe("snsSelectedFiltersStore", () => {
     beforeEach(() => {
       snsFiltersStore.reset();
     });
-    it("should return the selected decision status filters", () => {
-      const rootCanisterId = mockPrincipal;
-      const decisionStatus = [
-        {
-          id: "1",
-          name: "status-1",
-          value: SnsProposalDecisionStatus.PROPOSAL_DECISION_STATUS_OPEN,
-          checked: false,
-        },
-        {
-          id: "2",
-          name: "status-2",
-          value: SnsProposalDecisionStatus.PROPOSAL_DECISION_STATUS_ADOPTED,
-          checked: false,
-        },
-      ];
 
+    it("should return the selected decision status filters", () => {
       // Project rootCanisterId
-      snsFiltersStore.setDecisionStatus({ rootCanisterId, decisionStatus });
+      snsFiltersStore.setDecisionStatus({
+        rootCanisterId,
+        decisionStatus: unCheckedDecisionStatus,
+      });
 
       expect(
         get(snsSelectedFiltersStore)[rootCanisterId.toText()]?.decisionStatus
@@ -156,6 +231,29 @@ describe("sns-filters store", () => {
 
       expect(
         get(snsSelectedFiltersStore)[rootCanisterId.toText()]?.decisionStatus
+      ).toHaveLength(1);
+    });
+
+    it("should return the selected reward status filters", () => {
+      // Project rootCanisterId
+      snsFiltersStore.setRewardStatus({
+        rootCanisterId,
+        rewardStatus: unCheckedRewardStatus,
+      });
+
+      expect(
+        get(snsSelectedFiltersStore)[rootCanisterId.toText()]?.rewardStatus
+      ).toHaveLength(0);
+
+      snsFiltersStore.setCheckRewardStatus({
+        rootCanisterId,
+        checkedRewardStatus: [
+          SnsProposalRewardStatus.PROPOSAL_REWARD_STATUS_ACCEPT_VOTES,
+        ],
+      });
+
+      expect(
+        get(snsSelectedFiltersStore)[rootCanisterId.toText()]?.rewardStatus
       ).toHaveLength(1);
     });
   });
