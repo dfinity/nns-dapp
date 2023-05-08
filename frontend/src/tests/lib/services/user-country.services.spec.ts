11d8fcecf4d55e4c3cbe2f4811a01a64e15e0324
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-05-08 09:03:25 +0200

    GIX-1546: Integrate geo location service (#2447)
    # Motivation
    
    SNSs projects might want to restrict participation in some countries.
    
    To do so, the NNS Dapp needs to get the location of the user and compare
    that with the restriction list.
    
    In this PR: I setup the service and store that will have the user data
    
    # Changes
    
    * New location api. It returns a hardcoded value for now. Once we know
    which service we need to use, we can implement it.
    * New locationStore that has the location of the user as a country code.
    * New location services that uses the api to load the store.
    * New type `CountryCode` that we might want to improve. We might need to
    translate the country code to human readable and this type might help
    us. For now, this is just a string.
    
    # Tests
    
    * Test the new location service.
    * Test the new location store.

---
+++ frontend/src/tests/lib/services/user-country.services.spec.ts
@@ -0,0 +1,42 @@
+import * as locationApi from "$lib/api/location.api";
+import { loadUserCountry } from "$lib/services/user-country.services";
+import { userCountryStore } from "$lib/stores/user-country.store";
+import { blockAllCallsTo } from "$tests/utils/module.test-utils";
+import { get } from "svelte/store";
+
+jest.mock("$lib/api/location.api");
+
+describe("location services", () => {
+  blockAllCallsTo(["$lib/api/location.api"]);
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("loadUserLocation", () => {
+    it("should set the location store to api response", async () => {
+      expect(get(userCountryStore)).toBeUndefined();
+
+      const countryCode = "CH";
+      jest
+        .spyOn(locationApi, "queryUserCountryLocation")
+        .mockResolvedValue(countryCode);
+
+      await loadUserCountry();
+
+      expect(get(userCountryStore)).toBe(countryCode);
+    });
+
+    it("should not call api if location store is already set", async () => {
+      const countryCode = "CH";
+      const apiFn = jest
+        .spyOn(locationApi, "queryUserCountryLocation")
+        .mockResolvedValue(countryCode);
+      userCountryStore.set("CH");
+
+      await loadUserCountry();
+
+      expect(apiFn).not.toHaveBeenCalled();
+    });
+  });
+});
