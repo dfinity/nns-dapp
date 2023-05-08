36004810ca98684dbf677e6fecd651c969755dd9
David Dal Busco <david.dalbusco@dfinity.org>
2023-05-08 14:30:23 +0200

    feat: ckBTC minter canister id on mainnet

--- frontend/src/lib/constants/ckbtc-canister-ids.constants.ts
+++ frontend/src/lib/constants/ckbtc-canister-ids.constants.ts
@@ -3,21 +3,20 @@ import { Principal } from "@dfinity/principal";
 
 const envVars = getEnvVars();
 
-// TODO: environment variables for ckBTC "minter" canister ID
-export const CKBTC_MINTER_CANISTER_ID = Principal.fromText(
-  "q3fc5-haaaa-aaaaa-aaahq-cai"
-);
-
 // We fallback to hardcoded canister IDs because ckBTC is not deployed on every environment at the moment and, we do not want to introduce constants that can be undefined.
 // The feature flags should be set accordingly. The feature is active on mainnet, therefore the fallback values are the one to use on mainnet.
 const MAINNET_CKBTC_LEDGER_CANISTER_ID = "mxzaz-hqaaa-aaaar-qaada-cai";
 const MAINNET_CKBTC_INDEX_CANISTER_ID = "n5wcd-faaaa-aaaar-qaaea-cai";
+const MAINNET_CKBTC_MINTER_CANISTER_ID = "mqygn-kiaaa-aaaar-qaadq-cai";
 
 export const CKBTC_LEDGER_CANISTER_ID = Principal.fromText(
   envVars.ckbtcLedgerCanisterId ?? MAINNET_CKBTC_LEDGER_CANISTER_ID
 );
 export const CKBTC_INDEX_CANISTER_ID = Principal.fromText(
   envVars.ckbtcIndexCanisterId ?? MAINNET_CKBTC_INDEX_CANISTER_ID
+);
+export const CKBTC_MINTER_CANISTER_ID = Principal.fromText(
+    envVars.ckbtcIndexCanisterId ?? MAINNET_CKBTC_MINTER_CANISTER_ID
 );
 
 // TODO: to be remove - ckBTCTest on mainnet
