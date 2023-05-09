b7c3cdd2fc7ef4526cb55c1ed75185311a7f75ff
Llorenç <llorenc.muntaner@dfinity.org>
2023-05-09 14:39:37 +0200

    GIX-1546: Integrate Geo Location Services

---
+++ frontend/src/tests/lib/api/location.api.spec.ts
@@ -0,0 +1,85 @@
+import { queryUserCountryLocation } from "$lib/api/location.api";
+
+describe("location api", () => {
+  describe("queryUserCountryLocation", () => {
+    beforeEach(() => {
+      jest.resetAllMocks();
+    });
+
+    describe("if GeoIP service works", () => {
+      it("should return country code", async () => {
+        const countryCode = "CH";
+        const mockFetch = jest.fn();
+        mockFetch.mockReturnValueOnce(
+          Promise.resolve({
+            ok: true,
+            json: () => Promise.resolve({ countrycode: countryCode }),
+          })
+        );
+        global.fetch = mockFetch;
+
+        const result = await queryUserCountryLocation();
+        expect(result).toEqual(countryCode);
+        expect(mockFetch).toHaveBeenCalledWith(
+          "https://api.geoiplookup.net/?json=true"
+        );
+        expect(mockFetch).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    describe("if GeoIP service fails", () => {
+      it("should call IPLocation service and return country code", async () => {
+        const countryCode = "CH";
+        const ip = "1.1.1.1";
+        const mockFetch = jest.fn();
+        mockFetch
+          .mockReturnValueOnce(
+            Promise.resolve({
+              ok: false,
+            })
+          )
+          .mockReturnValueOnce(
+            Promise.resolve({
+              ok: true,
+              json: () => Promise.resolve({ ip }),
+            })
+          )
+          .mockReturnValueOnce(
+            Promise.resolve({
+              ok: true,
+              json: () => Promise.resolve({ country_code2: countryCode }),
+            })
+          );
+        global.fetch = mockFetch;
+
+        const BASE_URL = "https://api.iplocation.net";
+        const result = await queryUserCountryLocation();
+        expect(result).toEqual(countryCode);
+        expect(mockFetch).toHaveBeenCalledWith(
+          "https://api.geoiplookup.net/?json=true"
+        );
+        expect(mockFetch).toHaveBeenCalledWith(`${BASE_URL}/?cmd=get-ip`);
+        expect(mockFetch).toHaveBeenCalledWith(`${BASE_URL}/?ip=${ip}`);
+        expect(mockFetch).toHaveBeenCalledTimes(3);
+      });
+    });
+
+    describe("if GeoIP and IPLocation services fail", () => {
+      it("should raise error", async () => {
+        const mockFetch = jest.fn();
+        mockFetch.mockReturnValue(
+          Promise.resolve({
+            ok: false,
+          })
+        );
+        global.fetch = mockFetch;
+
+        const call = async () => queryUserCountryLocation();
+        await expect(call).rejects.toThrow(
+          "Failed to fetch ip from IP Location"
+        );
+        expect(mockFetch).toHaveBeenCalledTimes(2);
+      });
+    });
+  });
+});
