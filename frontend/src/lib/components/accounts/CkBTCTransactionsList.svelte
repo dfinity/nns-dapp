d8eca68ff36a38d43c3a13c49f08bccde2f6cd6d
David Dal Busco <david.dalbusco@dfinity.org>
2023-05-10 18:22:04 +0200

    fix: load next transactions after refresh balance

--- frontend/src/lib/components/accounts/CkBTCTransactionsList.svelte
+++ frontend/src/lib/components/accounts/CkBTCTransactionsList.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import type { Account } from "$lib/types/account";
-  import { onMount } from "svelte";
   import { loadCkBTCAccountNextTransactions } from "$lib/services/ckbtc-transactions.services";
   import type { IcrcTransactionData } from "$lib/types/transaction";
   import { icrcTransactionsStore } from "$lib/stores/icrc-transactions.store";
@@ -29,7 +28,7 @@
     loading = false;
   };
 
-  onMount(async () => await loadTransactions());
+  $: account, (async () => loadTransactions())();
 
   let transactions: IcrcTransactionData[];
   $: transactions = getSortedTransactionsFromStore({
