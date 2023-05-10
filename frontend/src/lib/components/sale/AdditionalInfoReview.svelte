e1b64c121188daf996c1bf198d6eff53661ec5b4
David de Kloet <david.de.kloet@dfinity.org>
2023-05-10 11:38:25 +0200

    Remove unused CSS

--- frontend/src/lib/components/sale/AdditionalInfoReview.svelte
+++ frontend/src/lib/components/sale/AdditionalInfoReview.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
   import { Checkbox } from "@dfinity/gix-components";
-  import { ICON_SIZE_LARGE } from "$lib/constants/layout.constants";
-  import { IconWarning } from "@dfinity/gix-components";
   import { i18n } from "$lib/stores/i18n";
 
   export let accepted = false;
@@ -24,16 +22,6 @@
     display: flex;
     flex-direction: column;
     --checkbox-label-order: 1;
-
-    .warning {
-      display: grid;
-      grid-template-columns: auto 1fr;
-      gap: var(--padding-2x);
-
-      .icon {
-        color: var(--warning-emphasis);
-      }
-    }
 
     :global(label) {
       order: 1;
