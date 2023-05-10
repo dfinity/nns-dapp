8f917aad92380525a9d07ed79c42328e4553479a
David Dal Busco <david.dalbusco@dfinity.org>
2023-05-10 15:19:05 +0200

    fix: estimated BTC and inter-networks fees height decimals

--- frontend/src/tests/lib/utils/bitcoin.utils.spec.ts
+++ frontend/src/tests/lib/utils/bitcoin.utils.spec.ts
@@ -2,7 +2,8 @@ import { formatEstimatedFee } from "$lib/utils/bitcoin.utils";
 
 describe("bitcoin.utils", () => {
   it("should format bitcoin estimated fee", () => {
+    expect(formatEstimatedFee(10n)).toEqual("0.00000010");
     expect(formatEstimatedFee(1083n)).toEqual("0.00001083");
-    expect(formatEstimatedFee(108310831083n)).toEqual("1083.10831083");
+    expect(formatEstimatedFee(108310831083n)).toEqual("1'083.10831083");
   });
 });
