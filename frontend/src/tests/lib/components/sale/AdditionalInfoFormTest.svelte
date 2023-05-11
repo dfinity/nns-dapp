f3a497df17f36b38a86ee5230d86f5fa7bad4f06
David de Kloet <david.de.kloet@dfinity.org>
2023-05-11 15:31:10 +0200

    GIX-1555 Support conditions in AdditionalInfoForm

---
+++ frontend/src/tests/lib/components/sale/AdditionalInfoFormTest.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import AdditionalInfoForm from "$lib/components/sale/AdditionalInfoForm.svelte";
+  import { ICPToken, TokenAmount } from "@dfinity/nns";
+
+  export let conditionsToAccept: string | undefined;
+  export let areConditionsAcceptedStore: Writable<boolean>;
+  let areConditionsAccepted: boolean = false;
+
+  $: areConditionsAcceptedStore &&
+    areConditionsAcceptedStore.set(areConditionsAccepted);
+
+  const minCommitment = TokenAmount.fromString({
+    amount: "1.00",
+    token: ICPToken,
+  });
+  const maxCommitment = TokenAmount.fromString({
+    amount: "1000.00",
+    token: ICPToken,
+  });
+  const userHasParticipated = false;
+</script>
+
+<AdditionalInfoForm
+  {minCommitment}
+  {maxCommitment}
+  {userHasParticipated}
+  bind:areConditionsAccepted
+  {conditionsToAccept}
+/>
