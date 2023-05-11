52e96b5e345dfd8b95d454e8ed34ea3cac098330
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2023-05-11 18:22:02 +0200

    GIX-1555 Support conditions in AdditionalInfoForm (#2496)
    # Motivation
    
    We want to allow SNS params to include conditions that have to be agreed
    to by swap participants.
    This PR only updates the component that will display the conditions and
    the checkbox.
    It is not hooked up to anything yet.
    
    # Changes
    
    1. In `frontend/src/lib/components/sale/AdditionalInfoForm.svelte`
    render conditions and a checkbox if conditions are passed as optional
    props.
    2. Add a page object for `AdditionalInfoForm`.
    3. Add a `AdditionalInfoFormTest` component to assist in testing by
    passing a svelte store to read out the value of the checkbox.
    4. Add tests.
    
    # Tests
    
    1. Added a unit test.
    2. Confirmed manually that nothing is displayed yet because the props
    aren't passed to the component.

---
+++ frontend/src/tests/lib/components/sale/AdditionalInfoFormTest.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import AdditionalInfoForm from "$lib/components/sale/AdditionalInfoForm.svelte";
+  import { ICPToken, TokenAmount } from "@dfinity/nns";
+  import type { Writable } from "svelte/store";
+
+  export let conditionsToAccept: string | undefined;
+  export let areConditionsAcceptedStore: Writable<boolean>;
+  let areConditionsAccepted = false;
+
+  $: areConditionsAcceptedStore &&
+    areConditionsAcceptedStore.set(areConditionsAccepted);
+
+  const minCommitment = TokenAmount.fromString({
+    amount: "1.00",
+    token: ICPToken,
+  });
+  const maxCommitment = TokenAmount.fromString({
+    amount: "1000.00",
+    token: ICPToken,
+  });
+  const userHasParticipated = false;
+</script>
+
+<AdditionalInfoForm
+  {minCommitment}
+  {maxCommitment}
+  {userHasParticipated}
+  bind:areConditionsAccepted
+  {conditionsToAccept}
+/>
