4afa369794cbe56f7169129cc8a183a71d63af84
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-05-12 07:01:44 +0200

    Feat: Change user country store type (#2493)
    # Motivation
    
    We need to handle three states in the country store: loading, error and
    success.
    
    In this PR: We change the type of the data in the store to better manage
    the different states.
    
    # Changes
    
    * Change the type inside `userCountryStore`.
    * Add two derived stores with the logic for the error and the loading
    state.
    * New type `Country`.
    
    # Tests
    
    * Fix tests accordingly.
    * Add tests for two new derived stores.

--- frontend/src/lib/stores/user-country.store.ts
+++ frontend/src/lib/stores/user-country.store.ts
@@ -1,5 +1,21 @@
-import type { CountryCode } from "$lib/types/location";
-import { writable } from "svelte/store";
+import type { Country } from "$lib/types/location";
+import { derived, writable } from "svelte/store";
+
+/**
+ * - Not Loaded: "not loaded"
+ * - Error: Error
+ * - Success: Country
+ */
+type UserCountryStore = Country | Error | "not loaded";
 
 // Stores the user's country code
-export const userCountryStore = writable<CountryCode | undefined>(undefined);
+export const userCountryStore = writable<UserCountryStore>("not loaded");
+
+export const isUserCountryLoadedStore = derived(
+  userCountryStore,
+  ($userCountry) => $userCountry !== "not loaded"
+);
+export const isUserCountryErrorStore = derived(
+  userCountryStore,
+  ($userCountry) => $userCountry instanceof Error
+);
--- frontend/src/tests/lib/stores/user-country.store.spec.ts
+++ frontend/src/tests/lib/stores/user-country.store.spec.ts
@@ -1,15 +1,62 @@
-import { userCountryStore } from "$lib/stores/user-country.store";
+import {
+  isUserCountryErrorStore,
+  isUserCountryLoadedStore,
+  userCountryStore,
+} from "$lib/stores/user-country.store";
 import { get } from "svelte/store";
 
 describe("userCountryStore", () => {
   beforeEach(() => {
-    userCountryStore.set(undefined);
+    userCountryStore.set("not loaded");
   });
-  it("should set the location store", () => {
-    userCountryStore.set("CH");
-    expect(get(userCountryStore)).toEqual("CH");
 
-    userCountryStore.set("US");
-    expect(get(userCountryStore)).toEqual("US");
+  describe("userCountryStore", () => {
+    it("should set the location store", () => {
+      const ch = { isoCode: "CH" };
+      userCountryStore.set(ch);
+      expect(get(userCountryStore)).toEqual(ch);
+
+      const us = { isoCode: "US" };
+      userCountryStore.set(us);
+      expect(get(userCountryStore)).toEqual(us);
+    });
+  });
+
+  describe("isUserCountryLoadedStore", () => {
+    beforeEach(() => {
+      userCountryStore.set("not loaded");
+    });
+
+    it("should return false when the location is 'not loaded'", () => {
+      userCountryStore.set("not loaded");
+      expect(get(isUserCountryLoadedStore)).toBe(false);
+    });
+
+    it("should return true when the location is loaded", () => {
+      userCountryStore.set({ isoCode: "CH" });
+      expect(get(isUserCountryLoadedStore)).toEqual(true);
+
+      userCountryStore.set(new Error("Error"));
+      expect(get(isUserCountryLoadedStore)).toEqual(true);
+    });
+  });
+
+  describe("isUserCountryErrorStore", () => {
+    beforeEach(() => {
+      userCountryStore.set("not loaded");
+    });
+
+    it("should return true when the location is an error", () => {
+      userCountryStore.set(new Error("Error"));
+      expect(get(isUserCountryErrorStore)).toBe(true);
+    });
+
+    it("should return false when the location is not an error", () => {
+      userCountryStore.set({ isoCode: "CH" });
+      expect(get(isUserCountryErrorStore)).toEqual(false);
+
+      userCountryStore.set("not loaded");
+      expect(get(isUserCountryErrorStore)).toEqual(false);
+    });
   });
 });
