f48397b5e62b744bce752522b5906cfd496e4806
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-05-16 09:23:05 +0200

    Feat: Add Participate Button Status helper (#2517)
    # Motivation
    
    The status of the Participate Button is becoming complex.
    
    To manage the complexity, in this PR, we add a helper function to
    encapsulate all the logic for the button status.
    
    # Changes
    
    * New project util `participateButtonStatus` and new type
    `ParticipationButtonStatus`.
    * Change return type in `getDeniedCountries`
    
    # Tests
    
    * Test new util.

--- frontend/src/lib/utils/projects.utils.ts
+++ frontend/src/lib/utils/projects.utils.ts
@@ -1,13 +1,14 @@
 import type { SnsFullProject } from "$lib/derived/sns/sns-projects.derived";
 import { getDeniedCountries } from "$lib/getters/sns-summary";
+import type { CountryCode } from "$lib/types/location";
 import type {
   SnsSummary,
   SnsSummarySwap,
   SnsSwapCommitment,
 } from "$lib/types/sns";
 import type { TokenAmount } from "@dfinity/nns";
-import { SnsSwapLifecycle } from "@dfinity/sns";
-import { nonNullish } from "@dfinity/utils";
+import { SnsSwapLifecycle, type SnsSwapTicket } from "@dfinity/sns";
+import { isNullish, nonNullish } from "@dfinity/utils";
 import { nowInSeconds } from "./date.utils";
 import type { I18nSubstitutions } from "./i18n.utils";
 import { getCommitmentE8s } from "./sns.utils";
@@ -261,4 +262,111 @@ export const validParticipation = ({
     };
   }
   return { valid: true };
+};
+
+type ParticipationButtonStatus =
+  | "disabled-max-participation"
+  | "disabled-not-eligible"
+  | "disabled-not-open"
+  | "enabled"
+  | "loading"
+  | "logged-out";
+
+/**
+ * Returns the status of the Participate Button.
+ *
+ * There are 6 possible statuses:
+ * - logged-out
+ * - loading
+ * - disabled-not-open
+ * - disabled-max-participation
+ * - disabled-not-eligible
+ * - enabled
+ *
+ * logged-out:
+ * - The user is not logged in.
+ *
+ * loading:
+ * - Fetching any of the data
+ * - Found an open ticket.
+ *
+ * disabled-max-participation:
+ * - The user has already participated to the swap with the maximum per user.
+ *
+ * disabled-not-eligible:
+ * - The user is in a restricted country.
+ *
+ * disabled-not-open:
+ * - The project is not open.
+ *
+ * enabled:
+ * - None of the above.
+ *
+ * @param {Object} params
+ * @param {SnsSummary | null | undefined} params.summary SNS Summary
+ * @param {SnsSwapCommitment | null | undefined} params.swapCommitment User's swap commitment
+ * @param {SnsSwapTicket | null | undefined} params.ticket The open ticket of the user. The meaning of the types is the same as in ticketsStore.
+ * @param {boolean} params.loggedIn Whether the user is logged in or not.
+ * @param {CountryCode | undefined | Error} params.userCountry The location of the user. The meaning of the types is the same as in userCountryStore.
+ * @returns {ParticipationButtonStatus} A string representing the status of the button.
+ */
+export const participateButtonStatus = ({
+  summary,
+  swapCommitment,
+  loggedIn,
+  ticket,
+  userCountry,
+}: {
+  summary: SnsSummary | undefined | null;
+  swapCommitment: SnsSwapCommitment | undefined | null;
+  loggedIn: boolean;
+  ticket: SnsSwapTicket | undefined | null;
+  userCountry: CountryCode | undefined | Error;
+}): ParticipationButtonStatus => {
+  if (!loggedIn) {
+    return "logged-out";
+  }
+
+  const currentCommitment = getCommitmentE8s(swapCommitment);
+  if (isNullish(summary) || isNullish(currentCommitment)) {
+    return "loading";
+  }
+
+  if (!isProjectOpen(summary)) {
+    return "disabled-not-open";
+  }
+
+  // If `ticket` is undefined, it means that the ticket is still being fetched.
+  // If `ticket` is not null, it means there is an ongoing participation already.
+  if (ticket === undefined || ticket !== null) {
+    return "loading";
+  }
+
+  // Whether user can still participate with 1 e8
+  const userReachedMaxCommitment = commitmentTooLarge({
+    summary,
+    amountE8s: currentCommitment + BigInt(1),
+  });
+  if (userReachedMaxCommitment) {
+    return "disabled-max-participation";
+  }
+
+  const projectDeniedCountries = getDeniedCountries(summary);
+  if (projectDeniedCountries.length > 0) {
+    // We tried to get the user country but it failed.
+    // We don't want to block the user from participating.
+    if (userCountry instanceof Error) {
+      return "enabled";
+    }
+
+    if (userCountry === undefined) {
+      return "loading";
+    }
+
+    if (projectDeniedCountries.includes(userCountry)) {
+      return "disabled-not-eligible";
+    }
+  }
+
+  return "enabled";
 };
