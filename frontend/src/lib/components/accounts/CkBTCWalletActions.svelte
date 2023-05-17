28598546e8f48a7a78123a67c094fa45d2780512
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-05-17 17:19:33 +0200

    chore: remove redundancy

--- frontend/src/lib/components/accounts/CkBTCWalletActions.svelte
+++ frontend/src/lib/components/accounts/CkBTCWalletActions.svelte
@@ -8,14 +8,13 @@
   export let inline = false;
 
   // Exposed for test purpose - i.e. we do not test with a delayed call
-  export let deferReload = true;
 
   // TODO(GIX-1320): ckBTC - update_balance is an happy path, improve UX once track_balance implemented
   const updateBalance = async () =>
     await updateBalanceService({
       minterCanisterId,
       reload,
-      deferReload,
+      deferReload: true,
     });
 </script>
 
