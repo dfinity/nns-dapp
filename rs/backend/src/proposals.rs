d3fe6b5a251435163754c5330fe6e108107e39b1
przydatek@github.com <bartosz.przydatek@dfinity.org>
2023-05-19 17:05:01 +0200

    chore: Update II init type

--- rs/backend/src/proposals.rs
+++ rs/backend/src/proposals.rs
@@ -62,6 +62,8 @@ pub struct InternetIdentityInit {
     pub archive_config: Option<ArchiveConfig>,
     pub canister_creation_cycles_cost: Option<u64>,
     pub register_rate_limit: Option<RateLimitConfig>,
+    pub max_num_latest_delegation_origins: Option<u64>,
+    pub migrate_storage_to_memory_manager: Option<bool>,
 }
 #[derive(CandidType, Serialize, Deserialize)]
 pub struct RateLimitConfig {
