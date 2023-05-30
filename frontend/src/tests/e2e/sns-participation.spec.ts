4b28a0f97374a5ce112719690e8400cac7dfc318
David Dal Busco <david.dalbusco@dfinity.org>
2023-05-30 19:21:59 +0200

    Update title and description (#2601)
    # Motivation
    
    Update NNS dapp title and description as executed on the portal and II.
    
    Portal:
    https://github.com/dfinity/portal/blob/d90e9a6acba468aa8fd9bbf44ac5434cfc21c33b/showcase.json#LL784C9-L784C9
    
    II:
    https://github.com/dfinity/internet-identity/blob/3ff8d257bfcb4adae35a5ebd1c733404d185a7d1/src/frontend/src/flows/dappsExplorer/dapps.json#L129

--- frontend/src/tests/e2e/sns-participation.spec.ts
+++ frontend/src/tests/e2e/sns-participation.spec.ts
@@ -6,7 +6,7 @@ import { expect, test } from "@playwright/test";
 
 test("Test SNS participation", async ({ page, context }) => {
   await page.goto("/");
-  await expect(page).toHaveTitle("Network Nervous System frontend dapp");
+  await expect(page).toHaveTitle("NNS Dapp");
   await signInWithNewUser({ page, context });
 
   const pageElement = PlaywrightPageObjectElement.fromPage(page);
