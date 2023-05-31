0872559cb4783ad0e448d75f899056fbdd25c0b0
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-05-31 14:50:42 +0200

    refactor: remove aria-hidden because of disabled state

--- frontend/src/tests/page-objects/ProposalNavigation.page-object.ts
+++ frontend/src/tests/page-objects/ProposalNavigation.page-object.ts
@@ -10,11 +10,6 @@ export class ProposalNavigationPo extends BasePageObject {
 
   getNextButtonPo = () => this.getButton("proposal-nav-next");
   getPreviousButtonPo = () => this.getButton("proposal-nav-previous");
-  isNextButtonHidden = async () =>
-    (await this.getNextButtonPo().root.getAttribute("aria-hidden")) === "true";
-  isPreviousButtonHidden = async () =>
-    (await this.getPreviousButtonPo().root.getAttribute("aria-hidden")) ===
-    "true";
   clickNextProposal = () => this.getNextButtonPo().click();
   clickPreviousProposal = () => this.getPreviousButtonPo().click();
 }
