b63fb2fbfdb8b70e8b52e322879d093d7ceefb5d
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-05-31 14:23:23 +0200

    feat: add aria-hidden

--- frontend/src/lib/components/proposal-detail/ProposalNavigation.svelte
+++ frontend/src/lib/components/proposal-detail/ProposalNavigation.svelte
@@ -43,19 +43,26 @@
     proposalIdString !== undefined &&
     nextIdString === undefined &&
     previousIdString === undefined;
+
+  let prevDisabled = true;
+  $: prevDisabled =
+    proposalIdString !== undefined && previousIdString === undefined;
+
+  let nextDisabled = true;
+  $: nextDisabled =
+    proposalIdString !== undefined && nextIdString === undefined;
 </script>
 
 {#if proposalIdString && !singleProposal}
-  <div role="toolbar">
+  <div role="toolbar" data-tid="proposal-nav">
     <button
       class="ghost"
       type="button"
       aria-label={$i18n.proposal_detail.newer}
+      aria-hidden={prevDisabled ? true : undefined}
       on:click={previous}
-      class:hidden={proposalIdString !== undefined &&
-        previousIdString === undefined}
-      disabled={proposalIdString !== undefined &&
-        previousIdString === undefined}
+      class:hidden={prevDisabled}
+      disabled={prevDisabled}
       data-tid="proposal-nav-previous"
     >
       <IconWest />
@@ -66,10 +73,10 @@
       class="ghost"
       type="button"
       aria-label={$i18n.proposal_detail.older}
+      aria-hidden={nextDisabled ? true : undefined}
       on:click={next}
-      class:hidden={proposalIdString !== undefined &&
-        nextIdString === undefined}
-      disabled={proposalIdString !== undefined && nextIdString === undefined}
+      class:hidden={nextDisabled}
+      disabled={nextDisabled}
       data-tid="proposal-nav-next"
     >
       {$i18n.proposal_detail.older_short}
