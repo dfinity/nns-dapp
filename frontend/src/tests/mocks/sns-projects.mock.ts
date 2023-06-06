9a8113944ab1354bd401c7123789e774dff00223
David Dal Busco <david.dalbusco@dfinity.org>
2023-06-06 09:07:33 +0200

    Expose ledger canister id in sns summary (#2642)
    # Motivation
    
    Regardless if Sns, ckBTC or ICP we want to be able to fetch balance and
    transactions uses the ICRC-1 ledger.
    
    In Snses we generally use the "wrapper" to access the canister but, in
    the particular crontab feature #2639 we want to avoid the use of such
    object and use `@dfinity/ledger` library directly regardless of the
    context.
    
    That is why we need to access the ledger canister Id of a Sns project.
    
    # Changes
    
    - export `ledgerCanisterId` in `SnsSummary` (as we already did for swap
    and governance)

--- frontend/src/tests/mocks/sns-projects.mock.ts
+++ frontend/src/tests/mocks/sns-projects.mock.ts
@@ -172,6 +172,7 @@ export const mockSnsSummaryList: SnsSummary[] = [
     rootCanisterId: principal(0),
     swapCanisterId: principal(3),
     governanceCanisterId: principal(2),
+    ledgerCanisterId: principal(1),
     metadata: mockMetadata,
     token: mockToken,
     swap: mockSwap,
@@ -181,6 +182,7 @@ export const mockSnsSummaryList: SnsSummary[] = [
     rootCanisterId: principal(1),
     swapCanisterId: principal(2),
     governanceCanisterId: principal(3),
+    ledgerCanisterId: principal(0),
     metadata: {
       logo: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADUAAAA0CAYAAAAqunDVAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAClSURBVHgB7dqxDYMwEEBRE2UET5I6icQUzMgc9EziHUAuqLElS+if/iuoruAXyCeL6fufjxTMuz5KKbeDOee0rXtq8Vs+TbM9cy3vWNX3fKWAjKIwisIoipBRU9iNYuTp3zM7eu6a9ZuiMIrCKAqjKNwoek711nuPkXPXrN8UhVEURlEYReFG4R3Fg4yiMIrCKAo3Cgr/o6AwisIoCqMojKIIuSadjJ5VyRrmqP4AAAAASUVORK5CYII=",
       name: "Pac-Man",
@@ -200,6 +202,7 @@ export const mockSnsSummaryList: SnsSummary[] = [
     rootCanisterId: principal(2),
     swapCanisterId: principal(1),
     governanceCanisterId: principal(3),
+    ledgerCanisterId: principal(0),
     metadata: {
       logo: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADUAAAA0CAYAAAAqunDVAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAACjSURBVHgB7dkxDkRAGEDh32ZPsuWWWy+JQ4gzimPolUpXIXMCM4nmjfcVqglegT+j+Xf9EZV5p8MyrZcL2/EX+7BFjs/8zVpbsi7nHpN0n6+okFEURlEYRVFlVPP4iaLkq37npFB6bZ8pCqMojKIwisKJomSP4s5zukcRvig4jKIwisKJgsK/HhRGURhFYRSFEwWFEwWFURRGURhFYRRFlWPSCah/Vck0pRWfAAAAAElFTkSuQmCC",
       name: "Super Mario",
@@ -219,6 +222,7 @@ export const mockSnsSummaryList: SnsSummary[] = [
     rootCanisterId: principal(3),
     swapCanisterId: principal(0),
     governanceCanisterId: principal(2),
+    ledgerCanisterId: principal(1),
     metadata: {
       logo: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADUAAAA0CAYAAAAqunDVAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAC6SURBVHgB7ZkxCsJAFAUTETyAexcbEfZSHsJLBcTGu6wHsFK3SL0/EIvZzBSpHj87gYXHz3i+5M/QGfv6OB2mZvD5zqHcnC2lNHMppb+8ezd0iFIUlKKgFIUupcbNN4rpeB0i5Ndt1ZnRefNM7xQFpSgoRUEpCv02ijX3CZXoTmFJLnLGSj2nd4qCUhSUoqAUBXcUSxrF497O/j6ofz2iKEVBKQpKUbBRUHBHQUEpCkpRUIqCUhS6rElfBK1VyaWjTNYAAAAASUVORK5CYII=",
       name: "Donkey Kong",
