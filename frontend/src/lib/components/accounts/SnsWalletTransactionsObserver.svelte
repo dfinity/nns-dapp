c5742ff08fe7a0896961e85bf5051d7ac675e810
David Dal Busco <david.dalbusco@dfinity.org>
2023-06-07 10:08:39 +0200

    feat: todo

--- frontend/src/lib/components/accounts/SnsWalletTransactionsObserver.svelte
+++ frontend/src/lib/components/accounts/SnsWalletTransactionsObserver.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
   import type { TransactionsObserverData } from "$lib/types/icrc.observer";
-  import { nonNullish } from "@dfinity/utils";
-  import { snsProjectSelectedStore } from "$lib/derived/sns/sns-selected-project.derived";
+  import { isNullish, nonNullish } from "@dfinity/utils";
+  import {
+    snsOnlyProjectStore,
+    snsProjectSelectedStore,
+  } from "$lib/derived/sns/sns-selected-project.derived";
   import {
     WALLET_CONTEXT_KEY,
     type WalletContext,
   } from "$lib/types/wallet.context";
   import { getContext } from "svelte";
   import IcrcTransactionsObserver from "$lib/components/accounts/IcrcTransactionsObserver.svelte";
+  import { icrcTransactionsStore } from "$lib/stores/icrc-transactions.store";
+  import type { UniverseCanisterId } from "$lib/types/universe";
+  import type {TransactionsCallback} from "$lib/services/worker-transactions.services";
 
   const context: WalletContext = getContext<WalletContext>(WALLET_CONTEXT_KEY);
   const { store }: WalletContext = context;
@@ -22,10 +28,29 @@
         }
       : undefined;
 
-  const callback = (something) => console.log("MSG", something);
+  const callback: TransactionsCallback = ({ transactions }) => {
+    if (isNullish(universeId)) {
+      // Cannot be undefined
+      return;
+    }
+
+    for (const account of transactions) {
+      const {transactions: ts, ...rest} = account;
+
+      // TODO: stringify principal
+      // icrcTransactionsStore.addTransactions({
+      //   ...rest,
+      //   transactions: ts,
+      //   canisterId: universeId,
+      // });
+    }
+  };
+
+  let universeId: UniverseCanisterId | undefined;
+  $: universeId = $snsOnlyProjectStore;
 </script>
 
-{#if nonNullish(data)}
+{#if nonNullish(data) && nonNullish(universeId)}
   <IcrcTransactionsObserver {data} {callback}>
     <slot />
   </IcrcTransactionsObserver>
