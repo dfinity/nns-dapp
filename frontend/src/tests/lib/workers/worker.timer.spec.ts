8abcaac1ee5426e567c9b7e16ba23222b124c154
David Dal Busco <david.dalbusco@dfinity.org>
2023-06-07 07:25:42 +0200

    test: worker timer

---
+++ frontend/src/tests/lib/workers/worker.timer.spec.ts
@@ -0,0 +1,165 @@
+import { WorkerTimer } from "$lib/workers/worker.timer";
+import { mockIdentity } from "$tests/mocks/auth.store.mock";
+import { advanceTime } from "$tests/utils/timers.test-utils";
+import { AuthClient } from "@dfinity/auth-client";
+import { mock } from "jest-mock-extended";
+
+describe("worker-timer", () => {
+  const now = Date.now();
+
+  beforeEach(() => jest.useFakeTimers().setSystemTime(now));
+  afterEach(jest.clearAllTimers);
+
+  describe("without identity", () => {
+    const mockAuthClient = mock<AuthClient>();
+    mockAuthClient.isAuthenticated.mockResolvedValue(false);
+
+    beforeAll(() =>
+      jest
+        .spyOn(AuthClient, "create")
+        .mockImplementation(async (): Promise<AuthClient> => mockAuthClient)
+    );
+
+    it("should not call if no identity", async () => {
+      const worker = new WorkerTimer();
+
+      const job = jest.fn();
+
+      await worker.start({
+        job,
+        data: {},
+        interval: 5000,
+      });
+
+      expect(job).not.toHaveBeenCalled();
+    });
+  });
+
+  describe("with identity", () => {
+    const mockAuthClient = mock<AuthClient>();
+    mockAuthClient.isAuthenticated.mockResolvedValue(true);
+    mockAuthClient.getIdentity.mockResolvedValue(mockIdentity as never);
+
+    beforeAll(() =>
+      jest
+        .spyOn(AuthClient, "create")
+        .mockImplementation(async (): Promise<AuthClient> => mockAuthClient)
+    );
+
+    it("should call job on start", async () => {
+      const worker = new WorkerTimer();
+
+      const job = jest.fn();
+
+      await worker.start({
+        job,
+        data: {},
+        interval: 5000,
+      });
+
+      expect(job).toHaveBeenCalledTimes(1);
+    });
+
+    it("should not call job if already started", async () => {
+      const worker = new WorkerTimer();
+
+      const job = jest.fn();
+
+      await worker.start({
+        job,
+        data: {},
+        interval: 5000,
+      });
+
+      expect(job).toHaveBeenCalledTimes(1);
+
+      const anotherJob = jest.fn();
+
+      await worker.start({
+        job: anotherJob,
+        data: {},
+        interval: 5000,
+      });
+
+      expect(anotherJob).not.toHaveBeenCalled();
+    });
+
+    it("should call job after interval", async () => {
+      const worker = new WorkerTimer();
+
+      const job = jest.fn();
+
+      await worker.start({
+        job,
+        data: {},
+        interval: 5000,
+      });
+
+      expect(job).toHaveBeenCalledTimes(1);
+
+      // wait for 5 seconds
+      await advanceTime(5000);
+
+      expect(job).toHaveBeenCalledTimes(2);
+
+      // wait for 5 seconds
+      await advanceTime(5000);
+
+      expect(job).toHaveBeenCalledTimes(3);
+    });
+
+    it("should call job with identity and data", async () => {
+      const worker = new WorkerTimer();
+
+      const job = jest.fn();
+      const data = { test: 123 };
+
+      await worker.start({
+        job,
+        data,
+        interval: 5000,
+      });
+
+      expect(job).toBeCalledWith({ identity: mockIdentity, data });
+    });
+
+    it("should stop timer", async () => {
+      const worker = new WorkerTimer();
+
+      const job = jest.fn();
+
+      await worker.start({
+        job,
+        data: {},
+        interval: 5000,
+      });
+
+      expect(job).toHaveBeenCalledTimes(1);
+
+      worker.stop();
+
+      // wait for 5 seconds
+      await advanceTime(5000);
+
+      expect(job).toHaveBeenCalledTimes(1);
+    });
+
+    it("should call cleanup on stop", async () => {
+      const worker = new WorkerTimer();
+
+      const job = jest.fn();
+
+      await worker.start({
+        job,
+        data: {},
+        interval: 5000,
+      });
+
+      const cleanup = jest.fn();
+
+      worker.stop(cleanup);
+
+      expect(cleanup).toHaveBeenCalledTimes(1);
+    });
+  });
+});
