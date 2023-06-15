a9b18cf70daef1510e371001f422d5ab9473e2d7
Llorenç <llorenc.muntaner@dfinity.org>
2023-06-15 10:20:21 +0200

    Feat: Add store utils describe

--- frontend/src/tests/lib/utils/store.utils.spec.ts
+++ frontend/src/tests/lib/utils/store.utils.spec.ts
@@ -1,12 +1,14 @@
 import { isStoreData } from "$lib/utils/store.utils";
 
-describe("isStoreData", () => {
-  it("should return false if data is error or not loaded", () => {
-    expect(isStoreData("not loaded")).toBe(false);
-    expect(isStoreData(new Error())).toBe(false);
-  });
+describe("store utils", () => {
+  describe("isStoreData", () => {
+    it("should return false if data is error or not loaded", () => {
+      expect(isStoreData("not loaded")).toBe(false);
+      expect(isStoreData(new Error())).toBe(false);
+    });
 
-  it("should return true if data is error or not loaded", () => {
-    expect(isStoreData({ data: [] })).toBe(true);
+    it("should return true if data is error or not loaded", () => {
+      expect(isStoreData({ data: [] })).toBe(true);
+    });
   });
 });
