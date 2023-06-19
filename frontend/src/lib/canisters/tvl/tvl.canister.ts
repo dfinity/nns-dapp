db1fae460ad4b412c699f5d535ff5b84da9aa671
David Dal Busco <david.dalbusco@dfinity.org>
2023-06-19 07:27:21 +0200

    feat: compatibility with new api

--- frontend/src/lib/canisters/tvl/tvl.canister.ts
+++ frontend/src/lib/canisters/tvl/tvl.canister.ts
@@ -1,9 +1,13 @@
 import type { TVLCanisterOptions } from "$lib/canisters/tvl/tvl.canister.types";
 import { Actor } from "@dfinity/agent";
-import { Canister } from "@dfinity/utils";
+import { Canister, toNullable, type QueryParams } from "@dfinity/utils";
 import { idlFactory as certifiedIdlFactory } from "./tvl.certified.idl";
 import { idlFactory } from "./tvl.idl";
-import type { TvlResult, _SERVICE as TVLService } from "./tvl.types";
+import type {
+  _SERVICE as TVLService,
+  TvlRequest,
+  TvlResult,
+} from "./tvl.types";
 
 export class TVLCanister extends Canister<TVLService> {
   public static create(options: TVLCanisterOptions): TVLCanister {
@@ -26,10 +30,13 @@ export class TVLCanister extends Canister<TVLService> {
     return new TVLCanister(canisterId, service, certifiedService);
   }
 
-  public getTVL = async (params: {
-    certified: boolean;
-  }): Promise<TvlResult> => {
-    const response = await this.caller(params).get_tvl();
+  public getTVL = async ({
+    certified,
+    request,
+  }: QueryParams & { request?: TvlRequest }): Promise<TvlResult> => {
+    const response = await this.caller({ certified }).get_tvl(
+      toNullable(request)
+    );
 
     if ("Err" in response) {
       throw new Error(response.Err.message);
