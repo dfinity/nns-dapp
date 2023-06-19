2a20f7b971ebf2754fc0deb6bf08e3e897e0a030
David Dal Busco <david.dalbusco@dfinity.org>
2023-06-19 12:51:41 +0200

    chore: format

--- frontend/src/lib/canisters/tvl/tvl.canister.types.ts
+++ frontend/src/lib/canisters/tvl/tvl.canister.types.ts
@@ -1,8 +1,11 @@
-import type { FiatCurrency, TvlResult } from "$lib/canisters/tvl/tvl.types";
+import type {
+  FiatCurrency,
+  _SERVICE as TVLService,
+  TvlResult,
+} from "$lib/canisters/tvl/tvl.types";
 import type { Agent } from "@dfinity/agent";
 import type { Principal } from "@dfinity/principal";
 import type { QueryParams } from "@dfinity/utils";
-import type { _SERVICE as TVLService } from "$lib/canisters/tvl/tvl.types";
 
 export interface TVLCanisterOptions {
   // The agent to use when communicating with the governance canister.
