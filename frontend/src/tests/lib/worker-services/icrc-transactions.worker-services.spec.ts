47063db4d3c5dfb0bd50757910cc8b03954bc5e6
David Dal Busco <david.dalbusco@dfinity.org>
2023-06-21 08:34:53 +0200

    test: multiple accounts

--- frontend/src/tests/lib/worker-services/icrc-transactions.worker-services.spec.ts
+++ frontend/src/tests/lib/worker-services/icrc-transactions.worker-services.spec.ts
@@ -3,7 +3,10 @@ import type { PostMessageDataRequestTransactions } from "$lib/types/post-message
 import { getIcrcAccountsTransactions } from "$lib/worker-services/icrc-transactions.worker-services";
 import { mockIdentity } from "$tests/mocks/auth.store.mock";
 import { mockCanisterId } from "$tests/mocks/canisters.mock";
-import { mockSnsMainAccount } from "$tests/mocks/sns-accounts.mock";
+import {
+  mockSnsMainAccount,
+  mockSnsSubAccount,
+} from "$tests/mocks/sns-accounts.mock";
 import { IcrcIndexCanister, type IcrcTransaction } from "@dfinity/ledger";
 import mock from "jest-mock-extended/lib/Mock";
 
@@ -22,6 +25,15 @@ describe("transactions.worker-services", () => {
     burn: [],
   } as unknown as IcrcTransaction;
 
+  const request: Omit<
+    PostMessageDataRequestTransactions,
+    "accountIdentifiers"
+  > = {
+    host: HOST,
+    fetchRootKey: true,
+    indexCanisterId: mockCanisterId.toText(),
+  };
+
   it("should returns new transactions", async () => {
     const id = BigInt(1);
     const transactions = [{ transaction, id }];
@@ -33,9 +45,7 @@ describe("transactions.worker-services", () => {
       });
 
     const data: PostMessageDataRequestTransactions = {
-      host: HOST,
-      fetchRootKey: true,
-      indexCanisterId: mockCanisterId.toText(),
+      ...request,
       accountIdentifiers: [mockSnsMainAccount.identifier],
     };
 
@@ -50,6 +60,48 @@ describe("transactions.worker-services", () => {
     expect(results).toEqual([
       {
         accountIdentifier: mockSnsMainAccount.identifier,
+        transactions,
+        mostRecentTxId: id,
+        oldestTxId: undefined,
+      },
+    ]);
+  });
+
+  it("should returns new transactions for multiple accounts", async () => {
+    const id = BigInt(1);
+    const transactions = [{ transaction, id }];
+
+    const getTransactionsSpy =
+      indexCanisterMock.getTransactions.mockResolvedValue({
+        transactions,
+        oldest_tx_id: [],
+      });
+
+    const data: PostMessageDataRequestTransactions = {
+      ...request,
+      accountIdentifiers: [
+        mockSnsMainAccount.identifier,
+        mockSnsSubAccount.identifier,
+      ],
+    };
+
+    const results = await getIcrcAccountsTransactions({
+      identity: mockIdentity,
+      state: {},
+      data,
+    });
+
+    expect(getTransactionsSpy).toHaveBeenCalledTimes(2);
+
+    expect(results).toEqual([
+      {
+        accountIdentifier: mockSnsMainAccount.identifier,
+        transactions,
+        mostRecentTxId: id,
+        oldestTxId: undefined,
+      },
+      {
+        accountIdentifier: mockSnsSubAccount.identifier,
         transactions,
         mostRecentTxId: id,
         oldestTxId: undefined,
