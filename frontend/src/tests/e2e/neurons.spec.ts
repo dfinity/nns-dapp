3d469d1f9825beaf45b884bafee5193045cdf51a
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-06-22 16:17:10 +0200

    test: vote for proposal e2e

--- frontend/src/tests/e2e/neurons.spec.ts
+++ frontend/src/tests/e2e/neurons.spec.ts
@@ -18,15 +18,82 @@ test("Test neuron management", async ({ page, context }) => {
     .getNnsAccountsPo()
     .getMainAccountCardPo()
     .waitFor();
-  await appPo.getTokens(20);
+  await appPo.getTokens(31);
 
   step("Go to the neurons tab");
   await appPo.goToNeurons();
 
-  step("Stake a neuron");
+  step("Stake neuron A (for voting)");
   const stake = 15;
   await appPo
     .getNeuronsPo()
     .getNnsNeuronsFooterPo()
     .stakeNeuron({ amount: stake });
+
+  step("Stake neuron B (for dummy proposals creation)");
+  await appPo
+    .getNeuronsPo()
+    .getNnsNeuronsFooterPo()
+    .stakeNeuron({ amount: stake });
+
+  step("Open neuron B details");
+
+  await appPo.getNeuronsPo().getNnsNeuronsPo().waitForContentLoaded();
+  const ids = await appPo.getNeuronsPo().getNnsNeuronsPo().getNeuronIds();
+
+  if (ids.length === 0) {
+    throw new Error("No neuron cards found");
+  }
+
+  // goto first neuron since it should be the one we just created
+  await appPo.goToNeuronDetails(ids[0]);
+
+  step("Create dummy proposal with neuron B");
+
+  // create dummy proposals
+  await appPo.getNeuronDetailPo().getNnsNeuronDetailPo().createDummyProposals();
+
+  step("Open proposals list");
+
+  // back to /neurons otherwise the menu is not available
+  await appPo.goBack();
+  await appPo.goToProposals();
+
+  await appPo.getProposalsPo().getNnsProposalListPo().waitForContentLoaded();
+
+  const proposalIds = await appPo
+    .getProposalsPo()
+    .getNnsProposalListPo()
+    .getProposalIds();
+
+  if (proposalIds.length === 0) {
+    throw new Error("No proposal cards found");
+  }
+
+  step("Open first proposal");
+  await (
+    await appPo.getProposalsPo().getNnsProposalListPo().getProposalCardPos()
+  )[0].click();
+
+  const proposalDetails = appPo.getProposalDetailPo().getNnsProposalPo();
+  await proposalDetails.waitForContentLoaded();
+
+  await page.waitForSelector("[data-tid='adopt']");
+  const initialAdoptVotingPower = await proposalDetails
+    .getVotesResultPo()
+    .getAdoptVotingPower();
+
+  step("Vote for proposal");
+
+  await proposalDetails.getVotingCardPo().voteYes();
+
+  step("Compare voting power before and after voting");
+
+  const changedAdoptVotingPower = await proposalDetails
+    .getVotesResultPo()
+    .getAdoptVotingPower();
+
+  expect(Number(changedAdoptVotingPower)).toBeGreaterThan(
+    Number(initialAdoptVotingPower)
+  );
 });
