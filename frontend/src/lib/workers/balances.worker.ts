83afa5595cae6aa61c1470d5bf0c954d85453152
David Dal Busco <david.dalbusco@dfinity.org>
2023-06-26 11:53:04 +0200

    feat: extract service and only apply values to store on update calls

--- frontend/src/lib/workers/balances.worker.ts
+++ frontend/src/lib/workers/balances.worker.ts
@@ -5,24 +5,19 @@ import type {
   PostMessageDataResponseBalances,
 } from "$lib/types/post-message.balances";
 import type { PostMessage } from "$lib/types/post-messages";
-import { getIcrcBalance } from "$lib/worker-api/icrc-ledger.worker-api";
-import type { DictionaryWorkerData } from "$lib/worker-stores/dictionary.worker-store";
+import type { GetAccountsBalanceData } from "$lib/worker-services/icrc-balances.worker-services";
+import { getIcrcAccountsBalances } from "$lib/worker-services/icrc-balances.worker-services";
 import { DictionaryWorkerStore } from "$lib/worker-stores/dictionary.worker-store";
 import {
   TimerWorkerUtils,
   type TimerWorkerUtilsJobData,
 } from "$lib/worker-utils/timer.worker-utils";
-import { decodeIcrcAccount } from "@dfinity/ledger";
 
 // Worker context to start and stop job
 const worker = new TimerWorkerUtils();
 
 // A worker store to keep track of account balances
-interface BalanceData extends DictionaryWorkerData {
-  balance: bigint;
-}
-
-const store = new DictionaryWorkerStore<BalanceData>();
+const store = new DictionaryWorkerStore<GetAccountsBalanceData>();
 
 onmessage = async ({
   data: dataMsg,
@@ -47,9 +42,8 @@ onmessage = async ({
 const syncBalances = async (
   params: TimerWorkerUtilsJobData<PostMessageDataRequestBalances>
 ) => {
-  // eslint-disable-next-line no-useless-catch
   try {
-    const queries = await getIcrcBalances({
+    const queries = await getIcrcAccountsBalances({
       ...params,
       certified: false,
     });
@@ -60,16 +54,13 @@ const syncBalances = async (
 
     if (changes.length === 0) {
       // Optimistic approach:
-      // Nothing has changed according query calls therefore we stop here for performance reason and, we spare the update calls.
-      // We do this for performance reason.
+      // Nothing has changed according query calls therefore, we spare the update calls.
+      // We do this for performance reason in order to reduce to the amount of update calls we perform from this worker.
       return;
     }
 
-    // Update store with queries
-    store.update(changes);
-
     // Call and update store with certified data
-    const updates = await getIcrcBalances({
+    const updates = await getIcrcAccountsBalances({
       ...params,
       certified: true,
     });
@@ -92,31 +83,6 @@ const syncBalances = async (
     throw err;
   }
 };
-
-const getIcrcBalances = ({
-  identity,
-  data: { accountIdentifiers, ledgerCanisterId, ...rest },
-  certified,
-}: TimerWorkerUtilsJobData<PostMessageDataRequestBalances> & {
-  certified: boolean;
-}): Promise<BalanceData[]> =>
-  Promise.all(
-    accountIdentifiers.map(async (accountIdentifier) => {
-      const balance = await getIcrcBalance({
-        ledgerCanisterId,
-        identity,
-        account: decodeIcrcAccount(accountIdentifier),
-        certified,
-        ...rest,
-      });
-
-      return {
-        balance,
-        key: accountIdentifier,
-        certified,
-      };
-    })
-  );
 
 const emitBalances = (balances: PostMessageDataResponseBalance[]) => {
   const data: PostMessageDataResponseBalances = { balances };
