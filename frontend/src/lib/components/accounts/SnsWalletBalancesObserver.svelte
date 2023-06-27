b5d1cd41be53655b48055069613ec8f4453bb17e
David Dal Busco <david.dalbusco@dfinity.org>
2023-06-27 15:30:55 +0200

    feat: refresh accounts balance periodically on Sns wallet

---
+++ frontend/src/lib/components/accounts/SnsWalletBalancesObserver.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import type { BalancesObserverData } from "$lib/types/icrc.observer";
+  import { nonNullish } from "@dfinity/utils";
+  import IcrcBalancesObserver from "$lib/components/accounts/IcrcBalancesObserver.svelte";
+  import type { BalancesCallback } from "$lib/services/worker-balances.services";
+  import { snsProjectAccountsStore } from "$lib/derived/sns/sns-project-accounts.derived";
+  import { snsAccountsStore } from "$lib/stores/sns-accounts.store";
+  import type { UniverseCanisterId } from "$lib/types/universe";
+  import type { Account } from "$lib/types/account";
+  import type { CanisterId } from "$lib/types/canister";
+
+  export let universeId: UniverseCanisterId;
+  export let ledgerCanisterId: CanisterId;
+  export let account: Account;
+
+  const callback: BalancesCallback = ({ balances }) => {
+    const accounts = balances
+      .map(({ balance, accountIdentifier }) => {
+        const selectedAccount = $snsProjectAccountsStore?.find(
+          ({ identifier }) => identifier === accountIdentifier
+        );
+
+        return nonNullish(selectedAccount)
+          ? {
+              ...selectedAccount,
+              balanceE8s: balance,
+            }
+          : undefined;
+      })
+      .filter(nonNullish);
+
+    snsAccountsStore.updateAccounts({
+      accounts,
+      rootCanisterId: universeId,
+      certified: true,
+    });
+  };
+
+  let data: BalancesObserverData;
+  $: data = {
+    account,
+    ledgerCanisterId: ledgerCanisterId.toText(),
+  };
+</script>
+
+{#if nonNullish(data)}
+  <IcrcBalancesObserver {data} {callback}>
+    <slot />
+  </IcrcBalancesObserver>
+{/if}
