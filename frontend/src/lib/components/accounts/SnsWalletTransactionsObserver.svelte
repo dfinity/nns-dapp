18f6500f8c69770edb203abd4b8c641f373f3130
David Dal Busco <david.dalbusco@dfinity.org>
2023-06-28 15:11:03 +0200

    Refresh transactions periodically on Sns wallet (#2639)
    # Motivation
    
    This PR was used to develop the new feature that refreshes periodically
    balance and transactions in a Web Worker. It introduces the remaining
    "transactions" feature for the Sns wallet page.
    
    # PRs
    
    - [x] #2641
    - [x] #2642
    - [x] #2643
    - [x] #2644
    - [x] #2648
    - [x] #2650
    - [x] #2651
    - [x] https://github.com/dfinity/ic-js/pull/343
    - [x] #2692
    - [x] #2708
    - [x] #2719
    - [x] #2721
    - [x] #2725
    - [x] #2731
    - [x] #2733
    - [x] #2751
    - [x] #2754
    - [x] #2755
    - [x] #2756
    - [x] #2759
    - [x] https://github.com/dfinity/gix-components/pull/224
    - [x] https://github.com/dfinity/gix-components/pull/225
    - [x] #2763
    - [x] #2764
    - [x] https://github.com/dfinity/gix-components/pull/226
    - [x] #2769
    
    # Changes
    
    - use feature reviewed and merged in separate PRs (list above) in
    `SnsTransactionsList` components
    - add components `SnsWalletTransactionsOberserver` to refresh UI stores
    when Web Workers communicate updates

---
+++ frontend/src/lib/components/accounts/SnsWalletTransactionsObserver.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import type { TransactionsObserverData } from "$lib/types/icrc.observer";
+  import { isNullish, nonNullish } from "@dfinity/utils";
+  import {
+    snsOnlyProjectStore,
+    snsProjectSelectedStore,
+  } from "$lib/derived/sns/sns-selected-project.derived";
+  import IcrcTransactionsObserver from "$lib/components/accounts/IcrcTransactionsObserver.svelte";
+  import { icrcTransactionsStore } from "$lib/stores/icrc-transactions.store";
+  import type { UniverseCanisterId } from "$lib/types/universe";
+  import type { TransactionsCallback } from "$lib/services/worker-transactions.services";
+  import { jsonReviver } from "$lib/utils/json.utils";
+  import type { Account } from "$lib/types/account";
+
+  export let account: Account;
+  export let completed: boolean;
+
+  let data: TransactionsObserverData | undefined;
+  $: data = nonNullish($snsProjectSelectedStore)
+    ? {
+        account,
+        indexCanisterId:
+          $snsProjectSelectedStore.summary.indexCanisterId.toText(),
+      }
+    : undefined;
+
+  const callback: TransactionsCallback = ({ transactions }) => {
+    if (isNullish(universeId)) {
+      // With current usage, can unlikely be undefined here
+      return;
+    }
+
+    for (const transaction of transactions) {
+      const {
+        transactions: jsonTransactions,
+        mostRecentTxId: _,
+        accountIdentifier,
+        ...rest
+      } = transaction;
+
+      icrcTransactionsStore.addTransactions({
+        ...rest,
+        accountIdentifier,
+        transactions: JSON.parse(jsonTransactions, jsonReviver),
+        canisterId: universeId,
+        completed,
+      });
+    }
+  };
+
+  let universeId: UniverseCanisterId | undefined;
+  $: universeId = $snsOnlyProjectStore;
+</script>
+
+{#if nonNullish(data) && nonNullish(universeId)}
+  <IcrcTransactionsObserver {data} {callback}>
+    <slot />
+  </IcrcTransactionsObserver>
+{/if}
---
+++ frontend/src/tests/lib/components/accounts/SnsWalletTransactionsObserverTest.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { mockSnsMainAccount } from "$tests/mocks/sns-accounts.mock";
+  import SnsWalletTransactionsObserver from "$lib/components/accounts/SnsWalletTransactionsObserver.svelte";
+</script>
+
+<SnsWalletTransactionsObserver account={mockSnsMainAccount} completed={true}>
+  <div data-tid="test-observer" />
+</SnsWalletTransactionsObserver>
