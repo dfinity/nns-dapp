1c7e2e78a4f67889353c5ff848f2a36f1e4fb57f
David Dal Busco <david.dalbusco@dfinity.org>
2023-07-03 13:34:21 +0200

    feat: observe ckBTC balances

---
+++ frontend/src/lib/components/accounts/CkBTCBalancesObserver.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import type { BalancesObserverData } from "$lib/types/icrc.observer";
+  import { nonNullish } from "@dfinity/utils";
+  import IcrcBalancesObserver from "$lib/components/accounts/IcrcBalancesObserver.svelte";
+  import type { BalancesCallback } from "$lib/services/worker-balances.services";
+  import type { Account } from "$lib/types/account";
+  import type { UniverseCanisterId } from "$lib/types/universe";
+  import { icrcAccountsStore } from "$lib/stores/icrc-accounts.store";
+
+  export let universeId: UniverseCanisterId;
+  export let accounts: Account[];
+  export let reload: (() => void) | undefined;
+
+  const callback: BalancesCallback = ({ balances }) => {
+    const accounts = balances
+      .map(({ balance, accountIdentifier }) => {
+        const selectedAccount = $icrcAccountsStore[universeId.toText()].accounts?.find(
+          ({ identifier }) => identifier === accountIdentifier
+        );
+
+        return nonNullish(selectedAccount)
+          ? {
+              ...selectedAccount,
+              balanceE8s: balance,
+            } as Account
+          : undefined;
+      })
+      .filter(nonNullish);
+
+    console.log(accounts)
+
+    icrcAccountsStore.update({
+      accounts: {
+        accounts,
+        certified: $icrcAccountsStore[universeId.toText()].certified,
+      },
+      universeId,
+    });
+
+    reload?.();
+  };
+
+  $: console.log('HERRE', $icrcAccountsStore)
+
+  let data: BalancesObserverData;
+  $: data = {
+    accounts,
+    ledgerCanisterId: universeId.toText(),
+  };
+</script>
+
+{#if nonNullish(data)}
+  <IcrcBalancesObserver {data} {callback}>
+    <slot />
+  </IcrcBalancesObserver>
+{/if}
