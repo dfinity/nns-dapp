c60ad98fd36d079bb9d6f0db0fb6e8e5eb61ca5e
David Dal Busco <david.dalbusco@dfinity.org>
2023-07-05 11:52:51 +0200

    docs: improve comment

--- frontend/src/lib/pages/CkBTCWallet.svelte
+++ frontend/src/lib/pages/CkBTCWallet.svelte
@@ -70,7 +70,9 @@
 
   const reloadOnlyAccountFromStore = () => setSelectedAccount();
 
-  // We omit await. The UI displays skeletons while loading and the user can proceed with other operations during this time.
+  // transactions?.reloadTransactions() returns a promise.
+  // However, the UI displays skeletons while loading and the user can proceed with other operations during this time.
+  // That is why we do not need to wait for the promise to resolve here.
   const reloadTransactions = () => transactions?.reloadTransactions();
 
   setContext<CkBTCWalletContext>(WALLET_CONTEXT_KEY, {
