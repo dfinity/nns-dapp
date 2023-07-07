ce76124f760bc236f8270e1acf1f36e2eceabec0
Max <bitdivine@users.noreply.github.com>
2023-07-07 14:04:37 +0200

    Make CI more robust (#2837)
    # Motivation
    CI has failed several times over the last day or so. Let's address some
    of the issues.
    
    - A pipefail when getting the first entry from `dfx canister url`.
    - A failure when rustfmt self-updates in a pipe.
    
    # Changes
    - Remove the failing pipe by getting the first line using bash. This is
    also nicer as it doesn't have to spin up another process.
    - Call rustfmt on its own before use. This costs a process call but
    makes rustfmt self-update in isolation before use.
    
    # Tests
    See CI.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    
    Co-authored-by: Max Murphy-Skvorzov <max@winning.black>

--- scripts/did2rs.sh
+++ scripts/did2rs.sh
@@ -43,6 +43,12 @@ DID_PATH="${GIT_ROOT}/declarations/${CANISTER_NAME}/${CANISTER_NAME}.did"
 
 cd "$GIT_ROOT"
 
+: "Ensure that tools are installed and working.  Rustfmt in particular can self-upgrade when called and the self-upgrade can fail."
+{
+  didc --version
+  rustfmt --version
+} >/dev/null
+
 ##########################
 # Translate candid to Rust
 ##########################
