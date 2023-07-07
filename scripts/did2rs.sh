1ec28185f387b3ecc63b1fe25b8974a60023e5df
Max Murphy-Skvorzov <max@winning.black>
2023-07-07 05:01:23 +0200

    Namespace Principal automatically

--- scripts/did2rs.sh
+++ scripts/did2rs.sh
@@ -93,7 +93,8 @@ cd "$GIT_ROOT"
             s/([{( ]Deserialize)([,})])/\1, Serialize, Clone, Debug\2/;
             s/^  [a-z].*:/  pub&/;s/^( *pub ) *pub /\1/;
 	    /impl SERVICE/,${s/-> Result/-> CallResult/g};
-	    /^pub struct /,/^}/{s/ *\{\},$/(EmptyRecord),/g}
+	    /^pub struct /,/^}/{s/ *\{\},$/(EmptyRecord),/g};
+	    s/\<Principal\>/candid::&/g;
 	    '
 } >"${RUST_PATH}"
 if test -f "${PATCH_PATH}"; then
