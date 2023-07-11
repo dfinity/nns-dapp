8224118fb68b8a90733501775cd5e3d42d27d5a9
Llorenç <llorenc.muntaner@dfinity.org>
2023-07-11 13:16:05 +0200

    GIX-1683: UniversePageSummary Component

--- frontend/src/lib/components/universe/UniverseLogo.svelte
+++ frontend/src/lib/components/universe/UniverseLogo.svelte
@@ -14,6 +14,7 @@
   export let universe: Universe;
   export let size: "big" | "small" = "small";
   export let framed = false;
+  export let horizontalPadding = true;
 
   let summary: SnsSummary | undefined;
   $: summary = universe.summary;
@@ -36,7 +37,7 @@
   $: title = universeLogoAlt(universe);
 </script>
 
-<div class={`${size}`} data-tid="project-logo">
+<div class={`${size}`} class:horizontalPadding data-tid="project-logo">
   <Logo src={logo} alt={title} {size} {framed} testId="logo" />
 </div>
 
@@ -48,7 +49,7 @@
     width: fit-content;
   }
 
-  .small {
+  .small.horizontalPadding {
     padding: 0 var(--padding);
   }
 </style>
