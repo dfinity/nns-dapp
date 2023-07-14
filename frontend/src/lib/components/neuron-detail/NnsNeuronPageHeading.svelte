f99b0fdc51b103347fa14511a745623f5ec322e1
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-07-14 10:06:27 +0200

    GIX-1683: SnsNeuronPageHeading & PageHeading (#2878)
    # Motivation
    
    We are reshuffling how the data is displayed in the neuron detail page.
    
    In this PR: Introduce the new heading component for the SNS neuron
    detail page.
    
    # Changes
    
    * New component in common `PageHeading`.
    * `NnsNeuronPageHeader` uses the new `PageHeading`.
    * New component `SnsNeuronPageHeading` that also uses `PageHeading`.
    * Add a wrapper that sets spacing for the new components of
    SnsNeuronDetailPage and render the new `SnsNeuronPageHeading` and a
    separator.
    
    # Tests
    
    * Spec file for new component `SnsNeuronPageHeading`.
    
    # Todos
    
    Changes not worth still a changelog entry.

--- frontend/src/lib/components/neuron-detail/NnsNeuronPageHeading.svelte
+++ frontend/src/lib/components/neuron-detail/NnsNeuronPageHeading.svelte
@@ -5,6 +5,7 @@
   import { formatVotingPower, neuronStake } from "$lib/utils/neuron.utils";
   import { replacePlaceholders } from "$lib/utils/i18n.utils";
   import { i18n } from "$lib/stores/i18n";
+  import PageHeading from "../common/PageHeading.svelte";
 
   export let neuron: NeuronInfo;
 
@@ -15,28 +16,11 @@
   });
 </script>
 
-<div class="container" data-tid="nns-neuron-page-heading-component">
-  <AmountDisplay {amount} size="huge" singleLine />
-  <h3 class="description" data-tid="voting-power-subtitle">
+<PageHeading testId="nns-neuron-page-heading-component">
+  <AmountDisplay slot="title" {amount} size="huge" singleLine />
+  <svelte:fragment slot="subtitle">
     {replacePlaceholders($i18n.neuron_detail.voting_power_subtitle, {
       $votingPower: formatVotingPower(neuron.votingPower),
     })}
-  </h3>
-</div>
-
-<style lang="scss">
-  .container {
-    display: flex;
-    flex-direction: column;
-    gap: var(--padding-1_5x);
-    justify-content: center;
-    align-items: center;
-
-    width: 100%;
-
-    h3 {
-      margin: 0;
-      font-weight: normal;
-    }
-  }
-</style>
+  </svelte:fragment>
+</PageHeading>
