afcf68a958506a28dd7946cf64cfcc6580f76df1
Llorenç <llorenc.muntaner@dfinity.org>
2023-07-14 10:53:43 +0200

    GIX-1684: Fix test

--- frontend/src/tests/lib/components/neuron-detail/actions/NnsIncreaseStakeButton.spec.ts
+++ frontend/src/tests/lib/components/neuron-detail/actions/NnsIncreaseStakeButton.spec.ts
@@ -29,16 +29,21 @@ describe("NnsIncreaseStakeButton", () => {
 
   it("uses variant", () => {
     const variant = "secondary";
-    const { container } = render(NeuronContextTest, {
+    const { queryByTestId } = render(NeuronContextTest, {
       props: {
         neuron: mockNeuron,
         testComponent: NnsIncreaseStakeButton,
+        componentProps: {
+          variant,
+        },
       },
     });
 
-    expect(container.querySelector("button").classList.contains(variant)).toBe(
-      true
-    );
+    expect(
+      queryByTestId("nns-increase-stake-button-component").classList.contains(
+        variant
+      )
+    ).toBe(true);
   });
 
   it("opens Increase Neuron Stake Modal", async () => {
