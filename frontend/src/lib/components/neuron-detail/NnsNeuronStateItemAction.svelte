187cdbfe97ba91dc417ccde47e468d2e370449b9
Llorenç <llorenc.muntaner@dfinity.org>
2023-07-17 16:05:25 +0200

    GIX-1684: Add tests

--- frontend/src/lib/components/neuron-detail/NnsNeuronStateItemAction.svelte
+++ frontend/src/lib/components/neuron-detail/NnsNeuronStateItemAction.svelte
@@ -2,45 +2,61 @@
   import { ItemAction } from "@dfinity/gix-components";
   import { NeuronState, type NeuronInfo } from "@dfinity/nns";
   import {
-    getAgeBonusText,
+    ageMultiplier,
     getStateInfo,
+    neuronAgeBonus,
     type StateInfo,
   } from "$lib/utils/neuron.utils";
   import { i18n } from "$lib/stores/i18n";
   import { keyOf } from "$lib/utils/utils";
   import DisburseButton from "./actions/DisburseButton.svelte";
   import DissolveActionButton from "./actions/DissolveActionButton.svelte";
+  import Tooltip from "../ui/Tooltip.svelte";
+  import { replacePlaceholders } from "$lib/utils/i18n.utils";
 
   export let neuron: NeuronInfo;
 
   let stateInfo: StateInfo | undefined;
   $: stateInfo = getStateInfo(neuron.state);
 
-  let ageBonusText: string;
-  $: ageBonusText = getAgeBonusText({ neuron, i18n: $i18n });
+  let ageBonus: number;
+  $: ageBonus = ageMultiplier(neuron.ageSeconds);
 </script>
 
-{#if stateInfo !== undefined}
-  <ItemAction testId="nns-stake-item-action-component">
-    <div slot="icon" class="icon">
-      <!-- TODO: Use new icons -->
-      <svelte:component this={stateInfo.Icon} />
-    </div>
-    <div class="content">
-      <p class="icp-value">
-        {keyOf({ obj: $i18n.neuron_state, key: NeuronState[neuron.state] })}
+<ItemAction testId="nns-neuron-state-item-action-component">
+  <svelte:fragment slot="icon">
+    {#if stateInfo !== undefined}
+      <div class="icon">
+        <svelte:component this={stateInfo.Icon} />
+      </div>
+    {/if}
+  </svelte:fragment>
+  <div class="content">
+    <h4 class="icp-value" data-tid="state-text">
+      {keyOf({ obj: $i18n.neuron_state, key: NeuronState[neuron.state] })}
+    </h4>
+    {#if neuron.state === NeuronState.Locked}
+      <Tooltip id="neuron-age-bonus" text={ageBonus.toFixed(8)}>
+        <p class="description" data-tid="age-bonus-text">
+          {replacePlaceholders($i18n.neuron_detail.age_bonus_label, {
+            $ageBonus: ageBonus.toFixed(2),
+          })}
+        </p>
+      </Tooltip>
+    {:else}
+      <p class="description" data-tid="age-bonus-text">
+        {$i18n.neuron_detail.no_age_bonus}
       </p>
-      <p class="description">{ageBonusText}</p>
-    </div>
-    <svelte:fragment slot="actions">
-      {#if neuron.state === NeuronState.Dissolved}
-        <DisburseButton />
-      {:else}
-        <DissolveActionButton neuronState={neuron.state} />
-      {/if}
-    </svelte:fragment>
-  </ItemAction>
-{/if}
+    {/if}
+  </div>
+  <svelte:fragment slot="actions">
+    {#if neuron.state === NeuronState.Dissolved}
+      <DisburseButton />
+    {:else}
+      <DissolveActionButton neuronState={neuron.state} />
+    {/if}
+  </svelte:fragment>
+</ItemAction>
 
 <style lang="scss">
   @use "@dfinity/gix-components/dist/styles/mixins/fonts";
@@ -62,7 +78,8 @@
     flex-direction: column;
     gap: var(--padding);
 
-    p {
+    p,
+    h4 {
       margin: 0;
     }
   }
