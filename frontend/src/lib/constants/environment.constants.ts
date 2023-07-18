4eb1af5472c17cf8c7431a319fb9be8978c53cf9
David Dal Busco <david.dalbusco@dfinity.org>
2023-07-18 12:09:31 +0200

    Add a flag for ICP Icrc (#2898)
    # Motivation
    
    Add a flag `ENABLE_ICP_ICRC` to the configuration which can be use to
    activate the support for Icrc in the ICP / Nns universe.
    
    Flag is per default set to `false` everywhere.

--- frontend/src/lib/constants/environment.constants.ts
+++ frontend/src/lib/constants/environment.constants.ts
@@ -42,6 +42,7 @@ export interface FeatureFlags<T> {
   ENABLE_SNS_AGGREGATOR: T;
   ENABLE_CKBTC: T;
   ENABLE_CKTESTBTC: T;
+  ENABLE_ICP_ICRC: T;
   ENABLE_SIMULATE_MERGE_NEURONS: T;
   ENABLE_NEURON_SETTINGS: T;
   ENABLE_INSTANT_UNLOCK: T;
@@ -59,7 +60,7 @@ export type FeatureKey = keyof FeatureFlags<boolean>;
  */
 export const FEATURE_FLAG_ENVIRONMENT: FeatureFlags<boolean> = JSON.parse(
   envVars?.featureFlags ??
-    '{"ENABLE_SNS_VOTING": true, "ENABLE_SNS_AGGREGATOR": true, "ENABLE_CKBTC": true, "ENABLE_CKTESTBTC": false, "ENABLE_SIMULATE_MERGE_NEURONS": false, "ENABLE_NEURON_SETTINGS": false, "ENABLE_INSTANT_UNLOCK": false}'
+    '{"ENABLE_SNS_VOTING": true, "ENABLE_SNS_AGGREGATOR": true, "ENABLE_CKBTC": true, "ENABLE_CKTESTBTC": false, "ENABLE_ICP_ICRC": false, "ENABLE_SIMULATE_MERGE_NEURONS": false, "ENABLE_NEURON_SETTINGS": false, "ENABLE_INSTANT_UNLOCK": false}'
 );
 
 export const IS_TESTNET: boolean =
