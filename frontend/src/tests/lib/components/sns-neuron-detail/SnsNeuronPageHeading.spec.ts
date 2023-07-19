4a7992bef2ec92fa8c8237204178cf613261dc2e
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-07-19 14:35:34 +0200

    Fix: Sns Voting Power in PageHeading (#2923)
    # Motivation
    
    The voting power was incorrect in the new SNS neuron page heading.
    
    # Changes
    
    * Use util to formatVotingPower in the SnsPageHeading
    
    # Tests
    
    * Add a test to check the value of the voting power in the
    SnsPageHeading.
    
    # Todos
    
    Not worth an entry yet.

--- frontend/src/tests/lib/components/sns-neuron-detail/SnsNeuronPageHeading.spec.ts
+++ frontend/src/tests/lib/components/sns-neuron-detail/SnsNeuronPageHeading.spec.ts
@@ -3,13 +3,16 @@
  */
 
 import SnsNeuronPageHeading from "$lib/components/sns-neuron-detail/SnsNeuronPageHeading.svelte";
+import { SECONDS_IN_EIGHT_YEARS } from "$lib/constants/constants";
 import { renderSelectedSnsNeuronContext } from "$tests/mocks/context-wrapper.mock";
 import {
+  createMockSnsNeuron,
   mockSnsNeuron,
   snsNervousSystemParametersMock,
 } from "$tests/mocks/sns-neurons.mock";
 import { SnsNeuronPageHeadingPo } from "$tests/page-objects/SnsNeuronPageHeading.page-object";
 import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
+import { NeuronState } from "@dfinity/nns";
 import type { SnsNeuron } from "@dfinity/sns";
 
 describe("SnsNeuronPageHeading", () => {
@@ -34,5 +37,19 @@ describe("SnsNeuronPageHeading", () => {
     });
 
     expect(await po.getStake()).toEqual("3.14");
+  });
+
+  it("should render the neuron's voting power", async () => {
+    const stake = 314_000_000n;
+    const neuron = createMockSnsNeuron({
+      id: [1],
+      state: NeuronState.Locked,
+      dissolveDelaySeconds: BigInt(SECONDS_IN_EIGHT_YEARS),
+      stakedMaturity: 100_000_000n,
+      stake,
+    });
+    const po = renderSnsNeuronCmp(neuron);
+
+    expect(await po.getVotingPower()).toEqual("Voting Power: 5.59");
   });
 });
