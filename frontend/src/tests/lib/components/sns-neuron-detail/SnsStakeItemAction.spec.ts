778b848c756664a3ed1e0cd9871bb3df51775f56
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-07-20 14:24:36 +0200

    GIX-1684: SnsNeuronVotingPowerSection (#2928)
    # Motivation
    
    We are reshuffling the data in the neuron detail page.
    
    In this PR: Setup voting power section for sns neuron detail page
    
    # Changes
    
    * Implement SnsNeuronVotingPowerSection.
    * New component SnsStakeItemAction.
    * Add "variant" prop to SnsIncreaseStakeButton.
    * Add Separator between Maturity and Voting Power sections in
    SnsNeutonDetail.
    
    # Tests
    
    * Test file and PO for SnsNeuronVotingPowerSection.
    * Test file and PO for SnsStakeItemAction
    
    # Todos
    
    Not yet worth an entry.

---
+++ frontend/src/tests/lib/components/sns-neuron-detail/SnsStakeItemAction.spec.ts
@@ -0,0 +1,45 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import SnsStakeItemAction from "$lib/components/sns-neuron-detail/SnsStakeItemAction.svelte";
+import {
+  createMockSnsNeuron,
+  mockSnsNeuron,
+} from "$tests/mocks/sns-neurons.mock";
+import { mockToken, mockUniverse } from "$tests/mocks/sns-projects.mock";
+import { SnsStakeItemActionPo } from "$tests/page-objects/SnsStakeItemAction.page-object";
+import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
+import type { SnsNeuron } from "@dfinity/sns";
+import { render } from "@testing-library/svelte";
+
+describe("SnsStakeItemAction", () => {
+  const renderComponent = (neuron: SnsNeuron) => {
+    const { container } = render(SnsStakeItemAction, {
+      props: {
+        neuron,
+        universe: mockUniverse,
+        token: mockToken,
+      },
+    });
+
+    return SnsStakeItemActionPo.under(new JestPageObjectElement(container));
+  };
+
+  it("should render Stake of the neuron", async () => {
+    const stake = 314000000n;
+    const neuron = createMockSnsNeuron({
+      id: [1],
+      stake,
+    });
+    const po = renderComponent(neuron);
+
+    expect(await po.getStake()).toBe("3.14");
+  });
+
+  it("should render increase stake button", async () => {
+    const po = renderComponent(mockSnsNeuron);
+
+    expect(await po.hasIncreaseStakeButton()).toBe(true);
+  });
+});
