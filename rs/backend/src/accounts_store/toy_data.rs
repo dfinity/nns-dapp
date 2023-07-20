36d26dd2c36903c3bcb3020e21c860fe779df50e
Max Murphy-Skvorzov <max@winning.black>
2023-07-20 17:03:16 +0200

    Vary the number of sub-accounts

--- rs/backend/src/accounts_store/toy_data.rs
+++ rs/backend/src/accounts_store/toy_data.rs
@@ -1,17 +1,23 @@
 //! Test data for unit tests and test networks.
 
-use crate::accounts_store::{AccountsStore, PrincipalId};
+use crate::accounts_store::{AccountsStore, PrincipalId, RegisterHardwareWalletRequest};
 
 
 impl AccountsStore {
     pub fn create_toy_accounts(&mut self, num_accounts: u64) {
-        const SUB_ACCOUNTS_PER_ACCOUNT: u64 = 3;
+        const MAX_SUB_ACCOUNTS_PER_ACCOUNT: u64 = 3;
+        const MAX_HARDWARE_WALLETS_PER_ACCOUNT: u64 = 1;
         let num_existing_accounts = self.accounts.len() as u64;
-        for i in num_existing_accounts..(num_existing_accounts + num_accounts) {
-            let account = PrincipalId::new_user_test_id(i);
+        for toy_account_index in num_existing_accounts..(num_existing_accounts + num_accounts) {
+            let account = PrincipalId::new_user_test_id(toy_account_index);
             self.add_account(account);
-            for i in 0..SUB_ACCOUNTS_PER_ACCOUNT {
-                self.create_sub_account(account, format!("sub_account_{}", i));
+            // Successive accounts have 0, 1, 2 ... SUB_ACCOUNTS_PER_ACCOUNT-1 sub accounts, restarting at 0.
+            for subaccount_index in 0..(toy_account_index % MAX_SUB_ACCOUNTS_PER_ACCOUNT) {
+                self.create_sub_account(account, format!("sub_account_{toy_account_index}_{subaccount_index}"));
+            }
+            // Successive accounts have 0, 1, 2 ... HARDWARE_WALLETS_PER_ACCOUNT-1 hardware wallets, restarting at 0.
+            for hardware_wallet_index in 0..(toy_account_index % MAX_HARDWARE_WALLETS_PER_ACCOUNT) {
+                self.register_hardware_wallet(account, RegisterHardwareWalletRequest{name: format!("hw_wallet_{toy_account_index}_{hardware_wallet_index}"), principal: account});
             }
         }
     }
