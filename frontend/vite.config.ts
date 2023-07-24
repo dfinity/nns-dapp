e0b11df220fcdd957431ba0e5f96e07b15d2f3ab
David Dal Busco <david.dalbusco@dfinity.org>
2023-07-24 11:26:27 +0200

    build: disable source map

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,7 +13,7 @@ const config: UserConfig = {
   plugins: [sveltekit()],
   build: {
     target: "es2020",
-    sourcemap: "hidden",
+    sourcemap: false,
     rollupOptions: {
       output: {
         manualChunks: (id) => {
