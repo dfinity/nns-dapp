1d4c131e54cf67b0be99947d6c67d6a7efc31785
David Dal Busco <david.dalbusco@dfinity.org>
2023-07-25 12:51:03 +0200

    fix: proposal back to referrer

--- frontend/src/lib/components/layout/LayoutNavGuard.svelte
+++ frontend/src/lib/components/layout/LayoutNavGuard.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
   import { isNullish } from "@dfinity/utils";
   import { navigating } from "$app/stores";
+  import { Spinner } from "@dfinity/gix-components";
+
+  // To be used only when the transition between pages lasts long; otherwise, it can appear as being glitchy.
+  export let spinner = false;
 </script>
 
 <!-- Workaround for SvelteKit issue https://github.com/sveltejs/kit/issues/5434 -->
 {#if isNullish($navigating)}
   <slot />
+{:else if spinner}
+  <Spinner />
 {/if}
