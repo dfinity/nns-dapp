4f872a93e42e3a87393247c946cc7d85da931154
David Dal Busco <david.dalbusco@dfinity.org>
2023-07-25 14:25:31 +0200

    feat: do not display proposals title on wide screen

--- frontend/src/routes/(app)/(u)/(list)/proposals/+layout.svelte
+++ frontend/src/routes/(app)/(u)/(list)/proposals/+layout.svelte
@@ -4,13 +4,17 @@
   import UniverseSplitContent from "$lib/components/layout/UniverseSplitContent.svelte";
   import type { SvelteComponent } from "svelte";
   import { ENABLE_SNS_VOTING } from "$lib/stores/feature-flags.store";
+  import { i18n } from "$lib/stores/i18n";
+  import LayoutList from "$lib/components/layout/LayoutList.svelte";
 
   let cmp: typeof SvelteComponent;
   $: cmp = $ENABLE_SNS_VOTING ? UniverseSplitContent : Content;
 </script>
 
-<Layout>
-  <svelte:component this={cmp}>
-    <slot />
-  </svelte:component>
-</Layout>
+<LayoutList title={$i18n.navigation.voting}>
+  <Layout>
+    <svelte:component this={cmp}>
+      <slot />
+    </svelte:component>
+  </Layout>
+</LayoutList>
