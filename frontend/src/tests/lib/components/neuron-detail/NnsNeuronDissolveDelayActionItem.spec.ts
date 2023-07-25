266527c39a8da6b677b9ae6d357730165c0a0246
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-07-25 11:52:06 +0200

    GIX-1765: Manage permissions in NNS neuron buttons (#2959)
    # Motivation
    
    Some neuron actions are available only for controllers.
    
    In this PR: Apply same rules to show/hide neuron buttons in new NNS
    neuron detail.
    
    Except one difference. The Join Neuron's fund is rendered but disabled
    if neuron is not controlled by user. This is similar to what we do for
    the auto stake checkbox. Currently, the whole card of Neurons' Fund
    doesn't appear if neuron is not controlled by the user.
    
    # Changes
    
    * Hide actions if user not controller in NnsAvailableMaturityActionItem.
    * Hide split button and disable join neurons' fund checkbox if user not
    controller in NnsNeuronAdvancedSection.
    * Hide actions if user not controller in
    NnsNeuronDissolveDelayActionItem.
    * Hide actions if user not controller in NnsNeuronStateItemAction.
    * Add `disabled` prop to JoinCommunityFundCheckbox
    
    # Tests
    
    * Test that buttons are hidden when user not controller in
    NnsAvailableMaturityActionItem.
    * Test that split button is hidden and join neurons' fund checkbox is
    disabled when user not controller in NnsNeuronAdvancedSection.
    * Test that buttons are hidden when user not controller in
    NnsNeuronDissolveDelayActionItem.
    * Test that buttons are hidden when user not controller in
    NnsNeuronStateItemAction.
    * Test new `disabled` prop in JoinCommunityFundCheckbox
    
    # Todos
    
    This is the current functionality, applied to the new UI. It doesn't
    deserver an entry in the changelog.

--- frontend/src/tests/lib/components/neuron-detail/NnsNeuronDissolveDelayActionItem.spec.ts
+++ frontend/src/tests/lib/components/neuron-detail/NnsNeuronDissolveDelayActionItem.spec.ts
@@ -4,6 +4,16 @@
 
 import NnsNeuronDissolveDelayActionItem from "$lib/components/neuron-detail/NnsNeuronDissolveDelayActionItem.svelte";
 import { SECONDS_IN_MONTH, SECONDS_IN_YEAR } from "$lib/constants/constants";
+import { authStore } from "$lib/stores/auth.store";
+import { icpAccountsStore } from "$lib/stores/icp-accounts.store";
+import {
+  mockAuthStoreSubscribe,
+  mockIdentity,
+} from "$tests/mocks/auth.store.mock";
+import {
+  mockHardwareWalletAccount,
+  mockMainAccount,
+} from "$tests/mocks/icp-accounts.store.mock";
 import { mockNeuron } from "$tests/mocks/neurons.mock";
 import { NnsNeuronDissolveDelayActionItemPo } from "$tests/page-objects/NnsNeuronDissolveDelayActionItem.page-object";
 import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
@@ -25,9 +35,24 @@ describe("NnsNeuronDissolveDelayActionItem", () => {
     );
   };
 
+  const controlledNeuron = {
+    ...mockNeuron,
+    fullNeuron: {
+      ...mockNeuron.fullNeuron,
+      controller: mockIdentity.getPrincipal().toText(),
+    },
+  };
+
+  beforeEach(() => {
+    icpAccountsStore.resetForTesting();
+    jest
+      .spyOn(authStore, "subscribe")
+      .mockImplementation(mockAuthStoreSubscribe);
+  });
+
   it("should render dissolve delay text and bonus if neuron is locked", async () => {
     const neuron: NeuronInfo = {
-      ...mockNeuron,
+      ...controlledNeuron,
       state: NeuronState.Locked,
       dissolveDelaySeconds: BigInt(SECONDS_IN_YEAR * 2),
     };
@@ -42,11 +67,11 @@ describe("NnsNeuronDissolveDelayActionItem", () => {
 
   it("should render remaining text and bonus if neuron is dissolving", async () => {
     const neuron: NeuronInfo = {
-      ...mockNeuron,
+      ...controlledNeuron,
       state: NeuronState.Dissolving,
       dissolveDelaySeconds: BigInt(SECONDS_IN_YEAR * 2),
       fullNeuron: {
-        ...mockNeuron.fullNeuron,
+        ...controlledNeuron.fullNeuron,
         dissolveState: {
           DissolveDelaySeconds: BigInt(SECONDS_IN_YEAR * 2),
         },
@@ -61,7 +86,7 @@ describe("NnsNeuronDissolveDelayActionItem", () => {
 
   it("should render no bonus text if neuron is dissolving less than 6 months", async () => {
     const neuron: NeuronInfo = {
-      ...mockNeuron,
+      ...controlledNeuron,
       state: NeuronState.Dissolving,
       dissolveDelaySeconds: BigInt(SECONDS_IN_MONTH * 4),
     };
@@ -72,7 +97,7 @@ describe("NnsNeuronDissolveDelayActionItem", () => {
 
   it("should render dissolve delay text with 0 and no bonus text if neuron is unlocked", async () => {
     const neuron: NeuronInfo = {
-      ...mockNeuron,
+      ...controlledNeuron,
       state: NeuronState.Dissolved,
       dissolveDelaySeconds: BigInt(0),
     };
@@ -80,6 +105,41 @@ describe("NnsNeuronDissolveDelayActionItem", () => {
 
     expect(await po.getDissolveState()).toBe("Dissolve Delay: 0");
     expect(await po.getDissolveBonus()).toBe("No dissolve delay bonus");
+    expect(await po.hasIncreaseDissolveDelayButton()).toBe(true);
+  });
+
+  it("should not render increase dissolve delay button if user is not the controller", async () => {
+    const neuron: NeuronInfo = {
+      ...mockNeuron,
+      state: NeuronState.Dissolved,
+      dissolveDelaySeconds: BigInt(0),
+      fullNeuron: {
+        ...mockNeuron.fullNeuron,
+        controller: "not-controller",
+      },
+    };
+    const po = renderComponent(neuron);
+
+    expect(await po.hasIncreaseDissolveDelayButton()).toBe(false);
+  });
+
+  it("should render increase dissolve delay button if controlled by hardware wallet", async () => {
+    icpAccountsStore.setForTesting({
+      main: mockMainAccount,
+      subAccounts: [],
+      hardwareWallets: [mockHardwareWalletAccount],
+    });
+    const neuron: NeuronInfo = {
+      ...mockNeuron,
+      state: NeuronState.Dissolved,
+      dissolveDelaySeconds: BigInt(0),
+      fullNeuron: {
+        ...mockNeuron.fullNeuron,
+        controller: mockHardwareWalletAccount.principal.toText(),
+      },
+    };
+    const po = renderComponent(neuron);
+
     expect(await po.hasIncreaseDissolveDelayButton()).toBe(true);
   });
 });
