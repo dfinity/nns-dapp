8367adf797583b01fb234e19dec9d2defe474d37
David Dal Busco <david.dalbusco@dfinity.org>
2023-07-25 15:30:01 +0200

    Do not display proposals title on wide screens (#2965)
    # Motivation
    
    Do not display the "Vote on Proposals" title in the page's header on
    wide screens to align the behavior with pages that support multiple
    projects.
    
    UI consistency.
    
    # Changes
    
    - refactor existing code in a new component `LayoutList` to avoid
    duplicate code and render a title according screen width
    - use new component in list layouts including in proposals

--- frontend/src/routes/(app)/(u)/(list)/accounts/+page.svelte
+++ frontend/src/routes/(app)/(u)/(list)/accounts/+page.svelte
@@ -1,22 +1,8 @@
 <script lang="ts">
   import SignInAccounts from "$lib/pages/SignInAccounts.svelte";
-  import { layoutTitleStore } from "$lib/stores/layout.store";
-  import { i18n } from "$lib/stores/i18n";
-  import { BREAKPOINT_LARGE } from "@dfinity/gix-components";
   import Accounts from "$lib/routes/Accounts.svelte";
   import { authSignedInStore } from "$lib/derived/auth.derived";
-
-  let innerWidth = 0;
-
-  let list = false;
-  $: list = innerWidth > BREAKPOINT_LARGE;
-
-  $: (() => {
-    layoutTitleStore.set(list ? "" : $i18n.navigation.tokens);
-  })();
 </script>
-
-<svelte:window bind:innerWidth />
 
 {#if $authSignedInStore}
   <Accounts />
--- frontend/src/routes/(app)/(u)/(list)/proposals/+layout.svelte
+++ frontend/src/routes/(app)/(u)/(list)/proposals/+layout.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
   import Layout from "$lib/components/layout/Layout.svelte";
   import UniverseSplitContent from "$lib/components/layout/UniverseSplitContent.svelte";
+  import { i18n } from "$lib/stores/i18n";
+  import LayoutList from "$lib/components/layout/LayoutList.svelte";
 </script>
 
-<Layout>
-  <UniverseSplitContent>
-    <slot />
-  </UniverseSplitContent>
-</Layout>
+<LayoutList title={$i18n.navigation.voting}>
+  <Layout>
+    <UniverseSplitContent>
+      <slot />
+    </UniverseSplitContent>
+  </Layout>
+</LayoutList>
