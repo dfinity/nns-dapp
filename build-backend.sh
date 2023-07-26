a4ea0c012f995a53b5f62d98f44165b894f63bd6
Max Murphy-Skvorzov <max@winning.black>
2023-07-26 13:44:03 +0200

    ++

--- build-backend.sh
+++ build-backend.sh
@@ -9,6 +9,5 @@ TOPLEVEL="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo Compiling rust package
 "$TOPLEVEL/build-rs.sh" nns-dapp
 
-# Note: We will need to find a way of dealing with feature flags that add methods.
-# echo Sanity check
-# scripts/nns-dapp/test-exports
+echo Sanity check
+scripts/nns-dapp/test-exports
