fbba7af06f26fc38aaf9e85a924c0daf99490d08
David Dal Busco <david.dalbusco@dfinity.org>
2023-07-28 22:32:53 +0200

    fix: back to accounts after transaction in wallet page

--- frontend/src/lib/components/layout/LayoutNavGuard.svelte
+++ frontend/src/lib/components/layout/LayoutNavGuard.svelte
@@ -1,15 +1,9 @@
 <script lang="ts">
   import { isNullish } from "@dfinity/utils";
   import { navigating } from "$app/stores";
-  import { Spinner } from "@dfinity/gix-components";
-
-  // To be used only when the transition between pages lasts long; otherwise, it can appear as being glitchy.
-  export let spinnerWhileNavigating = false;
 </script>
 
 <!-- Workaround for SvelteKit issue https://github.com/sveltejs/kit/issues/5434 -->
 {#if isNullish($navigating)}
   <slot />
-{:else if spinnerWhileNavigating}
-  <Spinner />
 {/if}
