7b7b5ce26f07781408c47c32355d065b5e4c4fd6
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-08-02 17:02:55 +0200

    chore: imports

--- frontend/src/lib/routes/Proposals.svelte
+++ frontend/src/lib/routes/Proposals.svelte
@@ -3,7 +3,6 @@
   import { isNnsUniverseStore } from "$lib/derived/selected-universe.derived";
   import SnsProposals from "$lib/pages/SnsProposals.svelte";
   import SummaryUniverse from "$lib/components/summary/SummaryUniverse.svelte";
-  import type { AppPath } from "$lib/constants/routes.constants";
   import { snsProjectSelectedStore } from "$lib/derived/sns/sns-selected-project.derived";
   import { nonNullish } from "@dfinity/utils";
 </script>
