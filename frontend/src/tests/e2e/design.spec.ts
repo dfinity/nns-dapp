06f3f7f4fa622afd51a6e8c660fa8d9766b1e8cb
David Dal Busco <david.dalbusco@dfinity.org>
2023-08-02 14:06:53 +0200

    test: screenshot accounts

--- frontend/src/tests/e2e/design.spec.ts
+++ frontend/src/tests/e2e/design.spec.ts
@@ -1,16 +1,41 @@
-import {expect, test} from "@playwright/test";
-import {PlaywrightPageObjectElement} from "$tests/page-objects/playwright.page-object";
-import {AppPo} from "$tests/page-objects/App.page-object";
+import { AppPo } from "$tests/page-objects/App.page-object";
+import { PlaywrightPageObjectElement } from "$tests/page-objects/playwright.page-object";
+import { signInWithNewUser } from "$tests/utils/e2e.test-utils";
+import { expect, test, type Page } from "@playwright/test";
 
 test.describe("Design", () => {
-  test("Login", async ({ page }) => {
-    await page.goto("/");
-
+  const waitForSignIn = async (page: Page) => {
     const pageElement = PlaywrightPageObjectElement.fromPage(page);
     const appPo = new AppPo(pageElement);
 
     await appPo.getSignInPo().waitFor();
+  };
+
+  test("Login", async ({ page }) => {
+    await page.goto("/");
+    await expect(page).toHaveTitle("NNS Dapp");
+
+    await waitForSignIn(page);
 
     await expect(page).toHaveScreenshot();
+  });
+
+  test("My Tokens", async ({ page, context }) => {
+    await page.goto("/");
+    await expect(page).toHaveTitle("NNS Dapp");
+
+    await waitForSignIn(page);
+
+    await signInWithNewUser({ page, context });
+
+    const pageElement = PlaywrightPageObjectElement.fromPage(page);
+    const appPo = new AppPo(pageElement);
+
+    await appPo.getAccountsPo().waitFor();
+    await appPo.getAccountsPo().getNnsAccountsPo().waitForContentLoaded();
+
+    await expect(page).toHaveScreenshot({
+      mask: [page.locator('[data-tid="identifier"]')],
+    });
   });
 });
