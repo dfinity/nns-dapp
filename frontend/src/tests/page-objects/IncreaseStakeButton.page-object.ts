752c2435c4f888f744ea3f6374b38080e31ddc7f
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2023-08-04 12:00:50 +0200

    Unify Sns/Nns StakeItemAction components  (#3032)
    # Motivation
    
    The HMTL in the `NnsStakeItemAction` and `SnsStakeItemAction` components
    is almost identical because these components serve the same purpose and
    we want them to look the same.
    Also their page objects are almost identical.
    
    # Changes
    
    1. Add `IncreaseStakeButton` which is used by both
    `NnsIncreaseStakeButton` and `SnsIncreaseStakeButton`.
    2. Add `StakeItemAction` which is used by both `NnsStakeItemAction` and
    `SnsStakeItemAction`. It uses the generic `IncreaseStakeButton` and
    controls the logic from the parent component.
    3. Add `StakeItemActionPo` to replace both `NnsStakeItemActionPo` and
    `SnsStakeItemActionPo`.
    
    # Tests
    
    Tests were added for `IncreaseStakeButton` and `StakeItemAction`.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    
    not necessary(?)

---
+++ frontend/src/tests/page-objects/IncreaseStakeButton.page-object.ts
@@ -0,0 +1,27 @@
+import { BasePageObject } from "$tests/page-objects/base.page-object";
+import type { PageObjectElement } from "$tests/types/page-object.types";
+
+export class IncreaseStakeButtonPo extends BasePageObject {
+  private static readonly TID = "increase-stake-button-component";
+
+  static under(element: PageObjectElement): IncreaseStakeButtonPo {
+    return new IncreaseStakeButtonPo(
+      element.byTestId(IncreaseStakeButtonPo.TID)
+    );
+  }
+
+  async getVariant(): Promise<string> {
+    const allowedVariants = ["primary", "secondary"];
+    const classes = (await this.root.getClasses()).filter((c) =>
+      allowedVariants.includes(c)
+    );
+    if (classes.length !== 1) {
+      throw new Error(
+        `Expected one of ${allowedVariants.join(", ")} but got ${classes.join(
+          ", "
+        )}`
+      );
+    }
+    return classes[0];
+  }
+}
