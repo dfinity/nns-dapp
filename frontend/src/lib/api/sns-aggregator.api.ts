6c63f97c6d9ce3acc95ed0d5cf739047b4c76c41
Llorenç <llorenc.muntaner@dfinity.org>
2023-08-04 08:35:41 +0200

    Fix: SNS Aggregator page 2 doesn't exist

--- frontend/src/lib/api/sns-aggregator.api.ts
+++ frontend/src/lib/api/sns-aggregator.api.ts
@@ -10,20 +10,31 @@ import { convertDtoData } from "$lib/utils/sns-aggregator-converters.utils";
 const aggergatorPageUrl = (page: number) => `/sns/list/page/${page}/slow.json`;
 
 const querySnsAggregator = async (page = 0): Promise<CachedSnsDto[]> => {
-  const response = await fetch(
-    `${SNS_AGGREGATOR_CANISTER_URL}/${AGGREGATOR_CANISTER_VERSION}${aggergatorPageUrl(
-      page
-    )}`
-  );
-  if (!response.ok) {
-    throw new Error("Error loading SNS projects from aggregator canister");
+  try {
+    const response = await fetch(
+      `${SNS_AGGREGATOR_CANISTER_URL}/${AGGREGATOR_CANISTER_VERSION}${aggergatorPageUrl(
+        page
+      )}`
+    );
+    if (!response.ok) {
+      throw new Error("Error loading SNS projects from aggregator canister");
+    }
+    const data: CachedSnsDto[] = await response.json();
+    if (data.length === AGGREGATOR_PAGE_SIZE) {
+      const nextPageData = await querySnsAggregator(page + 1);
+      return [...data, ...nextPageData];
+    }
+    return data;
+  } catch (e) {
+    // If the error is after the first page, is because there are no more pages it fails
+    if (page > 0) {
+      console.error(
+        `Error loading SNS project page ${page} from aggregator canister`
+      );
+      return [];
+    }
+    throw e;
   }
-  const data: CachedSnsDto[] = await response.json();
-  if (data.length === AGGREGATOR_PAGE_SIZE) {
-    const nextPageData = await querySnsAggregator(page + 1);
-    return [...data, ...nextPageData];
-  }
-  return data;
 };
 
 export const querySnsProjects = async (): Promise<CachedSns[]> => {
--- frontend/src/tests/lib/api/sns-aggregator.api.spec.ts
+++ frontend/src/tests/lib/api/sns-aggregator.api.spec.ts
@@ -50,6 +50,29 @@ describe("sns-aggregator api", () => {
       expect(mockFetch).toHaveBeenCalledTimes(2);
     });
 
+    it("should fetch not fail if second page failes", async () => {
+      jest.spyOn(console, "error").mockImplementation(() => undefined);
+      const mockFetch = jest.fn();
+      mockFetch
+        .mockReturnValueOnce(
+          Promise.resolve({
+            ok: true,
+            json: () => Promise.resolve(tenAggregatedSnses),
+          })
+        )
+        .mockReturnValueOnce(new Error("Second page doesn't exist"));
+      global.fetch = mockFetch;
+      const projects = await querySnsProjects();
+      expect(mockFetch).toHaveBeenCalledWith(
+        `https://5v72r-4aaaa-aaaaa-aabnq-cai.small12.testnet.dfinity.network/v1/sns/list/page/0/slow.json`
+      );
+      expect(mockFetch).toHaveBeenCalledWith(
+        `https://5v72r-4aaaa-aaaaa-aabnq-cai.small12.testnet.dfinity.network/v1/sns/list/page/1/slow.json`
+      );
+      expect(mockFetch).toHaveBeenCalledTimes(2);
+      expect(projects).toHaveLength(10);
+    });
+
     it("should convert response", async () => {
       const mockFetch = jest.fn();
       mockFetch.mockReturnValue(
