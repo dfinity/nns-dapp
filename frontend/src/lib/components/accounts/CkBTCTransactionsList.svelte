0ec6b23e02c542eedf4a92e2c11e7f43d9767188
David Dal Busco <david.dalbusco@dfinity.org>
2023-08-07 10:15:08 +0200

    Expose component props setter and getter (#3000)
    # Motivation
    
    Vitest (PR #2475) requires component's properties exposed as setter and
    getter if props are read within the test which is the case of
    `<CkBTCTransactionsList />`.

--- frontend/src/lib/components/accounts/CkBTCTransactionsList.svelte
+++ frontend/src/lib/components/accounts/CkBTCTransactionsList.svelte
@@ -1,3 +1,6 @@
+<!-- Expose getters and setters to access properties in a testing context -->
+<svelte:options accessors />
+
 <script lang="ts">
   import type { Account } from "$lib/types/account";
   import {
