230dbfa07becb64f399fe7a86d05a032dc1dfec8
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-08-10 21:28:31 +0200

    Chore: Use new sns project test util (#3079)
    # Motivation
    
    Final motivation: Stop using get_state. But on the way, clean the
    unnecessary complexity of snsQueryStore.
    
    In this PR: Use the new test util `setSnsProjects` in more tests.
    
    # Changes
    
    * Move from `snsQueryStore.setData` to `setSnsProjects` in some test
    files.
    
    # Tests
    
    Only test changes.
    
    # Todos
    
    Already covered by a changelog entry.

--- frontend/src/tests/lib/derived/sns/sns-selected-transaction-fee.store.spec.ts
+++ frontend/src/tests/lib/derived/sns/sns-selected-transaction-fee.store.spec.ts
@@ -2,44 +2,37 @@
  * @jest-environment jsdom
  */
 
+import { OWN_CANISTER_ID_TEXT } from "$lib/constants/canister-ids.constants";
 import { snsSelectedTransactionFeeStore } from "$lib/derived/sns/sns-selected-transaction-fee.store";
-import { snsQueryStore } from "$lib/stores/sns.store";
 import { transactionsFeesStore } from "$lib/stores/transaction-fees.store";
 import { page } from "$mocks/$app/stores";
 import { mockPrincipal } from "$tests/mocks/auth.store.mock";
-import { snsResponsesForLifecycle } from "$tests/mocks/sns-response.mock";
-import { IcrcMetadataResponseEntries } from "@dfinity/ledger";
-import { Principal } from "@dfinity/principal";
+import { mockToken } from "$tests/mocks/sns-projects.mock";
+import { resetSnsProjects, setSnsProjects } from "$tests/utils/sns.test-utils";
 import { SnsSwapLifecycle } from "@dfinity/sns";
 import { get } from "svelte/store";
 
 describe("snsSelectedTransactionFeeStore", () => {
-  const data = snsResponsesForLifecycle({
-    lifecycles: [SnsSwapLifecycle.Committed],
+  const rootCanisterId = mockPrincipal;
+  const universe = rootCanisterId.toText();
+  const projectParams = {
+    rootCanisterId: mockPrincipal,
+    lifecycle: SnsSwapLifecycle.Committed,
     certified: true,
-  });
-
+    tokenMetadata: mockToken,
+  };
   beforeEach(() => {
-    snsQueryStore.reset();
+    resetSnsProjects();
     page.mock({ data: { universe: mockPrincipal.toText() } });
   });
 
   it("returns transaction fee of current selected sns project", () => {
-    snsQueryStore.setData(data);
-    const [metadatas] = data;
-    page.mock({ data: { universe: metadatas[0].rootCanisterId } });
-    const ledgerMetadata = metadatas[0].token;
-    const symbolResponse = ledgerMetadata.find(
-      (metadata) => metadata[0] === IcrcMetadataResponseEntries.SYMBOL
-    );
-    const symbol =
-      symbolResponse !== undefined && "Text" in symbolResponse[1]
-        ? symbolResponse[1].Text
-        : undefined;
+    setSnsProjects([projectParams]);
+    page.mock({ data: { universe } });
 
     const fee = BigInt(10_000);
     transactionsFeesStore.setFee({
-      rootCanisterId: Principal.fromText(metadatas[0].rootCanisterId),
+      rootCanisterId,
       fee,
       certified: true,
     });
@@ -47,20 +40,21 @@ describe("snsSelectedTransactionFeeStore", () => {
     const actualFeeTokens = get(snsSelectedTransactionFeeStore);
 
     expect(actualFeeTokens?.toE8s()).toBe(fee);
-    expect(actualFeeTokens?.token.symbol).toBe(symbol);
+    expect(actualFeeTokens?.token.symbol).toBe(mockToken.symbol);
   });
 
   it("returns undefined if selected project is NNS", () => {
-    snsQueryStore.setData(data);
-    page.mock({ data: { universe: mockPrincipal.toText() } });
+    setSnsProjects([projectParams]);
+    page.mock({ data: { universe: OWN_CANISTER_ID_TEXT } });
+
     const actualFeeTokens = get(snsSelectedTransactionFeeStore);
 
     expect(actualFeeTokens).toBeUndefined();
   });
 
   it("returns undefined if current selected project has no data", () => {
-    const [metadatas] = data;
-    page.mock({ data: { universe: metadatas[0].rootCanisterId } });
+    page.mock({ data: { universe } });
+
     const actualFeeTokens = get(snsSelectedTransactionFeeStore);
 
     expect(actualFeeTokens).toBeUndefined();
--- frontend/src/tests/utils/sns.test-utils.ts
+++ frontend/src/tests/utils/sns.test-utils.ts
@@ -1,36 +1,48 @@
 import { snsQueryStore } from "$lib/stores/sns.store";
 import type { IcrcTokenMetadata } from "$lib/types/icrc";
-import { createQueryMetadataResponse } from "$tests/mocks/sns-projects.mock";
+import {
+  createQueryMetadataResponse,
+  principal,
+} from "$tests/mocks/sns-projects.mock";
 import {
   mergeSnsResponses,
   snsResponseFor,
 } from "$tests/mocks/sns-response.mock";
 import type { Principal } from "@dfinity/principal";
 import type { SnsSwapLifecycle } from "@dfinity/sns";
+import { isNullish } from "@dfinity/utils";
 
 export const setSnsProjects = (
   params: {
-    rootCanisterId: Principal;
+    rootCanisterId?: Principal;
     lifecycle: SnsSwapLifecycle;
     certified?: boolean;
     restrictedCountries?: string[];
     directParticipantCount?: [] | [bigint];
     projectName?: string;
-    tokenMetadata?: Partial<Pick<IcrcTokenMetadata, "name" | "symbol">>;
+    tokenMetadata?: Partial<IcrcTokenMetadata>;
   }[]
 ) => {
+  if (
+    params.filter(({ rootCanisterId }) => isNullish(rootCanisterId)).length > 5
+  ) {
+    throw new Error("Too many projects without canister id.");
+  }
   const responses = params.map(
-    ({
-      rootCanisterId,
-      lifecycle,
-      certified,
-      restrictedCountries,
-      directParticipantCount,
-      projectName,
-      tokenMetadata,
-    }) =>
+    (
+      {
+        rootCanisterId,
+        lifecycle,
+        certified,
+        restrictedCountries,
+        directParticipantCount,
+        projectName,
+        tokenMetadata,
+      },
+      index
+    ) =>
       snsResponseFor({
-        principal: rootCanisterId,
+        principal: rootCanisterId ?? principal(index),
         lifecycle,
         certified,
         restrictedCountries,
