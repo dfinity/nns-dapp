f3522483b6e8522680cccea36f652e23cbcb076b
David de Kloet <david.de.kloet@dfinity.org>
2023-08-14 10:34:17 +0200

    fmt

--- frontend/jest-spy.ts
+++ frontend/jest-spy.ts
@@ -2,7 +2,10 @@ import type { HttpAgent } from "@dfinity/agent";
 import { mock } from "jest-mock-extended";
 import * as agent from "./src/lib/api/agent.api";
 import * as authServices from "./src/lib/services/auth.services";
-import { resetIdentity, mockGetIdentity } from "./src/tests/mocks/auth.store.mock";
+import {
+  mockGetIdentity,
+  resetIdentity,
+} from "./src/tests/mocks/auth.store.mock";
 
 const mockCreateAgent = () => Promise.resolve(mock<HttpAgent>());
 jest.spyOn(agent, "createAgent").mockImplementation(mockCreateAgent);
