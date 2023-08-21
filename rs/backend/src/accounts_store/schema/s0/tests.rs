7c96754fcb43463f7d37ef4527b8cd398e66a6d8
Max Murphy-Skvorzov <max@winning.black>
2023-08-21 07:21:54 +0200

    ++

--- rs/backend/src/accounts_store/schema/s0/tests.rs
+++ rs/backend/src/accounts_store/schema/s0/tests.rs
@@ -1,8 +1,9 @@
 //! Tests for the S0 schema data storage.
-use super::{AccountStorageTrait, Account, AccountStorageKey, AccountStoragePage};
+use super::{Account, AccountStorageKey, AccountStoragePage, AccountStorageTrait};
+use crate::accounts_store::NamedCanister;
+use ic_base_types::{CanisterId, PrincipalId};
 use icp_ledger::AccountIdentifier;
 use std::collections::{BTreeMap, HashMap};
-use ic_base_types::PrincipalId;
 
 struct MockS0DataStorage {
     accounts_storage: BTreeMap<AccountStorageKey, AccountStoragePage>,
@@ -12,7 +13,11 @@ impl AccountStorageTrait for MockS0DataStorage {
     fn get_account_page(&self, account_storage_key: &AccountStorageKey) -> Option<AccountStoragePage> {
         self.accounts_storage.get(account_storage_key).cloned()
     }
-    fn insert_account_page(&mut self, account_storage_key: AccountStorageKey, account: AccountStoragePage) -> Option<AccountStoragePage> {
+    fn insert_account_page(
+        &mut self,
+        account_storage_key: AccountStorageKey,
+        account: AccountStoragePage,
+    ) -> Option<AccountStoragePage> {
         self.accounts_storage.insert(account_storage_key, account)
     }
     fn contains_account_page(&self, account_storage_key: &AccountStorageKey) -> bool {
@@ -24,17 +29,28 @@ impl AccountStorageTrait for MockS0DataStorage {
 }
 
 /// Creates atoy account.  The contents do not need to be meaningful; do need to have size.
-fn toy_account() -> Account {
-    let principal = PrincipalId::new_user_test_id(1);
+fn toy_account(account_index: u64, num_canisters: usize) -> Account {
+    let principal = PrincipalId::new_user_test_id(account_index);
     let account_identifier = AccountIdentifier::from(principal);
-    Account {
+    let mut account = Account {
         principal: Some(principal),
         account_identifier,
         default_account_transactions: Vec::new(),
         sub_accounts: HashMap::new(),
         hardware_wallet_accounts: Vec::new(),
         canisters: Vec::new(),
+    };
+    // Attaches canisters to the account.
+    for canister_index in 0..num_canisters {
+        let canister_id = CanisterId::from(canister_index);
+        let canister = NamedCanister {
+            name: format!("canister_{account_index}_{canister_index}"),
+            canister_id,
+        };
+        account.canisters.push(canister);
     }
+    // FIN
+    account
 }
 
 #[test]
@@ -43,7 +59,8 @@ fn test_account_storage() {
         accounts_storage: BTreeMap::new(),
     };
     let account_key = vec![1, 2, 3];
-    let account = toy_account();
+    let account = toy_account(1, 1000);
+    // TODO: Check that this spans several pages.
     storage.insert_account(&account_key, account.clone());
     assert_eq!(storage.contains_account(&account_key), true);
     assert_eq!(storage.get_account(&account_key), Some(account.clone()));
