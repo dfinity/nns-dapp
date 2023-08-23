f7d68d0701d7cb1de93a4ae82520f00b07251cec
David Dal Busco <david.dalbusco@dfinity.org>
2023-08-23 17:55:51 +0200

    Render accounts on root "/" page (#3187)
    # Motivation
    
    We noticed that using a `goto(accounts, replace)` on testnet leads to
    slowing down the boot time of the dapp.
    While we can display a spinner while loading, this would still feel like
    "being slow".
    
    Therefore and instead, we are proposing to render the accounts at the
    root of the dapp `/` as on the route `/accounts`.
    
    # Changes
    
    Routing and layout:
    
    - create a new subgroup `(app)/(u)/(accounts)`
    - move `(app)/(u)/accounts/+layout` to `(app)/(u)/(accounts)/+layout`
    that way both root and /accounts share the custom layout and still
    inherit all other layout
    - move and copy `(app)/(u)/accounts/+page` to
    `(app)/(u)/(accounts)/+page` and `(app)/(u)/(accounts)/accounts/+page`
    
    Changes:
    
    - remove enum `AppPath.Authentication`
    - use `AppPath.Accounts` as root

--- frontend/src/routes/(login)/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import { goto } from "$app/navigation";
-  import { AppPath } from "$lib/constants/routes.constants";
-  import { onMount } from "svelte";
-
-  onMount(() => {
-    goto(AppPath.Accounts, { replaceState: true });
-  });
-</script>
