99b3673bc370fc274703056b16c2bfc730193f9d
David Dal Busco <david.dalbusco@dfinity.org>
2023-08-23 14:09:28 +0200

    feat: move accounts to universe

--- frontend/src/tests/routes/app/accounts/page.spec.ts
+++ frontend/src/tests/routes/app/accounts/page.spec.ts
@@ -3,7 +3,7 @@
  */
 
 import { authStore } from "$lib/stores/auth.store";
-import AccountsPage from "$routes/(app)/(accounts)/accounts/+page.svelte";
+import AccountsPage from "$routes/(app)/(u)/(accounts)/accounts/+page.svelte";
 import {
   authStoreMock,
   mutableMockAuthStoreSubscribe,
