beef1bf513ae6100a3ae257849b0c15fe1c85e58
David Dal Busco <david.dalbusco@dfinity.org>
2023-08-24 08:51:44 +0200

    Static loading spinner (#3190)
    # Motivation
    
    We have no control on the way the chunks are loaded. As a result, it's
    possible that it waits until most chunks are loaded before displaying
    the hints - the spinner - that the dapp is booting which is leading to
    an unfriendly UX as a blank screen might be displaying for a while.
    
    By rendering a spinner within the static html page and removing it once
    most of the JS files are loaded, we can overcome this "race condition".
    
    # Changes
    
    - add (copy) spinner in `app.html`
    - when the auth is ready, which indicates most JS resources are loaded,
    remove the spinner
    
    # Tests
    
    https://kgqse-raaaa-aaaaa-aactq-cai.nnsdapp.dfinity.network/
    https://kgqse-raaaa-aaaaa-aactq-cai.nnsdapp.dfinity.network/accounts
    
    https://xnjld-hqaaa-aaaal-qb56q-cai.icp0.io/
    
    # Screenshots
    
    Mainnet - current main - Fast 3g network => spinner appears for a milli
    second when all js is loaded
    
    
    ![mainnet-no-spinner](https://github.com/dfinity/nns-dapp/assets/16886711/6a740ee4-0b83-4b1c-b3b4-137c78df2657)
    
    Mainnet - this PR - Fast 3g network
    
    
    ![mainnet-spinner](https://github.com/dfinity/nns-dapp/assets/16886711/5dd86910-d627-4e3e-b4ab-52e27f3a58dd)
    
    Mainnet this PR - full speed network
    
    
    ![full-speed](https://github.com/dfinity/nns-dapp/assets/16886711/d3efe0de-02f3-442b-89dd-c6364a997949)

--- frontend/src/routes/(app)/+layout.svelte
+++ frontend/src/routes/(app)/+layout.svelte
@@ -3,7 +3,7 @@
   import { confirmCloseApp } from "$lib/utils/before-unload.utils";
   import { voteRegistrationActive } from "$lib/utils/proposals.utils";
   import { onMount } from "svelte";
-  import { Toasts, BusyScreen, Spinner } from "@dfinity/gix-components";
+  import { Toasts, BusyScreen } from "@dfinity/gix-components";
   import {
     initAppAuth,
     initAppPublicData,
@@ -15,6 +15,7 @@
   import { referrerPathStore } from "$lib/stores/routes.store";
   import { referrerPathForNav } from "$lib/utils/page.utils";
   import { authStore } from "$lib/stores/auth.store";
+  import { browser } from "$app/environment";
 
   onMount(async () => await Promise.all([initAppAuth(), initAppPublicData()]));
 
@@ -29,12 +30,26 @@
     // TODO: e2e to test this
     referrerPathStore.set(referrerPathForNav(nav))
   );
+
+  // To improve the UX while the app is loading on mainnet we display a spinner which is attached statically in the index.html files.
+  // Once the authentication has been initialized we know most JavaScript resources has been loaded and therefore we can hide the spinner, the loading information.
+  $: (() => {
+    if (!browser) {
+      return;
+    }
+
+    // We want to display a spinner until the authentication is loaded. This to avoid a glitch when either the landing page or effective content (sign-in / sign-out) is presented.
+    if ($authStore === undefined) {
+      return;
+    }
+
+    const spinner = document.querySelector("body > #app-spinner");
+    spinner?.remove();
+  })();
 </script>
 
 <!-- We are waiting agent-js / the auth store to be loaded before rendering the pages to avoid visual glitch -->
-{#if $authStore.identity === undefined}
-  <Spinner />
-{:else}
+{#if $authStore.identity !== undefined}
   <slot />
 {/if}
 
