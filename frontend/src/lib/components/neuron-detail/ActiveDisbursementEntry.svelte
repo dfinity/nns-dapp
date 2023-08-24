a2f6bed88f1fb5227b1139a644b86ff07ea682df
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-08-24 14:31:33 +0200

    refactor: ActiveDisbursementEntry

---
+++ frontend/src/lib/components/neuron-detail/ActiveDisbursementEntry.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { i18n } from "$lib/stores/i18n";
+  import { IconClockNoFill } from "@dfinity/gix-components";
+  import { replacePlaceholders } from "$lib/utils/i18n.utils";
+
+  export let formattedTime: string;
+  export let formattedAccount: string;
+  export let formattedAmount: string;
+</script>
+
+<div class="disbursement" data-tid="active-disbursement-entry-component">
+  <div class="description">
+    <IconClockNoFill size="20" />
+    <span>
+      {replacePlaceholders(
+        $i18n.neuron_detail.view_active_disbursements_entry_description,
+        {
+          $time: formattedTime,
+          $account: formattedAccount,
+        }
+      )}
+    </span>
+  </div>
+  <span class="value">
+    {replacePlaceholders(
+      $i18n.neuron_detail.view_active_disbursements_maturity,
+      {
+        $maturity: formattedAmount,
+      }
+    )}
+  </span>
+</div>
+
+<style lang="scss">
+  .disbursement {
+    display: flex;
+    flex-direction: column;
+    gap: var(--padding-0_5x);
+  }
+
+  .description {
+    display: flex;
+    gap: var(--padding);
+    align-items: center;
+  }
+</style>
--- frontend/src/lib/modals/sns/neurons/SnsActiveDisbursementEntry.svelte
+++ frontend/src/lib/modals/sns/neurons/SnsActiveDisbursementEntry.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-  import { i18n } from "$lib/stores/i18n";
-  import { IconClockNoFill } from "@dfinity/gix-components";
   import type { DisburseMaturityInProgress } from "@dfinity/sns/dist/candid/sns_governance";
   import { secondsToDuration } from "$lib/utils/date.utils";
   import { fromDefinedNullable } from "@dfinity/utils";
   import { shortenWithMiddleEllipsis } from "$lib/utils/format.utils";
   import type { Account } from "@dfinity/sns/dist/candid/sns_governance";
-  import { replacePlaceholders } from "$lib/utils/i18n.utils";
+  import ActiveDisbursementItem from "$lib/components/neuron-detail/ActiveDisbursementEntry.svelte";
 
   export let disbursement: DisburseMaturityInProgress;
 
@@ -21,43 +19,8 @@
   let formattedAccount: string;
   $: formattedAccount = shortenWithMiddleEllipsis(account.owner.toString());
 
-  let amount: bigint;
-  $: amount = disbursement.amount_e8s;
+  let formattedAmount: bigint;
+  $: formattedAmount = disbursement.amount_e8s;
 </script>
 
-<div class="content">
-  <div class="description">
-    <IconClockNoFill size="20" />
-    <span>
-      {replacePlaceholders(
-        $i18n.neuron_detail.view_active_disbursements_entry_description,
-        {
-          $time: formattedTime,
-          $account: formattedAccount,
-        }
-      )}
-    </span>
-  </div>
-  <span class="value">
-    {replacePlaceholders(
-      $i18n.neuron_detail.view_active_disbursements_maturity,
-      {
-        $maturity: amount,
-      }
-    )}
-  </span>
-</div>
-
-<style lang="scss">
-  .content {
-    display: flex;
-    flex-direction: column;
-    gap: var(--padding-0_5x);
-  }
-
-  .description {
-    display: flex;
-    gap: var(--padding);
-    align-items: center;
-  }
-</style>
+<ActiveDisbursementItem {formattedTime} {formattedAccount} {formattedAmount} />
