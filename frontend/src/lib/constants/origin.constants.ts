06c1260ce9d8287c688ccb6c80e1215c08d88ad3
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2023-08-31 17:07:34 +0200

    GIX-1843 Support wallet.~ domains (#3255)
    # Motivation
    
    We want to make NNS Dapp accessible on `wallet.ic0.app` and
    `wallet.internetcomputer.org`.
    
    # Changes
    
    1. Add both domains to `frontend/static/.well-known/ic-domains`. This
    allows the canister to be accessible on those domain.
    2. Add both origins to
    `frontend/static/.well-known/ii-alternative-origins`. This allows people
    to get the same principal from Internet Identity on those domains as
    they get on `nns.ic0.app`.
    3. Make `isNnsAlternativeOrigin` return `true` for the new origins. This
    causes the app to pass a `derivationOrigin` of `"https://nns.ic0.app"`
    to Internet Identity on line 90 of
    `frontend/src/lib/stores/auth.store.ts`.
    4. Use `identity.ic0.app` (instead of `identity.internetcomputer.org`)
    for any domain ending in `.ic0.app`, not just `nns.ic0.app`.
    5. Add missing test that we use `identity.internetcomputer.org` by
    default.
    6. Add tests for the other changes.
    
    # Tests
    
    Unit tests pass.
    Not sure how to test if it actually works, short of releasing.
    
    # Todos
    
    - [x] Add entry to changelog (if necessary).

--- frontend/src/lib/constants/origin.constants.ts
+++ frontend/src/lib/constants/origin.constants.ts
@@ -1,2 +1,6 @@
 export const NNS_IC_APP_DERIVATION_ORIGIN = "https://nns.ic0.app";
-export const NNS_IC_ORG_ALTERNATIVE_ORIGIN = "https://nns.internetcomputer.org";
+export const NNS_IC_ORG_ALTERNATIVE_ORIGINS = [
+  "https://nns.internetcomputer.org",
+  "https://wallet.internetcomputer.org",
+  "https://wallet.ic0.app",
+];
