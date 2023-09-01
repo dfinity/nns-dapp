1c411f868599f2d18c5186cddac02c7e9708faca
Llorenç <llorenc.muntaner@dfinity.org>
2023-09-01 13:38:37 +0200

    GIX-1760: Fix tests

--- frontend/src/lib/components/ui/SkeletonCard.svelte
+++ frontend/src/lib/components/ui/SkeletonCard.svelte
@@ -2,43 +2,38 @@
   import { Card } from "@dfinity/gix-components";
   import CardInfo from "./CardInfo.svelte";
   import { SkeletonText } from "@dfinity/gix-components";
-  import type { SvelteComponent } from "svelte";
   import type { CardType } from "$lib/types/card";
   import Separator from "$lib/components/ui/Separator.svelte";
+  import SkeletonCardContent from "./SkeletonCardContent.svelte";
 
   export let size: "small" | "medium" | "large" = "small";
   export let cardType: CardType = "card";
   export let separator = false;
   export let noMargin = false;
-
-  const cards: Record<CardType, typeof SvelteComponent> = {
-    card: Card,
-    info: CardInfo,
-  };
 </script>
 
-<svelte:component this={cards[cardType]} testId="skeleton-card" {noMargin}>
-  <div class="small" slot="start">
-    <SkeletonText />
-  </div>
-  <div class="small" slot="end">
-    <SkeletonText />
-  </div>
-  <div class="content">
-    <SkeletonText />
-    <SkeletonText />
-
-    {#if ["large", "medium"].includes(size)}
+<!-- We need to explictly use the `Card` or `CardInfo` because props are different -->
+{#if cardType === "info"}
+  <CardInfo {noMargin} testId="skeleton-card">
+    <div class="small" slot="start">
       <SkeletonText />
+    </div>
+    <div class="small" slot="end">
       <SkeletonText />
-    {/if}
-
-    {#if size === "large"}
+    </div>
+    <SkeletonCardContent {size} />
+  </CardInfo>
+{:else}
+  <Card testId="skeleton-card">
+    <div class="small" slot="start">
       <SkeletonText />
+    </div>
+    <div class="small" slot="end">
       <SkeletonText />
-    {/if}
-  </div>
-</svelte:component>
+    </div>
+    <SkeletonCardContent {size} />
+  </Card>
+{/if}
 
 {#if separator}
   <Separator />
@@ -47,11 +42,5 @@
 <style lang="scss">
   .small {
     width: 20%;
-  }
-
-  .content {
-    display: flex;
-    flex-direction: column;
-    gap: var(--padding_2x);
   }
 </style>
