0c985f8a220c59d7c5fffbbedcddc2b8cee0e6ae
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2023-09-04 14:02:23 +0200

    Use page objects in SelectUniverseCard.spec.ts (#3270)
    # Motivation
    
    It's easier to modify the test when it uses page objects.
    I want to modify the test in another PR in order to show the universe
    selector, but without balances, even when the user is not logged in.
    
    # Changes
    
    1. Add required functionality to page objects.
    2. Add required `data-tid` to
    `frontend/src/lib/components/universe/UniverseAccountsBalance.svelte`.
    3. Use page objects in
    `frontend/src/tests/lib/components/universe/SelectUniverseCard.spec.ts`
    4. Use real `icpAccountsStore` instead of mocking it.
    5. Set and expect explicit values for the balances.
    
    # Tests
    
    yes
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    not necessary

---
+++ frontend/src/tests/page-objects/UniverseAccountsBalance.page-object.ts
@@ -0,0 +1,25 @@
+import { AmountDisplayPo } from "$tests/page-objects/AmountDisplay.page-object";
+import { BasePageObject } from "$tests/page-objects/base.page-object";
+import type { PageObjectElement } from "$tests/types/page-object.types";
+
+export class UniverseAccountsBalancePo extends BasePageObject {
+  private static readonly TID = "universe-accounts-balance-component";
+
+  static under(element: PageObjectElement): UniverseAccountsBalancePo {
+    return new UniverseAccountsBalancePo(
+      element.byTestId(UniverseAccountsBalancePo.TID)
+    );
+  }
+
+  getAmountDisplayPo(): AmountDisplayPo {
+    return AmountDisplayPo.under(this.root);
+  }
+
+  isLoaded(): Promise<boolean> {
+    return this.getAmountDisplayPo().isPresent();
+  }
+
+  getBalance(): Promise<string> {
+    return this.getAmountDisplayPo().getAmount();
+  }
+}
