c5a9be88a295f1bbc82094fd59e4f1d1b7bbb498
David de Kloet <david.de.kloet@dfinity.org>
2023-09-07 10:12:56 +0200

    SummaryUniverse

--- frontend/src/lib/pages/SignInAccounts.svelte
+++ frontend/src/lib/pages/SignInAccounts.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
   import EmptyCards from "$lib/components/common/EmptyCards.svelte";
   import SignIn from "$lib/components/common/SignIn.svelte";
+  import SummaryUniverse from "$lib/components/summary/SummaryUniverse.svelte";
   import { i18n } from "$lib/stores/i18n";
   import { PageBanner, IconAccountsPage } from "@dfinity/gix-components";
 </script>
 
 <main class="sign-in" data-tid="accounts-landing-page">
+  <SummaryUniverse />
+
   <PageBanner>
     <IconAccountsPage slot="image" />
     <svelte:fragment slot="title">{$i18n.auth_accounts.title}</svelte:fragment>
--- frontend/src/lib/pages/SignInNeurons.svelte
+++ frontend/src/lib/pages/SignInNeurons.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
   import EmptyCards from "$lib/components/common/EmptyCards.svelte";
   import SignIn from "$lib/components/common/SignIn.svelte";
+  import SummaryUniverse from "$lib/components/summary/SummaryUniverse.svelte";
   import { i18n } from "$lib/stores/i18n";
   import { PageBanner, IconNeuronsPage } from "@dfinity/gix-components";
 </script>
 
 <main class="sign-in">
+  <SummaryUniverse />
+
   <PageBanner>
     <IconNeuronsPage slot="image" />
     <svelte:fragment slot="title">{$i18n.auth_neurons.title}</svelte:fragment>
