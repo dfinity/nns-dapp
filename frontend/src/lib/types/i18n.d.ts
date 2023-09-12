51e8a1745fb78acefd7c7bcfb88dea01606f9d35
Max Strasinsky <98811342+mstrasinskis@users.noreply.github.com>
2023-09-12 08:16:43 +0200

    remove extra disburse_maturity label (#3299)
    # Motivation
    
    Remove redundant `disburse_maturity` label.
    
    # Tests
    
    - unit tests work
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    not necessary

--- frontend/src/lib/types/i18n.d.ts
+++ frontend/src/lib/types/i18n.d.ts
@@ -615,7 +615,6 @@ interface I18nNeuron_detail {
   maturity_last_distribution: string;
   maturity_last_distribution_info: string;
   stake_maturity: string;
-  disburse_maturity: string;
   view_active_disbursements_total: string;
   view_active_disbursements: string;
   view_active_disbursements_status: string;
@@ -655,6 +654,7 @@ interface I18nNeuron_detail {
   stake_maturity_modal_title: string;
   disburse_maturity_modal_title: string;
   disburse_maturity_modal_description: string;
+  disburse_maturity: string;
   disburse_maturity_confirmation_modal_title: string;
   disburse_maturity_confirmation_description: string;
   disburse_maturity_success: string;
