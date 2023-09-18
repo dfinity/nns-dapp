08003ca27279531bda941fd91ed2e8db1b3bc358
Llorenç <llorenc.muntaner@dfinity.org>
2023-09-18 14:53:35 +0200

    GIX-1890: Fix tests

--- frontend/src/tests/lib/components/sns-neuron-detail/actions/SnsDisburseMaturityButton.spec.ts
+++ frontend/src/tests/lib/components/sns-neuron-detail/actions/SnsDisburseMaturityButton.spec.ts
@@ -6,13 +6,16 @@ import SnsDisburseMaturityButton from "$lib/components/sns-neuron-detail/actions
 import { mockSnsNeuron } from "$tests/mocks/sns-neurons.mock";
 import { DisburseMaturityButtonPo } from "$tests/page-objects/DisburseMaturityButton.page-object";
 import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
+import type { SnsNeuron } from "@dfinity/sns";
 import { render } from "@testing-library/svelte";
 
 describe("SnsDisburseMaturityButton", () => {
-  const renderComponent = (neuron) => {
+  const fee = 10_000n;
+  const renderComponent = (neuron: SnsNeuron, feeE8s: bigint) => {
     const { container } = render(SnsDisburseMaturityButton, {
       props: {
         neuron,
+        feeE8s,
       },
     });
     return DisburseMaturityButtonPo.under(new JestPageObjectElement(container));
@@ -23,21 +26,27 @@ describe("SnsDisburseMaturityButton", () => {
   });
 
   it("should be enabled if enough maturity is available", async () => {
-    const po = renderComponent({
-      ...mockSnsNeuron,
-      maturity_e8s_equivalent: 1n,
-      staked_maturity_e8s_equivalent: [],
-    });
+    const po = renderComponent(
+      {
+        ...mockSnsNeuron,
+        maturity_e8s_equivalent: fee + 10n,
+        staked_maturity_e8s_equivalent: [],
+      },
+      fee
+    );
 
     expect(await po.isDisabled()).toBe(false);
   });
 
   it("should be disabled if no maturity to disburse", async () => {
-    const po = renderComponent({
-      ...mockSnsNeuron,
-      maturity_e8s_equivalent: 0n,
-      staked_maturity_e8s_equivalent: [],
-    });
+    const po = renderComponent(
+      {
+        ...mockSnsNeuron,
+        maturity_e8s_equivalent: fee - 1n,
+        staked_maturity_e8s_equivalent: [],
+      },
+      fee
+    );
 
     expect(await po.isDisabled()).toBe(true);
   });
