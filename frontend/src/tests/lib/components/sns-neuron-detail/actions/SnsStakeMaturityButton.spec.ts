dcb98b4e27aaaff7fa4171b83031cc24e4c742ca
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-09-19 21:37:37 +0200

    Fix: SnsStakeMaturityButton spec file (#3335)
    # Motivation
    
    One test for SnsStakeMaturityButton was not correct. The test passed
    because the expected text (title of the modal) was the same text as the
    button.
    
    In this PR, I added a testId to the modal and checked the presence of
    the modal with test id. The test failed.
    
    The test also failed because it required a token in the store to be
    present. That is not necessary for the modal, but it was inside an `if`
    checking the presence of the token.
    
    Therefore, in this PR, I also refactored the SnsNeuronModals to check
    for presence next to the type of the modal. This way it's easier to
    understand what is strictly necessary in each modal.
    
    # Changes
    
    * Add testId to StakeMaturityModal.
    * Move modals and conditions in SnsNeuronModals.
    
    # Tests
    
    * Use `SnsNeuronContextTest` in `SnsStakeMaturityButton.spec` to check
    that modal is opened.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    Not necessary.

--- frontend/src/tests/lib/components/sns-neuron-detail/actions/SnsStakeMaturityButton.spec.ts
+++ frontend/src/tests/lib/components/sns-neuron-detail/actions/SnsStakeMaturityButton.spec.ts
@@ -3,9 +3,10 @@
  */
 
 import SnsStakeMaturityButton from "$lib/components/sns-neuron-detail/actions/SnsStakeMaturityButton.svelte";
-import en from "$tests/mocks/i18n.mock";
+import { mockPrincipal } from "$tests/mocks/auth.store.mock";
 import { mockSnsNeuron } from "$tests/mocks/sns-neurons.mock";
-import { fireEvent, render, waitFor } from "@testing-library/svelte";
+import { fireEvent, render } from "@testing-library/svelte";
+import SnsNeuronContextTest from "../SnsNeuronContextTest.svelte";
 
 describe("SnsStakeMaturityButton", () => {
   afterEach(() => {
@@ -13,21 +14,27 @@ describe("SnsStakeMaturityButton", () => {
   });
 
   it("should open stake maturity modal", async () => {
-    const { getByText, getByTestId } = render(SnsStakeMaturityButton, {
+    const { queryByTestId, getByTestId } = render(SnsNeuronContextTest, {
       props: {
         neuron: {
           ...mockSnsNeuron,
+          maturity_e8s_equivalent: 100_000_000n,
         },
+        testComponent: SnsStakeMaturityButton,
+        passPropNeuron: true,
+        rootCanisterId: mockPrincipal,
       },
     });
 
-    fireEvent.click(getByTestId("stake-maturity-button") as HTMLButtonElement);
+    expect(
+      queryByTestId("stake-maturity-modal-component")
+    ).not.toBeInTheDocument();
 
-    await waitFor(() =>
-      expect(
-        getByText(en.neuron_detail.stake_maturity_modal_title)
-      ).toBeInTheDocument()
+    await fireEvent.click(
+      getByTestId("stake-maturity-button") as HTMLButtonElement
     );
+
+    expect(queryByTestId("stake-maturity-modal-component")).toBeInTheDocument();
   });
 
   it("should be disabled if no maturity to stake", async () => {
