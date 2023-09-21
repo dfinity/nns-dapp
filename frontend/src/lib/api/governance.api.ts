3cda50014197eaf4552fa02827fc030983d4ab6a
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2023-09-21 14:08:45 +0200

    Add governance API function stakeNeuronIcrc1 (#3346)
    # Motivation
    
    We want to stake neurons using an ICRC-1 transfer when possible.
    
    # Changes
    
    Support the new `stakeNeuronIcrc1` function in the governance API.
    
    # Tests
    
    Improved existing test for `stakeNeuron` and added an analogous test for
    `stakeNeuronIcrc1`.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    Will add when it's all hooked up.

--- frontend/src/lib/api/governance.api.ts
+++ frontend/src/lib/api/governance.api.ts
@@ -411,13 +411,53 @@ export const stakeNeuron = async ({
 
   const createdAt = nowInBigIntNanoSeconds();
   const response = await canister.stakeNeuron({
-    stake: stake,
+    stake,
     principal: controller,
     fromSubAccount,
     ledgerCanister,
     createdAt,
   });
   logWithTimestamp(`Staking Neuron complete.`);
+  return response;
+};
+
+export type ApiStakeNeuronIcrc1Params = ApiCallParams & {
+  stake: bigint;
+  controller: Principal;
+  ledgerCanisterIdentity: Identity;
+  fromSubAccount?: Uint8Array;
+};
+
+/**
+ * Uses governance and ledger canisters to create a neuron
+ */
+export const stakeNeuronIcrc1 = async ({
+  stake,
+  controller,
+  ledgerCanisterIdentity,
+  identity,
+  fromSubAccount,
+}: ApiStakeNeuronIcrc1Params): Promise<NeuronId> => {
+  // Ledger HW app currently (2023-09-21) doesn't support staking with ICRC-1
+  // but it should be supported in the future so we keep the code similar to the
+  // non-ICRC-1 flow above.
+  logWithTimestamp(`Staking Neuron ICRC-1 call...`);
+  const { canister } = await governanceCanister({ identity });
+
+  // The use case of staking from Hardware wallet uses a different agent for governance and ledger canister.
+  const { canister: ledgerCanister } = await getLedgerCanister({
+    identity: ledgerCanisterIdentity,
+  });
+
+  const createdAt = nowInBigIntNanoSeconds();
+  const response = await canister.stakeNeuronIcrc1({
+    stake,
+    principal: controller,
+    fromSubAccount,
+    ledgerCanister,
+    createdAt,
+  });
+  logWithTimestamp(`Staking Neuron ICRC-1 complete.`);
   return response;
 };
 
