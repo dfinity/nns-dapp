30d6a380dea27415acad7b6b56edee3178057f70
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-09-25 16:14:09 +0200

    GIX-1881: Add destination disburse maturity (#3367)
    # Motivation
    
    Users can select a destination when disbursing maturity.
    
    # Changes
    
    * Use `QrWizardModal` and `SelectDestinationAddress` in
    DisburseMaturityModal.
    * Add props `rootCanisterId`, `token` and `selectedNetwork` to
    DisburrseMaturityModal.
    * SnsDisburseMaturityModal passes new props to DisburseMaturityModal.
    `selectedNetwork` is left `undefined` on purpose because it means using
    ICRC accounts.
    * Add `destinationAddress` to the `nnsDisburseMaturity` event caught by
    SnsDisburseMaturityModal.
    * Use the new parameter `toAccount` of the disburse maturity service.
    
    # Tests
    
    * Add a test case that user can disburse maturity to another account and
    that it gets an error if a non-ICRC account is used.
    * Add `blur` method to jest-page-object.
    * Add a few methods to the related POs.
    
    # Todos
    
    - [x] Add entry to changelog (if necessary).

--- frontend/src/tests/page-objects/AddressInput.page-object.ts
+++ frontend/src/tests/page-objects/AddressInput.page-object.ts
@@ -1,6 +1,8 @@
 import { BasePageObject } from "$tests/page-objects/base.page-object";
 import type { PageObjectElement } from "$tests/types/page-object.types";
 
+// NOTE: This element is an InputWithError itself.
+// DO NOT TRY TO SELECT THE INPUT WITHIN IT.
 export class AddressInputPo extends BasePageObject {
   private static readonly TID = "address-input-component";
 
@@ -10,5 +12,13 @@ export class AddressInputPo extends BasePageObject {
 
   enterAddress(address: string): Promise<void> {
     return this.getTextInput().typeText(address);
+  }
+
+  async getErrorMessage(): Promise<string> {
+    return (await this.getText("input-error-message")).trim();
+  }
+
+  blur(): Promise<void> {
+    return this.getTextInput().blur();
   }
 }
