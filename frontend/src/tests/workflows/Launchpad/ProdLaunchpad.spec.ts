de9a7551eff733c3eb0ff95af8417e9b27a63187
Llorenç <llorenc.muntaner@dfinity.org>
2023-10-02 09:34:22 +0200

    Chore: CR changes

--- frontend/src/tests/workflows/Launchpad/ProdLaunchpad.spec.ts
+++ frontend/src/tests/workflows/Launchpad/ProdLaunchpad.spec.ts
@@ -7,6 +7,7 @@ import { authStore } from "$lib/stores/auth.store";
 import { LaunchpadPo } from "$tests/page-objects/Launchpad.page-object";
 import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
 import { toastsStore } from "@dfinity/gix-components";
+import { isNullish } from "@dfinity/utils";
 import { render, waitFor } from "@testing-library/svelte";
 import { get } from "svelte/store";
 import Launchpad from "./LaunchpadWithLayout.svelte";
@@ -24,16 +25,26 @@ describe("Launchpad", () => {
       .spyOn(proposalsApi, "queryProposals")
       .mockImplementation(() => Promise.resolve([]));
 
+    // Depends on the `snsAggregatorUrl` set in `jest-setup.ts`.
+    const aggUrlRegex =
+      /https:\/\/5v72r-4aaaa-aaaaa-aabnq-cai\.small12\.testnet\.dfinity\.network\/v1\/sns\/list\/page\/(.)\/slow\.json/;
+    const pagesMockMapper = {
+      0: snsPage0,
+      1: snsPage1,
+    };
+
     const mockFetch = jest.fn();
-    mockFetch
-      .mockResolvedValueOnce({
+    mockFetch.mockImplementation(async (url: string) => {
+      const [_, page] = url.match(aggUrlRegex);
+      const mock = pagesMockMapper[page];
+      if (isNullish(mock)) {
+        throw new Error(`No mock for page ${page}`);
+      }
+      return {
         ok: true,
-        json: () => Promise.resolve(snsPage0),
-      })
-      .mockResolvedValueOnce({
-        ok: true,
-        json: () => Promise.resolve(snsPage1),
-      });
+        json: () => Promise.resolve(mock),
+      };
+    });
     global.fetch = mockFetch;
   });
 
