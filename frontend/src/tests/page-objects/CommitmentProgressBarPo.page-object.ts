e5d90ce358ba4a39c02ec0b19f7f4108c9a25b6a
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-10-03 12:46:49 +0200

    GIX-1922: Undo CommitmentProgressBar split commitments (#3433)
    # Motivation
    
    The new participation UI doesn't show a progress bar with commitments of
    NF and direct as different colors. Instead, if shows only the direct
    participation only.
    
    In this PR, I remove the props, logic and tests that allow the
    CommitmentProgressBar to show multiple commitments.
    
    I have also adapted the commitment when NF is present, to show only the
    direct participation. Yet, the UI still needs some changes that will be
    done in another PR.
    
    # Changes
    
    * Change props `directParticipation` and `nfParticipation` for
    `participationE8s` in CommitmentProgressBar. Adapt logic inside.
    * In ProjectCommitment, use the overall commitment or the direct
    commitment for the CommitmentProgressBar. I also moved the label and
    value of the NF commitment after the progress bar. Even though this UI
    is not available, I didn't want it to be in a confusing state. I also
    removed the left spacing of the labels for direct and NF commitments.
    
    # Tests
    
    * Fix, change and remove tests in ProjectCommitment.
    * Fix, change and remove tests in CommitmentProgressBar.
    * Rename and remove unncessary methods from CommitmentProgressBarPo.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    Not yet necessary.

--- frontend/src/tests/page-objects/CommitmentProgressBarPo.page-object.ts
+++ frontend/src/tests/page-objects/CommitmentProgressBarPo.page-object.ts
@@ -26,49 +26,11 @@ export class CommitmentProgressBarPo extends BasePageObject {
     return (await this.getText("commitment-max-indicator-value")).trim();
   }
 
-  async getTotalCommitmentE8s(): Promise<bigint> {
+  async getCommitmentE8s(): Promise<bigint> {
     // The `value` property has the value of the max value but the `value` attribute has the value of the progress.
     const valueString = await this.root
       .querySelector("progress")
       .getAttribute("value");
     return BigInt(valueString);
-  }
-
-  /**
-   * Uses the slices of the linear gradient to determine the segments of the progress bar.
-   *
-   * Example of inline-style: "--progress-bar-background: linear-gradient(to right, var(--positive-emphasis) 0% 25%, var(--warning-emphasis) 25% 100%);"
-   */
-  async getNFCommitmentE8s(): Promise<bigint> {
-    const inlineStyleRegex =
-      /--progress-bar-background: linear-gradient\(to right, var\(.*\) 0% (\d+(?:\.\d+)?)%, var\(.*\) (\d+(?:\.\d+)?)% 100%\);/g;
-
-    const inlineStyle = await this.root
-      .querySelector("progress")
-      .getAttribute("style");
-
-    const matches = inlineStyleRegex.exec(inlineStyle);
-
-    if (matches === null) {
-      throw new Error(`Invalid inline style: ${inlineStyle}`);
-    }
-
-    const [_inlineStyle, firstPercentage, secondPercentage] = matches;
-
-    if (firstPercentage !== secondPercentage) {
-      throw new Error(
-        `The first and second percentage should be the same: ${firstPercentage} !== ${secondPercentage}`
-      );
-    }
-
-    const totalCommitment = Number(await this.getTotalCommitmentE8s());
-    const nfPercentage = Number(firstPercentage);
-
-    return BigInt(Math.round((totalCommitment * nfPercentage) / 100));
-  }
-
-  async getDirectCommitmentE8s(): Promise<bigint> {
-    const totalCommitment = await this.getTotalCommitmentE8s();
-    return BigInt(totalCommitment - (await this.getNFCommitmentE8s()));
   }
 }
