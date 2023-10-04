63449f3dd9e2c83218c05bf75f0d7dace57ed9bf
David Dal Busco <david.dalbusco@dfinity.org>
2023-10-04 14:30:47 +0200

    test: migrate Accounts components to vitest

---
+++ frontend/src/vitests/lib/components/IcrcTransactionsObserver.spec.ts
@@ -0,0 +1,71 @@
+import IcrcTransactionsObserver from "$lib/components/accounts/IcrcTransactionsObserver.svelte";
+import { FETCH_ROOT_KEY, HOST } from "$lib/constants/environment.constants";
+import type { TransactionsObserverData } from "$lib/types/icrc.observer";
+import type { PostMessageDataRequestTransactions } from "$lib/types/post-message.transactions";
+import { mockMainAccount } from "$tests/mocks/icp-accounts.store.mock";
+import { indexCanisterIdMock } from "$tests/mocks/sns.api.mock";
+import { render, waitFor } from "@testing-library/svelte";
+
+describe("IcrcTransactionsObserver", () => {
+  let spyPostMessage;
+
+  beforeEach(() => {
+    spyPostMessage = vi.fn();
+
+    vi.doMock("$lib/workers/transactions.worker?worker", () => ({
+      default: class TransactionsWorker {
+        postMessage(data: {
+          msg: "nnsStartTransactionsTimer";
+          data: PostMessageDataRequestTransactions;
+        }) {
+          spyPostMessage(data);
+        }
+      },
+    }));
+  });
+
+  const data: TransactionsObserverData = {
+    indexCanisterId: indexCanisterIdMock.toText(),
+    account: mockMainAccount,
+  };
+
+  it("should init worker with parameters", async () => {
+    render(IcrcTransactionsObserver, {
+      props: {
+        data,
+        callback: vi.fn(),
+      },
+    });
+
+    await waitFor(() =>
+      expect(spyPostMessage).toBeCalledWith({
+        msg: "nnsStartTransactionsTimer",
+        data: {
+          indexCanisterId: data.indexCanisterId,
+          accountIdentifiers: [data.account.identifier],
+          host: HOST,
+          fetchRootKey: FETCH_ROOT_KEY,
+        },
+      })
+    );
+  });
+
+  it("should stop worker on destroy", async () => {
+    const { unmount } = render(IcrcTransactionsObserver, {
+      props: {
+        data,
+        callback: vi.fn(),
+      },
+    });
+
+    await waitFor(() => expect(spyPostMessage).toHaveBeenCalledTimes(1));
+
+    unmount();
+
+    await waitFor(() => expect(spyPostMessage).toHaveBeenCalledTimes(2));
+
+    expect(spyPostMessage).toBeCalledWith({
+      msg: "nnsStopTransactionsTimer",
+    });
+  });
+});
