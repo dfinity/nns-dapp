65612a2c78c4cab4ba62af3aaea63d1a83131714
David Dal Busco <david.dalbusco@dfinity.org>
2023-10-04 07:35:24 +0200

    build: init vitest

---
+++ frontend/vitest-setup.ts
@@ -0,0 +1,95 @@
+import { Crypto as SubtleCrypto } from "@peculiar/webcrypto";
+import "@testing-library/jest-dom";
+import { configure } from "@testing-library/svelte";
+// jsdom does not implement TextEncoder
+// Polyfill the encoders with node
+import { TextDecoder, TextEncoder } from "util";
+import { vi } from "vitest";
+import { browser, building } from "./__mocks__/$app/environment";
+import { afterNavigate, goto } from "./__mocks__/$app/navigation";
+import { page } from "./__mocks__/$app/stores";
+import { IntersectionObserverPassive } from "./src/tests/mocks/infinitescroll.mock";
+import localStorageMock from "./src/tests/mocks/local-storage.mock";
+import { failTestsThatLogToConsole } from "./src/tests/utils/console.test-utils";
+import {
+  mockedConstants,
+  setDefaultTestConstants,
+} from "./src/tests/utils/mockable-constants.test-utils";
+
+// Mock SubtleCrypto to test @dfinity/auth-client
+const crypto = new SubtleCrypto();
+Object.defineProperty(global, "crypto", {
+  value: crypto,
+});
+
+global.TextEncoder = TextEncoder;
+(global as { TextDecoder: typeof TextDecoder }).TextDecoder = TextDecoder;
+(
+  global as { IntersectionObserver: typeof IntersectionObserver }
+).IntersectionObserver = IntersectionObserverPassive;
+
+// Environment Variables Setup
+vi.mock("./src/lib/utils/env-vars.utils.ts", () => ({
+  getEnvVars: () => ({
+    ckbtcIndexCanisterId: "n5wcd-faaaa-aaaar-qaaea-cai",
+    ckbtcLedgerCanisterId: "mxzaz-hqaaa-aaaar-qaada-cai",
+    cyclesMintingCanisterId: "rkp4c-7iaaa-aaaaa-aaaca-cai",
+    dfxNetwork: "testnet",
+    featureFlags: JSON.stringify({
+      ENABLE_CKBTC: true,
+      ENABLE_CKTESTBTC: true,
+      ENABLE_ICP_ICRC: false,
+      ENABLE_INSTANT_UNLOCK: true,
+      ENABLE_SNS_AGGREGATOR_STORE: true,
+      ENABLE_STAKE_NEURON_ICRC1: true,
+      ENABLE_SWAP_ICRC1: true,
+      ENABLE_FULL_WIDTH_PROPOSAL: true,
+      TEST_FLAG_EDITABLE: true,
+      TEST_FLAG_NOT_EDITABLE: true,
+    }),
+    fetchRootKey: "false",
+    host: "https://icp-api.io",
+    governanceCanisterId: "rrkah-fqaaa-aaaaa-aaaaq-cai",
+    identityServiceUrl: "http://localhost:8000/",
+    ledgerCanisterId: "ryjl3-tyaaa-aaaaa-aaaba-cai",
+    ownCanisterId: "qhbym-qaaaa-aaaaa-aaafq-cai",
+    // Environments without SNS aggregator are valid
+    snsAggregatorUrl:
+      "https://5v72r-4aaaa-aaaaa-aabnq-cai.small12.testnet.dfinity.network",
+    wasmCanisterId: "u7xn3-ciaaa-aaaaa-aaa4a-cai",
+    tvlCanisterId: "ewh3f-3qaaa-aaaap-aazjq-cai",
+  }),
+}));
+
+vi.mock("./src/lib/constants/mockable.constants.ts", () => mockedConstants);
+setDefaultTestConstants({
+  DEV: false,
+  ENABLE_METRICS: false,
+  FORCE_CALL_STRATEGY: undefined,
+  IS_TEST_ENV: true,
+  QR_CODE_RENDERED_DEFAULT_STATE: true,
+  ENABLE_QR_CODE_READER: false,
+});
+
+global.localStorage = localStorageMock;
+
+failTestsThatLogToConsole();
+
+// testing-library setup
+configure({
+  testIdAttribute: "data-tid",
+});
+
+vi.mock("$app/environment", () => ({
+  browser,
+  building,
+}));
+
+vi.mock("$app/navigation", () => ({
+  goto,
+  afterNavigate,
+}));
+
+vi.mock("$app/stores", () => ({
+  page,
+}));
