e132b88b9fa55efe1caf4bc691be3922509ff4d1
David Dal Busco <david.dalbusco@dfinity.org>
2023-10-05 08:26:57 +0200

    test: migrare routes test to vitest

--- frontend/src/vitests/routes/app/wallet/page.spec.ts
+++ frontend/src/vitests/routes/app/wallet/page.spec.ts
@@ -1,19 +1,15 @@
-/**
- * @jest-environment jsdom
- */
-
 import { authStore } from "$lib/stores/auth.store";
-import CanisterPage from "$routes/(app)/(nns)/canister/+page.svelte";
+import WalletPage from "$routes/(app)/(u)/(detail)/wallet/+page.svelte";
 import {
   authStoreMock,
   mutableMockAuthStoreSubscribe,
 } from "$tests/mocks/auth.store.mock";
 import { render } from "@testing-library/svelte";
 
-describe("Canister page", () => {
-  jest
-    .spyOn(authStore, "subscribe")
-    .mockImplementation(mutableMockAuthStoreSubscribe);
+describe("Wallet page", () => {
+  vi.spyOn(authStore, "subscribe").mockImplementation(
+    mutableMockAuthStoreSubscribe
+  );
 
   beforeAll(() => {
     authStoreMock.next({
@@ -21,13 +17,13 @@ describe("Canister page", () => {
     });
   });
 
-  afterAll(() => jest.clearAllMocks());
+  afterAll(() => vi.clearAllMocks());
 
   it("should render sign-in if not logged in", () => {
-    const { getByTestId } = render(CanisterPage, {
+    const { getByTestId } = render(WalletPage, {
       props: {
         data: {
-          canister: "test",
+          account: "test",
         },
       },
     });
