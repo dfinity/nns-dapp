c6db9c37fe0714f21d99373d96e96ec421df6e4b
Max Murphy <max@winning.black>
2023-10-06 13:56:35 +0200

    CLI tool for proposal rendering

---
+++ rs/proposals/src/main.rs
@@ -0,0 +1,17 @@
+use ic_nns_governance::pb::v1::ProposalInfo;
+use proposals as lib;
+use std::io::{self, Read};
+
+/// Reads IDL from stdin, writes JSON to stdout.
+fn main() {
+    let mut idl_str = String::new();
+    io::stdin()
+        .read_to_string(&mut idl_str)
+        .expect("Failed to read string from stdin");
+    let idl_bytes = hex::decode(idl_str.trim()).expect("Invalid hex");
+    let (idl_args,): (Option<ProposalInfo>,) = candid::decode_args(&idl_bytes).expect("Malformed input");
+    let idl_args = idl_args.expect("Missing ProposalInfo");
+
+    let json_str = lib::process_proposal_payload(idl_args);
+    println!("{json_str}");
+}
