663eee54f1ab207181a2f619501795bbb5e90c8c
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-10-10 11:51:17 +0200

    GIX-1948: Use success intent for accepting participation tag (#3503)
    # Motivation
    
    Improve the information on the status of a swap by changing the Tag
    style when the swap is open.
    
    # Changes
    
    * Set the "intent" of the Tag to "success" when the swap is open.
    
    # Tests
    
    * Test that the Tag has "success" when open and "info" when not.
    
    # Todos
    
    - [x] Add entry to changelog (if necessary).

--- frontend/src/vitests/lib/components/project-detail/ProjectStatus.spec.ts
+++ frontend/src/vitests/lib/components/project-detail/ProjectStatus.spec.ts
@@ -18,17 +18,19 @@ describe("ProjectStatus", () => {
     resetSnsFinalizationStatusStore();
   });
 
-  it("should render accepting participation text when open", () => {
-    const { queryByText } = renderContextCmp({
+  it("should render accepting participation text when open with success intent", () => {
+    const { queryByText, queryByTestId } = renderContextCmp({
       summary: summaryForLifecycle(SnsSwapLifecycle.Open),
       swapCommitment: mockSnsFullProject.swapCommitment as SnsSwapCommitment,
       Component: ProjectStatus,
     });
     expect(queryByText(en.sns_project_detail.status_open)).toBeInTheDocument();
+    expect(queryByTestId("tag").classList.contains("success")).toBe(true);
+    expect(queryByTestId("tag").classList.contains("info")).toBe(false);
   });
 
   it("should render pending text when not yet open", () => {
-    const { queryByText } = renderContextCmp({
+    const { queryByText, queryByTestId } = renderContextCmp({
       summary: summaryForLifecycle(SnsSwapLifecycle.Pending),
       swapCommitment: mockSnsFullProject.swapCommitment as SnsSwapCommitment,
       Component: ProjectStatus,
@@ -37,10 +39,12 @@ describe("ProjectStatus", () => {
     expect(
       queryByText(en.sns_project_detail.status_pending)
     ).toBeInTheDocument();
+    expect(queryByTestId("tag").classList.contains("info")).toBe(true);
+    expect(queryByTestId("tag").classList.contains("success")).toBe(false);
   });
 
   it("should render unspecified text if not defined", () => {
-    const { queryByText } = renderContextCmp({
+    const { queryByText, queryByTestId } = renderContextCmp({
       summary: summaryForLifecycle(SnsSwapLifecycle.Unspecified),
       swapCommitment: mockSnsFullProject.swapCommitment as SnsSwapCommitment,
       Component: ProjectStatus,
@@ -49,10 +53,12 @@ describe("ProjectStatus", () => {
     expect(
       queryByText(en.sns_project_detail.status_unspecified)
     ).toBeInTheDocument();
+    expect(queryByTestId("tag").classList.contains("info")).toBe(true);
+    expect(queryByTestId("tag").classList.contains("success")).toBe(false);
   });
 
   it("should render committed text", () => {
-    const { queryByText } = renderContextCmp({
+    const { queryByText, queryByTestId } = renderContextCmp({
       summary: summaryForLifecycle(SnsSwapLifecycle.Committed),
       swapCommitment: mockSnsFullProject.swapCommitment as SnsSwapCommitment,
       Component: ProjectStatus,
@@ -61,10 +67,11 @@ describe("ProjectStatus", () => {
     expect(
       queryByText(en.sns_project_detail.status_committed)
     ).toBeInTheDocument();
+    expect(queryByTestId("tag").classList.contains("info")).toBe(true);
   });
 
   it("should render starting soon text", () => {
-    const { queryByText } = renderContextCmp({
+    const { queryByText, queryByTestId } = renderContextCmp({
       summary: summaryForLifecycle(SnsSwapLifecycle.Adopted),
       swapCommitment: mockSnsFullProject.swapCommitment as SnsSwapCommitment,
       Component: ProjectStatus,
@@ -73,10 +80,12 @@ describe("ProjectStatus", () => {
     expect(
       queryByText(en.sns_project_detail.status_adopted)
     ).toBeInTheDocument();
+    expect(queryByTestId("tag").classList.contains("info")).toBe(true);
+    expect(queryByTestId("tag").classList.contains("success")).toBe(false);
   });
 
   it("should render aborted text when cancelled", () => {
-    const { queryByText } = renderContextCmp({
+    const { queryByText, queryByTestId } = renderContextCmp({
       summary: summaryForLifecycle(SnsSwapLifecycle.Aborted),
       swapCommitment: mockSnsFullProject.swapCommitment as SnsSwapCommitment,
       Component: ProjectStatus,
@@ -85,6 +94,8 @@ describe("ProjectStatus", () => {
     expect(
       queryByText(en.sns_project_detail.status_aborted)
     ).toBeInTheDocument();
+    expect(queryByTestId("tag").classList.contains("info")).toBe(true);
+    expect(queryByTestId("tag").classList.contains("success")).toBe(false);
   });
 
   it("should render finalizing text when swap is finalizing", () => {
@@ -94,7 +105,7 @@ describe("ProjectStatus", () => {
       data: createFinalizationStatusMock(true),
       certified: true,
     });
-    const { queryByText } = renderContextCmp({
+    const { queryByText, queryByTestId } = renderContextCmp({
       summary,
       swapCommitment: mockSnsFullProject.swapCommitment as SnsSwapCommitment,
       Component: ProjectStatus,
@@ -103,6 +114,8 @@ describe("ProjectStatus", () => {
     expect(
       queryByText(en.sns_project_detail.status_finalizing)
     ).toBeInTheDocument();
+    expect(queryByTestId("tag").classList.contains("info")).toBe(true);
+    expect(queryByTestId("tag").classList.contains("success")).toBe(false);
   });
 
   it("should render committed text if finalizing data is not finalizing", () => {
