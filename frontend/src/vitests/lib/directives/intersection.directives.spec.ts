903572f9e594120bdfd7338e0fbbedf25349e404
David Dal Busco <david.dalbusco@dfinity.org>
2023-10-12 07:33:44 +0200

    test: migrate directives to vitest

--- frontend/src/vitests/lib/directives/intersection.directives.spec.ts
+++ frontend/src/vitests/lib/directives/intersection.directives.spec.ts
@@ -1,41 +1,32 @@
-/**
- * @jest-environment jsdom
- */
-
 import * as dispatchEvents from "$lib/utils/events.utils";
 import {
   IntersectionObserverActive,
-  IntersectionObserverPassive,
   mockIntersectionObserverIsIntersecting,
 } from "$tests/mocks/infinitescroll.mock";
 import { render } from "@testing-library/svelte";
 import IntersectionTest from "./IntersectionTest.svelte";
 
 describe("IntersectionDirectives", () => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore: test file
-  beforeAll(() => (global.IntersectionObserver = IntersectionObserverActive));
+  beforeAll(() =>
+      vi.stubGlobal("IntersectionObserver", IntersectionObserverActive)
+  );
 
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore: test file
-  afterAll(() => (global.IntersectionObserver = IntersectionObserverPassive));
+  afterAll(() => vi.unstubAllGlobals());
 
   let spy;
   let testIntersecting: boolean;
 
   beforeEach(
-    () =>
-      (spy = jest
-        .spyOn(dispatchEvents, "dispatchIntersecting")
-        .mockImplementation(
-          ({ intersecting }) => (testIntersecting = intersecting)
-        ))
+      () =>
+          (spy = vi
+              .spyOn(dispatchEvents, "dispatchIntersecting")
+              .mockImplementation(
+                  ($event) => (testIntersecting = $event?.intersecting ?? false)
+              ))
   );
-  afterEach(() => jest.clearAllMocks());
+  afterEach(() => vi.clearAllMocks());
 
   it("should trigger an intersect event", () => {
-    mockIntersectionObserverIsIntersecting(true);
-
     render(IntersectionTest);
 
     expect(spy).toHaveBeenCalled();
