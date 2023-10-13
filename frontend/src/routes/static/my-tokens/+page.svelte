af6ccca45c41e611e7cd70de231b3a1401ea5d41
Llorenç <llorenc.muntaner@dfinity.org>
2023-10-13 13:43:14 +0200

    GIX-1962: Static page

---
+++ frontend/src/routes/static/my-tokens/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import MyTokens from "$lib/pages/MyTokens.svelte";
+
+  // This is rendered in `/static/my-tokens/`.
+
+  // This would be a mocked data. Or we could put the mocked data in stores and use the same derived store.
+  const data = [
+    { name: "ICP", balance: "3.14", id: "1" },
+    { name: "Open Chat", balance: "1.2311", id: "2" },
+  ];
+</script>
+
+<MyTokens {data} />
