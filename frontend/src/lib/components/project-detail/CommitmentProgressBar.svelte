27ad98d0b4f2dc1319459900b47578e934d2f12c
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-10-17 13:01:14 +0200

    GIX-1974: Improve min indicator in commitment bar (#3560)
    # Motivation
    
    Improve the indicator of the minimum commitment in the project progress
    bar.
    
    # Changes
    
    * Add a triangle in top of the line and move it so it it overflows the
    line from above instead of below.
    
    # Tests
    
    * No new tests required. Same functionality.
    
    # Todos
    
    - [x] Add entry to changelog (if necessary).

--- frontend/src/lib/components/project-detail/CommitmentProgressBar.svelte
+++ frontend/src/lib/components/project-detail/CommitmentProgressBar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { TokenAmount, ICPToken } from "@dfinity/utils";
+  import { TokenAmount, ICPToken, nonNullish } from "@dfinity/utils";
   import { i18n } from "$lib/stores/i18n";
   import AmountDisplay from "../ic/AmountDisplay.svelte";
   import { ProgressBar } from "@dfinity/gix-components";
@@ -15,6 +15,10 @@
     minimumIndicator !== undefined && width !== undefined
       ? (Number(minimumIndicator) / Number(max)) * width
       : undefined;
+
+  // Triangle base is 12px, the middle is half that.
+  // But we want it aligned with the line below, that has width of 2px.
+  const triangleleftOffset = "(12px / 2) + (2px / 2)";
 </script>
 
 <ProgressBar
@@ -23,15 +27,25 @@
   {color}
   testId="commitment-progress-bar-component"
 >
+  <svelte:fragment slot="top">
+    {#if nonNullish(minIndicatorPosition)}
+      <div class="indicator-wrapper">
+        <div
+          class="triangle"
+          style={`left: calc(${minIndicatorPosition}px - ${triangleleftOffset});`}
+        ></div>
+        <div class="indicator-line-wrapper">
+          <span
+            class="min-indicator"
+            data-tid="commitment-min-indicator"
+            style={`left: calc(${minIndicatorPosition}px);`}
+          />
+        </div>
+      </div>
+    {/if}
+  </svelte:fragment>
   <div class="info" bind:clientWidth={width} slot="bottom">
     {#if minimumIndicator !== undefined}
-      <div class="indicator-wrapper">
-        <span
-          class="min-indicator"
-          data-tid="commitment-min-indicator"
-          style={`left: calc(${minIndicatorPosition}px);`}
-        />
-      </div>
       <div class="value-labels">
         <p class="value-label value-label-min">
           <span>
@@ -75,18 +89,34 @@
   }
 
   .indicator-wrapper {
-    height: var(--padding-2x);
+    --line-width: 2px;
+    --triangle-size: 6px;
 
-    position: relative;
-    // Position the indicator above the progress bar.
-    margin-top: calc(-1 * var(--padding-1_5x));
+    .triangle {
+      position: relative;
+      --triangle-sides-border: var(--triangle-size) solid transparent;
+      display: block;
+      width: 0;
+      height: 0;
+      border-left: var(--triangle-sides-border);
+      border-right: var(--triangle-sides-border);
+      border-top: var(--triangle-size) solid var(--positive-emphasis);
+
+      top: calc(-1 * var(--padding-0_5x));
+    }
+
+    .indicator-line-wrapper {
+      position: relative;
+      // Position the indicator above the progress bar.
+      margin-bottom: calc(-1 * var(--padding-1x));
+    }
   }
 
   .min-indicator {
     position: absolute;
     display: block;
 
-    width: 1px;
+    width: var(--line-width);
     height: var(--padding-1_5x);
 
     background-color: var(--positive-emphasis);
@@ -95,6 +125,8 @@
   .value-labels {
     display: flex;
     justify-content: space-between;
+
+    margin-top: var(--padding-0_5x);
   }
 
   .value-label {
