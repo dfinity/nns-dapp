5f97dfebc7ed53b28bca01c156fb4a0b97d0abd2
David Dal Busco <david.dalbusco@dfinity.org>
2023-10-20 17:11:50 +0200

    test: add delay to workaround flakyness

--- frontend/src/tests/e2e/accounts.spec.ts
+++ frontend/src/tests/e2e/accounts.spec.ts
@@ -29,7 +29,8 @@ test("Test accounts requirements", async ({ page, context }) => {
   expect(await accountNames()).toEqual([mainAccountName, subAccountName]);
 
   // The linked account should still be present after refresh
-  page.reload();
+  await page.reload();
+  await page.waitForTimeout(5000);
   expect(await accountNames()).toEqual([mainAccountName, subAccountName]);
 
   // Get some ICP to be able to transfer
