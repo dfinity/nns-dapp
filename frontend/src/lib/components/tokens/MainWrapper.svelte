f958dd98024b6ec865919dac2afce388e166e656
Llorenç <llorenc.muntaner@dfinity.org>
2023-10-23 09:43:37 +0200

    GIX-1982: Tokens page layout

---
+++ frontend/src/lib/components/tokens/MainWrapper.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  export let testId: string;
+</script>
+
+<main data-tid={testId}>
+  <div class="content">
+    <slot />
+  </div>
+</main>
+
+<style lang="scss">
+  @use "@dfinity/gix-components/dist/styles/mixins/media";
+
+  // TODO: Confirm spacings https://dfinity.atlassian.net/browse/GIX-2005
+  main {
+    display: flex;
+    align-items: stretch;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+
+    padding: var(--padding-2x) var(--padding-2x) var(--padding)
+      var(--padding-2x);
+
+    @include media.min-width(large) {
+      padding: var(--padding-4x) 0 0 0;
+    }
+
+    & .content {
+      width: 100%;
+      height: 100%;
+
+      overflow-x: hidden;
+      overflow-y: auto;
+
+      @include media.min-width(large) {
+        width: var(--island-width);
+      }
+    }
+  }
+</style>
