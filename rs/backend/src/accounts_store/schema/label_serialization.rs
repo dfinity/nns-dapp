c603ba866f33556277a1a77a0c63fa4b2a8540ae
Max <bitdivine@users.noreply.github.com>
2023-10-24 04:19:12 +0200

    Schema label checksum (#3590)
    # Motivation
    The schema migration calls for storing a schema ID+checksum. The schema
    ID enum is already defined but we still need serialization and parsing
    code.
    
    # Changes
    - Add a type corresponding to serializing the `SchemaLabel`. The
    serialized data includes a checksum.
    - Add serializing and parsing functions.
    
    # Tests
    - Unit tests are included.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
      None, as the new types are unused.
    
    ---------
    
    Co-authored-by: Max Murphy <max@winning.black>

---
+++ rs/backend/src/accounts_store/schema/label_serialization.rs
@@ -0,0 +1,108 @@
+//! Methods for serializing and deserializing schema labels.
+use super::{SchemaLabel, SchemaLabelBytes, SchemaLabelError};
+use ic_crypto_sha::Sha256;
+use std::convert::{TryFrom, TryInto};
+
+#[cfg(test)]
+mod tests;
+
+/// Internal type for just the serialized schema label without a checksum.
+type SchemaBytesWithoutChecksum = [u8; SchemaLabel::LABEL_BYTES];
+
+impl SchemaLabel {
+    /// When serialized, the offset of the bytes containing the label.
+    pub const LABEL_OFFSET: usize = 0;
+    /// The number of bytes needed to store just the schema label.
+    pub const LABEL_BYTES: usize = 4;
+    /// When serialized, the offset of the bytes containing the checksum, if included.
+    pub const CHECKSUM_OFFSET: usize = Self::LABEL_BYTES;
+    /// The length of the SHA256 checksum of the schema label.
+    pub const CHECKSUM_BYTES: usize = 32;
+    /// The number of bytes needed to store the schema label and its checksum.
+    pub const MAX_BYTES: usize = Self::CHECKSUM_OFFSET + Self::CHECKSUM_BYTES;
+    /// String used to distinguish this checksum from checksums for other purposes.
+    pub const DOMAIN_SEPARATOR: &[u8; 12] = b"schema-label";
+
+    /// Checksum of the label.
+    fn checksum(self_bytes: &[u8; Self::LABEL_BYTES]) -> [u8; Self::CHECKSUM_BYTES] {
+        let mut state = Sha256::new();
+        state.write(Self::DOMAIN_SEPARATOR);
+        state.write(self_bytes);
+        state.finish()
+    }
+
+    /// Adds a checksum to the label bytes.
+    fn with_checksum(label_bytes: SchemaBytesWithoutChecksum) -> SchemaLabelBytes {
+        let mut bytes = [0u8; SchemaLabel::MAX_BYTES];
+        bytes[SchemaLabel::LABEL_OFFSET..SchemaLabel::LABEL_OFFSET + SchemaLabel::LABEL_BYTES]
+            .copy_from_slice(&label_bytes);
+        let checksum = SchemaLabel::checksum(&label_bytes);
+        bytes[SchemaLabel::CHECKSUM_OFFSET..SchemaLabel::CHECKSUM_OFFSET + SchemaLabel::CHECKSUM_BYTES]
+            .copy_from_slice(&checksum);
+        bytes
+    }
+}
+
+impl TryFrom<u32> for SchemaLabel {
+    type Error = SchemaLabelError;
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(Self::Map),
+            #[cfg(test)]
+            1 => Ok(Self::AccountsInStableMemory),
+            _ => Err(SchemaLabelError::InvalidLabel),
+        }
+    }
+}
+
+impl From<SchemaLabel> for SchemaBytesWithoutChecksum {
+    fn from(label: SchemaLabel) -> [u8; SchemaLabel::LABEL_BYTES] {
+        (label as u32).to_le_bytes()
+    }
+}
+
+impl TryFrom<&SchemaBytesWithoutChecksum> for SchemaLabel {
+    type Error = SchemaLabelError;
+    fn try_from(value: &[u8; SchemaLabel::LABEL_BYTES]) -> Result<Self, Self::Error> {
+        let label_num = u32::from_le_bytes(*value);
+        Self::try_from(label_num)
+    }
+}
+
+impl From<SchemaLabel> for SchemaLabelBytes {
+    fn from(label: SchemaLabel) -> [u8; SchemaLabel::MAX_BYTES] {
+        let label_bytes = SchemaBytesWithoutChecksum::from(label);
+        SchemaLabel::with_checksum(label_bytes)
+    }
+}
+
+impl TryFrom<&SchemaLabelBytes> for SchemaLabel {
+    type Error = SchemaLabelError;
+    fn try_from(value: &[u8; SchemaLabel::MAX_BYTES]) -> Result<Self, Self::Error> {
+        let label_bytes: [u8; SchemaLabel::LABEL_BYTES] = value
+            [SchemaLabel::LABEL_OFFSET..SchemaLabel::LABEL_OFFSET + SchemaLabel::LABEL_BYTES]
+            .try_into()
+            .map_err(|_| SchemaLabelError::InsufficientBytes)?;
+        let actual_checksum =
+            &value[SchemaLabel::CHECKSUM_OFFSET..SchemaLabel::CHECKSUM_OFFSET + SchemaLabel::CHECKSUM_BYTES];
+        let expected_checksum = Self::checksum(&label_bytes);
+        if expected_checksum == *actual_checksum {
+            Self::try_from(&label_bytes)
+        } else {
+            Err(SchemaLabelError::InvalidChecksum)
+        }
+    }
+}
+
+impl TryFrom<&[u8]> for SchemaLabel {
+    type Error = SchemaLabelError;
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        let bytes: &SchemaLabelBytes = value
+            .chunks(SchemaLabel::MAX_BYTES)
+            .next()
+            .ok_or(SchemaLabelError::InsufficientBytes)? // Zero bytes lead to no chunks.
+            .try_into()
+            .map_err(|_| SchemaLabelError::InsufficientBytes)?; // There are some bytes but not enough to make a full chunk.
+        Self::try_from(bytes)
+    }
+}
---
+++ rs/backend/src/accounts_store/schema/label_serialization/tests.rs
@@ -0,0 +1,87 @@
+//! Tests for schema label serilaization.
+
+use super::*;
+use crate::accounts_store::schema::SchemaLabel;
+use strum::IntoEnumIterator;
+
+#[test]
+fn every_schema_label_should_serialize_and_deserialize() {
+    for label in SchemaLabel::iter() {
+        let label_bytes = SchemaLabelBytes::from(label);
+        let label_deserialized = SchemaLabel::try_from(&label_bytes).unwrap();
+        assert_eq!(label, label_deserialized);
+    }
+}
+
+#[test]
+fn unknown_schema_label_should_fail() {
+    let invalid_label_bytes_without_checksum = [0x69; 4];
+    assert!(
+        SchemaLabel::try_from(&invalid_label_bytes_without_checksum).is_err(),
+        "Test error: The bytes actually correspond to a legitimate schema label."
+    );
+    let label_bytes = SchemaLabel::with_checksum(invalid_label_bytes_without_checksum);
+    assert_eq!(Err(SchemaLabelError::InvalidLabel), SchemaLabel::try_from(&label_bytes));
+}
+
+#[test]
+fn unknown_schema_label_with_invalid_checksum_should_report_invalid_checksum() {
+    let label_bytes = [0x69; SchemaLabel::MAX_BYTES];
+    let just_the_label: &SchemaBytesWithoutChecksum = &label_bytes
+        [SchemaLabel::LABEL_OFFSET..SchemaLabel::LABEL_OFFSET + SchemaLabel::LABEL_BYTES]
+        .try_into()
+        .unwrap();
+    assert!(
+        SchemaLabel::try_from(just_the_label).is_err(),
+        "Test error: The bytes actually correspond to a legitimate schema label."
+    );
+    // Note: If that is a valid checksum I must have won the lottery a zillion times over.
+    assert_eq!(
+        Err(SchemaLabelError::InvalidChecksum),
+        SchemaLabel::try_from(&label_bytes)
+    );
+}
+
+#[test]
+fn valid_label_with_invalid_checksum_should_fail() {
+    let label_bytes = SchemaLabelBytes::from(SchemaLabel::Map);
+    let mut label_bytes_with_invalid_checksum = label_bytes;
+    label_bytes_with_invalid_checksum[SchemaLabel::CHECKSUM_OFFSET] ^= 1;
+    assert_eq!(
+        Err(SchemaLabelError::InvalidChecksum),
+        SchemaLabel::try_from(&label_bytes_with_invalid_checksum)
+    );
+}
+
+#[test]
+fn empty_slice_should_fail_with_length_error() {
+    let empty_slice: &[u8] = &[];
+    assert_eq!(
+        Err(SchemaLabelError::InsufficientBytes),
+        SchemaLabel::try_from(empty_slice)
+    );
+}
+
+#[test]
+fn short_slice_should_fail_with_length_error() {
+    let short_slice: &[u8] = &[8u8; SchemaLabel::MAX_BYTES - 1];
+    assert_eq!(
+        Err(SchemaLabelError::InsufficientBytes),
+        SchemaLabel::try_from(short_slice)
+    );
+}
+
+#[test]
+fn adequate_slice_should_succeed() {
+    let valid_label_bytes = SchemaLabelBytes::from(SchemaLabel::Map);
+    for surplus in [0, 100] {
+        let mut valid_label_bytes_with_surplus = vec![9; SchemaLabel::MAX_BYTES + surplus];
+        valid_label_bytes_with_surplus[..SchemaLabel::MAX_BYTES].copy_from_slice(&valid_label_bytes);
+        assert_eq!(
+            Ok(SchemaLabel::Map),
+            SchemaLabel::try_from(&valid_label_bytes_with_surplus[..]),
+            "Failed to parse with a surplus of {} bytes",
+            surplus
+        );
+    }
+}
