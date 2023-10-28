7f1532e9d611b51fe887225bddc1b18b7e97f841
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-10-28 16:36:26 +0200

    feat: switch between tree and raw payload data

--- frontend/src/lib/components/proposal-detail/ProposalProposerPayloadEntry.svelte
+++ frontend/src/lib/components/proposal-detail/ProposalProposerPayloadEntry.svelte
@@ -4,6 +4,8 @@
   import { SkeletonText } from "@dfinity/gix-components";
   import { expandObject } from "$lib/utils/utils";
   import { isNullish } from "@dfinity/utils";
+  import TreeRawToggle from "$lib/components/proposal-detail/DataRepresentationToggle.svelte";
+  import { type Writable, writable } from "svelte/store";
 
   // `undefined` means that the payload is not loaded yet
   // `null` means that the payload was not found
@@ -13,32 +15,62 @@
   $: expandedPayload = isNullish(payload)
     ? payload
     : expandObject(payload as Record<string, unknown>);
+
+  const toggleStore: Writable<"tree" | "raw"> = writable("tree");
 </script>
 
 <div class="content-cell-island">
-  <h2
-    class="content-cell-title"
-    data-tid="proposal-proposer-payload-entry-title"
-  >
-    {$i18n.proposal_detail.payload}
-  </h2>
+  <div class="header">
+    <h2
+      class="content-cell-title"
+      data-tid="proposal-proposer-payload-entry-title"
+    >
+      {$i18n.proposal_detail.payload}
+    </h2>
+
+    <TreeRawToggle store={toggleStore} />
+  </div>
 
   <div class="content-cell-details">
-    <!-- `null` payload should be shown as `null` -->
-    {#if expandedPayload !== undefined}
-      <div class="json" data-tid="json-wrapper">
-        <Json json={expandedPayload} />
-      </div>
-    {:else}
-      <SkeletonText />
-      <SkeletonText />
-      <SkeletonText />
-    {/if}
+    <div class="content-cell-island markdown-container">
+      <!-- `null` payload should be shown as `null` -->
+      {#if expandedPayload !== undefined}
+        {#if $toggleStore === "tree"}
+          <div class="json" data-tid="json-wrapper">
+            <Json json={expandedPayload} />
+          </div>
+        {:else}
+          TBD
+        {/if}
+      {:else}
+        <SkeletonText />
+        <SkeletonText />
+        <SkeletonText />
+      {/if}
+    </div>
   </div>
 </div>
 
 <style lang="scss">
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
   .json {
     word-break: break-word;
+  }
+
+  // TODO(max): rename and move to gix-components
+  .markdown-container {
+    // custom island styles
+    background: var(--card-background-disabled);
+    color: var(--description-color);
+  }
+
+  :global(.markdown-container > :last-child) {
+    // remove marginы from the markdown container to avoid extra spacing inside sub-island
+    margin-bottom: 0;
   }
 </style>
