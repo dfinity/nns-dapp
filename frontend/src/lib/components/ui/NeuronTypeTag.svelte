fbf99f30197c30f866dd9daf4b066b2945e6e1e1
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-10-28 11:05:47 +0200

    wip: neuronTypeTag types update

--- frontend/src/lib/components/ui/NeuronTypeTag.svelte
+++ frontend/src/lib/components/ui/NeuronTypeTag.svelte
@@ -5,36 +5,43 @@
   import TestIdWrapper from "$lib/components/common/TestIdWrapper.svelte";
   import { Tag } from "@dfinity/gix-components";
 
+  type NeuronIntent = "warning" | "info";
+  const toLabelKey = (type: NeuronType) => {
+    if (type === NeuronType.Seed) {
+      return "seed";
+    }
+    if (type === NeuronType.Ect) {
+      return "ect";
+    }
+  };
+
+  const toDescriptionKey = (type: NeuronType) => {
+    if (type === NeuronType.Seed) {
+      return "seedDescription";
+    }
+    if (type === NeuronType.Ect) {
+      return "ectDescription";
+    }
+  };
+
   export let type: NeuronType;
   export let top = false;
 
   let label: string;
-  $: label =
-    $i18n.neuron_types[
-      {
-        [NeuronType.Seed]: "seed",
-        [NeuronType.Ect]: "ect",
-      }[type]
-    ];
+  $: label = $i18n.neuron_types[toLabelKey(type)];
 
   let description: string;
-  $: description =
-    $i18n.neuron_types[
-      {
-        [NeuronType.Seed]: "seedDescription",
-        [NeuronType.Ect]: "ectDescription",
-      }[type]
-    ];
+  $: description = $i18n.neuron_types[toDescriptionKey(type)];
 
-  let intent: string;
-  $: intent = {
+  let intent: NeuronIntent;
+  $: intent = ({
     [NeuronType.Seed]: "warning",
     [NeuronType.Ect]: "info",
-  }[type];
+  }[type] ?? "info") as NeuronIntent;
 </script>
 
 <TestIdWrapper testId="neuron-type-tag">
-  <Tooltip text={description} {top}>
+  <Tooltip id="neuron-type-tag-tooltip" text={description} {top}>
     <Tag {intent}>{label}</Tag>
   </Tooltip>
 </TestIdWrapper>
