0b0bf39888b0e5947748d1d19079e11fa47afcb8
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-11-01 17:15:39 +0100

    refactor: simplify the toggle dom

--- frontend/src/lib/components/proposal-detail/JsonRepresentationModeToggle.svelte
+++ frontend/src/lib/components/proposal-detail/JsonRepresentationModeToggle.svelte
@@ -14,41 +14,31 @@
 </script>
 
 <div class="data-representation-toggle" data-tid="data-representation-toggle">
-  <div class="toggle">
-    <button
-      class="ghost"
-      type="button"
-      on:click={setTree}
-      data-tid="toggle-tree">{$i18n.proposal_detail.toggle_tree}</button
-    >
-    <Toggle
-      bind:checked
-      on:nnsToggle={toggle}
-      ariaLabel={$i18n.proposal_detail.toggle_lable}
-    />
-    <button class="ghost" type="button" on:click={setRaw} data-tid="toggle-tree"
-      >{$i18n.proposal_detail.toggle_raw}</button
-    >
-  </div>
+  <button class="ghost" type="button" on:click={setTree} data-tid="toggle-tree"
+    >{$i18n.proposal_detail.toggle_tree}</button
+  >
+  <Toggle
+    bind:checked
+    on:nnsToggle={toggle}
+    ariaLabel={$i18n.proposal_detail.toggle_lable}
+  />
+  <button class="ghost" type="button" on:click={setRaw} data-tid="toggle-tree"
+    >{$i18n.proposal_detail.toggle_raw}</button
+  >
 </div>
 
 <style lang="scss">
   .data-representation-toggle {
     display: flex;
     align-items: center;
+    grid-template-columns: repeat(3, auto);
+    grid-column-gap: 2px;
 
     font-size: var(--font-size-h4);
     line-height: var(--line-height-h4);
 
     padding: 0 var(--padding-0_5x);
     gap: var(--padding);
-  }
-
-  .toggle {
-    display: flex;
-    align-items: center;
-    grid-template-columns: repeat(3, auto);
-    grid-column-gap: 2px;
 
     :global(svg) {
       width: var(--padding-2x);
