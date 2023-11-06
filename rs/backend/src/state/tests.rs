6ad4c5c14405b975699e0cef5a487f90dc89b238
Max Murphy <max@winning.black>
2023-11-06 14:49:47 +0100

    Update tests

--- rs/backend/src/state/tests.rs
+++ rs/backend/src/state/tests.rs
@@ -37,11 +37,11 @@ fn schema_can_be_read_from_memory() {
     // Pre-populate the memory so that it contains more than just the schema.
     memory.write(0, &[0xa5u8; 1000]);
     // Without a checksummed schema, we should get schema None.
-    assert_eq!(None, State::schema_version_from_memory(memory.clone()));
+    assert_eq!(None, State::schema_version_from_memory(&memory));
     // With a checksummed schema, we should get the schema.
     for schema in SchemaLabel::iter() {
         let schema_bytes = SchemaLabelBytes::from(schema);
         memory.write(0, &schema_bytes);
-        assert_eq!(Some(schema), State::schema_version_from_memory(memory.clone()));
+        assert_eq!(Some(schema), State::schema_version_from_memory(&memory));
     }
 }
