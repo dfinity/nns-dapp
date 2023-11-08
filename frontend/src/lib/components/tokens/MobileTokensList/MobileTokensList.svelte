ebb97b917b8055e3a33b7f992d8f57b7736c95a9
Llorenç <llorenc.muntaner@dfinity.org>
2023-11-08 09:38:33 +0100

    Untitled commit

--- frontend/src/lib/components/tokens/MobileTokensList/MobileTokensList.svelte
+++ frontend/src/lib/components/tokens/MobileTokensList/MobileTokensList.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+  import type { UserTokenData } from "$lib/types/tokens-page";
+
   /* eslint-disable-next-line */
   export let userTokensData: UserTokenData[];
 </script>
 
-<div data-tid="mobile-tokens-list-component">Mobile List</div>
+<div data-tid="mobile-tokens-list-component">
+  {`Mobile List: ${userTokensData.length}`}
+</div>
