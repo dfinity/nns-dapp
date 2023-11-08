80c35dc46fd5f9cab3799ffb96e8c56fb9711887
Llorenç <llorenc.muntaner@dfinity.org>
2023-11-08 14:32:13 +0100

    Untitled commit

---
+++ frontend/src/lib/components/tokens/TokenBalance.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { UserTokenData } from "$lib/types/tokens-page";
+  import { UnavailableTokenAmount } from "$lib/utils/token.utils";
+  import AmountDisplay from "../ic/AmountDisplay.svelte";
+
+  export let userTokenData: UserTokenData;
+</script>
+
+{#if userTokenData.balance instanceof UnavailableTokenAmount}
+  <span data-tid="token-value-label"
+    >{`-/- ${userTokenData.balance.token.symbol}`}</span
+  >
+{:else}
+  <AmountDisplay singleLine amount={userTokenData.balance} />
+{/if}
