d174ab2583d9281e702bd8657999a4b1c4b29b9f
Llorenç <llorenc.muntaner@dfinity.org>
2023-11-10 11:31:15 +0100

    Untitled commit

--- frontend/src/lib/utils/sns-aggregator-converters.utils.ts
+++ frontend/src/lib/utils/sns-aggregator-converters.utils.ts
@@ -357,7 +357,7 @@ const convertDtoToSnsSummaryMetadata = (
 /**
  * Token metadata is given only if all IcrcTokenMetadata properties are defined.
  */
-const convertDtoToTokenMetadata = (
+export const convertDtoToTokenMetadata = (
   data: CachedSnsTokenMetadataDto
 ): IcrcTokenMetadata | undefined =>
   mapOptionalToken(convertIcrc1Metadata(data));
