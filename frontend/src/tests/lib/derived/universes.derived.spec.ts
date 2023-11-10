4d83b77cc635e7346a18c45a7463228df13b770f
Llorenç <llorenc.muntaner@dfinity.org>
2023-11-10 17:14:59 +0100

    Untitled commit

--- frontend/src/tests/lib/derived/universes.derived.spec.ts
+++ frontend/src/tests/lib/derived/universes.derived.spec.ts
@@ -1,6 +1,11 @@
 import { OWN_CANISTER_ID } from "$lib/constants/canister-ids.constants";
-import { CKBTC_UNIVERSE_CANISTER_ID } from "$lib/constants/ckbtc-canister-ids.constants";
+import {
+  CKBTC_UNIVERSE_CANISTER_ID,
+  CKTESTBTC_UNIVERSE_CANISTER_ID,
+} from "$lib/constants/ckbtc-canister-ids.constants";
 import { universesStore } from "$lib/derived/universes.derived";
+import { overrideFeatureFlagsStore } from "$lib/stores/feature-flags.store";
+import { principal } from "$tests/mocks/sns-projects.mock";
 import { rootCanisterIdMock } from "$tests/mocks/sns.api.mock";
 import { resetSnsProjects, setSnsProjects } from "$tests/utils/sns.test-utils";
 import { SnsSwapLifecycle } from "@dfinity/sns";
@@ -12,26 +17,97 @@ describe("universes derived stores", () => {
     resetSnsProjects();
   });
 
-  it("should return Nns, ckBTC and ckTESTBTC (flag for test is true) per default", () => {
-    const store = get(universesStore);
-    expect(store.length).toEqual(3);
-    expect(store[0].summary).toBeUndefined();
-    expect(store[0].canisterId).toEqual(OWN_CANISTER_ID.toText());
-    expect(store[1].summary).toBeUndefined();
-    expect(store[1].canisterId).toEqual(CKBTC_UNIVERSE_CANISTER_ID.toText());
+  describe("ckBTC both enabled", () => {
+    beforeEach(() => {
+      overrideFeatureFlagsStore.setFlag("ENABLE_CKBTC", true);
+      overrideFeatureFlagsStore.setFlag("ENABLE_CKTESTBTC", true);
+    });
+
+    it("should return Nns, ckBTC and ckTESTBTC (flag for test is true) per default", () => {
+      const store = get(universesStore);
+      expect(store.length).toEqual(3);
+      expect(store[0].summary).toBeUndefined();
+      expect(store[0].canisterId).toEqual(OWN_CANISTER_ID.toText());
+      expect(store[1].summary).toBeUndefined();
+      expect(store[1].canisterId).toEqual(CKBTC_UNIVERSE_CANISTER_ID.toText());
+      expect(store[2].summary).toBeUndefined();
+      expect(store[2].canisterId).toEqual(
+        CKTESTBTC_UNIVERSE_CANISTER_ID.toText()
+      );
+    });
+
+    it("should return Nns, ckBTC, ckTESTBTC (flag for test is true) and SNS projects", () => {
+      const snsRootCanisterId = rootCanisterIdMock;
+      setSnsProjects([
+        {
+          lifecycle: SnsSwapLifecycle.Committed,
+          rootCanisterId: snsRootCanisterId,
+        },
+      ]);
+      const store = get(universesStore);
+      expect(store.length).toEqual(4);
+      expect(store[3].summary).not.toBeUndefined();
+      expect(store[3].canisterId).toEqual(snsRootCanisterId.toText());
+    });
   });
 
-  it("should return Nns, ckBTC, ckTESTBTC (flag for test is true) and SNS projects", () => {
-    const snsRootCanisterId = rootCanisterIdMock;
-    setSnsProjects([
-      {
-        lifecycle: SnsSwapLifecycle.Committed,
-        rootCanisterId: snsRootCanisterId,
-      },
-    ]);
-    const store = get(universesStore);
-    expect(store.length).toEqual(4);
-    expect(store[3].summary).not.toBeUndefined();
-    expect(store[3].canisterId).toEqual(snsRootCanisterId.toText());
+  describe("only ckBTC enabled", () => {
+    beforeEach(() => {
+      overrideFeatureFlagsStore.setFlag("ENABLE_CKBTC", true);
+      overrideFeatureFlagsStore.setFlag("ENABLE_CKTESTBTC", false);
+    });
+
+    it("should return Nns, ckBTC and ckTESTBTC (flag for test is true) per default", () => {
+      const store = get(universesStore);
+      expect(store.length).toEqual(2);
+      expect(store[0].summary).toBeUndefined();
+      expect(store[0].canisterId).toEqual(OWN_CANISTER_ID.toText());
+      expect(store[1].summary).toBeUndefined();
+      expect(store[1].canisterId).toEqual(CKBTC_UNIVERSE_CANISTER_ID.toText());
+    });
+  });
+
+  describe("ckBTC NOT enabled", () => {
+    beforeEach(() => {
+      overrideFeatureFlagsStore.setFlag("ENABLE_CKBTC", false);
+      overrideFeatureFlagsStore.setFlag("ENABLE_CKTESTBTC", false);
+    });
+
+    it("should return Nns per default", () => {
+      const store = get(universesStore);
+      expect(store.length).toEqual(1);
+      expect(store[0].summary).toBeUndefined();
+      expect(store[0].canisterId).toEqual(OWN_CANISTER_ID.toText());
+    });
+
+    it("should return Nns and SNS projects", () => {
+      const snsRootCanisterId = rootCanisterIdMock;
+      setSnsProjects([
+        {
+          lifecycle: SnsSwapLifecycle.Committed,
+          rootCanisterId: snsRootCanisterId,
+        },
+      ]);
+      const store = get(universesStore);
+      expect(store.length).toEqual(2);
+      expect(store[1].summary).not.toBeUndefined();
+      expect(store[1].canisterId).toEqual(snsRootCanisterId.toText());
+    });
+
+    it("should not include open SNS projects", () => {
+      const snsRootCanisterId = rootCanisterIdMock;
+      setSnsProjects([
+        {
+          lifecycle: SnsSwapLifecycle.Committed,
+          rootCanisterId: snsRootCanisterId,
+        },
+        {
+          lifecycle: SnsSwapLifecycle.Open,
+          rootCanisterId: principal(2),
+        },
+      ]);
+      const store = get(universesStore);
+      expect(store.length).toEqual(2);
+    });
   });
 });
