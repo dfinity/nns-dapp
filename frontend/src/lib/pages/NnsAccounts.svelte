63b3c4b2064b4533d0bab587ca6d69d3b2b12fc8
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-11-17 07:18:22 +0100

    GIX-1980: Setup icp accounts with tokens table (#3775)
    # Motivation
    
    When we migrate to the tokens page, we also want a similar view for the
    ICP accounts, subaccounts and hardware wallet accounts.
    
    In this PR, I introduce the TokensTable for NNS Accounts.
    
    # Changes
    
    * Render `TokensTable` in `NnsAccounts` depending on feature flag.
    * Expect an optional list of `UserTokenData` in `NnsAccounts`.
    * Hard code a list of `UserTokenData` in the Accounts route and pass it
    to `NnsAccounts`.
    
    # Tests
    
    * Add test in NnsAccounts.spec that the tokens table is there if feature
    flag is enabled. I added myself some TODOs comments to remember
    improving the test before adding more features here.
    * Add methods to NnsAccountsPo.
    * Add test in Accounts route to check that tokens table is rendered if
    flag is enabled.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).

--- frontend/src/lib/pages/NnsAccounts.svelte
+++ frontend/src/lib/pages/NnsAccounts.svelte
@@ -11,37 +11,52 @@
     pollAccounts,
   } from "$lib/services/icp-accounts.services";
   import { ICPToken } from "@dfinity/utils";
+  import type { UserTokenData } from "$lib/types/tokens-page";
+  import { ENABLE_MY_TOKENS } from "$lib/stores/feature-flags.store";
+  import MainWrapper from "$lib/components/tokens/MainWrapper.svelte";
+  import TokensTable from "$lib/components/tokens/TokensTable/TokensTable.svelte";
 
   onMount(() => {
-    pollAccounts();
+    if (!$ENABLE_MY_TOKENS) {
+      pollAccounts();
+    }
   });
 
   onDestroy(() => {
     cancelPollAccounts();
   });
+
+  // TODO: Remove default value when we remove the feature flag
+  export let userTokensData: UserTokenData[] = [];
 </script>
 
-<div class="card-grid" data-tid="accounts-body">
-  {#if nonNullish($icpAccountsStore?.main)}
-    <!-- Workaround: Type checker does not get $accountsStore.main is defined here -->
-    {@const mainAccount = $icpAccountsStore.main}
+{#if $ENABLE_MY_TOKENS}
+  <MainWrapper testId="accounts-body">
+    <TokensTable {userTokensData} />
+  </MainWrapper>
+{:else}
+  <div class="card-grid" data-tid="accounts-body">
+    {#if nonNullish($icpAccountsStore?.main)}
+      <!-- Workaround: Type checker does not get $accountsStore.main is defined here -->
+      {@const mainAccount = $icpAccountsStore.main}
 
-    <AccountCard account={mainAccount} token={ICPToken}
-      >{$i18n.accounts.main}</AccountCard
-    >
-    {#each $icpAccountsStore.subAccounts ?? [] as subAccount}
-      <AccountCard account={subAccount} token={ICPToken}
-        >{subAccount.name}</AccountCard
+      <AccountCard account={mainAccount} token={ICPToken}
+        >{$i18n.accounts.main}</AccountCard
       >
-    {/each}
-    {#each $icpAccountsStore.hardwareWallets ?? [] as walletAccount}
-      <AccountCard account={walletAccount} token={ICPToken}
-        >{walletAccount.name}</AccountCard
-      >
-    {/each}
+      {#each $icpAccountsStore.subAccounts ?? [] as subAccount}
+        <AccountCard account={subAccount} token={ICPToken}
+          >{subAccount.name}</AccountCard
+        >
+      {/each}
+      {#each $icpAccountsStore.hardwareWallets ?? [] as walletAccount}
+        <AccountCard account={walletAccount} token={ICPToken}
+          >{walletAccount.name}</AccountCard
+        >
+      {/each}
 
-    <NnsAddAccount />
-  {:else}
-    <SkeletonCard size="medium" />
-  {/if}
-</div>
+      <NnsAddAccount />
+    {:else}
+      <SkeletonCard size="medium" />
+    {/if}
+  </div>
+{/if}
--- frontend/src/tests/routes/app/accounts/page.spec.ts
+++ frontend/src/tests/routes/app/accounts/page.spec.ts
@@ -1,29 +1,71 @@
-import { authStore } from "$lib/stores/auth.store";
+import { OWN_CANISTER_ID_TEXT } from "$lib/constants/canister-ids.constants";
+import { AppPath } from "$lib/constants/routes.constants";
+import { overrideFeatureFlagsStore } from "$lib/stores/feature-flags.store";
+import { page } from "$mocks/$app/stores";
 import AccountsPage from "$routes/(app)/(u)/(accounts)/accounts/+page.svelte";
-import {
-  authStoreMock,
-  mutableMockAuthStoreSubscribe,
-} from "$tests/mocks/auth.store.mock";
+import { resetIdentity, setNoIdentity } from "$tests/mocks/auth.store.mock";
+import { AccountsPo } from "$tests/page-objects/Accounts.page-object";
+import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
 import { render } from "@testing-library/svelte";
 
+vi.mock("$lib/api/ckbtc-ledger.api");
+vi.mock("$lib/api/sns-ledger.api");
+
 describe("Accounts page", () => {
-  vi.spyOn(authStore, "subscribe").mockImplementation(
-    mutableMockAuthStoreSubscribe
-  );
+  const renderComponent = () => {
+    const { container } = render(AccountsPage);
+    return AccountsPo.under(new JestPageObjectElement(container));
+  };
 
-  beforeAll(() => {
-    authStoreMock.next({
-      identity: undefined,
-    });
-  });
-
-  afterAll(() => {
+  beforeEach(() => {
     vi.clearAllMocks();
   });
 
-  it("should render sign-in if not logged in", () => {
-    const { getByTestId } = render(AccountsPage);
+  describe("not logged in", () => {
+    beforeEach(() => {
+      setNoIdentity();
+    });
 
-    expect(getByTestId("login-button")).not.toBeNull();
+    it("should render sign-in if not logged in", () => {
+      const { getByTestId } = render(AccountsPage);
+
+      expect(getByTestId("login-button")).not.toBeNull();
+    });
+  });
+
+  describe("logged in NNS Accounts", () => {
+    beforeEach(() => {
+      resetIdentity();
+      page.mock({
+        routeId: AppPath.Accounts,
+        data: { universe: OWN_CANISTER_ID_TEXT },
+      });
+    });
+
+    describe("tokens flag enabled", () => {
+      beforeEach(() => {
+        overrideFeatureFlagsStore.setFlag("ENABLE_MY_TOKENS", true);
+      });
+
+      it("renders tokens table for NNS accounts", async () => {
+        const po = renderComponent();
+
+        const pagePo = po.getNnsAccountsPo();
+        expect(await pagePo.hasTokensTable()).toBe(true);
+      });
+    });
+
+    describe("tokens flag disabled", () => {
+      beforeEach(() => {
+        overrideFeatureFlagsStore.setFlag("ENABLE_MY_TOKENS", false);
+      });
+
+      it("does not render tokens table for NNS accounts", async () => {
+        const po = renderComponent();
+
+        const pagePo = po.getNnsAccountsPo();
+        expect(await pagePo.hasTokensTable()).toBe(false);
+      });
+    });
   });
 });
