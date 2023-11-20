3b4ce33e77c1b72087fa6fee217c1aac34cb80d6
Llorenç <llorenc.muntaner@dfinity.org>
2023-11-20 16:08:34 +0100

    Untitled commit

--- frontend/src/routes/(app)/(u)/(accounts)/+page.svelte
+++ frontend/src/routes/(app)/(u)/(accounts)/+page.svelte
@@ -2,10 +2,13 @@
   import SignInAccounts from "$lib/pages/SignInAccounts.svelte";
   import Accounts from "$lib/routes/Accounts.svelte";
   import { authSignedInStore } from "$lib/derived/auth.derived";
+  import TestIdWrapper from "$lib/components/common/TestIdWrapper.svelte";
 </script>
 
-{#if $authSignedInStore}
-  <Accounts />
-{:else}
-  <SignInAccounts />
-{/if}
+<TestIdWrapper testId="accounts-plus-page-component">
+  {#if $authSignedInStore}
+    <Accounts />
+  {:else}
+    <SignInAccounts />
+  {/if}
+</TestIdWrapper>
