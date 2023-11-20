44e1dcd3fb1c1a8f22f549825e39404252946b36
Llorenç <llorenc.muntaner@dfinity.org>
2023-11-20 14:50:55 +0100

    Untitled commit

--- frontend/src/tests/page-objects/AccountsPlusPage.page-object.ts
+++ frontend/src/tests/page-objects/AccountsPlusPage.page-object.ts
@@ -1,7 +1,6 @@
 import { BasePageObject } from "$tests/page-objects/base.page-object";
 import type { PageObjectElement } from "$tests/types/page-object.types";
 import { AccountsPo } from "./Accounts.page-object";
-import { SignInAccountsPo } from "./SignInAccounts.page-object";
 
 export class AccountsPlusPagePo extends BasePageObject {
   private static readonly TID = "accounts-plus-page-component";
@@ -12,9 +11,5 @@ export class AccountsPlusPagePo extends BasePageObject {
 
   getAccountsPo(): AccountsPo {
     return AccountsPo.under(this.root);
-  }
-
-  getSignInAccountsPo(): SignInAccountsPo {
-    return SignInAccountsPo.under(this.root);
   }
 }
