b2b1d4e471900a693208e80dda3f49e61e01ef42
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-11-21 13:28:43 +0100

    GIX-2085: Use TokensTable in SignInAccounts (#3810)
    # Motivation
    
    The accounts page for non-logged in users should be similar to the
    logged-in page.
    
    Therefore, we use the TokensTable in SignInAccounts when flag is
    enabled.
    
    # Changes
    
    * Move the `MainWrapper` to the layout of the route accounts and apply
    it when flag is enabled.
    * Add logic in the `SignInAccounts` to render different components
    depending on flag value.
    * Use the `icpTokensListVisitors` when rendering `SignInAccounts` in the
    accounts route pages.
    
    # Tests
    
    * New SignInAccountsPo.
    * Add methods to PO
    * Add a test in the routes layout spec to check that token navigation is
    present or not depending on the flag.
    * Add tests in the routes page to check that either tokens table or
    empty cards are present depending on the flag.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    
    Not necessary.

--- frontend/src/routes/(app)/(u)/(accounts)/+layout.svelte
+++ frontend/src/routes/(app)/(u)/(accounts)/+layout.svelte
@@ -3,12 +3,23 @@
   import UniverseSplitContent from "$lib/components/layout/UniverseSplitContent.svelte";
   import { i18n } from "$lib/stores/i18n";
   import LayoutList from "$lib/components/layout/LayoutList.svelte";
+  import { ENABLE_MY_TOKENS } from "$lib/stores/feature-flags.store";
+  import MainWrapper from "$lib/components/tokens/MainWrapper.svelte";
+  import Content from "$lib/components/layout/Content.svelte";
 </script>
 
 <LayoutList title={$i18n.navigation.tokens}>
   <Layout>
-    <UniverseSplitContent>
-      <slot />
-    </UniverseSplitContent>
+    {#if $ENABLE_MY_TOKENS}
+      <Content>
+        <MainWrapper>
+          <slot />
+        </MainWrapper>
+      </Content>
+    {:else}
+      <UniverseSplitContent>
+        <slot />
+      </UniverseSplitContent>
+    {/if}
   </Layout>
 </LayoutList>
--- frontend/src/tests/page-objects/AccountsPlusPage.page-object.ts
+++ frontend/src/tests/page-objects/AccountsPlusPage.page-object.ts
@@ -1,6 +1,7 @@
 import { BasePageObject } from "$tests/page-objects/base.page-object";
 import type { PageObjectElement } from "$tests/types/page-object.types";
 import { AccountsPo } from "./Accounts.page-object";
+import { SignInAccountsPo } from "./SignInAccounts.page-object";
 
 export class AccountsPlusPagePo extends BasePageObject {
   private static readonly TID = "accounts-plus-page-component";
@@ -11,5 +12,9 @@ export class AccountsPlusPagePo extends BasePageObject {
 
   getAccountsPo(): AccountsPo {
     return AccountsPo.under(this.root);
+  }
+
+  getSignInAccountsPo(): SignInAccountsPo {
+    return SignInAccountsPo.under(this.root);
   }
 }
