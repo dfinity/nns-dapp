d9fe8c09a996f2e9b5bdaf8d5c8d239266984375
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-11-23 15:27:33 +0100

    test: adjust tests to writable-store logic update

--- frontend/src/tests/lib/stores/writable-stored.spec.ts
+++ frontend/src/tests/lib/stores/writable-stored.spec.ts
@@ -1,6 +1,5 @@
 import { StoreLocalStorageKey } from "$lib/constants/stores.constants";
 import { writableStored } from "$lib/stores/writable-stored";
-import { allowLoggingInOneTestForDebugging } from "$tests/utils/console.test-utils";
 import { get } from "svelte/store";
 import { describe } from "vitest";
 
@@ -48,51 +47,29 @@ describe("writableStored", () => {
     expect(get(store)).toEqual(defaultValue);
   });
 
+  it("should serialize bigint values", () => {
+    const defaultValue =
+      1000000000000000000000000000000000000000000000000000000n;
+    const store = writableStored({
+      key: StoreLocalStorageKey.ProposalFilters,
+      defaultValue,
+    });
+    expect(get(store)).toEqual(defaultValue);
+
+    const store2 = writableStored({
+      key: StoreLocalStorageKey.ProposalFilters,
+      defaultValue: 1,
+    });
+    expect(get(store2)).toEqual(defaultValue);
+  });
+
   describe("version upgrade", () => {
-    it("should replace deprecated value from local storage when LS version is older", () => {
-      const storedState = { filter: "old" };
-      const defaultValue = { filter: "new" };
+    it("should replace value from local storage when it has not the same version", () => {
+      const storedState = "123";
+      const defaultValue = { newValue: 123 };
       window.localStorage.setItem(
         StoreLocalStorageKey.ProposalFilters,
-        JSON.stringify({ data: storedState, version: 1 })
-      );
-      const store = writableStored({
-        key: StoreLocalStorageKey.ProposalFilters,
-        defaultValue,
-        version: 2,
-      });
-
-      expect(get(store)).toEqual(defaultValue);
-    });
-
-    it("should upgrade the structure of version-less value to versioned", () => {
-      const storedState = { filter: "old" };
-      const defaultValue = { filter: "new" };
-      window.localStorage.setItem(
-        StoreLocalStorageKey.ProposalFilters,
-        JSON.stringify(storedState)
-      );
-      expect(
-        window.localStorage.getItem(StoreLocalStorageKey.ProposalFilters)
-      ).toEqual(JSON.stringify(storedState));
-      const store = writableStored({
-        key: StoreLocalStorageKey.ProposalFilters,
-        defaultValue,
-        version: 1,
-      });
-
-      expect(get(store)).toEqual(defaultValue);
-      expect(
-        window.localStorage.getItem(StoreLocalStorageKey.ProposalFilters)
-      ).toEqual(JSON.stringify({ data: defaultValue, version: 1 }));
-    });
-
-    it("should not replace value from local storage when it has the same version", () => {
-      const storedState = { filter: "old" };
-      const defaultValue = { filter: "new" };
-      window.localStorage.setItem(
-        StoreLocalStorageKey.ProposalFilters,
-        JSON.stringify({ data: storedState, version: 1 })
+        JSON.stringify({ data: storedState, version: 0 })
       );
       const store = writableStored({
         key: StoreLocalStorageKey.ProposalFilters,
@@ -100,12 +77,12 @@ describe("writableStored", () => {
         version: 1,
       });
 
-      expect(get(store)).toEqual(storedState);
+      expect(get(store)).toEqual(defaultValue);
     });
 
-    it("should not replace value from local storage when it has older version", () => {
-      const storedState = { filter: "old" };
-      const defaultValue = { filter: "new" };
+    it("should replace value from local storage even when it has newer version", () => {
+      const storedState = null;
+      const defaultValue = 123;
       window.localStorage.setItem(
         StoreLocalStorageKey.ProposalFilters,
         JSON.stringify({ data: storedState, version: 2 })
@@ -116,15 +93,41 @@ describe("writableStored", () => {
         version: 1,
       });
 
-      expect(get(store)).toEqual(storedState);
+      expect(get(store)).toEqual(defaultValue);
+      expect(
+        localStorage.getItem(StoreLocalStorageKey.ProposalFilters)
+      ).toEqual(
+        JSON.stringify({
+          data: defaultValue,
+          version: 1,
+        })
+      );
     });
 
-    it("should not replace value w/o version when there is a versioned value in the store", () => {
-      const storedState = { filter: "old" };
-      const defaultValue = { filter: "new" };
+    it("should replace value from local storage even when the default has no version", () => {
+      const storedState = null;
+      const defaultValue = 123;
       window.localStorage.setItem(
         StoreLocalStorageKey.ProposalFilters,
-        JSON.stringify({ data: storedState, version: 1 })
+        JSON.stringify({ data: storedState, version: 2 })
+      );
+      const store = writableStored({
+        key: StoreLocalStorageKey.ProposalFilters,
+        defaultValue,
+      });
+
+      expect(get(store)).toEqual(defaultValue);
+      expect(
+        localStorage.getItem(StoreLocalStorageKey.ProposalFilters)
+      ).toEqual(JSON.stringify(defaultValue));
+    });
+
+    it("should not replace value in local storage when no versions provided", () => {
+      const storedState = { filter: "old" };
+      const defaultValue = 1;
+      window.localStorage.setItem(
+        StoreLocalStorageKey.ProposalFilters,
+        JSON.stringify(storedState)
       );
       const store = writableStored({
         key: StoreLocalStorageKey.ProposalFilters,
@@ -134,105 +137,41 @@ describe("writableStored", () => {
       expect(get(store)).toEqual(storedState);
     });
 
-    it("should not remove value from local storage when no version provided", () => {
+    it("should not replace value in local storage when same version", () => {
       const storedState = { filter: "old" };
-      const defaultValue = { filter: "new" };
+      const defaultValue = 1n;
       window.localStorage.setItem(
         StoreLocalStorageKey.ProposalFilters,
-        JSON.stringify(storedState)
+        JSON.stringify({ data: storedState, version: 5 })
       );
       const store = writableStored({
         key: StoreLocalStorageKey.ProposalFilters,
         defaultValue,
+        version: 5,
       });
 
       expect(get(store)).toEqual(storedState);
     });
   });
 
-  describe("upgradeStateVersion", () => {
-    it("should upgrade data version from version-less state", () => {
-      const storedState = { data: { filter: "old" } };
-      const upgradedValue = { filter: "new" };
-      window.localStorage.setItem(
-        StoreLocalStorageKey.ProposalFilters,
-        JSON.stringify(storedState)
-      );
+  describe("unsubscribeStorage", () => {
+    it("unsubscribes storing in local storage", async () => {
+      const defaultValue = { filter: "new" };
       const store = writableStored({
         key: StoreLocalStorageKey.ProposalFilters,
-        defaultValue: {},
+        defaultValue,
       });
-
-      store.upgradeStateVersion({ newVersionValue: upgradedValue, version: 1 });
-
-      expect(get(store)).toEqual(upgradedValue);
+      const newState = { filter: "old" };
+      store.set(newState);
       expect(
         window.localStorage.getItem(StoreLocalStorageKey.ProposalFilters)
-      ).toEqual(JSON.stringify({ data: upgradedValue, version: 1 }));
-    });
+      ).toEqual(JSON.stringify(newState));
 
-    it("should upgrade data version from versioned state", () => {
-      const storedState = { data: { filter: "old" }, version: 0 };
-      const upgradedValue = { filter: "new" };
-      window.localStorage.setItem(
-        StoreLocalStorageKey.ProposalFilters,
-        JSON.stringify(storedState)
-      );
-      const store = writableStored({
-        key: StoreLocalStorageKey.ProposalFilters,
-        defaultValue: {},
-      });
-
-      store.upgradeStateVersion({ newVersionValue: upgradedValue, version: 1 });
-
-      expect(get(store)).toEqual(upgradedValue);
+      store.unsubscribeStorage();
+      store.set(defaultValue);
       expect(
         window.localStorage.getItem(StoreLocalStorageKey.ProposalFilters)
-      ).toEqual(JSON.stringify({ data: upgradedValue, version: 1 }));
-    });
-
-    it("should skip data upgrade when the storage version is newer", () => {
-      // because of the error logging
-      allowLoggingInOneTestForDebugging();
-
-      const storedState = { data: { filter: "old" }, version: 2 };
-      const upgradedValue = { filter: "new" };
-      window.localStorage.setItem(
-        StoreLocalStorageKey.ProposalFilters,
-        JSON.stringify(storedState)
-      );
-      const store = writableStored({
-        key: StoreLocalStorageKey.ProposalFilters,
-        defaultValue: {},
-      });
-
-      store.upgradeStateVersion({ newVersionValue: upgradedValue, version: 1 });
-
-      expect(get(store)).toEqual(storedState.data);
-      expect(
-        window.localStorage.getItem(StoreLocalStorageKey.ProposalFilters)
-      ).toEqual(JSON.stringify(storedState));
-    });
-
-    describe("unsubscribeStorage", () => {
-      it("unsubscribes storing in local storage", async () => {
-        const defaultValue = { filter: "new" };
-        const store = writableStored({
-          key: StoreLocalStorageKey.ProposalFilters,
-          defaultValue,
-        });
-        const newState = { filter: "old" };
-        store.set(newState);
-        expect(
-          window.localStorage.getItem(StoreLocalStorageKey.ProposalFilters)
-        ).toEqual(JSON.stringify(newState));
-
-        store.unsubscribeStorage();
-        store.set(defaultValue);
-        expect(
-          window.localStorage.getItem(StoreLocalStorageKey.ProposalFilters)
-        ).toEqual(JSON.stringify(newState));
-      });
+      ).toEqual(JSON.stringify(newState));
     });
   });
 });
