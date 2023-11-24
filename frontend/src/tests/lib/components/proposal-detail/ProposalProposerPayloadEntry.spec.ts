9251730e11c424b63cbb25c54d5d8bba06bb1536
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-11-24 16:09:33 +0100

    test: update ProposalProposerActionsEntry specs

--- frontend/src/tests/lib/components/proposal-detail/ProposalProposerPayloadEntry.spec.ts
+++ frontend/src/tests/lib/components/proposal-detail/ProposalProposerPayloadEntry.spec.ts
@@ -1,32 +1,45 @@
 import ProposalProposerPayloadEntry from "$lib/components/proposal-detail/ProposalProposerPayloadEntry.svelte";
-import { overrideFeatureFlagsStore } from "$lib/stores/feature-flags.store";
+import { jsonRepresentationStore } from "$lib/stores/json-representation.store";
+import { JsonRepresentationModeTogglePo } from "$tests/page-objects/JsonRepresentationModeToggle.page-object";
+import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
 import { simplifyJson } from "$tests/utils/json.test-utils";
+import { expect } from "@playwright/test";
 import { render, waitFor } from "@testing-library/svelte";
 import { beforeEach } from "vitest";
 
 describe("ProposalProposerPayloadEntry", () => {
-  // TODO(GIX-2030) remove this once the feature flag is removed
-  beforeEach(() =>
-    overrideFeatureFlagsStore.setFlag("ENABLE_FULL_WIDTH_PROPOSAL", false)
-  );
-
   const nestedObj = { b: "c" };
-  const payloadWithJsonString = {
-    a: JSON.stringify(nestedObj),
-  };
+
+  beforeEach(() => {
+    jsonRepresentationStore.setMode("raw");
+  });
 
   it("should parse JSON strings and render them", async () => {
     const { queryByTestId } = render(ProposalProposerPayloadEntry, {
       props: {
-        payload: payloadWithJsonString,
+        payload: JSON.stringify(nestedObj),
       },
     });
 
-    const jsonElement = queryByTestId("json-wrapper");
+    const jsonElement = queryByTestId("json-preview-component");
     await waitFor(() =>
       expect(simplifyJson(jsonElement.textContent)).toBe(
-        simplifyJson(JSON.stringify(payloadWithJsonString))
+        simplifyJson(JSON.stringify("{b:c}"))
       )
     );
+  });
+
+  it("should render json mode toggle", async () => {
+    const { container } = render(ProposalProposerPayloadEntry, {
+      props: {
+        payload: JSON.stringify(nestedObj),
+      },
+    });
+
+    expect(
+      await JsonRepresentationModeTogglePo.under(
+        new JestPageObjectElement(container)
+      ).isPresent()
+    ).toBe(true);
   });
 });
