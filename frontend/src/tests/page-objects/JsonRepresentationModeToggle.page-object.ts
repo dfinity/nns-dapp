0d5b6c3b73d374722dd4b37cd58922d37c18e5f8
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-11-27 10:29:09 +0100

    Squashed commit of the following:
    commit caa305e8106690d7ab4108e231a3ddf93afc6bf8
    Author: mstrasinskis <maksims.strasinskis@dfinity.org>
    Date:   Mon Nov 27 09:52:06 2023 +0100
    
        test: for JsonRepresentationModeToggle

---
+++ frontend/src/tests/page-objects/JsonRepresentationModeToggle.page-object.ts
@@ -0,0 +1,10 @@
+import { TogglePo } from "$tests/page-objects/Toggle.page-object";
+import type { PageObjectElement } from "$tests/types/page-object.types";
+
+export class JsonRepresentationModeTogglePo extends TogglePo {
+  static readonly TID = "data-representation-mode-toggle-component";
+
+  static under(element: PageObjectElement): TogglePo {
+    return new TogglePo(element.byTestId(JsonRepresentationModeTogglePo.TID));
+  }
+}
