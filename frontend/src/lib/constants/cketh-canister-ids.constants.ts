47b78d808a1db96358ae8ad86841f9d7e3da066e
Llorenç <llorenc.muntaner@dfinity.org>
2023-11-27 10:36:44 +0100

    Untitled commit

---
+++ frontend/src/lib/constants/cketh-canister-ids.constants.ts
@@ -0,0 +1,37 @@
+import { getEnvVars } from "$lib/utils/env-vars.utils";
+import { Principal } from "@dfinity/principal";
+
+const envVars = getEnvVars();
+
+// TODO: Fallback to CKETH canister IDs on mainnet. These are CKBTC canister IDs.
+const MAINNET_CKETH_LEDGER_CANISTER_ID = "mxzaz-hqaaa-aaaar-qaada-cai";
+const MAINNET_CKETH_INDEX_CANISTER_ID = "n5wcd-faaaa-aaaar-qaaea-cai";
+const MAINNET_CKETH_MINTER_CANISTER_ID = "mqygn-kiaaa-aaaar-qaadq-cai";
+
+// TODO: Add ckETHTEST canister IDs on env vars https://dfinity.atlassian.net/browse/GIX-2137
+export const CKTESTETH_MINTER_CANISTER_ID = Principal.fromText(
+  "ml52i-qqaaa-aaaar-qaaba-cai"
+);
+
+export const CKTESTETH_LEDGER_CANISTER_ID = Principal.fromText(
+  "mc6ru-gyaaa-aaaar-qaaaq-cai"
+);
+
+export const CKTESTETH_INDEX_CANISTER_ID = Principal.fromText(
+  "mm444-5iaaa-aaaar-qaabq-cai"
+);
+
+// TODO: Add ckETH canister IDs on env vars https://dfinity.atlassian.net/browse/GIX-2137
+export const CKETH_LEDGER_CANISTER_ID = Principal.fromText(
+  envVars.ckbtcLedgerCanisterId ?? MAINNET_CKETH_LEDGER_CANISTER_ID
+);
+export const CKETH_INDEX_CANISTER_ID = Principal.fromText(
+  envVars.ckbtcIndexCanisterId ?? MAINNET_CKETH_INDEX_CANISTER_ID
+);
+export const CKETH_MINTER_CANISTER_ID = Principal.fromText(
+  envVars.ckbtcMinterCanisterId ?? MAINNET_CKETH_MINTER_CANISTER_ID
+);
+
+// Universes: the universe === the ledger canister ID
+export const CKETH_UNIVERSE_CANISTER_ID = CKETH_LEDGER_CANISTER_ID;
+export const CKETHTEST_UNIVERSE_CANISTER_ID = CKTESTETH_LEDGER_CANISTER_ID;
