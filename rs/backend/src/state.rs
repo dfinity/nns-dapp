a7ed27672090bbd73fa616044c1caafc03fabc23
Max Murphy <max@winning.black>
2023-11-28 11:54:17 +0100

    refactor

--- rs/backend/src/state.rs
+++ rs/backend/src/state.rs
@@ -12,6 +12,8 @@ use ic_stable_structures::{DefaultMemoryImpl, Memory};
 use on_wire::{FromWire, IntoWire};
 use partitions::Partitions;
 use regex::bytes;
+
+pub mod in_raw_memory;
 pub mod partitions;
 #[cfg(test)]
 pub mod tests;
@@ -171,27 +173,10 @@ impl State {
     }
 }
 
-// State from/to stable memory in the classic "Candid in raw memory" format.
-impl State {
-    /// Save any unsaved state to stable memory in the `SchemaLabel::Map` format.
-    fn save_to_raw_memory(&self) {
-        let bytes = self.encode();
-        stable::set(&bytes);
-    }
-    /// Create the state from stable memory in the `SchemaLabel::Map` format.
-    fn recover_from_raw_memory() -> Self {
-        let bytes = stable::get();
-        State::decode(bytes).unwrap_or_else(|e| {
-            trap_with(&format!("Decoding stable memory failed. Error: {e:?}"));
-            unreachable!();
-        })
-    }
-}
-
 // State from/to a stable memory partition in the `SchemaLabel::Map` format.
 impl State {
     /// Save as Map in virtual memory.
-    fn save_to_map(&self,  memory: DefaultMemoryImpl) {
+    fn save_to_map(&self, memory: DefaultMemoryImpl) {
         let bytes = self.encode();
         let len = bytes.len();
         let length_field = u64::try_from(len)
