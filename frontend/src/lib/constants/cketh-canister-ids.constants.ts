3daddd3cf144a4acba9b8148156efa5293a06ce1
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-11-28 13:00:39 +0100

    Chore: Add ckETHSEPOLIA index canister id (#3882)
    # Motivation
    
    ckETHSEPOLIA Index is already present in mainnet.
    
    In this PR, I add the mainnet canister id for that canister.
    
    # Changes
    
    * Changes in `cketh-canister-ids.constants`.
    
    # Tests
    
    Still pass.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    
    Not necessary.

--- frontend/src/lib/constants/cketh-canister-ids.constants.ts
+++ frontend/src/lib/constants/cketh-canister-ids.constants.ts
@@ -2,16 +2,14 @@ import { Principal } from "@dfinity/principal";
 
 // TODO: Fallback to ckETH canister IDs on mainnet. These are Sepolia canister IDs.
 const MAINNET_CKETH_LEDGER_CANISTER_ID = "apia6-jaaaa-aaaar-qabma-cai";
-// TODO: Change to Sepolia index canister
-const MAINNET_CKETH_INDEX_CANISTER_ID = "apia6-jaaaa-aaaar-qabma-cai";
+const MAINNET_CKETH_INDEX_CANISTER_ID = "sh5u2-cqaaa-aaaar-qacna-cai";
 
-// TODO: Add ckETHTEST canister IDs on env vars https://dfinity.atlassian.net/browse/GIX-2137
 export const CKETHSEPOLIA_LEDGER_CANISTER_ID = Principal.fromText(
   "apia6-jaaaa-aaaar-qabma-cai"
 );
 
 export const CKETHSEPOLIA_INDEX_CANISTER_ID = Principal.fromText(
-  "apia6-jaaaa-aaaar-qabma-cai"
+  "sh5u2-cqaaa-aaaar-qacna-cai"
 );
 
 // TODO: Add ckETH canister IDs on env vars https://dfinity.atlassian.net/browse/GIX-2137
