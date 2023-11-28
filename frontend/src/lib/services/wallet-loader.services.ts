bf4f85d781efd165f1255172042026947d4f5a2f
David Dal Busco <david.dalbusco@dfinity.org>
2023-11-28 14:53:17 +0100

    docs: add TODO

--- frontend/src/lib/services/wallet-loader.services.ts
+++ frontend/src/lib/services/wallet-loader.services.ts
@@ -3,15 +3,20 @@ import type { Account, AccountType } from "$lib/types/account";
 import type { Identity } from "@dfinity/agent";
 import type { Principal } from "@dfinity/principal";
 
-export const getAccounts = async ({
-  identity,
-  certified,
-  universeId,
-}: {
-  identity: Identity;
-  certified: boolean;
-  universeId: Principal;
-}): Promise<Account[]> => {
+/**
+ * TODO: This function is called `getAccounts` because it was originally refactored from a `getCkBtcAccounts` function. It can be renamed to something more suitable given that this services is a loader that load that from the API to the store.
+ */
+export const getAccounts = async (
+  {
+    identity,
+    certified,
+    universeId,
+  }: {
+    identity: Identity;
+    certified: boolean;
+    universeId: Principal;
+  }
+): Promise<Account[]> => {
   // TODO: Support subaccounts
   const mainAccount: { owner: Principal; type: AccountType } = {
     owner: identity.getPrincipal(),
