d834fa297f75c8c465792382acb5cab78f1cb8a3
Max Murphy <max@winning.black>
2023-11-28 11:58:01 +0100

    ++

--- rs/backend/src/state.rs
+++ rs/backend/src/state.rs
@@ -14,6 +14,7 @@ use partitions::Partitions;
 use regex::bytes;
 
 pub mod in_raw_memory;
+pub mod in_virtual_memory_map;
 pub mod partitions;
 #[cfg(test)]
 pub mod tests;
@@ -170,42 +171,6 @@ impl State {
     /// Save any unsaved state to stable memory.
     pub fn pre_upgrade(&self) {
         self.save_to_raw_memory()
-    }
-}
-
-// State from/to a stable memory partition in the `SchemaLabel::Map` format.
-impl State {
-    /// Save as Map in virtual memory.
-    fn save_to_map(&self, memory: DefaultMemoryImpl) {
-        let bytes = self.encode();
-        let len = bytes.len();
-        let length_field = u64::try_from(len)
-            .unwrap_or_else(|e| {
-                trap_with(&format!(
-                    "The serialized memory takes more than 2**64 bytes.  Amazing: {e:?}"
-                ));
-                unreachable!();
-            })
-            .to_be_bytes();
-        memory.write(0, &length_field);
-        memory.write(8, &bytes);
-    }
-    /// Create the state from stable memory in the `SchemaLabel::Map` format.
-    fn recover_from_map(memory: VirtualMemory<DefaultMemoryImpl>) -> Self {
-        let candid_len = {
-            let mut length_field = [0u8; 8];
-            memory.read(0, &mut length_field);
-            u64::from_be_bytes(length_field) as usize
-        };
-        let candid_bytes = {
-            let mut candid_bytes = vec![0u8; candid_len];
-            memory.read(8, &mut candid_bytes);
-            candid_bytes
-        };
-        State::decode(candid_bytes).unwrap_or_else(|e| {
-            trap_with(&format!("Decoding stable memory failed. Error: {e:?}"));
-            unreachable!();
-        })
     }
 }
 
