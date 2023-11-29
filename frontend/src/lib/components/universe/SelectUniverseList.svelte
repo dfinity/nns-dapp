4351f3c55cdf70e76380432f40566a621dd6eabb
David Dal Busco <david.dalbusco@dfinity.org>
2023-11-29 16:25:47 +0100

    feat: smoothness universes list appearance

--- frontend/src/lib/components/universe/SelectUniverseList.svelte
+++ frontend/src/lib/components/universe/SelectUniverseList.svelte
@@ -4,6 +4,7 @@
   import SelectUniverseCard from "$lib/components/universe/SelectUniverseCard.svelte";
   import { createEventDispatcher } from "svelte";
   import { selectedUniverseIdStore } from "$lib/derived/selected-universe.derived";
+  import { blur } from "svelte/transition";
 
   export let role: "link" | "button" = "link";
 
@@ -15,11 +16,13 @@
 
 <TestIdWrapper testId="select-universe-list-component">
   {#each $selectableUniversesStore as universe (universe.canisterId)}
-    <SelectUniverseCard
-      {universe}
-      {role}
-      selected={universe.canisterId === selectedCanisterId}
-      on:click={() => dispatch("nnsSelectUniverse", universe.canisterId)}
-    />
+    <div in:blur>
+      <SelectUniverseCard
+              {universe}
+              {role}
+              selected={universe.canisterId === selectedCanisterId}
+              on:click={() => dispatch("nnsSelectUniverse", universe.canisterId)}
+      />
+    </div>
   {/each}
 </TestIdWrapper>
