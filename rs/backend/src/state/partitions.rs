6c8e5f6da1521fbce95c0336353a3edefe4cc123
Max Murphy <max@winning.black>
2023-11-29 15:00:19 +0100

    ++

---
+++ rs/backend/src/state/partitions.rs
@@ -0,0 +1,85 @@
+//! Stable Memory Layout
+use super::State;
+use crate::accounts_store::schema::SchemaLabel;
+use core::borrow::Borrow;
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+use ic_stable_structures::{DefaultMemoryImpl, Memory};
+
+#[cfg(test)]
+pub mod tests;
+
+/// Memory layout consisting of a memory manager and some virtual memory.
+pub struct Partitions {
+    pub memory_manager: MemoryManager<DefaultMemoryImpl>,
+}
+impl Partitions {
+    /// The partition containing metadata such as schema version.
+    pub const METADATA_MEMORY_ID: MemoryId = MemoryId::new(0);
+    /// The partition containing heap data
+    pub const HEAP_MEMORY_ID: MemoryId = MemoryId::new(1);
+
+    /// Determines whether the given memory is managed by a memory manager.
+    fn is_managed(memory: &DefaultMemoryImpl) -> bool {
+        dfn_core::api::print(format!("START memory is_managed: ()"));
+        let memory_pages = memory.size();
+        if memory_pages == 0 {
+            return false;
+        }
+        // TODO: This is private in ic-stable-structures.  We should make it public, or have a public method for determining whether there is a memory manager at a given offset.
+        const MEMORY_MANAGER_MAGIC_BYTES: &[u8; 3] = b"MGR"; // From the spec: https://docs.rs/ic-stable-structures/0.6.0/ic_stable_structures/memory_manager/struct.MemoryManager.html#v1-layout
+        let mut actual_first_bytes = [0u8; MEMORY_MANAGER_MAGIC_BYTES.len()];
+        memory.read(0, &mut actual_first_bytes);
+        let ans = actual_first_bytes == *MEMORY_MANAGER_MAGIC_BYTES;
+        dfn_core::api::print(format!(
+            "END memory is_managed: {}, {:?}",
+            ans,
+            String::from_utf8(actual_first_bytes.to_vec())
+        ));
+        ans
+    }
+
+    /// Get the schema label
+    pub fn schema_label(&self) -> Option<SchemaLabel> {
+        dfn_core::api::print(format!("START Partitions::schema_label: ()"));
+        let metadata_memory = self.get(Self::METADATA_MEMORY_ID);
+        State::schema_version_from_memory(&metadata_memory)
+    }
+
+    /// Gets a partition.
+    pub fn get(&self, memory_id: MemoryId) -> VirtualMemory<DefaultMemoryImpl> {
+        self.memory_manager.borrow().get(memory_id)
+    }
+}
+
+impl From<DefaultMemoryImpl> for Partitions {
+    /// Gets an existing memory manager, if there is one.  If not, creates a new memory manager,
+    /// obliterating any existing memory.
+    ///
+    /// Note: This is equivalent to `MemoryManager::init()`.
+    fn from(memory: DefaultMemoryImpl) -> Self {
+        dfn_core::api::print(format!("START Partitions::from<DefaultMemoryImpl>: ()"));
+        let memory_manager = MemoryManager::init(memory);
+        Partitions { memory_manager }
+    }
+}
+
+/// Gets an existing memory manager, if there is one.  If not, returns the unmodified memory.
+///
+/// Typical usage:
+/// - The canister is upgraded.
+/// - The stable memory may contain a memory manager _or_ serialized heap data directly in raw memory.
+/// - This method gets the memory manager while being non-destructive if there is none.
+// Note: Would prefer to use TryFrom, but that causes a conflict.  DefaultMemoryImpl a type alias which
+// may refer to a type that has a generic implementation of TryFrom.  This is frustrating.
+//impl TryFrom<DefaultMemoryImpl> for Partitions {
+//    type Error = DefaultMemoryImpl;
+impl Partitions {
+    pub fn try_from_memory(memory: DefaultMemoryImpl) -> Result<Self, DefaultMemoryImpl> {
+        dfn_core::api::print(format!("START Partitions::try_from<DefaultMemoryImpl>: ()"));
+        if Self::is_managed(&memory) {
+            Ok(Self::from(memory))
+        } else {
+            Err(memory)
+        }
+    }
+}
---
+++ rs/backend/src/state/partitions/tests.rs
@@ -0,0 +1,124 @@
+//! Tests for stable memory layout code.
+use super::*;
+use crate::state::{tests::setup_test_state, StableState};
+
+#[test]
+fn is_managed_should_recognize_memory_manager() {
+    let toy_memory = DefaultMemoryImpl::default();
+    assert_eq!(
+        Partitions::is_managed(&toy_memory),
+        false,
+        "Empty (zero pages) of memory should not be recognized as a memory manager."
+    );
+    toy_memory.grow(5);
+    assert_eq!(
+        Partitions::is_managed(&toy_memory),
+        false,
+        "Blank, unpopulated memory should not be recognized as managed."
+    );
+    let old_style_memory = setup_test_state().encode();
+    toy_memory.write(0, &old_style_memory);
+    assert_eq!(Partitions::is_managed(&toy_memory), false, "Random fill or old style memory should not be recognized as managed.  There _should_ be only a 2**24 bit chance of a false positive.");
+    MemoryManager::init(toy_memory.clone()); // Note: The clone() clones the Rc, not the memory.
+    assert!(
+        Partitions::is_managed(&toy_memory),
+        "Memory manager should be recognized as managed."
+    );
+}
+
+#[test]
+fn should_be_able_to_get_partitions_from_managed_memory() {
+    fn should_contain(partitions: &Partitions, memory_id: MemoryId, expected_contents: &[u8]) {
+        let memory = partitions.get(memory_id);
+        let mut actual_contents = vec![0u8; expected_contents.len()];
+        memory.read(0, &mut actual_contents);
+        assert_eq!(
+            expected_contents, actual_contents,
+            "Partition {:?} should contain the expected contents.",
+            memory_id
+        );
+    }
+    // Prepare some memory.
+    let toy_memory = DefaultMemoryImpl::default();
+    assert!(
+        Partitions::try_from(toy_memory.clone()).is_err(),
+        "Zero bytes of memory should not yeild a partition table."
+    );
+    toy_memory.grow(5);
+    assert!(
+        Partitions::try_from(toy_memory.clone()).is_err(),
+        "Blank memory should not yield a partition table."
+    );
+    // Create a memory manager.
+    let memory_manager = MemoryManager::init(toy_memory.clone()); // Note: The clone() clones the Rc, not the memory.
+    let partitions = Partitions::try_from(toy_memory.clone());
+    assert!(
+        partitions.is_ok(),
+        "Managed memory should yield a partition table, even if it is empty."
+    );
+    // Check that the partitions are at least basically functional and refer to the same memory that was provided:
+    // The partitions should be empty:
+    let partitions = partitions.unwrap();
+    assert_eq!(
+        partitions.get(Partitions::METADATA_MEMORY_ID).size(),
+        0,
+        "Metadata partition should be empty."
+    );
+    assert_eq!(
+        partitions.get(Partitions::HEAP_MEMORY_ID).size(),
+        0,
+        "Heap partition should be empty."
+    );
+    // Populate a partition with some data.
+    let toy_metadata_fill = b"foo";
+    let metadata_memory = memory_manager.borrow().get(Partitions::METADATA_MEMORY_ID);
+    metadata_memory.grow(1);
+    assert_eq!(
+        partitions.get(Partitions::METADATA_MEMORY_ID).size(),
+        1,
+        "Metadata partition should have grown to 1."
+    );
+    assert_eq!(
+        partitions.get(Partitions::HEAP_MEMORY_ID).size(),
+        0,
+        "Heap partition should still be empty."
+    );
+    metadata_memory.write(0, toy_metadata_fill);
+    assert_eq!(
+        partitions.get(Partitions::METADATA_MEMORY_ID).size(),
+        1,
+        "Metadata partition should still be 1."
+    );
+    assert_eq!(
+        partitions.get(Partitions::HEAP_MEMORY_ID).size(),
+        0,
+        "Heap partition should still be empty."
+    );
+    should_contain(&partitions, Partitions::METADATA_MEMORY_ID, toy_metadata_fill);
+    // Populate another partition.
+    let toy_heap_fill = b"bar".repeat(1000);
+    let heap_memory = memory_manager.borrow().get(Partitions::HEAP_MEMORY_ID);
+    heap_memory.grow(2);
+    assert_eq!(
+        partitions.get(Partitions::METADATA_MEMORY_ID).size(),
+        1,
+        "Metadata partition should still be 1."
+    );
+    assert_eq!(
+        partitions.get(Partitions::HEAP_MEMORY_ID).size(),
+        0,
+        "Heap partition should have grown to 2."
+    );
+    heap_memory.write(0, &toy_heap_fill[..]);
+    assert_eq!(
+        partitions.get(Partitions::METADATA_MEMORY_ID).size(),
+        1,
+        "Metadata partition should still be 1."
+    );
+    assert_eq!(
+        partitions.get(Partitions::HEAP_MEMORY_ID).size(),
+        0,
+        "Heap partition should still be 2."
+    );
+    // Basic sanity check seems OK!
+}
