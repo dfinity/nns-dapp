c9f5442d61912d270a743646a96d18a9bdaa45c0
Llorenç <llorenc.muntaner@dfinity.org>
2023-12-05 11:55:18 +0100

    GIX-2150: New constant ULPS_PER_MATURITY

--- frontend/src/lib/api/sns.api.ts
+++ frontend/src/lib/api/sns.api.ts
@@ -1,4 +1,3 @@
-import { E8S_PER_ICP } from "$lib/constants/icp.constants";
 import type { SnsSwapCommitment } from "$lib/types/sns";
 import type {
   QueryRootCanisterId,
@@ -326,9 +325,7 @@ export const stakeNeuron = async ({
   source: IcrcAccount;
   fee: bigint;
 }): Promise<SnsNeuronId> => {
-  logWithTimestamp(
-    `Staking neuron with ${Number(stakeE8s) / E8S_PER_ICP}: call...`
-  );
+  logWithTimestamp(`Staking neuron with ${Number(stakeE8s)}: call...`);
 
   const { stakeNeuron: stakeNeuronApi } = await wrapper({
     identity,
@@ -345,9 +342,7 @@ export const stakeNeuron = async ({
     fee,
   });
 
-  logWithTimestamp(
-    `Staking neuron with ${Number(stakeE8s) / E8S_PER_ICP}: complete`
-  );
+  logWithTimestamp(`Staking neuron with ${Number(stakeE8s)}: complete`);
   return newNeuronId;
 };
 
@@ -364,9 +359,7 @@ export const increaseStakeNeuron = async ({
   identity: Identity;
   source: IcrcAccount;
 }): Promise<void> => {
-  logWithTimestamp(
-    `Increase stake neuron with ${Number(stakeE8s) / E8S_PER_ICP}: call...`
-  );
+  logWithTimestamp(`Increase stake neuron with ${Number(stakeE8s)}: call...`);
 
   const { increaseStakeNeuron: increaseStakeNeuronApi } = await wrapper({
     identity,
@@ -380,7 +373,5 @@ export const increaseStakeNeuron = async ({
     neuronId,
   });
 
-  logWithTimestamp(
-    `Increase stake neuron with ${Number(stakeE8s) / E8S_PER_ICP}: complete`
-  );
+  logWithTimestamp(`Increase stake neuron with ${Number(stakeE8s)}: complete`);
 };
