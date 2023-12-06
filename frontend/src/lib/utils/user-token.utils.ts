99370611a2f85155fa2161f885aa24c01a9c8243
Llorenç <llorenc.muntaner@dfinity.org>
2023-12-06 09:45:48 +0100

    Untitled commit

--- frontend/src/lib/utils/user-token.utils.ts
+++ frontend/src/lib/utils/user-token.utils.ts
@@ -1,11 +1,7 @@
-import type {
-  UserTokenBase,
-  UserTokenData,
-  UserTokenLoading,
-} from "$lib/types/tokens-page";
+import type { UserTokenData, UserTokenLoading } from "$lib/types/tokens-page";
 
 export const isUserTokenData = (
-  userToken: UserTokenData | UserTokenLoading | UserTokenBase
+  userToken: UserTokenData | UserTokenLoading
 ): userToken is UserTokenData => {
   return "balance" in userToken && userToken.balance !== "loading";
 };
