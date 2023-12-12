d1947ce8e7e351b74aa893fc03d3819caa1cffcb
Llorenç <llorenc.muntaner@dfinity.org>
2023-12-12 15:39:49 +0100

    GIX-2062: Use loading status Tokens Page

--- frontend/src/lib/types/tokens-page.ts
+++ frontend/src/lib/types/tokens-page.ts
@@ -35,3 +35,5 @@ export type UserTokenData = UserTokenBase & {
   // Fees are included in the metadata of ICRC tokens, but this is not a list of only ICRC tokens
   fee: TokenAmountV2;
 };
+
+export type UserToken = UserTokenLoading | UserTokenData;
