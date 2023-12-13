8beeb185b4a4ba4b7d36eb06f47e65c073da985a
sa-github-api <138766536+sa-github-api@users.noreply.github.com>
2023-12-13 14:21:37 +0100

    Update aggregator (#4015)
    # Motivation
    We would like to parse all the latest SNS data.
    
    # Changes
    * Updated the Rust code derived from `.did` files in the aggregator.
    * Note: The candid files under `declarations/nns-$CANISTER` are used as
    inputs.
    
    # Tests
    - [x] Please check the API updates for any breaking changes that affect
    our code.
    
    Co-authored-by: gix-bot <gix-bot@users.noreply.github.com>

--- rs/sns_aggregator/src/types/ic_sns_swap.rs
+++ rs/sns_aggregator/src/types/ic_sns_swap.rs
@@ -1,5 +1,5 @@
 //! Rust code created from candid by: scripts/did2rs.sh --canister sns_swap --out ic_sns_swap.rs --header did2rs.header --traits Serialize\,\ Clone\,\ Debug
-//! Candid for canister `sns_swap` obtained by `scripts/update_ic_commit` from: <https://raw.githubusercontent.com/dfinity/ic/release-2023-11-29_23-01/rs/sns/swap/canister/swap.did>
+//! Candid for canister `sns_swap` obtained by `scripts/update_ic_commit` from: <https://raw.githubusercontent.com/dfinity/ic/release-2023-12-06_23-01+p2p/rs/sns/swap/canister/swap.did>
 #![allow(clippy::all)]
 #![allow(unused_imports)]
 #![allow(clippy::missing_docs_in_private_items)]
