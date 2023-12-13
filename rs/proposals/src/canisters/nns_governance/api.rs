c9f7a9c37e88543d72f6fd94d861cf6a781483ef
Llorenç <llorenc.muntaner@dfinity.org>
2023-12-13 09:41:16 +0100

    Untitled commit

--- rs/proposals/src/canisters/nns_governance/api.rs
+++ rs/proposals/src/canisters/nns_governance/api.rs
@@ -4,13 +4,11 @@
 #![allow(clippy::missing_docs_in_private_items)]
 #![allow(non_camel_case_types)]
 #![allow(dead_code, unused_imports)]
-use candid::{self, Decode, Deserialize, Encode};
-use ic_cdk::export::Principal;
-use ic_cdk::export::candid::CandidType;
+use candid::{self, CandidType, Decode, Deserialize, Encode, Principal};
 use ic_cdk::api::call::CallResult;
 use serde::Serialize;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, CandidType, Deserialize)]
 pub struct EmptyRecord {}
 // This is an experimental feature to generate Rust binding from Candid.
 // You may want to manually adjust some of the types.
