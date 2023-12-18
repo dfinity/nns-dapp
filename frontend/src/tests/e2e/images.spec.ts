fff34a8ac85f99a3271560b4ca1e16046d3e1a9b
David Dal Busco <david.dalbusco@dfinity.org>
2023-12-18 11:25:18 +0100

    test: some images have no hash

--- frontend/src/tests/e2e/images.spec.ts
+++ frontend/src/tests/e2e/images.spec.ts
@@ -10,13 +10,12 @@ const expectImagesLoaded = async ({ page, sources }) => {
   );
   // We only look at the basename (stripping path and content hash extension)
   // because the rest might differ depending on the environment.
-  const baseImageSources = imageSources.map((src) =>
-    src
-      .replace(/.*\//, "")
-      .split(".")
-      .filter((_, i) => i !== 1) // Remove hash from src. e.g. "logo-onchain-light.L_mfZB1R.svg" -> "logo-onchain-light.svg"
-      .join(".")
-  );
+  const baseImageSources = imageSources.map((src) => {
+    const split = src.replace(/.*\//, "").split(".");
+    // Remove hash if existing from src. e.g. "logo-onchain-light.L_mfZB1R.svg" -> "logo-onchain-light.svg"
+    const { 0: filename, [split.length - 1]: extension } = split;
+    return `${filename}.${extension}`;
+  });
   baseImageSources.sort();
   expect(baseImageSources).toEqual(sources);
 
