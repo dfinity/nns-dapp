c93dcc59e53225795a81d1feeebbad1747116cde
Max Murphy <max@winning.black>
2023-12-19 12:07:33 +0100

    implement trait

--- rs/backend/src/accounts_store.rs
+++ rs/backend/src/accounts_store.rs
@@ -13,11 +13,13 @@ use ic_ledger_core::timestamp::TimeStamp;
 use ic_ledger_core::tokens::SignedTokens;
 use ic_nns_common::types::NeuronId;
 use ic_nns_constants::{CYCLES_MINTING_CANISTER_ID, GOVERNANCE_CANISTER_ID};
+use ic_stable_structures::{storable::Bound, Storable};
 use icp_ledger::Operation::{self, Approve, Burn, Mint, Transfer, TransferFrom};
 use icp_ledger::{AccountIdentifier, BlockIndex, Memo, Subaccount, Tokens};
 use itertools::Itertools;
 use on_wire::{FromWire, IntoWire};
 use serde::Deserialize;
+use std::borrow::Cow;
 use std::cmp::{min, Ordering};
 use std::collections::{BTreeMap, HashMap, HashSet, VecDeque};
 use std::fmt;
@@ -94,6 +96,20 @@ pub struct Account {
     sub_accounts: HashMap<u8, NamedSubAccount>,
     hardware_wallet_accounts: Vec<NamedHardwareWalletAccount>,
     canisters: Vec<NamedCanister>,
+}
+
+impl Storable for Account {
+    const BOUND: Bound = Bound::Unbounded;
+    fn to_bytes(&self) -> Cow<'_, [u8]> {
+        let account_serialized = Candid((self,)).into_bytes().expect("Failed to serialize account");
+        account_serialized.to_owned().into()
+    }
+    fn from_bytes(bytes: Cow<'_, [u8]>) -> Self {
+        let (account,) = Candid::from_bytes(bytes.as_ref().to_owned())
+            .map(|c| c.0)
+            .expect("Failed to parse account from store.");
+        account
+    }
 }
 
 #[derive(CandidType, Deserialize, Debug, Eq, PartialEq, Clone)]
