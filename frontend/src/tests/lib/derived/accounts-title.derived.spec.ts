5cb8a0fceff811f50be7e6e3e20c2c3d1c99a290
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2023-12-20 12:20:02 +0100

    GIX-2184: Add accountsTitleStore (#4090)
    # Motivation
    
    Change the title of the accounts page to have the name of the token on
    it.
    
    This is a change requested regarding the new tokens page, but I believe
    that it's a good change even for the current accounts page. Therefore, I
    didn't put it behind the feature flag.
    
    In this PR, I introduce the store that I want to use in the layout to
    set the title. But I don't use it yet.
    
    # Changes
    
    * New store `accountsTitleStore`.
    * New i18n key
    
    # Tests
    
    * Test new store `accountsTitleStore`.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    
    Not yet necessary.

---
+++ frontend/src/tests/lib/derived/accounts-title.derived.spec.ts
@@ -0,0 +1,49 @@
+import { OWN_CANISTER_ID_TEXT } from "$lib/constants/canister-ids.constants";
+import { AppPath } from "$lib/constants/routes.constants";
+import { accountsTitleStore } from "$lib/derived/accounts-title.derived";
+import { tokensStore } from "$lib/stores/tokens.store";
+import { page } from "$mocks/$app/stores";
+import { mockSnsToken } from "$tests/mocks/sns-projects.mock";
+import { rootCanisterIdMock } from "$tests/mocks/sns.api.mock";
+import { get } from "svelte/store";
+
+describe("accountsTitleStore", () => {
+  beforeEach(() => {
+    page.mock({
+      routeId: AppPath.Accounts,
+      data: { universe: OWN_CANISTER_ID_TEXT },
+    });
+    tokensStore.reset();
+  });
+
+  it("returns 'My Tokens' if no token is found", () => {
+    // the ICP token is hardcoded in the `tokensStore`, so it will always be found
+    page.mock({
+      routeId: AppPath.Accounts,
+      data: { universe: rootCanisterIdMock.toText() },
+    });
+
+    expect(get(accountsTitleStore)).toEqual("My Tokens");
+  });
+
+  it("returns the ICP in the title", () => {
+    expect(get(accountsTitleStore)).toEqual("My ICP Tokens");
+  });
+
+  it("returns the token of the universe in the title", () => {
+    page.mock({
+      routeId: AppPath.Accounts,
+      data: { universe: rootCanisterIdMock.toText() },
+    });
+
+    tokensStore.setToken({
+      canisterId: rootCanisterIdMock,
+      token: {
+        ...mockSnsToken,
+        symbol: "TTRS",
+      },
+    });
+
+    expect(get(accountsTitleStore)).toEqual("My TTRS Tokens");
+  });
+});
