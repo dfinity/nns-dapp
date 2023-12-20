f85315b5dc33e6c2ba8cd8befba42c2b6c148b8d
Max Strasinsky <maksims.strasinskis@dfinity.org>
2023-12-20 16:50:25 +0100

    feat: sns filter store types

--- frontend/src/lib/types/filters.ts
+++ frontend/src/lib/types/filters.ts
@@ -1,3 +1,10 @@
+// artificial proposal type id to filter by all generic SNS types
+export const ALL_GENERIC_PROPOSAL_TYPES_ID = "sns_specific";
+
+export type SnsProposalTypeFilterData =
+  | keyof I18nSns_types
+  | typeof ALL_GENERIC_PROPOSAL_TYPES_ID;
+
 export type Filter<T> = {
   name: string;
   value: T;
