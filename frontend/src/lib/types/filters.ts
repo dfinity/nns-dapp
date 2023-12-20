a94db3e292a55ced254440cf66b196393bc5daba
Max Strasinsky <maksims.strasinskis@dfinity.org>
2023-12-20 18:11:20 +0100

    refactor: ALL_SNS_GENERIC_PROPOSAL_TYPES_ID

--- frontend/src/lib/types/filters.ts
+++ frontend/src/lib/types/filters.ts
@@ -1,10 +1,10 @@
 // artificial proposal type id to filter by all generic SNS types
-export const ALL_GENERIC_PROPOSAL_TYPES_ID = "sns_specific";
+export const ALL_SNS_GENERIC_PROPOSAL_TYPES_ID = "all_sns_generic_types";
 
 // Stringified nsFunction id.
 export type SnsProposalTypeFilterId =
   | string
-  | typeof ALL_GENERIC_PROPOSAL_TYPES_ID;
+  | typeof ALL_SNS_GENERIC_PROPOSAL_TYPES_ID;
 
 export type Filter<T> = {
   name: string;
