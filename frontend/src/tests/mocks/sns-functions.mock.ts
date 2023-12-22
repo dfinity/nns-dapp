1f7e244868777607500f9511ab692d58483f08c2
Max Strasinsky <maksims.strasinskis@dfinity.org>
2023-12-22 10:54:15 +0100

    test: extend ns function mock

--- frontend/src/tests/mocks/sns-functions.mock.ts
+++ frontend/src/tests/mocks/sns-functions.mock.ts
@@ -6,3 +6,43 @@ export const nervousSystemFunctionMock: SnsNervousSystemFunction = {
   description: ["This is a description"],
   function_type: [{ NativeNervousSystemFunction: {} }],
 };
+
+export const allTopicsNervousSystemFunctionMock: SnsNervousSystemFunction = {
+  id: 0n,
+  name: "All Topics",
+  description: ["Catch-all w.r.t to following for all types of proposals."],
+  function_type: [
+    {
+      NativeNervousSystemFunction: {},
+    },
+  ],
+};
+
+export const nativeNervousSystemFunctionMock: SnsNervousSystemFunction = {
+  id: 1n,
+  name: "Motion",
+  description: [
+    "Side-effect-less proposals to set general governance direction.",
+  ],
+  function_type: [
+    {
+      NativeNervousSystemFunction: {},
+    },
+  ],
+};
+
+export const genericNervousSystemFunctionMock: SnsNervousSystemFunction = {
+  id: 1001n,
+  name: "Generic Motion",
+  description: ["Proposal to upgrade the WASM of a core SNS canister."],
+  function_type: [
+    {
+      GenericNervousSystemFunction: {
+        validator_canister_id: [],
+        target_canister_id: [],
+        validator_method_name: [],
+        target_method_name: [],
+      },
+    },
+  ],
+};
