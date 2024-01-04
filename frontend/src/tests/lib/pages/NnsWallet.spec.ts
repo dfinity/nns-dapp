097933f9043941e024ba206297af33c9736958a8
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2024-01-04 11:37:05 +0100

    Use page objects in NnsWallet.spec.ts (#4125)
    # Motivation
    
    Making `NnsWallet.spec.ts` more reliable and easier to maintain.
    
    # Changes
    
    1. Use page objects.
    2. Extend the page object with necessary methods.
    3. Expect explicit values defined in the test instead of values on a
    mock defined somewhere else.
    4. Do not rely on being able to make expectations before the component
    goes into loaded state but instead explicitly block API calls to
    simulate loading state.
    5. Remove all usages of `waitFor`.
    
    # Tests
    
    Still pass
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    not necessary

--- frontend/src/tests/lib/pages/NnsWallet.spec.ts
+++ frontend/src/tests/lib/pages/NnsWallet.spec.ts
@@ -1,15 +1,13 @@
 import * as accountsApi from "$lib/api/accounts.api";
 import * as ledgerApi from "$lib/api/icp-ledger.api";
 import * as nnsDappApi from "$lib/api/nns-dapp.api";
+import type { Transaction } from "$lib/canisters/nns-dapp/nns-dapp.types";
 import { SYNC_ACCOUNTS_RETRY_SECONDS } from "$lib/constants/accounts.constants";
 import NnsWallet from "$lib/pages/NnsWallet.svelte";
 import { cancelPollAccounts } from "$lib/services/icp-accounts.services";
 import { authStore } from "$lib/stores/auth.store";
 import { icpAccountsStore } from "$lib/stores/icp-accounts.store";
-import { replacePlaceholders } from "$lib/utils/i18n.utils";
-import { formatTokenE8s } from "$lib/utils/token.utils";
 import { mockAuthStoreSubscribe } from "$tests/mocks/auth.store.mock";
-import en from "$tests/mocks/i18n.mock";
 import {
   mockAccountDetails,
   mockAccountsStoreData,
@@ -17,18 +15,16 @@ import {
   mockMainAccount,
   mockSubAccount,
 } from "$tests/mocks/icp-accounts.store.mock";
-import {
-  modalToolbarSelector,
-  waitModalIntroEnd,
-} from "$tests/mocks/modal.mock";
-import { testAccountsModal } from "$tests/utils/accounts.test-utils";
+import { IcpTransactionModalPo } from "$tests/page-objects/IcpTransactionModal.page-object";
+import { NnsWalletPo } from "$tests/page-objects/NnsWallet.page-object";
+import { ReceiveModalPo } from "$tests/page-objects/ReceiveModal.page-object";
+import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
 import {
   advanceTime,
   runResolvedPromises,
 } from "$tests/utils/timers.test-utils";
-import { ICPToken } from "@dfinity/utils";
-import { fireEvent, render, waitFor } from "@testing-library/svelte";
-import { tick } from "svelte";
+import { Principal } from "@dfinity/principal";
+import { render } from "@testing-library/svelte";
 import type { SpyInstance } from "vitest";
 import AccountsTest from "./AccountsTest.svelte";
 
@@ -55,19 +51,52 @@ describe("NnsWallet", () => {
     vi.spyOn(accountsApi, "getTransactions").mockResolvedValue([]);
   });
 
-  const testToolbarButton = ({
-    container,
-    disabled,
-  }: {
-    container: HTMLElement;
-    disabled: boolean;
-  }) => {
-    const button = container.querySelector("footer div.toolbar button");
+  const renderWallet = async (props) => {
+    const { container } = render(NnsWallet, props);
+    await runResolvedPromises();
+    return NnsWalletPo.under(new JestPageObjectElement(container));
+  };
 
-    expect(button).not.toBeNull();
-    expect((button as HTMLButtonElement).hasAttribute("disabled")).toEqual(
-      disabled
+  const renderWalletAndModals = async (
+    props
+  ): Promise<{
+    walletPo: NnsWalletPo;
+    transactionModalPo: IcpTransactionModalPo;
+    receiveModalPo: ReceiveModalPo;
+  }> => {
+    const { container } = render(AccountsTest, props);
+    await runResolvedPromises();
+    return {
+      walletPo: NnsWalletPo.under(new JestPageObjectElement(container)),
+      transactionModalPo: IcpTransactionModalPo.under(
+        new JestPageObjectElement(container)
+      ),
+      receiveModalPo: ReceiveModalPo.under(
+        new JestPageObjectElement(container)
+      ),
+    };
+  };
+
+  const pauseQueryAccountBalance = () => {
+    let resolveQueryBalance;
+    vi.spyOn(ledgerApi, "queryAccountBalance").mockImplementation(
+      () =>
+        new Promise<bigint>((resolve) => {
+          resolveQueryBalance = () => resolve(mainBalanceE8s);
+        })
     );
+    return () => resolveQueryBalance();
+  };
+
+  const pauseGetTransactions = () => {
+    let resolveGetTransactions;
+    vi.spyOn(accountsApi, "getTransactions").mockImplementation(
+      () =>
+        new Promise<Transaction[]>((resolve) => {
+          resolveGetTransactions = () => resolve([]);
+        })
+    );
+    return () => resolveGetTransactions();
   };
 
   describe("no accounts", () => {
@@ -77,40 +106,42 @@ describe("NnsWallet", () => {
       );
     });
 
-    it("should render a spinner while loading", () => {
-      const { getByTestId } = render(NnsWallet);
+    it("should render a spinner while loading", async () => {
+      const po = await renderWallet({});
 
-      expect(getByTestId("spinner")).not.toBeNull();
+      expect(await po.hasSpinner()).toBe(true);
     });
 
-    it("new transaction action should be disabled while loading", () => {
-      const { container } = render(NnsWallet);
+    it("new transaction action should be disabled while loading", async () => {
+      const po = await renderWallet({});
 
-      testToolbarButton({ container, disabled: true });
+      expect(await po.getSendButtonPo().isDisabled()).toBe(true);
     });
 
     it("new transaction should remain disabled if route is valid but store is not loaded", async () => {
-      const { container } = render(NnsWallet, props);
+      const resolveQueryBalance = pauseQueryAccountBalance();
+      const po = await renderWallet(props);
 
-      // init
-      testToolbarButton({ container, disabled: true });
+      await runResolvedPromises();
+      expect(await po.getSendButtonPo().isDisabled()).toBe(true);
 
-      await tick();
+      resolveQueryBalance();
 
-      // route set triggers get account
-      testToolbarButton({ container, disabled: true });
+      await runResolvedPromises();
+      expect(await po.getSendButtonPo().isDisabled()).toBe(false);
     });
 
     it("should show new accounts after being loaded", async () => {
-      const { queryByTestId } = render(NnsWallet, props);
+      const resolveQueryBalance = pauseQueryAccountBalance();
+      const po = await renderWallet(props);
 
-      expect(queryByTestId("wallet-page-heading-component")).toBeNull();
+      await runResolvedPromises();
+      expect(await po.getWalletPageHeadingPo().isPresent()).toBe(false);
 
-      await waitFor(() =>
-        expect(
-          queryByTestId("wallet-page-heading-component")
-        ).toBeInTheDocument()
-      );
+      resolveQueryBalance();
+
+      await runResolvedPromises();
+      expect(await po.getWalletPageHeadingPo().isPresent()).toBe(true);
     });
   });
 
@@ -120,31 +151,30 @@ describe("NnsWallet", () => {
     });
 
     it("should render nns project name", async () => {
-      const { getByTestId } = render(NnsWallet, props);
+      const po = await renderWallet(props);
 
-      const titleRow = getByTestId("universe-page-summary-component");
-
-      expect(titleRow.textContent.trim()).toBe("Internet Computer");
+      expect(await po.getWalletPageHeaderPo().getUniverse()).toBe(
+        "Internet Computer"
+      );
     });
 
     it("should render a balance with token in summary", async () => {
-      const { getByTestId } = render(NnsWallet, props);
+      icpAccountsStore.setForTesting({
+        ...mockAccountsStoreData,
+        main: {
+          ...mockMainAccount,
+          balanceUlps: 432_100_000n,
+        },
+      });
+      const po = await renderWallet(props);
 
-      await waitFor(() =>
-        expect(getByTestId("token-value-label")).not.toBeNull()
-      );
-
-      expect(getByTestId("token-value-label")?.textContent.trim()).toEqual(
-        `${formatTokenE8s({
-          value: mockMainAccount.balanceUlps,
-        })} ${ICPToken.symbol}`
-      );
+      expect(await po.getWalletPageHeadingPo().getTitle()).toBe("4.32 ICP");
     });
 
     it("should enable new transaction action for route and store", async () => {
-      const { container } = render(NnsWallet, props);
+      const po = await renderWallet(props);
 
-      await waitFor(() => testToolbarButton({ container, disabled: false }));
+      expect(await po.getSendButtonPo().isDisabled()).toBe(false);
     });
 
     const modalProps = {
@@ -153,78 +183,68 @@ describe("NnsWallet", () => {
     };
 
     it("should open transaction modal", async () => {
-      const result = render(AccountsTest, { props: modalProps });
-
-      await testAccountsModal({ result, testId: "new-transaction" });
+      const { walletPo, transactionModalPo } =
+        await renderWalletAndModals(modalProps);
+      expect(await transactionModalPo.isPresent()).toBe(false);
+      await walletPo.clickSend();
+      expect(await transactionModalPo.isPresent()).toBe(true);
     });
 
     it("should open transaction modal on step select destination because selected account is current account", async () => {
-      const result = render(AccountsTest, { props: modalProps });
-
-      await testAccountsModal({ result, testId: "new-transaction" });
-
-      const { getByTestId } = result;
-
-      await waitFor(() =>
-        expect(getByTestId("transaction-step-1")).toBeInTheDocument()
+      const { walletPo, transactionModalPo } =
+        await renderWalletAndModals(modalProps);
+      await walletPo.clickSend();
+      expect(await transactionModalPo.getTransactionFormPo().isPresent()).toBe(
+        true
       );
     });
 
     it("should display SkeletonCard while loading transactions", async () => {
-      const { getByTestId } = render(NnsWallet, props);
+      const resolveGetTransactions = pauseGetTransactions();
+      const po = await renderWallet(props);
 
-      expect(getByTestId("skeleton-card")).toBeInTheDocument();
+      await runResolvedPromises();
+      expect(
+        await po.getTransactionListPo().getSkeletonCardPo().isPresent()
+      ).toBe(true);
+
+      resolveGetTransactions();
+
+      await runResolvedPromises();
+      expect(
+        await po.getTransactionListPo().getSkeletonCardPo().isPresent()
+      ).toBe(false);
     });
 
     it("should open receive modal", async () => {
-      const result = render(AccountsTest, { props: modalProps });
+      const { walletPo, receiveModalPo } =
+        await renderWalletAndModals(modalProps);
 
-      await testAccountsModal({ result, testId: "receive-icp" });
-
-      const { getByTestId } = result;
-
-      expect(getByTestId("receive-modal")).not.toBeNull();
+      expect(await receiveModalPo.isPresent()).toBe(false);
+      await walletPo.clickReceive();
+      expect(await receiveModalPo.isPresent()).toBe(true);
     });
 
     it("should display receive modal information", async () => {
-      const result = render(AccountsTest, { props: modalProps });
+      const { walletPo, receiveModalPo } =
+        await renderWalletAndModals(modalProps);
 
-      await testAccountsModal({ result, testId: "receive-icp" });
-
-      const { getByText } = result;
-
-      expect(
-        getByText(
-          replacePlaceholders(en.wallet.token_address, {
-            $tokenSymbol: en.core.icp,
-          })
-        )
-      ).toBeInTheDocument();
+      await walletPo.clickReceive();
+      expect(await receiveModalPo.getTokenAddressLabel()).toBe("ICP Address");
     });
 
     it("should reload account after finish receiving tokens", async () => {
-      const result = render(AccountsTest, { props: modalProps });
+      const { walletPo, receiveModalPo } =
+        await renderWalletAndModals(modalProps);
 
-      await testAccountsModal({ result, testId: "receive-icp" });
+      await walletPo.clickReceive();
 
-      const { getByTestId, container } = result;
-
-      await waitModalIntroEnd({ container, selector: modalToolbarSelector });
-
-      await waitFor(() =>
-        expect(accountsApi.getTransactions).toBeCalledTimes(2)
-      );
+      expect(accountsApi.getTransactions).toBeCalledTimes(2);
       expect(ledgerApi.queryAccountBalance).not.toBeCalled();
 
-      await waitFor(() => expect(getByTestId("receive-modal")).not.toBeNull());
+      await receiveModalPo.clickFinish();
 
-      fireEvent.click(
-        getByTestId("reload-receive-account") as HTMLButtonElement
-      );
-
-      await waitFor(() =>
-        expect(accountsApi.getTransactions).toBeCalledTimes(4)
-      );
+      expect(accountsApi.getTransactions).toBeCalledTimes(4);
       expect(ledgerApi.queryAccountBalance).toBeCalledTimes(2);
     });
   });
@@ -242,18 +262,25 @@ describe("NnsWallet", () => {
     };
 
     it("should Rename button", async () => {
-      const { queryByTestId } = render(NnsWallet, props);
-      expect(
-        queryByTestId("open-rename-subaccount-button")
-      ).toBeInTheDocument();
+      const po = await renderWallet(props);
+
+      expect(await po.getRenameButtonPo().isPresent()).toBe(true);
     });
   });
 
   describe("accounts loaded (Hardware Wallet)", () => {
+    const testHwPrincipalText = "5dstn-f5lvo-v2xk5-lvmja-g";
+    const testHwPrincipal = Principal.fromText(testHwPrincipalText);
+
     beforeEach(() => {
       icpAccountsStore.setForTesting({
         ...mockAccountsStoreData,
-        hardwareWallets: [mockHardwareWalletAccount],
+        hardwareWallets: [
+          {
+            ...mockHardwareWalletAccount,
+            principal: testHwPrincipal,
+          },
+        ],
       });
     });
 
@@ -266,21 +293,17 @@ describe("NnsWallet", () => {
     });
 
     it("should display principal", async () => {
-      const { queryByText } = render(NnsWallet, props);
-      const principal = mockHardwareWalletAccount?.principal?.toString();
+      const po = await renderWallet(props);
 
-      expect(principal?.length).toBeGreaterThan(0);
-      expect(
-        queryByText(`${principal}`, {
-          exact: false,
-        })
-      ).toBeInTheDocument();
+      expect(await po.getWalletPageHeadingPo().getPrincipal()).toBe(
+        testHwPrincipalText
+      );
     });
 
     it("should display hardware wallet buttons", async () => {
-      const { queryByTestId } = render(NnsWallet, props);
-      expect(queryByTestId("ledger-list-button")).toBeInTheDocument();
-      expect(queryByTestId("ledger-show-button")).toBeInTheDocument();
+      const po = await renderWallet(props);
+      expect(await po.getListNeuronsButtonPo().isPresent()).toBe(true);
+      expect(await po.getShowHardwareWalletButtonPo().isPresent()).toBe(true);
     });
   });
 
