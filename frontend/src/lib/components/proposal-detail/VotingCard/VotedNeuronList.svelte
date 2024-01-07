4961a6bc8662d9122e5f596f228577ebb8cf9a5e
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-01-07 16:43:09 +0100

    feat: display voting result in neuron block headline

--- frontend/src/lib/components/proposal-detail/VotingCard/VotedNeuronList.svelte
+++ frontend/src/lib/components/proposal-detail/VotingCard/VotedNeuronList.svelte
@@ -9,6 +9,8 @@
   import { replacePlaceholders } from "$lib/utils/i18n.utils";
   import ExpandableProposalNeurons from "$lib/components/proposal-detail/VotingCard/ExpandableProposalNeurons.svelte";
   import MyVotes from "$lib/components/proposal-detail/MyVotes.svelte";
+  import { Vote } from "@dfinity/nns";
+  import VoteResultIcon from "$lib/components/proposal-detail/VotingCard/VoteResultIcon.svelte";
 
   export let neuronsVotedForProposal: CompactNeuronInfo[];
 
@@ -17,14 +19,33 @@
 
   let votedNeuronCount: number;
   $: votedNeuronCount = neuronsVotedForProposal.length;
+
+  // Equals `Vote.Unspecified` when not all neurons voted the same way
+  let allVotedVote: Vote;
+  $: allVotedVote = neuronsVotedForProposal.some(
+    ({ vote }) => neuronsVotedForProposal[0]?.vote !== vote
+  )
+    ? Vote.Unspecified
+    : neuronsVotedForProposal?.[0].vote ?? Vote.Unspecified;
+
+  $: console.log("allVotedVote", allVotedVote, neuronsVotedForProposal);
 </script>
 
 {#if votedNeuronCount > 0}
   <ExpandableProposalNeurons testId="voted-neurons">
     <svelte:fragment slot="start">
-      {replacePlaceholders($i18n.proposal_detail.neurons_voted, {
-        $count: `${votedNeuronCount}`,
-      })}
+      <span
+        class="headline"
+        class:yes={allVotedVote === Vote.Yes}
+        class:no={allVotedVote === Vote.No}
+      >
+        {replacePlaceholders($i18n.proposal_detail.neurons_voted, {
+          $count: `${votedNeuronCount}`,
+        })}
+        {#if allVotedVote !== Vote.Unspecified}
+          <VoteResultIcon vote={allVotedVote} />
+        {/if}
+      </span>
     </svelte:fragment>
     <svelte:fragment slot="end">
       <span class="label">{$i18n.proposal_detail__vote.voting_power}</span>
@@ -35,3 +56,18 @@
     <MyVotes {neuronsVotedForProposal} />
   </ExpandableProposalNeurons>
 {/if}
+
+<style lang="scss">
+  .headline {
+    display: flex;
+    align-items: center;
+    gap: var(--padding);
+
+    &.yes {
+      color: var(--positive-emphasis);
+    }
+    &.no {
+      color: var(--negative-emphasis);
+    }
+  }
+</style>
