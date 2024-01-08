3c86a5c78dc9dd7891d136a622e1cd65bdbb8862
David de Kloet <david.de.kloet@dfinity.org>
2024-01-08 09:50:57 +0100

    Allow absent wallet address in WalletPageHeader

--- frontend/src/tests/lib/components/accounts/WalletPageHeader.spec.ts
+++ frontend/src/tests/lib/components/accounts/WalletPageHeader.spec.ts
@@ -1,18 +1,25 @@
 import WalletPageHeader from "$lib/components/accounts/WalletPageHeader.svelte";
 import type { Universe } from "$lib/types/universe";
-import { mockMainAccount } from "$tests/mocks/icp-accounts.store.mock";
 import { mockUniverse } from "$tests/mocks/sns-projects.mock";
 import { WalletPageHeaderPo } from "$tests/page-objects/WalletPageHeader.page-object";
 import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
 import { render } from "@testing-library/svelte";
 
 describe("WalletPageHeading", () => {
+  const universeName = "Test universe";
+  const universe: Universe = {
+    ...mockUniverse,
+    title: universeName,
+  };
+  const walletAddress =
+    "d4685b31b51450508aff0331584df7692a84467b680326f5c5f7d30ae711682f";
+
   const renderComponent = ({
-    walletAddress = mockMainAccount.identifier,
-    universe = mockUniverse,
+    walletAddress,
+    universe,
   }: {
-    walletAddress?: string;
-    universe?: Universe;
+    walletAddress: string;
+    universe: Universe;
   }) => {
     const { container } = render(WalletPageHeader, {
       props: {
@@ -23,25 +30,23 @@ describe("WalletPageHeading", () => {
     return WalletPageHeaderPo.under(new JestPageObjectElement(container));
   };
 
-  it("should render account identifier", async () => {
+  it("should render universe name and account identifier", async () => {
     const po = renderComponent({
-      walletAddress:
-        "d4685b31b51450508aff0331584df7692a84467b680326f5c5f7d30ae711682f",
+      universe,
+      walletAddress,
     });
 
-    expect(await po.getWalletAddress()).toBe(
-      "d4685b31b51450508aff0331584df7692a84467b680326f5c5f7d30ae711682f"
-    );
+    expect(await po.getUniverse()).toBe(universeName);
+    expect(await po.getWalletAddress()).toBe(walletAddress);
   });
 
-  it("should render universe name", async () => {
-    const universeName = "Test universe";
-    const universe: Universe = {
-      ...mockUniverse,
-      title: universeName,
-    };
-    const po = renderComponent({ universe });
+  it("should render universe name and no account identifier", async () => {
+    const po = renderComponent({
+      universe,
+      walletAddress: undefined,
+    });
 
     expect(await po.getUniverse()).toBe(universeName);
+    expect(await po.getHashPo().isPresent()).toBe(false);
   });
 });
