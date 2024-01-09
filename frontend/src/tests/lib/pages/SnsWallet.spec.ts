dfinity/nns-dapp

commit 40e8cefb4d55669bda46df441b8c7751aaaaab47

Renamed Account.balanceE8s to balanceUlps (#3995)
# Motivation

The nns-dapp `Account` type has a field called `balanceE8s` but this
type is also used for ckETH which has 18 decimals.
So the name `balanceE8s` is misleading in that case.

# Changes

1. Renamed `balanceE8s` to `balanceUlps`.
2. Added a few TODOs of related things that should be renamed.

# Tests

pass

# Todos

- [ ] Add entry to changelog (if necessary).
not necessary

frontend/src/lib/components/accounts/IcrcWalletPage.svelte
@@ -135,7 +135,7 @@
             accountName={$selectedAccountStore.account.name ??
               $i18n.accounts.main}
             balance={TokenAmountV2.fromUlps({
-              amount: $selectedAccountStore.account.balanceE8s,
+              amount: $selectedAccountStore.account.balanceUlps,
               token,
             })}
           >

frontend/src/lib/pages/SnsWallet.svelte
@@ -137,7 +137,7 @@
           />
           <WalletPageHeading
             balance={TokenAmount.fromE8s({
-              amount: $selectedAccountStore.account.balanceE8s,
+              amount: $selectedAccountStore.account.balanceUlps,
               token,
             })}
             accountName={$selectedAccountStore.account.name ??

frontend/src/tests/lib/pages/SnsWallet.spec.ts
@@ -161,7 +161,7 @@ describe("SnsWallet", () => {
           ?.textContent.trim()
       ).toEqual(
         `${formatToken({
-          value: mockSnsMainAccount.balanceE8s,
+          value: mockSnsMainAccount.balanceUlps,
         })} ${mockSnsToken.symbol}`
       );
     });

commit 0850290fbb70a3c637a54031a1fef53b49129a44

Fix balances observers (#4151)
# Motivation

We want to be able to render wallet pages to signed-out users.
When the user is not signed in, there is no balance to observer so the
observer should be separated from the content that can be displayed to
signed-out users. So the content should not be passed as a slot of the
observer, but should be a sibling of the observer.
After I made this change, I tested if the observer (still) worked, and I
realized that the observer was actually already broken before.
It was broken by https://github.com/dfinity/nns-dapp/pull/3995 which
changed `Account.balanceE8s` to `Account.balanceUlps` while the
observers kept setting the `balanceE8s` field. This was not caught by
the test because the test also checked that the `balanceE8s` field was
set.
So in addition to fixing the usage of the field, I wanted to add a test
that would have caught the issue.
And while adding the test I ran into what seems like a bug in Svelte
where it would not update values render inside a slot.
Since my plan was to move the content outside the slot of the observer,
I decided not to investigate further and just do the fix and the move in
the same PR.

# Changes

1. In `IcrcBalancesObserver.svelte` and `SnsBalancesObserver.svelte` set
`balanceUlps` instead of `balanceE8s`.
2. In `IcrcWalletPage.svelte` and `SnsWallet.svelte` move the content
outside the respective Observer slot.

# Tests

1. Update the Observer tests to check the `balanceUlps` field instead of
the `balanceE8s` field.
2. Add wallet tests to check that the balances passed from the observer
callback are actually rendered.

# Todos

- [x] Add entry to changelog (if necessary).

frontend/src/lib/components/accounts/IcrcBalancesObserver.svelte
@@ -21,7 +21,7 @@
         return nonNullish(selectedAccount)
           ? ({
               ...selectedAccount,
-              balanceE8s: balance,
+              balanceUlps: balance,
             } as Account)
           : undefined;
       })

frontend/src/lib/components/accounts/IcrcWalletPage.svelte
@@ -136,35 +136,34 @@
           universeId={selectedUniverseId}
           accounts={[$selectedAccountStore.account]}
           reload={reloadOnlyAccountFromStore}
+        />
+        <WalletPageHeader
+          universe={$selectedUniverseStore}
+          walletAddress={$selectedAccountStore.account.identifier}
+        />
+        <WalletPageHeading
+          accountName={$selectedAccountStore.account.name ??
+            $i18n.accounts.main}
+          balance={TokenAmountV2.fromUlps({
+            amount: $selectedAccountStore.account.balanceUlps,
+            token,
+          })}
         >
-          <WalletPageHeader
-            universe={$selectedUniverseStore}
-            walletAddress={$selectedAccountStore.account.identifier}
-          />
-          <WalletPageHeading
-            accountName={$selectedAccountStore.account.name ??
-              $i18n.accounts.main}
-            balance={TokenAmountV2.fromUlps({
-              amount: $selectedAccountStore.account.balanceUlps,
-              token,
-            })}
-          >
-            <slot name="header-actions" />
-          </WalletPageHeading>
+          <slot name="header-actions" />
+        </WalletPageHeading>
 
-          {#if $$slots["info-card"]}
-            <div class="content-cell-island info-card">
-              <slot name="info-card" />
-            </div>
-          {/if}
-
-          <Separator spacing="none" />
-
-          <!-- Transactions and the explanation go together. -->
-          <div>
-            <slot name="page-content" />
+        {#if $$slots["info-card"]}
+          <div class="content-cell-island info-card">
+            <slot name="info-card" />
           </div>
-        </IcrcBalancesObserver>
+        {/if}
+
+        <Separator spacing="none" />
+
+        <!-- Transactions and the explanation go together. -->
+        <div>
+          <slot name="page-content" />
+        </div>
       {:else}
         <Spinner />
       {/if}

frontend/src/lib/components/accounts/SnsBalancesObserver.svelte
@@ -22,7 +22,7 @@
         return nonNullish(selectedAccount)
           ? {
               ...selectedAccount,
-              balanceE8s: balance,
+              balanceUlps: balance,
             }
           : undefined;
       })

frontend/src/lib/pages/SnsWallet.svelte
@@ -155,28 +155,27 @@
             rootCanisterId={$snsOnlyProjectStore}
             accounts={[$selectedAccountStore.account]}
             ledgerCanisterId={$snsProjectSelectedStore.summary.ledgerCanisterId}
-          >
-            <WalletPageHeader
-              universe={$selectedUniverseStore}
-              walletAddress={$selectedAccountStore.account.identifier}
-            />
-            <WalletPageHeading
-              balance={TokenAmount.fromE8s({
-                amount: $selectedAccountStore.account.balanceUlps,
-                token,
-              })}
-              accountName={$selectedAccountStore.account.name ??
-                $i18n.accounts.main}
-            />
+          />
+          <WalletPageHeader
+            universe={$selectedUniverseStore}
+            walletAddress={$selectedAccountStore.account.identifier}
+          />
+          <WalletPageHeading
+            balance={TokenAmount.fromE8s({
+              amount: $selectedAccountStore.account.balanceUlps,
+              token,
+            })}
+            accountName={$selectedAccountStore.account.name ??
+              $i18n.accounts.main}
+          />
 
-            <Separator spacing="none" />
+          <Separator spacing="none" />
 
-            <SnsTransactionsList
-              rootCanisterId={$snsOnlyProjectStore}
-              account={$selectedAccountStore.account}
-              {token}
-            />
-          </SnsBalancesObserver>
+          <SnsTransactionsList
+            rootCanisterId={$snsOnlyProjectStore}
+            account={$selectedAccountStore.account}
+            {token}
+          />
         {:else}
           <Spinner />
         {/if}

frontend/src/tests/lib/components/accounts/IcrcBalancesObserver.spec.ts
@@ -93,7 +93,7 @@ describe("IcrcBalancesObserver", () => {
       const updatedStore = get(icrcAccountsStore);
       expect(
         updatedStore[CKTESTBTC_UNIVERSE_CANISTER_ID.toText()].accounts
-      ).toEqual([{ ...mockCkBTCMainAccount, balanceE8s: 123456n }]);
+      ).toEqual([{ ...mockCkBTCMainAccount, balanceUlps: 123456n }]);
     });
   });
 

frontend/src/tests/lib/components/accounts/SnsBalancesObserver.spec.ts
@@ -107,7 +107,7 @@ describe("SnsBalancesObserver", () => {
     await waitFor(() => {
       const updatedStore = get(snsAccountsStore);
       expect(updatedStore[rootCanisterIdMock.toText()].accounts).toEqual([
-        { ...mockSnsMainAccount, balanceE8s: 123456n },
+        { ...mockSnsMainAccount, balanceUlps: 123456n },
       ]);
     });
   });

frontend/src/tests/lib/pages/IcrcWallet.spec.ts
@@ -32,11 +32,13 @@ import { get } from "svelte/store";
 
 const expectedBalanceAfterTransfer = 11_111n;
 
+let balancesObserverCallback;
+
 vi.mock("$lib/services/worker-balances.services", () => ({
   initBalancesWorker: vi.fn(() =>
     Promise.resolve({
-      startBalancesTimer: () => {
-        // Do nothing
+      startBalancesTimer: ({ callback }) => {
+        balancesObserverCallback = callback;
       },
       stopBalancesTimer: () => {
         // Do nothing
@@ -288,6 +290,29 @@ describe("IcrcWallet", () => {
         universe: CKETHSEPOLIA_UNIVERSE_CANISTER_ID.toText(),
       });
       expect(get(toastsStore)).toEqual([]);
+    });
+
+    it("should display the balance from the observer", async () => {
+      const po = await renderWallet(props);
+      await runResolvedPromises();
+
+      expect(await po.getWalletPageHeadingPo().getTitle()).toBe(
+        "123.00 ckETHTEST"
+      );
+
+      balancesObserverCallback({
+        balances: [
+          {
+            balance: 3_330_000_000_000_000_000n,
+            accountIdentifier: mockCkETHMainAccount.identifier,
+          },
+        ],
+      });
+
+      await runResolvedPromises();
+      expect(await po.getWalletPageHeadingPo().getTitle()).toBe(
+        "3.33 ckETHTEST"
+      );
     });
   });
 });

frontend/src/tests/lib/pages/SnsWallet.spec.ts
@@ -29,6 +29,8 @@ import { get } from "svelte/store";
 vi.mock("$lib/api/sns-ledger.api");
 vi.mock("$lib/api/sns-index.api");
 
+let balancesObserverCallback;
+
 vi.mock("$lib/services/worker-transactions.services", () => ({
   initTransactionsWorker: vi.fn(() =>
     Promise.resolve({
@@ -45,8 +47,8 @@ vi.mock("$lib/services/worker-transactions.services", () => ({
 vi.mock("$lib/services/worker-balances.services", () => ({
   initBalancesWorker: vi.fn(() =>
     Promise.resolve({
-      startBalancesTimer: () => {
-        // Do nothing
+      startBalancesTimer: ({ callback }) => {
+        balancesObserverCallback = callback;
       },
       stopBalancesTimer: () => {
         // Do nothing
@@ -341,6 +343,34 @@ describe("SnsWallet", () => {
         universe: rootCanisterIdText,
       });
       expect(get(toastsStore)).toEqual([]);
+    });
+
+    it("should display the balance from the observer", async () => {
+      const oldBalance = 123_000_000n;
+      const newBalance = 456_000_000n;
+
+      vi.spyOn(snsLedgerApi, "getSnsAccounts").mockResolvedValue([
+        {
+          ...mockSnsMainAccount,
+          balanceUlps: oldBalance,
+        },
+      ]);
+
+      const po = await renderComponent(props);
+
+      expect(await po.getWalletPageHeadingPo().getTitle()).toBe("1.23 OOO");
+
+      balancesObserverCallback({
+        balances: [
+          {
+            balance: newBalance,
+            accountIdentifier: mockSnsMainAccount.identifier,
+          },
+        ],
+      });
+
+      await runResolvedPromises();
+      expect(await po.getWalletPageHeadingPo().getTitle()).toBe("4.56 OOO");
     });
   });
 });