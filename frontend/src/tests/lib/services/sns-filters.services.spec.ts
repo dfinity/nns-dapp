6da1bb953ee861c3ef368d9278d559e74e018e5c
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-01-11 15:46:46 +0100

    test: updateSnsTypeFilter

--- frontend/src/tests/lib/services/sns-filters.services.spec.ts
+++ frontend/src/tests/lib/services/sns-filters.services.spec.ts
@@ -1,7 +1,11 @@
-import { loadSnsFilters } from "$lib/services/sns-filters.services";
+import {
+  loadSnsFilters,
+  updateSnsTypeFilter,
+} from "$lib/services/sns-filters.services";
 import { snsFiltersStore } from "$lib/stores/sns-filters.store";
 import { enumSize } from "$lib/utils/enum.utils";
 import { mockPrincipal } from "$tests/mocks/auth.store.mock";
+import { nativeNervousSystemFunctionMock } from "$tests/mocks/sns-functions.mock";
 import {
   SnsProposalDecisionStatus,
   SnsProposalRewardStatus,
@@ -9,41 +13,43 @@ import {
 import { get } from "svelte/store";
 
 describe("sns-filters services", () => {
+  const getFiltersStoreData = () =>
+    get(snsFiltersStore)[mockPrincipal.toText()];
+
   describe("loadSnsFilters", () => {
     beforeEach(() => {
       snsFiltersStore.reset();
     });
     it("should load the sns decision status filters store but not Unspecified", async () => {
+      expect(getFiltersStoreData()?.decisionStatus).toBeUndefined();
       await loadSnsFilters(mockPrincipal);
 
-      const projectStore = get(snsFiltersStore)[mockPrincipal.toText()];
-
-      expect(projectStore.decisionStatus).toHaveLength(
+      expect(getFiltersStoreData().decisionStatus).toHaveLength(
         enumSize(SnsProposalDecisionStatus) - 1
       );
       expect(
-        projectStore.decisionStatus.map(({ value }) => value)
+        getFiltersStoreData().decisionStatus.map(({ value }) => value)
       ).not.toContainEqual(
         SnsProposalDecisionStatus.PROPOSAL_DECISION_STATUS_UNSPECIFIED
       );
     });
 
     it("should load the sns reward status filters store but not Unspecified", async () => {
+      expect(getFiltersStoreData()?.rewardStatus).toBeUndefined();
       await loadSnsFilters(mockPrincipal);
 
-      const projectStore = get(snsFiltersStore)[mockPrincipal.toText()];
-
-      expect(projectStore.rewardStatus).toHaveLength(
+      expect(getFiltersStoreData().rewardStatus).toHaveLength(
         enumSize(SnsProposalRewardStatus) - 1
       );
       expect(
-        projectStore.rewardStatus.map(({ value }) => value)
+        getFiltersStoreData().rewardStatus.map(({ value }) => value)
       ).not.toContainEqual(
         SnsProposalRewardStatus.PROPOSAL_REWARD_STATUS_UNSPECIFIED
       );
     });
 
     it("should not change filters if they are already present", async () => {
+      expect(getFiltersStoreData()?.decisionStatus).toBeUndefined();
       const decisionStatus = [];
       snsFiltersStore.setDecisionStatus({
         rootCanisterId: mockPrincipal,
@@ -52,8 +58,34 @@ describe("sns-filters services", () => {
 
       await loadSnsFilters(mockPrincipal);
 
-      const projectStore = get(snsFiltersStore)[mockPrincipal.toText()];
-      expect(projectStore.decisionStatus).toHaveLength(decisionStatus.length);
+      expect(getFiltersStoreData().decisionStatus).toHaveLength(
+        decisionStatus.length
+      );
+    });
+  });
+
+  describe("updateSnsTypeFilter", () => {
+    beforeEach(() => {
+      snsFiltersStore.reset();
+    });
+
+    it("should fill up the sns type filter store", async () => {
+      expect(getFiltersStoreData()?.types).toBeUndefined();
+
+      updateSnsTypeFilter({
+        rootCanisterId: mockPrincipal,
+        nsFunctions: [nativeNervousSystemFunctionMock],
+        snsName: "sns-name",
+      });
+
+      expect(getFiltersStoreData().types).toEqual([
+        {
+          checked: true,
+          id: "1",
+          name: "Motion",
+          value: "1",
+        },
+      ]);
     });
   });
 });
