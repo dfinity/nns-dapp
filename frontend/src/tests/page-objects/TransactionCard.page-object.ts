0b48a814801df92eba6a2372f0cc80c6f2309412
David de Kloet <david.de.kloet@dfinity.org>
2024-01-12 09:05:21 +0100

    Extract TransactionIcon from TransactionCard component

--- frontend/src/tests/page-objects/TransactionCard.page-object.ts
+++ frontend/src/tests/page-objects/TransactionCard.page-object.ts
@@ -1,4 +1,5 @@
 import { AmountDisplayPo } from "$tests/page-objects/AmountDisplay.page-object";
+import { TransactionIconPo } from "$tests/page-objects/TransactionIcon.page-object";
 import { BasePageObject } from "$tests/page-objects/base.page-object";
 import type { PageObjectElement } from "$tests/types/page-object.types";
 
@@ -15,6 +16,10 @@ export class TransactionCardPo extends BasePageObject {
     return Array.from(await element.allByTestId(TransactionCardPo.TID)).map(
       (el) => new TransactionCardPo(el)
     );
+  }
+
+  getTransactionIconPo(): TransactionIconPo {
+    return TransactionIconPo.under(this.root);
   }
 
   getHeadline(): Promise<string> {
@@ -37,38 +42,23 @@ export class TransactionCardPo extends BasePageObject {
     return this.getAmountDisplayPo().getAmount();
   }
 
-  async hasIconClass(className: string): Promise<boolean> {
-    const classNames = await this.root.byTestId("icon").getClasses();
-    return classNames.includes(className);
-  }
-
   async hasSentIcon(): Promise<boolean> {
-    const hasIcon = await this.isPresent("icon-up");
-    const hasClass = await this.hasIconClass("send");
-    return hasIcon && hasClass;
+    return this.getTransactionIconPo().isSentIcon();
   }
 
   async hasReceivedIcon(): Promise<boolean> {
-    const hasIcon = await this.isPresent("icon-down");
-    const hasClass = await this.hasIconClass("send");
-    return hasIcon && !hasClass;
+    return this.getTransactionIconPo().isReceivedIcon();
   }
 
   async hasPendingReceiveIcon(): Promise<boolean> {
-    const hasIcon = await this.isPresent("icon-down");
-    const hasClass = await this.hasIconClass("pending");
-    return hasIcon && hasClass;
+    return this.getTransactionIconPo().isPendingReceiveIcon();
   }
 
   async hasReimbursementIcon(): Promise<boolean> {
-    const hasIcon = await this.isPresent("icon-reimbursed");
-    const hasClass = await this.hasIconClass("failed");
-    return hasIcon && hasClass;
+    return this.getTransactionIconPo().isReimbursementIcon();
   }
 
   async hasFailedIcon(): Promise<boolean> {
-    const hasIcon = await this.isPresent("icon-error-outline");
-    const hasClass = await this.hasIconClass("failed");
-    return hasIcon && hasClass;
+    return this.getTransactionIconPo().isFailedIcon();
   }
 }
