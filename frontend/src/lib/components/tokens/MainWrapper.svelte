1e0d992f69c9a35c7de4708f02441bc8211f316d
David de Kloet <david.de.kloet@dfinity.org>
2024-01-13 08:13:08 +0100

    Remove height: 100% from MainWrapper

--- frontend/src/lib/components/tokens/MainWrapper.svelte
+++ frontend/src/lib/components/tokens/MainWrapper.svelte
@@ -15,7 +15,6 @@
     display: flex;
     align-items: stretch;
     justify-content: center;
-    height: 100%;
     width: 100%;
 
     padding: var(--padding-2x) var(--padding-2x) var(--padding)
@@ -27,11 +26,8 @@
 
     & .content {
       width: 100%;
-      height: 100%;
 
       overflow-x: hidden;
-      overflow-y: auto;
-
       @include media.min-width(large) {
         width: var(--island-width);
         max-width: var(--island-max-width);
