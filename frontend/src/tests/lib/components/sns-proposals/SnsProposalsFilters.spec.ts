c6f8c0a7d8731cfb55d8a4cea4442b471539e67a
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-01-14 21:52:12 +0100

    reset SnsProposalsFilters.spec.ts

--- frontend/src/tests/lib/components/sns-proposals/SnsProposalsFilters.spec.ts
+++ frontend/src/tests/lib/components/sns-proposals/SnsProposalsFilters.spec.ts
@@ -1,25 +1,15 @@
 import SnsProposalsFilters from "$lib/components/sns-proposals/SnsProposalsFilters.svelte";
-import { page } from "$mocks/$app/stores";
-import { mockSnsFullProject } from "$tests/mocks/sns-projects.mock";
 import { fireEvent, render, waitFor } from "@testing-library/svelte";
 
 describe("SnsProposalsFilters", () => {
-  const renderComponent = () => render(SnsProposalsFilters);
-
-  beforeEach(() => {
-    page.mock({
-      data: { universe: mockSnsFullProject.rootCanisterId.toText() },
-    });
-  });
-
   it("should render status filter button", () => {
-    const { queryByTestId } = renderComponent();
+    const { queryByTestId } = render(SnsProposalsFilters);
 
     expect(queryByTestId("filters-by-status")).toBeInTheDocument();
   });
 
   it("should show filter modal when status filter is clicked", async () => {
-    const { queryByTestId } = renderComponent();
+    const { queryByTestId } = render(SnsProposalsFilters);
 
     const statusFilterButton = queryByTestId("filters-by-status");
     statusFilterButton && fireEvent.click(statusFilterButton);
@@ -30,7 +20,7 @@ describe("SnsProposalsFilters", () => {
   });
 
   it("should show filter modal when rewards filter is clicked", async () => {
-    const { queryByTestId } = renderComponent();
+    const { queryByTestId } = render(SnsProposalsFilters);
 
     const statusFilterButton = queryByTestId("filters-by-rewards");
     statusFilterButton && fireEvent.click(statusFilterButton);
