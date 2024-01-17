00f91698232579e34d050c272659da90582acc59
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2024-01-17 18:58:40 +0100

    Remove ENABLE_CKBTC_ICRC2 (#4224)
    # Motivation
    
    The feature was launched and the code no longer depends on the feature
    flag.
    
    # Changes
    
    Remove `ENABLE_CKBTC_ICRC2`.
    
    # Tests
    
    pass
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    not necessary

--- frontend/src/lib/constants/environment.constants.ts
+++ frontend/src/lib/constants/environment.constants.ts
@@ -43,7 +43,6 @@ export interface FeatureFlags<T> {
   ENABLE_ICP_ICRC: T;
   ENABLE_MY_TOKENS: T;
   ENABLE_CKETH: T;
-  ENABLE_CKBTC_ICRC2: T;
   ENABLE_SNS_TYPES_FILTER: T;
   // Used only in tests and set up in jest-setup.ts
   TEST_FLAG_EDITABLE: T;
