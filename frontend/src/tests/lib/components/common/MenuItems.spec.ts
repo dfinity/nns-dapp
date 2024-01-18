2c5a2f10a14f5667d51c77b24f80b87b0c445c79
Llorenç <llorenc.muntaner@dfinity.org>
2024-01-18 13:28:25 +0100

    GIX-2080: Fix failing tests with default `ENABLE_MY_TOKENS` true

--- frontend/src/tests/lib/components/common/MenuItems.spec.ts
+++ frontend/src/tests/lib/components/common/MenuItems.spec.ts
@@ -58,19 +58,25 @@ describe("MenuItems", () => {
     expect(() => getByTestId("get-icp-button")).toThrow();
   });
 
-  it("should point Tokens and Neurons to NNS from the project page", () => {
-    page.mock({ data: { universe: OWN_CANISTER_ID_TEXT } });
-    const { getByTestId } = render(MenuItems);
+  describe("when My Tokens page is disabled", () => {
+    beforeEach(() => {
+      overrideFeatureFlagsStore.setFlag("ENABLE_MY_TOKENS", false);
+    });
 
-    const accountsLink = getByTestId("menuitem-accounts");
-    expect(accountsLink.getAttribute("href")).toEqual(
-      `${AppPath.Accounts}/?${UNIVERSE_PARAM}=${OWN_CANISTER_ID.toText()}`
-    );
+    it("should point Tokens and Neurons to NNS from the project page", () => {
+      page.mock({ data: { universe: OWN_CANISTER_ID_TEXT } });
+      const { getByTestId } = render(MenuItems);
 
-    const neuronsLink = getByTestId("menuitem-neurons");
-    expect(neuronsLink.getAttribute("href")).toEqual(
-      `${AppPath.Neurons}/?${UNIVERSE_PARAM}=${OWN_CANISTER_ID.toText()}`
-    );
+      const accountsLink = getByTestId("menuitem-accounts");
+      expect(accountsLink.getAttribute("href")).toEqual(
+        `${AppPath.Accounts}/?${UNIVERSE_PARAM}=${OWN_CANISTER_ID.toText()}`
+      );
+
+      const neuronsLink = getByTestId("menuitem-neurons");
+      expect(neuronsLink.getAttribute("href")).toEqual(
+        `${AppPath.Neurons}/?${UNIVERSE_PARAM}=${OWN_CANISTER_ID.toText()}`
+      );
+    });
   });
 
   describe("when My Tokens page is enabled", () => {
