f84d15fa46600ce820e70c9a4ad6b7247ae9bc26
Max Murphy <max@winning.black>
2024-01-22 14:39:06 +0100

    Rust changes

--- rs/backend/src/state/partitions/schemas/tests.rs
+++ rs/backend/src/state/partitions/schemas/tests.rs
@@ -5,6 +5,19 @@ use strum::IntoEnumIterator;
 #[test]
 fn schema_label_should_be_persisted() {
     for schema in SchemaLabel::iter().filter(|schema| *schema != SchemaLabel::Map) {
-        unimplemented!()
+        // Prepare partitions with empty memory.
+        let toy_memory = DefaultMemoryImpl::default();
+        let memory_manager = MemoryManager::init(Rc::clone(&toy_memory));
+        let partitions = Partitions::try_from_memory(Rc::clone(&toy_memory)).expect("Failed to get empty partitions");
+        partitions.set_schema_label(schema);
+        let read_schema = partitions.schema_label();
+        assert_eq!(
+            schema, read_schema,
+            "Read schema does not match schema that was just set"
+        );
+        // Make sure that the label is persisted in stable memory, not just in RAM:
+        let partitions = Partitions::try_from_memory(Rc::clone(&toy_memory)).expect("Failed to get empty partitions");
+        let read_schema = partitions.schema_label();
+        assert_eq!(schema, read_schema, "Schema label was not persisted to stable memory.");
     }
 }
