c3770b1353b7d1e2dd8057b58a04ba8ebbb00159
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2024-01-23 11:18:23 +0100

    GIX-2060: Remove deprecated transactionsFeeStore (#4269)
    # Motivation
    
    Remove deprecated `transactionsFeesStore`.
    
    In this PR, remove the usage in the derived stores that returns the fee
    of the ICP.
    
    # Changes
    
    * `mainTransactionFeeStore` uses `tokensStore`.
    * `mainTransactionFeeE8sStore` uses `tokensStore`.
    * `mainTransactionFeeStoreAsToken` uses `tokensStore`.
    
    # Tests
    
    * Adapt `mainTransactionFeeStoreAsToken` spec file to use tokensStore.
    * Adapt `mainTransactionFeeStore` test case to use tokensStore.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    
    Not necessary.

--- frontend/src/lib/derived/main-transaction-fee.derived.ts
+++ frontend/src/lib/derived/main-transaction-fee.derived.ts
@@ -1,8 +1,13 @@
-import { transactionsFeesStore } from "$lib/stores/transaction-fees.store";
-import { ICPToken, TokenAmount } from "@dfinity/utils";
+import { OWN_CANISTER_ID_TEXT } from "$lib/constants/canister-ids.constants";
+import { tokensStore } from "$lib/stores/tokens.store";
+import { TokenAmount, isNullish } from "@dfinity/utils";
 import { derived } from "svelte/store";
 
-export const mainTransactionFeeStoreAsToken = derived(
-  transactionsFeesStore,
-  ($store) => TokenAmount.fromE8s({ amount: $store.main, token: ICPToken })
-);
+export const mainTransactionFeeStoreAsToken = derived(tokensStore, ($store) => {
+  const icpToken = $store[OWN_CANISTER_ID_TEXT].token;
+  if (isNullish(icpToken)) {
+    // This can't happen because the tokensStore always contains the NNS token.
+    throw new Error("ICP token not found");
+  }
+  return TokenAmount.fromE8s({ amount: icpToken.fee, token: icpToken });
+});
