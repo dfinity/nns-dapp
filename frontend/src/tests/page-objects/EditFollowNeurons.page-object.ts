4729996883c7deea762fd28dfb7e85930cdafc3f
David de Kloet <david.de.kloet@dfinity.org>
2024-01-24 14:30:02 +0100

    number

--- frontend/src/tests/page-objects/EditFollowNeurons.page-object.ts
+++ frontend/src/tests/page-objects/EditFollowNeurons.page-object.ts
@@ -1,7 +1,6 @@
 import { FollowTopicSectionPo } from "$tests/page-objects/FollowTopicSection.page-object";
 import { BasePageObject } from "$tests/page-objects/base.page-object";
 import type { PageObjectElement } from "$tests/types/page-object.types";
-import type { Topic } from "@dfinity/nns";
 
 export class EditFollowNeuronsPo extends BasePageObject {
   private static readonly TID = "edit-followers-screen";
@@ -10,14 +9,14 @@ export class EditFollowNeuronsPo extends BasePageObject {
     return new EditFollowNeuronsPo(element.byTestId(EditFollowNeuronsPo.TID));
   }
 
-  getFollowTopicSectionPo(topic: Topic): FollowTopicSectionPo {
+  getFollowTopicSectionPo(topic: number): FollowTopicSectionPo {
     return FollowTopicSectionPo.under({
       element: this.root,
       topic,
     });
   }
 
-  getBadgeNumber(topic: Topic): Promise<number> {
+  getBadgeNumber(topic: number): Promise<number> {
     return this.getFollowTopicSectionPo(topic).getBadgeNumber();
   }
 }
