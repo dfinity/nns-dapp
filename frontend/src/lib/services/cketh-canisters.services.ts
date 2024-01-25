c3ce7e54357272130bb1cc403e0de5ed2959b730
Llorenç <llorenc.muntaner@dfinity.org>
2024-01-25 12:18:05 +0100

    GIX-2149: Remove ENABLE_CKETH flag

--- frontend/src/lib/services/cketh-canisters.services.ts
+++ frontend/src/lib/services/cketh-canisters.services.ts
@@ -4,10 +4,7 @@ import {
   CKETH_INDEX_CANISTER_ID,
   CKETH_LEDGER_CANISTER_ID,
 } from "$lib/constants/cketh-canister-ids.constants";
-import {
-  ENABLE_CKETH,
-  ENABLE_CKTESTBTC,
-} from "$lib/stores/feature-flags.store";
+import { ENABLE_CKTESTBTC } from "$lib/stores/feature-flags.store";
 import { icrcCanistersStore } from "$lib/stores/icrc-canisters.store";
 import { isNullish } from "@dfinity/utils";
 import { get } from "svelte/store";
@@ -16,10 +13,7 @@ export const loadCkETHCanisters = async () => {
   const storeData = get(icrcCanistersStore);
   // To avoid rerendering the UI and possibly triggering new requests
   // We don't change the store if it's already filled.
-  if (
-    get(ENABLE_CKETH) &&
-    isNullish(storeData[CKETH_LEDGER_CANISTER_ID.toText()])
-  ) {
+  if (isNullish(storeData[CKETH_LEDGER_CANISTER_ID.toText()])) {
     icrcCanistersStore.setCanisters({
       ledgerCanisterId: CKETH_LEDGER_CANISTER_ID,
       indexCanisterId: CKETH_INDEX_CANISTER_ID,
