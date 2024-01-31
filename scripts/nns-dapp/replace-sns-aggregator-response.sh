076b4dd8589ab50f948a0e0773e985c2f73bf8c1
Llorenç <llorenc.muntaner@dfinity.org>
2024-01-31 13:32:16 +0100

    Pair programming

--- scripts/nns-dapp/replace-sns-aggregator-response.sh
+++ scripts/nns-dapp/replace-sns-aggregator-response.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
-test_files_base_path="./frontend/src/tests/workflows/Launchpad"
+# Path to the directory where the JSON files live
+TEST_FILES_PATH="./frontend/src/tests/workflows/Launchpad"
 
 # Base URL of the API
-base_url="https://3r4gx-wqaaa-aaaaq-aaaia-cai.icp0.io/v1/sns/list/page"
-
-# Starting page number
-page=0
+BASE_URL="https://3r4gx-wqaaa-aaaaq-aaaia-cai.icp0.io/v1/sns/list/page"
 
 # Loop until an empty or error response is received
-while :; do
+for ((page=0;; page++)); do
   # Construct the URL
-  url="${base_url}/${page}/slow.json"
-  output="${test_files_base_path}/sns-agg-page-${page}.json"
+  url="${BASE_URL}/${page}/slow.json"
+  output="${TEST_FILES_PATH}/sns-agg-page-${page}.json"
 
   if curl -fsSL "$url" | jq . >"$output"; then
     echo "Downloaded page ${page}."
@@ -22,9 +20,6 @@ while :; do
     rm "$output"
     break
   fi
-
-  # Increment the page number
-  page=$((page + 1))
 done
 
 echo "All pages downloaded."
