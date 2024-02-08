c2ff607fabe7919ff00b44010a31fce74aff34ae
Max <bitdivine@users.noreply.github.com>
2024-02-08 17:11:54 +0100

    fix: Include omitted code (#4432)
    # Motivation
    Code was accidentally omitted from #4425 
    
    # Changes
    - `replace_accounts_db()` now returns the original database, as
    intended.

--- rs/backend/src/accounts_store/constructors.rs
+++ rs/backend/src/accounts_store/constructors.rs
@@ -1,5 +1,7 @@
 //! Account store constructors.
 use super::*;
+#[cfg(test)]
+use std::mem;
 
 impl From<AccountsDb> for AccountsStore {
     fn from(db: AccountsDb) -> Self {
@@ -19,8 +21,11 @@ impl AccountsStore {
     ///
     /// When recreating state post upgrade, the accounts store sans `accounts_db` is recovered from
     /// one virtual memory, then the `accounts_db` is added from another virtual memory.
+    ///
+    /// # Returns
+    /// - The original accounts database.  This is always an `AccountsDbAsProxy`.
     #[must_use]
-    pub fn replace_accounts_db(&mut self, accounts_db: AccountsDb) {
-        self.accounts_db = AccountsDbAsProxy::from(accounts_db);
+    pub fn replace_accounts_db(&mut self, accounts_db: AccountsDb) -> AccountsDbAsProxy {
+        mem::replace(&mut self.accounts_db, AccountsDbAsProxy::from(accounts_db))
     }
 }
