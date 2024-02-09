2dfd6878331e89fe44f19c383707ce4e307f196e
Max Murphy <max@winning.black>
2024-02-09 11:22:01 +0100

    docs++

--- rs/proposals/src/lib.rs
+++ rs/proposals/src/lib.rs
@@ -1,3 +1,4 @@
+//! Tests that verify the primary goal of the proposals crate: to provide a way to fetch proposals and convert them to JSON.
 use crate::canisters::internet_identity::InternetIdentityInit;
 use crate::canisters::nns_governance::api::{Action, ProposalInfo};
 use crate::def::*;
