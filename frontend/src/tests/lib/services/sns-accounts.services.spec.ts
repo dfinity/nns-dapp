2e272c2c7fa84e04519df5395389e77a58d3d911
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2024-02-16 14:10:34 +0100

    NNS1-2851: Remove snsTransferTokens (#4499)
    # Motivation
    
    Remove unused code.
    
    In this PR, remove the specific service to make SNS transfers because
    now SNSes use ICRC services.
    
    # Changes
    
    * Remove `snsTransferTokens`.
    * Remove sns-ledger api file.
    
    # Tests
    
    * Remove test for `snsTransferTokens`.
    * Remove test file for sns-ledger api.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    
    Not necessary.

--- frontend/src/tests/lib/services/sns-accounts.services.spec.ts
+++ frontend/src/tests/lib/services/sns-accounts.services.spec.ts
@@ -1,11 +1,9 @@
 import * as agent from "$lib/api/agent.api";
 import * as ledgerApi from "$lib/api/icrc-ledger.api";
-import * as snsLedgerApi from "$lib/api/sns-ledger.api";
 import { snsAccountsStore } from "$lib/derived/sns/sns-accounts.derived";
 import * as services from "$lib/services/sns-accounts.services";
 import { icrcAccountsStore } from "$lib/stores/icrc-accounts.store";
 import { icrcTransactionsStore } from "$lib/stores/icrc-transactions.store";
-import * as toastsStore from "$lib/stores/toasts.store";
 import {
   mockIdentity,
   mockPrincipal,
@@ -13,7 +11,7 @@ import {
 } from "$tests/mocks/auth.store.mock";
 import { mockIcrcTransactionWithId } from "$tests/mocks/icrc-transactions.mock";
 import { mockSnsMainAccount } from "$tests/mocks/sns-accounts.mock";
-import { mockSnsToken, principal } from "$tests/mocks/sns-projects.mock";
+import { principal } from "$tests/mocks/sns-projects.mock";
 import { resetSnsProjects, setSnsProjects } from "$tests/utils/sns.test-utils";
 import type { HttpAgent } from "@dfinity/agent";
 import { waitFor } from "@testing-library/svelte";
@@ -191,91 +189,6 @@ describe("sns-accounts-services", () => {
       const transactionsStore = get(icrcTransactionsStore);
       expect(transactionsStore[rootCanisterId.toText()]).toBeUndefined();
       expect(spyQuery).toBeCalled();
-    });
-  });
-
-  describe("snsTransferTokens", () => {
-    let spyAccounts;
-
-    beforeEach(() => {
-      vi.clearAllMocks();
-      icrcAccountsStore.reset();
-      vi.spyOn(console, "error").mockImplementation(() => undefined);
-
-      spyAccounts = vi
-        .spyOn(ledgerApi, "queryIcrcBalance")
-        .mockResolvedValue(mockSnsMainAccount.balanceUlps);
-    });
-
-    it("should call sns transfer tokens", async () => {
-      setSnsProjects([
-        {
-          rootCanisterId: mockPrincipal,
-          tokenMetadata: {
-            ...mockSnsToken,
-            fee: 100n,
-          },
-        },
-      ]);
-      const spyTransfer = vi
-        .spyOn(snsLedgerApi, "snsTransfer")
-        .mockResolvedValue(123n);
-
-      const { blockIndex } = await services.snsTransferTokens({
-        rootCanisterId: mockPrincipal,
-        source: mockSnsMainAccount,
-        destinationAddress: "aaaaa-aa",
-        amount: 1,
-      });
-
-      expect(blockIndex).toEqual(123n);
-      expect(spyTransfer).toBeCalled();
-      expect(spyAccounts).toBeCalled();
-    });
-
-    it("should show toast and return success false if transfer fails", async () => {
-      setSnsProjects([
-        {
-          rootCanisterId: mockPrincipal,
-          tokenMetadata: { ...mockSnsToken, fee: 100n },
-        },
-      ]);
-      const spyTransfer = vi
-        .spyOn(snsLedgerApi, "snsTransfer")
-        .mockRejectedValue(new Error("test error"));
-      const spyOnToastsError = vi.spyOn(toastsStore, "toastsError");
-
-      const { blockIndex } = await services.snsTransferTokens({
-        rootCanisterId: mockPrincipal,
-        source: mockSnsMainAccount,
-        destinationAddress: "aaaaa-aa",
-        amount: 1,
-      });
-
-      expect(blockIndex).toBeUndefined();
-      expect(spyTransfer).toBeCalled();
-      expect(spyAccounts).not.toBeCalled();
-      expect(spyOnToastsError).toBeCalled();
-    });
-
-    it("should show toast and return success false if there is no transaction fee", async () => {
-      resetSnsProjects();
-      const spyTransfer = vi
-        .spyOn(snsLedgerApi, "snsTransfer")
-        .mockRejectedValue(new Error("test error"));
-      const spyOnToastsError = vi.spyOn(toastsStore, "toastsError");
-
-      const { blockIndex } = await services.snsTransferTokens({
-        rootCanisterId: mockPrincipal,
-        source: mockSnsMainAccount,
-        destinationAddress: "aaaaa-aa",
-        amount: 1,
-      });
-
-      expect(blockIndex).toBeUndefined();
-      expect(spyTransfer).not.toBeCalled();
-      expect(spyAccounts).not.toBeCalled();
-      expect(spyOnToastsError).toBeCalled();
     });
   });
 });
