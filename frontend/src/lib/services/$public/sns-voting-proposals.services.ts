554cd995acf71107e8a1282c7235ea87ae4f1fca
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-02-19 17:54:43 +0100

    feat: sns voting proposals based on include_ballots_by_caller

--- frontend/src/lib/services/$public/sns-voting-proposals.services.ts
+++ frontend/src/lib/services/$public/sns-voting-proposals.services.ts
@@ -5,10 +5,12 @@ import { selectableUniversesStore } from "$lib/derived/selectable-universes.deri
 import { getAuthenticatedIdentity } from "$lib/services/auth.services";
 import { snsNeuronsStore } from "$lib/stores/sns-neurons.store";
 import { votingSnsProposalsStore } from "$lib/stores/voting-sns-proposals.store";
+import { votableSnsNeurons } from "$lib/utils/sns-neuron.utils";
+import type { Identity } from "@dfinity/agent";
 import { Principal } from "@dfinity/principal";
-import type { SnsNeuron, SnsProposalData } from "@dfinity/sns";
+import type { SnsListProposalsResponse, SnsNeuron } from "@dfinity/sns";
 import { SnsProposalRewardStatus } from "@dfinity/sns";
-import { nonNullish } from "@dfinity/utils";
+import { fromDefinedNullable, nonNullish } from "@dfinity/utils";
 import { get } from "svelte/store";
 
 // TODO(max): use the store first, clean the store after user voted
@@ -18,38 +20,72 @@ export const queryVotingSnsProposals = async () => {
     .filter(({ canisterId }) => canisterId !== OWN_CANISTER_ID_TEXT)
     .map(({ canisterId }) => canisterId);
 
-  // TODO(max): error handling?
   await Promise.all(
-    // TODO(max): refactor me, need a separate function
-    canisterIds.map(async (canisterId) => {
-      const storeValue = get(votingSnsProposalsStore)[canisterId];
-      if (nonNullish(storeValue)) {
-        // already fetched
-        return;
-      }
-
-      const neurons = await queryNeurons(canisterId);
-
-      // TODO(max): apply also upcoming ballots here
-      if (neurons?.length === 0) {
-        return [];
-      }
-      const proposals = await querySnsProposals(canisterId);
-      votingSnsProposalsStore.setProposals({
-        rootCanisterId: Principal.fromText(canisterId),
-        proposals,
-      });
-    })
+    canisterIds.map(async (canisterId) => updateVotingProposals(canisterId))
   );
+
+  console.log("queryVotingSnsProposals done", get(votingSnsProposalsStore));
 };
 
-const queryNeurons = async (rootCanisterId: string): Promise<SnsNeuron[]> => {
+const updateVotingProposals = async (canisterId: string): Promise<void> => {
+  try {
+    const storeValue = get(votingSnsProposalsStore)[canisterId];
+    if (nonNullish(storeValue)) {
+      // already fetched
+      return;
+    }
+
+    const identity = await getAuthenticatedIdentity();
+    const neurons = await queryNeurons({
+      rootCanisterId: canisterId,
+      identity,
+    });
+
+    // no need to fetch proposals if there are no neurons to vote with
+    if (neurons.length === 0) {
+      return;
+    }
+
+    const { proposals: allProposals, include_ballots_by_caller } =
+      await querySnsProposals(canisterId);
+    const includeBallotsByCaller = fromDefinedNullable(
+      include_ballots_by_caller
+    );
+
+    if (includeBallotsByCaller === false) {
+      // not possible to filter out proposals that are votable
+      return;
+    }
+
+    const votableProposals = allProposals.filter(
+      (proposal) =>
+        votableSnsNeurons({
+          neurons,
+          proposal,
+          identity,
+        }).length > 0
+    );
+    votingSnsProposalsStore.setProposals({
+      rootCanisterId: Principal.fromText(canisterId),
+      proposals: votableProposals,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const queryNeurons = async ({
+  rootCanisterId,
+  identity,
+}: {
+  rootCanisterId: string;
+  identity: Identity;
+}): Promise<SnsNeuron[]> => {
   const storeNeurons = get(snsNeuronsStore)[rootCanisterId];
   if (nonNullish(storeNeurons?.neurons)) {
     return storeNeurons.neurons;
   }
 
-  const identity = await getAuthenticatedIdentity();
   return await querySnsNeurons({
     identity,
     rootCanisterId: Principal.fromText(rootCanisterId),
@@ -57,9 +93,10 @@ const queryNeurons = async (rootCanisterId: string): Promise<SnsNeuron[]> => {
   });
 };
 
+/** Fetches proposals that accept votes */
 const querySnsProposals = async (
   rootCanisterId: string
-): Promise<SnsProposalData[]> => {
+): Promise<SnsListProposalsResponse> => {
   const identity = await getAuthenticatedIdentity();
   return queryProposals({
     params: {
