7ce5bc0a9b49f6c99b3c07ad79c097f8ef1eb1d6
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-02-19 18:05:15 +0100

    refactor: votingProposalCountStore

--- frontend/src/lib/derived/voting-proposal-indication.derived.ts
+++ frontend/src/lib/derived/voting-proposal-indication.derived.ts
@@ -1,6 +1,11 @@
 import { OWN_CANISTER_ID } from "$lib/constants/canister-ids.constants";
+import { AppPath } from "$lib/constants/routes.constants";
+import { authSignedInStore } from "$lib/derived/auth.derived";
+import { pageStore } from "$lib/derived/page.derived";
 import { votingNnsProposalsStore } from "$lib/stores/voting-proposals.store";
 import { votingSnsProposalsStore } from "$lib/stores/voting-sns-proposals.store";
+import { isSelectedPath } from "$lib/utils/navigation.utils";
+import { isNullish } from "@dfinity/utils";
 import { derived, type Readable } from "svelte/store";
 
 export interface VotingProposalCountData {
@@ -8,6 +13,20 @@ export interface VotingProposalCountData {
   [rootCanisterId: string]: number | undefined;
 }
 
+/** Returns true when the indication needs to be shown */
+export const votingProposalIndicationEnabledStore: Readable<boolean> = derived(
+  [pageStore, authSignedInStore],
+  ([{ path: currentPath }, isSignedInd]) =>
+    isNullish(currentPath)
+      ? false
+      : isSignedInd &&
+        isSelectedPath({
+          currentPath,
+          paths: [AppPath.Proposals],
+        })
+);
+
+/** A store that contains the count of proposals that can be voted on by the user mapped by canister id (nns + snses) */
 export const votingProposalCountStore: Readable<VotingProposalCountData> =
   derived(
     [votingNnsProposalsStore, votingSnsProposalsStore],
