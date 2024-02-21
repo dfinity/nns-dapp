f23048b98ab01b4d2ff9ab37025381b45ca5f309
David de Kloet <david.de.kloet@dfinity.org>
2024-02-21 17:51:54 +0100

    format

--- frontend/src/tests/mocks/icp-transactions.mock.ts
+++ frontend/src/tests/mocks/icp-transactions.mock.ts
@@ -1,19 +1,16 @@
+import type { Transaction, TransactionWithId } from "@dfinity/ledger-icp";
 import { mockMainAccount, mockSubAccount } from "./icp-accounts.store.mock";
-import type {
-  Transaction,
-  TransactionWithId,
-} from "@dfinity/ledger-icp";
 
 export const mockTransactionTransfer: Transaction = {
   memo: 0n,
   icrc1_memo: [],
   operation: {
     Transfer: {
-    to: mockSubAccount.identifier,
-    fee: {e8s: 10_000n},
-    from: mockMainAccount.identifier,
-    amount: {e8s: 100_000_000n},
-    spender: [],
+      to: mockSubAccount.identifier,
+      fee: { e8s: 10_000n },
+      from: mockMainAccount.identifier,
+      amount: { e8s: 100_000_000n },
+      spender: [],
     },
   },
   created_at_time: [{ timestamp_nanos: 234n }],
