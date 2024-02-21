6875d7ff07ba003cd3a2b32307d551e899c896cc
Max Strasinsky <98811342+mstrasinskis@users.noreply.github.com>
2024-02-21 14:46:26 +0100

    New actionable nns proposals store (#4520)
    # Motivation
    
    Add `actionableNnsProposalsStore` to store proposals that user can vote
    for. Will be used for displaying voting proposal count and in
    "Actionable Proposals" tab.
    
    # Changes
    
    - add a store
    
    # Tests
    
    - test the new store
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    Not yet

---
+++ frontend/src/tests/lib/stores/actionable-nns-proposals.store.spec.ts
@@ -0,0 +1,25 @@
+import { actionableNnsProposalsStore } from "$lib/stores/actionable-nns-proposals.store";
+import { mockProposalInfo } from "$tests/mocks/proposal.mock";
+import { get } from "svelte/store";
+
+describe("actionable Nns proposals store", () => {
+  beforeEach(() => {
+    actionableNnsProposalsStore.reset();
+  });
+
+  it("should push proposal", () => {
+    actionableNnsProposalsStore.setProposals([mockProposalInfo]);
+    expect(get(actionableNnsProposalsStore).proposals).toEqual([
+      mockProposalInfo,
+    ]);
+  });
+
+  it("should reset proposals", () => {
+    actionableNnsProposalsStore.setProposals([mockProposalInfo]);
+    expect(get(actionableNnsProposalsStore).proposals).toEqual([
+      mockProposalInfo,
+    ]);
+    actionableNnsProposalsStore.reset();
+    expect(get(actionableNnsProposalsStore).proposals).toEqual(undefined);
+  });
+});
