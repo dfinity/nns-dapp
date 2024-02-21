920864530311acf7f0401c6f59da4cc5146980f1
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-02-21 11:53:05 +0100

    test: actionableNnsProposalsStore.reset

--- frontend/src/lib/stores/actionable-nns-proposals.store.ts
+++ frontend/src/lib/stores/actionable-nns-proposals.store.ts
@@ -14,9 +14,6 @@ export interface ActionableNnsProposalsStore
 /**
  * A store that contains proposals that can be voted on by the user (ballots w/ state 0).
  * Better keep nns and sns stores separate, as they should be available for "Actionable Proposals" tab.
- *
- * The update can't be merged with the current state because the proposals status can be updated.
- * - setProposals: replace the current list of proposals with a new list
  */
 const initActionableNnsProposalsStore = (): ActionableNnsProposalsStore => {
   const { subscribe, set } = writable<ActionableNnsProposalsStoreData>({
