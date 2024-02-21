dc3ef68a7193266a02162b5fb067e98d0bd0f5c7
David de Kloet <david.de.kloet@dfinity.org>
2024-02-21 16:23:20 +0100

    cp icrc-transactions.store.ts icp-transactions.store.ts

---
+++ frontend/src/lib/stores/icp-transactions.store.ts
@@ -0,0 +1,143 @@
+import type { GetTransactionsResponse } from "$lib/api/icrc-index.api";
+import type { IcrcAccountIdentifierText } from "$lib/types/icrc";
+import type {
+  UniverseCanisterId,
+  UniverseCanisterIdText,
+} from "$lib/types/universe";
+import { getUniqueTransactions } from "$lib/utils/icrc-transactions.utils";
+import { removeKeys } from "$lib/utils/utils";
+import type { IcrcTransactionWithId } from "@dfinity/ledger-icrc";
+import type { Principal } from "@dfinity/principal";
+import { nonNullish } from "@dfinity/utils";
+import { writable, type Readable } from "svelte/store";
+
+// Each Icrc Account - Sns or ckBTC - is an entry in this store.
+// We use the account string representation as the key to identify the transactions.
+export type IcrcTransactions = Record<
+  IcrcAccountIdentifierText,
+  {
+    transactions: IcrcTransactionWithId[];
+    oldestTxId?: bigint;
+    completed: boolean;
+  }
+>;
+
+// Each universe - except Nns - is an entry in this store.
+// We use the root canister id and then the account identifier as the key to identify the transactions.
+export type IcrcTransactionsStoreData = Record<
+  UniverseCanisterIdText,
+  IcrcTransactions
+>;
+
+export interface IcrcTransactionsStore
+  extends Readable<IcrcTransactionsStoreData> {
+  addTransactions: (
+    data: {
+      accountIdentifier: string;
+      canisterId: UniverseCanisterId;
+      completed: boolean;
+    } & GetTransactionsResponse
+  ) => void;
+  reset: () => void;
+  resetUniverse: (canisterId: UniverseCanisterId) => void;
+  resetAccount: (params: {
+    canisterId: UniverseCanisterId;
+    accountIdentifier: string;
+  }) => void;
+}
+
+/**
+ * A store that contains the transactions for each account in sns projects.
+ *
+ * - addTransactions: adds new transactions for a specific account in a specific sns project. If the state does not exist, it will be created.
+ * - reset: reset the store to an empty state.
+ * - resetProject: removed the transactions for a specific project.
+ */
+const initIcrcTransactionsStore = (): IcrcTransactionsStore => {
+  const { subscribe, update, set } = writable<IcrcTransactionsStoreData>({});
+
+  return {
+    subscribe,
+
+    addTransactions({
+      accountIdentifier,
+      canisterId,
+      transactions,
+      oldestTxId,
+      completed,
+    }: {
+      accountIdentifier: string;
+      canisterId: Principal;
+      transactions: IcrcTransactionWithId[];
+      oldestTxId?: bigint;
+      completed: boolean;
+    }) {
+      update((currentState: IcrcTransactionsStoreData) => {
+        const projectState = currentState[canisterId.toText()];
+        const accountState = projectState?.[accountIdentifier];
+        const allTransactions = getUniqueTransactions([
+          ...(accountState?.transactions ?? []),
+          ...transactions,
+        ]);
+        // Ids are in increasing order. We want to keep the oldest id.
+        const newOldestTxId =
+          oldestTxId === undefined
+            ? accountState?.oldestTxId
+            : oldestTxId <= (accountState?.oldestTxId ?? oldestTxId)
+            ? oldestTxId
+            : accountState?.oldestTxId;
+        return {
+          ...currentState,
+          [canisterId.toText()]: {
+            ...projectState,
+            [accountIdentifier]: {
+              transactions: allTransactions,
+              oldestTxId: newOldestTxId,
+              completed,
+            },
+          },
+        };
+      });
+    },
+
+    // Used in tests
+    reset() {
+      set({});
+    },
+
+    resetUniverse(canisterId: UniverseCanisterId) {
+      update((currentState: IcrcTransactionsStoreData) =>
+        removeKeys({
+          obj: currentState,
+          keysToRemove: [canisterId.toText()],
+        })
+      );
+    },
+
+    resetAccount({
+      canisterId,
+      accountIdentifier,
+    }: {
+      canisterId: UniverseCanisterId;
+      accountIdentifier: string;
+    }) {
+      update((currentState: IcrcTransactionsStoreData) => {
+        const projectState = currentState[canisterId.toText()];
+        return {
+          ...removeKeys({
+            obj: currentState,
+            keysToRemove: [canisterId.toText()],
+          }),
+          ...(nonNullish(projectState) && {
+            [canisterId.toText()]: removeKeys({
+              obj: projectState,
+              keysToRemove: [accountIdentifier],
+            }),
+          }),
+        };
+      });
+    },
+  };
+};
+
+export const icrcTransactionsStore = initIcrcTransactionsStore();
---
+++ frontend/src/tests/lib/stores/icp-transactions.store.spec.ts
@@ -0,0 +1,88 @@
+import { CKTESTBTC_UNIVERSE_CANISTER_ID } from "$lib/constants/ckbtc-canister-ids.constants";
+import { icrcTransactionsStore } from "$lib/stores/icrc-transactions.store";
+import { mockCkBTCMainAccount } from "$tests/mocks/ckbtc-accounts.mock";
+import { mockIcrcTransactionWithId } from "$tests/mocks/icrc-transactions.mock";
+import { get } from "svelte/store";
+
+describe("icrc-transactions", () => {
+  it("should reset account", () => {
+    const otherAccountIdentifier = "otherAccountIdentifier";
+
+    icrcTransactionsStore.addTransactions({
+      canisterId: CKTESTBTC_UNIVERSE_CANISTER_ID,
+      transactions: [mockIcrcTransactionWithId],
+      accountIdentifier: mockCkBTCMainAccount.identifier,
+      oldestTxId: 10n,
+      completed: false,
+    });
+
+    const accounts = get(icrcTransactionsStore);
+    expect(
+      accounts[CKTESTBTC_UNIVERSE_CANISTER_ID.toText()]
+    ).not.toBeUndefined();
+    expect(
+      accounts[CKTESTBTC_UNIVERSE_CANISTER_ID.toText()][
+        mockCkBTCMainAccount.identifier
+      ]
+    ).not.toBeUndefined();
+
+    icrcTransactionsStore.addTransactions({
+      canisterId: CKTESTBTC_UNIVERSE_CANISTER_ID,
+      transactions: [mockIcrcTransactionWithId],
+      accountIdentifier: otherAccountIdentifier,
+      oldestTxId: 10n,
+      completed: false,
+    });
+
+    const accounts2 = get(icrcTransactionsStore);
+    expect(
+      accounts2[CKTESTBTC_UNIVERSE_CANISTER_ID.toText()][otherAccountIdentifier]
+    ).not.toBeUndefined();
+
+    icrcTransactionsStore.resetAccount({
+      canisterId: CKTESTBTC_UNIVERSE_CANISTER_ID,
+      accountIdentifier: mockCkBTCMainAccount.identifier,
+    });
+
+    const accounts3 = get(icrcTransactionsStore);
+    expect(
+      accounts3[CKTESTBTC_UNIVERSE_CANISTER_ID.toText()]
+    ).not.toBeUndefined();
+    expect(
+      accounts3[CKTESTBTC_UNIVERSE_CANISTER_ID.toText()][
+        mockCkBTCMainAccount.identifier
+      ]
+    ).toBeUndefined();
+  });
+
+  it("should dedupe transactions", () => {
+    const canisterId = CKTESTBTC_UNIVERSE_CANISTER_ID;
+    const identifier = mockCkBTCMainAccount.identifier;
+
+    icrcTransactionsStore.addTransactions({
+      canisterId,
+      transactions: [mockIcrcTransactionWithId],
+      accountIdentifier: identifier,
+      oldestTxId: 10n,
+      completed: false,
+    });
+
+    expect(
+      get(icrcTransactionsStore)[canisterId.toText()][identifier].transactions
+        .length
+    ).toBe(1);
+
+    icrcTransactionsStore.addTransactions({
+      canisterId,
+      transactions: [mockIcrcTransactionWithId, mockIcrcTransactionWithId],
+      accountIdentifier: identifier,
+      oldestTxId: 10n,
+      completed: false,
+    });
+
+    expect(
+      get(icrcTransactionsStore)[canisterId.toText()][identifier].transactions
+        .length
+    ).toBe(1);
+  });
+});
