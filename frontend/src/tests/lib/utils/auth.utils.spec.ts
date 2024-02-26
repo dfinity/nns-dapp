11c7a44eebbaa79e2725de38dc608b2cb4cb93d8
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-02-26 14:32:19 +0100

    test: getIdentityProviderUrl

--- frontend/src/tests/lib/utils/auth.utils.spec.ts
+++ frontend/src/tests/lib/utils/auth.utils.spec.ts
@@ -1,4 +1,12 @@
-import { isSignedIn, loadIdentity } from "$lib/utils/auth.utils";
+import {
+  IDENTITY_SERVICE_URL,
+  OLD_MAINNET_IDENTITY_SERVICE_URL,
+} from "$lib/constants/identity.constants";
+import {
+  getIdentityProviderUrl,
+  isSignedIn,
+  loadIdentity,
+} from "$lib/utils/auth.utils";
 import { mockIdentity } from "$tests/mocks/auth.store.mock";
 import { AuthClient } from "@dfinity/auth-client";
 import { mock } from "vitest-mock-extended";
@@ -45,6 +53,41 @@ describe("auth-utils", () => {
       it("should return identity if authenticated", async () => {
         expect(await loadIdentity()).toEqual(mockIdentity);
       });
+    });
+  });
+  describe("getIdentityProviderUrl", () => {
+    const { host } = window.location;
+
+    afterEach(() => {
+      // restore original host
+      window.location.host = host;
+    });
+
+    /*
+        vi.stubEnv(
+      "VITE_IDENTITY_SERVICE_URL",
+      "http://qhbym-qaaaa-aaaaa-aaafq-cai.localhost:8080"
+    );
+     */
+
+    it("should return old mainnet identity from ic0.app", async () => {
+      Object.defineProperty(window, "location", {
+        writable: true,
+        value: { host: ".ic0.app" },
+      });
+
+      expect(await getIdentityProviderUrl()).toEqual(
+        OLD_MAINNET_IDENTITY_SERVICE_URL
+      );
+    });
+
+    it("should return IDENTITY_SERVICE_URL when not old mainnet", async () => {
+      Object.defineProperty(window, "location", {
+        writable: true,
+        value: { host: "nns.internetcomputer.org" },
+      });
+
+      expect(await getIdentityProviderUrl()).toEqual(IDENTITY_SERVICE_URL);
     });
   });
 });
