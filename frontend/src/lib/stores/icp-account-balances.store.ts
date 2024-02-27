b4e3e35acfbc8e094902a1a4630c73a9b7c19992
David de Kloet <david.de.kloet@dfinity.org>
2024-02-27 13:26:07 +0100

    Add icpAccountBalancesStore

---
+++ frontend/src/lib/stores/icp-account-balances.store.ts
@@ -0,0 +1,56 @@
+import { writable, type Readable } from "svelte/store";
+
+export interface IcpAccountBalanceStoreData {
+  balanceE8s: bigint;
+  certified: boolean;
+}
+
+// The key is the ICP account identifier.
+export type IcpAccountBalancesStoreData = Record<
+  string,
+  IcpAccountBalanceStoreData
+>;
+
+export interface IcpAccountBalancesStore
+  extends Readable<IcpAccountBalancesStoreData> {
+  setBalance: ({
+    accountIdentifier,
+    balanceE8s,
+    certified,
+  }: {
+    accountIdentifier: string;
+    balanceE8s: bigint;
+    certified: boolean;
+  }) => void;
+  reset: () => void;
+}
+
+const initIpcAccountBalancesStore = (): IcpAccountBalancesStore => {
+  const initialStoreData = {};
+  const { subscribe, set, update } =
+    writable<IcpAccountBalancesStoreData>(initialStoreData);
+
+  return {
+    subscribe,
+    setBalance({
+      accountIdentifier,
+      balanceE8s,
+      certified,
+    }: {
+      accountIdentifier: string;
+      balanceE8s: bigint;
+      certified: boolean;
+    }) {
+      update((storeData) => ({
+        ...storeData,
+        [accountIdentifier]: {
+          balanceE8s,
+          certified,
+        },
+      }));
+    },
+    reset: () => set(initialStoreData),
+  };
+};
+
+export const icpAccountBalancesStore = initIpcAccountBalancesStore();
