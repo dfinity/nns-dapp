efc43a3c2fca3648e252e4a2afc2094cc7e4d394
David de Kloet <david.de.kloet@dfinity.org>
2024-02-28 16:29:20 +0100

    Make icpAccountBalancesStore a queued store

--- frontend/src/lib/stores/icp-account-balances.store.ts
+++ frontend/src/lib/stores/icp-account-balances.store.ts
@@ -1,5 +1,7 @@
 import type { AccountIdentifierString } from "$lib/canisters/nns-dapp/nns-dapp.types";
-import { writable, type Readable } from "svelte/store";
+import type { QueryAndUpdateStrategy } from "$lib/services/utils.services";
+import type { Readable } from "svelte/store";
+import { queuedStore } from "./queued-store";
 
 export interface IcpAccountBalanceStoreData {
   balanceE8s: bigint;
@@ -11,8 +13,14 @@ export type IcpAccountBalancesStoreData = Record<
   IcpAccountBalanceStoreData
 >;
 
-export interface IcpAccountBalancesStore
-  extends Readable<IcpAccountBalancesStoreData> {
+export interface SingleMutationIcpAccountBalancesStore {
+  set: ({
+    data,
+    certified,
+  }: {
+    data: IcpAccountBalancesStoreData;
+    certified: boolean;
+  }) => void;
   setBalance: ({
     accountIdentifier,
     balanceE8s,
@@ -22,34 +30,71 @@ export interface IcpAccountBalancesStore
     balanceE8s: bigint;
     certified: boolean;
   }) => void;
-  reset: () => void;
+  reset: ({ certified }: { certified: boolean }) => void;
+  cancel: () => void;
+}
+
+export interface IcpAccountBalancesStore
+  extends Readable<IcpAccountBalancesStoreData> {
+  getSingleMutationIcpAccountBalancesStore: (
+    strategy?: QueryAndUpdateStrategy | undefined
+  ) => SingleMutationIcpAccountBalancesStore;
+  resetForTesting: () => void;
+  // Set the store contents if you don't care about the queryAndUpdate race
+  // condition.
+  setForTesting: (data: IcpAccountBalancesStoreData) => void;
 }
 
 const initIpcAccountBalancesStore = (): IcpAccountBalancesStore => {
   const initialStoreData = {};
-  const { subscribe, set, update } =
-    writable<IcpAccountBalancesStoreData>(initialStoreData);
+
+  const { subscribe, getSingleMutationStore, resetForTesting } =
+    queuedStore<IcpAccountBalancesStoreData>(initialStoreData);
+
+  const getSingleMutationIcpAccountBalancesStore = (
+    strategy?: QueryAndUpdateStrategy | undefined
+  ): SingleMutationIcpAccountBalancesStore => {
+    const { set, update, cancel } = getSingleMutationStore(strategy);
+
+    return {
+      set,
+
+      setBalance({
+        accountIdentifier,
+        balanceE8s,
+        certified,
+      }: {
+        accountIdentifier: string;
+        balanceE8s: bigint;
+        certified: boolean;
+      }) {
+        update({
+          mutation: (storeData) => ({
+            ...storeData,
+            [accountIdentifier]: {
+              balanceE8s,
+              certified,
+            },
+          }),
+          certified,
+        });
+      },
+      reset: ({ certified }: { certified: boolean }) =>
+        set({ data: initialStoreData, certified }),
+
+      cancel,
+    };
+  };
 
   return {
     subscribe,
-    setBalance({
-      accountIdentifier,
-      balanceE8s,
-      certified,
-    }: {
-      accountIdentifier: string;
-      balanceE8s: bigint;
-      certified: boolean;
-    }) {
-      update((storeData) => ({
-        ...storeData,
-        [accountIdentifier]: {
-          balanceE8s,
-          certified,
-        },
-      }));
+    getSingleMutationIcpAccountBalancesStore,
+    resetForTesting,
+
+    setForTesting(data: IcpAccountBalancesStoreData) {
+      const mutableStore = getSingleMutationIcpAccountBalancesStore();
+      mutableStore.set({ data, certified: true });
     },
-    reset: () => set(initialStoreData),
   };
 };
 
