7519378ca2533c0227dc3b83a553daa4d4c7e3c7
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2024-02-29 17:29:23 +0100

    NNS1-2857: Replace icpAccountsStore with derived icpAccountsStore (#4566)
    # Motivation
    
    The current icpAccountStore holds a combination of accounts and balances
    in a single store. This means all the data has to be loaded together and
    if we just want to change a balances, we have to manipulate the data in
    the store, which can interfere with loading the accounts.
    
    The cleaner way is to have a separate store for accounts and a separate
    store for balances and a derived store which merged both into accounts
    with balances. That way when we update a balance in the balances store,
    it will just automatically recreate the accounts with balances.
    
    The derived accounts store has already been created in previous PRs.
    
    In this PR, we replace the old `icpAccountsStore` with the dervied
    `icpAccountsStore` for reading, and fill the `icpAccountDetailsStore`
    and `icpAccountBalanacesStore` for writing.
    
    There are several flows in which the accounts store is filled. In order
    to keep all of them working the same, I tried to keep the same code
    structure of first querying both accounts and balances and then filling
    stores. If we did it from scratch, we'd probably separate these stops
    more but by keeping the same structure it's easier to guarantee that we
    keep the same behavior.
    
    # Changes
    
    1. Replace `loadAccounts` with a function `loadAccountsStoresData` that
    loads account details and balances separately and returns both.
    2. Change the error handler to reset both the `icpAccountDetailsStore`
    and the `icpAccountBalancesStore`.
    3. Change `loadBalance` to update the `icpAccountBalancesStore` instead
    of the old `icpAccountsStore`
    4. Temporarily change `frontend/src/lib/stores/icp-accounts.store.ts` to
    just export the new derived store so we can change all the clients in a
    separate PR.
    
    # Tests
    
    1. Remove the tests for `loadAccounts` as it has been replaced by
    `loadAccountsData` which is not exported out of the file.
    2. Add extra expectations to check the new stores in addition to the
    existing expectations which now check the derived store.
    3. Remove `frontend/src/tests/lib/stores/icp-accounts.store.spec.ts`
    4. Update `setAccountsForTesting`, `setAccountBalanceForTesting` and
    `resetAccountsForTesting` to work on the new stores.
    5. Tested manually at
    https://qsgjb-riaaa-aaaaa-aaaga-cai.dskloet-ingress.devenv.dfinity.network/
    
    # Todos
    
    - [x] Add entry to changelog (if necessary).

--- frontend/src/tests/utils/accounts.test-utils.ts
+++ frontend/src/tests/utils/accounts.test-utils.ts
@@ -1,7 +1,6 @@
-import {
-  icpAccountsStore,
-  type IcpAccountsStoreData,
-} from "$lib/stores/icp-accounts.store";
+import type { IcpAccountsStoreData } from "$lib/derived/icp-accounts.derived";
+import { icpAccountBalancesStore } from "$lib/stores/icp-account-balances.store";
+import { icpAccountDetailsStore } from "$lib/stores/icp-account-details.store";
 import { fireEvent, waitFor, type RenderResult } from "@testing-library/svelte";
 import type { SvelteComponent } from "svelte";
 
@@ -34,8 +33,66 @@ export const selectSegmentBTC = async (container: HTMLElement) => {
   await fireEvent.click(button);
 };
 
-export const setAccountsForTesting = (data: IcpAccountsStoreData) => {
-  icpAccountsStore.setForTesting(data);
+export const setAccountsForTesting = ({
+  main,
+  subAccounts = [],
+  hardwareWallets = [],
+  certified = false,
+}: IcpAccountsStoreData & {
+  certified?: boolean;
+}) => {
+  icpAccountDetailsStore.set({
+    accountDetails: {
+      principal: main.principal,
+      account_identifier: main.identifier,
+      hardware_wallet_accounts: hardwareWallets.map((icpAccount) => ({
+        principal: icpAccount.principal,
+        name: icpAccount.name,
+        account_identifier: icpAccount.identifier,
+      })),
+      sub_accounts: subAccounts.map((icpAccount) => ({
+        name: icpAccount.name,
+        sub_account: icpAccount.subAccount,
+        account_identifier: icpAccount.identifier,
+      })),
+    },
+    certified,
+  });
+
+  const setBalance = ({
+    accountIdentifier,
+    balanceE8s,
+  }: {
+    accountIdentifier: string;
+    balanceE8s: bigint;
+  }) => {
+    const mutableStore =
+      icpAccountBalancesStore.getSingleMutationIcpAccountBalancesStore(
+        certified ? "update" : "query"
+      );
+    mutableStore.setBalance({
+      accountIdentifier,
+      balanceE8s,
+      certified,
+    });
+  };
+
+  setBalance({
+    accountIdentifier: main.identifier,
+    balanceE8s: main.balanceUlps,
+  });
+  subAccounts.forEach((icpAccount) => {
+    setBalance({
+      accountIdentifier: icpAccount.identifier,
+      balanceE8s: icpAccount.balanceUlps,
+    });
+  });
+  hardwareWallets.forEach((icpAccount) => {
+    setBalance({
+      accountIdentifier: icpAccount.identifier,
+      balanceE8s: icpAccount.balanceUlps,
+    });
+  });
 };
 
 export const setAccountBalanceForTesting = ({
@@ -45,10 +102,16 @@ export const setAccountBalanceForTesting = ({
   accountIdentifier: string;
   balanceE8s: bigint;
 }) => {
-  const mutableStore = icpAccountsStore.getSingleMutationIcpAccountsStore();
-  mutableStore.setBalance({ accountIdentifier, balanceE8s, certified: true });
+  const mutableStore =
+    icpAccountBalancesStore.getSingleMutationIcpAccountBalancesStore();
+  mutableStore.setBalance({
+    accountIdentifier,
+    balanceE8s,
+    certified: true,
+  });
 };
 
 export const resetAccountsForTesting = () => {
-  icpAccountsStore.resetForTesting();
+  icpAccountDetailsStore.reset();
+  icpAccountBalancesStore.resetForTesting();
 };
