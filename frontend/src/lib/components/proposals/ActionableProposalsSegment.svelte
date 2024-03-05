8377a95a0554b08f2db0516a6b47ca58bb64a471
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-03-05 14:07:17 +0100

    feat: ActionableProposalsSegment component

---
+++ frontend/src/lib/components/proposals/ActionableProposalsSegment.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { Segment, SegmentButton } from "@dfinity/gix-components";
+  import TestIdWrapper from "$lib/components/common/TestIdWrapper.svelte";
+
+  export let selectedSegment: "all" | "actionable";
+
+  const actionableProposalsSegmentId = Symbol();
+  const allProposalsSegmentId = Symbol();
+
+  let segment: Segment;
+  let selectedSegmentId: symbol = allProposalsSegmentId;
+
+  $: selectedSegment =
+    selectedSegmentId === actionableProposalsSegmentId ? "actionable" : "all";
+</script>
+
+<TestIdWrapper testId="actionable-proposals-segment-component">
+  <Segment bind:selectedSegmentId bind:this={segment}>
+    <!--    TODO(max): move to i18n-->
+    <SegmentButton testId="all-proposals" segmentId={allProposalsSegmentId}
+      >All Proposals</SegmentButton
+    >
+    <SegmentButton
+      testId="actionable-proposals"
+      segmentId={actionableProposalsSegmentId}
+      >Actionable Proposals</SegmentButton
+    >
+  </Segment>
+</TestIdWrapper>
