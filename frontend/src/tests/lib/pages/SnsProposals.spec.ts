55332cb30e78c3653be3ddb31fc5ca10fde26691
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-03-15 11:09:35 +0100

    test: actionable in SnsProposals

--- frontend/src/tests/lib/pages/SnsProposals.spec.ts
+++ frontend/src/tests/lib/pages/SnsProposals.spec.ts
@@ -1,18 +1,25 @@
 import SnsProposals from "$lib/pages/SnsProposals.svelte";
+import { actionableSnsProposalsStore } from "$lib/stores/actionable-sns-proposals.store";
 import { authStore } from "$lib/stores/auth.store";
 import { snsFiltersStore } from "$lib/stores/sns-filters.store";
 import { snsFunctionsStore } from "$lib/stores/sns-functions.store";
 import { snsProposalsStore } from "$lib/stores/sns-proposals.store";
+import { getSnsNeuronIdAsHexString } from "$lib/utils/sns-neuron.utils";
+import { numberToE8s } from "$lib/utils/token.utils";
 import { page } from "$mocks/$app/stores";
 import * as fakeSnsGovernanceApi from "$tests/fakes/sns-governance-api.fake";
 import {
   mockAuthStoreNoIdentitySubscribe,
   mockAuthStoreSubscribe,
+  mockIdentity,
   mockPrincipal,
+  resetIdentity,
 } from "$tests/mocks/auth.store.mock";
 import en from "$tests/mocks/i18n.mock";
 import { nervousSystemFunctionMock } from "$tests/mocks/sns-functions.mock";
 import { createSnsProposal } from "$tests/mocks/sns-proposals.mock";
+import { SnsProposalListPo } from "$tests/page-objects/SnsProposalList.page-object";
+import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
 import { setSnsProjects } from "$tests/utils/sns.test-utils";
 import { runResolvedPromises } from "$tests/utils/timers.test-utils";
 import { AnonymousIdentity } from "@dfinity/agent";
@@ -20,6 +27,10 @@ import {
   SnsProposalDecisionStatus,
   SnsProposalRewardStatus,
   SnsSwapLifecycle,
+  SnsVote,
+  neuronSubaccount,
+  type SnsNeuron,
+  type SnsNeuronId,
   type SnsProposalData,
 } from "@dfinity/sns";
 import { fireEvent, render, waitFor } from "@testing-library/svelte";
@@ -350,6 +361,119 @@ describe("SnsProposals", () => {
       expect(queryByTestId("filter-modal")).not.toBeInTheDocument();
 
       expect(queryAllByTestId("proposal-card").length).toBe(1);
+    });
+  });
+
+  describe("actionable proposals segment", () => {
+    const renderComponent = async () => {
+      const { container } = render(SnsProposals);
+      await runResolvedPromises();
+      return SnsProposalListPo.under(new JestPageObjectElement(container));
+    };
+
+    beforeEach(() => {
+      // authStoreMock.next({
+      //   identity: mockIdentity,
+      // });
+      resetIdentity();
+
+      vi.spyOn(authStore, "subscribe").mockImplementation(
+        mockAuthStoreSubscribe
+      );
+    });
+
+    it("should render all proposals by default", async () => {
+      const po = await renderComponent();
+
+      expect(await po.getAllProposalList().isPresent()).toEqual(true);
+      expect(await po.getActionableProposalList().isPresent()).toEqual(false);
+    });
+
+    it("should switch proposal lists on actionable toggle", async () => {
+      const po = await renderComponent();
+      await po
+        .getSnsProposalFiltersPo()
+        .getActionableProposalsSegmentPo()
+        .clickActionableProposals();
+      await runResolvedPromises();
+
+      expect(await po.getAllProposalList().isPresent()).toEqual(false);
+      expect(await po.getActionableProposalList().isPresent()).toEqual(true);
+
+      await po
+        .getSnsProposalFiltersPo()
+        .getActionableProposalsSegmentPo()
+        .clickAllProposals();
+      await runResolvedPromises();
+
+      expect(await po.getAllProposalList().isPresent()).toEqual(true);
+      expect(await po.getActionableProposalList().isPresent()).toEqual(false);
+    });
+
+    it.only("should render spinner while loading actionable", async () => {
+      const po = await renderComponent();
+      await po
+        .getSnsProposalFiltersPo()
+        .getActionableProposalsSegmentPo()
+        .clickActionableProposals();
+      await runResolvedPromises();
+
+      expect(await po.getSkeletonCardPo().isPresent()).toEqual(true);
+
+      // mock a proposal
+      const subaccount = neuronSubaccount({
+        controller: rootCanisterId,
+        index: 0,
+      });
+      const neuronId: SnsNeuronId = { id: subaccount };
+      fakeSnsGovernanceApi.addNeuronWith({
+        identity: mockIdentity,
+        rootCanisterId,
+        id: [neuronId],
+        cached_neuron_stake_e8s: numberToE8s(10),
+      });
+      fakeSnsGovernanceApi.addProposalWith({
+        identity: mockIdentity,
+        rootCanisterId,
+        ...createSnsProposal({
+          proposalId: 0n,
+          status: SnsProposalDecisionStatus.PROPOSAL_DECISION_STATUS_OPEN,
+          ballots: [
+            [
+              getSnsNeuronIdAsHexString({
+                id: [neuronId] as [SnsNeuronId],
+              } as SnsNeuron),
+              {
+                vote: SnsVote.Unspecified,
+                cast_timestamp_seconds: 123n,
+                voting_power: 10000n,
+              },
+            ],
+          ],
+        }),
+      });
+
+      actionableSnsProposalsStore.set({
+        rootCanisterId,
+        proposals: [
+          createSnsProposal({
+            proposalId: 10n,
+            status: SnsProposalDecisionStatus.PROPOSAL_DECISION_STATUS_OPEN,
+          }),
+        ],
+        includeBallotsByCaller: true,
+      });
+      await runResolvedPromises();
+
+      expect(await po.getSkeletonCardPo().isPresent()).toEqual(false);
+    });
+
+    it("should display login CTA", async () => {
+      // TODO(max): TBD
+    });
+
+    it("should display not supported page", async () => {
+      // TODO(max): TBD
     });
   });
 });
