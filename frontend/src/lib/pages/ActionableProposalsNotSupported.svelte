9ff2bbaa8f8a98f51f0410316cba0570e8bd5d90
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-03-17 21:23:49 +0100

    Add ActionableProposalsNotSupported component

---
+++ frontend/src/lib/pages/ActionableProposalsNotSupported.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import SignIn from "$lib/components/common/SignIn.svelte";
+  import { i18n } from "$lib/stores/i18n";
+  import { PageBanner, IconNotificationPage } from "@dfinity/gix-components";
+  import { replacePlaceholders } from "$lib/utils/i18n.utils.js";
+
+  export let snsName: string;
+</script>
+
+<PageBanner testId="actionable-proposals-not-supported">
+  <IconNotificationPage slot="image" />
+  <svelte:fragment slot="title"
+    >{replacePlaceholders($i18n.actionable_proposals_not_supported.title, {
+      $snsName: snsName,
+    })}</svelte:fragment
+  >
+  <p class="description" slot="description">
+    {$i18n.actionable_proposals_not_supported.text}
+  </p>
+</PageBanner>
--- frontend/src/lib/types/i18n.d.ts
+++ frontend/src/lib/types/i18n.d.ts
@@ -399,6 +399,12 @@ interface I18nActionable_proposals_empty {
   title: string;
   text: string;
 }
+
+interface I18nActionable_proposals_not_supported {
+  title: string;
+  text: string;
+}
+
 interface I18nCanisters {
   aria_label_canister_card: string;
   text: string;
@@ -1285,6 +1291,8 @@ interface I18n {
   follow_neurons: I18nFollow_neurons;
   voting: I18nVoting;
   actionable_proposals_sign_in: I18nActionable_proposals_sign_in;
+  actionable_proposals_empty: I18nActionable_proposals_empty;
+  actionable_proposals_not_supported: I18nActionable_proposals_not_supported;
   canisters: I18nCanisters;
   canister_detail: I18nCanister_detail;
   transaction_names: I18nTransaction_names;
