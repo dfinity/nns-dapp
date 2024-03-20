7dcaf8565998e57c9af980fbfcb9334ef0404d6a
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2024-03-20 11:57:17 +0100

    NNS1-2918: Add HideZeroBalancesToggle component (#4634)
    # Motivation
    
    We want to add a setting to hide tokens with a zero balance.
    We're adding a settings button in the top right of the tokens table.
    This button will open a popup with a toggle.
    This PR adds the component that will be used as the content inside this
    popup.
    
    # Changes
    
    Add `HideZeroBalancesToggle` component to be used inside the settings
    popup.
    
    # Tests
    
    Added unit test for the component.
    Here's what the component will look like in context:
    
    <img width="381" alt="image"
    src="https://github.com/dfinity/nns-dapp/assets/122978264/54df1e25-5b03-4139-b001-e5777e95fc04">
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    not yet

---
+++ frontend/src/tests/lib/components/tokens/TokensTable/HideZeroBalancesToggle.spec.ts
@@ -0,0 +1,56 @@
+import HideZeroBalancesToggle from "$lib/components/tokens/TokensTable/HideZeroBalancesToggle.svelte";
+import { hideZeroBalancesStore } from "$lib/stores/hide-zero-balances.store";
+import { HideZeroBalancesTogglePo } from "$tests/page-objects/HideZeroBalancesToggle.page-object";
+import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
+import { runResolvedPromises } from "$tests/utils/timers.test-utils";
+import { render } from "@testing-library/svelte";
+import { get } from "svelte/store";
+
+describe("HideZeroBalancesToggle", () => {
+  beforeEach(() => {
+    hideZeroBalancesStore.resetForTesting();
+  });
+
+  const renderComponent = () => {
+    const { container } = render(HideZeroBalancesToggle);
+    return HideZeroBalancesTogglePo.under(new JestPageObjectElement(container));
+  };
+
+  it("should update the store when toggled", async () => {
+    const po = renderComponent();
+    const toggle = po.getTogglePo();
+
+    expect(get(hideZeroBalancesStore)).toBe("show");
+    expect(await toggle.isEnabled()).toBe(false);
+
+    await toggle.toggle();
+
+    expect(get(hideZeroBalancesStore)).toBe("hide");
+    expect(await toggle.isEnabled()).toBe(true);
+
+    await toggle.toggle();
+
+    expect(get(hideZeroBalancesStore)).toBe("show");
+    expect(await toggle.isEnabled()).toBe(false);
+  });
+
+  it("should update toggle when store is set", async () => {
+    const po = renderComponent();
+    const toggle = po.getTogglePo();
+
+    expect(get(hideZeroBalancesStore)).toBe("show");
+    expect(await toggle.isEnabled()).toBe(false);
+
+    hideZeroBalancesStore.set("hide");
+    await runResolvedPromises();
+
+    expect(get(hideZeroBalancesStore)).toBe("hide");
+    expect(await toggle.isEnabled()).toBe(true);
+
+    hideZeroBalancesStore.set("show");
+    await runResolvedPromises();
+
+    expect(get(hideZeroBalancesStore)).toBe("show");
+    expect(await toggle.isEnabled()).toBe(false);
+  });
+});
