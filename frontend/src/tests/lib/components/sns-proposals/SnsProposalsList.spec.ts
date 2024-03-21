24d3757ae37c5d22ac49886f203b8df268a42ab0
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-03-21 19:03:07 +0100

    test: Actionable proposals in SnsProposalList

--- frontend/src/tests/lib/components/sns-proposals/SnsProposalsList.spec.ts
+++ frontend/src/tests/lib/components/sns-proposals/SnsProposalsList.spec.ts
@@ -1,10 +1,24 @@
 import SnsProposalsList from "$lib/components/sns-proposals/SnsProposalsList.svelte";
 import { actionableProposalsSegmentStore } from "$lib/stores/actionable-proposals-segment.store";
-import { mockSnsProposal } from "$tests/mocks/sns-proposals.mock";
+import { resetIdentity, setNoIdentity } from "$tests/mocks/auth.store.mock";
+import {
+  createSnsProposal,
+  mockSnsProposal,
+} from "$tests/mocks/sns-proposals.mock";
+import { SnsProposalListPo } from "$tests/page-objects/SnsProposalList.page-object";
+import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
+import { runResolvedPromises } from "$tests/utils/timers.test-utils";
 import type { SnsProposalData } from "@dfinity/sns";
-import { render } from "@testing-library/svelte";
+import { SnsProposalDecisionStatus } from "@dfinity/sns";
+import { cleanup, render } from "@testing-library/svelte";
 
 describe("SnsProposalsList", () => {
+  const renderComponent = async (props) => {
+    cleanup();
+    const { container } = render(SnsProposalsList, { props });
+    await runResolvedPromises();
+    return SnsProposalListPo.under(new JestPageObjectElement(container));
+  };
   const proposal1: SnsProposalData = {
     ...mockSnsProposal,
     id: [{ id: 1n }],
@@ -83,16 +97,112 @@ describe("SnsProposalsList", () => {
   });
 
   describe("actionable proposals", () => {
+    const actionableProposalA = createSnsProposal({
+      proposalId: 123n,
+      status: SnsProposalDecisionStatus.PROPOSAL_DECISION_STATUS_OPEN,
+    });
+    const actionableProposalB = createSnsProposal({
+      proposalId: 321n,
+      status: SnsProposalDecisionStatus.PROPOSAL_DECISION_STATUS_OPEN,
+    });
+    beforeEach(() => {
+      resetIdentity();
+    });
+
     it("should render skeletons while proposals are loading", async () => {
-      const { queryByTestId } = render(SnsProposalsList, {
-        props: {
-          proposals: undefined,
-          isActionable: true,
-          nsFunctions: [],
-        },
+      const po = await renderComponent({
+        proposals: undefined,
+        includeBallots: true,
+        snsName: "sns-name",
+        actionableSelected: true,
+        nsFunctions: [],
       });
 
-      expect(queryByTestId("proposals-loading")).toBeInTheDocument();
+      expect(await po.getSkeletonCardPo().isPresent()).toBe(true);
+    });
+
+    it('should display "Not signIn" banner', async () => {
+      setNoIdentity();
+      const po = await renderComponent({
+        proposals: undefined,
+        includeBallots: false,
+        snsName: "sns-name",
+        actionableSelected: true,
+        nsFunctions: [],
+      });
+      expect(await po.getActionableSignInBanner().isPresent()).toBe(true);
+
+      resetIdentity();
+      const po2 = await renderComponent({
+        proposals: undefined,
+        includeBallots: false,
+        snsName: "sns-name",
+        actionableSelected: true,
+        nsFunctions: [],
+      });
+      expect(await po2.getActionableSignInBanner().isPresent()).toBe(false);
+    });
+
+    it('should display "No actionable proposals" banner', async () => {
+      const po = await renderComponent({
+        proposals: [],
+        includeBallots: true,
+        snsName: "sns-name",
+        actionableSelected: true,
+        nsFunctions: [],
+      });
+      expect(await po.getActionableEmptyBanner().isPresent()).toBe(true);
+
+      const po2 = await renderComponent({
+        proposals: [actionableProposalA],
+        includeBallots: true,
+        snsName: "sns-name",
+        actionableSelected: true,
+        nsFunctions: [],
+      });
+      expect(await po2.getActionableEmptyBanner().isPresent()).toBe(false);
+    });
+
+    it('should display "Actionable not supported" banner', async () => {
+      const po = await renderComponent({
+        proposals: [],
+        includeBallots: false,
+        snsName: "sns-name",
+        actionableSelected: true,
+        nsFunctions: [],
+      });
+      expect(await po.getActionableNotSupportedBanner().isPresent()).toBe(true);
+      expect(await po.getActionableNotSupportedBanner().getTitleText()).toBe(
+        "sns-name doesn't yet support actionable proposals."
+      );
+
+      const poTwo = await renderComponent({
+        proposals: [],
+        includeBallots: true,
+        snsName: "sns-name",
+        actionableSelected: true,
+        nsFunctions: [],
+      });
+      expect(await poTwo.getActionableNotSupportedBanner().isPresent()).toBe(
+        false
+      );
+    });
+
+    it("should display actionable proposals", async () => {
+      const po = await renderComponent({
+        proposals: [actionableProposalA, actionableProposalB],
+        includeBallots: true,
+        snsName: "sns-name",
+        actionableSelected: true,
+        nsFunctions: [],
+      });
+      expect((await po.getProposalCardPos()).length).toEqual(2);
+      expect(await (await po.getProposalCardPos())[0].getProposalId()).toEqual(
+        "ID: 123"
+      );
+      expect(await (await po.getProposalCardPos())[1].getProposalId()).toEqual(
+        "ID: 321"
+      );
     });
   });
 });
