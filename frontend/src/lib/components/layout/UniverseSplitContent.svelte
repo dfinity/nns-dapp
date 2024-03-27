76833ade2f6d152ab7fd71ca7345b74f9d50e460
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-03-27 17:01:57 +0100

    reset: UniverseSplitContent

--- frontend/src/lib/components/layout/UniverseSplitContent.svelte
+++ frontend/src/lib/components/layout/UniverseSplitContent.svelte
@@ -3,10 +3,9 @@
   import Title from "$lib/components/header/Title.svelte";
   import SelectUniverseNav from "$lib/components/universe/SelectUniverseNav.svelte";
   import HeaderToolbar from "$lib/components/header/HeaderToolbar.svelte";
-  import { ENABLE_VOTING_INDICATION } from "$lib/stores/feature-flags.store";
 </script>
 
-<SplitContent perceivedOnMobile={!$ENABLE_VOTING_INDICATION}>
+<SplitContent>
   <SelectUniverseNav slot="start" />
 
   <Title slot="title" />
