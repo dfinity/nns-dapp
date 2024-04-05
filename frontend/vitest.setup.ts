13d0330eb53208e127485b0f271c9f43d8528131
David de Kloet <david.de.kloet@dfinity.org>
2024-04-05 14:58:01 +0200

    vi.unstubAllGlobals in NnsWallet.spec.ts

--- frontend/vitest.setup.ts
+++ frontend/vitest.setup.ts
@@ -110,6 +110,8 @@ vi.mock("$app/stores", () => ({
 }));
 
 // Issue: https://github.com/testing-library/svelte-testing-library/issues/206
-vi.stubGlobal("requestAnimationFrame", (fn) => {
-  return window.setTimeout(() => fn(Date.now()), 0);
+Object.defineProperty(global, "requestAnimationFrame", {
+  value: (fn) => {
+    return window.setTimeout(() => fn(Date.now()), 0);
+  },
 });
