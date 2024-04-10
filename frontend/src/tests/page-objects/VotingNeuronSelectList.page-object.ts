cef6ee34561ec1f35b9ce2a9b0f831297a03ec4a
David de Kloet <david.de.kloet@dfinity.org>
2024-04-10 13:02:35 +0200

    format

--- frontend/src/tests/page-objects/VotingNeuronSelectList.page-object.ts
+++ frontend/src/tests/page-objects/VotingNeuronSelectList.page-object.ts
@@ -1,11 +1,8 @@
 import { VotingNeuronListItemPo } from "$tests/page-objects/VotingNeuronListItem.page-object";
-<<<<<<< HEAD
-=======
 import { NnsProposalFiltersPo } from "$tests/page-objects/NnsProposalFilters.page-object";
 import { PageBannerPo } from "$tests/page-objects/PageBanner.page-object";
 import { ProposalCardPo } from "$tests/page-objects/ProposalCard.page-object";
 import { SkeletonCardPo } from "$tests/page-objects/SkeletonCard.page-object";
->>>>>>> 42e4305c7 (Use page objects in VotingNeuronSelectList)
 import { BasePageObject } from "$tests/page-objects/base.page-object";
 import type { PageObjectElement } from "$tests/types/page-object.types";
 
