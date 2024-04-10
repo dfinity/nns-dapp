2df15d60ec7327093f0560ca118e333012cbeb76
David de Kloet <david.de.kloet@dfinity.org>
2024-04-10 13:36:19 +0200

    Use page objects in VotableNeuronList.spec.ts

---
+++ frontend/src/tests/page-objects/VotableNeuronList.page-object.ts
@@ -0,0 +1,25 @@
+import {  VotingNeuronSelectListPo } from "$tests/page-objects/VotingNeuronSelectList.page-object";
+import { BasePageObject } from "$tests/page-objects/base.page-object";
+import type { PageObjectElement } from "$tests/types/page-object.types";
+
+export class VotableNeuronListPo extends BasePageObject {
+  private static readonly TID = "votable-neurons";
+
+  static under(element: PageObjectElement): VotableNeuronListPo {
+    return new VotableNeuronListPo(
+      element.byTestId(VotableNeuronListPo.TID)
+    );
+  }
+
+  getVotingNeuronSelectListPo(): VotingNeuronSelectListPo {
+    return VotingNeuronSelectListPo.under(this.root);
+  }
+
+  getTitle()  {
+    return this.getText("voting-collapsible-toolbar-neurons");
+  }
+
+  getDisplayedTotalSelectedVotingPower(): Promise<string> {
+    return this.getText("voting-collapsible-toolbar-voting-power");
+  }
+}
