556cfcae460b82849fa0fc3366d58cd9be6bbcbc
David de Kloet <david.de.kloet@dfinity.org>
2024-04-10 13:02:35 +0200

    format

--- frontend/src/tests/page-objects/VotingNeuronSelectList.page-object.ts
+++ frontend/src/tests/page-objects/VotingNeuronSelectList.page-object.ts
@@ -1,8 +1,4 @@
 import { VotingNeuronListItemPo } from "$tests/page-objects/VotingNeuronListItem.page-object";
-import { NnsProposalFiltersPo } from "$tests/page-objects/NnsProposalFilters.page-object";
-import { PageBannerPo } from "$tests/page-objects/PageBanner.page-object";
-import { ProposalCardPo } from "$tests/page-objects/ProposalCard.page-object";
-import { SkeletonCardPo } from "$tests/page-objects/SkeletonCard.page-object";
 import { BasePageObject } from "$tests/page-objects/base.page-object";
 import type { PageObjectElement } from "$tests/types/page-object.types";
 
@@ -10,7 +6,9 @@ export class VotingNeuronSelectListPo extends BasePageObject {
   private static readonly TID = "voting-neuron-select-list-component";
 
   static under(element: PageObjectElement): VotingNeuronSelectListPo {
-    return new VotingNeuronSelectListPo(element.byTestId(VotingNeuronSelectListPo.TID));
+    return new VotingNeuronSelectListPo(
+      element.byTestId(VotingNeuronSelectListPo.TID)
+    );
   }
 
   getVotingNeuronListItemPos(): Promise<VotingNeuronListItemPo[]> {
