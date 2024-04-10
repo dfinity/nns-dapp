42e4305c79da33d81e5e0594e6b5bd6d712eadb8
David de Kloet <david.de.kloet@dfinity.org>
2024-04-10 12:56:36 +0200

    Use page objects in VotingNeuronSelectList

--- frontend/src/tests/page-objects/VotingNeuronListItem.page-object.ts
+++ frontend/src/tests/page-objects/VotingNeuronListItem.page-object.ts
@@ -5,6 +5,14 @@ import type { PageObjectElement } from "$tests/types/page-object.types";
 export class VotingNeuronListItemPo extends BasePageObject {
   private static readonly TID = "voting-neuron-list-item-component";
 
+  static async allUnder(
+    element: PageObjectElement
+  ): Promise<VotingNeuronListItemPo[]> {
+    return Array.from(
+      await element.allByTestId(VotingNeuronListItemPo.TID)
+    ).map((el) => new VotingNeuronListItemPo(el));
+  }
+
   static under(element: PageObjectElement): VotingNeuronListItemPo {
     return new VotingNeuronListItemPo(
       element.byTestId(VotingNeuronListItemPo.TID)
---
+++ frontend/src/tests/page-objects/VotingNeuronSelectList.page-object.ts
@@ -0,0 +1,19 @@
+import { VotingNeuronListItemPo } from "$tests/page-objects/VotingNeuronListItem.page-object";
+import { NnsProposalFiltersPo } from "$tests/page-objects/NnsProposalFilters.page-object";
+import { PageBannerPo } from "$tests/page-objects/PageBanner.page-object";
+import { ProposalCardPo } from "$tests/page-objects/ProposalCard.page-object";
+import { SkeletonCardPo } from "$tests/page-objects/SkeletonCard.page-object";
+import { BasePageObject } from "$tests/page-objects/base.page-object";
+import type { PageObjectElement } from "$tests/types/page-object.types";
+
+export class VotingNeuronSelectListPo extends BasePageObject {
+  private static readonly TID = "voting-neuron-select-list-component";
+
+  static under(element: PageObjectElement): VotingNeuronSelectListPo {
+    return new VotingNeuronSelectListPo(element.byTestId(VotingNeuronSelectListPo.TID));
+  }
+
+  getVotingNeuronListItemPos(): Promise<VotingNeuronListItemPo[]> {
+    return VotingNeuronListItemPo.allUnder(this.root);
+  }
+}
