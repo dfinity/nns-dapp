566a35e5b0c52fc1415ab77262ee9abc18820798
Max Strasinsky <98811342+mstrasinskis@users.noreply.github.com>
2024-04-29 13:44:52 +0200

    Restore NNS1-3004: Message informing about topic renames (#4719) (#4795)
    # Motivation
    
    This is a restored from https://github.com/dfinity/nns-dapp/pull/4719 PR
    w/o any changes.
    
    We are going to change some topics and proposal types and want to make
    sure the users are informed.
    
    # Changes
    
    Add a message linking to the forum post on top of the neurons page for
    users who have neurons.
    
    # Tests
    
    - Tested locally.
    - Unit tests added.
    
    # Todos
    
    - [x] Add entry to changelog (if necessary).
    
    # Screenshots
    
    <img width="367" alt="image"
    src="https://github.com/dfinity/nns-dapp/assets/98811342/a86adc78-e84e-4c57-8c40-0d68a43ddefe">
    
    Co-authored-by: David de Kloet <122978264+dskloetd@users.noreply.github.com>

--- frontend/src/lib/types/i18n.d.ts
+++ frontend/src/lib/types/i18n.d.ts
@@ -35,6 +35,7 @@ interface I18nCore {
   receive: string;
   send_with_token: string;
   collapse_all: string;
+  learn_more: string;
 }
 
 interface I18nError {
@@ -249,6 +250,8 @@ interface I18nNeuron_types {
 interface I18nNeurons {
   title: string;
   text: string;
+  rename_topic_message: string;
+  rename_topic_learn_more_label: string;
   stake_token: string;
   merge_neurons: string;
   merge_neurons_modal_title: string;
