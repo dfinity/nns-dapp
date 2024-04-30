0823e6075c33bc0dbb05fcfdb78261e2b197bd30
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-04-30 18:27:06 +0200

    feat: display ActionableProposalTotalCountBadge

---
+++ frontend/src/lib/components/proposals/ActionableProposalTotalCountBadge.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import ActionableProposalCountBadge from "$lib/components/proposals/ActionableProposalCountBadge.svelte";
+  import { actionableProposalTotalCountStore } from "$lib/derived/actionable-proposals.derived";
+  import { nonNullish } from "@dfinity/utils";
+  import { cubicOut } from "svelte/easing";
+  import { scale } from "svelte/transition";
+
+  let count = 0;
+  $: count = $actionableProposalTotalCountStore;
+</script>
+
+{#if nonNullish(count) && count > 0}
+  <div
+    transition:scale={{
+      duration: 250,
+      easing: cubicOut,
+    }}
+  >
+    <ActionableProposalCountBadge {count} universe="all" noAnimation />
+  </div>
+{/if}
