fa6f8ee99efe37a9ae1c1fe7df0add4ea68be5e1
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-05-07 09:16:55 +0200

    style: update width to make it rounder

--- frontend/src/lib/components/proposals/ActionableProposalCountBadge.svelte
+++ frontend/src/lib/components/proposals/ActionableProposalCountBadge.svelte
@@ -55,7 +55,7 @@
     background: var(--primary);
     color: var(--primary-contrast);
     border-radius: var(--padding-8x);
-    min-width: var(--padding);
+    min-width: 7px;
     justify-content: center;
   }
 </style>
