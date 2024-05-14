58796223f1cd20dc77f577f57e6a2bd1ea314353
David de Kloet <david.de.kloet@dfinity.org>
2024-05-14 14:21:19 +0200

    remove neuron following card changes

--- frontend/src/lib/components/neuron-detail/NeuronFollowingCard/NeuronFollowingCard.svelte
+++ frontend/src/lib/components/neuron-detail/NeuronFollowingCard/NeuronFollowingCard.svelte
@@ -34,7 +34,7 @@
   onMount(listKnownNeurons);
 </script>
 
-<CardInfo noMargin testId="neuron-following-card-component">
+<CardInfo noMargin>
   <KeyValuePairInfo testId="neuron-following">
     <h3 slot="key">{$i18n.neuron_detail.following_title}</h3>
     <svelte:fragment slot="info"
