dfd80766a809344913561968b35ebc6338bb7cde
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-05-16 19:05:18 +0200

    Add actionable to the pageStore

--- frontend/src/routes/(app)/+layout.ts
+++ frontend/src/routes/(app)/+layout.ts
@@ -1,7 +1,11 @@
 import { browser } from "$app/environment";
 import { OWN_CANISTER_ID_TEXT } from "$lib/constants/canister-ids.constants";
-import { UNIVERSE_PARAM } from "$lib/constants/routes.constants";
+import {
+  ACTIONABLE_PARAM,
+  UNIVERSE_PARAM,
+} from "$lib/constants/routes.constants";
 import type { Page } from "$lib/derived/page.derived";
+import { nonNullish } from "@dfinity/utils";
 import type { LoadEvent } from "@sveltejs/kit";
 import type { LayoutLoad } from "./$types";
 
@@ -9,6 +13,7 @@ export const load: LayoutLoad = ($event: LoadEvent): Partial<Page> => {
   if (!browser) {
     return {
       universe: OWN_CANISTER_ID_TEXT,
+      actionable: false,
     };
   }
 
@@ -18,5 +23,6 @@ export const load: LayoutLoad = ($event: LoadEvent): Partial<Page> => {
 
   return {
     universe: searchParams?.get(UNIVERSE_PARAM) ?? undefined,
+    actionable: nonNullish(searchParams?.get(ACTIONABLE_PARAM)),
   };
 };
