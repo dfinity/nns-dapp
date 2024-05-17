c65e5d1b66777e85ce954020387fd919e6d7f1b2
David de Kloet <david.de.kloet@dfinity.org>
2024-05-17 08:46:47 +0200

    lint

--- frontend/src/lib/constants/ckusdc-canister-ids.constants.ts
+++ frontend/src/lib/constants/ckusdc-canister-ids.constants.ts
@@ -3,6 +3,10 @@ import { Principal } from "@dfinity/principal";
 
 const envVars = getEnvVars();
 
+// TODO: Use mainnet canister IDs when available. These are sepolia canister IDs.
+const MAINNET_CKUSDC_LEDGER_CANISTER_ID = "yfumr-cyaaa-aaaar-qaela-cai";
+const MAINNET_CKUSDC_INDEX_CANISTER_ID = "ycvkf-paaaa-aaaar-qaelq-cai";
+
 export const CKUSDCSEPOLIA_LEDGER_CANISTER_ID = Principal.fromText(
   "yfumr-cyaaa-aaaar-qaela-cai"
 );
@@ -10,10 +14,6 @@ export const CKUSDCSEPOLIA_LEDGER_CANISTER_ID = Principal.fromText(
 export const CKUSDCSEPOLIA_INDEX_CANISTER_ID = Principal.fromText(
   "ycvkf-paaaa-aaaar-qaelq-cai"
 );
-
-// TODO: Add mainnet canister IDs.
-const MAINNET_CKUSDC_LEDGER_CANISTER_ID = CKUSDCSEPOLIA_LEDGER_CANISTER_ID;
-const MAINNET_CKUSDC_INDEX_CANISTER_ID = CKUSDCSEPOLIA_INDEX_CANISTER_ID;
 
 export const CKUSDC_LEDGER_CANISTER_ID = Principal.fromText(
   envVars.ckusdcLedgerCanisterId ?? MAINNET_CKUSDC_LEDGER_CANISTER_ID
