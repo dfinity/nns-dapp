d940e600759727eea431b78987bc6fd7791e8e37
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-05-21 13:22:14 +0200

    Add skeletons and all set banners

--- frontend/src/lib/pages/ActionableProposals.svelte
+++ frontend/src/lib/pages/ActionableProposals.svelte
@@ -4,43 +4,31 @@
   import { actionableNnsProposalsStore } from "$lib/stores/actionable-nns-proposals.store";
   import NnsProposalCard from "$lib/components/proposals/NnsProposalCard.svelte";
   import {
+    actionableProposalsLoadedStore,
     actionableProposalTotalCountStore,
     actionableSnsProposalsByUniverseStore,
   } from "$lib/derived/actionable-proposals.derived";
   import SnsWithActionableProposals from "$lib/components/proposals/SnsWithActionableProposals.svelte";
-
-  actionableProposalTotalCountStore;
+  import ActionableProposalsEmpty from "$lib/components/proposals/ActionableProposalsEmpty.svelte";
+  import LoadingProposals from "$lib/components/proposals/LoadingProposals.svelte";
 </script>
 
-<!-- Nns -->
-<UniverseWithActionableProposals universe={$nnsUniverseStore}>
-  {#each $actionableNnsProposalsStore?.proposals ?? [] as proposalInfo (proposalInfo.id)}
-    <NnsProposalCard actionable fromActionablePage {proposalInfo} />
-  {/each}
-</UniverseWithActionableProposals>
-
-<!-- Sns -->
-{#each $actionableSnsProposalsByUniverseStore ?? [] as snsUniverse (snsUniverse.universe.canisterId)}
-  <SnsWithActionableProposals universe={snsUniverse.universe} />
-{/each}
-
-<!--
-<LoadingProposals />
-<ActionableProposalsEmpty />
-
-<div in:fade data-tid="actionable-proposal-list">
-  {#if !$authSignedInStore}
-    <ActionableProposalsSignIn />
-  {:else if isNullish(actionableProposals)}
-    <LoadingProposals />
-  {:else if actionableProposals?.length === 0}
+{#if $actionableProposalTotalCountStore === 0}
+  {#if $actionableProposalsLoadedStore}
     <ActionableProposalsEmpty />
   {:else}
-    <InfiniteScroll layout="grid" disabled>
-      {#each actionableProposals ?? [] as proposalInfo (proposalInfo.id)}
-        <NnsProposalCard {hidden} actionable {proposalInfo} />
-      {/each}
-    </InfiniteScroll>
+    <LoadingProposals />
   {/if}
-</div>
--->
+{:else}
+  <!-- Nns -->
+  <UniverseWithActionableProposals universe={$nnsUniverseStore}>
+    {#each $actionableNnsProposalsStore?.proposals ?? [] as proposalInfo (proposalInfo.id)}
+      <NnsProposalCard actionable fromActionablePage {proposalInfo} />
+    {/each}
+  </UniverseWithActionableProposals>
+
+  <!-- Sns -->
+  {#each $actionableSnsProposalsByUniverseStore ?? [] as snsUniverse (snsUniverse.universe.canisterId)}
+    <SnsWithActionableProposals universe={snsUniverse.universe} />
+  {/each}
+{/if}
