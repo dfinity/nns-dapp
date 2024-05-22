5e2a01c54b9d646f3d66b3cf15009ad09a2e8ca8
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-05-22 10:24:49 +0200

    Add ActionableProposalsNotSupportedSnses component

---
+++ frontend/src/lib/components/proposals/ActionableProposalsNotSupportedSnses.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { i18n } from "$lib/stores/i18n";
+  import { PageBanner, IconNotificationPage } from "@dfinity/gix-components";
+  import { replacePlaceholders } from "$lib/utils/i18n.utils.js";
+  import { actionableProposalNotSupportedUniversesStore } from "$lib/derived/actionable-proposals.derived";
+  import TestIdWrapper from "$lib/components/common/TestIdWrapper.svelte";
+
+  let snsNames: string;
+  $: snsNames =
+    $actionableProposalNotSupportedUniversesStore
+      .map((universe) => universe.title)
+      .join(", ");
+</script>
+
+<TestIdWrapper testId="actionable-proposals-not-supported-snses">
+  {#if snsNames !== ""}
+    <PageBanner>
+      <IconNotificationPage slot="image" />
+      <svelte:fragment slot="title"
+        >{replacePlaceholders(
+          $i18n.actionable_proposals_not_supported_snses.title,
+          {
+            $snsNames: snsNames,
+          }
+        )}</svelte:fragment
+      >
+      <p class="description" slot="description">
+        {replacePlaceholders(
+          $i18n.actionable_proposals_not_supported_snses.text,
+          {
+            $snsName: snsNames,
+          }
+        )}
+      </p>
+    </PageBanner>
+  {/if}
+</TestIdWrapper>
