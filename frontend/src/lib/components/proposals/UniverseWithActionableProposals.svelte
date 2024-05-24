17b0f41b4e17a2b52af0822257e8c10081ee91b1
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-05-24 18:05:51 +0200

    refactor: use UniversePageSummary

--- frontend/src/lib/components/proposals/UniverseWithActionableProposals.svelte
+++ frontend/src/lib/components/proposals/UniverseWithActionableProposals.svelte
@@ -1,19 +1,13 @@
 <script lang="ts">
   import type { Universe } from "$lib/types/universe";
-  import UniverseLogo from "$lib/components/universe/UniverseLogo.svelte";
   import { InfiniteScroll } from "@dfinity/gix-components";
+  import UniversePageSummary from "$lib/components/universe/UniversePageSummary.svelte";
 
   export let universe: Universe;
 </script>
 
 <div class="container" data-tid="universe-with-actionable-proposals-component">
-  <div class="summary" data-tid="projects-summary">
-    <h1 class="title">
-      <span class="title">{universe.title}</span>
-
-      <UniverseLogo {universe} framed />
-    </h1>
-  </div>
+  <UniversePageSummary {universe} />
 
   <InfiniteScroll layout="grid" disabled>
     <slot />
@@ -21,31 +15,7 @@
 </div>
 
 <style lang="scss">
-  @use "@dfinity/gix-components/dist/styles/mixins/media";
-  @use "@dfinity/gix-components/dist/styles/mixins/text";
-  @use "@dfinity/gix-components/dist/styles/mixins/fonts";
-
   .container {
     margin-bottom: var(--padding-4x);
-  }
-
-  .summary {
-    display: flex;
-    flex-direction: column;
-
-    margin: 0 0 var(--padding-3x);
-  }
-
-  .title {
-    display: inline-flex;
-  }
-
-  span {
-    @include fonts.h3;
-
-    max-width: calc(100% - var(--padding-6x));
-    @include text.truncate;
-
-    margin: 0;
   }
 </style>
