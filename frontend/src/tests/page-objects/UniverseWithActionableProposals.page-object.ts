adb149bc3a079adef64a862aae154e52ec3788da
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-05-24 16:54:25 +0200

    test: add UniverseWithActionableProposalsPo

---
+++ frontend/src/tests/page-objects/UniverseWithActionableProposals.page-object.ts
@@ -0,0 +1,32 @@
+import { ProposalCardPo } from "$tests/page-objects/ProposalCard.page-object";
+import { BasePageObject } from "$tests/page-objects/base.page-object";
+import type { PageObjectElement } from "$tests/types/page-object.types";
+import { assertNonNullish } from "$tests/utils/utils.test-utils";
+
+export class UniverseWithActionableProposalsPo extends BasePageObject {
+  static readonly TID = "universe-with-actionable-proposals-component";
+
+  static under(element: PageObjectElement): UniverseWithActionableProposalsPo {
+    return new UniverseWithActionableProposalsPo(
+      element.byTestId(UniverseWithActionableProposalsPo.TID)
+    );
+  }
+
+  static async allUnder(
+    element: PageObjectElement
+  ): Promise<UniverseWithActionableProposalsPo[]> {
+    return Array.from(
+      await element.allByTestId(UniverseWithActionableProposalsPo.TID)
+    ).map((el) => new UniverseWithActionableProposalsPo(el));
+  }
+
+  async getTitle(): Promise<string> {
+    return (
+      await assertNonNullish(this.root.querySelector("h1")).getText()
+    ).trim();
+  }
+
+  getProposalCardPos(): Promise<ProposalCardPo[]> {
+    return ProposalCardPo.allUnder(this.root);
+  }
+}
