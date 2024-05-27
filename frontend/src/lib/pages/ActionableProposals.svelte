9a1d0ec4fc7fa347d4d777328fd56d02a7e95ca3
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-05-27 18:36:08 +0200

    Display "no actionable proposals" banner

--- frontend/src/lib/pages/ActionableProposals.svelte
+++ frontend/src/lib/pages/ActionableProposals.svelte
@@ -2,16 +2,23 @@
   import TestIdWrapper from "$lib/components/common/TestIdWrapper.svelte";
   import ActionableSnses from "$lib/components/proposals/ActionableSnses.svelte";
   import ActionableNnsProposals from "$lib/components/proposals/ActionableNnsProposals.svelte";
-  import { actionableProposalsLoadedStore } from "$lib/derived/actionable-proposals.derived";
+  import {
+    actionableProposalsLoadedStore,
+    actionableProposalTotalCountStore,
+  } from "$lib/derived/actionable-proposals.derived";
   import LoadingActionableProposals from "$lib/components/proposals/LoadingActionableProposals.svelte";
+  import ActionableProposalsEmpty from "$lib/components/proposals/ActionableProposalsEmpty.svelte";
 </script>
 
 <TestIdWrapper testId="actionable-proposals-component">
   {#if $actionableProposalsLoadedStore}
-    <ActionableNnsProposals />
-    <ActionableSnses />
+    {#if $actionableProposalTotalCountStore > 0}
+      <ActionableNnsProposals />
+      <ActionableSnses />
+    {:else}
+      <ActionableProposalsEmpty />
+    {/if}
   {:else}
     <LoadingActionableProposals />
   {/if}
-  <!-- TODO: No proposals banner -->
 </TestIdWrapper>
