f2d406024609777d21ddf658800fa01d0afe786a
Max Strasinsky <98811342+mstrasinskis@users.noreply.github.com>
2024-05-27 15:08:43 +0200

    Display actionable Sns proposals (#4923)
    # Motivation
    
    The actionable proposals page should include all votable proposals.
    Here, we display Sns proposals that users can vote for.
    
    Note: should be merged after
    https://github.com/dfinity/nns-dapp/pull/4922
    
    # Changes
    
    - Add `ActionableSnsProposals` component.
    - Add `ActionableSnses` component.
    - Display actionable Sns proposals on actionable proposals page.
    
    # Tests
    
    - Add `ActionableSnsProposalsPo`.
    - Add `ActionableSnsesPo`.
    - Test that actionable Sns proposals have been rendered.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    Not yet.

--- frontend/src/tests/lib/pages/ActionableProposals.spec.ts
+++ frontend/src/tests/lib/pages/ActionableProposals.spec.ts
@@ -2,15 +2,24 @@ import { OWN_CANISTER_ID_TEXT } from "$lib/constants/canister-ids.constants";
 import { AppPath } from "$lib/constants/routes.constants";
 import ActionableProposals from "$lib/pages/ActionableProposals.svelte";
 import { actionableNnsProposalsStore } from "$lib/stores/actionable-nns-proposals.store";
+import { actionableSnsProposalsStore } from "$lib/stores/actionable-sns-proposals.store";
 import { page } from "$mocks/$app/stores";
 import { resetIdentity } from "$tests/mocks/auth.store.mock";
 import { mockProposalInfo } from "$tests/mocks/proposal.mock";
+import { principal } from "$tests/mocks/sns-projects.mock";
+import { createSnsProposal } from "$tests/mocks/sns-proposals.mock";
 import { ActionableProposalsPo } from "$tests/page-objects/ActionableProposals.page-object";
 import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
-import { resetSnsProjects } from "$tests/utils/sns.test-utils";
+import { resetSnsProjects, setSnsProjects } from "$tests/utils/sns.test-utils";
 import { render } from "$tests/utils/svelte.test-utils";
 import { runResolvedPromises } from "$tests/utils/timers.test-utils";
 import type { ProposalInfo } from "@dfinity/nns";
+import {
+  SnsProposalDecisionStatus,
+  SnsProposalRewardStatus,
+  SnsSwapLifecycle,
+  type SnsProposalData,
+} from "@dfinity/sns";
 
 describe("ActionableProposals", () => {
   const renderComponent = async () => {
@@ -24,6 +33,7 @@ describe("ActionableProposals", () => {
     resetIdentity();
     resetSnsProjects();
     actionableNnsProposalsStore.reset();
+    actionableSnsProposalsStore.resetForTesting();
 
     page.mock({
       data: { universe: OWN_CANISTER_ID_TEXT },
@@ -57,6 +67,128 @@ describe("ActionableProposals", () => {
       expect(proposalCardPos.length).toEqual(2);
       expect(await proposalCardPos[0].getProposalId()).toEqual("ID: 11");
       expect(await proposalCardPos[1].getProposalId()).toEqual("ID: 22");
+    });
+  });
+
+  describe("Actionable Sns proposals", () => {
+    const createProposal = (proposalId: bigint): SnsProposalData =>
+      createSnsProposal({
+        status: SnsProposalDecisionStatus.PROPOSAL_DECISION_STATUS_OPEN,
+        rewardStatus:
+          SnsProposalRewardStatus.PROPOSAL_REWARD_STATUS_ACCEPT_VOTES,
+        proposalId,
+      });
+    const proposal0 = createProposal(11n);
+    const proposal1 = createProposal(22n);
+    const proposal2 = createProposal(33n);
+    const principal0 = principal(0);
+    const principal1 = principal(1);
+    const principal2 = principal(2);
+    const principal3 = principal(3);
+
+    beforeEach(() => {
+      setSnsProjects([
+        {
+          lifecycle: SnsSwapLifecycle.Committed,
+          projectName: "Sns Project 0",
+          rootCanisterId: principal0,
+        },
+        {
+          lifecycle: SnsSwapLifecycle.Committed,
+          projectName: "Sns Project 1",
+          rootCanisterId: principal1,
+        },
+        {
+          lifecycle: SnsSwapLifecycle.Committed,
+          projectName: "Sns Project 2",
+          rootCanisterId: principal2,
+        },
+        {
+          lifecycle: SnsSwapLifecycle.Committed,
+          projectName: "Sns Project 3",
+          rootCanisterId: principal3,
+        },
+      ]);
+    });
+
+    it("should render actionable Sns proposals", async () => {
+      const po = await renderComponent();
+
+      expect(
+        await po.getActionableSnses().getActionableSnsProposalsPos()
+      ).toHaveLength(0);
+
+      actionableSnsProposalsStore.set({
+        rootCanisterId: principal0,
+        proposals: [proposal0],
+        includeBallotsByCaller: true,
+      });
+      actionableSnsProposalsStore.set({
+        rootCanisterId: principal1,
+        proposals: [proposal1, proposal2],
+        includeBallotsByCaller: true,
+      });
+
+      await runResolvedPromises();
+      const snsProposalsPos = await po
+        .getActionableSnses()
+        .getActionableSnsProposalsPos();
+      expect(snsProposalsPos).toHaveLength(2);
+      expect(
+        await snsProposalsPos[0]
+          .getUniverseWithActionableProposalsPo()
+          .getTitle()
+      ).toEqual("Sns Project 0");
+      const proposalCardPos0 = await snsProposalsPos[0].getProposalCardPos();
+      expect(proposalCardPos0.length).toEqual(1);
+      expect(await proposalCardPos0[0].getProposalId()).toEqual("ID: 11");
+
+      expect(
+        await snsProposalsPos[1]
+          .getUniverseWithActionableProposalsPo()
+          .getTitle()
+      ).toEqual("Sns Project 1");
+      const proposalCardPos1 = await snsProposalsPos[1].getProposalCardPos();
+      expect(proposalCardPos1.length).toEqual(2);
+      expect(await proposalCardPos1[0].getProposalId()).toEqual("ID: 22");
+      expect(await proposalCardPos1[1].getProposalId()).toEqual("ID: 33");
+    });
+
+    it("should ignore snses w/o ballot or actionable proposals", async () => {
+      const po = await renderComponent();
+
+      expect(
+        await po.getActionableSnses().getActionableSnsProposalsPos()
+      ).toHaveLength(0);
+
+      actionableSnsProposalsStore.set({
+        rootCanisterId: principal0,
+        // no proposals
+        proposals: [],
+        includeBallotsByCaller: true,
+      });
+      actionableSnsProposalsStore.set({
+        rootCanisterId: principal1,
+        proposals: [proposal0],
+        // no ballots
+        includeBallotsByCaller: false,
+      });
+
+      await runResolvedPromises();
+      expect(
+        await po.getActionableSnses().getActionableSnsProposalsPos()
+      ).toHaveLength(0);
+
+      actionableSnsProposalsStore.set({
+        rootCanisterId: principal2,
+        proposals: [proposal1, proposal2],
+        includeBallotsByCaller: true,
+      });
+
+      await runResolvedPromises();
+      expect(
+        await po.getActionableSnses().getActionableSnsProposalsPos()
+      ).toHaveLength(1);
     });
   });
 });
--- frontend/src/tests/page-objects/ActionableProposals.page-object.ts
+++ frontend/src/tests/page-objects/ActionableProposals.page-object.ts
@@ -1,4 +1,5 @@
 import { ActionableNnsProposalsPo } from "$tests/page-objects/ActionableNnsProposals.page-object";
+import { ActionableSnsesPo } from "$tests/page-objects/ActionableSnses.page-object";
 import { BasePageObject } from "$tests/page-objects/base.page-object";
 import type { PageObjectElement } from "$tests/types/page-object.types";
 
@@ -13,6 +14,10 @@ export class ActionableProposalsPo extends BasePageObject {
 
   getActionableNnsProposalsPo(): ActionableNnsProposalsPo {
     return ActionableNnsProposalsPo.under(this.root);
+  }
+
+  getActionableSnses(): ActionableSnsesPo {
+    return ActionableSnsesPo.under(this.root);
   }
 
   hasActionableNnsProposals(): Promise<boolean> {
