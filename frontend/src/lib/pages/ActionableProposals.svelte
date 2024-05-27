4d7acf436b748d30e78856f333501c2ebdb0b877
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-05-27 16:06:39 +0200

    Display LoadingActionableProposals while loading

--- frontend/src/lib/pages/ActionableProposals.svelte
+++ frontend/src/lib/pages/ActionableProposals.svelte
@@ -2,11 +2,16 @@
   import TestIdWrapper from "$lib/components/common/TestIdWrapper.svelte";
   import ActionableSnses from "$lib/components/proposals/ActionableSnses.svelte";
   import ActionableNnsProposals from "$lib/components/proposals/ActionableNnsProposals.svelte";
+  import { actionableProposalsLoadedStore } from "$lib/derived/actionable-proposals.derived";
+  import LoadingActionableProposals from "$lib/components/proposals/LoadingActionableProposals.svelte";
 </script>
 
 <TestIdWrapper testId="actionable-proposals-component">
-  <!-- TODO: Loading state -->
-  <ActionableNnsProposals />
-  <ActionableSnses />
+  {#if $actionableProposalsLoadedStore}
+    <ActionableNnsProposals />
+    <ActionableSnses />
+  {:else}
+    <LoadingActionableProposals />
+  {/if}
   <!-- TODO: No proposals banner -->
 </TestIdWrapper>
