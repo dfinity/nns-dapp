5c459bb2b578a6c8449c564518576d13dedd3f87
Max Strasinsky <98811342+mstrasinskis@users.noreply.github.com>
2024-05-27 13:44:41 +0200

    Display actionable proposals nns (#4922)
    # Motivation
    
    The actionable proposals page should include all votable proposals.
    Here, we display Nns proposals that users can vote for.
    
    # Changes
    
    - Add `ActionableNnsProposals` component.
    - Display actionable Nns proposals.
    
    # Tests
    
    - Add `ActionableNnsProposalsPo`.
    - Test that actionable Nns proposals have been rendered.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    Not yet.

--- frontend/src/lib/pages/ActionableProposals.svelte
+++ frontend/src/lib/pages/ActionableProposals.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
   import TestIdWrapper from "$lib/components/common/TestIdWrapper.svelte";
+  import ActionableNnsProposals from "$lib/components/proposals/ActionableNnsProposals.svelte";
 </script>
 
 <TestIdWrapper testId="actionable-proposals-component">
-  <h1>Under construction</h1>
+  <!-- TODO: Loading state -->
+  <ActionableNnsProposals />
+  <!-- TODO: Actionable sns proposals -->
+  <!-- TODO: No proposals banner -->
 </TestIdWrapper>
