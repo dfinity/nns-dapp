dde0433e27b193ce007d2f0ee1bc28655387d64d
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-05-27 16:06:24 +0200

    Add LoadingActionableProposals component

---
+++ frontend/src/lib/components/proposals/LoadingActionableProposals.svelte
@@ -0,0 +1,24 @@
+<script>
+  import SkeletonCard from "../ui/SkeletonCard.svelte";
+  import { SkeletonText } from "@dfinity/gix-components";
+  import TestIdWrapper from "$lib/components/common/TestIdWrapper.svelte";
+</script>
+
+<TestIdWrapper testId="loading-actionable-proposals">
+  <div class="headline">
+    <SkeletonText />
+  </div>
+  <div class="card-grid" data-tid="proposals-loading">
+    <SkeletonCard />
+    <SkeletonCard />
+    <SkeletonCard />
+  </div>
+</TestIdWrapper>
+
+<style lang="scss">
+  .headline {
+    margin-bottom: var(--padding-3x);
+    width: 210px;
+    height: var(--padding-3x);
+  }
+</style>
