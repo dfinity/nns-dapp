e8f6e3fa2fb40f2c18574b5b2884dc066c49d503
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-05-28 08:20:56 +0200

    Display ActionableProposalsNotSupportedSnses banner

--- frontend/src/lib/pages/ActionableProposals.svelte
+++ frontend/src/lib/pages/ActionableProposals.svelte
@@ -3,11 +3,13 @@
   import ActionableSnses from "$lib/components/proposals/ActionableSnses.svelte";
   import ActionableNnsProposals from "$lib/components/proposals/ActionableNnsProposals.svelte";
   import {
+    actionableProposalNotSupportedUniversesStore,
     actionableProposalsLoadedStore,
     actionableProposalTotalCountStore,
   } from "$lib/derived/actionable-proposals.derived";
   import LoadingActionableProposals from "$lib/components/proposals/LoadingActionableProposals.svelte";
   import ActionableProposalsEmpty from "$lib/components/proposals/ActionableProposalsEmpty.svelte";
+  import ActionableProposalsNotSupportedSnses from "$lib/components/proposals/ActionableProposalsNotSupportedSnses.svelte";
 </script>
 
 <TestIdWrapper testId="actionable-proposals-component">
@@ -21,4 +23,15 @@
   {:else}
     <LoadingActionableProposals />
   {/if}
+  {#if $actionableProposalNotSupportedUniversesStore.length > 0}
+    <div class="unsupported-snses">
+      <ActionableProposalsNotSupportedSnses />
+    </div>
+  {/if}
 </TestIdWrapper>
+
+<style lang="scss">
+  .unsupported-snses {
+    margin-top: var(--padding-3x);
+  }
+</style>
