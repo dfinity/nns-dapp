c782935355581372300cc1fcb6d63c990cf5c8e6
David de Kloet <david.de.kloet@dfinity.org>
2024-05-28 10:10:49 +0200

    NON_SNS_NAMES

--- frontend/src/tests/page-objects/TokensTable.page-object.ts
+++ frontend/src/tests/page-objects/TokensTable.page-object.ts
@@ -16,6 +16,7 @@ export class TokensTablePo extends BasePageObject {
     "ckTESTBTC",
     "ckETH",
     "ckETHSepolia",
+    "ckUSDC",
   ];
 
   static under(element: PageObjectElement): TokensTablePo {
