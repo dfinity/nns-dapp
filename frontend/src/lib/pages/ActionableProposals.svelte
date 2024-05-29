6f753f4d617c643f102d66d4b6ef5cda4ea65064
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-05-29 17:35:09 +0200

    Reset ActionableProposals

--- frontend/src/lib/pages/ActionableProposals.svelte
+++ frontend/src/lib/pages/ActionableProposals.svelte
@@ -3,13 +3,11 @@
   import ActionableSnses from "$lib/components/proposals/ActionableSnses.svelte";
   import ActionableNnsProposals from "$lib/components/proposals/ActionableNnsProposals.svelte";
   import {
-    actionableProposalNotSupportedUniversesStore,
     actionableProposalsLoadedStore,
     actionableProposalTotalCountStore,
   } from "$lib/derived/actionable-proposals.derived";
   import LoadingActionableProposals from "$lib/components/proposals/LoadingActionableProposals.svelte";
   import ActionableProposalsEmpty from "$lib/components/proposals/ActionableProposalsEmpty.svelte";
-  import ActionableProposalsNotSupportedSnses from "$lib/components/proposals/ActionableProposalsNotSupportedSnses.svelte";
 </script>
 
 <TestIdWrapper testId="actionable-proposals-component">
@@ -23,15 +21,4 @@
   {:else}
     <LoadingActionableProposals />
   {/if}
-  {#if $actionableProposalNotSupportedUniversesStore.length > 0}
-    <div class="unsupported-snses">
-      <ActionableProposalsNotSupportedSnses />
-    </div>
-  {/if}
 </TestIdWrapper>
-
-<style lang="scss">
-  .unsupported-snses {
-    margin-top: var(--padding-3x);
-  }
-</style>
