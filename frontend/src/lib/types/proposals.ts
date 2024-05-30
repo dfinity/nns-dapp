2327bee6c9e8d721cc2d42e7d4a8136e8abd8f6c
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-05-30 17:03:44 +0200

    update types

--- frontend/src/lib/types/proposals.ts
+++ frontend/src/lib/types/proposals.ts
@@ -29,6 +29,6 @@ export type BasisPoints = bigint;
 
 // An entry for proposal navigation on the proposal detail page.
 export interface ProposalsNavigationId {
-  proposalId: ProposalId;
-  universe: UniverseCanisterIdText;
+  proposalId: string;
+  universe: string;
 }
