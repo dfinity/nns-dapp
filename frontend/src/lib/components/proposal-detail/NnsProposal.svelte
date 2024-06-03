109cbdda8244e60e36a7fdf43b7fda0126d235b1
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-06-03 15:13:50 +0200

    Cleanup imports

--- frontend/src/lib/components/proposal-detail/NnsProposal.svelte
+++ frontend/src/lib/components/proposal-detail/NnsProposal.svelte
@@ -24,7 +24,6 @@
   import { nonNullish } from "@dfinity/utils";
   import { actionableNnsProposalsStore } from "$lib/stores/actionable-nns-proposals.store";
   import { actionableProposalsActiveStore } from "$lib/derived/actionable-proposals.derived";
-  import { pageStore } from "$lib/derived/page.derived";
 
   const { store } = getContext<SelectedProposalContext>(
     SELECTED_PROPOSAL_CONTEXT_KEY
