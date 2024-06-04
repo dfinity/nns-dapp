29f3babef787cd7d4e3212cd9eb652cedfc80dd2
David de Kloet <david.de.kloet@dfinity.org>
2024-06-04 09:46:20 +0200

    NNS1-3092: Render neuron tags in table

--- frontend/src/lib/components/neurons/NeuronsTable/NeuronIdCell.svelte
+++ frontend/src/lib/components/neurons/NeuronsTable/NeuronIdCell.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
   import type { TableNeuron } from "$lib/types/neurons-table";
   import IdentifierHash from "$lib/components/ui/IdentifierHash.svelte";
+  import { Tag } from "@dfinity/gix-components";
 
   export let rowData: TableNeuron;
 </script>
 
 <div data-tid="neuron-id-cell-component">
   <IdentifierHash identifier={rowData.neuronId} />
+  <div class="tags">
+    {#each rowData.tags as tag}
+      <Tag testId="neuron-tag">{tag}</Tag>
+    {/each}
+  </div>
 </div>
+
+<style lang="scss">
+  .tags {
+    display: flex;
+    gap: var(--padding);
+  }
+</style>
