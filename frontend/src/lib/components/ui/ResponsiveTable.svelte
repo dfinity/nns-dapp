9c204f1eb63aeb30c70b13e5596259531a638bf8
David de Kloet <david.de.kloet@dfinity.org>
2024-06-05 15:40:07 +0200

    NNS1-3092: Add horizontal scrollbar to neurons table

--- frontend/src/lib/components/ui/ResponsiveTable.svelte
+++ frontend/src/lib/components/ui/ResponsiveTable.svelte
@@ -89,7 +89,7 @@
 
     border-radius: var(--border-radius);
     // Otherwise the non-rounded corners of the header and last row would be visible.
-    overflow: hidden;
+    overflow-y: auto;
 
     @include media.min-width(medium) {
       display: grid;
