f66e161ce78c9f90d72116c8ae3a27707d9218f9
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-06-06 12:13:46 +0200

    New actionableProposalsNavigationIdsStore

--- frontend/src/lib/derived/actionable-proposals.derived.ts
+++ frontend/src/lib/derived/actionable-proposals.derived.ts
@@ -7,11 +7,12 @@ import { snsProjectsCommittedStore } from "$lib/derived/sns/sns-projects.derived
 import { actionableNnsProposalsStore } from "$lib/stores/actionable-nns-proposals.store";
 import { actionableProposalsSegmentStore } from "$lib/stores/actionable-proposals-segment.store";
 import { actionableSnsProposalsStore } from "$lib/stores/actionable-sns-proposals.store";
+import type { ProposalsNavigationId } from "$lib/types/proposals";
 import type { Universe } from "$lib/types/universe";
 import { isSelectedPath } from "$lib/utils/navigation.utils";
 import { mapEntries } from "$lib/utils/utils";
 import type { SnsProposalData } from "@dfinity/sns";
-import { nonNullish } from "@dfinity/utils";
+import { fromDefinedNullable, nonNullish } from "@dfinity/utils";
 import { derived, type Readable } from "svelte/store";
 
 export interface ActionableProposalCountData {
@@ -126,4 +127,30 @@ export const actionableProposalsLoadedStore: Readable<boolean> = derived(
     // It is expected to have at least one SNS to cover when the projects have not yet been loaded.
     committedSnsProjects.length > 0 &&
     committedSnsProjects.length === Object.keys(snsProposals).length
+);
+
+// Generate list of ProposalsNavigationId using universes to provide correct order
+// of proposals in the UI.
+export const actionableProposalsNavigationIdsStore: Readable<
+  Array<ProposalsNavigationId>
+> = derived(
+  [
+    selectableUniversesStore,
+    actionableNnsProposalsStore,
+    actionableSnsProposalsStore,
+  ],
+  ([universes, nnsProposals, actionableSnsProposals]) =>
+    universes
+      .map(({ canisterId }) =>
+        canisterId === OWN_CANISTER_ID_TEXT
+          ? (nnsProposals.proposals ?? []).map(({ id }) => ({
+              universe: OWN_CANISTER_ID_TEXT,
+              proposalId: id as bigint,
+            }))
+          : (actionableSnsProposals[canisterId]?.proposals ?? []).map((aa) => ({
+              universe: canisterId,
+              proposalId: fromDefinedNullable(aa.id).id,
+            }))
+      )
+      .flatMap((ids) => (nonNullish(ids) ? ids : []))
 );
