df2add3876ac2954d51a4bac9c7011c58f87f78f
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-06-10 21:53:19 +0200

    Use UniversePageSummary instead of custom styled title and logo

--- frontend/src/lib/components/summary/Summary.svelte
+++ frontend/src/lib/components/summary/Summary.svelte
@@ -1,38 +1,21 @@
 <script lang="ts">
   import { selectedUniverseStore } from "$lib/derived/selected-universe.derived";
-  import UniverseLogo from "$lib/components/universe/UniverseLogo.svelte";
+  import UniversePageSummary from "$lib/components/universe/UniversePageSummary.svelte";
 </script>
 
 <div class="summary" data-tid="projects-summary">
   <h1 class="title">
-    <span>{$selectedUniverseStore.title}</span>
-
-    <UniverseLogo universe={$selectedUniverseStore} framed />
+    <UniversePageSummary universe={$selectedUniverseStore} />
   </h1>
 </div>
 
 <style lang="scss">
   @use "@dfinity/gix-components/dist/styles/mixins/media";
-  @use "@dfinity/gix-components/dist/styles/mixins/text";
-  @use "@dfinity/gix-components/dist/styles/mixins/fonts";
 
   .summary {
     display: flex;
     flex-direction: column;
 
     margin: 0 0 var(--padding-3x);
-  }
-
-  .title {
-    display: inline-flex;
-  }
-
-  span {
-    @include fonts.h3;
-
-    max-width: calc(100% - var(--padding-6x));
-    @include text.truncate;
-
-    margin: 0;
   }
 </style>
