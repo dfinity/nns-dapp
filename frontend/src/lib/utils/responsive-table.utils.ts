443bd47b152589f8435c35b3d105514e0f2ebaad
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2024-06-25 18:01:44 +0200

    NNS1-3135: Add util for table resorting logic (#5107)
    # Motivation
    
    When we make the neurons table sortable we want to use the following
    logic:
    1. If you click on a column header, the rows should be sorted by that
    column.
    2. If you click again on the same column header, the rows should be
    sorted by the reverse of that column.
    3. When changing sort order, ties in the chosen column should be broken
    based on the previous order.
    
    So if you sort by stake and then by dissolve delay, neurons with the
    same dissolve delay should be sorted by stake because that was the
    previous order.
    
    This PR does not make any UI changes but adds the utility that
    implements the above logic.
    
    # Changes
    
    1. Add optional `reversed` field to the elements of
    `ResponsiveTableOrder`.
    2. Add `selectPrimaryOrder` function which creates a new order following
    the above logic.
    3. Support the `reversed` field in `sortTableData`.
    
    # Tests
    
    1. Unit tests added for `selectPrimaryOrder`.
    2. Unit test extended for `sortTableData` with `reversed: true` items.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    not necessary

--- frontend/src/lib/utils/responsive-table.utils.ts
+++ frontend/src/lib/utils/responsive-table.utils.ts
@@ -37,14 +37,40 @@ export const createDescendingComparator = <RowDataType, Comparable>(
   };
 };
 
+const negate = <T>(comparator: Comparator<T>): Comparator<T> => {
+  // `|| 0` is to avoid the value `-0`.
+  return (a, b) => -comparator(a, b) || 0;
+};
+
 // Same as createDescendingComparator but returns a comparator for sorting in
 // ascending order.
 export const createAscendingComparator = <RowDataType, Comparable>(
   getter: (rowData: RowDataType) => Comparable
-): Comparator<RowDataType> => {
-  const descendingComparator = createDescendingComparator(getter);
-  // `|| 0` is to avoid the value `-0`.
-  return (a, b) => -descendingComparator(a, b) || 0;
+): Comparator<RowDataType> => negate(createDescendingComparator(getter));
+
+// Creates the new table order resulting from clicking on a column header.  This
+// means reversing the order if the selected column is already the primary
+// order, or making the selected column the primary order if it is not.
+export const selectPrimaryOrder = ({
+  order,
+  selectedColumnId,
+}: {
+  order: ResponsiveTableOrder;
+  selectedColumnId: string;
+}): ResponsiveTableOrder => {
+  if (order[0].columnId === selectedColumnId) {
+    return [
+      {
+        columnId: order[0].columnId,
+        ...(order[0].reversed ? {} : { reversed: true }),
+      },
+      ...order.slice(1),
+    ];
+  }
+  return [
+    { columnId: selectedColumnId },
+    ...order.filter((order) => order.columnId !== selectedColumnId),
+  ];
 };
 
 // Sorts rows based on a provided custom ordering.
@@ -60,8 +86,9 @@ export const sortTableData = <RowDataType extends ResponsiveTableRowData>({
   const comparatorByColumnId = Object.fromEntries(
     columns.filter((c) => c.id && c.comparator).map((c) => [c.id, c.comparator])
   );
-  const comparators = order.map(
-    ({ columnId }) => comparatorByColumnId[columnId]
-  );
+  const comparators = order.map(({ columnId, reversed }) => {
+    const comparator = comparatorByColumnId[columnId];
+    return reversed ? negate(comparator) : comparator;
+  });
   return [...tableData].sort(mergeComparators(comparators));
 };
