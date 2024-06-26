78c5025fe30f0ae979beeb4bbb9102d86f0266a7
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-06-26 13:28:20 +0200

    Rename actionableProposalIndicationVisibleStore

--- frontend/src/lib/derived/actionable-proposals.derived.ts
+++ frontend/src/lib/derived/actionable-proposals.derived.ts
@@ -25,7 +25,7 @@ export interface ActionableProposalCountData {
 
 /** Returns true when the indication needs to be shown */
 // TODO(max): rename into actionableProposalIndicationVisibleStore (related to the visibility on neurons page bug)
-export const actionableProposalIndicationEnabledStore: Readable<boolean> =
+export const actionableProposalIndicationVisibleStore: Readable<boolean> =
   derived(
     [pageStore, authSignedInStore],
     ([{ path: currentPath }, isSignedIn]) =>
