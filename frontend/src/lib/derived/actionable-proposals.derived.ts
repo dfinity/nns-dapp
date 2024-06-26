d205aaeef7320ce4a6c300c4ad797a21ea482f1b
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-06-26 13:29:52 +0200

    Remove outdated comment

--- frontend/src/lib/derived/actionable-proposals.derived.ts
+++ frontend/src/lib/derived/actionable-proposals.derived.ts
@@ -24,7 +24,6 @@ export interface ActionableProposalCountData {
 }
 
 /** Returns true when the indication needs to be shown */
-// TODO(max): rename into actionableProposalIndicationVisibleStore (related to the visibility on neurons page bug)
 export const actionableProposalIndicationVisibleStore: Readable<boolean> =
   derived(
     [pageStore, authSignedInStore],
