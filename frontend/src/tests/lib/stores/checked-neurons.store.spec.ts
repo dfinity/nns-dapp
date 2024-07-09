2907aa5c0471f697086d01111d1cb834d167e9ba
David de Kloet <david.de.kloet@dfinity.org>
2024-07-09 11:27:33 +0200

    Refresh SNS neuron from details page if needed

---
+++ frontend/src/tests/lib/stores/checked-neurons.store.spec.ts
@@ -0,0 +1,52 @@
+import { checkedNeuronSubaccountsStore } from "$lib/stores/checked-neurons.store";
+import { get } from "svelte/store";
+
+describe("checked-neurons.store", () => {
+  describe("checkedNeuronSubaccountsStore", () => {
+    beforeEach(() => {
+      checkedNeuronSubaccountsStore.reset();
+    });
+
+    it("should initially be empty", () => {
+      expect(get(checkedNeuronSubaccountsStore)).toEqual({});
+    });
+
+    it("should add a subaccount", () => {
+      expect(get(checkedNeuronSubaccountsStore)).toEqual({});
+      const universeId = "abc-efg-cai";
+      const subaccountHex = "12ab90";
+      checkedNeuronSubaccountsStore.addSubaccount({
+        universeId,
+        subaccountHex,
+      });
+      expect(get(checkedNeuronSubaccountsStore)).toEqual({
+        [universeId]: {
+          [subaccountHex]: true,
+        },
+      });
+    });
+
+    it("should return whether a subaccount was added", () => {
+      const universeId = "abc-efg-cai";
+      const subaccountHex = "12ab90";
+      expect(
+        checkedNeuronSubaccountsStore.addSubaccount({
+          universeId,
+          subaccountHex,
+        })
+      ).toBe(true);
+      expect(
+        checkedNeuronSubaccountsStore.addSubaccount({
+          universeId,
+          subaccountHex,
+        })
+      ).toBe(false);
+      expect(
+        checkedNeuronSubaccountsStore.addSubaccount({
+          universeId,
+          subaccountHex,
+        })
+      ).toBe(false);
+    });
+  });
+});
