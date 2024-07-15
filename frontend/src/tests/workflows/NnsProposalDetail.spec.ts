aca469a830a05712b47d5bd625ed5a5fd3d8bc9a
David de Kloet <david.de.kloet@dfinity.org>
2024-07-15 17:54:24 +0200

    fix src/tests/workflows/NnsProposalDetail.spec.ts

--- frontend/src/tests/workflows/NnsProposalDetail.spec.ts
+++ frontend/src/tests/workflows/NnsProposalDetail.spec.ts
@@ -113,6 +113,7 @@ describe("Proposal detail page when not logged in user", () => {
         expect(governanceApi.queryNeurons).toHaveBeenCalledWith({
           identity: mockIdentity,
           certified: true,
+          includeEmptyNeurons: false,
         })
       );
 
@@ -160,11 +161,13 @@ describe("Proposal detail page when not logged in user", () => {
         expect(governanceApi.queryNeurons).toHaveBeenCalledWith({
           identity: mockIdentity,
           certified: true,
+          includeEmptyNeurons: false,
         })
       );
       expect(governanceApi.queryNeurons).toHaveBeenCalledWith({
         identity: mockIdentity,
         certified: false,
+        includeEmptyNeurons: false,
       });
     });
   });
