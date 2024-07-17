099ec7f2893c2e7ecfc129979dc84f8c4a9810c2
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2024-07-17 10:17:54 +0200

    NNS1-3094: Add stake field to TableProject (#5206)
    # Motivation
    
    We want to show total stake in the staking projects table.
    In this PR we just add the data but don't show it in the UI yet.
    
    # Changes
    
    1. Add `stake` field to `TableProject` type.
    2. Calculate the stake for each project in `getTableProjects`.
    
    # Tests
    
    1. Unit tests updated.
    2. Tested manually in a branch including UI changes.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    not yet

--- frontend/src/lib/types/staking.ts
+++ frontend/src/lib/types/staking.ts
@@ -1,4 +1,5 @@
 import type { ResponsiveTableColumn } from "$lib/types/responsive-table";
+import type { TokenAmountV2 } from "@dfinity/utils";
 
 export type TableProject = {
   rowHref?: string;
@@ -6,6 +7,7 @@ export type TableProject = {
   title: string;
   logo: string;
   neuronCount: number | undefined;
+  stake: TokenAmountV2 | undefined;
 };
 
 export type ProjectsTableColumn = ResponsiveTableColumn<TableProject>;
--- frontend/src/lib/utils/staking.utils.ts
+++ frontend/src/lib/utils/staking.utils.ts
@@ -2,10 +2,88 @@ import { OWN_CANISTER_ID_TEXT } from "$lib/constants/canister-ids.constants";
 import type { TableProject } from "$lib/types/staking";
 import type { Universe } from "$lib/types/universe";
 import { buildNeuronsUrl } from "$lib/utils/navigation.utils";
-import { hasValidStake as nnsHasValidStake } from "$lib/utils/neuron.utils";
-import { hasValidStake as snsHasValidStake } from "$lib/utils/sns-neuron.utils";
+import {
+  neuronStake,
+  hasValidStake as nnsHasValidStake,
+} from "$lib/utils/neuron.utils";
+import {
+  getSnsNeuronStake,
+  hasValidStake as snsHasValidStake,
+} from "$lib/utils/sns-neuron.utils";
 import type { NeuronInfo } from "@dfinity/nns";
 import type { SnsNeuron } from "@dfinity/sns";
+import { ICPToken, TokenAmountV2, isNullish, type Token } from "@dfinity/utils";
+
+const getNnsNeuronCountAndStake = (
+  nnsNeurons: NeuronInfo[] | undefined
+): {
+  neuronCount: number | undefined;
+  stake: bigint | undefined;
+  token: Token;
+} => {
+  const neurons = nnsNeurons?.filter(nnsHasValidStake);
+  const stake = neurons?.reduce((acc, neuron) => acc + neuronStake(neuron), 0n);
+  return {
+    neuronCount: neurons?.length,
+    stake,
+    token: ICPToken,
+  };
+};
+
+const getSnsNeuronCountAndStake = ({
+  universe,
+  snsNeurons,
+}: {
+  universe: Universe;
+  snsNeurons: { [rootCanisterId: string]: { neurons: SnsNeuron[] } };
+}): {
+  neuronCount: number | undefined;
+  stake: bigint | undefined;
+  token: Token | undefined;
+} => {
+  const neurons =
+    snsNeurons[universe.canisterId]?.neurons.filter(snsHasValidStake);
+  const stake = neurons?.reduce(
+    (acc, neuron) => acc + getSnsNeuronStake(neuron),
+    0n
+  );
+  const token = universe.summary?.token;
+  return {
+    neuronCount: neurons?.length,
+    stake,
+    token,
+  };
+};
+
+const getNeuronCountAndStake = ({
+  isSignedIn,
+  universe,
+  nnsNeurons,
+  snsNeurons,
+}: {
+  isSignedIn: boolean;
+  universe: Universe;
+  nnsNeurons: NeuronInfo[] | undefined;
+  snsNeurons: { [rootCanisterId: string]: { neurons: SnsNeuron[] } };
+}): { neuronCount: number | undefined; stake: TokenAmountV2 | undefined } => {
+  if (!isSignedIn) {
+    return { neuronCount: undefined, stake: undefined };
+  }
+  const { neuronCount, stake, token } =
+    universe.canisterId === OWN_CANISTER_ID_TEXT
+      ? getNnsNeuronCountAndStake(nnsNeurons)
+      : getSnsNeuronCountAndStake({ snsNeurons, universe });
+  return {
+    neuronCount,
+    stake:
+      isNullish(stake) || isNullish(token)
+        ? undefined
+        : TokenAmountV2.fromUlps({
+            amount: stake,
+            token,
+          }),
+  };
+};
 
 export const getTableProjects = ({
   universes,
@@ -19,12 +97,12 @@ export const getTableProjects = ({
   snsNeurons: { [rootCanisterId: string]: { neurons: SnsNeuron[] } };
 }): TableProject[] => {
   return universes.map((universe) => {
-    const neuronCount = !isSignedIn
-      ? undefined
-      : universe.canisterId === OWN_CANISTER_ID_TEXT
-      ? nnsNeurons?.filter(nnsHasValidStake).length
-      : snsNeurons[universe.canisterId]?.neurons.filter(snsHasValidStake)
-          .length;
+    const { neuronCount, stake } = getNeuronCountAndStake({
+      isSignedIn,
+      universe,
+      nnsNeurons,
+      snsNeurons,
+    });
     const rowHref =
       (neuronCount ?? 0) > 0
         ? buildNeuronsUrl({ universe: universe.canisterId })
@@ -35,6 +113,7 @@ export const getTableProjects = ({
       title: universe.title,
       logo: universe.logo,
       neuronCount,
+      stake,
     };
   });
 };
