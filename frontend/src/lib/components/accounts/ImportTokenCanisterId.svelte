22d7ddef22367b3544b0a16d8fde4e9c383ba669
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-07-18 13:42:02 +0200

    Add ImportTokenCanisterId component

---
+++ frontend/src/lib/components/accounts/ImportTokenCanisterId.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { Copy } from "@dfinity/gix-components";
+  import { nonNullish } from "@dfinity/utils";
+  import LinkIcon from "$lib/components/common/LinkIcon.svelte";
+
+  export let label: string;
+  export let canisterId: string | undefined = undefined;
+  export let canisterLinkHref: string | undefined = undefined;
+  export let canisterIdFallback: string | undefined = undefined;
+</script>
+
+<div data-tid="import-token-canister-id-component">
+  <span>{label}</span>
+  <div class="canister-id">
+    <span class="value description" data-tid="canister-id">{canisterId}</span>
+    {#if nonNullish(canisterId) && nonNullish(canisterLinkHref)}
+      <Copy value={canisterId ?? ""} />
+      <LinkIcon href={canisterLinkHref} />
+    {:else if nonNullish(canisterIdFallback)}
+      <span class="fallback description" data-tid="canister-id-fallback"
+        >{canisterIdFallback}</span
+      >
+    {/if}
+  </div>
+</div>
+
+<style lang="scss">
+  .canister-id {
+    display: flex;
+    align-items: center;
+    color: var(--primary);
+  }
+
+  .value {
+    margin-right: var(--padding-0_5x);
+  }
+</style>
