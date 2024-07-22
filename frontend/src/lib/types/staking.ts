48f4aabac741b95750193b6abe8c7641b9d59e33
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2024-07-22 15:53:56 +0200

    NNS1-3094: Show token symbol for unavailable stake (#5229)
    # Motivation
    
    In the staking projects table, we show total stake per project.
    When the user isn't signed in, or neurons aren't loaded yet, we show
    `-/-` for the total stake of a project.
    We want to include the token symbol in this, as we do in the tokens
    table.
    
    # Changes
    
    1. Use `UnavailableTokenAmount` instead of `undefined` to indicate that
    a stake amount isn't available for a project.
    2. Rely on `AmountDisplay` to render the unavailable token amount. (Zero
    is still not rendered at all.)
    
    # Tests
    
    1. Unit tests updated.
    2. Tested manually on
    [DevEnv](https://qsgjb-riaaa-aaaaa-aaaga-cai.dskloet-ingress.devenv.dfinity.network/staking/)
    (with feature flag ENABLE_PROJECTS_TABLE)
    
    Before:
    <img width="717" alt="image"
    src="https://github.com/user-attachments/assets/c07cf4e8-2cf4-4338-b471-6c1edebe3f37">
    
    After:
    <img width="711" alt="image"
    src="https://github.com/user-attachments/assets/69fcf46e-6799-4841-a200-bbd8ba2873b1">
    
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    not yet

--- frontend/src/lib/types/staking.ts
+++ frontend/src/lib/types/staking.ts
@@ -1,4 +1,5 @@
 import type { ResponsiveTableColumn } from "$lib/types/responsive-table";
+import type { UnavailableTokenAmount } from "$lib/utils/token.utils";
 import type { TokenAmountV2 } from "@dfinity/utils";
 
 export type TableProject = {
@@ -7,7 +8,7 @@ export type TableProject = {
   title: string;
   logo: string;
   neuronCount: number | undefined;
-  stake: TokenAmountV2 | undefined;
+  stake: TokenAmountV2 | UnavailableTokenAmount;
 };
 
 export type ProjectsTableColumn = ResponsiveTableColumn<TableProject>;
