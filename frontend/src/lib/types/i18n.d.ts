66ddafd39f47c1dbf4e5c52c190418bbaf799c4a
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-07-30 13:36:56 +0200

    Revert label changes

--- frontend/src/lib/types/i18n.d.ts
+++ frontend/src/lib/types/i18n.d.ts
@@ -1102,10 +1102,6 @@ interface I18nTokens {
   remove_imported_token_success: string;
 }
 
-interface I18nImport_token {
-  warning: string;
-}
-
 interface I18nNeuron_state {
   Unspecified: string;
   Locked: string;
@@ -1390,7 +1386,6 @@ interface I18n {
   settings: I18nSettings;
   sync: I18nSync;
   tokens: I18nTokens;
-  import_token: I18nImport_token;
   neuron_state: I18nNeuron_state;
   topics: I18nTopics;
   topics_description: I18nTopics_description;
