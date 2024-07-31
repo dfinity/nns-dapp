e988727368c2f44c036c6b6a765efd4bf0918b84
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-07-31 11:25:07 +0200

    New component ImportTokenModal

---
+++ frontend/src/lib/modals/accounts/ImportTokenModal.svelte
@@ -0,0 +1,106 @@
+<script lang="ts">
+  import {
+    WizardModal,
+    type WizardStep,
+    type WizardSteps,
+  } from "@dfinity/gix-components";
+  import { i18n } from "$lib/stores/i18n";
+  import type { Principal } from "@dfinity/principal";
+  import ImportTokenForm from "$lib/components/accounts/ImportTokenForm.svelte";
+  import type { IcrcTokenMetadata } from "$lib/types/icrc";
+  import { isNullish, nonNullish } from "@dfinity/utils";
+  import ImportTokenReview from "$lib/components/accounts/ImportTokenReview.svelte";
+  import { startBusy, stopBusy } from "$lib/stores/busy.store";
+  import { fetchIcrcTokenMetaData } from "$lib/services/icrc-accounts.services";
+  import { toastsError } from "$lib/stores/toasts.store";
+
+  export let currentStep: WizardStep | undefined = undefined;
+
+  const STEP_FORM = "Form";
+  const STEP_REVIEW = "Review";
+
+  const steps: WizardSteps = [
+    {
+      name: STEP_FORM,
+      title: $i18n.import_token.import_token,
+    },
+    {
+      name: STEP_REVIEW,
+      title: $i18n.import_token.review_token_info,
+    },
+  ];
+
+  let modal: WizardModal;
+  const next = () => {
+    modal?.next();
+  };
+  const back = () => {
+    modal?.back();
+  };
+
+  let ledgerCanisterId: Principal | undefined;
+  let indexCanisterId: Principal | undefined;
+  let tokenMetaData: IcrcTokenMetadata | undefined;
+
+  const updateTokenMetaData = async () => {
+    if (isNullish(ledgerCanisterId)) {
+      return;
+    }
+    const meta = await fetchIcrcTokenMetaData({ ledgerCanisterId });
+    if (isNullish(meta)) {
+      tokenMetaData = undefined;
+      toastsError({
+        labelKey: "import_token.ledger_canister_loading_error",
+      });
+      return;
+    }
+    tokenMetaData = meta;
+  };
+
+  const onUserInput = async () => {
+    startBusy({
+      initiator: "import-token-validation",
+      labelKey: "import_token.verifying",
+    });
+    await updateTokenMetaData();
+    // TODO: validate index canister id here (if provided)
+    stopBusy("import-token-validation");
+
+    if (nonNullish(tokenMetaData)) {
+      next();
+    }
+  };
+
+  const onUserConfirm = async () => {
+    // TODO: save imported token to the backend canister
+    // TODO: navigate to imported token details page
+  };
+</script>
+
+<WizardModal
+  testId="import-token-modal"
+  {steps}
+  bind:currentStep
+  bind:this={modal}
+  on:nnsClose
+>
+  <svelte:fragment slot="title">{currentStep?.title}</svelte:fragment>
+
+  {#if currentStep?.name === STEP_FORM}
+    <ImportTokenForm
+      bind:ledgerCanisterId
+      bind:indexCanisterId
+      on:nnsClose
+      on:nnsSubmit={onUserInput}
+    />
+  {/if}
+  {#if currentStep?.name === STEP_REVIEW && nonNullish(ledgerCanisterId) && nonNullish(tokenMetaData)}
+    <ImportTokenReview
+      {ledgerCanisterId}
+      {indexCanisterId}
+      {tokenMetaData}
+      on:nnsBack={back}
+      on:nnsConfirm={onUserConfirm}
+    />
+  {/if}
+</WizardModal>
