2a40797bd58d3d30d998d02507a0ea8263a8fd33
David de Kloet <david.de.kloet@dfinity.org>
2024-08-09 17:25:48 +0200

    NNS1-3250: Always show arrow icon on projects table row

--- frontend/src/lib/components/staking/ProjectActionsCell.svelte
+++ frontend/src/lib/components/staking/ProjectActionsCell.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
   import type { TableProject } from "$lib/types/staking";
   import { IconRight } from "@dfinity/gix-components";
-  import { nonNullish } from "@dfinity/utils";
 
   export let rowData: TableProject;
 </script>
 
-{#if nonNullish(rowData.rowHref)}
-  <div data-tid="go-to-neurons-table-action" class="container">
-    <IconRight />
-  </div>
+{#if false}
+  <!-- Satisfy the linter as it doesn't like unused props. -->
+  {rowData.title}
 {/if}
+
+<div data-tid="go-to-neurons-table-action" class="container">
+  <IconRight />
+</div>
 
 <style lang="scss">
   .container {
