fc7b22379ac82c3d8bd0c5a93c942b8d4a594fa1
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-08-12 06:40:14 +0200

    Add slot actions to WalletPageHeader

--- frontend/src/lib/components/universe/UniversePageSummary.svelte
+++ frontend/src/lib/components/universe/UniversePageSummary.svelte
@@ -6,24 +6,15 @@
   import { isImportedToken } from "$lib/utils/imported-tokens.utils";
   import { importedTokensStore } from "$lib/stores/imported-tokens.store";
   import { Principal } from "@dfinity/principal";
-  import { createEventDispatcher } from "svelte";
 
   export let universe: Universe;
 
-  const dispatch = createEventDispatcher();
-
-  // TODO: test me
+  // TODO: refactoring: switch to slot
   let importedToken = false;
   $: importedToken = isImportedToken({
     ledgerCanisterId: Principal.fromText(universe.canisterId),
     importedTokens: $importedTokensStore.importedTokens,
   });
-
-  // TODO: remove me after context menu implementation
-  const remove = () =>
-    dispatch("nnsRemove", {
-      ledgerCanisterId: Principal.fromText(universe.canisterId),
-    });
 </script>
 
 <span class="summary" data-tid="universe-page-summary-component">
@@ -31,13 +22,8 @@
   <span class="summary-title">{universe.title}</span>
   {#if importedToken}
     <Tag testId="imported-token-tag">{$i18n.import_token.imported_token}</Tag>
-    <button
-      class="ghost remove"
-      type="button"
-      on:click={remove}
-      data-tid="remove-button">{$i18n.core.remove}</button
-    >
   {/if}
+  <slot name="actions" />
 </span>
 
 <style lang="scss">
