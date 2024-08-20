e53c18b4e28f461fd9d18318f67e54e723c89a84
Martin Raszyk <martin.raszyk@dfinity.org>
2024-08-20 17:21:13 +0200

    spelling

--- rs/sns_aggregator/src/lib.rs
+++ rs/sns_aggregator/src/lib.rs
@@ -129,7 +129,7 @@ fn tail_log(limit: Option<u16>) -> String {
 
 /// Web server
 ///
-/// Note: we use a decoding quota of 10000 corresponding to roughly 10KB of decoded data incl. overhead,
+/// Note: we use a decoding quota of 10000 corresponding to roughly 10 KB of decoded data incl. overhead,
 /// see the Candid cost model (https://github.com/dfinity/candid/blob/f324a1686d6f2bd4fba9307a37f8e3f90cc7222b/rust/candid/src/de.rs#L170)
 /// for more details.
 #[candid_method(query)]
