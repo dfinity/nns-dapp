b1f2c77876dd959fb247961c5574a66451638d5d
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-08-23 09:49:45 +0200

    Replace  LinkIcon with LinkToDashboardCanister

--- frontend/src/lib/components/accounts/ImportTokenCanisterId.svelte
+++ frontend/src/lib/components/accounts/ImportTokenCanisterId.svelte
@@ -1,23 +1,22 @@
 <script lang="ts">
   import { Copy } from "@dfinity/gix-components";
   import { nonNullish } from "@dfinity/utils";
-  import LinkIcon from "$lib/components/common/LinkIcon.svelte";
+  import LinkToDashboardCanister from "$lib/components/common/LinkToDashboardCanister.svelte";
+  import type { Principal } from "@dfinity/principal";
 
   export let label: string;
-  export let canisterId: string | undefined = undefined;
-  export let canisterLinkHref: string | undefined = undefined;
+  export let testId: string = "import-token-canister-id-component";
+  export let canisterId: Principal | undefined = undefined;
   export let canisterIdFallback: string | undefined = undefined;
 </script>
 
-<div class="container" data-tid="import-token-canister-id-component">
+<div class="container" data-tid={testId}>
   <span data-tid="label">{label}</span>
   <div class="canister-id">
     {#if nonNullish(canisterId)}
       <span class="value description" data-tid="canister-id">{canisterId}</span>
-      <Copy value={canisterId} />
-      {#if nonNullish(canisterLinkHref)}
-        <LinkIcon href={canisterLinkHref} />
-      {/if}
+      <Copy value={canisterId.toText()} />
+      <LinkToDashboardCanister noLabel {canisterId} />
     {:else if nonNullish(canisterIdFallback)}
       <span class="fallback description" data-tid="canister-id-fallback"
         >{canisterIdFallback}</span
