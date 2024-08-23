941d51342d107291fc8aa7a01a0994f997755bd3
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-08-23 09:57:02 +0200

    Reset modal po

--- frontend/src/tests/page-objects/ImportTokenModal.page-object.ts
+++ frontend/src/tests/page-objects/ImportTokenModal.page-object.ts
@@ -1,5 +1,4 @@
 import { ImportTokenFormPo } from "$tests/page-objects/ImportTokenForm.page-object";
-import { ImportTokenReviewPo } from "$tests/page-objects/ImportTokenReview.page-object";
 import { ModalPo } from "$tests/page-objects/Modal.page-object";
 import type { PageObjectElement } from "$tests/types/page-object.types";
 
@@ -12,9 +11,5 @@ export class ImportTokenModalPo extends ModalPo {
 
   getImportTokenFormPo(): ImportTokenFormPo {
     return ImportTokenFormPo.under(this.root);
-  }
-
-  getImportTokenReviewPo(): ImportTokenReviewPo {
-    return ImportTokenReviewPo.under(this.root);
   }
 }
