50b3be2ee493e49ed0fbb0a86a547ef62ccce3d2
Max Strasinsky <98811342+mstrasinskis@users.noreply.github.com>
2024-08-23 14:25:10 +0200

    Refactor import token canister (#5347)
    # Motivation
    
    The `ImportTokenCanisterId` component will be reused in different places
    on the token page. To simplify its reusability, we are providing some
    small refactoring and enhancements.
    
    # Changes
    
    **ImportTokenCanisterId**
    
    - Add a `testId` parameter to accommodate the use of multiple components
    within a single parent.
    - Provide canisterId as a Principal (instead of a string) to enforce
    stricter typing.
    - Use `LinkToDashboardCanister` instead of `LinkIcon`, as it is more
    appropriate for linking to the canister on the dashboard.
    
    **LinkToDashboardCanister**
    
    - Add a `noLabel` parameter to have an icon-only component version.
    
    # Tests
    
    - POs and tests updated.
    - Can be tested on [beta](https://beta.nns.ic0.app/).
    
    <img width="327" alt="image"
    src="https://github.com/user-attachments/assets/c699531c-683a-44f0-bc25-fe929a11fc9b">
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    Not necessary.

--- frontend/src/tests/lib/components/accounts/ImportTokenCanisterId.spec.ts
+++ frontend/src/tests/lib/components/accounts/ImportTokenCanisterId.spec.ts
@@ -1,20 +1,22 @@
 import ImportTokenCanisterId from "$lib/components/accounts/ImportTokenCanisterId.svelte";
 import { ImportTokenCanisterIdPo } from "$tests/page-objects/ImportTokenCanisterId.page-object";
 import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
+import { Principal } from "@dfinity/principal";
 import { render } from "@testing-library/svelte";
 
 describe("ImportTokenCanisterId", () => {
   const props = {
     label: "test label",
-    canisterId: "aaaaa-aa",
-    canisterLinkHref: "http://test.com",
+    canisterId: Principal.fromText("aaaaa-aa"),
     canisterIdFallback: "test fallback",
   };
   const renderComponent = (props) => {
     const { container } = render(ImportTokenCanisterId, {
       props,
     });
-    return ImportTokenCanisterIdPo.under(new JestPageObjectElement(container));
+    return ImportTokenCanisterIdPo.under({
+      element: new JestPageObjectElement(container),
+    });
   };
 
   it("should render label, canister id and buttons", async () => {
@@ -25,15 +27,17 @@ describe("ImportTokenCanisterId", () => {
     expect(await po.getCanisterId().isPresent()).toEqual(true);
     expect(await po.getCanisterIdText()).toEqual("aaaaa-aa");
     expect(await po.getCopyButtonPo().isPresent()).toEqual(true);
-    expect(await po.getLinkIconPo().isPresent()).toEqual(true);
+    expect(await po.getLinkToDashboardCanisterPo().isPresent()).toEqual(true);
   });
 
   it("should render buttons", async () => {
     const po = renderComponent(props);
 
     expect(await po.getCopyButtonPo().isPresent()).toEqual(true);
-    expect(await po.getLinkIconPo().isPresent()).toEqual(true);
-    expect(await po.getLinkIconPo().getHref()).toEqual("http://test.com");
+    expect(await po.getLinkToDashboardCanisterPo().isPresent()).toEqual(true);
+    expect(await po.getLinkToDashboardCanisterPo().getHref()).toEqual(
+      "https://dashboard.internetcomputer.org/canister/aaaaa-aa"
+    );
   });
 
   it("should not render a fallback when canister id provided", async () => {
@@ -46,7 +50,6 @@ describe("ImportTokenCanisterId", () => {
   it("should render fallback when canister id not provided", async () => {
     const po = renderComponent({
       label: "test label",
-      canisterLinkHref: "http://test.com",
       canisterIdFallback: "test fallback",
     });
 
@@ -61,12 +64,11 @@ describe("ImportTokenCanisterId", () => {
   it("should render no buttons when canister id not provided", async () => {
     const po = renderComponent({
       label: "test label",
-      canisterLinkHref: "http://test.com",
       canisterIdFallback: "test fallback",
     });
 
     expect(await po.getCopyButtonPo().isPresent()).toEqual(false);
-    expect(await po.getLinkIconPo().isPresent()).toEqual(false);
+    expect(await po.getLinkToDashboardCanisterPo().isPresent()).toEqual(false);
   });
 
   it("Should render neither the canister id nor the fallback when both parameters are not provided", async () => {
