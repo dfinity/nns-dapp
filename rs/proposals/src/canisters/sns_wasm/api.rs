52348e6546658d834ffa9b30792564d51e66672d
sa-github-api <138766536+sa-github-api@users.noreply.github.com>
2024-08-26 20:34:53 +0200

    bot: Update proposals candid bindings (#5362)
    # Motivation
    We would like to render all the latest proposal types.
    Even with no changes, just updating the reference is good practice.
    
    # Changes
    * Update the version of `IC_COMMIT_FOR_PROPOSALS` specified in
    `dfx.json`.
    * Updated the `proposals` candid files to the versions in that commit.
    * Updated the Rust code derived from `.did` files in the proposals
    payload rendering crate.
    
    # Tests
    - [ ] Please check the API updates for any breaking changes that affect
    our code.
      - [ ] Please check for new proposal types and add tests for them.
    
    Breaking changes are:
      * New mandatory fields
        * Removing mandatory fields
        * Renaming fields
        * Changing the type of a field
        * Adding new variants
    
    Co-authored-by: gix-bot <gix-bot@users.noreply.github.com>

--- rs/proposals/src/canisters/sns_wasm/api.rs
+++ rs/proposals/src/canisters/sns_wasm/api.rs
@@ -1,5 +1,5 @@
 //! Rust code created from candid by: `scripts/did2rs.sh --canister sns_wasm --out api.rs --header did2rs.header --traits Serialize`
-//! Candid for canister `sns_wasm` obtained by `scripts/update_ic_commit` from: <https://raw.githubusercontent.com/dfinity/ic/release-2024-08-02_01-30-base/rs/nns/sns-wasm/canister/sns-wasm.did>
+//! Candid for canister `sns_wasm` obtained by `scripts/update_ic_commit` from: <https://raw.githubusercontent.com/dfinity/ic/release-2024-08-21_15-36-canister-snapshots/rs/nns/sns-wasm/canister/sns-wasm.did>
 #![allow(clippy::all)]
 #![allow(missing_docs)]
 #![allow(clippy::missing_docs_in_private_items)]
@@ -80,27 +80,6 @@ pub struct NeuronsFundParticipationConstraints {
     pub ideal_matched_participation_function: Option<IdealMatchedParticipationFunction>,
 }
 #[derive(Serialize, CandidType, Deserialize)]
-pub struct Principals {
-    pub principals: Vec<Principal>,
-}
-#[derive(Serialize, CandidType, Deserialize)]
-pub struct CfNeuron {
-    pub has_created_neuron_recipes: Option<bool>,
-    pub hotkeys: Option<Principals>,
-    pub nns_neuron_id: u64,
-    pub amount_icp_e8s: u64,
-}
-#[derive(Serialize, CandidType, Deserialize)]
-pub struct CfParticipant {
-    pub controller: Option<Principal>,
-    pub hotkey_principal: String,
-    pub cf_neurons: Vec<CfNeuron>,
-}
-#[derive(Serialize, CandidType, Deserialize)]
-pub struct NeuronsFundParticipants {
-    pub participants: Vec<CfParticipant>,
-}
-#[derive(Serialize, CandidType, Deserialize)]
 pub struct TreasuryDistribution {
     pub total_e8s: u64,
 }
@@ -169,7 +148,6 @@ pub struct SnsInitPayload {
     pub transaction_fee_e8s: Option<u64>,
     pub dapp_canisters: Option<DappCanisters>,
     pub neurons_fund_participation_constraints: Option<NeuronsFundParticipationConstraints>,
-    pub neurons_fund_participants: Option<NeuronsFundParticipants>,
     pub max_age_bonus_percentage: Option<u64>,
     pub initial_token_distribution: Option<InitialTokenDistribution>,
     pub reward_rate_transition_duration_seconds: Option<u64>,
