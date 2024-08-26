5ec4f893d1c49d3d4b814784313985a7f433ae3f
David de Kloet <david.de.kloet@dfinity.org>
2024-08-26 17:11:21 +0200

    proposal_info_with_action

--- rs/proposals/src/tests/mod.rs
+++ rs/proposals/src/tests/mod.rs
@@ -19,31 +19,3 @@ fn payload_deserialization() {
         println!("{}", transformed.unwrap());
     }
 }
-
-/// Constructs a `ProposalInfo` with the given action for testing.
-fn proposal_info_with_action(action: Action) -> ProposalInfo {
-    ProposalInfo {
-        id: Some(NeuronId { id: 1 }),
-        status: 1,
-        topic: 1,
-        failure_reason: None,
-        ballots: vec![],
-        proposal_timestamp_seconds: 1,
-        reward_event_round: 1,
-        deadline_timestamp_seconds: None,
-        failed_timestamp_seconds: 0,
-        reject_cost_e8s: 0,
-        derived_proposal_information: None,
-        latest_tally: None,
-        reward_status: 1,
-        decided_timestamp_seconds: 1,
-        proposal: Some(Box::new(Proposal {
-            url: "".to_string(),
-            summary: "".to_string(),
-            title: None,
-            action: Some(action),
-        })),
-        proposer: None,
-        executed_timestamp_seconds: 1,
-    }
-}
