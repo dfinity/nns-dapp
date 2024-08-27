2acd2fd0596001b422f5e403f207fa531fbebb65
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-08-27 11:57:53 +0200

    Use ConfirmationModal for ImportTokenRemoveConfirmation

--- frontend/src/lib/components/accounts/ImportTokenRemoveConfirmation.svelte
+++ frontend/src/lib/components/accounts/ImportTokenRemoveConfirmation.svelte
@@ -1,26 +1,21 @@
 <script lang="ts">
   import { i18n } from "$lib/stores/i18n";
-  import { Modal, Tag } from "@dfinity/gix-components";
-  import { createEventDispatcher } from "svelte";
   import UniversePageSummary from "$lib/components/universe/UniversePageSummary.svelte";
   import type { Universe } from "$lib/types/universe";
   import { nonNullish } from "@dfinity/utils";
-
-  const dispatch = createEventDispatcher();
+  import ConfirmationModal from "$lib/modals/common/ConfirmationModal.svelte";
+  import { Tag } from "@dfinity/gix-components";
 
   export let universe: Universe | undefined;
 </script>
 
-<Modal
-  role="alert"
-  on:nnsClose
+<ConfirmationModal
   testId="import-token-remove-confirmation-component"
-  disablePointerEvents={true}
+  on:nnsClose
+  on:nnsConfirm
+  noLabel={$i18n.core.cancel}
+  yesLabel={$i18n.core.remove}
 >
-  <svelte:fragment slot="title">
-    {$i18n.import_token.remove_confirmation_title}
-  </svelte:fragment>
-
   <div class="content">
     <div class="headline">
       {#if nonNullish(universe)}<UniversePageSummary {universe} />{/if}
@@ -28,25 +23,8 @@
     </div>
     <p>{$i18n.import_token.remove_confirmation_description_1}</p>
     <p>{$i18n.import_token.remove_confirmation_description_2}</p>
-    <div class="toolbar">
-      <button
-        class="secondary"
-        data-tid="close-button"
-        on:click={() => dispatch("nnsClose")}
-      >
-        {$i18n.core.cancel}
-      </button>
-
-      <button
-        data-tid="confirm-button"
-        class="primary"
-        on:click={() => dispatch("nnsConfirm")}
-      >
-        {$i18n.core.remove}
-      </button>
-    </div>
   </div>
-</Modal>
+</ConfirmationModal>
 
 <style lang="scss">
   .content {
