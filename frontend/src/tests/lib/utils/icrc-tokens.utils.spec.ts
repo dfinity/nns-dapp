577c0c8f53e6c5c135fd7927abbaef03fbc45245
Max Strasinsky <98811342+mstrasinskis@users.noreply.github.com>
2024-08-29 14:35:30 +0200

    Is important tokens utility (#5372)
    # Motivation
    
    It’s not allowed to import CKBTC, CKETH, and similar tokens. During
    imported token validation, we need to ensure that the provided ledger
    canister ID does not belong to these restricted tokens.
    
    # Changes
    
    - Add isImportantCkToken util.
    - Use it in the token table sorting.
    
    # Tests
    
    - Added.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    Not necessary.

--- frontend/src/tests/lib/utils/icrc-tokens.utils.spec.ts
+++ frontend/src/tests/lib/utils/icrc-tokens.utils.spec.ts
@@ -1,10 +1,23 @@
-import { OWN_CANISTER_ID_TEXT } from "$lib/constants/canister-ids.constants";
+import {
+  OWN_CANISTER_ID,
+  OWN_CANISTER_ID_TEXT,
+} from "$lib/constants/canister-ids.constants";
+import {
+  CKBTC_LEDGER_CANISTER_ID,
+  CKTESTBTC_LEDGER_CANISTER_ID,
+} from "$lib/constants/ckbtc-canister-ids.constants";
+import { CKETH_LEDGER_CANISTER_ID } from "$lib/constants/cketh-canister-ids.constants";
+import { CKUSDC_LEDGER_CANISTER_ID } from "$lib/constants/ckusdc-canister-ids.constants";
 import { tokensStore } from "$lib/stores/tokens.store";
-import { fillTokensStoreFromAggregatorData } from "$lib/utils/icrc-tokens.utils";
+import {
+  fillTokensStoreFromAggregatorData,
+  isImportantCkToken,
+} from "$lib/utils/icrc-tokens.utils";
 import {
   aggregatorSnsMockDto,
   aggregatorTokenMock,
 } from "$tests/mocks/sns-aggregator.mock";
+import { principal } from "$tests/mocks/sns-projects.mock";
 import { get } from "svelte/store";
 
 describe("ICRC tokens utils", () => {
@@ -34,6 +47,35 @@ describe("ICRC tokens utils", () => {
         tokensStoreData[aggregatorSnsMockDto.canister_ids.root_canister_id]
           .token
       ).toEqual(aggregatorTokenMock);
+    });
+  });
+
+  describe("isImportantCkToken", () => {
+    it("should return true for important token ledger canisters", () => {
+      expect(
+        isImportantCkToken({ ledgerCanisterId: CKBTC_LEDGER_CANISTER_ID })
+      ).toEqual(true);
+      expect(
+        isImportantCkToken({ ledgerCanisterId: CKETH_LEDGER_CANISTER_ID })
+      ).toEqual(true);
+      expect(
+        isImportantCkToken({ ledgerCanisterId: CKUSDC_LEDGER_CANISTER_ID })
+      ).toEqual(true);
+      expect(
+        isImportantCkToken({ ledgerCanisterId: CKBTC_LEDGER_CANISTER_ID })
+      ).toEqual(true);
+    });
+
+    it("should return false for not important token ledger canisters", () => {
+      expect(isImportantCkToken({ ledgerCanisterId: principal(0) })).toEqual(
+        false
+      );
+      expect(isImportantCkToken({ ledgerCanisterId: OWN_CANISTER_ID })).toEqual(
+        false
+      );
+      expect(
+        isImportantCkToken({ ledgerCanisterId: CKTESTBTC_LEDGER_CANISTER_ID })
+      ).toEqual(false);
     });
   });
 });
