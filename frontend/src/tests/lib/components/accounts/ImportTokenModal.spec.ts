e754acb977ea1a8724ba28b568e54d8ebaf49cc4
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-08-29 21:43:22 +0200

    test: replace startBusySpy with get(busyStore)

--- frontend/src/tests/lib/components/accounts/ImportTokenModal.spec.ts
+++ frontend/src/tests/lib/components/accounts/ImportTokenModal.spec.ts
@@ -2,7 +2,6 @@ import * as icrcIndexApi from "$lib/api/icrc-index.api";
 import * as ledgerApi from "$lib/api/icrc-ledger.api";
 import { CKBTC_LEDGER_CANISTER_ID } from "$lib/constants/ckbtc-canister-ids.constants";
 import ImportTokenModal from "$lib/modals/accounts/ImportTokenModal.svelte";
-import * as busyServices from "$lib/stores/busy.store";
 import { importedTokensStore } from "$lib/stores/imported-tokens.store";
 import type { IcrcTokenMetadata } from "$lib/types/icrc";
 import { resetIdentity } from "$tests/mocks/auth.store.mock";
@@ -12,7 +11,7 @@ import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
 import { resetSnsProjects, setSnsProjects } from "$tests/utils/sns.test-utils";
 import { render } from "$tests/utils/svelte.test-utils";
 import { runResolvedPromises } from "$tests/utils/timers.test-utils";
-import { toastsStore } from "@dfinity/gix-components";
+import { busyStore, toastsStore } from "@dfinity/gix-components";
 import { get } from "svelte/store";
 import type { SpyInstance } from "vitest";
 
@@ -55,6 +54,7 @@ describe("ImportTokenModal", () => {
     resetIdentity();
     resetSnsProjects();
     toastsStore.reset();
+    busyStore.resetForTesting();
 
     queryIcrcTokenSpy = vi
       .spyOn(ledgerApi, "queryIcrcToken")
@@ -69,7 +69,6 @@ describe("ImportTokenModal", () => {
 
   describe("Form Step", () => {
     it("should catch duplications", async () => {
-      const startBusySpy = vi.spyOn(busyServices, "startBusy");
       importedTokensStore.set({
         importedTokens: [
           {
@@ -90,7 +89,7 @@ describe("ImportTokenModal", () => {
 
       await formPo.getSubmitButtonPo().click();
 
-      expect(startBusySpy).toBeCalledTimes(0);
+      expect(get(busyStore)).toEqual([]);
       expectToastError(
         "You have already imported this token, you can find it in the token list."
       );
@@ -100,7 +99,6 @@ describe("ImportTokenModal", () => {
     });
 
     it("should catch importing of Snses", async () => {
-      const startBusySpy = vi.spyOn(busyServices, "startBusy");
       setSnsProjects([
         {
           ledgerCanisterId,
@@ -118,7 +116,7 @@ describe("ImportTokenModal", () => {
 
       await formPo.getSubmitButtonPo().click();
 
-      expect(startBusySpy).toBeCalledTimes(0);
+      expect(get(busyStore)).toEqual([]);
       expectToastError(
         "You cannot import SNS tokens, they are added automatically."
       );
@@ -128,7 +126,6 @@ describe("ImportTokenModal", () => {
     });
 
     it("should catch importing of an Important token", async () => {
-      const startBusySpy = vi.spyOn(busyServices, "startBusy");
       const { formPo } = renderComponent();
 
       await formPo
@@ -140,7 +137,7 @@ describe("ImportTokenModal", () => {
 
       await formPo.getSubmitButtonPo().click();
 
-      expect(startBusySpy).toBeCalledTimes(0);
+      expect(get(busyStore)).toEqual([]);
       expectToastError("This token is already in the token list.");
       expect(queryIcrcTokenSpy).toBeCalledTimes(0);
       // Stays on the form.
@@ -148,7 +145,6 @@ describe("ImportTokenModal", () => {
     });
 
     it("should fetch meta data", async () => {
-      const startBusySpy = vi.spyOn(busyServices, "startBusy");
       const { formPo } = renderComponent();
 
       await formPo
@@ -156,15 +152,11 @@ describe("ImportTokenModal", () => {
         .typeText(ledgerCanisterId.toText());
 
       expect(queryIcrcTokenSpy).toBeCalledTimes(0);
+      expect(get(busyStore)).toEqual([]);
 
       await formPo.getSubmitButtonPo().click();
 
       expect(queryIcrcTokenSpy).toBeCalledTimes(1);
-      expect(startBusySpy).toBeCalledTimes(1);
-      expect(startBusySpy).toBeCalledWith({
-        initiator: "import-token-validation",
-        labelKey: "import_token.verifying",
-      });
     });
 
     it("should catch not a ledger canister id", async () => {
@@ -212,18 +204,22 @@ describe("ImportTokenModal", () => {
   });
 
   it("should leave the form after successful validation", async () => {
-    const startBusySpy = vi.spyOn(busyServices, "startBusy");
-    const stopBusySpy = vi.spyOn(busyServices, "stopBusy");
     const { formPo } = renderComponent();
 
     await formPo.getLedgerCanisterInputPo().typeText(ledgerCanisterId.toText());
     await formPo.getSubmitButtonPo().click();
 
+    expect(get(busyStore)).toEqual([
+      {
+        initiator: "import-token-validation",
+        text: "Verifying token details...",
+      },
+    ]);
+
     // Wait for toast error to be called.
     await runResolvedPromises();
 
-    expect(startBusySpy).toBeCalledTimes(1);
-    expect(stopBusySpy).toBeCalledTimes(1);
+    expect(get(busyStore)).toEqual([]);
     expect(await formPo.isPresent()).toEqual(false);
   });
 });
