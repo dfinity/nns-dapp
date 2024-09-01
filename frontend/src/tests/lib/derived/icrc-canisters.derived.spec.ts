f5c49ff91f035672c885a98e1ef866b0a9194018
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-09-01 20:19:05 +0200

    test: icrcCanistersStore

---
+++ frontend/src/tests/lib/derived/icrc-canisters.derived.spec.ts
@@ -0,0 +1,78 @@
+import { icrcCanistersStore } from "$lib/derived/icrc-canisters.derived";
+import { defaultIcrcCanistersStore } from "$lib/stores/default-icrc-canisters.store";
+import { importedTokensStore } from "$lib/stores/imported-tokens.store";
+import { principal } from "$tests/mocks/sns-projects.mock";
+import { get } from "svelte/store";
+
+describe("icrcCanistersStore", () => {
+  const ledgerCanisterId = principal(0);
+  const indexCanisterId = principal(1);
+  const ledgerCanisterId2 = principal(2);
+
+  beforeEach(() => {
+    defaultIcrcCanistersStore.reset();
+    importedTokensStore.reset();
+  });
+
+  it("returns empty object when no icrc tokens are present", () => {
+    expect(get(icrcCanistersStore)).toEqual({});
+  });
+
+  it("return data from defaultIcrcCanistersStore", () => {
+    defaultIcrcCanistersStore.setCanisters({
+      ledgerCanisterId,
+      indexCanisterId,
+    });
+
+    expect(get(icrcCanistersStore)).toEqual({
+      [ledgerCanisterId.toText()]: {
+        ledgerCanisterId,
+        indexCanisterId,
+      },
+    });
+  });
+
+  it("return data from importedTokensStore", () => {
+    importedTokensStore.set({
+      importedTokens: [
+        {
+          ledgerCanisterId,
+          indexCanisterId: undefined,
+        },
+      ],
+      certified: true,
+    });
+
+    expect(get(icrcCanistersStore)).toEqual({
+      [ledgerCanisterId.toText()]: {
+        ledgerCanisterId,
+      },
+    });
+  });
+
+  it("return data from the defaultIcrcCanistersStore and the importedTokensStore", () => {
+    defaultIcrcCanistersStore.setCanisters({
+      ledgerCanisterId,
+      indexCanisterId,
+    });
+    importedTokensStore.set({
+      importedTokens: [
+        {
+          ledgerCanisterId: ledgerCanisterId2,
+          indexCanisterId: undefined,
+        },
+      ],
+      certified: true,
+    });
+
+    expect(get(icrcCanistersStore)).toEqual({
+      [ledgerCanisterId.toText()]: {
+        ledgerCanisterId,
+        indexCanisterId,
+      },
+      [ledgerCanisterId2.toText()]: {
+        ledgerCanisterId: ledgerCanisterId2,
+      },
+    });
+  });
+});
