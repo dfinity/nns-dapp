5efa894c619346fac836927cb3d5f0c33c0548ea
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-09-03 14:02:25 +0200

    Revert "Add createDeferredPromise test util"
    This reverts commit b08a63b081d61d22a434d21b6f6ae040d968ea5b.

--- frontend/src/tests/utils/utils.test-utils.ts
+++ frontend/src/tests/utils/utils.test-utils.ts
@@ -44,31 +44,3 @@ export const isNotVisible = (element: Element): boolean => {
     return nonNullish(heightString) && Number(heightString) === 0;
   }
 };
-
-// Return a promise-like object that exposes its resolve and reject functions.
-export const createDeferredPromise = <T>(): Promise<T> & {
-  promise: Promise<T>;
-  resolve: (value: T) => void;
-  reject: (reason?: unknown) => void;
-} => {
-  let resolve: (value: T) => void;
-  let reject: (reason?: unknown) => void;
-
-  const promise = new Promise<T>((res, rej) => {
-    resolve = res;
-    reject = rej;
-  });
-
-  return {
-    promise,
-    resolve,
-    reject,
-    then: (onFulfilled, onRejected) => promise.then(onFulfilled, onRejected),
-    catch: (onRejected) => promise.catch(onRejected),
-    finally: (onFinally) => promise.finally(onFinally),
-  } as Promise<T> & {
-    promise: Promise<T>;
-    resolve: (value: T) => void;
-    reject: (reason?: unknown) => void;
-  };
-};
