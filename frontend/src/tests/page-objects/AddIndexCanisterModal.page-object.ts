dbd7ff6436f23cc71ad369f57a3995b632181b4c
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-09-03 16:18:31 +0200

    test: add AddIndexCanisterModalPo

---
+++ frontend/src/tests/page-objects/AddIndexCanisterModal.page-object.ts
@@ -0,0 +1,17 @@
+import { ImportTokenFormPo } from "$tests/page-objects/ImportTokenForm.page-object";
+import { ModalPo } from "$tests/page-objects/Modal.page-object";
+import type { PageObjectElement } from "$tests/types/page-object.types";
+
+export class AddIndexCanisterModalPo extends ModalPo {
+  private static readonly TID = "add-index-canister-modal-component";
+
+  static under(element: PageObjectElement): AddIndexCanisterModalPo {
+    return new AddIndexCanisterModalPo(
+      element.byTestId(AddIndexCanisterModalPo.TID)
+    );
+  }
+
+  getImportTokenFormPo(): ImportTokenFormPo {
+    return ImportTokenFormPo.under(this.root);
+  }
+}
