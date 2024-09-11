eb8367fd8ee6749208ed1c577a6f3213702e5e4e
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-09-11 23:54:17 +0200

    Display failed token info

--- frontend/src/lib/components/tokens/TokensTable/TokenTitleCell.svelte
+++ frontend/src/lib/components/tokens/TokensTable/TokenTitleCell.svelte
@@ -6,10 +6,13 @@
   } from "$lib/types/tokens-page";
   import { importedTokensStore } from "$lib/stores/imported-tokens.store";
   import { isImportedToken } from "$lib/utils/imported-tokens.utils";
-  import Logo from "../../ui/Logo.svelte";
+  import Logo from "$lib/components/ui/Logo.svelte";
   import { nonNullish } from "@dfinity/utils";
-  import { Tag } from "@dfinity/gix-components";
+  import { IconError, IconWarning, Tag } from "@dfinity/gix-components";
   import { i18n } from "$lib/stores/i18n";
+  import LogoWrapper from "$lib/components/ui/LogoWrapper.svelte";
+  import Hash from "$lib/components/ui/Hash.svelte";
+  import { isUserTokenFailed } from "$lib/utils/user-token.utils";
 
   export let rowData: UserTokenData | UserTokenLoading | UserTokenFailed;
 
@@ -21,17 +24,36 @@
 </script>
 
 <div class="title-logo-wrapper">
-  <Logo src={rowData.logo} alt={rowData.title} size="medium" framed />
-  <div class="title-wrapper">
-    <h5 data-tid="project-name">{rowData.title}</h5>
-    {#if nonNullish(rowData.subtitle)}
-      <span data-tid="project-subtitle" class="description"
-        >{rowData.subtitle}</span
-      >
-    {/if}
-  </div>
+  {#if isUserTokenFailed(rowData)}
+    <LogoWrapper size="medium" framed testId="failed-logo">
+      <div data-tid="vote-icon">
+        <IconWarning size="24px" />
+      </div>
+    </LogoWrapper>
+    <Hash
+      testId="failed-ledger-canister-id"
+      text={`${rowData.universeId.toText()}`}
+      tagName="span"
+      splitLength={6}
+      tooltipTop
+      showCopy
+    />
+  {:else}
+    <Logo src={rowData.logo} alt={rowData.title} size="medium" framed />
+    <div class="title-wrapper">
+      <h5 data-tid="project-name">{rowData.title}</h5>
+      {#if nonNullish(rowData.subtitle)}
+        <span data-tid="project-subtitle" class="description"
+          >{rowData.subtitle}</span
+        >
+      {/if}
+    </div>
+  {/if}
   {#if importedToken}
     <Tag testId="imported-token-tag">{$i18n.import_token.imported_token}</Tag>
+  {/if}
+  {#if isUserTokenFailed(rowData)}
+    <div class="failed-token-icon"><IconError size="20px" /></div>
   {/if}
 </div>
 
@@ -50,5 +72,10 @@
       flex-direction: column;
       gap: var(--padding-0_5x);
     }
+  }
+
+  .failed-token-icon {
+    color: var(--alert);
+    line-height: 0;
   }
 </style>
