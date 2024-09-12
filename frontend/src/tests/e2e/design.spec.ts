dfb3a3fc446400877ee803503193b1981a3d29a1
David de Kloet <david.de.kloet@dfinity.org>
2024-09-12 08:50:35 +0200

    fmt

--- frontend/src/tests/e2e/design.spec.ts
+++ frontend/src/tests/e2e/design.spec.ts
@@ -1,6 +1,6 @@
 import { AppPo } from "$tests/page-objects/App.page-object";
 import { PlaywrightPageObjectElement } from "$tests/page-objects/playwright.page-object";
-import { replaceContent, signInWithNewUser } from "$tests/utils/e2e.test-utils";
+import { signInWithNewUser } from "$tests/utils/e2e.test-utils";
 import { expect, test, type Page } from "@playwright/test";
 
 test.describe("Design", () => {
