0597ad6c58cda738d907dd815ea64fa5a6b34db1
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-09-18 09:46:29 +0200

    test: should display ledger ID when universe is not available

--- frontend/src/tests/page-objects/ImportTokenRemoveConfirmation.page-object.ts
+++ frontend/src/tests/page-objects/ImportTokenRemoveConfirmation.page-object.ts
@@ -14,4 +14,8 @@ export class ImportTokenRemoveConfirmationPo extends ConfirmationModalPo {
   getUniverseSummaryPo(): UniverseSummaryPo {
     return UniverseSummaryPo.under(this.root);
   }
+
+  getLedgerCanisterId(): Promise<string> {
+    return this.getText("ledger-canister-id");
+  }
 }
