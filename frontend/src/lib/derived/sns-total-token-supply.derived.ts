59ba1b201bb66d858c527e044cb4650708348e54
David de Kloet <david.de.kloet@dfinity.org>
2024-09-19 13:32:44 +0200

    Extract snsAggregatorDerived function

--- frontend/src/lib/derived/sns-total-token-supply.derived.ts
+++ frontend/src/lib/derived/sns-total-token-supply.derived.ts
@@ -1,6 +1,6 @@
-import { snsAggregatorStore } from "$lib/stores/sns-aggregator.store";
 import type { RootCanisterIdText } from "$lib/types/sns";
-import { derived, type Readable } from "svelte/store";
+import { type Readable } from "svelte/store";
+import { snsAggregatorDerived } from "./sns-aggregator.derived";
 
 interface ProjectTotalSupplyData {
   totalSupply: bigint;
@@ -17,15 +17,7 @@ export interface SnsTotalTokenSupplyStore
 /**
  * A store that contains the total token supply of SNS projects.
  */
-export const snsTotalTokenSupplyStore: SnsTotalTokenSupplyStore = derived(
-  snsAggregatorStore,
-  (aggregatorStore) =>
-    Object.fromEntries(
-      aggregatorStore.data?.map((sns) => [
-        sns.canister_ids.root_canister_id,
-        {
-          totalSupply: BigInt(sns.icrc1_total_supply),
-        },
-      ]) ?? []
-    )
-);
+export const snsTotalTokenSupplyStore: SnsTotalTokenSupplyStore =
+  snsAggregatorDerived((sns) => ({
+    totalSupply: BigInt(sns.icrc1_total_supply),
+  }));
