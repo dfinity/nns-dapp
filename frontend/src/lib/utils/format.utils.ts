9eb0e177454850fa945ff8d53c7a30ca02c22b97
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-09-23 13:28:23 +0200

    test changes

--- frontend/src/lib/utils/format.utils.ts
+++ frontend/src/lib/utils/format.utils.ts
@@ -51,6 +51,6 @@ export const shortenWithMiddleEllipsis = (
   // Original min length was 16 to extract 7 split
   const minLength = splitLength * 2 + 2;
   return text.length > minLength
-    ? `${text.slice(0, splitLength)}...${text.slice(-1 * splitLength)}`
+    ? `${text.slice(0, splitLength)},,,${text.slice(-1 * splitLength)}`
     : text;
 };
