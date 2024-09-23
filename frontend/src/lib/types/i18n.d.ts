087c564fbf46e92d557fb106ac29aa2a28a4436f
“Cosku <cosku.cinkilic@dfinity.org>
2024-09-23 16:26:41 +0200

    Add text in i18n and update types

--- frontend/src/lib/types/i18n.d.ts
+++ frontend/src/lib/types/i18n.d.ts
@@ -332,7 +332,12 @@ interface I18nNeurons {
   merge_neurons_more_info: string;
   stake_amount: string;
   state: string;
-  public: string;
+  public_neuron_tooltip: string;
+  public_neuron: string;
+  private_neuron: string;
+  public_neuron_description: string;
+  private_neuron_description: string;
+  learn_more: string;
 }
 
 interface I18nNew_followee {
