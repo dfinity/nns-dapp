3c2b118259e75679f5bc8ee5a5d6bbf27991b34d
Cosku Cinkilic <cosku.cinkilic@dfinity.org>
2024-09-24 14:30:22 +0200

    Fix i18n variable usage

--- frontend/src/lib/components/neurons/NeuronsTable/NeuronIdCell.svelte
+++ frontend/src/lib/components/neurons/NeuronsTable/NeuronIdCell.svelte
@@ -18,7 +18,11 @@
   />
   {#if $ENABLE_NEURON_VISIBILITY && rowData.isPublic}
     <span class="public-icon-container" data-tid="public-icon-container">
-      <Tooltip top id="neuron-id-cell-public-icon" text={$i18n.neurons.public}>
+      <Tooltip
+        top
+        id="neuron-id-cell-public-icon"
+        text={$i18n.neurons.public_neuron_tooltip}
+      >
         <IconPublicBadge />
       </Tooltip>
     </span>
