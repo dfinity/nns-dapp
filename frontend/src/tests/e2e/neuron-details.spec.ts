4cdba24c036a87e805262edd513d7019148c6498
Cosku Cinkilic <cosku.cinkilic@dfinity.org>
2024-09-30 10:49:59 +0200

    Update replace content replacements

--- frontend/src/tests/e2e/neuron-details.spec.ts
+++ frontend/src/tests/e2e/neuron-details.spec.ts
@@ -41,7 +41,7 @@ test("Test neuron details", async ({ page, context }) => {
     page,
     selectors: ['[data-tid="identifier"]', '[data-tid="neuron-id"]'],
     pattern: /[0-9a-f]{19}/,
-    replacements: ["7737260276268288098", "7737260276268288098"],
+    replacements: ["7737260276268288098"],
   });
   await replaceContent({
     page,
