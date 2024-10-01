365073ae4dc9e37c41a2af102c15841781c44217
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-10-01 10:36:54 +0200

    Add a label

--- frontend/src/lib/types/i18n.d.ts
+++ frontend/src/lib/types/i18n.d.ts
@@ -1121,6 +1121,7 @@ interface I18nImport_token {
   add_index_canister: string;
   add_index_description: string;
   failed_tooltip: string;
+  maximum_reached_tooltip: string;
 }
 
 interface I18nNeuron_state {
