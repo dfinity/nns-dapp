a77b284b07d1be10b93b9d3f0756a61c9ed86544
David de Kloet <david.de.kloet@dfinity.org>
2024-10-07 14:10:47 +0200

    Disable Metrics in IS_TEST_ENV for src/tests/routes/app/layout.spec.ts

--- frontend/src/lib/components/warnings/Warnings.svelte
+++ frontend/src/lib/components/warnings/Warnings.svelte
@@ -2,13 +2,20 @@
   import Metrics from "$lib/components/metrics/Metrics.svelte";
   import TestEnvironmentWarning from "$lib/components/warnings/TestEnvironmentWarning.svelte";
   import TransactionRateWarning from "$lib/components/warnings/TransactionRateWarning.svelte";
+  import { IS_TEST_ENV } from "$lib/constants/mockable.constants";
   import { Toasts } from "@dfinity/gix-components";
 
   export let bringToastsForward = false;
   export let testEnvironmentWarning = false;
 </script>
 
-<Metrics />
+<!--
+  Avoid `ReferenceError: Worker is not defined` in
+  `src/tests/routes/app/layout.spec.ts`.
+-->
+{#if !IS_TEST_ENV}
+  <Metrics />
+{/if}
 
 <div class:forward={bringToastsForward}>
   <Toasts position="top" />
