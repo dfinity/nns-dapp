9e2ef5e03e642532b517017044a33ed032d9392b
David de Kloet <david.de.kloet@dfinity.org>
2024-10-08 16:29:19 +0200

    FOLLOW-1337: Disable FORCE_CALL_STRATEGY for 10% of sessions

--- frontend/src/lib/constants/mockable.constants.ts
+++ frontend/src/lib/constants/mockable.constants.ts
@@ -5,7 +5,40 @@
 
 export const DEV = import.meta.env.DEV;
 
-export const FORCE_CALL_STRATEGY: "query" | undefined = "query";
+// We use FORCE_CALL_STRATEGY to turn query+update into just a query call in a
+// number of places. We want to go back to doing query+update calls in all these
+// places. We want to do this as a percentage rollout.
+//
+//   0: means we still do only query calls in these places.
+// 100: means we do query+update calls in all these places.
+//  10: means we do query+update calls in these places for 10% of sessions.
+//
+// Whether we do query+update calls remains fixed for the duration of the
+// sessions, by which we mean until the next time the app/page is reloaded.
+const RESTORE_QUERY_AND_UPDATE_ROLLOUT_PERCENTAGE: number = 10;
+
+const getForceCallStrategyForPercentage = (
+  percentage: number
+): undefined | "query" => {
+  // Treat 0 and 100 explicitly to avoid edge cases.
+  if (percentage === 0) {
+    return "query";
+  }
+  if (percentage === 100) {
+    return undefined;
+  }
+  return Math.random() * 100 <= percentage ? undefined : "query";
+};
+
+const initForceCallStrategy = (): undefined | "query" => {
+  const strategy = getForceCallStrategyForPercentage(
+    RESTORE_QUERY_AND_UPDATE_ROLLOUT_PERCENTAGE
+  );
+  console.info("FORCE_CALL_STRATEGY is initialized to:", strategy);
+  return strategy;
+};
+
+export const FORCE_CALL_STRATEGY: "query" | undefined = initForceCallStrategy();
 
 export const isForceCallStrategy = (): boolean =>
   FORCE_CALL_STRATEGY === "query";
