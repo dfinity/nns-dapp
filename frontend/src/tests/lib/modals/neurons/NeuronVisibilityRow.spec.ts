43564dd13943a0dd963b9aecba7d026b084326ca
Cosku Cinkilic <cosku.cinkilic@dfinity.org>
2024-10-16 13:32:10 +0200

    Add NeuronVisibilityRow.svelte (#5621)
    # Motivation
    
    Have a component to display neuron rows visible to this [figma
    file](https://www.figma.com/design/x8dk5axQUf8Bi43uUsqCHc/NNS-Production-%26-Hand-over?node-id=4526-25430&node-type=frame&t=T9Ymc4jGOTY09elF-0).
    
    # Changes
    
    Add NeuronVisibilityRow component which can display
    NeuronVisibilityRowData
    
    # Tests
    
    Add tests to check NeuronVisibilityRow functioning as expected
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    not necessary

---
+++ frontend/src/tests/lib/modals/neurons/NeuronVisibilityRow.spec.ts
@@ -0,0 +1,153 @@
+import NeuronVisibilityRow from "$lib/modals/neurons/NeuronVisibilityRow.svelte";
+import type { NeuronVisibilityRowData } from "$lib/types/neuron-visibility-row";
+import { NeuronVisibilityRowPo } from "$tests/page-objects/NeuronVisibilityRow.page-object";
+import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
+import { render } from "@testing-library/svelte";
+
+describe("NeuronVisibilityRow", () => {
+  const renderComponent = (rowData: NeuronVisibilityRowData) => {
+    const { container } = render(NeuronVisibilityRow, {
+      props: { rowData },
+    });
+
+    return NeuronVisibilityRowPo.under({
+      element: new JestPageObjectElement(container),
+      neuronId: rowData.neuronId.toString(),
+    });
+  };
+
+  it("should display the correct neuron ID", async () => {
+    const rowData: NeuronVisibilityRowData = {
+      neuronId: BigInt(123).toString(),
+      isPublic: false,
+      tags: [],
+    };
+
+    const po = renderComponent(rowData);
+
+    expect(await po.getNeuronId()).toBe("123");
+  });
+
+  it("should display the public icon for public neurons", async () => {
+    const rowData: NeuronVisibilityRowData = {
+      neuronId: BigInt(123).toString(),
+      isPublic: true,
+      tags: [],
+    };
+
+    const po = renderComponent(rowData);
+
+    expect(await po.isPublic()).toBe(true);
+  });
+
+  it("should not display the public icon for private neurons", async () => {
+    const rowData: NeuronVisibilityRowData = {
+      neuronId: BigInt(123).toString(),
+      isPublic: false,
+      tags: [],
+    };
+
+    const po = renderComponent(rowData);
+
+    expect(await po.isPublic()).toBe(false);
+  });
+
+  it("should display tags when present", async () => {
+    const rowData: NeuronVisibilityRowData = {
+      neuronId: BigInt(123).toString(),
+      isPublic: false,
+      tags: ["Tag1", "Tag2"],
+    };
+
+    const po = renderComponent(rowData);
+
+    expect(await po.getTags()).toEqual(["Tag1", "Tag2"]);
+  });
+
+  it("should not display tags when not present", async () => {
+    const rowData: NeuronVisibilityRowData = {
+      neuronId: BigInt(123).toString(),
+      isPublic: false,
+      tags: [],
+    };
+
+    const po = renderComponent(rowData);
+
+    expect(await po.getTags()).toEqual([]);
+  });
+
+  it("should display uncontrolled neuron details for hardware wallet", async () => {
+    const rowData: NeuronVisibilityRowData = {
+      neuronId: BigInt(123).toString(),
+      isPublic: false,
+      tags: [],
+      uncontrolledNeuronDetails: {
+        type: "hardwareWallet",
+        text: "Hardware Wallet",
+      },
+    };
+
+    const po = renderComponent(rowData);
+
+    expect(await po.getUncontrolledNeuronDetailsText()).toEqual(
+      "Hardware Wallet"
+    );
+  });
+
+  it("should display uncontrolled neuron details for hotkey", async () => {
+    const rowData: NeuronVisibilityRowData = {
+      neuronId: BigInt(123).toString(),
+      isPublic: false,
+      tags: [],
+      uncontrolledNeuronDetails: {
+        type: "hotkey",
+        text: "1231392...2831823",
+      },
+    };
+
+    const po = renderComponent(rowData);
+
+    expect(await po.getUncontrolledNeuronDetailsText()).toEqual(
+      "1231392...2831823"
+    );
+  });
+
+  it("should not display uncontrolled neuron details when not present", async () => {
+    const rowData: NeuronVisibilityRowData = {
+      neuronId: BigInt(123).toString(),
+      isPublic: false,
+      tags: [],
+    };
+
+    const po = renderComponent(rowData);
+
+    expect(await po.getUncontrolledNeuronDetailsText()).toBeNull();
+  });
+
+  it("should display public icon tooltip", async () => {
+    const rowData: NeuronVisibilityRowData = {
+      neuronId: BigInt(123).toString(),
+      isPublic: true,
+      tags: [],
+    };
+
+    const po = renderComponent(rowData);
+
+    const tooltipPo = await po.getPublicNeuronTooltipPo();
+    expect(await tooltipPo.getTooltipText()).toBe("Neuron is public");
+  });
+
+  it("should display multiple tags when present", async () => {
+    const rowData: NeuronVisibilityRowData = {
+      neuronId: BigInt(123).toString(),
+      isPublic: false,
+      tags: ["First tag", "Second tag"],
+    };
+
+    const po = renderComponent(rowData);
+
+    const tags = await po.getTags();
+    expect(tags).toEqual(["First tag", "Second tag"]);
+    expect(tags).toHaveLength(2);
+  });
+});
---
+++ frontend/src/tests/page-objects/NeuronVisibilityRow.page-object.ts
@@ -0,0 +1,41 @@
+import { BasePageObject } from "$tests/page-objects/base.page-object";
+import { TooltipPo } from "$tests/page-objects/Tooltip.page-object";
+import type { PageObjectElement } from "$tests/types/page-object.types";
+
+export class NeuronVisibilityRowPo extends BasePageObject {
+  private static readonly BASE_TID = "neuron-visibility-row-component";
+
+  static under({
+    element,
+    neuronId,
+  }: {
+    element: PageObjectElement;
+    neuronId: string;
+  }): NeuronVisibilityRowPo {
+    const testId = `${NeuronVisibilityRowPo.BASE_TID}-${neuronId}`;
+    return new NeuronVisibilityRowPo(element.byTestId(testId));
+  }
+
+  getNeuronId(): Promise<string> {
+    return this.getText("neuron-id");
+  }
+
+  isPublic(): Promise<boolean> {
+    return this.isPresent("public-icon-container");
+  }
+
+  getPublicNeuronTooltipPo(): TooltipPo {
+    return TooltipPo.under(this.root.byTestId("public-icon-container"));
+  }
+
+  async getTags(): Promise<string[]> {
+    const tagElements = await this.root.querySelectorAll(
+      "[data-tid='neuron-tag']"
+    );
+    return Promise.all(tagElements.map((el) => el.getText()));
+  }
+
+  getUncontrolledNeuronDetailsText(): Promise<string> {
+    return this.getText("uncontrolled-neuron-detail");
+  }
+}
