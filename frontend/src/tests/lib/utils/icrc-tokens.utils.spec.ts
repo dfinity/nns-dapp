281a68ab8870c20c196d77a0d8878b71f216cea4
David de Kloet <david.de.kloet@dfinity.org>
2024-10-16 16:11:34 +0200

    Merge mocks/icrc-tokens.utils.spec.ts with tests/lib/utils/icrc-tokens.utils.spec.ts

--- frontend/src/tests/lib/utils/icrc-tokens.utils.spec.ts
+++ frontend/src/tests/lib/utils/icrc-tokens.utils.spec.ts
@@ -12,17 +12,72 @@ import { tokensStore } from "$lib/stores/tokens.store";
 import {
   fillTokensStoreFromAggregatorData,
   isImportantCkToken,
+  mapOptionalToken,
 } from "$lib/utils/icrc-tokens.utils";
 import {
   aggregatorSnsMockDto,
   aggregatorTokenMock,
 } from "$tests/mocks/sns-aggregator.mock";
-import { principal } from "$tests/mocks/sns-projects.mock";
+import {
+  mockQueryTokenResponse,
+  mockSnsToken,
+  principal,
+} from "$tests/mocks/sns-projects.mock";
+import { IcrcMetadataResponseEntries } from "@dfinity/ledger-icrc";
 import { get } from "svelte/store";
 
 describe("ICRC tokens utils", () => {
   beforeEach(() => {
     tokensStore.reset();
+  });
+
+  describe("mapOptionalToken", () => {
+    it("should return token", () => {
+      const token = mapOptionalToken(mockQueryTokenResponse);
+      expect(token?.name).toBeDefined();
+      expect(token?.symbol).toBeDefined();
+      expect(token?.fee).toBeDefined();
+    });
+
+    it("should not return token if name is missing", () => {
+      const token = mapOptionalToken(
+        mockQueryTokenResponse.filter(
+          ([key]) => key !== IcrcMetadataResponseEntries.NAME
+        )
+      );
+      expect(token).toBeUndefined();
+    });
+
+    it("should not return token if symbol is missing", () => {
+      const token = mapOptionalToken(
+        mockQueryTokenResponse.filter(
+          ([key]) => key !== IcrcMetadataResponseEntries.SYMBOL
+        )
+      );
+      expect(token).toBeUndefined();
+    });
+
+    it("should not return token if fee is missing", () => {
+      const token = mapOptionalToken(
+        mockQueryTokenResponse.filter(
+          ([key]) => key !== IcrcMetadataResponseEntries.FEE
+        )
+      );
+      expect(token).toBeUndefined();
+    });
+
+    it("should return token with decimals and logo", () => {
+      const logo = "data:image/svg+xml;base64...";
+      const token = mapOptionalToken([
+        ...mockQueryTokenResponse,
+        [IcrcMetadataResponseEntries.LOGO, { Text: logo }],
+      ]);
+      expect(token).toEqual({
+        ...mockSnsToken,
+        decimals: 8,
+        logo,
+      });
+    });
   });
 
   describe("fillTokensStoreFromAggregatorData", () => {
