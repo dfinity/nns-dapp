a1bef221d3a7c9cf349927568f0aed5530da9bc7
Cosku Cinkilic <cosku.cinkilic@dfinity.org>
2024-10-18 00:07:49 +0200

    Create utils for repeated code

--- frontend/src/lib/stores/neurons-table-order-sorted-neuronIds-store.ts
+++ frontend/src/lib/stores/neurons-table-order-sorted-neuronIds-store.ts
@@ -1,20 +1,18 @@
 import { icpAccountsStore } from "$lib/derived/icp-accounts.derived";
 import { authStore } from "$lib/stores/auth.store";
 import { i18n } from "$lib/stores/i18n";
-
 import { neuronsTableOrderStore } from "$lib/stores/neurons-table.store";
 import { definedNeuronsStore } from "$lib/stores/neurons.store";
 import {
-  comparators,
-  tableNeuronsFromNeuronInfos,
-} from "$lib/utils/neurons-table.utils";
-import { mergeComparators, negate } from "$lib/utils/responsive-table.utils";
-
+  createNeuronsStore,
+  sortNeuronIds,
+} from "$lib/utils/neurons-table-order-sorted-neuronids-store.utils";
+import { tableNeuronsFromNeuronInfos } from "$lib/utils/neurons-table.utils";
 import { derived } from "svelte/store";
 
-export const neuronsStore = derived(
+export const neuronsStore = createNeuronsStore(
   [authStore, icpAccountsStore, i18n, definedNeuronsStore],
-  ([$authStore, $icpAccountsStore, $i18n, $definedNeuronsStore]) =>
+  ($authStore, $icpAccountsStore, $i18n, $definedNeuronsStore) =>
     tableNeuronsFromNeuronInfos({
       identity: $authStore.identity,
       accounts: $icpAccountsStore,
@@ -25,25 +23,5 @@ export const neuronsStore = derived(
 
 export const neuronsTableOrderSortedNeuronIdsStore = derived(
   [neuronsTableOrderStore, neuronsStore],
-  ([$order, $neurons]) => {
-    const comparatorByColumnId = comparators;
-    const comparatorsArray = $order
-      .map(({ columnId, reversed }) => {
-        const comparator = comparatorByColumnId[columnId];
-        return comparator
-          ? reversed
-            ? negate(comparator)
-            : comparator
-          : undefined;
-      })
-      .filter((c): c is NonNullable<typeof c> => c !== undefined);
-
-    if (comparatorsArray.length === 0) {
-      return $neurons.map((n) => n.neuronId);
-    }
-
-    return [...$neurons]
-      .sort(mergeComparators(comparatorsArray))
-      .map((n) => n.neuronId);
-  }
+  ([$order, $neurons]) => sortNeuronIds($order, $neurons)
 );
