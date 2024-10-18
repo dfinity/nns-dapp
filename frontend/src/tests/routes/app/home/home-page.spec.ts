adc1c39757e2efb039ebdf6d5360fb7065d7f971
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-10-18 16:56:10 +0200

    Revert "Fix home-page.spec"
    This reverts commit 612d62fe3cc74d166f75230cc52996ab83e8cd7a.

--- frontend/src/tests/routes/app/home/home-page.spec.ts
+++ frontend/src/tests/routes/app/home/home-page.spec.ts
@@ -1,14 +1,16 @@
-import * as icrcLedgerApi from "$lib/api/icrc-ledger.api";
+import * as agent from "$lib/api/agent.api";
 import { overrideFeatureFlagsStore } from "$lib/stores/feature-flags.store";
 import HomePage from "$routes/(app)/(home)/+page.svelte";
-import { mockCkBTCToken } from "$tests/mocks/ckbtc-accounts.mock";
+import type { HttpAgent } from "@dfinity/agent";
 import { render } from "@testing-library/svelte";
+import { mock } from "vitest-mock-extended";
 
 describe("Home page", () => {
   beforeEach(() => {
     overrideFeatureFlagsStore.reset();
 
-    vi.spyOn(icrcLedgerApi, "queryIcrcToken").mockResolvedValue(mockCkBTCToken);
+    // TODO: agent mocked because some calls to global.fetch were exposed when we migrated to agent-js v0.20.2
+    vi.spyOn(agent, "createAgent").mockResolvedValue(mock<HttpAgent>());
   });
 
   describe("Tokens flag enabled", () => {
