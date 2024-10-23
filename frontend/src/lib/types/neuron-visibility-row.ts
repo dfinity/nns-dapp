06e3d6c2ba293566a2e00a5920355938cc87b358
Cosku Cinkilic <cosku.cinkilic@dfinity.org>
2024-10-23 17:02:47 +0200

    Add staked amount to neuronVisibilityRow (#5673)
    # Motivation
    
    The design of form has been changed and now includes the staked ICP in
    neuron visibility row.
    
    Still the rows needs to be sorted based on staked amount, this
    functionality added in a [seperate
    PR.](https://github.com/dfinity/nns-dapp/pull/5676)
    
    # Changes
    
    1. Add label color to AmountDisplay to control the color from parent.
    2. Add stake to NeuronVisibilityRowData and update
    createNeuronVisibilityRowData to provide stake data
    
    # Tests
    
    <img width="613" alt="Screenshot 2024-10-23 at 12 01 07"
    src="https://github.com/user-attachments/assets/ba166712-ab55-418b-8899-5f3e39310be5">
    
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    not necessary

--- frontend/src/lib/types/neuron-visibility-row.ts
+++ frontend/src/lib/types/neuron-visibility-row.ts
@@ -1,7 +1,10 @@
+import type { TokenAmountV2 } from "@dfinity/utils";
+
 export type NeuronVisibilityRowData = {
   neuronId: string;
   isPublic: boolean;
   tags: string[];
+  stake?: TokenAmountV2;
   uncontrolledNeuronDetails?: UncontrolledNeuronDetailsData;
 };
 
