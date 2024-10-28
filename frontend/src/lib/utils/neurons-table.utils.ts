38ef5da770ef9313f61d2e5dcdd68ac8e2da6e1d
Cosku Cinkilic <cosku.cinkilic@dfinity.org>
2024-10-28 16:08:00 +0300

    Add comment comparatorsByColumnId to explain why id is not there

--- frontend/src/lib/utils/neurons-table.utils.ts
+++ frontend/src/lib/utils/neurons-table.utils.ts
@@ -150,6 +150,9 @@ export const compareById = mergeComparators([
   createAscendingComparator((neuron: TableNeuron) => neuron.neuronId),
 ]);
 
+// The 'id' comparator is intentionally omitted from this mapping to prevent the Neurons column
+// from being sortable in the neurons table. This is a deliberate UX decision to avoid confusion,
+// as sorting by neuron ID typically doesn't provide meaningful information to users.
 export const comparatorsByColumnId: Partial<
   Record<NeuronsTableColumnId, TableNeuronComparator>
 > = {
