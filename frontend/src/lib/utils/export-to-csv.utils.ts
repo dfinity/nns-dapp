2e84862ee17960ca5a7d7c62bfa0fc41714f5d98
Yusef Habib <yusef.fernandez@dfinity.org>
2024-11-20 16:00:44 +0100

    NNS1-3450: Utility function to convert an object into a valid CSV string (#5815)
    # Motivation
    
    NNS1-3450 will require functionality to download .csv files.
    
    # Changes
    
    - New utility function to convert an object into a string in csv format.
    
    # Tests
    
    - Unit tests for the utility function
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    Not necessary.
    
    Nex PR: #5812

---
+++ frontend/src/lib/utils/export-to-csv.utils.ts
@@ -0,0 +1,47 @@
+import { isNullish } from "@dfinity/utils";
+
+const escapeCsvValue = (value: unknown): string => {
+  if (isNullish(value)) return "";
+
+  let stringValue = String(value);
+
+  const patternForSpecialCharacters = /[",\r\n=+-@|]/;
+  if (!patternForSpecialCharacters.test(stringValue)) {
+    return stringValue;
+  }
+
+  const formulaInjectionCharacters = "=+-@|";
+  const characterToBreakFormula = "'";
+  if (formulaInjectionCharacters.includes(stringValue[0])) {
+    stringValue = `${characterToBreakFormula}${stringValue}`;
+  }
+
+  const patternForCharactersToQuote = /[",\r\n]/;
+  if (patternForCharactersToQuote.test(stringValue)) {
+    stringValue = `"${stringValue.replace(/"/g, '""')}"`;
+  }
+
+  return stringValue;
+};
+
+export const convertToCsv = <T>({
+  data,
+  headers,
+}: {
+  data: T[];
+  headers: { id: keyof T }[];
+}) => {
+  if (headers.length === 0) return "";
+
+  const sanitizedHeaders = headers
+    .map(({ id }) => id)
+    .map((header) => escapeCsvValue(header));
+  const csvRows = [sanitizedHeaders.join(",")];
+
+  for (const row of data) {
+    const values = headers.map((header) => escapeCsvValue(row[header.id]));
+    csvRows.push(values.join(","));
+  }
+
+  return csvRows.join("\n");
+};
