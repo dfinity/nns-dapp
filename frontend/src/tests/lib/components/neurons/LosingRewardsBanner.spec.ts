a29e619ccb032a406631a97756134c9c80f463a6
Max Strasinsky <98811342+mstrasinskis@users.noreply.github.com>
2024-11-29 16:13:49 +0100

    Confirm following banner (#5881)
    # Motivation
    
    As part of the periodic confirmation feature, a banner should be
    displayed on the Staking and ICP Neurons pages whenever a neuron is
    losing or about to lose rewards. This implementation creates a banner to
    integrate it later into the specified pages. The button action will be
    addressed in the following PR.
    
    # Changes
    
    - New LosingRewardsBanner component.
    
    # Tests
    
    - Added.
    
    # Screenshots
    
    <img width="783" alt="image"
    src="https://github.com/user-attachments/assets/6b5d83cc-7865-48a4-b9a6-d686281b8167">
    <img width="783" alt="image"
    src="https://github.com/user-attachments/assets/784f69fd-5bc5-4635-a5a2-dbb9c283d891">
    <img width="288" alt="image"
    src="https://github.com/user-attachments/assets/d49dd1b6-547e-4f58-af45-d99881964085">
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    Not necessary.

---
+++ frontend/src/tests/lib/components/neurons/LosingRewardsBanner.spec.ts
@@ -0,0 +1,102 @@
+import LosingRewardsBanner from "$lib/components/neurons/LosingRewardsBanner.svelte";
+import { SECONDS_IN_DAY, SECONDS_IN_HALF_YEAR } from "$lib/constants/constants";
+import { neuronsStore } from "$lib/stores/neurons.store";
+import { nowInSeconds } from "$lib/utils/date.utils";
+import { mockFullNeuron, mockNeuron } from "$tests/mocks/neurons.mock";
+import { LosingRewardsBannerPo } from "$tests/page-objects/LosingRewardsBanner.page-object";
+import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
+import { runResolvedPromises } from "$tests/utils/timers.test-utils";
+import { render } from "@testing-library/svelte";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+
+describe("LosingRewardsBanner", () => {
+  const nowSeconds = nowInSeconds();
+  const activeNeuron = {
+    ...mockNeuron,
+    fullNeuron: {
+      ...mockFullNeuron,
+      votingPowerRefreshedTimestampSeconds: BigInt(nowSeconds),
+    },
+  };
+  const in10DaysLosingRewardsNeuron = {
+    ...mockNeuron,
+    fullNeuron: {
+      ...mockFullNeuron,
+      votingPowerRefreshedTimestampSeconds: BigInt(
+        nowSeconds - SECONDS_IN_HALF_YEAR + 10 * SECONDS_IN_DAY
+      ),
+    },
+  };
+  const losingRewardsNeuron = {
+    ...mockNeuron,
+    fullNeuron: {
+      ...mockFullNeuron,
+      votingPowerRefreshedTimestampSeconds: BigInt(
+        nowSeconds - SECONDS_IN_HALF_YEAR
+      ),
+    },
+  };
+
+  const renderComponent = () => {
+    const { container } = render(LosingRewardsBanner);
+    return LosingRewardsBannerPo.under(new JestPageObjectElement(container));
+  };
+
+  beforeEach(() => {
+    vi.useFakeTimers({
+      now: nowSeconds * 1000,
+    });
+  });
+
+  it("should not display banner when all neurons are active", async () => {
+    neuronsStore.setNeurons({
+      neurons: [activeNeuron],
+      certified: true,
+    });
+    const po = await renderComponent();
+    expect(await po.isVisible()).toBe(false);
+  });
+
+  it("should display banner when soon inactive neuron available", async () => {
+    neuronsStore.setNeurons({
+      neurons: [losingRewardsNeuron],
+      certified: true,
+    });
+    const po = await renderComponent();
+    expect(await po.isVisible()).toBe(true);
+  });
+
+  it("displays soon losing title ", async () => {
+    neuronsStore.setNeurons({
+      neurons: [activeNeuron, in10DaysLosingRewardsNeuron],
+      certified: true,
+    });
+    const po = await renderComponent();
+    expect(await po.getTitle()).toBe(
+      "10 days left to confirm your neuron following"
+    );
+  });
+
+  it("displays losing rewards title ", async () => {
+    neuronsStore.setNeurons({
+      neurons: [activeNeuron, in10DaysLosingRewardsNeuron, losingRewardsNeuron],
+      certified: true,
+    });
+    const po = await renderComponent();
+    expect(await po.getTitle()).toBe(
+      "One or more of your neurons are missing voting rewards"
+    );
+  });
+
+  it("displays description ", async () => {
+    neuronsStore.setNeurons({
+      neurons: [activeNeuron, in10DaysLosingRewardsNeuron],
+      certified: true,
+    });
+    const po = await renderComponent();
+    await runResolvedPromises();
+    expect(await po.getText()).toBe(
+      "ICP neurons that are inactive for 6 months start losing voting rewards. In order to avoid losing rewards, vote manually, edit or confirm your following."
+    );
+  });
+});
