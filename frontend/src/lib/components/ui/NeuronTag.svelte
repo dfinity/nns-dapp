9a263a9307c2671e3fb89aa76e63be1f7c2030f7
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-12-03 16:06:40 +0100

    Add size property

--- frontend/src/lib/components/ui/NeuronTag.svelte
+++ frontend/src/lib/components/ui/NeuronTag.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-  import TestIdWrapper from "$lib/components/common/TestIdWrapper.svelte";
   import type { NeuronTagData } from "$lib/utils/neuron.utils";
   import { Tag } from "@dfinity/gix-components";
 
   export let tag: NeuronTagData;
+  export let size: "medium" | "large" | undefined = undefined;
 </script>
 
-<TestIdWrapper testId="neuron-tag">
-  <Tag>{tag.text}</Tag>
-</TestIdWrapper>
+<Tag testId="neuron-tag-component" {size}>{tag.text}</Tag>
