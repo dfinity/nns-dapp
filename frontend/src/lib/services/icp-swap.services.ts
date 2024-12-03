fb796399e0fc702c66378541e8daea84c9808ecd
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2024-12-03 10:15:52 +0100

    Load ICP Swap tickers once per session (#5902)
    # Motivation
    
    We agree with PM to load exchange rates only once per session.
    
    # Changes
    
    1. In `loadIcpSwapTickers` if the store is already loaded, do nothing.
    
    # Tests
    
    1. Unit test added.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    not yet

--- frontend/src/lib/services/icp-swap.services.ts
+++ frontend/src/lib/services/icp-swap.services.ts
@@ -1,7 +1,12 @@
 import { queryIcpSwapTickers } from "$lib/api/icp-swap.api";
 import { icpSwapTickersStore } from "$lib/stores/icp-swap.store";
+import { get } from "svelte/store";
 
 export const loadIcpSwapTickers = async (): Promise<void> => {
+  if (get(icpSwapTickersStore) !== undefined) {
+    // We keep the existing tickers for the duration of the session.
+    return;
+  }
   try {
     const tickers = await queryIcpSwapTickers();
     icpSwapTickersStore.set(tickers);
