1de5c6fb7973145dad1eb7ea4225ccd6a8632a88
sa-github-api <138766536+sa-github-api@users.noreply.github.com>
2024-12-03 08:49:25 +0100

    bot: Update sns_aggregator candid bindings (#5900)
    # Motivation
    A newer release of the internet computer is available.
    We would like to parse all the latest SNS data. To do so, we update the
    candid interfaces for the SNS aggregator.
    Even with no changes, just updating the reference is good practice.
    
    # Changes
    * Update the version of `IC_COMMIT_FOR_SNS_AGGREGATOR` specified in
    `dfx.json`.
    * Updated the `sns_aggregator` candid files to the versions in that
    commit.
    * Updated the Rust code derived from `.did` files in the aggregator.
    
    # Tests
    - [ ] Please check the API updates for any breaking changes that affect
    our code.
      Breaking changes are:
        * New mandatory fields
        * Removing mandatory fields
        * Renaming fields
        * Changing the type of a field
        * Adding new variants
    
    Co-authored-by: gix-bot <gix-bot@users.noreply.github.com>

--- rs/sns_aggregator/src/types/ic_sns_swap.rs
+++ rs/sns_aggregator/src/types/ic_sns_swap.rs
@@ -1,5 +1,5 @@
 //! Rust code created from candid by: `scripts/did2rs.sh --canister sns_swap --out ic_sns_swap.rs --header did2rs.header --traits Serialize\,\ Clone\,\ Debug`
-//! Candid for canister `sns_swap` obtained by `scripts/update_ic_commit` from: <https://raw.githubusercontent.com/dfinity/ic/release-2024-11-21_03-11-24.04-base-kernel/rs/sns/swap/canister/swap.did>
+//! Candid for canister `sns_swap` obtained by `scripts/update_ic_commit` from: <https://raw.githubusercontent.com/dfinity/ic/release-2024-11-28_03-15-revert-hashes-in-blocks/rs/sns/swap/canister/swap.did>
 #![allow(clippy::all)]
 #![allow(unused_imports)]
 #![allow(missing_docs)]
