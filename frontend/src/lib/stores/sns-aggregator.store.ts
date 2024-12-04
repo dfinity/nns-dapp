eeb70c900353b58b15e9b764412a8cfdb5fb19d0
Yusef Habib Fernandez <yusef.fernandez@dfinity.org>
2024-12-04 12:09:29 +0100

    rename sns information at the snsAggregator level

--- frontend/src/lib/stores/sns-aggregator.store.ts
+++ frontend/src/lib/stores/sns-aggregator.store.ts
@@ -1,4 +1,7 @@
-import type { CachedSnsDto } from "$lib/types/sns-aggregator";
+import type {
+  CachedSnsDto,
+  CachedSnsTokenMetadataDto,
+} from "$lib/types/sns-aggregator";
 import { SnsSwapLifecycle } from "@dfinity/sns";
 import { nonNullish } from "@dfinity/utils";
 import { derived, writable, type Readable } from "svelte/store";
@@ -35,10 +38,52 @@ export const snsAggregatorIncludingAbortedProjectsStore =
 export const snsAggregatorStore: SnsAggregatorStore = derived(
   snsAggregatorIncludingAbortedProjectsStore,
   (store) => ({
-    data: store.data?.filter(
-      (sns) =>
-        nonNullish(sns.lifecycle) &&
-        sns.lifecycle.lifecycle !== SnsSwapLifecycle.Aborted
-    ),
+    data: store.data
+      ?.filter(
+        (sns) =>
+          nonNullish(sns.lifecycle) &&
+          sns.lifecycle.lifecycle !== SnsSwapLifecycle.Aborted
+      )
+      .map(fixBrokenSnsMetadataBasedOnId),
   })
 );
+
+// TODO: Find a better way to fix broken SNS metadata.
+const brokenSnsOverrides: Record<
+  string,
+  { name: string; tokenSymbol: string }
+> = {
+  "bd3sg-teaaa-aaaaa-qaaba-cai": {
+    name: "CYCLES_TRANSFER_STATION",
+    tokenSymbol: "CTS",
+  },
+};
+
+const fixBrokenSnsMetadataBasedOnId = (sns: CachedSnsDto): CachedSnsDto => {
+  const override = brokenSnsOverrides[sns.list_sns_canisters.root];
+  if (!nonNullish(override)) return sns;
+  const newMeta = {
+    ...sns.meta,
+    name: `${sns.meta.name} (formerly ${override.name})`,
+  };
+
+  const newIcrc1Metadata = sns.icrc1_metadata.map<
+    [string, CachedSnsTokenMetadataDto[0][1]]
+  >(([name, value]) => {
+    if (name === "icrc1:symbol" && "Text" in value) {
+      return [
+        name,
+        {
+          Text: `${value.Text} (${override.tokenSymbol})`,
+        },
+      ];
+    }
+    return [name, value];
+  });
+
+  return {
+    ...sns,
+    meta: { ...newMeta },
+    icrc1_metadata: [...newIcrc1Metadata],
+  };
+};
