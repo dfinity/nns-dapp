3c2c868b8a15e82a7f0b9d01dab12ab47ba1455a
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-12-05 11:05:05 +0100

    Update page objects

---
+++ frontend/src/tests/page-objects/LosingRewardNeuronsModal.page-object.ts
@@ -0,0 +1,17 @@
+import { ModalPo } from "$tests/page-objects/Modal.page-object";
+import type { PageObjectElement } from "$tests/types/page-object.types";
+import { NnsLosingRewardsNeuronCardPo } from "./NnsLosingRewardsNeuronCard.page-object";
+
+export class LosingRewardNeuronsModalPo extends ModalPo {
+  static readonly TID = "losing-reward-neurons-modal-component";
+
+  static under(element: PageObjectElement): LosingRewardNeuronsModalPo {
+    return new LosingRewardNeuronsModalPo(
+      element.byTestId(LosingRewardNeuronsModalPo.TID)
+    );
+  }
+
+  getNnsLosingRewardsNeuronCardPos(): Promise<NnsLosingRewardsNeuronCardPo[]> {
+    return NnsLosingRewardsNeuronCardPo.allUnder(this.root);
+  }
+}
