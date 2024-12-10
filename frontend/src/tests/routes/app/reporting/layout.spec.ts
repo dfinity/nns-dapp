69a2cd9da52a88b53ce57e311fe5b56dff06a574
Yusef Habib Fernandez <yusef.fernandez@dfinity.org>
2024-12-10 11:54:33 +0100

    redirect out if feature flag is off

--- frontend/src/tests/routes/app/reporting/layout.spec.ts
+++ frontend/src/tests/routes/app/reporting/layout.spec.ts
@@ -1,41 +1,66 @@
+import { goto } from "$app/navigation";
 import { OWN_CANISTER_ID_TEXT } from "$lib/constants/canister-ids.constants";
 import { AppPath } from "$lib/constants/routes.constants";
 import { pageStore } from "$lib/derived/page.derived";
+import { overrideFeatureFlagsStore } from "$lib/stores/feature-flags.store";
 import { referrerPathStore } from "$lib/stores/routes.store";
 import { page } from "$mocks/$app/stores";
-import Layout from "$routes/(app)/(nns)/settings/+layout.svelte";
+import Layout from "$routes/(app)/(nns)/reporting/+layout.svelte";
 import { fireEvent } from "@testing-library/dom";
 import { render, waitFor } from "@testing-library/svelte";
 import { get } from "svelte/store";
 
 describe("Layout", () => {
-  const renderReportingAndBack = () => {
+  vi.mock("$app/navigation", () => ({
+    goto: vi.fn(() => Promise.resolve()),
+  }));
+
+  const renderReporting = () => {
     page.mock({
       data: {
         universe: OWN_CANISTER_ID_TEXT,
       },
       routeId: AppPath.Reporting,
     });
-    const { queryByTestId } = render(Layout);
+
+    return render(Layout);
+  };
+
+  beforeEach(() => {
+    overrideFeatureFlagsStore.setFlag("ENABLE_EXPORT_NEURONS_REPORT", true);
+  });
+
+  it("should redirect to /accounts if feature flag is off", async () => {
+    overrideFeatureFlagsStore.setFlag("ENABLE_EXPORT_NEURONS_REPORT", false);
+
+    renderReporting();
+
+    expect(goto).toHaveBeenCalledWith(AppPath.Accounts);
+    expect(goto).toHaveBeenCalledTimes(1);
+  });
+
+  it("should stay in the page if feature flag is on", () => {
+    renderReporting();
+
+    expect(goto).not.toHaveBeenCalled();
+  });
+
+  it("should go back to the accounts page as fallback", async () => {
+    referrerPathStore.set(undefined);
+
+    const { queryByTestId } = renderReporting();
 
     const { path } = get(pageStore);
+
     expect(path).toEqual(AppPath.Reporting);
 
     const backButton = queryByTestId("back");
     expect(backButton).toBeInTheDocument();
 
     fireEvent.click(backButton);
-  };
 
-  it("should go back to the accounts page as fallback", async () => {
-    referrerPathStore.set(undefined);
-
-    renderReportingAndBack();
-
-    await waitFor(() => {
-      const { path } = get(pageStore);
-      expect(path).toEqual(AppPath.Accounts);
-    });
+    expect(goto).toHaveBeenCalledWith(AppPath.Accounts);
+    expect(goto).toHaveBeenCalledTimes(1);
   });
 
   it("should go back to referrer", async () => {
@@ -43,7 +68,16 @@ describe("Layout", () => {
 
     const spy = vi.spyOn(history, "back");
 
-    renderReportingAndBack();
+    const { queryByTestId } = renderReporting();
+
+    const { path } = get(pageStore);
+
+    expect(path).toEqual(AppPath.Reporting);
+
+    const backButton = queryByTestId("back");
+    expect(backButton).toBeInTheDocument();
+
+    fireEvent.click(backButton);
 
     await waitFor(() => expect(spy).toHaveBeenCalled());
   });
