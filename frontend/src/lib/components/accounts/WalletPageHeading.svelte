ca10c5ebf2e2d65001b15f37484b1a4dd69aad2b
David de Kloet <david.de.kloet@dfinity.org>
2024-12-10 18:25:54 +0100

    fix prettier-ignore

--- frontend/src/lib/components/accounts/WalletPageHeading.svelte
+++ frontend/src/lib/components/accounts/WalletPageHeading.svelte
@@ -94,6 +94,7 @@
     use:onIntersection
   >
     <HeadingSubtitle testId="wallet-page-heading-subtitle">
+      <!-- prettier-ignore -->
       <div class="subtitle">
         {#if $ENABLE_USD_VALUES}
           <div class="usd-balance" class:icp-swap-has-error={icpSwapHasError}>
@@ -109,9 +110,7 @@
             </TooltipIcon>
           </div>
           <div class="vertical-divider" />
-          <!-- prettier-ignore -->
-        {/if}
-        <div class="account-name">
+        {/if}<div class="account-name">
           {accountName}
         </div>
       </div>
