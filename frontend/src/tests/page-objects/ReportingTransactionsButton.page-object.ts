95644087a048ce4dd504fa10adb897aa7b812deb
Yusef Habib Fernandez <yusef.fernandez@dfinity.org>
2024-12-10 23:24:59 +0100

    changes to component

--- frontend/src/tests/page-objects/ReportingTransactionsButton.page-object.ts
+++ frontend/src/tests/page-objects/ReportingTransactionsButton.page-object.ts
@@ -2,7 +2,7 @@ import { ButtonPo } from "$tests/page-objects/Button.page-object";
 import type { PageObjectElement } from "$tests/types/page-object.types";
 
 export class ReportingTransactionsButtonPo extends ButtonPo {
-  static readonly TID = "export-icp-transactions-button-component";
+  static readonly TID = "reporting-transactions-button-component";
 
   static under({
     element,
