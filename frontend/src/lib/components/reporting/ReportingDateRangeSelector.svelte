77435582232f88c5db20ed8d881dc1c6acc86438
Yusef Habib Fernandez <yusef.fernandez@dfinity.org>
2024-12-16 22:26:32 +0100

    Styles

--- frontend/src/lib/components/reporting/ReportingDateRangeSelector.svelte
+++ frontend/src/lib/components/reporting/ReportingDateRangeSelector.svelte
@@ -19,61 +19,73 @@
 </script>
 
 <fieldset data-tid="reporting-data-range-selector-component">
-  <legend>{$i18n.reporting.range_filter_title}</legend>
-  <div role="radiogroup">
-    {#each options as option}
-      <label class="radio-option">
-        <input
-          type="radio"
-          name="dateRange"
-          value={option.value}
-          checked={selectedRange === option.value}
-          aria-checked={selectedRange === option.value}
-          on:change={() => handleChange(option.value)}
-        />
-        <span class="label">{option.label}</span>
-      </label>
-    {/each}
-  </div>
+  <div class="wrapper">
+    <legend>{$i18n.reporting.range_filter_title}</legend>
+    <div role="radiogroup">
+      {#each options as option}
+        <label class="radio-option">
+          <input
+            type="radio"
+            name="dateRange"
+            value={option.value}
+            checked={selectedRange === option.value}
+            aria-checked={selectedRange === option.value}
+            on:change={() => handleChange(option.value)}
+          />
+          <span class="label">{option.label}</span>
+        </label>
+      {/each}
+    </div></div
+  >
 </fieldset>
 
 <style lang="scss">
+  @use "@dfinity/gix-components/dist/styles/mixins/fonts";
+
   fieldset {
-    display: flex;
-    flex-direction: column;
-    gap: var(--padding-2x);
+    all: unset; // Reset all styles
 
-    legend {
-    }
-
-    div {
+    .wrapper {
       display: flex;
-      gap: var(--padding-3x);
+      flex-direction: column;
+      gap: var(--padding-2x);
+      border: none;
+      padding: 0;
+      margin: 0; /* Add this to remove default margin */
 
-      .radio-option {
+      legend {
+        @include fonts.h5;
+      }
+
+      div {
         display: flex;
-        align-items: center;
-        gap: var(--padding);
-        cursor: pointer;
+        gap: var(--padding-3x);
 
-        .label {
-          color: var(--text-primary);
-          font-size: var(--font-size-body);
-        }
-
-        input[type="radio"] {
-          appearance: none;
-          width: 18px;
-          height: 18px;
-          border: 2px solid var(--primary);
-          border-radius: 50%;
-          margin: 0;
+        .radio-option {
+          display: flex;
+          align-items: center;
+          gap: var(--padding);
           cursor: pointer;
-          position: relative;
-          background: transparent;
 
-          &:checked {
-            border: 5px solid var(--primary, #666);
+          .label {
+            color: var(--text-description);
+            font-size: var(--font-size-body);
+          }
+
+          input[type="radio"] {
+            appearance: none;
+            width: 18px;
+            height: 18px;
+            border: 2px solid var(--primary);
+            border-radius: 50%;
+            margin: 0;
+            cursor: pointer;
+            position: relative;
+            background: transparent;
+
+            &:checked {
+              border: 5px solid var(--primary, #666);
+            }
           }
         }
       }
