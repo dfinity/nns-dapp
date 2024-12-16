2f776017bfa9184e1c2b7b73c78e2bf9bb729193
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2024-12-16 15:50:06 +0100

    Support rendering stake in USD in NeuronsTable (#6018)
    # Motivation
    
    We want to show USD values in the neurons tables, similar to the tokens
    table and the projects table.
    
    This PR adds support for rendering the `stakeInUsd` field in the
    `NeuronsTable` components.
    
    Still missing (for future PRs):
    1. Passing `icpSwapUsdPrices` to `tableNeuronsFromNeuronInfos` and
    `tableNeuronsFromSnsNeurons`.
    2. Calling `loadIcpSwapTickers` on the neurons table page to make sure
    data is available.
    
    # Changes
    
    1. Render stake in USD in `NeuronStakeCell.svelte` if
    `ENABLE_USD_VALUES_FOR_NEURONS` is enabled.
    
    # Tests
    
    1. Unit tests added.
    2. Manually at
    https://qsgjb-riaaa-aaaaa-aaaga-cai.dskloet-ingress.devenv.dfinity.network/neurons/?u=qsgjb-riaaa-aaaaa-aaaga-cai
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    not yet

--- frontend/src/lib/components/neurons/NeuronsTable/NeuronStakeCell.svelte
+++ frontend/src/lib/components/neurons/NeuronsTable/NeuronStakeCell.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
   import AmountDisplay from "$lib/components/ic/AmountDisplay.svelte";
+  import AmountWithUsd from "$lib/components/ic/AmountWithUsd.svelte";
+  import { ENABLE_USD_VALUES_FOR_NEURONS } from "$lib/stores/feature-flags.store";
   import type { TableNeuron } from "$lib/types/neurons-table";
 
   export let rowData: TableNeuron;
 </script>
 
 <div data-tid="neuron-stake-cell-component" class="container">
-  <AmountDisplay singleLine amount={rowData.stake} />
+  {#if $ENABLE_USD_VALUES_FOR_NEURONS}
+    <AmountWithUsd amount={rowData.stake} amountInUsd={rowData.stakeInUsd} />
+  {:else}
+    <AmountDisplay singleLine amount={rowData.stake} />
+  {/if}
 </div>
 
 <style lang="scss">
