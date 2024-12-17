35cb27c9394f1f20c90968482add5273a04e3aef
Yusef Habib <yusef.fernandez@dfinity.org>
2024-12-17 11:56:04 +0100

    NNS1-3486: adds ReportingTransactions component to orchestrate dateRange (#6026)
    # Motivation
    
    Users can filter their export by choosing one of three options: all,
    this year, or last year.
    
    A follow up PR, will hooked both `ReportingRangeDateSelector` and
    `ReportingTransactionsButton` to download transactions for the selected
    period of time.
    
    # Changes
    
    - Adds a new component called `ReportingTransactions` to orchestrate
    downloading transactions for a period
    
    # Tests
    
    - Not necessary
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    Not necessary
    
    Prev. PR: #6019

---
+++ frontend/src/lib/components/reporting/ReportingTransactions.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { i18n } from "$lib/stores/i18n";
+  import type { ReportingDateRange } from "$lib/types/reporting";
+  import ReportingTransactionsButton from "./ReportingTransactionsButton.svelte";
+
+  // TODO: This will be hooked up in a follow up
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  let selectedRange: ReportingDateRange = "all";
+</script>
+
+<div class="wrapper">
+  <div>
+    <h3>{$i18n.reporting.transactions_title}</h3>
+    <p class="description">{$i18n.reporting.transactions_description}</p>
+  </div>
+  <ReportingTransactionsButton />
+</div>
+
+<style lang="scss">
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: var(--padding-3x);
+  }
+</style>
