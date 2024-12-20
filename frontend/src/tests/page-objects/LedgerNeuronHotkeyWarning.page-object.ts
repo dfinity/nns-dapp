dd321bb49d712196fb160d4659d2f6183e0c0e26
Max Strasinsky <maksims.strasinskis@dfinity.org>
2024-12-20 17:03:56 +0100

    test: LedgerNeuronHotkeyWarning

---
+++ frontend/src/tests/page-objects/LedgerNeuronHotkeyWarning.page-object.ts
@@ -0,0 +1,21 @@
+import { BasePageObject } from "$tests/page-objects/base.page-object";
+import type { PageObjectElement } from "$tests/types/page-object.types";
+import { BannerPo } from "./Banner.page-object";
+
+export class LedgerNeuronHotkeyWarningPo extends BasePageObject {
+  private static readonly TID = "ledger-neuron-hotkey-warning-component";
+
+  static under(element: PageObjectElement): LedgerNeuronHotkeyWarningPo {
+    return new LedgerNeuronHotkeyWarningPo(
+      element.byTestId(LedgerNeuronHotkeyWarningPo.TID)
+    );
+  }
+
+  getBannerPo(): BannerPo {
+    return BannerPo.under(this.root);
+  }
+
+  async isBannerVisible(): Promise<boolean> {
+    return this.getBannerPo().isPresent();
+  }
+}
