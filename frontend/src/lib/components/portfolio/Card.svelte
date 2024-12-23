b8cb3a3f09d62de153b237dc670ff572f0d650e2
Yusef Habib Fernandez <yusef.fernandez@dfinity.org>
2024-12-23 10:44:18 +0100

    A UI Component card

---
+++ frontend/src/lib/components/portfolio/Card.svelte
@@ -0,0 +1,14 @@
+<article><slot /></article>
+
+<style lang="scss">
+  article {
+    height: auto;
+    box-sizing: border-box;
+    background: var(--card-background);
+    /* Designs use a different shadow */
+    box-shadow: var(--box-shadow);
+    transition: color var(--animation-time-normal);
+    /*  Designs define it as 16px */
+    border-radius: var(--border-radius);
+  }
+</style>
