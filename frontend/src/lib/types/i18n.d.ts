2f2aea78f4a4188f38c8cbf408d71277d942524e
Yusef Habib Fernandez <yusef.fernandez@dfinity.org>
2024-12-23 10:51:12 +0100

    new LoginCard for portfolio page

--- frontend/src/lib/types/i18n.d.ts
+++ frontend/src/lib/types/i18n.d.ts
@@ -1254,6 +1254,11 @@ interface I18nImport_token {
   doc_link_label: string;
 }
 
+interface I18nPortfolio {
+  login_title: string;
+  login_description: string;
+}
+
 interface I18nNeuron_state {
   Unspecified: string;
   Locked: string;
@@ -1544,6 +1549,7 @@ interface I18n {
   sync: I18nSync;
   tokens: I18nTokens;
   import_token: I18nImport_token;
+  portfolio: I18nPortfolio;
   neuron_state: I18nNeuron_state;
   topics: I18nTopics;
   topics_description: I18nTopics_description;
