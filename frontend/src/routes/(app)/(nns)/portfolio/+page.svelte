336215a313895d041bfe9986f2dc6533a13f7082
Yusef Habib Fernandez <yusef.fernandez@dfinity.org>
2024-12-27 16:45:38 +0100

    fetch tokens

--- frontend/src/routes/(app)/(nns)/portfolio/+page.svelte
+++ frontend/src/routes/(app)/(nns)/portfolio/+page.svelte
@@ -1,6 +1,28 @@
 <script lang="ts">
   import TestIdWrapper from "$lib/components/common/TestIdWrapper.svelte";
+  import { authSignedInStore } from "$lib/derived/auth.derived";
+  import { ckBTCUniversesStore } from "$lib/derived/ckbtc-universes.derived";
+  import { icrcCanistersStore } from "$lib/derived/icrc-canisters.derived";
+  import { snsProjectsCommittedStore } from "$lib/derived/sns/sns-projects.derived";
   import Portfolio from "$lib/pages/Portfolio.svelte";
+  import { balanceLoader } from "$lib/services/accounts-balances.services";
+  import { loadCkBTCTokens } from "$lib/services/ckbtc-tokens.services";
+  import { loadIcpSwapTickers } from "$lib/services/icp-swap.services";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    balanceLoader.reset();
+    loadCkBTCTokens();
+    loadIcpSwapTickers();
+  });
+
+  $: if ($authSignedInStore) {
+    balanceLoader.loadAllBalances({
+      snsProjects: $snsProjectsCommittedStore,
+      ckBTCUniverses: $ckBTCUniversesStore,
+      icrcCanisters: $icrcCanistersStore,
+    });
+  }
 </script>
 
 <TestIdWrapper testId="portfolio-route-component"><Portfolio /></TestIdWrapper>
