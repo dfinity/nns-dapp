f800f41719479c21153c4fcba5523a1a2a91c5cd
David de Kloet <david.de.kloet@dfinity.org>
2025-01-06 10:18:10 +0100

    import

--- frontend/src/tests/e2e/neurons-table.spec.ts
+++ frontend/src/tests/e2e/neurons-table.spec.ts
@@ -2,6 +2,7 @@ import { AppPo } from "$tests/page-objects/App.page-object";
 import { PlaywrightPageObjectElement } from "$tests/page-objects/playwright.page-object";
 import {
   replaceContent,
+  setFeatureFlag,
   signInWithNewUser,
   step,
 } from "$tests/utils/e2e.test-utils";
