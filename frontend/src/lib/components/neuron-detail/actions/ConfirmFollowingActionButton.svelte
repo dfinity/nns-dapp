313bca7ca6c1d7544fba4bede395900d57af0547
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-01-07 15:22:34 +0100

    Add ConfirmFollowingActionButton component

---
+++ frontend/src/lib/components/neuron-detail/actions/ConfirmFollowingActionButton.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { i18n } from "$lib/stores/i18n";
+  import { openNnsNeuronModal } from "$lib/utils/modals.utils";
+  import type { NeuronInfo } from "@dfinity/nns";
+
+  export let neuron: NeuronInfo;
+</script>
+
+<button
+  data-tid="confirm-following-action-button-component"
+  class="secondary"
+  on:click={() =>
+    openNnsNeuronModal({
+      type: "confirm-following",
+      data: { neuron },
+    })}>{$i18n.losing_rewards.confirm}</button
+>
