a9da3e001097cf2eaf09efad1eadc7d799c19767
Yusef Habib Fernandez <yusef.fernandez@dfinity.org>
2025-01-09 14:38:22 +0100

    pass values from route to page

--- frontend/src/routes/(app)/(nns)/portfolio/+page.svelte
+++ frontend/src/routes/(app)/(nns)/portfolio/+page.svelte
@@ -2,8 +2,11 @@
   import TestIdWrapper from "$lib/components/common/TestIdWrapper.svelte";
   import { authSignedInStore } from "$lib/derived/auth.derived";
   import { ckBTCUniversesStore } from "$lib/derived/ckbtc-universes.derived";
+  import { icpSwapUsdPricesStore } from "$lib/derived/icp-swap.derived";
   import { icrcCanistersStore } from "$lib/derived/icrc-canisters.derived";
+  import { selectableUniversesStore } from "$lib/derived/selectable-universes.derived";
   import { snsProjectsCommittedStore } from "$lib/derived/sns/sns-projects.derived";
+  import { tokensListUserStore } from "$lib/derived/tokens-list-user.derived";
   import Portfolio from "$lib/pages/Portfolio.svelte";
   import {
     loadAccountsBalances,
@@ -12,6 +15,11 @@
   } from "$lib/services/accounts-balances.services";
   import { loadCkBTCTokens } from "$lib/services/ckbtc-tokens.services";
   import { loadIcpSwapTickers } from "$lib/services/icp-swap.services";
+  import { neuronsStore } from "$lib/stores/neurons.store";
+  import { snsNeuronsStore } from "$lib/stores/sns-neurons.store";
+  import type { TableProject } from "$lib/types/staking";
+  import type { UserToken } from "$lib/types/tokens-page";
+  import { getTableProjects } from "$lib/utils/staking.utils";
   import { onMount } from "svelte";
 
   onMount(() => {
@@ -19,6 +27,9 @@
     loadCkBTCTokens();
     loadIcpSwapTickers();
   });
+
+  let userTokensData: UserToken[] = [];
+  let tableProjects: TableProject[] = [];
 
   $: if ($authSignedInStore) {
     const ckBTCUniverseIds = $ckBTCUniversesStore.map(
@@ -33,7 +44,18 @@
       ({ rootCanisterId }) => rootCanisterId
     );
     loadSnsAccountsBalances(snsRootCanisterIds);
+
+    userTokensData = $tokensListUserStore;
+    tableProjects = getTableProjects({
+      universes: $selectableUniversesStore,
+      isSignedIn: $authSignedInStore,
+      nnsNeurons: $neuronsStore?.neurons,
+      snsNeurons: $snsNeuronsStore,
+      icpSwapUsdPrices: $icpSwapUsdPricesStore,
+    });
   }
 </script>
 
-<TestIdWrapper testId="portfolio-route-component"><Portfolio /></TestIdWrapper>
+<TestIdWrapper testId="portfolio-route-component"
+  ><Portfolio {userTokensData} {tableProjects} /></TestIdWrapper
+>
