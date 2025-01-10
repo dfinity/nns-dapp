ac54b1a32a779b1d95bef1600c5b0921d42a5945
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-01-10 12:10:54 +0100

    Display VP in expandable

--- frontend/src/lib/components/neuron-detail/NnsNeuronVotingPowerSection.svelte
+++ frontend/src/lib/components/neuron-detail/NnsNeuronVotingPowerSection.svelte
@@ -16,7 +16,7 @@
   import NnsNeuronDissolveDelayItemAction from "./NnsNeuronDissolveDelayItemAction.svelte";
   import NnsNeuronStateItemAction from "./NnsNeuronStateItemAction.svelte";
   import NnsStakeItemAction from "./NnsStakeItemAction.svelte";
-  import { Html, Section } from "@dfinity/gix-components";
+  import { Html, KeyValuePairInfo, Section } from "@dfinity/gix-components";
   import type { NeuronInfo } from "@dfinity/nns";
   import { ENABLE_PERIODIC_FOLLOWING_CONFIRMATION } from "$lib/stores/feature-flags.store";
 
@@ -29,61 +29,69 @@
 </script>
 
 <Section testId="nns-neuron-voting-power-section-component">
-  <h3 slot="title">{$i18n.neurons.voting_power}</h3>
-  <p slot="end" class="title-value" data-tid="voting-power">
-    {#if canVote}
-      {formatVotingPower(neuron.decidingVotingPower ?? 0n)}
-    {:else}
-      {$i18n.neuron_detail.voting_power_zero}
-    {/if}
-  </p>
-  <svelte:fragment slot="description">
-    {#if canVote}
-      <p class="description">
-        {$i18n.neuron_detail.calculated_as}
-      </p>
-      <p
-        class="description calculation"
-        data-tid="voting-power-generic-description"
-      >
-        {$ENABLE_PERIODIC_FOLLOWING_CONFIRMATION
-          ? $i18n.neuron_detail.voting_power_section_calculation_generic_new
-          : $i18n.neuron_detail.voting_power_section_calculation_generic}
-      </p>
-      <p class="description">
-        {$i18n.neuron_detail.this_neuron_calculation}
-      </p>
-      <p class="description calculation" data-tid="voting-power-description">
-        {replacePlaceholders(
-          $ENABLE_PERIODIC_FOLLOWING_CONFIRMATION
-            ? $i18n.neuron_detail.voting_power_section_calculation_specific_new
-            : $i18n.neuron_detail.voting_power_section_calculation_specific,
-          {
-            $stake: formatTokenE8s({
-              value: neuronStake(neuron),
-            }),
-            $maturityStaked: formattedStakedMaturity(neuron),
-            $ageMultiplier: ageMultiplier(neuron.ageSeconds).toFixed(2),
-            $dissolveMultiplier: dissolveDelayMultiplier(
-              neuron.dissolveDelaySeconds
-            ).toFixed(2),
-            $activityMultiplier: activityMultiplier(neuron).toFixed(2),
-            $votingPower: formatVotingPower(neuron.decidingVotingPower ?? 0n),
-          }
-        )}
-      </p>
-    {:else}
-      <p class="description" data-tid="voting-power-description">
-        <Html
-          text={replacePlaceholders(
-            $i18n.neuron_detail
-              .voting_power_section_description_expanded_zero_nns,
-            { $dashboardLink: neuronDashboardUrl(neuron) }
-          )}
-        />
-      </p>
-    {/if}
-  </svelte:fragment>
+  <KeyValuePairInfo slot="description">
+    <h3 class="" slot="key">{$i18n.neurons.voting_power}</h3>
+    <p slot="value" class="title-value" data-tid="voting-power">
+      {#if canVote}
+        {formatVotingPower(neuron.decidingVotingPower ?? 0n)}
+      {:else}
+        {$i18n.neuron_detail.voting_power_zero}
+      {/if}
+    </p>
+
+    <span class="info" slot="info">
+      {#if canVote}
+        <span class="info-item">
+          <span class="label">
+            {$i18n.neuron_detail.calculated_as}
+          </span>
+          <span class="calculation" data-tid="voting-power-generic-description">
+            {$ENABLE_PERIODIC_FOLLOWING_CONFIRMATION
+              ? $i18n.neuron_detail.voting_power_section_calculation_generic_new
+              : $i18n.neuron_detail.voting_power_section_calculation_generic}
+          </span>
+        </span>
+        <span class="info-item">
+          <span class="label">
+            {$i18n.neuron_detail.this_neuron_calculation}
+          </span>
+          <span class="calculation" data-tid="voting-power-description">
+            {replacePlaceholders(
+              $ENABLE_PERIODIC_FOLLOWING_CONFIRMATION
+                ? $i18n.neuron_detail
+                    .voting_power_section_calculation_specific_new
+                : $i18n.neuron_detail.voting_power_section_calculation_specific,
+              {
+                $stake: formatTokenE8s({
+                  value: neuronStake(neuron),
+                }),
+                $maturityStaked: formattedStakedMaturity(neuron),
+                $ageMultiplier: ageMultiplier(neuron.ageSeconds).toFixed(2),
+                $dissolveMultiplier: dissolveDelayMultiplier(
+                  neuron.dissolveDelaySeconds
+                ).toFixed(2),
+                $activityMultiplier: activityMultiplier(neuron).toFixed(2),
+                $votingPower: formatVotingPower(
+                  neuron.decidingVotingPower ?? 0n
+                ),
+              }
+            )}
+          </span>
+        </span>
+      {:else}
+        <span data-tid="voting-power-description">
+          <Html
+            text={replacePlaceholders(
+              $i18n.neuron_detail
+                .voting_power_section_description_expanded_zero_nns,
+              { $dashboardLink: neuronDashboardUrl(neuron) }
+            )}
+          />
+        </span>
+      {/if}
+    </span>
+  </KeyValuePairInfo>
+
   <ul class="content">
     <NnsStakeItemAction {neuron} />
     <NnsNeuronStateItemAction {neuron} />
@@ -95,6 +103,8 @@
 </Section>
 
 <style lang="scss">
+  @use "@dfinity/gix-components/dist/styles/mixins/fonts";
+
   h3 {
     margin: 0;
   }
@@ -111,8 +121,27 @@
     padding: 0;
   }
 
-  .calculation {
-    font-family: monospace;
-    font-size: 12px;
+  .info,
+  .info-item {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .info {
+    @include fonts.small;
+    gap: var(--padding-2x);
+
+    .info-item {
+      gap: var(--padding-0_5x);
+    }
+
+    .label {
+      color: var(--text-color);
+    }
+
+    .calculation {
+      font-family: monospace;
+      font-size: 12px;
+    }
   }
 </style>
