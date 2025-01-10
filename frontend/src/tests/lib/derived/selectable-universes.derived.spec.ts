e038cc7c137ff34b428da1589b736cae867c51c6
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2025-01-10 08:36:07 +0100

    NNS1-3504: Sort selectable universes by actionable proposals (#6131)
    # Motivation
    
    On the voting page we want to sort the universes (both in the selector
    and in the actionable proposals) based on the number of actionably
    proposals or otherwise alphabetically.
    
    Both the selector and the actionable proposals sections are rendered
    based on `selectableUniversesStore`.
    (This store is also used for other things, such as [finding the
    currently selected
    universe](https://github.com/dfinity/nns-dapp/blob/58b8d349c2912b678f69b1060342430f6992fb12/frontend/src/lib/components/accounts/IcrcWalletPage.svelte#L166),
    which is why it can still also contain non-voting universes such as
    ckBTC.)
    
    # Changes
    
    1. Sort `selectableUniversesStore` based on actionable proposal count
    and title.
    
    # Tests
    
    1. Simplify the existing tests for `selectableUniversesStore`. Because
    of the re-ordering, the tests were confusing in their previous form.
    2. Add unit tests with multiple SNS projects.
    3. Adapt `ActionableProposals.spec.ts` to the new ordering.
    4. Manually tested at
    https://qsgjb-riaaa-aaaaa-aaaga-cai.dskloet-ingress.devenv.dfinity.network/proposals/?actionable
    
    # Todos
    
    - [x] Add entry to changelog (if necessary).

--- frontend/src/tests/lib/derived/selectable-universes.derived.spec.ts
+++ frontend/src/tests/lib/derived/selectable-universes.derived.spec.ts
@@ -1,18 +1,15 @@
 import { OWN_CANISTER_ID } from "$lib/constants/canister-ids.constants";
-import { CKBTC_UNIVERSE_CANISTER_ID } from "$lib/constants/ckbtc-canister-ids.constants";
-import { CKETH_UNIVERSE_CANISTER_ID } from "$lib/constants/cketh-canister-ids.constants";
 import { AppPath } from "$lib/constants/routes.constants";
 import { selectableUniversesStore } from "$lib/derived/selectable-universes.derived";
-import { snsProjectsCommittedStore } from "$lib/derived/sns/sns-projects.derived";
+import { actionableSnsProposalsStore } from "$lib/stores/actionable-sns-proposals.store";
 import { page } from "$mocks/$app/stores";
-import {
-  mockProjectSubscribe,
-  mockSnsFullProject,
-} from "$tests/mocks/sns-projects.mock";
+import { principal } from "$tests/mocks/sns-projects.mock";
+import { mockSnsProposal } from "$tests/mocks/sns-proposals.mock";
 import {
   resetCkETHCanisters,
   setCkETHCanisters,
 } from "$tests/utils/cketh.test-utils";
+import { setSnsProjects } from "$tests/utils/sns.test-utils";
 import { get } from "svelte/store";
 
 describe("selectable universes derived stores", () => {
@@ -27,64 +24,125 @@ describe("selectable universes derived stores", () => {
 
   it("should return Nns, ckBTC and ckTESTBTC (flag for test is true) per default", () => {
     const store = get(selectableUniversesStore);
-    expect(store.length).toEqual(3);
-    expect(store[0].summary).toBeUndefined();
-    expect(store[0].canisterId).toEqual(OWN_CANISTER_ID.toText());
-    expect(store[1].summary).toBeUndefined();
-    expect(store[1].canisterId).toEqual(CKBTC_UNIVERSE_CANISTER_ID.toText());
+    expect(store.map(({ title }) => title)).toEqual([
+      "Internet Computer",
+      "ckBTC",
+      "ckTESTBTC",
+    ]);
   });
 
   it("should return CkETH in Accounts page", () => {
     setCkETHCanisters();
     const store = get(selectableUniversesStore);
-    expect(store.length).toEqual(4);
-    expect(store[3].summary).toBeUndefined();
-    expect(store[3].canisterId).toEqual(CKETH_UNIVERSE_CANISTER_ID.toText());
+    expect(store.map(({ title }) => title)).toEqual([
+      "Internet Computer",
+      "ckBTC",
+      "ckETH",
+      "ckTESTBTC",
+    ]);
   });
 
-  it("should not return ckBTC if path is not Account", () => {
+  it("should not return ckBTC or ckETH if path is not Account", () => {
     page.mock({
       routeId: AppPath.Neurons,
       data: { universe: OWN_CANISTER_ID.toText() },
     });
 
     const store = get(selectableUniversesStore);
-    // 1 length = only NNS
-    expect(store.length).toEqual(1);
-    expect(store[0].canisterId).not.toEqual(
-      CKBTC_UNIVERSE_CANISTER_ID.toText()
-    );
-  });
-
-  it("should not return ckETH if path is not Account", () => {
-    page.mock({
-      routeId: AppPath.Neurons,
-      data: { universe: OWN_CANISTER_ID.toText() },
-    });
-    setCkETHCanisters();
-
-    const store = get(selectableUniversesStore);
-    // 1 length = only NNS
-    expect(store.length).toEqual(1);
-    expect(store[0].canisterId).not.toEqual(
-      CKBTC_UNIVERSE_CANISTER_ID.toText()
-    );
+    expect(store.map(({ title }) => title)).toEqual(["Internet Computer"]);
   });
 
   describe("with projects", () => {
-    beforeEach(() => {
-      vi.spyOn(snsProjectsCommittedStore, "subscribe").mockImplementation(
-        mockProjectSubscribe([mockSnsFullProject])
-      );
-    });
-
     it("should return Nns, ckBTC, ckTESTBTC (flag for test is true) and another project", () => {
+      setSnsProjects([
+        {
+          projectName: "SNS Project",
+        },
+      ]);
       const store = get(selectableUniversesStore);
       expect(store.length).toEqual(4);
-      expect(store[3].summary).not.toBeUndefined();
-      expect(store[3].canisterId).toEqual(
-        mockSnsFullProject.rootCanisterId.toText()
-      );
+      expect(store.map(({ title }) => title)).toEqual([
+        "Internet Computer",
+        "ckBTC",
+        "ckTESTBTC",
+        "SNS Project",
+      ]);
+    });
+
+    it("should return NNS followed by SNS projects ordered alphabetically", () => {
+      page.mock({
+        routeId: AppPath.Proposals,
+        data: { universe: OWN_CANISTER_ID.toText() },
+      });
+
+      const rootCanisterId1 = principal(1);
+      const rootCanisterId2 = principal(2);
+      const rootCanisterId3 = principal(3);
+
+      setSnsProjects([
+        {
+          rootCanisterId: rootCanisterId1,
+          projectName: "Bravo",
+        },
+        {
+          rootCanisterId: rootCanisterId2,
+          projectName: "Alfa",
+        },
+        {
+          rootCanisterId: rootCanisterId3,
+          projectName: "Charlie",
+        },
+      ]);
+
+      expect(get(selectableUniversesStore).map(({ title }) => title)).toEqual([
+        "Internet Computer",
+        "Alfa",
+        "Bravo",
+        "Charlie",
+      ]);
+    });
+
+    it("should return NNS followed by SNS projects in reverse order of actionable proposals", () => {
+      page.mock({
+        routeId: AppPath.Proposals,
+        data: { universe: OWN_CANISTER_ID.toText() },
+      });
+
+      const rootCanisterIdA = principal(1);
+      const rootCanisterIdB = principal(2);
+      const rootCanisterIdC = principal(3);
+
+      setSnsProjects([
+        {
+          rootCanisterId: rootCanisterIdB,
+          projectName: "Bravo",
+        },
+        {
+          rootCanisterId: rootCanisterIdA,
+          projectName: "Alfa",
+        },
+        {
+          rootCanisterId: rootCanisterIdC,
+          projectName: "Charlie",
+        },
+      ]);
+
+      actionableSnsProposalsStore.set({
+        rootCanisterId: rootCanisterIdC,
+        proposals: [mockSnsProposal, mockSnsProposal],
+      });
+
+      actionableSnsProposalsStore.set({
+        rootCanisterId: rootCanisterIdB,
+        proposals: [mockSnsProposal],
+      });
+
+      expect(get(selectableUniversesStore).map(({ title }) => title)).toEqual([
+        "Internet Computer",
+        "Charlie",
+        "Bravo",
+        "Alfa",
+      ]);
     });
   });
 });
