827d41739fa348bf85342461047b78eb82044dc6
Yusef Habib Fernandez <yusef.fernandez@dfinity.org>
2025-01-10 16:14:14 +0100

    add projects

--- frontend/src/routes/(app)/(nns)/portfolio/+page.svelte
+++ frontend/src/routes/(app)/(nns)/portfolio/+page.svelte
@@ -2,7 +2,9 @@
   import TestIdWrapper from "$lib/components/common/TestIdWrapper.svelte";
   import { authSignedInStore } from "$lib/derived/auth.derived";
   import { ckBTCUniversesStore } from "$lib/derived/ckbtc-universes.derived";
+  import { icpSwapUsdPricesStore } from "$lib/derived/icp-swap.derived";
   import { icrcCanistersStore } from "$lib/derived/icrc-canisters.derived";
+  import { selectableUniversesStore } from "$lib/derived/selectable-universes.derived";
   import { snsProjectsCommittedStore } from "$lib/derived/sns/sns-projects.derived";
   import { tokensListUserStore } from "$lib/derived/tokens-list-user.derived";
   import Portfolio from "$lib/pages/Portfolio.svelte";
@@ -12,6 +14,9 @@
     resetBalanceLoading,
   } from "$lib/services/accounts-balances.services";
   import { loadIcpSwapTickers } from "$lib/services/icp-swap.services";
+  import { neuronsStore } from "$lib/stores/neurons.store";
+  import { snsNeuronsStore } from "$lib/stores/sns-neurons.store";
+  import { getTableProjects } from "$lib/utils/staking.utils";
 
   resetBalanceLoading();
   loadIcpSwapTickers();
@@ -37,5 +42,14 @@
 </script>
 
 <TestIdWrapper testId="portfolio-route-component"
-  ><Portfolio userTokensData={$tokensListUserStore} /></TestIdWrapper
+  ><Portfolio
+    userTokensData={$tokensListUserStore}
+    tableProjects={getTableProjects({
+      universes: $selectableUniversesStore,
+      isSignedIn: $authSignedInStore,
+      nnsNeurons: $neuronsStore?.neurons,
+      snsNeurons: $snsNeuronsStore,
+      icpSwapUsdPrices: $icpSwapUsdPricesStore,
+    })}
+  /></TestIdWrapper
 >
