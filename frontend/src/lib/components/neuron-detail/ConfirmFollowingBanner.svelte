41ea85453cbb70c756f666d6504d9f1a2bdafa0b
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-01-15 17:54:43 +0100

    Switch to economics in ConfirmFollowingBanner

--- frontend/src/lib/components/neuron-detail/ConfirmFollowingBanner.svelte
+++ frontend/src/lib/components/neuron-detail/ConfirmFollowingBanner.svelte
@@ -4,21 +4,25 @@
   import Banner from "$lib/components/ui/Banner.svelte";
   import BannerIcon from "$lib/components/ui/BannerIcon.svelte";
   import { replacePlaceholders } from "$lib/utils/i18n.utils";
-  import { START_REDUCING_VOTING_POWER_AFTER_SECONDS } from "$lib/constants/neurons.constants";
   import { secondsToDissolveDelayDuration } from "$lib/utils/date.utils";
+  import { startReducingVotingPowerAfterSecondsStore } from "$lib/derived/network-economics.derived";
 
   let title: string;
   $: title = $i18n.losing_rewards_banner.confirm_title;
-
-  const text = replacePlaceholders($i18n.losing_rewards.description, {
-    $period: secondsToDissolveDelayDuration(
-      BigInt(START_REDUCING_VOTING_POWER_AFTER_SECONDS)
-    ),
-  });
 </script>
 
-<Banner testId="confirm-following-banner-component" {title} {text}>
-  <BannerIcon slot="icon" status="error">
-    <IconErrorOutline />
-  </BannerIcon>
-</Banner>
+{#if $startReducingVotingPowerAfterSecondsStore}
+  <Banner
+    testId="confirm-following-banner-component"
+    {title}
+    text={replacePlaceholders($i18n.losing_rewards.description, {
+      $period: secondsToDissolveDelayDuration(
+        $startReducingVotingPowerAfterSecondsStore
+      ),
+    })}
+  >
+    <BannerIcon slot="icon" status="error">
+      <IconErrorOutline />
+    </BannerIcon>
+  </Banner>
+{/if}
