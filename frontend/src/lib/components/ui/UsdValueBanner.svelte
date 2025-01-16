71dc46f9b4e2c5d2d3b3398eb7656e845324d13f
Yusef Habib Fernandez <yusef.fernandez@dfinity.org>
2025-01-16 14:13:28 +0100

    refactor to use headless

--- frontend/src/lib/components/ui/UsdValueBanner.svelte
+++ frontend/src/lib/components/ui/UsdValueBanner.svelte
@@ -1,66 +1,48 @@
 <script lang="ts">
   import IcpExchangeRate from "$lib/components/ui/IcpExchangeRate.svelte";
   import TooltipIcon from "$lib/components/ui/TooltipIcon.svelte";
-  import { LEDGER_CANISTER_ID } from "$lib/constants/canister-ids.constants";
-  import { icpSwapUsdPricesStore } from "$lib/derived/icp-swap.derived";
   import { i18n } from "$lib/stores/i18n";
-  import { formatNumber } from "$lib/utils/format.utils";
-  import { isNullish, nonNullish } from "@dfinity/utils";
+  import UsdValueHeadless from "./UsdValueHeadless.svelte";
 
   export let usdAmount: number | undefined;
   export let hasUnpricedTokens: boolean;
 
   const absentValue = "-/-";
-
-  let hasError: boolean;
-  $: hasError = $icpSwapUsdPricesStore === "error";
-
-  let hasPrices: boolean;
-  $: hasPrices = !hasError && nonNullish($icpSwapUsdPricesStore);
-
-  let usdAmountFormatted: string;
-  $: usdAmountFormatted =
-    nonNullish(usdAmount) && hasPrices ? formatNumber(usdAmount) : absentValue;
-
-  let icpPrice: number | undefined;
-  $: icpPrice =
-    isNullish($icpSwapUsdPricesStore) || $icpSwapUsdPricesStore === "error"
-      ? undefined
-      : $icpSwapUsdPricesStore[LEDGER_CANISTER_ID.toText()];
-
-  let icpAmount: number | undefined;
-  $: icpAmount = icpPrice && usdAmount && usdAmount / icpPrice;
-
-  let icpAmountFormatted: string;
-  $: icpAmountFormatted = nonNullish(icpAmount)
-    ? formatNumber(icpAmount)
-    : absentValue;
 </script>
 
-<div class="wrapper" data-tid="usd-value-banner-component">
-  <div class="table-banner-icon">
-    <slot name="icon" />
-  </div>
-  <div class="text-content">
-    <div class="totals">
-      <div class="primary-amount-row">
-        <span class="primary-amount" data-tid="primary-amount">
-          ${usdAmountFormatted}
-        </span>
-        {#if hasPrices && hasUnpricedTokens}
-          <TooltipIcon>
-            {$i18n.accounts.unpriced_tokens_warning}
-          </TooltipIcon>
-        {/if}
-      </div>
-      <div class="secondary-amount" data-tid="secondary-amount">
-        {icpAmountFormatted}
-        {$i18n.core.icp}
-      </div>
+<UsdValueHeadless
+  {usdAmount}
+  {hasUnpricedTokens}
+  let:icpPrice
+  let:usdAmountFormatted
+  let:icpAmountFormatted
+  let:hasError
+  let:hasPricesAndUnpricedTokens
+  ><div class="wrapper" data-tid="usd-value-banner-component">
+    <div class="table-banner-icon">
+      <slot name="icon" />
     </div>
-    <IcpExchangeRate {icpPrice} {hasError} {absentValue} />
+    <div class="text-content">
+      <div class="totals">
+        <div class="primary-amount-row">
+          <span class="primary-amount" data-tid="primary-amount">
+            ${usdAmountFormatted}
+          </span>
+          {#if hasPricesAndUnpricedTokens}
+            <TooltipIcon>
+              {$i18n.accounts.unpriced_tokens_warning}
+            </TooltipIcon>
+          {/if}
+        </div>
+        <div class="secondary-amount" data-tid="secondary-amount">
+          {icpAmountFormatted}
+          {$i18n.core.icp}
+        </div>
+      </div>
+      <IcpExchangeRate {icpPrice} {hasError} {absentValue} />
+    </div>
   </div>
-</div>
+</UsdValueHeadless>
 
 <style lang="scss">
   @use "@dfinity/gix-components/dist/styles/mixins/media";
