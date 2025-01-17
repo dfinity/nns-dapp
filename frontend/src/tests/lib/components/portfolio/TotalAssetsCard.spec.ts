4bbe64bb912e6964d02d779ec2d022b4a14d8e7e
Yusef Habib <yusef.fernandez@dfinity.org>
2025-01-17 10:55:55 +0100

    feat(portfolio): implement TotalAssetsCard component (#6170)
    # Motivation
    
    We want to display the total value of the user's portfolio on the
    Portfolio page.
    
    # Changes
    
    - Adds the `TotalAssetsCard` component.
    
    # Tests
    
    - Unit tests for the component  
    -  New page object for the component 
    - Manually tested in [E2E
    environment](https://qsgjb-riaaa-aaaaa-aaaga-cai.yhabib-ingress.devenv.dfinity.network/)
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    Not necessary

---
+++ frontend/src/tests/lib/components/portfolio/TotalAssetsCard.spec.ts
@@ -0,0 +1,152 @@
+import TotalAssetsCard from "$lib/components/portfolio/TotalAssetsCard.svelte";
+import { CKUSDC_UNIVERSE_CANISTER_ID } from "$lib/constants/ckusdc-canister-ids.constants";
+import { icpSwapTickersStore } from "$lib/stores/icp-swap.store";
+import en from "$tests/mocks/i18n.mock";
+import { mockIcpSwapTicker } from "$tests/mocks/icp-swap.mock";
+import { TotalAssetsCardPo } from "$tests/page-objects/TotalAssetsCard.page-object";
+import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
+import { render } from "$tests/utils/svelte.test-utils";
+
+describe("UsdValueBanner", () => {
+  const renderComponent = ({
+    usdAmount,
+    hasUnpricedTokens,
+  }: {
+    usdAmount: number;
+    hasUnpricedTokens: boolean;
+  }) => {
+    const { container } = render(TotalAssetsCard, {
+      usdAmount,
+      hasUnpricedTokens,
+    });
+    return TotalAssetsCardPo.under(new JestPageObjectElement(container));
+  };
+
+  const setIcpPrice = (icpPrice: number) => {
+    icpSwapTickersStore.set([
+      {
+        ...mockIcpSwapTicker,
+        base_id: CKUSDC_UNIVERSE_CANISTER_ID.toText(),
+        last_price: String(icpPrice),
+      },
+    ]);
+  };
+
+  it("should display the USD amount as absent", async () => {
+    const usdAmount = undefined;
+    const po = renderComponent({ usdAmount, hasUnpricedTokens: false });
+
+    expect(await po.getPrimaryAmount()).toEqual("$-/-");
+  });
+
+  it("should display the USD amount", async () => {
+    const usdAmount = 50;
+    const icpPrice = 10;
+
+    setIcpPrice(icpPrice);
+
+    const po = renderComponent({ usdAmount, hasUnpricedTokens: false });
+
+    expect(await po.getPrimaryAmount()).toEqual("$50.00");
+  });
+
+  it("should display the ICP amount", async () => {
+    const usdAmount = 50;
+    const icpPrice = 10;
+
+    setIcpPrice(icpPrice);
+
+    const po = renderComponent({ usdAmount, hasUnpricedTokens: false });
+
+    expect(await po.getSecondaryAmount()).toEqual("5.00 ICP");
+  });
+
+  it("should display the ICP amount as absent without exchange rates", async () => {
+    const usdAmount = 50;
+    const po = renderComponent({ usdAmount, hasUnpricedTokens: false });
+
+    expect(await po.getSecondaryAmount()).toEqual("-/- ICP");
+  });
+
+  it("should display the ICP price", async () => {
+    const usdAmount = 50;
+    const icpPrice = 10;
+
+    setIcpPrice(icpPrice);
+
+    const po = renderComponent({ usdAmount, hasUnpricedTokens: false });
+
+    expect(await po.getIcpPrice()).toEqual("10.00");
+  });
+
+  it("should display the ICP price as absent without exchange rates", async () => {
+    const usdAmount = 50;
+    const po = renderComponent({ usdAmount, hasUnpricedTokens: false });
+
+    expect(await po.getIcpPrice()).toEqual("-/-");
+  });
+
+  it("should display the ICP price in the tooltip", async () => {
+    const usdAmount = 50;
+    const icpPrice = 10;
+
+    setIcpPrice(icpPrice);
+
+    const po = renderComponent({ usdAmount, hasUnpricedTokens: false });
+    const message = `1 ICP = $10.00 ${en.accounts.token_price_source}`;
+
+    expect(await po.getIcpExchangeRatePo().getTooltipText()).toEqual(message);
+  });
+
+  it("should not have an error by default", async () => {
+    const usdAmount = 50;
+    const icpPrice = 10;
+
+    setIcpPrice(icpPrice);
+
+    const po = renderComponent({ usdAmount, hasUnpricedTokens: false });
+
+    expect(await po.hasError()).toBe(false);
+  });
+
+  it("should have an error without ICP price", async () => {
+    const usdAmount = 50;
+    const icpPrice = 0;
+
+    setIcpPrice(icpPrice);
+
+    const po = renderComponent({ usdAmount, hasUnpricedTokens: false });
+    const message = en.accounts.token_price_error;
+
+    expect(await po.hasError()).toBe(true);
+    expect(await po.getIcpExchangeRatePo().getTooltipText()).toEqual(message);
+  });
+
+  it("should show a tooltip about unpriced tokens", async () => {
+    const hasUnpricedTokens = true;
+    const usdAmount = 50;
+    const icpPrice = 10;
+
+    setIcpPrice(icpPrice);
+
+    const po = renderComponent({ usdAmount, hasUnpricedTokens });
+
+    expect(await po.getTotalsTooltipIconPo().isPresent()).toBe(
+      hasUnpricedTokens
+    );
+  });
+
+  it("should not show a tooltip about unpriced tokens", async () => {
+    const hasUnpricedTokens = false;
+    const usdAmount = 50;
+    const icpPrice = 10;
+
+    setIcpPrice(icpPrice);
+
+    const po = renderComponent({ usdAmount, hasUnpricedTokens });
+
+    expect(await po.getTotalsTooltipIconPo().isPresent()).toBe(
+      hasUnpricedTokens
+    );
+  });
+});
