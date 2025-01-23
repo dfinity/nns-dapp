3a8a1a733eaa43e05de182f97be6212cf5b89d3b
Yusef Habib Fernandez <yusef.fernandez@dfinity.org>
2025-01-23 17:21:32 +0100

    fmt

--- frontend/src/tests/e2e/multi-tab-auth.spec.ts
+++ frontend/src/tests/e2e/multi-tab-auth.spec.ts
@@ -1,9 +1,9 @@
 import { AppPo } from "$tests/page-objects/App.page-object";
 import { PlaywrightPageObjectElement } from "$tests/page-objects/playwright.page-object";
 import {
-    setFeatureFlag,
-    signInWithNewUser,
-    step,
+  setFeatureFlag,
+  signInWithNewUser,
+  step,
 } from "$tests/utils/e2e.test-utils";
 import { expect, test } from "@playwright/test";
 
