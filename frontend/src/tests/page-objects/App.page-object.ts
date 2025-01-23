578c498916ec1f3de131ad558b705928a687825f
Yusef Habib Fernandez <yusef.fernandez@dfinity.org>
2025-01-23 17:22:07 +0100

    relative import

--- frontend/src/tests/page-objects/App.page-object.ts
+++ frontend/src/tests/page-objects/App.page-object.ts
@@ -9,6 +9,7 @@ import { LaunchpadPo } from "$tests/page-objects/Launchpad.page-object";
 import { MenuItemsPo } from "$tests/page-objects/MenuItems.page-object";
 import { NeuronDetailPo } from "$tests/page-objects/NeuronDetail.page-object";
 import { NeuronsPo } from "$tests/page-objects/Neurons.page-object";
+import { PortfolioRoutePo } from "$tests/page-objects/PortfolioRoute.page-object";
 import { ProjectDetailPo } from "$tests/page-objects/ProjectDetail.page-object";
 import { ProposalDetailPo } from "$tests/page-objects/ProposalDetail.page-object";
 import { ProposalsPo } from "$tests/page-objects/Proposals.page-object";
@@ -22,7 +23,6 @@ import { WalletPo } from "$tests/page-objects/Wallet.page-object";
 import { BasePageObject } from "$tests/page-objects/base.page-object";
 import { isNullish } from "@dfinity/utils";
 import { expect } from "@playwright/test";
-import { PortfolioRoutePo } from "./PortfolioRoute.page-object";
 
 export class AppPo extends BasePageObject {
   getSignInPo(): SignInPo {
