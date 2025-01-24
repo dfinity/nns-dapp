d82a621df32bb4476a789825bc62dcb65a997243
Yusef Habib Fernandez <yusef.fernandez@dfinity.org>
2025-01-24 13:09:50 +0100

    back button in nns account

--- frontend/src/routes/(app)/(u)/(list)/accounts/+layout.svelte
+++ frontend/src/routes/(app)/(u)/(list)/accounts/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { goto } from "$app/navigation";
+  import { afterNavigate, goto } from "$app/navigation";
   import Content from "$lib/components/layout/Content.svelte";
   import IslandWidthMain from "$lib/components/layout/IslandWidthMain.svelte";
   import Layout from "$lib/components/layout/Layout.svelte";
@@ -7,7 +7,13 @@
   import { AppPath } from "$lib/constants/routes.constants";
   import { accountsTitleStore } from "$lib/derived/accounts-title.derived";
 
-  const back = (): Promise<void> => goto(AppPath.Tokens);
+  let previousPath: string | null = null;
+
+  afterNavigate(({ from }) => {
+    if (from) previousPath = from.url.pathname;
+  });
+
+  const back = () => goto(previousPath || AppPath.Tokens);
 </script>
 
 <LayoutList title={$accountsTitleStore}>
