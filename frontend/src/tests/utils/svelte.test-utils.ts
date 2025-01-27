8fc3f6ec201ea965d16af08edd565ca77906182f
David Dal Busco <david.dalbusco@dfinity.org>
2025-01-27 08:36:39 +0100

    test: add event to render utils
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- frontend/src/tests/utils/svelte.test-utils.ts
+++ frontend/src/tests/utils/svelte.test-utils.ts
@@ -1,16 +1,41 @@
-import { render as svelteRender } from "@testing-library/svelte";
+import {
+  render as svelteRender,
+  type RenderResult,
+} from "@testing-library/svelte";
+import {
+  type Component,
+  type ComponentProps,
+  type SvelteComponent as LegacyComponent,
+} from "svelte";
+
+// Duplicate Testing-library ComponentType which is not exposed
+type ComponentType<C> = C extends LegacyComponent
+  ? // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    new (...args: any[]) => C
+  : C;
 
 // Adapted from Svelte render to work around the surprising behavior that render
 // reuses the same container element between different calls from the same test.
-export const render = (
-  component,
-  componentOptions = {},
-  renderOptions = {}
-) => {
+export const render = <C extends Component>(
+  component: ComponentType<C>,
+  props?: ComponentProps<C>,
+  renderOptions = {},
+  events?: Record<string, ($event: CustomEvent) => void>
+): RenderResult<C> => {
   const container = document.createElement("div");
   document.body.appendChild(container);
-  return svelteRender(component, componentOptions, {
-    ...renderOptions,
-    baseElement: container,
-  });
+  return svelteRender(
+    component,
+    {
+      props: props ?? {},
+      // TODO: remove once events are migrated to callback props
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      events,
+    },
+    {
+      ...renderOptions,
+      baseElement: container,
+    }
+  );
 };
