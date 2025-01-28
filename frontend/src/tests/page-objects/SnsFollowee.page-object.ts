7286a8889c5ce90bbe1e498684695cc64cb55221
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2025-01-28 11:56:17 +0100

    Use page objects in SnsNeuronFollowingCard.spec.ts (#6267)
    # Motivation
    
    Make the test easier to read and maintain.
    
    # Changes
    
    1. Update page objects.
    2. Change `SnsNeuronFollowingCard.spec.ts` to use page objects.
    
    # Tests
    
    1. Test still passes.
    2. Test now also passes with the change from
    https://github.com/dfinity/nns-dapp/pull/6263
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    not necessary

--- frontend/src/tests/page-objects/SnsFollowee.page-object.ts
+++ frontend/src/tests/page-objects/SnsFollowee.page-object.ts
@@ -1,5 +1,6 @@
-import { BasePageObject } from "$tests/page-objects/base.page-object";
 import { HashPo } from "$tests/page-objects/Hash.page-object";
+import { TagPo } from "$tests/page-objects/Tag.page-object";
+import { BasePageObject } from "$tests/page-objects/base.page-object";
 import type { PageObjectElement } from "$tests/types/page-object.types";
 
 export class SnsFolloweePo extends BasePageObject {
@@ -9,7 +10,17 @@ export class SnsFolloweePo extends BasePageObject {
     return new SnsFolloweePo(element.byTestId(SnsFolloweePo.TID));
   }
 
+  static async allUnder(element: PageObjectElement): Promise<SnsFolloweePo[]> {
+    return Array.from(await element.allByTestId(SnsFolloweePo.TID)).map(
+      (el) => new SnsFolloweePo(el)
+    );
+  }
+
   getHashPo(): HashPo {
     return HashPo.under(this.root);
+  }
+
+  getTagPos(): Promise<TagPo[]> {
+    return TagPo.allUnder(this.root);
   }
 }
