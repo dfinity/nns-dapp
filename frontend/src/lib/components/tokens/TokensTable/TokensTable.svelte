0b8be872edfb2731132ccc5cf3401bef34726176
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2025-01-28 19:12:38 +0100

    Support sorting in TokensTable (#6273)
    # Motivation
    
    We want to allow people to sort the tokens table based on name or
    balance.
    The tokens table is used on the tokens page but also on the ICP accounts
    page.
    On the accounts page we don't want to enable sorting.
    
    This PR supports sorting in the tokens table be specifying the `order`
    prop on the `TokensTable` component.
    We should not specify this prop on the accounts page.
    On the tokens page we would use the `$tokensTableOrderStore` as the
    `order prop to persist the order between navigation.
    
    BUT! We can not enable this on the tokens page yet either because the
    sort button on mobile interferes with the settings button for "hide zero
    balances".
    
    So this PR only supports the functionality in the component but does not
    yet enable it on the page.
    
    # Changes
    
    1. Support sorting based on either balance or title in the `TokensTable`
    component, if the `order` prop is specified to indicate the order that
    should be used.
    
    # Tests
    
    1. Unit tests added.
    2. Tested manually after passing `$tokensTableOrderStore` as the `order`
    prop.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    not yet

--- frontend/src/lib/components/tokens/TokensTable/TokensTable.svelte
+++ frontend/src/lib/components/tokens/TokensTable/TokensTable.svelte
@@ -4,23 +4,48 @@
   import TokenTitleCell from "$lib/components/tokens/TokensTable/TokenTitleCell.svelte";
   import ResponsiveTable from "$lib/components/ui/ResponsiveTable.svelte";
   import { i18n } from "$lib/stores/i18n";
+  import { importedTokensStore } from "$lib/stores/imported-tokens.store";
   import type { TokensTableColumn, UserToken } from "$lib/types/tokens-page";
+  import type { TokensTableOrder } from "$lib/types/tokens-page";
+  import {
+    compareTokensAlphabetically,
+    compareTokensByBalance,
+  } from "$lib/utils/tokens-table.utils";
 
   export let userTokensData: Array<UserToken>;
   export let firstColumnHeader: string;
+  export let order: TokensTableOrder = [];
 
-  const columns: TokensTableColumn[] = [
+  let enableSorting: boolean;
+  $: enableSorting = order.length > 0;
+
+  let importedTokenIds: Set<string> = new Set();
+  $: importedTokenIds = new Set(
+    ($importedTokensStore.importedTokens ?? []).map(({ ledgerCanisterId }) =>
+      ledgerCanisterId.toText()
+    )
+  );
+
+  let columns: TokensTableColumn[];
+
+  $: columns = [
     {
+      id: "title",
       title: firstColumnHeader,
       cellComponent: TokenTitleCell,
       alignment: "left",
       templateColumns: ["1fr"],
+      comparator: enableSorting ? compareTokensAlphabetically : undefined,
     },
     {
+      id: "balance",
       title: $i18n.tokens.balance_header,
       cellComponent: TokenBalanceCell,
       alignment: "right",
       templateColumns: ["max-content"],
+      comparator: enableSorting
+        ? compareTokensByBalance({ importedTokenIds })
+        : undefined,
     },
     {
       title: "",
@@ -35,6 +60,7 @@
   testId="tokens-table-component"
   tableData={userTokensData}
   {columns}
+  bind:order
   on:nnsAction
 >
   <slot name="last-row" slot="last-row" />
