d5d149b0c2ab34923afbc813340669136b92b423
Yusef Habib Fernandez <yusef.fernandez@dfinity.org>
2025-01-29 22:53:53 +0100

    force certified call when getting icp-xdr conversion

--- frontend/src/tests/lib/api/canisters.api.spec.ts
+++ frontend/src/tests/lib/api/canisters.api.spec.ts
@@ -219,7 +219,9 @@ describe("canisters-api", () => {
       mockCMCCanister.getIcpToCyclesConversionRate.mockResolvedValue(10_000n);
 
       const response = await getIcpToCyclesExchangeRate(mockIdentity);
-      expect(mockCMCCanister.getIcpToCyclesConversionRate).toBeCalled();
+      expect(mockCMCCanister.getIcpToCyclesConversionRate).toHaveBeenCalledWith(
+        { certified: true }
+      );
       expect(response).toEqual(10_000n);
     });
   });
