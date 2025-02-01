81e83c15d6dd2e80b76b1235b670bc2db68170bf
Yusef Habib Fernandez <yusef.fernandez@dfinity.org>
2025-02-01 10:09:52 +0100

    fix time tests

--- frontend/src/tests/lib/components/reporting/ReportingNeuronsButton.spec.ts
+++ frontend/src/tests/lib/components/reporting/ReportingNeuronsButton.spec.ts
@@ -143,7 +143,7 @@ describe("ReportingNeuronsButton", () => {
 
     const expectedCsv = [
       "NNS Account Principal ID,xlmdg-vkosz-ceopx-7wtgu-g3xmd-koiyc-awqaq-7modz-zf6r6-364rh-oqe",
-      'Export Date Time,"Oct 14, 2023 12:00 AM"',
+      'Export Date Time,"Oct 14, 2023 12:00 AM"',
       "",
       ",,Neuron ID,Project Name,Symbol,Neuron Account ID,Controller Principal ID,Stake,Available Maturity,Staked Maturity,Dissolve Delay,Dissolve Date,Creation Date,State",
       ',,"\'=""1""",Internet Computer,ICP,d0654c53339c85e0e5fff46a2d800101bc3d896caef34e1a0597426792ff9f32,1,30.00,0.0000001,0,"3 hours, 5 minutes",N/A,"Jan 1, 1970",Locked',
--- frontend/src/tests/lib/components/reporting/ReportingTransactionsButton.spec.ts
+++ frontend/src/tests/lib/components/reporting/ReportingTransactionsButton.spec.ts
@@ -138,8 +138,8 @@ describe("ReportingTransactionsButton", () => {
       'Export Date Time,"Oct 14, 2023 12:00 AM"',
       "",
       ",,TX ID,Project Name,Symbol,To,From,TX Type,Amount(ICP),Date Time",
-      ',,1234,Internet Computer,ICP,d0654c53339c85e0e5fff46a2d800101bc3d896caef34e1a0597426792ff9f32,d4685b31b51450508aff0331584df7692a84467b680326f5c5f7d30ae711682f,Sent,-1.0001,"Jan 1, 2023 12:00 AM"',
-      ',,1,Internet Computer,ICP,d0654c53339c85e0e5fff46a2d800101bc3d896caef34e1a0597426792ff9f32,d4685b31b51450508aff0331584df7692a84467b680326f5c5f7d30ae711682f,Sent,-1.0001,"Jan 1, 2023 12:00 AM"',
+      ',,1234,Internet Computer,ICP,d0654c53339c85e0e5fff46a2d800101bc3d896caef34e1a0597426792ff9f32,d4685b31b51450508aff0331584df7692a84467b680326f5c5f7d30ae711682f,Sent,-1.0001,"Jan 1, 2023 12:00 AM"',
+      ',,1,Internet Computer,ICP,d0654c53339c85e0e5fff46a2d800101bc3d896caef34e1a0597426792ff9f32,d4685b31b51450508aff0331584df7692a84467b680326f5c5f7d30ae711682f,Sent,-1.0001,"Jan 1, 2023 12:00 AM"',
     ].join("\n");
 
     expect(spySaveGeneratedCsv).toBeCalledWith(
