f54e86895a6b03915bb1ceee982bc2828cc77242
David Dal Busco <david.dalbusco@dfinity.org>
2025-02-04 06:07:26 +0100

    chore: try without animate again
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- frontend/vitest.setup.ts
+++ frontend/vitest.setup.ts
@@ -94,10 +94,6 @@ global.ResizeObserver = class ResizeObserver {
   }
 };
 
-Element.prototype.animate = vi
-  .fn()
-  .mockImplementation(() => ({ cancel: vi.fn(), finished: Promise.resolve() }));
-
 // In Gix-components and OISY we "only" mock Element.prototype.animate but, this is insufficent for the existing test suite of NNS-dapp.
 // Transition set as |global lead the test suite to fail with errors such as "TypeError: Cannot set properties of undefined (setting 'onfinish')".
 // That is why we are mocking all transition as well.
