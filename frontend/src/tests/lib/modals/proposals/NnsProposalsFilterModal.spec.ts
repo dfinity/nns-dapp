28621cd6e69bf971d76d51fc7a4305a679df7856
Max Strasinsky <98811342+mstrasinskis@users.noreply.github.com>
2025-02-06 10:18:46 +0100

    PO for NnsProposalsFilterModal.spec.ts (#6355)
    # Motivation
    
    Use PO in `NnsProposalsFilterModal` specs to streamline testing and
    simplify future changes.
    
    # Changes
    
    - Use FilterModalPo.
    
    # Tests
    
    - Yes
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    Not necessary.

--- frontend/src/tests/lib/modals/proposals/NnsProposalsFilterModal.spec.ts
+++ frontend/src/tests/lib/modals/proposals/NnsProposalsFilterModal.spec.ts
@@ -4,10 +4,10 @@ import { proposalsFiltersStore } from "$lib/stores/proposals.store";
 import type { ProposalsFilterModalProps } from "$lib/types/proposals";
 import { enumKeys } from "$lib/utils/enum.utils";
 import en from "$tests/mocks/i18n.mock";
+import { FilterModalPo } from "$tests/page-objects/FilterModal.page-object";
+import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
 import { render } from "$tests/utils/svelte.test-utils";
-import { clickByTestId } from "$tests/utils/utils.test-utils";
 import { Topic } from "@dfinity/nns";
-import { fireEvent, waitFor } from "@testing-library/svelte";
 import { get } from "svelte/store";
 
 describe("ProposalsFilterModal", () => {
@@ -19,121 +19,111 @@ describe("ProposalsFilterModal", () => {
     },
   };
 
-  it("should display modal", () => {
+  const renderComponent = (onNnsClose?: () => void): FilterModalPo => {
     const { container } = render(ProposalsFilterModal, {
       props,
+      events: { nnsClose: onNnsClose },
     });
 
-    expect(container.querySelector("div.modal")).not.toBeNull();
+    return FilterModalPo.under(new JestPageObjectElement(container));
+  };
+
+  it("should display modal", async () => {
+    const po = renderComponent();
+
+    expect(await po.isPresent()).toBe(true);
   });
 
-  it("should render title", () => {
-    const { getByText } = render(ProposalsFilterModal, {
-      props,
-    });
+  it("should render title", async () => {
+    const po = renderComponent();
 
-    expect(getByText(en.voting.topics)).toBeInTheDocument();
+    expect(await po.getModalTitle()).toBe("Topics");
   });
 
-  it("should render checkboxes", () => {
-    const { getByText } = render(ProposalsFilterModal, {
-      props,
-    });
-
-    enumKeys(Topic)
+  it("should render checkboxes", async () => {
+    const po = renderComponent();
+    const checkboxes = await po.getFilterEntryPos();
+    const checkboxLabels = (
+      await Promise.all(checkboxes.map((checkbox) => checkbox.getText()))
+    ).map((s) => s.trim());
+    const expectedLabels = enumKeys(Topic)
       .filter(
         (key: string) =>
           key !== "Unspecified" && key !== "SnsDecentralizationSale"
       )
-      .forEach((key: string) =>
-        expect(getByText(en.topics[key])).toBeInTheDocument()
-      );
+      .map((key: string) => en.topics[key]);
+
+    expect(checkboxLabels).toEqual(expectedLabels);
   });
 
-  it("should not render filter Unspecified", () => {
-    const { getByText } = render(ProposalsFilterModal, {
-      props,
-    });
+  it("should not render filter Unspecified", async () => {
+    const po = renderComponent();
+    const checkboxes = await po.getFilterEntryPos();
+    const checkboxLabels = (
+      await Promise.all(checkboxes.map((checkbox) => checkbox.getText()))
+    ).map((s) => s.trim());
 
-    expect(() => getByText(en.topics.Unspecified)).toThrow();
+    expect(checkboxLabels).not.contain(en.topics.Unspecified);
   });
 
-  it("should forward close modal event", () =>
-    new Promise<void>((done) => {
-      const { container } = render(ProposalsFilterModal, {
-        props,
-        events: {
-          nnsClose: () => done(),
-        },
-      });
-
-      const button: HTMLButtonElement | null = container.querySelector(
-        "button:first-of-type"
-      );
-
-      button && fireEvent.click(button);
-    }));
+  it("should forward close modal event", async () => {
+    const onNnsClose = vitest.fn();
+    const po = renderComponent(onNnsClose);
+    expect(onNnsClose).toHaveBeenCalledTimes(0);
+    await po.clickCloseButton();
+    expect(onNnsClose).toHaveBeenCalledTimes(1);
+  });
 
   it("should filter filters", async () => {
-    const { queryAllByTestId, queryByTestId } = render(ProposalsFilterModal, {
-      props,
-    });
+    const po = renderComponent();
+    const checkboxes = await po.getFilterEntryPos();
 
-    const inputs = queryAllByTestId("checkbox") as HTMLInputElement[];
-    const firstInput = inputs[0];
-    fireEvent.click(firstInput);
-    await waitFor(() => expect(firstInput.checked).toBeTruthy());
+    const firstInput = checkboxes[0];
+    await firstInput.click();
+    expect(await firstInput.isChecked()).toBe(true);
 
-    const secondInput = inputs[1];
-    fireEvent.click(secondInput);
-    await waitFor(() => expect(secondInput.checked).toBeTruthy());
+    const secondInput = checkboxes[1];
+    await secondInput.click();
+    expect(await secondInput.isChecked()).toBe(true);
 
-    await clickByTestId(queryByTestId, "apply-filters");
+    expect(get(proposalsFiltersStore).topics).toEqual([]);
 
-    const selectedTopics = get(proposalsFiltersStore).topics;
+    await po.clickConfirmButton();
 
-    expect(selectedTopics).toEqual([...DEFAULT_PROPOSALS_FILTERS.topics, 1, 2]);
+    expect(get(proposalsFiltersStore).topics).toEqual([
+      ...DEFAULT_PROPOSALS_FILTERS.topics,
+      1,
+      2,
+    ]);
   });
 
   it("should select all filters", async () => {
-    const { queryAllByTestId, queryByTestId } = render(ProposalsFilterModal, {
-      props,
-    });
+    const po = renderComponent();
+    const checkboxes = await po.getFilterEntryPos();
+    const getCheckboxStates = () =>
+      Promise.all(checkboxes.map((checkbox) => checkbox.isChecked()));
 
-    const inputs = queryAllByTestId("checkbox") as HTMLInputElement[];
-    await waitFor(() =>
-      expect(inputs.reduce((acc, input) => acc && input.checked, true)).toBe(
-        false
-      )
+    expect((await getCheckboxStates()).length).toEqual(
+      // Unspecified and SnsDecentralizationSale are not displayed
+      enumKeys(Topic).length - 2
     );
+    expect(await getCheckboxStates()).not.include(true);
 
-    await clickByTestId(queryByTestId, "filter-modal-select-all");
+    await po.clickSelectAllButton();
 
-    const inputs2 = queryAllByTestId("checkbox") as HTMLInputElement[];
-    await waitFor(() =>
-      expect(inputs2.reduce((acc, input) => acc && input.checked, true)).toBe(
-        true
-      )
-    );
+    expect(await getCheckboxStates()).not.include(false);
   });
 
   it("should clear all filters", async () => {
-    const { queryAllByTestId, queryByTestId } = render(ProposalsFilterModal, {
-      props,
-    });
+    const po = renderComponent();
+    const checkboxes = await po.getFilterEntryPos();
+    const getCheckboxStates = () =>
+      Promise.all(checkboxes.map((checkbox) => checkbox.isChecked()));
 
-    const inputs = queryAllByTestId("checkbox") as HTMLInputElement[];
-    const firstInput = inputs[0];
-    fireEvent.click(firstInput);
-    await waitFor(() => expect(firstInput.checked).toBe(true));
+    await po.clickSelectAllButton();
+    expect(await getCheckboxStates()).not.include(false);
 
-    await clickByTestId(queryByTestId, "filter-modal-clear");
-
-    const inputs2 = queryAllByTestId("checkbox") as HTMLInputElement[];
-    await waitFor(() =>
-      expect(inputs2.reduce((acc, input) => acc || input.checked, false)).toBe(
-        false
-      )
-    );
+    await po.clickClearSelectionButton();
+    expect(await getCheckboxStates()).not.include(true);
   });
 });
