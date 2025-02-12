b87951dad2669b936c1b73e4fe917f6120996c4c
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-02-12 11:10:32 +0100

    test: "renders topics in specific order"

--- frontend/src/tests/lib/components/neurons/EditFollowNeurons.spec.ts
+++ frontend/src/tests/lib/components/neurons/EditFollowNeurons.spec.ts
@@ -1,6 +1,8 @@
 import * as governanceApi from "$lib/api/governance.api";
 import EditFollowNeurons from "$lib/components/neurons/EditFollowNeurons.svelte";
+import { DEPRECATED_TOPICS } from "$lib/constants/proposals.constants";
 import { neuronsStore } from "$lib/stores/neurons.store";
+import { enumValues } from "$lib/utils/enum.utils";
 import { resetIdentity } from "$tests/mocks/auth.store.mock";
 import { mockFullNeuron, mockNeuron } from "$tests/mocks/neurons.mock";
 import { EditFollowNeuronsPo } from "$tests/page-objects/EditFollowNeurons.page-object";
@@ -48,6 +50,43 @@ describe("EditFollowNeurons", () => {
     const po = renderComponent();
     expect(await po.getBadgeNumber(Topic.ExchangeRate)).toBe(2);
     expect(await po.getBadgeNumber(Topic.Governance)).toBe(0);
+  });
+
+  it("renders topics in specific order", async () => {
+    const po = renderComponent();
+    const topicSections = await po.getFollowNnsTopicSectionPos();
+
+    // Deprecated + NeuronManagement (because it's not public)
+    const hiddenTopicCount = DEPRECATED_TOPICS.length + 1;
+    expect(topicSections.length).toBe(
+      enumValues(Topic).length - hiddenTopicCount
+    );
+
+    const topicTitles = await Promise.all(
+      topicSections.map(async (x) =>
+        (await x.getFollowTopicSectionPo()).getTopicTitle()
+      )
+    );
+
+    expect(topicTitles).toEqual([
+      "Governance",
+      "SNS & Neurons' Fund",
+      "All Except Governance, and SNS & Neurons' Fund",
+      "API Boundary Node Management",
+      "Application Canister Management",
+      "Exchange Rate",
+      "IC OS Version Deployment",
+      "IC OS Version Election",
+      "KYC",
+      "Network Economics",
+      "Node Admin",
+      "Node Provider Rewards",
+      "Participant Management",
+      "Protocol Canister Management",
+      "Service Nervous System Management",
+      "Subnet Management",
+      "Subnet Rental",
+    ]);
   });
 
   it("displays the followees of the user in specific topic", async () => {
