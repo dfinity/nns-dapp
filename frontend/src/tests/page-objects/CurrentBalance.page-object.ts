a37be5a77e3aa7bb4f39a923af7bbe485336223d
David de Kloet <david.de.kloet@dfinity.org>
2025-02-13 14:38:22 +0100

    Use page objects in CurrentBalance.spec.ts

---
+++ frontend/src/tests/page-objects/CurrentBalance.page-object.ts
@@ -0,0 +1,15 @@
+import { BasePageObject } from "$tests/page-objects/base.page-object";
+import type { PageObjectElement } from "$tests/types/page-object.types";
+import { AmountDisplayPo } from "$tests/page-objects/AmountDisplay.page-object";
+
+export class CurrentBalancePo extends BasePageObject {
+  private static readonly TID = "current-balance-component";
+
+  static under(element: PageObjectElement): CurrentBalancePo {
+    return new CurrentBalancePo(element.byTestId(CurrentBalancePo.TID));
+  }
+
+  get AmountDisplayPo(): AmountDisplayPo {
+    return AmountDisplayPo.under(this.root);
+  }
+}
