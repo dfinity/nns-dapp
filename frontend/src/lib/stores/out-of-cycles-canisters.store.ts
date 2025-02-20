214596c1eadcfa51ca3a0ef862b8e4d6015c6121
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-02-20 13:36:31 +0100

    rebane store to be more precise

---
+++ frontend/src/lib/stores/out-of-cycles-canisters.store.ts
@@ -0,0 +1,34 @@
+import type { CanisterIdString } from "@dfinity/nns";
+import { writable, type Readable } from "svelte/store";
+
+export type OutOfCyclesCanistersStoreData = CanisterIdString[];
+
+export interface OutOfCyclesCanistersStore
+  extends Readable<OutOfCyclesCanistersStoreData> {
+  add: (canisterId: CanisterIdString) => void;
+  delete: (canisterId: CanisterIdString) => void;
+}
+
+export const initOutOfCyclesCanistersStore = (): OutOfCyclesCanistersStore => {
+  const initialOutOfCyclesCanisters: OutOfCyclesCanistersStoreData = [];
+
+  const { subscribe, update } = writable<OutOfCyclesCanistersStoreData>(
+    initialOutOfCyclesCanisters
+  );
+
+  return {
+    subscribe,
+    add: (canisterId) => {
+      update((outOfCyclesCanisters: OutOfCyclesCanistersStoreData) =>
+        outOfCyclesCanisters.includes(canisterId)
+          ? outOfCyclesCanisters
+          : [...outOfCyclesCanisters, canisterId]
+      );
+    },
+    delete: (canisterId) => {
+      update((canistersids) => canistersids.filter((id) => id !== canisterId));
+    },
+  };
+};
+
+export const outOfCyclesCanistersStore = initOutOfCyclesCanistersStore();
