dfinity/nns-dapp

commit a9ea09aee37e1882523ab6da650adb1910f222f4

feat(store): new store to track canister errors (#6418)
# Motivation

Sometimes, canisters may become non-operative, such as when they run
out-of-cycles. We need to be aware of these situations so we can update
the UI accordingly.

This new store contains a map of canisters and their errors.

Out of scope: Parsing errors and making them usable.

# Changes

- New store `canistersErrorsStore`

# Tests

- Unit tests for the store

# Todos

- [ ] Add entry to changelog (if necessary).
Not necessary

frontend/src/lib/stores/canisters-errors.store.ts
@@ -1,0 +1,45 @@
+import { writable, type Readable } from "svelte/store";
+
+type CanisterId = string;
+type CanisterError = {
+  raw: unknown;
+};
+export type CanistersErrorsStoreData = Record<CanisterId, CanisterError>;
+
+export interface CanistersErrorsStore
+  extends Readable<CanistersErrorsStoreData> {
+  set: (params: { canisterId: CanisterId; rawError: unknown }) => void;
+  delete: (canisterId: CanisterId) => void;
+}
+
+export const initCanistersErrorsStore = (): CanistersErrorsStore => {
+  const initialCanistersErrors: CanistersErrorsStoreData = {};
+
+  const { subscribe, update } = writable<CanistersErrorsStoreData>(
+    initialCanistersErrors
+  );
+
+  return {
+    subscribe,
+    set: ({ canisterId, rawError }) => {
+      update((currentState: CanistersErrorsStoreData) => ({
+        ...currentState,
+        [canisterId]: {
+          raw: rawError,
+        },
+      }));
+    },
+    delete: (canisterId) => {
+      update(
+        ({
+          [canisterId]: _,
+          ...restOfCanisters
+        }: CanistersErrorsStoreData) => ({
+          ...restOfCanisters,
+        })
+      );
+    },
+  };
+};
+
+export const canistersErrorsStore = initCanistersErrorsStore();

frontend/src/tests/lib/stores/canisters-errors.store.spec.ts
@@ -1,0 +1,50 @@
+import { canistersErrorsStore } from "$lib/stores/canisters-errors.store";
+import { get } from "svelte/store";
+
+describe("canisters-errors.store", () => {
+  it("should initialize it empty", () => {
+    expect(get(canistersErrorsStore)).toEqual({});
+  });
+
+  it("should set error", () => {
+    const canisterId = "canister-id";
+    const error = "something went wrong";
+
+    canistersErrorsStore.set({ canisterId, rawError: error });
+
+    expect(get(canistersErrorsStore)).toEqual({
+      [canisterId]: {
+        raw: error,
+      },
+    });
+  });
+
+  it("should remove error", () => {
+    const canisterId = "canister-id";
+    const anotherCanisterId = "another-canister-id";
+    const error = "something went wrong";
+
+    canistersErrorsStore.set({ canisterId, rawError: error });
+    canistersErrorsStore.set({
+      canisterId: anotherCanisterId,
+      rawError: error,
+    });
+
+    expect(get(canistersErrorsStore)).toEqual({
+      [canisterId]: {
+        raw: error,
+      },
+      [anotherCanisterId]: {
+        raw: error,
+      },
+    });
+
+    canistersErrorsStore.delete(anotherCanisterId);
+
+    expect(get(canistersErrorsStore)).toEqual({
+      [canisterId]: {
+        raw: error,
+      },
+    });
+  });
+});

commit 32cc24faa9ca45ff5993b11dcd41c685469a0001

refactor(store): simplify canister errors to track only out-of-cycles state (#6464)
# Motivation

Refactors the `canisters-errors.store` introduced in #6418 to focus on
out-of-cycles canisters.

# Changes

- Rename store
- Simplify store to track list of canisters that ran out of cycles

# Tests

- Update tests

# Todos

- [ ] Add entry to changelog (if necessary).
Not necessary

frontend/src/lib/stores/canisters-errors.store.ts
@@ -1,45 +1,0 @@
-import { writable, type Readable } from "svelte/store";
-
-type CanisterId = string;
-type CanisterError = {
-  raw: unknown;
-};
-export type CanistersErrorsStoreData = Record<CanisterId, CanisterError>;
-
-export interface CanistersErrorsStore
-  extends Readable<CanistersErrorsStoreData> {
-  set: (params: { canisterId: CanisterId; rawError: unknown }) => void;
-  delete: (canisterId: CanisterId) => void;
-}
-
-export const initCanistersErrorsStore = (): CanistersErrorsStore => {
-  const initialCanistersErrors: CanistersErrorsStoreData = {};
-
-  const { subscribe, update } = writable<CanistersErrorsStoreData>(
-    initialCanistersErrors
-  );
-
-  return {
-    subscribe,
-    set: ({ canisterId, rawError }) => {
-      update((currentState: CanistersErrorsStoreData) => ({
-        ...currentState,
-        [canisterId]: {
-          raw: rawError,
-        },
-      }));
-    },
-    delete: (canisterId) => {
-      update(
-        ({
-          [canisterId]: _,
-          ...restOfCanisters
-        }: CanistersErrorsStoreData) => ({
-          ...restOfCanisters,
-        })
-      );
-    },
-  };
-};
-
-export const canistersErrorsStore = initCanistersErrorsStore();

frontend/src/lib/stores/out-of-cycles-canisters.store.ts
@@ -1,0 +1,34 @@
+import type { CanisterIdString } from "@dfinity/nns";
+import { writable, type Readable } from "svelte/store";
+
+export type OutOfCyclesCanistersStoreData = CanisterIdString[];
+
+export interface OutOfCyclesCanistersStore
+  extends Readable<OutOfCyclesCanistersStoreData> {
+  add: (canisterId: CanisterIdString) => void;
+  delete: (canisterId: CanisterIdString) => void;
+}
+
+export const initOutOfCyclesCanistersStore = (): OutOfCyclesCanistersStore => {
+  const initialOutOfCyclesCanisters: OutOfCyclesCanistersStoreData = [];
+
+  const { subscribe, update } = writable<OutOfCyclesCanistersStoreData>(
+    initialOutOfCyclesCanisters
+  );
+
+  return {
+    subscribe,
+    add: (canisterId) => {
+      update((outOfCyclesCanisters: OutOfCyclesCanistersStoreData) =>
+        outOfCyclesCanisters.includes(canisterId)
+          ? outOfCyclesCanisters
+          : [...outOfCyclesCanisters, canisterId]
+      );
+    },
+    delete: (canisterId) => {
+      update((canistersids) => canistersids.filter((id) => id !== canisterId));
+    },
+  };
+};
+
+export const outOfCyclesCanistersStore = initOutOfCyclesCanistersStore();

frontend/src/tests/lib/stores/canisters-errors.store.spec.ts
@@ -1,50 +1,0 @@
-import { canistersErrorsStore } from "$lib/stores/canisters-errors.store";
-import { get } from "svelte/store";
-
-describe("canisters-errors.store", () => {
-  it("should initialize it empty", () => {
-    expect(get(canistersErrorsStore)).toEqual({});
-  });
-
-  it("should set error", () => {
-    const canisterId = "canister-id";
-    const error = "something went wrong";
-
-    canistersErrorsStore.set({ canisterId, rawError: error });
-
-    expect(get(canistersErrorsStore)).toEqual({
-      [canisterId]: {
-        raw: error,
-      },
-    });
-  });
-
-  it("should remove error", () => {
-    const canisterId = "canister-id";
-    const anotherCanisterId = "another-canister-id";
-    const error = "something went wrong";
-
-    canistersErrorsStore.set({ canisterId, rawError: error });
-    canistersErrorsStore.set({
-      canisterId: anotherCanisterId,
-      rawError: error,
-    });
-
-    expect(get(canistersErrorsStore)).toEqual({
-      [canisterId]: {
-        raw: error,
-      },
-      [anotherCanisterId]: {
-        raw: error,
-      },
-    });
-
-    canistersErrorsStore.delete(anotherCanisterId);
-
-    expect(get(canistersErrorsStore)).toEqual({
-      [canisterId]: {
-        raw: error,
-      },
-    });
-  });
-});

frontend/src/tests/lib/stores/out-of-cycles-canisters.store.spec.ts
@@ -1,0 +1,28 @@
+import { outOfCyclesCanistersStore } from "$lib/stores/out-of-cycles-canisters.store";
+import { get } from "svelte/store";
+
+describe("out-of-cycles-canisters.store", () => {
+  it("should initialize it empty", () => {
+    expect(get(outOfCyclesCanistersStore)).toEqual([]);
+  });
+
+  it("should set error", () => {
+    const canisterId = "canister-id";
+
+    outOfCyclesCanistersStore.add(canisterId);
+
+    expect(get(outOfCyclesCanistersStore)).toEqual([canisterId]);
+  });
+
+  it("should remove error", () => {
+    const canisterId = "canister-id";
+
+    outOfCyclesCanistersStore.add(canisterId);
+
+    expect(get(outOfCyclesCanistersStore)).toEqual([canisterId]);
+
+    outOfCyclesCanistersStore.delete(canisterId);
+
+    expect(get(outOfCyclesCanistersStore)).toEqual([]);
+  });
+});