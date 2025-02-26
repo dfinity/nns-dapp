5ffc4f8fbe618612a2cc859e9baeebea8d060c6c
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-02-26 18:16:07 +0100

    update tests

--- frontend/src/tests/lib/utils/format.utils.spec.ts
+++ frontend/src/tests/lib/utils/format.utils.spec.ts
@@ -96,44 +96,26 @@ describe("format.utils", () => {
       expect(formatCurrencyNumber(0.009)).toBe("$0.01");
       expect(formatCurrencyNumber(1)).toBe("$1.00");
       expect(formatCurrencyNumber(9.9)).toBe("$9.90");
-      expect(formatCurrencyNumber(10)).toBe("$10.00");
-      expect(formatCurrencyNumber(99.99)).toBe("$99.99");
-      expect(formatCurrencyNumber(100)).toBe("$100.00");
-      expect(formatCurrencyNumber(420.69)).toBe("$420.69");
       expect(formatCurrencyNumber(999.99)).toBe("$999.99");
     });
 
     it("formats values between 1,000 and 1,000,000 with 0 decimal points", () => {
       expect(formatCurrencyNumber(1000)).toBe("$1’000");
-      expect(formatCurrencyNumber(1000.5)).toBe("$1’001");
-      expect(formatCurrencyNumber(1996)).toBe("$1’996");
-      expect(formatCurrencyNumber(9999)).toBe("$9’999");
-      expect(formatCurrencyNumber(10000)).toBe("$10’000");
-      expect(formatCurrencyNumber(100000)).toBe("$100’000");
+      expect(formatCurrencyNumber(1000.4)).toBe("$1’000");
+      expect(formatCurrencyNumber(1996.6)).toBe("$1’997");
       expect(formatCurrencyNumber(999999)).toBe("$999’999");
-      expect(formatCurrencyNumber(999999.99)).toBe("$1’000’000");
     });
 
     it("formats values between 1,000,000 and 1,000,000,000 with M suffix and 2 decimal points", () => {
       expect(formatCurrencyNumber(1000000)).toBe("$1.00M");
-      expect(formatCurrencyNumber(1500000)).toBe("$1.50M");
       expect(formatCurrencyNumber(1550000)).toBe("$1.55M");
-      expect(formatCurrencyNumber(9990000)).toBe("$9.99M");
-      expect(formatCurrencyNumber(10000000)).toBe("$10.00M");
       expect(formatCurrencyNumber(24800000)).toBe("$24.80M");
-      expect(formatCurrencyNumber(420690000)).toBe("$420.69M");
-      expect(formatCurrencyNumber(999999999)).toBe("$1’000.00M");
     });
 
     it("formats values greater than or equal to 1,000,000,000 with B suffix and 2 decimal points", () => {
       expect(formatCurrencyNumber(1000000000)).toBe("$1.00B");
-      expect(formatCurrencyNumber(1500000000)).toBe("$1.50B");
       expect(formatCurrencyNumber(9990000000)).toBe("$9.99B");
-      expect(formatCurrencyNumber(10000000000)).toBe("$10.00B");
       expect(formatCurrencyNumber(24800000000)).toBe("$24.80B");
-      expect(formatCurrencyNumber(420690000000)).toBe("$420.69B");
-      expect(formatCurrencyNumber(999999999999)).toBe("$1’000.00B");
-      expect(formatCurrencyNumber(1000000000000)).toBe("$1’000.00B");
     });
   });
 });
