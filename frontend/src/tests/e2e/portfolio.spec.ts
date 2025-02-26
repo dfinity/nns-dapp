4357a0d18083ba027e008c3b42dd52c60c2dc4d6
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-02-26 22:02:22 +0100

    fmt

--- frontend/src/tests/e2e/portfolio.spec.ts
+++ frontend/src/tests/e2e/portfolio.spec.ts
@@ -1,9 +1,9 @@
 import { AppPo } from "$tests/page-objects/App.page-object";
 import { PlaywrightPageObjectElement } from "$tests/page-objects/playwright.page-object";
 import {
-    replaceContent,
-    signInWithNewUser,
-    step,
+  replaceContent,
+  signInWithNewUser,
+  step,
 } from "$tests/utils/e2e.test-utils";
 import { expect, test } from "@playwright/test";
 
@@ -24,7 +24,7 @@ test("Visual test Landing Page", async ({ page, browser }) => {
   await page.setViewportSize(VIEWPORT_SIZES.desktop);
 
   await portfolioPo.getPortfolioPagePo().getTotalAssetsCardPo().waitForLoaded();
-    await appPo.getMenuItemsPo().getTotalValueLockedLinkPo().waitFor();
+  await appPo.getMenuItemsPo().getTotalValueLockedLinkPo().waitFor();
 
   // The governance metrics are only updated once a day so for the first 24h
   // after a snapshot is created, the metrics might be different than what
