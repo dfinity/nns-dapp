2ce76664fd0316f2280b3d456d9fd1f531e390c6
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-03-04 22:01:47 +0100

    refactors layout to make space for new cards

--- frontend/src/lib/pages/Portfolio.svelte
+++ frontend/src/lib/pages/Portfolio.svelte
@@ -111,15 +111,15 @@
 </script>
 
 <main data-tid="portfolio-page-component">
-  <div class="top" class:single-card={$authSignedInStore}>
-    {#if !$authSignedInStore}
-      <LoginCard />
-    {/if}
+  <div class="top" class:signed-in={$authSignedInStore}>
     <TotalAssetsCard
       usdAmount={totalUsdAmount}
       hasUnpricedTokens={hasUnpricedTokensOrStake}
       isLoading={isSomethingLoading}
     />
+    {#if !$authSignedInStore}
+      <LoginCard />
+    {/if}
   </div>
   <div class="content">
     {#if heldTokensCard === "skeleton"}
@@ -172,14 +172,18 @@
       grid-template-columns: 1fr;
       gap: var(--padding-2x);
 
+      :global([data-tid="portfolio-login-card"]) {
+        order: -1;
+      }
+
       @include media.min-width(large) {
         grid-template-columns: 1fr 2fr;
 
-        > :global(article:first-of-type) {
-          order: 1;
+        :global([data-tid="portfolio-login-card"]) {
+          order: 0;
         }
-
-        &.single-card {
+        // Case: signed in
+        &.signed-in {
           grid-template-columns: 1fr;
         }
       }
