d26a8dd1c9b963b6e74c5bbc3abf060a829d4a5a
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-03-06 16:30:05 +0100

    test

--- frontend/src/lib/components/portfolio/LoginCard.svelte
+++ frontend/src/lib/components/portfolio/LoginCard.svelte
@@ -6,35 +6,36 @@
 </script>
 
 <Card testId="portfolio-login-card">
-  <div class="wrapper">
-    <div class="icon">
-      <IconUserLogin />
-    </div>
-    <div class="content">
-      <h2>{$i18n.portfolio.login_title}</h2>
-      <p>{$i18n.portfolio.login_description}</p>
-    </div>
-    <div class="action">
-      <SignIn />
+  <div class="wrap">
+    <div class="wrapper">
+      <div class="icon">
+        <IconUserLogin />
+      </div>
+      <div class="content">
+        <h2>{$i18n.portfolio.login_title}</h2>
+        <p>{$i18n.portfolio.login_description}</p>
+      </div>
+      <div class="action">
+        <SignIn />
+      </div>
     </div>
   </div>
 </Card>
 
 <style lang="scss">
   @use "@dfinity/gix-components/dist/styles/mixins/media";
-  .wrapper {
-    display: grid;
-    grid-template-columns: auto 1fr;
-    grid-template-areas:
-      "icon content"
-      "action action";
-    gap: var(--padding-2x);
-    padding: var(--padding-2x);
+  .wrap {
+    container-type: inline-size;
     background-color: var(--card-background-tint);
-    height: 100%;
     box-sizing: border-box;
+    height: 100%;
 
-    @include media.min-width(medium) {
+    .wrapper {
+      box-sizing: border-box;
+      height: 100%;
+
+      display: grid;
+      grid-template-columns: auto 1fr;
       grid-template-areas:
         "icon content"
         "icon action";
@@ -65,20 +66,72 @@
       height: 80px;
 
       @include media.min-width(medium) {
-        width: 144px;
-        height: 144px;
-      }
-    }
-
-    .action {
-      grid-area: action;
-      :global(button) {
-        width: 100%;
+        grid-template-areas:
+          "icon content"
+          "icon action";
+        gap: var(--padding-3x);
+        padding: var(--padding-3x) var(--padding-4x);
       }
 
-      @include media.min-width(medium) {
+      .content {
+        grid-area: content;
+        display: flex;
+        flex-direction: column;
+        gap: var(--padding);
+
+        h2,
+        p {
+          margin: 0;
+          padding: 0;
+        }
+        p {
+          color: var(--color-text-secondary);
+        }
+      }
+
+      .icon {
+        grid-area: icon;
+        width: 80px;
+        height: 80px;
+
+        @include media.min-width(medium) {
+          width: 144px;
+          height: 144px;
+        }
+      }
+
+      .action {
+        grid-area: action;
+        align-self: flex-end;
         :global(button) {
-          width: auto;
+          width: 100%;
+        }
+
+        @include media.min-width(medium) {
+          :global(button) {
+            width: auto;
+          }
+        }
+      }
+
+      // Progressive enhancement
+      @include media.min-width(large) {
+        @container (max-width: 460px) {
+          & {
+            grid-template-areas:
+              "icon content"
+              "action action";
+            gap: var(--padding-2x);
+
+            .icon {
+              width: 80px;
+              height: 80px;
+            }
+
+            .action :global(button) {
+              width: 100%;
+            }
+          }
         }
       }
     }
