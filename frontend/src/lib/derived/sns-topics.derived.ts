79d4b8c5ade85fe671fe5e7d7d2a2d9ad51eca72
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-03-14 18:35:35 +0100

    Update snsTopicsStore

--- frontend/src/lib/derived/sns-topics.derived.ts
+++ frontend/src/lib/derived/sns-topics.derived.ts
@@ -1,13 +1,19 @@
 import { snsAggregatorDerived } from "$lib/derived/sns-aggregator.derived";
-import type { CachedListTopicsResponseDto } from "$lib/types/sns-aggregator";
+import type { ListTopicsResponse } from "@dfinity/sns/dist/candid/sns_governance_test";
+import { isNullish } from "@dfinity/utils";
 import { type Readable } from "svelte/store";
+import { convertDtoToListTopicsResponse } from "../utils/sns-aggregator-converters.utils";
 
 export interface SnsParametersStore {
-  [rootCanisterId: string]: CachedListTopicsResponseDto;
+  [rootCanisterId: string]: ListTopicsResponse | undefined;
 }
 
 /**
  * A store that contains the sns nervous system topics for each project.
  */
 export const snsTopicsStore: Readable<SnsParametersStore> =
-  snsAggregatorDerived((sns) => sns.topics);
+  snsAggregatorDerived((sns) =>
+    isNullish(sns?.topics)
+      ? undefined
+      : convertDtoToListTopicsResponse(sns.topics)
+  );
