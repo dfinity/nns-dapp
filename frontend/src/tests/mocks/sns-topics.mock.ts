755fbe064788dad5007e3e5d4c002b63459e54ad
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-03-18 12:36:46 +0100

    Add topicInfoDtoMock

---
+++ frontend/src/tests/mocks/sns-topics.mock.ts
@@ -0,0 +1,57 @@
+import type {
+  CachedNervousFunctionDto,
+  TopicInfoDto,
+} from "$lib/types/sns-aggregator";
+import type { Topic } from "@dfinity/sns/dist/candid/sns_governance";
+
+export const topicTypeMock = "DaoCommunitySettings";
+export const topicMock: Topic = {
+  [topicTypeMock]: null,
+};
+
+export const cachedNativeNFDtoMock: CachedNervousFunctionDto = {
+  id: 1,
+  name: "Motion",
+  description: "Native Nervous System Function Description",
+  function_type: {
+    NativeNervousSystemFunction: {},
+  },
+};
+
+export const cachedGenericNFDtoMock: CachedNervousFunctionDto = {
+  id: 1,
+  name: "Generic Motion",
+  description: "Generic Nervous System Function Description",
+  function_type: {
+    GenericNervousSystemFunction: {
+      validator_canister_id: null,
+      target_canister_id: null,
+      validator_method_name: null,
+      target_method_name: null,
+      topic: topicMock,
+    },
+  },
+};
+
+export const topicInfoDtoMock = ({
+  topic,
+  name,
+  description,
+  isCritical = false,
+  nativeFunctions = [],
+  customFunctions = [],
+}: {
+  topic: string;
+  name: string;
+  description: string;
+  isCritical?: boolean;
+  nativeFunctions?: CachedNervousFunctionDto[];
+  customFunctions?: CachedNervousFunctionDto[];
+}): TopicInfoDto => ({
+  topic,
+  name,
+  description,
+  native_functions: nativeFunctions,
+  custom_functions: customFunctions,
+  is_critical: isCritical,
+});
