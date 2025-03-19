1b2be1aa6670959b2b3fd0542e9978eee7091865
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-03-19 13:00:57 +0100

    new component

---
+++ frontend/src/tests/page-objects/AmountInputFiatValue.page-object.ts
@@ -0,0 +1,30 @@
+import { BasePageObject } from "$tests/page-objects/base.page-object";
+import type { PageObjectElement } from "$tests/types/page-object.types";
+import { AmountDisplayPo } from "./AmountDisplay.page-object";
+
+export class AmountInputFiatValuePo extends BasePageObject {
+  private static readonly TID = "amount-input-fiat-value-component";
+
+  static under(element: PageObjectElement): AmountInputFiatValuePo {
+    return new AmountInputFiatValuePo(
+      element.byTestId(AmountInputFiatValuePo.TID)
+    );
+  }
+
+  getFiatValue(): Promise<string> {
+    return this.getText("fiat-value");
+  }
+
+  getBalancePo(): AmountDisplayPo {
+    return AmountDisplayPo.under(this.root);
+  }
+
+  hasBalance(): Promise<boolean> {
+    return this.getBalancePo().isPresent();
+  }
+
+  async hasError(): Promise<boolean> {
+    const classes = await this.root.getClasses();
+    return classes.includes("has-error");
+  }
+}
