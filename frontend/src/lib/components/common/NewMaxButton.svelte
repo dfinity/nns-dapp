2aa069a7dcc6d76891ae4352758f869cb08cf548
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-03-19 14:01:47 +0100

    new maxbutton to be used inside the field

---
+++ frontend/src/lib/components/common/NewMaxButton.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { i18n } from "$lib/stores/i18n";
+  export let disabled = false;
+  // TODO(yhabib): remove duplication with MaxButton once designs are unified
+</script>
+
+<button data-tid="max-button" on:click|preventDefault type="button" {disabled}>
+  {$i18n.core.max}
+</button>
+
+<style lang="scss">
+  @use "@dfinity/gix-components/dist/styles/mixins/fonts";
+  button {
+    display: flex;
+    align-items: center;
+    gap: var(--padding-0_5x);
+    color: var(--button-primary);
+    text-decoration: none;
+    @include fonts.h5(true);
+  }
+</style>
