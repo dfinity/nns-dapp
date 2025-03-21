94682fa9d8f4855cd52ea320a212192b28e1637c
Max Strasinsky <98811342+mstrasinskis@users.noreply.github.com>
2025-03-21 08:33:09 +0100

    Sns topics follow modal (#6603)
    # Motivation
    
    Migrating from following by type to following by topic to improve neuron
    management. This update prepares replacement of the ‘follow by type’
    modal with a ‘follow by topic’ modal. In this PR, we introduce the basic
    modal structure with the logic to display it.
    
    # Changes
    
    - New modal component and logic to display it only when topics are
    available.
    
    # Tests
    
    - Added unit tests.
    - Tested locally.
    
    
    https://github.com/user-attachments/assets/c469828c-4f50-4c2c-a65e-8201ce8c4115
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    Not necessary.

---
+++ frontend/src/lib/modals/sns/neurons/FollowSnsNeuronsByTopicModal.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Separator from "$lib/components/ui/Separator.svelte";
+  import { i18n } from "$lib/stores/i18n";
+  import { Modal } from "@dfinity/gix-components";
+  import { createEventDispatcher } from "svelte";
+
+  const dispatcher = createEventDispatcher();
+
+  const close = () => dispatcher("nnsClose");
+</script>
+
+<Modal
+  on:nnsClose
+  testId="follow-sns-neurons-by-topic-modal"
+  --modal-content-overflow-y="scroll"
+>
+  <svelte:fragment slot="title"
+    >{$i18n.neurons.follow_neurons_screen}</svelte:fragment
+  >
+  <p class="description">{$i18n.follow_neurons.description}</p>
+
+  <Separator spacing="medium" />
+
+  TBD
+
+  <div class="toolbar">
+    <button data-tid="close-button" class="secondary" on:click={close}>
+      {$i18n.core.close}
+    </button>
+  </div>
+</Modal>
