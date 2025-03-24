cec02e8c78046a2500692534259e7723671768e7
David Dal Busco <david.dalbusco@dfinity.org>
2025-03-24 16:52:12 +0100

    chore: remove script
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- frontend/update-sveltev5.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-npm rm @dfinity/gix-components @sveltejs/adapter-static @sveltejs/kit @sveltejs/vite-plugin-svelte @testing-library/jest-dom @testing-library/svelte @testing-library/user-event @vitest/coverage-v8 svelte svelte-check svelte-preprocess svelte2tsx vite vitest vitest-mock-extended globals @types/node
-
-npm i @sveltejs/adapter-static @sveltejs/kit @sveltejs/vite-plugin-svelte @testing-library/jest-dom @testing-library/svelte @testing-library/user-event @vitest/coverage-v8 svelte svelte-check svelte-preprocess vite vitest vitest-mock-extended globals @types/node -D
-
-npm i @dfinity/gix-components
