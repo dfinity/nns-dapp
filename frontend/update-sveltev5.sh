cfb3fb46a56822a506ed09aa2f4ba76d79b2f802
David Dal Busco <david.dalbusco@dfinity.org>
2025-03-24 16:50:57 +0100

    feat: use release gix-cmp
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- frontend/update-sveltev5.sh
+++ frontend/update-sveltev5.sh
@@ -4,4 +4,4 @@ npm rm @dfinity/gix-components @sveltejs/adapter-static @sveltejs/kit @sveltejs/
 
 npm i @sveltejs/adapter-static @sveltejs/kit @sveltejs/vite-plugin-svelte @testing-library/jest-dom @testing-library/svelte @testing-library/user-event @vitest/coverage-v8 svelte svelte-check svelte-preprocess vite vitest vitest-mock-extended globals @types/node -D
 
-npm i dfinity-gix-components-5.1.0.tgz
+npm i @dfinity/gix-components
