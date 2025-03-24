3b04f8c2408a37991420c22b813cdafe2e8482a4
David Dal Busco <david.dalbusco@dfinity.org>
2025-03-24 06:42:34 +0100

    chore: fmt
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- frontend/update-sveltev5.sh
+++ frontend/update-sveltev5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-npm rm @dfinity/gix-components @sveltejs/adapter-static  @sveltejs/kit @sveltejs/vite-plugin-svelte @testing-library/jest-dom @testing-library/svelte @testing-library/user-event @vitest/coverage-v8 svelte svelte-check svelte-preprocess svelte2tsx vite vitest vitest-mock-extended globals @types/node
+npm rm @dfinity/gix-components @sveltejs/adapter-static @sveltejs/kit @sveltejs/vite-plugin-svelte @testing-library/jest-dom @testing-library/svelte @testing-library/user-event @vitest/coverage-v8 svelte svelte-check svelte-preprocess svelte2tsx vite vitest vitest-mock-extended globals @types/node
 
-npm i @sveltejs/adapter-static  @sveltejs/kit @sveltejs/vite-plugin-svelte @testing-library/jest-dom @testing-library/svelte @testing-library/user-event @vitest/coverage-v8 svelte svelte-check svelte-preprocess vite vitest vitest-mock-extended globals @types/node -D
+npm i @sveltejs/adapter-static @sveltejs/kit @sveltejs/vite-plugin-svelte @testing-library/jest-dom @testing-library/svelte @testing-library/user-event @vitest/coverage-v8 svelte svelte-check svelte-preprocess vite vitest vitest-mock-extended globals @types/node -D
 
 npm i dfinity-gix-components-5.1.0.tgz
