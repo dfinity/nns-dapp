8b1a640a08d4246bed09d9e2085d6480451ae917
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-04-02 17:40:10 +0200

    fix unit test

--- frontend/src/tests/routes/app/portfolio/page.spec.ts
+++ frontend/src/tests/routes/app/portfolio/page.spec.ts
@@ -131,12 +131,7 @@ describe("Portfolio route", () => {
     const nativeBalances =
       await tokensCardPo.getHeldTokensBalanceInNativeCurrency();
 
-    expect(
-      await portfolioPagePo.getTotalAssetsCardPo().getPrimaryAmount()
-    ).toBe("$-/-");
-    expect(
-      await portfolioPagePo.getTotalAssetsCardPo().getSecondaryAmount()
-    ).toBe("-/- ICP");
+    expect(await portfolioPagePo.getLoginCard().isPresent()).toBe(true);
 
     expect(titles.length).toBe(4);
     expect(titles).toEqual(["Internet Computer", "ckBTC", "ckETH", "ckUSDC"]);
