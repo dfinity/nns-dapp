f36b41518a2800a591658912f20faeb03e8fac05
Yusef Habib <yusef.fernandez@dfinity.org>
2025-04-08 12:23:45 +0200

    feat(portfolio): add SNS creation proposal card (#6663)
    # Motivation
    
    We want to display NSN proposals for creating an SNS directly on the
    Portfolio page. A new card will be used to present this information,
    showing basic details about the proposal and the current voting status.
    
    This initial PR creates the new component, which will later be utilized
    by the Portfolio page.
    
    [NNS1-3637](https://dfinity.atlassian.net/browse/NNS1-3637)
    
    Note: The differences in the remaining time copy are intentional to
    ensure consistency between the Portfolio cards.
    
    Design reference:
    
    <img width="410" alt="Screenshot 2025-04-04 at 16 31 19"
    src="https://github.com/user-attachments/assets/fd16296f-ba94-42bd-a79d-3420835639b4"
    />
    
    Screenshots:
    
    <img width="565" alt="Screenshot 2025-04-04 at 16 34 34"
    src="https://github.com/user-attachments/assets/8f47f459-82de-42a0-87aa-003b746c25e9"
    />
    
    <img width="549" alt="Screenshot 2025-04-04 at 16 33 42"
    src="https://github.com/user-attachments/assets/f517411f-16e8-4a15-aa83-b8db20798f7e"
    />
    
    # Changes
    
    - New components `VotesResult` and `NewSnsProposalCard`
    - New copy for the component
    
    # Tests
    
    - New unit tests and page object for the new component.
    - Tested in [devenv]() with other changes
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    Not necessary.
    
    [NNS1-3637]:
    https://dfinity.atlassian.net/browse/NNS1-3637?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ frontend/src/tests/page-objects/NewSnsProposalCard.page-object.ts
@@ -0,0 +1,38 @@
+import { BasePageObject } from "$tests/page-objects/base.page-object";
+import { LinkPo } from "$tests/page-objects/Link.page-object";
+import type { PageObjectElement } from "$tests/types/page-object.types";
+
+export class NewSnsProposalCardPo extends BasePageObject {
+  private static readonly TID = "new-sns-proposal-card";
+
+  static under(element: PageObjectElement): NewSnsProposalCardPo {
+    return new NewSnsProposalCardPo(element.byTestId(NewSnsProposalCardPo.TID));
+  }
+
+  getTitle(): Promise<string> {
+    return this.getText("project-name");
+  }
+
+  getProposalTitle(): Promise<string> {
+    return this.getText("proposal-title");
+  }
+
+  getAdoptPercentage(): Promise<string> {
+    return this.getText("adopt-percentage");
+  }
+
+  getRejectPercentage(): Promise<string> {
+    return this.getText("reject-percentage");
+  }
+
+  getTimeRemaining(): Promise<string> {
+    return this.getText("time-remaining");
+  }
+
+  getLinkPo(): LinkPo {
+    return LinkPo.under({
+      element: this.root,
+      testId: "proposal-link",
+    });
+  }
+}
