434ca8a736b09abf31f4a6cc758ec17e3f595e69
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-04-12 20:45:31 +0200

    Extend PO

--- frontend/src/tests/page-objects/FollowSnsNeuronsByTopicModal.page-object.ts
+++ frontend/src/tests/page-objects/FollowSnsNeuronsByTopicModal.page-object.ts
@@ -1,3 +1,5 @@
+import { FollowSnsNeuronsByTopicStepNeuronPo } from "$tests/page-objects/FollowSnsNeuronsByTopicStepNeuron.page-object";
+import { FollowSnsNeuronsByTopicStepTopicsPo } from "$tests/page-objects/FollowSnsNeuronsByTopicStepTopics.page-object";
 import { ModalPo } from "$tests/page-objects/Modal.page-object";
 import type { PageObjectElement } from "$tests/types/page-object.types";
 
@@ -14,5 +16,13 @@ export class FollowSnsNeuronsByTopicModalPo extends ModalPo {
 
   clickCloseButton(): Promise<void> {
     return this.click("close-button");
+  }
+
+  getFollowSnsNeuronsByTopicStepTopicsPo(): FollowSnsNeuronsByTopicStepTopicsPo {
+    return FollowSnsNeuronsByTopicStepTopicsPo.under(this.root);
+  }
+
+  getFollowSnsNeuronsByTopicStepNeuronPo(): FollowSnsNeuronsByTopicStepNeuronPo {
+    return FollowSnsNeuronsByTopicStepNeuronPo.under(this.root);
   }
 }
