048813aa33cc8079e8aa05ff9fa44446a496c6a7
Max Strasinsky <98811342+mstrasinskis@users.noreply.github.com>
2025-04-15 11:51:00 +0200

    Neuron step in follow by Sns topics modal (#6700)
    # Motivation
    
    After selecting topics in the [first
    step](https://github.com/dfinity/nns-dapp/pull/6697), the user needs to
    enter a neuron ID. This PR adds the component that will be used as the
    second step in the follow by SNS Topics
    [modal](https://github.com/dfinity/nns-dapp/pull/6699).
    
    [NNS1-3665](https://dfinity.atlassian.net/browse/NNS1-3665)
    Demo:
    https://qsgjb-riaaa-aaaaa-aaaga-cai.mstr-ingress.devenv.dfinity.network/
    
    <img width="447" alt="image"
    src="https://github.com/user-attachments/assets/d46f3ddb-4264-4c0f-828a-7f4947fedce0"
    />
    
    # Changes
    
    - New component.
    
    # Tests
    
    - Added.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    Not yet.
    
    [NNS1-3665]:
    https://dfinity.atlassian.net/browse/NNS1-3665?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ frontend/src/tests/page-objects/FollowSnsNeuronsByTopicStepNeuron.page-object.ts
@@ -0,0 +1,50 @@
+import { ButtonPo } from "$tests/page-objects/Button.page-object";
+import { TextInputPo } from "$tests/page-objects/TextInput.page-object";
+import { BasePageObject } from "$tests/page-objects/base.page-object";
+import type { PageObjectElement } from "$tests/types/page-object.types";
+
+export class FollowSnsNeuronsByTopicStepNeuronPo extends BasePageObject {
+  private static readonly TID =
+    "follow-sns-neurons-by-topic-step-neuron-component";
+
+  static under(
+    element: PageObjectElement
+  ): FollowSnsNeuronsByTopicStepNeuronPo {
+    return new FollowSnsNeuronsByTopicStepNeuronPo(
+      element.byTestId(FollowSnsNeuronsByTopicStepNeuronPo.TID)
+    );
+  }
+
+  getNeuronIdInputPo(): TextInputPo {
+    return TextInputPo.under({
+      element: this.root,
+      testId: "new-followee-id",
+    });
+  }
+
+  getNeuronIdValue(): Promise<string> {
+    return this.getNeuronIdInputPo().getValue();
+  }
+
+  getBackButtonPo(): ButtonPo {
+    return ButtonPo.under({
+      element: this.root,
+      testId: "back-button",
+    });
+  }
+
+  getConfirmButtonPo(): ButtonPo {
+    return ButtonPo.under({
+      element: this.root,
+      testId: "add-followee-button",
+    });
+  }
+
+  clickConfirmButton(): Promise<void> {
+    return this.getConfirmButtonPo().click();
+  }
+
+  clickBackButton(): Promise<void> {
+    return this.getBackButtonPo().click();
+  }
+}
