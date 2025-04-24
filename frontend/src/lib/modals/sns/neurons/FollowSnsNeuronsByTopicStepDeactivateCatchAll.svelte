984184d1a69c1531cb009f089e6d3e4d8ba6ea0a
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-04-24 16:13:58 +0200

    New FollowSnsNeuronsByTopicStepDeactivateCatchAll component

---
+++ frontend/src/lib/modals/sns/neurons/FollowSnsNeuronsByTopicStepDeactivateCatchAll.svelte
@@ -0,0 +1,101 @@
+<script lang="ts">
+  import TestIdWrapper from "$lib/components/common/TestIdWrapper.svelte";
+  import Separator from "$lib/components/ui/Separator.svelte";
+  import FollowSnsNeuronsByTopicLegacyFollowee from "$lib/modals/sns/neurons//FollowSnsNeuronsByTopicLegacyFollowee.svelte";
+  import { i18n } from "$lib/stores/i18n";
+  import type { SnsLegacyFollowings } from "$lib/types/sns";
+  import { subaccountToHexString } from "$lib/utils/sns-neuron.utils";
+  import { IconErrorOutline } from "@dfinity/gix-components";
+
+  type Props = {
+    catchAllFollowees: SnsLegacyFollowings[];
+    confirm: () => void;
+    cancel: () => void;
+  };
+  let { catchAllFollowees, confirm, cancel }: Props = $props();
+
+  const nsFunction = $derived(catchAllFollowees[0]?.nsFunction);
+  const followees = $derived(
+    catchAllFollowees.flatMap((followee) => followee.followees)
+  );
+</script>
+
+<TestIdWrapper testId="follow-sns-neurons-by-topic-step-legacy-component">
+  <div class="header">
+    <div class="icon-wrapper">
+      <IconErrorOutline size="75px" />
+    </div>
+    <p class="description">{$i18n.follow_sns_topics.catch_all_description}</p>
+  </div>
+
+  <Separator spacing="medium" />
+
+  <h5>{$i18n.follow_sns_topics.legacy_followees_header}</h5>
+
+  <ul class="list legacy-followings">
+    {#each followees as neuronId (subaccountToHexString(neuronId.id))}
+      <li>
+        <FollowSnsNeuronsByTopicLegacyFollowee {nsFunction} {neuronId} />
+      </li>
+    {/each}
+  </ul>
+
+  <div class="toolbar">
+    <button
+      class="secondary"
+      type="button"
+      data-tid="cancel-button"
+      onclick={cancel}
+    >
+      {$i18n.core.cancel}
+    </button>
+
+    <button data-tid="confirm-button" class="primary" onclick={confirm}>
+      {$i18n.core.confirm}
+    </button>
+  </div>
+</TestIdWrapper>
+
+<style lang="scss">
+  @use "@dfinity/gix-components/dist/styles/mixins/fonts";
+
+  .header {
+    margin: 0 var(--padding-4x);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: var(--padding-2x);
+
+    .description {
+      @include fonts.standard(true);
+      text-align: center;
+    }
+  }
+
+  h5 {
+    @include fonts.standard(true);
+    color: var(--text-description);
+    margin-bottom: var(--padding-1_5x);
+  }
+
+  .icon-wrapper {
+    padding: var(--padding-3x);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border-radius: 50%;
+    color: var(--elements-icons);
+    background-color: var(--tag-background);
+  }
+
+  .list {
+    margin-bottom: var(--padding-3x);
+    padding: 0;
+    list-style-type: none;
+
+    display: flex;
+    flex-wrap: wrap;
+    gap: var(--padding);
+  }
+</style>
