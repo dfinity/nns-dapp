12ea74f8abe147305c913a60b8bac33ba92f19f6
Yusef Habib <yusef.fernandez@dfinity.org>
2025-04-25 11:36:17 +0200

    feat(proposals): add SNS proposal topic filter modal (#6738)
    # Motivation
    
    We want users to be able to filter proposals by topic. This PR
    introduces a new modal component similar to `SnsFilterStatusModal` and
    `SnsFilterTypesModal`. It uses the `FilterModal` component to display a
    list of topics, enabling users to select which ones they want to filter
    out.
    
    [NNS1-3666](https://dfinity.atlassian.net/browse/NNS1-3666)
    
    # Changes
    
    - New modal to filter proposals by topic.
    
    # Tests
    
    - Unit tests for the new component
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    Not necessary.
    
    [NNS1-3666]:
    https://dfinity.atlassian.net/browse/NNS1-3666?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ frontend/src/lib/modals/sns/proposals/SnsFilterTopicsModal.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import FilterModal from "$lib/modals/common/FilterModal.svelte";
+  import { i18n } from "$lib/stores/i18n";
+  import { snsFiltersStore } from "$lib/stores/sns-filters.store";
+  import type { Filter, SnsProposalTopicFilterId } from "$lib/types/filters";
+  import type { Principal } from "@dfinity/principal";
+
+  type Props = {
+    rootCanisterId: Principal;
+    filters: Filter<SnsProposalTopicFilterId>[] | undefined;
+    closeModal: () => void;
+  };
+
+  const { rootCanisterId, filters = [], closeModal }: Props = $props();
+
+  const filtersValues = $derived<Filter<SnsProposalTopicFilterId>[]>(
+    filters.map((filter) => ({
+      ...filter,
+      checked: selectedFilters.includes(filter.value),
+    }))
+  );
+  let selectedFilters = $derived<SnsProposalTopicFilterId[]>(
+    filters.filter((item) => item.checked).map(({ value }) => value)
+  );
+
+  const filter = () => {
+    snsFiltersStore.setCheckTopics({
+      checkedTopics: selectedFilters,
+      rootCanisterId,
+    });
+    closeModal();
+  };
+
+  const onChange = ({
+    detail: { filter },
+  }: CustomEvent<{
+    filter: Filter<SnsProposalTopicFilterId> | undefined;
+  }>) => {
+    if (filter === undefined) return;
+    selectedFilters = [
+      ...selectedFilters.filter((status) => status !== filter?.value),
+      // Toggle the checked value
+      ...(filter.checked ? [] : [filter.value]),
+    ];
+  };
+  const clear = () => (selectedFilters = []);
+  const selectAll = () => (selectedFilters = filters.map(({ value }) => value));
+</script>
+
+<FilterModal
+  on:nnsClose={closeModal}
+  on:nnsConfirm={filter}
+  on:nnsChange={onChange}
+  on:nnsSelectAll={selectAll}
+  on:nnsClearSelection={clear}
+  filters={filtersValues}
+  category="topics"
+>
+  <span slot="title">{$i18n.voting.topics}</span>
+</FilterModal>
