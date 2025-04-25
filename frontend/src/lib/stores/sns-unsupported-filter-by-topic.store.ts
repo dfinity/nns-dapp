87f7e27a4ef2515a7544fc719df4745b7cffa3d5
Yusef Habib <yusef.fernandez@dfinity.org>
2025-04-25 17:35:52 +0200

    feat(proposals): add SNS topic filtering support store (#6740)
    # Motivation
    
    We want users to filter proposals by topic; however, not all SNS may
    have upgraded to the latest version, so not all will support this
    feature. Therefore, the canister added a new field to the
    [response](https://github.com/dfinity/ic/blob/579b8ba3a31341f354f4ddb3d60ac44548a91bc2/rs/sns/governance/canister/governance.did#L383)
    that can be used to determine which filter to display to the user:
    topics or types.
    
    This PR introduces a new store to track those projects that don't
    support yet this new API.
    
    Note: As of today, most existing projects support the new API. The
    approach is to track those that do not support it and update the UI
    accordingly.
    
    [NNS1-3666](https://dfinity.atlassian.net/browse/NNS1-3666)
    
    # Changes
    
    - New store to track a list of all sns that don't support the new API.
    
    # Tests
    
    - Unit tests for the new store.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    Not necessary.
    
    [NNS1-3666]:
    https://dfinity.atlassian.net/browse/NNS1-3666?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ frontend/src/lib/stores/sns-unsupported-filter-by-topic.store.ts
@@ -0,0 +1,44 @@
+import type { CanisterIdString } from "@dfinity/nns";
+import { get, writable, type Readable } from "svelte/store";
+
+export type UnsupportedFilterByTopicCanistersStoreData =
+  Array<CanisterIdString>;
+
+export interface UnsupportedFilterByTopicCanistersStore
+  extends Readable<UnsupportedFilterByTopicCanistersStoreData> {
+  add: (rootCanisterId: CanisterIdString) => void;
+  has: (rootCanisterId: CanisterIdString) => boolean;
+  delete: (rootCanisterId: CanisterIdString) => void;
+}
+
+export const initUnsupportedFilterByTopicSnsesStore =
+  (): UnsupportedFilterByTopicCanistersStore => {
+    const initialUnsupportedCanisters: Array<CanisterIdString> = [];
+
+    const { subscribe, update } =
+      writable<UnsupportedFilterByTopicCanistersStoreData>(
+        initialUnsupportedCanisters
+      );
+
+    return {
+      subscribe,
+      add: (rootCanisterId) => {
+        update((canisters) => {
+          if (canisters.includes(rootCanisterId)) return canisters;
+          return [...canisters, rootCanisterId];
+        });
+      },
+      has: (rootCanisterId) => {
+        const canisters = get({ subscribe });
+        return canisters.includes(rootCanisterId);
+      },
+      delete: (rootCanisterId) => {
+        update((canisters) => {
+          return [...canisters].filter((id) => id !== rootCanisterId);
+        });
+      },
+    };
+  };
+
+export const unsupportedFilterByTopicSnsesStore =
+  initUnsupportedFilterByTopicSnsesStore();
