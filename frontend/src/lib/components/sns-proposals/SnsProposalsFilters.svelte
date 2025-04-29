9dcd642e00e012fab29512f0f8e379287bfd4241
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-04-29 13:49:40 +0200

    new filter to UI

--- frontend/src/lib/components/sns-proposals/SnsProposalsFilters.svelte
+++ frontend/src/lib/components/sns-proposals/SnsProposalsFilters.svelte
@@ -6,15 +6,22 @@
   import { actionableProposalsActiveStore } from "$lib/derived/actionable-proposals.derived";
   import { authSignedInStore } from "$lib/derived/auth.derived";
   import { selectedUniverseIdStore } from "$lib/derived/selected-universe.derived";
+  import { createSnsTopicsProjectStore } from "$lib/derived/sns-topics.derived";
   import SnsFilterStatusModal from "$lib/modals/sns/proposals/SnsFilterStatusModal.svelte";
+  import SnsFilterTopicsModal from "$lib/modals/sns/proposals/SnsFilterTopicsModal.svelte";
   import SnsFilterTypesModal from "$lib/modals/sns/proposals/SnsFilterTypesModal.svelte";
+  import { ENABLE_SNS_TOPICS } from "$lib/stores/feature-flags.store";
   import { i18n } from "$lib/stores/i18n";
   import {
     snsFiltersStore,
     type ProjectFiltersStoreData,
   } from "$lib/stores/sns-filters.store";
+  import { unsupportedFilterByTopicSnsesStore } from "$lib/stores/sns-unsupported-filter-by-topic.store";
+  import { nonNullish } from "@dfinity/utils";
+  import { get } from "svelte/store";
 
-  type Filters = "types" | "status";
+  type Filters = "types" | "status" | "topics";
+
   let modal = $state<Filters | undefined>();
 
   const rootCanisterId = $derived($selectedUniverseIdStore);
@@ -24,6 +31,14 @@
 
   const openFilters = (filtersModal: Filters) => (modal = filtersModal);
   const closeModal = () => (modal = undefined);
+
+  const topics = $derived(get(createSnsTopicsProjectStore(rootCanisterId)));
+  const isTopicFilteringUnsupported = $derived(
+    unsupportedFilterByTopicSnsesStore.has(rootCanisterId.toText())
+  );
+  const isFilterByTopicVisible = $derived(
+    $ENABLE_SNS_TOPICS && nonNullish(topics) && !isTopicFilteringUnsupported
+  );
 </script>
 
 <TestIdWrapper testId="sns-proposals-filters-component">
@@ -34,15 +49,27 @@
 
     {#if !$actionableProposalsActiveStore}
       <FiltersWrapper>
-        <FiltersButton
-          testId="filters-by-types"
-          totalFilters={filtersStore?.types.length ?? 0}
-          activeFilters={filtersStore?.types.filter(({ checked }) => checked)
-            .length ?? 0}
-          on:nnsFilter={() => openFilters("types")}
-        >
-          {$i18n.voting.types}
-        </FiltersButton>
+        {#if isFilterByTopicVisible}
+          <FiltersButton
+            testId="filters-by-topics"
+            totalFilters={filtersStore?.topics.length ?? 0}
+            activeFilters={filtersStore?.topics.filter(({ checked }) => checked)
+              .length ?? 0}
+            on:nnsFilter={() => openFilters("topics")}
+          >
+            {$i18n.voting.topics}
+          </FiltersButton>
+        {:else}
+          <FiltersButton
+            testId="filters-by-types"
+            totalFilters={filtersStore?.types.length ?? 0}
+            activeFilters={filtersStore?.types.filter(({ checked }) => checked)
+              .length ?? 0}
+            on:nnsFilter={() => openFilters("types")}
+          >
+            {$i18n.voting.types}
+          </FiltersButton>
+        {/if}
         <FiltersButton
           testId="filters-by-status"
           totalFilters={filtersStore?.decisionStatus.length ?? 0}
@@ -61,6 +88,14 @@
       filters={filtersStore?.types}
       {rootCanisterId}
       {closeModal}
+    />
+  {/if}
+
+  {#if modal === "topics"}
+    <SnsFilterTopicsModal
+      filters={filtersStore?.topics}
+      {closeModal}
+      {rootCanisterId}
     />
   {/if}
 
