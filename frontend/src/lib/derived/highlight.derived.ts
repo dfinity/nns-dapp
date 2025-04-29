3a94608c0b32743c364e8507e2970dfdea68ee2c
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-04-29 17:33:48 +0200

    derived store

---
+++ frontend/src/lib/derived/highlight.derived.ts
@@ -0,0 +1,23 @@
+import { authSignedInStore } from "$lib/derived/auth.derived";
+import { snsProjectsRecordStore } from "$lib/derived/sns/sns-projects.derived";
+import { tokensListUserStore } from "$lib/derived/tokens-list-user.derived";
+import { isUserTokenData } from "$lib/utils/user-token.utils";
+import { nonNullish, TokenAmountV2 } from "@dfinity/utils";
+import { derived, type Readable } from "svelte/store";
+
+export const shouldShowHighlightForSnsTopics: Readable<boolean> = derived(
+  [authSignedInStore, tokensListUserStore, snsProjectsRecordStore],
+  ([isSignedIn, tokensListUserStore, snsProjectsRecordStore]) => {
+    const listOfSnsTokensWithBalance = tokensListUserStore
+      .filter(isUserTokenData)
+      .filter(
+        ({ balance }) =>
+          balance instanceof TokenAmountV2 && balance.toUlps() > 0n
+      )
+      .filter(({ universeId }) =>
+        nonNullish(snsProjectsRecordStore?.[universeId.toText()])
+      );
+
+    return isSignedIn && listOfSnsTokensWithBalance.length > 0;
+  }
+);
