ef88b5df65d675ae5f0c37a54180c0f406fb1ecb
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-04-30 09:16:17 +0200

    cleanup

--- frontend/src/lib/modals/sns/neurons/FollowSnsNeuronsByTopicModal.svelte
+++ frontend/src/lib/modals/sns/neurons/FollowSnsNeuronsByTopicModal.svelte
@@ -135,7 +135,7 @@
     })
   );
 
-  const selectedTopicsContainLegacyFollowee = $derived<boolean>(
+  const selectedTopicsContainLegacyFollowee: boolean = $derived(
     getLegacyFolloweesByTopics({
       neuron,
       topicInfos: topicInfos.filter((topicInfo) =>
