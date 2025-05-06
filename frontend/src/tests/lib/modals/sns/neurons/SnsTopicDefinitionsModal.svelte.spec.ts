e54f47bc199714c99ff1c88b8e7f44fd1e74fba3
Yusef Habib <yusef.fernandez@dfinity.org>
2025-05-06 13:02:24 +0200

    feat(topics): implement topic priority sorting in store (#6788)
    # Motivation
    
    We want to apply the same sorting used for the proposals filter #6745 in
    other parts of the application.
    
    This PR implements the sorting logic directly in the derived store.
    Since there is no need to retain the original data, we can apply it
    directly to the store and avoid duplication.
    
    The sorting logic is as follows:
    - First, critical topics are sorted alphabetically.
    - Second, non-critical topics are sorted alphabetically.
    
    [NNS1-3792](https://dfinity.atlassian.net/browse/NNS1-3792)
    
    
    https://github.com/user-attachments/assets/0660382f-3027-4f7b-b32f-c5dc0e42bfa5
    
    # Changes
    
    - Introduce sorting logic to `createSnsTopicsProjectStore`.
    
    # Tests
    
    - Add test to validate new sorting logic.
    - Fix existing tests to easily visualize sorting.
    - Tested in beta.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    Not necessary.
    
    [NNS1-3792]:
    https://dfinity.atlassian.net/browse/NNS1-3792?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- frontend/src/tests/lib/modals/sns/neurons/SnsTopicDefinitionsModal.svelte.spec.ts
+++ frontend/src/tests/lib/modals/sns/neurons/SnsTopicDefinitionsModal.svelte.spec.ts
@@ -1,5 +1,4 @@
 import SnsTopicDefinitionsModal from "$lib/modals/sns/neurons/SnsTopicDefinitionsModal.svelte";
-import type { SnsTopicKey } from "$lib/types/sns";
 import { principal } from "$tests/mocks/sns-projects.mock";
 import { topicInfoDtoMock } from "$tests/mocks/sns-topics.mock";
 import { SnsTopicDefinitionsModalPo } from "$tests/page-objects/SnsTopicDefinitionsModal.page-object";
@@ -9,14 +8,6 @@ import { render } from "@testing-library/svelte";
 
 describe("SnsTopicDefinitionsModal", () => {
   const rootCanisterId = principal(1);
-  const criticalTopicKey1: SnsTopicKey = "CriticalDappOperations";
-  const criticalTopicName1 = "Critical Dapp Operations";
-  const criticalTopicKey2: SnsTopicKey = "TreasuryAssetManagement";
-  const criticalTopicName2 = "Treasury Asset Management";
-  const topicKey1: SnsTopicKey = "DaoCommunitySettings";
-  const topicName1 = "Dao Community Settings";
-  const topicKey2: SnsTopicKey = "ApplicationBusinessLogic";
-  const topicName2 = "Application Business Logic";
 
   const renderComponent = ({
     onClose = () => {},
@@ -39,26 +30,26 @@ describe("SnsTopicDefinitionsModal", () => {
         topics: {
           topics: [
             topicInfoDtoMock({
-              topic: criticalTopicKey1,
-              name: criticalTopicName1,
+              topic: "CriticalDappOperations",
+              name: "CriticalDappOperations",
               description: "",
               isCritical: true,
             }),
             topicInfoDtoMock({
-              topic: criticalTopicKey2,
-              name: criticalTopicName2,
+              topic: "TreasuryAssetManagement",
+              name: "TreasuryAssetManagement",
               description: "",
               isCritical: true,
             }),
             topicInfoDtoMock({
-              topic: topicKey1,
-              name: topicName1,
+              topic: "DaoCommunitySettings",
+              name: "DaoCommunitySettings",
               description: "",
               isCritical: false,
             }),
             topicInfoDtoMock({
-              topic: topicKey2,
-              name: topicName2,
+              topic: "ApplicationBusinessLogic",
+              name: "ApplicationBusinessLogic",
               description: "",
               isCritical: false,
             }),
@@ -73,13 +64,13 @@ describe("SnsTopicDefinitionsModal", () => {
     const po = renderComponent();
 
     expect([...(await po.getCriticalTopicNames())]).toEqual([
-      criticalTopicName1,
-      criticalTopicName2,
+      "CriticalDappOperations",
+      "TreasuryAssetManagement",
     ]);
 
     expect(await po.getNonCriticalTopicNames()).toEqual([
-      topicName1,
-      topicName2,
+      "ApplicationBusinessLogic",
+      "DaoCommunitySettings",
     ]);
   });
 
