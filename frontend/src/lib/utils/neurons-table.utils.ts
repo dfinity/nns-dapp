db64ec25aaaf4cb48d9487a7a953e47a24002da4
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-05-14 14:18:11 +0200

    cleanup

--- frontend/src/lib/utils/neurons-table.utils.ts
+++ frontend/src/lib/utils/neurons-table.utils.ts
@@ -198,10 +198,6 @@ export const tableNeuronsFromSnsNeurons = ({
       amount: stake,
       tokenPrice,
     });
-    const voteDelegationState = getSnsNeuronVoteDelegationState({
-      topicCount: topicInfos.length,
-      neuron: snsNeuron,
-    });
     return {
       rowHref,
       domKey: neuronIdString,
