78d91fb07f2f87b5a4fc2c31fae137ace7b9236c
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-05-14 18:50:19 +0200

    Make voteDelegationState not optional

--- frontend/src/tests/lib/components/neurons/NeuronsTable/NeuronsTable.svelte.spec.ts
+++ frontend/src/tests/lib/components/neurons/NeuronsTable/NeuronsTable.svelte.spec.ts
@@ -28,6 +28,7 @@ describe("NeuronsTable", () => {
     rowHref: "/neurons/10",
     domKey: "10",
     neuronId: "10",
+    voteDelegationState: "none",
   };
 
   const neuron2: TableNeuron = {
@@ -35,6 +36,7 @@ describe("NeuronsTable", () => {
     rowHref: "/neurons/99",
     domKey: "99",
     neuronId: "99",
+    voteDelegationState: "some",
   };
 
   const neuron3: TableNeuron = {
@@ -42,6 +44,7 @@ describe("NeuronsTable", () => {
     rowHref: "/neurons/200",
     domKey: "200",
     neuronId: "200",
+    voteDelegationState: "all",
   };
 
   const neuron4: TableNeuron = {
@@ -95,6 +98,8 @@ describe("NeuronsTable", () => {
       "",
       "Maturity",
       "",
+      "Vote Delegation",
+      "",
       "Dissolve Delay",
       "",
       "State",
@@ -142,6 +147,8 @@ describe("NeuronsTable", () => {
       expect.any(String), // gap
       "desktop-align-left", // Dissolve Delay
       expect.any(String), // gap
+      "desktop-align-left", // Vote Delegation
+      expect.any(String), // gap
       "desktop-align-left", // State
       "desktop-align-right", // Actions
     ]);
@@ -160,12 +167,14 @@ describe("NeuronsTable", () => {
         "1fr", // gap
         "max-content", // State
         "1fr", // gap
+        "max-content", // Vote Delegation
+        "1fr", // gap
         "max-content", // Dissolve Delay
         "max-content", // Actions
       ].join(" ")
     );
     expect(await po.getMobileGridTemplateAreas()).toBe(
-      '"first-cell last-cell" "cell-1 cell-1" "cell-3 cell-3" "cell-5 cell-5" "cell-7 cell-7"'
+      '"first-cell last-cell" "cell-1 cell-1" "cell-3 cell-3" "cell-5 cell-5" "cell-7 cell-7" "cell-9 cell-9"'
     );
   });
 
