084d99bd2ca43a6dd70d2bedc953f1874a30292b
Yusef Habib <yusef.fernandez@dfinity.org>
2025-05-19 12:33:42 +0200

    feat(balances): add balance display settings store (#6838)
    # Motivation
    
    We want to introduce a toggle that allows users to hide or show their
    balances on the main pages. This PR introduces a new writable store,
    which is a store that is saved in local storage, to track the user's
    selection of `hide/show`.
    
    [NNS1-3721](https://dfinity.atlassian.net/browse/NNS1-3721)
    
    # Changes
    
    - Add new writable store.
    
    # Tests
    
    - Add unit tests for the store.
    
    # Todos
    
    - [ ] Add entry to changelog (if necessary).
    Not necessary.
    
    [NNS1-3721]:
    https://dfinity.atlassian.net/browse/NNS1-3721?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- frontend/src/lib/constants/stores.constants.ts
+++ frontend/src/lib/constants/stores.constants.ts
@@ -8,6 +8,7 @@ export enum StoreLocalStorageKey {
   HideZeroBalances = "nnsHideZeroBalanceTokens",
   HideZeroNeurons = "nnsHideZeroNeuronProjects",
   HighlightDisplay = "nnsHighlightDisplay-",
+  BalancePrivacyMode = "nnsBalancePrivacyMode",
 }
 
 export const NOT_LOADED = Symbol("NOT_LOADED");
