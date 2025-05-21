6ab6ec6b489c30878c466aac2aedfb2740dbe32e
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-05-21 16:45:51 +0200

    new component

---
+++ frontend/src/lib/components/ui/PrivacyAwareAmount.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { isBalancePrivacyOptionStore } from "$lib/derived/balance-privacy-active.derived";
+  import { renderPrivacyModeBalance } from "$lib/utils/format.utils";
+
+  type Props = {
+    value: string;
+    length: number;
+  };
+
+  const { value, length }: Props = $props();
+
+  const renderedValue = $derived(
+    $isBalancePrivacyOptionStore ? renderPrivacyModeBalance(length) : value
+  );
+</script>
+
+{renderedValue}
