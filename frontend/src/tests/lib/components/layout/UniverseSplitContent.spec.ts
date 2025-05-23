a318da82e843b74b5994a48498dfddb55b3ce492
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-05-23 16:46:47 +0200

    fix test

--- frontend/src/tests/lib/components/layout/UniverseSplitContent.spec.ts
+++ frontend/src/tests/lib/components/layout/UniverseSplitContent.spec.ts
@@ -1,9 +1,17 @@
 import UniverseSplitContent from "$lib/components/layout/UniverseSplitContent.svelte";
 import { layoutTitleStore } from "$lib/stores/layout.store";
 import { resetIdentity, setNoIdentity } from "$tests/mocks/auth.store.mock";
+import { createMockSnippet } from "$tests/mocks/snippet.mock";
 import { render } from "@testing-library/svelte";
 
 describe("UniverseSplitContent", () => {
+  const renderComponent = () => {
+    return render(UniverseSplitContent, {
+      props: {
+        children: createMockSnippet(),
+      },
+    });
+  };
   beforeEach(() => {
     resetIdentity();
 
@@ -13,12 +21,12 @@ describe("UniverseSplitContent", () => {
   });
 
   it("should render the universe nav", () => {
-    const { getByTestId } = render(UniverseSplitContent);
+    const { getByTestId } = renderComponent();
     expect(getByTestId("select-universe-nav-title")).not.toBeNull();
   });
 
   it("should render a header", () => {
-    const { getByText } = render(UniverseSplitContent);
+    const { getByText } = renderComponent();
 
     expect(getByText("the header")).toBeInTheDocument();
   });
@@ -26,14 +34,14 @@ describe("UniverseSplitContent", () => {
   it("should render the login button in the header", () => {
     setNoIdentity();
 
-    const { getByTestId } = render(UniverseSplitContent);
+    const { getByTestId } = renderComponent();
     expect(getByTestId("toolbar-login")).not.toBeNull();
   });
 
   it("should render the account menu", () => {
     resetIdentity();
 
-    const { getByTestId } = render(UniverseSplitContent);
+    const { getByTestId } = renderComponent();
     expect(getByTestId("account-menu")).not.toBeNull();
   });
 });
