f83d0d7ce30f1ca9bca9705a7696f0931d8834ac
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-05-28 10:49:25 +0200

    cleanup

--- frontend/src/lib/modals/neurons/NnsDisburseMaturityModal.svelte
+++ frontend/src/lib/modals/neurons/NnsDisburseMaturityModal.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import { OWN_CANISTER_ID } from "$lib/constants/canister-ids.constants";
   import DisburseMaturityModal from "$lib/modals/neurons/DisburseMaturityModal.svelte";
-  import { disburseMaturity } from "$lib/services/neurons.services";
+  import { disburseMaturity as disburseMaturityService } from "$lib/services/neurons.services";
   import { startBusy, stopBusy } from "$lib/stores/busy.store";
   import { toastsSuccess } from "$lib/stores/toasts.store";
   import type { NeuronId, NeuronInfo } from "@dfinity/nns";
   import { ICPToken } from "@dfinity/utils";
-  import { createEventDispatcher } from "svelte";
   import { get } from "svelte/store";
-  import { icpAccountsStore } from "../../derived/icp-accounts.derived";
+  import { icpAccountsStore } from "$lib/derived/icp-accounts.derived";
+  import { mainTransactionFeeE8sStore } from "../../derived/main-transaction-fee.derived";
 
   type Props = {
     neuron: NeuronInfo;
@@ -18,13 +18,10 @@
 
   const { neuron, neuronId, close }: Props = $props();
 
-  // TODO(disburse-maturity): use the real fee (networkEconomics?)
-  const minimumAmountE8s = $derived(0n); // minimumAmountToDisburseMaturity(ICPToken?.fee ?? 0n);
+  const minimumAmountE8s = $derived($mainTransactionFeeE8sStore);
 
-  const dispatcher = createEventDispatcher();
-
-  // TODO(disburse-maturity): add validation (isIcpAccountIdentifier)
-  const disburseMaturity2 = async ({
+  // TODO(disburse-maturity): ¿add validation (isIcpAccountIdentifier)
+  const disburseMaturity = async ({
     detail: { percentageToDisburse, destinationAddress },
   }: CustomEvent<{
     percentageToDisburse: number;
@@ -48,18 +45,14 @@
     //   });
     // }
 
-    // log account and subAccount
-    console.log("Disburse maturity account", account);
-    console.log("Disburse maturity subAccount", subAccount);
-
     startBusy({ initiator: "disburse-maturity" });
 
     // TODO(disburse-maturity): switch to account identifier when API supports it
-    await disburseMaturity({
+    await disburseMaturityService({
       neuronId,
       percentageToDisburse,
-      account,
-      subAccount,
+      // account,
+      // subAccount,
     });
 
     console.log("Disburse maturity success", neuron.fullNeuron);
@@ -81,7 +74,7 @@
 <DisburseMaturityModal
   {availableMaturityE8s}
   {minimumAmountE8s}
-  on:nnsDisburseMaturity={disburseMaturity2}
+  on:nnsDisburseMaturity={disburseMaturity}
   rootCanisterId={OWN_CANISTER_ID}
   token={ICPToken}
   on:nnsClose={close}
