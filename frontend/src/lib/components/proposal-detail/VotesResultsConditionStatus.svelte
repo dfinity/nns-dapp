633b63469cd45a3294438e82e6eaee8efa79ac88
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-05-28 08:16:44 +0200

    car

--- frontend/src/lib/components/proposal-detail/VotesResultsConditionStatus.svelte
+++ frontend/src/lib/components/proposal-detail/VotesResultsConditionStatus.svelte
@@ -23,28 +23,29 @@
   }: Props = $props();
 </script>
 
-<Collapsible
-  expandButton={false}
-  externalToggle={true}
-  bind:toggleContent
-  wrapHeight
->
+<Collapsible expandButton={false} externalToggle bind:toggleContent wrapHeight>
   <div slot="header" class="wrapper" data-tid={testId}>
     {@render title()}
-
-    {#if status === "default"}
-      <span class="icon default" data-tid="vote-result-condition-default-icon">
-        <IconCheckCircleV2 size={20} />
-      </span>
-    {:else if status === "success"}
-      <span class="icon success" data-tid="vote-result-condition-success-icon">
-        <IconCheckCircleFill size={20} />
-      </span>
-    {:else if status === "failed"}
-      <span class="icon failed" data-tid="vote-result-condition-failed-icon">
-        <IconCloseCircleFill size={20} />
-      </span>
-    {/if}
+    <span
+      class="icon"
+      data-tid={`${testId}-status`}
+      data-status={status}
+      aria-label={`Condition status: ${status}`}
+    >
+      {#if status === "default"}
+        <span class="default" aria-hidden="true">
+          <IconCheckCircleV2 size={20} />
+        </span>
+      {:else if status === "success"}
+        <span class="success" aria-hidden="true">
+          <IconCheckCircleFill size={20} />
+        </span>
+      {:else if status === "failed"}
+        <span class="failed" aria-hidden="true">
+          <IconCloseCircleFill size={20} />
+        </span>
+      {/if}
+    </span>
   </div>
   {@render children()}
 </Collapsible>
@@ -56,17 +57,17 @@
     justify-content: space-between;
     flex-grow: 1;
 
-    .icon.default {
-      /* TODO: Add to GIX once it is part of the design system */
-      color: #bec2d6;
-    }
-
-    .icon.success {
-      color: var(--positive-emphasis);
-    }
-
-    .icon.failed {
-      color: var(--negative-emphasis);
+    .icon {
+      .default {
+        /* TODO: Add to GIX once it is part of the design system */
+        color: #bec2d6;
+      }
+      .success {
+        color: var(--positive-emphasis);
+      }
+      .failed {
+        color: var(--negative-emphasis);
+      }
     }
   }
 </style>
