834d3fb743146cbbcdb4f983d7394b1491e76840
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-06-01 08:16:57 +0200

    refactor: reuse DisburseMaturityButton

--- frontend/src/lib/components/neuron-detail/actions/NnsDisburseMaturityButton.svelte
+++ frontend/src/lib/components/neuron-detail/actions/NnsDisburseMaturityButton.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import TestIdWrapper from "$lib/components/common/TestIdWrapper.svelte";
+  import DisburseMaturityButton from "$lib/components/neuron-detail/actions/DisburseMaturityButton.svelte";
   import {
     MATURITY_MODULATION_VARIANCE_PERCENTAGE,
     MINIMUM_DISBURSEMENT,
@@ -10,7 +10,6 @@
   import { replacePlaceholders } from "$lib/utils/i18n.utils";
   import { openNnsNeuronModal } from "$lib/utils/modals.utils";
   import { isEnoughMaturityToDisburse } from "$lib/utils/neuron.utils";
-  import { Tooltip } from "@dfinity/gix-components";
   import type { NeuronInfo } from "@dfinity/nns";
   import { nonNullish } from "@dfinity/utils";
 
@@ -21,17 +20,37 @@
   const { neuron }: Props = $props();
   const enoughMaturity = $derived(
     nonNullish(neuron.fullNeuron) &&
-      // Same logic as in SpawnNeuronButton
       isEnoughMaturityToDisburse({
         neuron,
         percentage: 100,
       })
   );
-  const minIcpEquivalent =
-    Number(MINIMUM_DISBURSEMENT) /
-    ULPS_PER_MATURITY /
-    MATURITY_MODULATION_VARIANCE_PERCENTAGE;
-
+  const getDisabledText = () => {
+    const minIcpEquivalent =
+      Number(MINIMUM_DISBURSEMENT) /
+      ULPS_PER_MATURITY /
+      MATURITY_MODULATION_VARIANCE_PERCENTAGE;
+    return replacePlaceholders(
+      $i18n.neuron_detail.disburse_maturity_disabled_tooltip,
+      {
+        $amount: formatNumber(minIcpEquivalent, {
+          minFraction: 4,
+          maxFraction: 4,
+        }),
+        $min: formatNumber(Number(MINIMUM_DISBURSEMENT) / ULPS_PER_MATURITY, {
+          minFraction: 0,
+          maxFraction: 0,
+        }),
+        $variability: formatPercentage(
+          MATURITY_MODULATION_VARIANCE_PERCENTAGE,
+          {
+            minFraction: 0,
+            maxFraction: 0,
+          }
+        ),
+      }
+    );
+  };
   const showModal = () =>
     openNnsNeuronModal({
       type: "disburse-maturity",
@@ -39,42 +58,7 @@
     });
 </script>
 
-<TestIdWrapper testId="disburse-maturity-button-component">
-  {#if enoughMaturity}
-    <button
-      data-tid="disburse-maturity-button"
-      class="secondary"
-      onclick={showModal}
-    >
-      {$i18n.neuron_detail.disburse_maturity}
-    </button>
-  {:else}
-    <Tooltip
-      id="disburse-maturity-button"
-      text={replacePlaceholders(
-        $i18n.neuron_detail.disburse_maturity_disabled_tooltip,
-        {
-          $amount: formatNumber(minIcpEquivalent, {
-            minFraction: 4,
-            maxFraction: 4,
-          }),
-          $min: formatNumber(Number(MINIMUM_DISBURSEMENT) / ULPS_PER_MATURITY, {
-            minFraction: 0,
-            maxFraction: 0,
-          }),
-          $variability: formatPercentage(
-            MATURITY_MODULATION_VARIANCE_PERCENTAGE,
-            {
-              minFraction: 0,
-              maxFraction: 0,
-            }
-          ),
-        }
-      )}
-    >
-      <button data-tid="disburse-maturity-button" disabled class="secondary">
-        {$i18n.neuron_detail.disburse_maturity}
-      </button>
-    </Tooltip>
-  {/if}
-</TestIdWrapper>
+<DisburseMaturityButton
+  on:click={showModal}
+  disabledText={enoughMaturity ? undefined : getDisabledText()}
+/>
