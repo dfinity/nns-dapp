024bc877a9962e5f7a6715926168a72bc728cfd8
Max Strasinsky <98811342+mstrasinskis@users.noreply.github.com>
2025-06-12 14:10:55 +0200

    feat(disburse maturity): Limit active disbursements (#6954)
    # Motivation
    
    There is a limit on how many disbursements a neuron can have in progress
    (maximum 10). This PR disables the button when the limit is reached and
    shows a tooltip explaining why.
    
    # Changes
    
    - Disable the button when the limit is reached.
    
    # Tests
    
    - Added tests.
    
    # Todos
    
    - [x] Accessibility (a11y) – any impact?
    - [x] Changelog – is it needed?

--- frontend/src/lib/components/neuron-detail/actions/NnsDisburseMaturityButton.svelte
+++ frontend/src/lib/components/neuron-detail/actions/NnsDisburseMaturityButton.svelte
@@ -2,6 +2,7 @@
   import DisburseMaturityButton from "$lib/components/neuron-detail/actions/DisburseMaturityButton.svelte";
   import {
     MATURITY_MODULATION_VARIANCE_PERCENTAGE,
+    MAX_DISBURSEMENTS_IN_PROGRESS,
     MIN_DISBURSEMENT_WITH_VARIANCE_ICP,
     MINIMUM_DISBURSEMENT,
     ULPS_PER_MATURITY,
@@ -26,8 +27,13 @@
         percentage: 100,
       })
   );
-  const getDisabledText = () => {
-    return replacePlaceholders(
+  const isMaximumDisbursementsReached = $derived.by(() => {
+    const activeDisbursements =
+      neuron.fullNeuron?.maturityDisbursementsInProgress ?? [];
+    return activeDisbursements.length >= MAX_DISBURSEMENTS_IN_PROGRESS;
+  });
+  const getNotEnoughMaturityDisabledText = () =>
+    replacePlaceholders(
       $i18n.neuron_detail.disburse_maturity_disabled_tooltip,
       {
         $amount: formatNumber(MIN_DISBURSEMENT_WITH_VARIANCE_ICP, {
@@ -47,7 +53,20 @@
         ),
       }
     );
-  };
+  const getMaximumReachedDisabledText = () =>
+    replacePlaceholders(
+      $i18n.neuron_detail.stake_maturity_disabled_tooltip_max_disbursements,
+      {
+        $maxDisbursements: `${MAX_DISBURSEMENTS_IN_PROGRESS}`,
+      }
+    );
+  const disabledText = $derived(
+    !enoughMaturity
+      ? getNotEnoughMaturityDisabledText()
+      : isMaximumDisbursementsReached
+        ? getMaximumReachedDisabledText()
+        : undefined
+  );
   const showModal = () =>
     openNnsNeuronModal({
       type: "disburse-maturity",
@@ -55,7 +74,4 @@
     });
 </script>
 
-<DisburseMaturityButton
-  on:click={showModal}
-  disabledText={enoughMaturity ? undefined : getDisabledText()}
-/>
+<DisburseMaturityButton on:click={showModal} {disabledText} />
