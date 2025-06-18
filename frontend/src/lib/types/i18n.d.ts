64235bffc51676201c03c8224f2b1d7b6cfe1bf8
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-06-18 08:44:37 +0200

    Remove sns topics hightlight

--- frontend/src/lib/types/i18n.d.ts
+++ frontend/src/lib/types/i18n.d.ts
@@ -1364,11 +1364,6 @@ interface I18nPortfolio {
   previous_card: string;
 }
 
-interface I18nHighlight {
-  topics_feature_title: string;
-  topics_feature_description: string;
-}
-
 interface I18nNeuron_state {
   Unspecified: string;
   Locked: string;
@@ -1663,7 +1658,6 @@ interface I18n {
   tokens: I18nTokens;
   import_token: I18nImport_token;
   portfolio: I18nPortfolio;
-  highlight: I18nHighlight;
   neuron_state: I18nNeuron_state;
   topics: I18nTopics;
   topics_description: I18nTopics_description;
