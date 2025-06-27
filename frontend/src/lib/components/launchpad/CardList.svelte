9e42daa0ff8166312021141cad4f5fdb11454e22
Max Strasinsky <98811342+mstrasinskis@users.noreply.github.com>
2025-06-27 17:50:01 +0200

    feat(launchpad2): CardList component (#7021)
    # Motivation
    
    For the Launchpad redesign, a new card list component is required. This
    PR adds a basic version of the component, which will later be extended
    with optional horizontal scrolling on mobile.
    
    https://dfinity.atlassian.net/browse/NNS1-3906
    
    # Changes
    
    - New component.
    
    # Tests
    
    - Added.
    
    # Todos
    
    - [ ] Accessibility (a11y) – any impact?
    - [ ] Changelog – is it needed?

---
+++ frontend/src/lib/components/launchpad/CardList.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import type { ComponentWithProps } from "$lib/types/svelte";
+
+  type Props = {
+    cards: ComponentWithProps[];
+  };
+  const { cards }: Props = $props();
+</script>
+
+<ul data-tid="card-list-component">
+  {#each cards as { Component, props }}
+    <li data-tid="card-entry">
+      <Component {...props} />
+    </li>
+  {/each}
+</ul>
+
+<style lang="scss">
+  @use "@dfinity/gix-components/dist/styles/mixins/media";
+
+  ul {
+    // reset default styles
+    list-style: none;
+    margin: 0;
+    padding: 0;
+
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: var(--padding);
+
+    @include media.min-width(medium) {
+      grid-template-columns: repeat(2, 1fr);
+      gap: var(--padding-2x);
+    }
+
+    @include media.min-width(large) {
+      grid-template-columns: repeat(3, 1fr);
+    }
+  }
+</style>
