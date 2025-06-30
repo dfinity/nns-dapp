9d8042baff9308f74f3a5c2a9baf1f70aa94db8b
David Dal Busco <david.dalbusco@dfinity.org>
2025-06-30 11:19:37 +0300

    feat: upgrade ic-js next and adapt imports
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- frontend/src/tests/lib/components/accounts/HardwareWalletInfo.spec.ts
+++ frontend/src/tests/lib/components/accounts/HardwareWalletInfo.spec.ts
@@ -1,7 +1,7 @@
 import HardwareWalletInfo from "$lib/components/accounts/HardwareWalletInfo.svelte";
 import { mockIdentity } from "$tests/mocks/auth.store.mock";
 import en from "$tests/mocks/i18n.mock";
-import { principalToAccountIdentifier } from "@dfinity/ledger-icp";
+import { principalToAccountIdentifier } from "@dfinity/nns";
 import { render } from "@testing-library/svelte";
 
 describe("HardwareWalletInfo", () => {
