5407904f14efb3911e0e8c2766578a1e2846f1bb
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-07-02 15:25:54 +0200

    IconSearch

---
+++ frontend/src/lib/components/ui/icons/IconSearch.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { DEFAULT_ICON_SIZE } from "@dfinity/gix-components";
+
+  export let size = `${DEFAULT_ICON_SIZE}px`;
+</script>
+
+<svg
+  height={size}
+  width={size}
+  viewBox="0 0 20 20"
+  fill="none"
+  stroke="currentColor"
+  xmlns="http://www.w3.org/2000/svg"
+  data-tid="document"
+>
+  <path
+    d="M17.5 17.5L14.5834 14.5833M16.6667 9.58333C16.6667 13.4954 13.4954 16.6667 9.58333 16.6667C5.67132 16.6667 2.5 13.4954 2.5 9.58333C2.5 5.67132 5.67132 2.5 9.58333 2.5C13.4954 2.5 16.6667 5.67132 16.6667 9.58333Z"
+    stroke-width="1.66667"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+</svg>
