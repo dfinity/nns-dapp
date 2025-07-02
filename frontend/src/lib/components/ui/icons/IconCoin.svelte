e8988a2a701102e71a7e3b769332702ef57e01e4
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-07-02 15:25:48 +0200

    IconCoin

---
+++ frontend/src/lib/components/ui/icons/IconCoin.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { DEFAULT_ICON_SIZE } from "@dfinity/gix-components";
+
+  export let size = `${DEFAULT_ICON_SIZE}px`;
+</script>
+
+<svg
+  height={size}
+  width={size}
+  viewBox="0 0 16 16"
+  fill="none"
+  stroke="currentColor"
+  xmlns="http://www.w3.org/2000/svg"
+  data-tid="document"
+>
+  <path
+    d="M8 9.5C11.5899 9.5 14.5 8.15685 14.5 6.5C14.5 4.84315 11.5899 3.5 8 3.5C4.41015 3.5 1.5 4.84315 1.5 6.5C1.5 8.15685 4.41015 9.5 8 9.5Z"
+    stroke-width="1.2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M8 9.5V12.5"
+    stroke-width="1.2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M1.5 6.5V9.5C1.5 11 4 12.5 8 12.5C12 12.5 14.5 11 14.5 9.5V6.5"
+    stroke-width="1.2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M12 8.88086V11.8809"
+    stroke-width="1.2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M4 8.88086V11.8809"
+    stroke-width="1.2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+</svg>
