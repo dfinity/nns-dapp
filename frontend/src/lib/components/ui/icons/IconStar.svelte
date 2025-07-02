b90d4d1ac1979e09fbde37f5eb9a3e63936fd037
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-07-02 15:26:03 +0200

    IconStar

---
+++ frontend/src/lib/components/ui/icons/IconStar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { DEFAULT_ICON_SIZE } from "@dfinity/gix-components";
+
+  export let size = `${DEFAULT_ICON_SIZE}px`;
+</script>
+
+<svg
+  height={size}
+  width={size}
+  viewBox="0 0 20 20"
+  fill="none"
+  stroke="currentColor"
+  xmlns="http://www.w3.org/2000/svg"
+  data-tid="document"
+>
+  <path
+    d="M10.333 2.33301L12.805 7.26992L18.333 8.06645L14.333 11.9071L15.277 17.333L10.333 14.7699L5.38901 17.333L6.33301 11.9071L2.33301 8.06645L7.86101 7.26992L10.333 2.33301Z"
+    stroke-width="1.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+</svg>
