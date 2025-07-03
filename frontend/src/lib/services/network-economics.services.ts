706c13322ba88858df2862a84b3bb2a95cc74c17
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-07-03 17:15:46 +0200

    migrate to query and update

--- frontend/src/lib/services/network-economics.services.ts
+++ frontend/src/lib/services/network-economics.services.ts
@@ -1,23 +1,24 @@
 import { governanceApiService } from "$lib/api-services/governance.api-service";
-import { getAuthenticatedIdentity } from "$lib/services/auth.services";
+import { FORCE_CALL_STRATEGY } from "$lib/constants/mockable.constants";
+import { queryAndUpdate } from "$lib/services/utils.services";
 import { networkEconomicsStore } from "$lib/stores/network-economics.store";
+import type { NetworkEconomics } from "@dfinity/nns";
 
 export const loadNetworkEconomicsParameters = async (): Promise<void> => {
-  try {
-    const identity = await getAuthenticatedIdentity();
-    const certified = true;
-    const parameters = await governanceApiService.getNetworkEconomicsParameters(
-      {
-        identity,
+  return queryAndUpdate<NetworkEconomics, unknown>({
+    strategy: FORCE_CALL_STRATEGY,
+    request: (options) =>
+      governanceApiService.getNetworkEconomicsParameters(options),
+    onLoad: ({ response: parameters, certified }) => {
+      networkEconomicsStore.setParameters({
+        parameters,
         certified,
-      }
-    );
-
-    networkEconomicsStore.setParameters({
-      parameters,
-      certified,
-    });
-  } catch (error) {
-    console.error(error);
-  }
+      });
+    },
+    onError: ({ error: err }) => {
+      console.error(err);
+      // We swallow the error because the reward event is not critical to the app.
+    },
+    logMessage: "Getting latest reward event for NNS",
+  });
 };
