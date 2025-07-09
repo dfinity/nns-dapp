204ef4d7e539715051906c3c2ee1bc76eff13464
Francesco <francesco.rizzi@dfinity.org>
2025-07-09 13:23:37 +0200

    merge main

--- frontend/src/lib/services/nns-total-voting-power.service.ts
+++ frontend/src/lib/services/nns-total-voting-power.service.ts
@@ -3,13 +3,14 @@ import { getCurrentIdentity } from "$lib/services/auth.services";
 import { nnsTotalVotingPower } from "$lib/stores/nns-total-voting-power.store";
 
 export const loadNnsTotalVotingPower = async (): Promise<void> => {
-  const _proposals = await queryProposals({
+  const proposals = await queryProposals({
     beforeProposal: undefined,
     identity: getCurrentIdentity(),
     certified: false,
   });
 
-  // nnsTotalVotingPower.set(proposals[0].totalVotingPower || 0n);
   // @TODO UPDATE WITH REAL API DATA
-  nnsTotalVotingPower.set(0n);
+  nnsTotalVotingPower.set(
+    proposals[0]?.totalPotentialVotingPower || BigInt(50_276_005_084_190_970)
+  );
 };
