d4009d134b474d5d0a08b0ebc7404f319a4b4333
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-07-11 17:49:48 +0200

    cleanup

--- frontend/src/lib/derived/sns-metrics.derived.ts
+++ frontend/src/lib/derived/sns-metrics.derived.ts
@@ -1,11 +1,12 @@
 import { snsAggregatorDerived } from "$lib/derived/sns-aggregator.derived";
 import type { MetricsDto } from "$lib/types/sns-aggregator";
+import type { CanisterIdString } from "@dfinity/nns";
 import { isNullish } from "@dfinity/utils";
 import { type Readable } from "svelte/store";
 
 export interface SnsMetricsStoreData {
   // Root canister id is the key to identify the metrics for a specific project.
-  [rootCanisterId: string]: MetricsDto | undefined;
+  [rootCanisterId: CanisterIdString]: MetricsDto | undefined;
 }
 
 /**
