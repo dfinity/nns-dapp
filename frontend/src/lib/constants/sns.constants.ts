6ece27cedf1001a8e6646599e1e18acd57527c13
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-07-17 11:35:55 +0200

    Add MAX_SNS_FAV_PROJECTS const

--- frontend/src/lib/constants/sns.constants.ts
+++ frontend/src/lib/constants/sns.constants.ts
@@ -8,3 +8,7 @@ export const WATCH_SALE_STATE_EVERY_MILLISECONDS = 10_000;
  * ref: https://github.com/dfinity/nns-dapp/blob/38c66bafd1130be921fe9b27296b9d8e5338b6ff/rs/sns_aggregator/src/upstream.rs#L19
  */
 export const AGGREGATOR_METRICS_TIME_WINDOW_SECONDS = 2 * 30 * 24 * 3600;
+
+// Should be in sync with the backend:
+// https://github.com/dfinity/nns-dapp/blob/645f1bf762fed70230ed8779b5dbd92cc35ac023/rs/backend/src/accounts_store.rs#L35
+export const MAX_SNS_FAV_PROJECTS = 20;
