278dc2906e679ec7904d3fe5a6638957547367e4
David Dal Busco <david.dalbusco@dfinity.org>
2025-07-22 14:46:15 +0200

    feat: migrate to Svelte v5 to resolve naming clashes on "value"
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- frontend/src/lib/components/proposal-detail/ProposalSystemInfoEntry.svelte
+++ frontend/src/lib/components/proposal-detail/ProposalSystemInfoEntry.svelte
@@ -3,17 +3,21 @@
   import { Html, KeyValuePairInfo } from "@dfinity/gix-components";
   import TestIdWrapper from "$lib/components/common/TestIdWrapper.svelte";
 
-  export let label: string;
-  export let testId: string;
-  export let value: string;
-  export let description: string | undefined;
+  interface Props {
+    label: string;
+    testId: string;
+    value: string;
+    description: string | undefined;
+  }
+
+  let { label, testId, value: valueInfo, description }: Props = $props();
 </script>
 
 <KeyValuePairInfo {testId} alignIconRight>
   {#snippet key()}
     <span class="description">{label}</span>{/snippet}
   {#snippet value()}<span class="value" data-tid={`${testId}-value`}
-      >{value}</span
+      >{valueInfo}</span
     >{/snippet}
 
   {#snippet info()}
