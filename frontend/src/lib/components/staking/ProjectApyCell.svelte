a64ce5257ca858d52f27813152fe9ddd680a71f0
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-07-25 15:45:24 +0200

    ProjectApyCell to svelte5

--- frontend/src/lib/components/staking/ProjectApyCell.svelte
+++ frontend/src/lib/components/staking/ProjectApyCell.svelte
@@ -2,7 +2,11 @@
   import ApyDisplay from "$lib/components/ic/ApyDisplay.svelte";
   import type { TableProject } from "$lib/types/staking";
 
-  export let rowData: TableProject;
+  interface Props {
+    rowData: TableProject;
+  }
+
+  let { rowData }: Props = $props();
 </script>
 
 <div data-tid="project-apy-cell-component">
