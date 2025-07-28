2e61f8c8070a183c44e537dc905a28d2d0de4b9d
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-07-28 11:40:47 +0200

    Svelte5 -> 4 because of ResponsiveTableColumn type

--- frontend/src/lib/components/staking/ProjectApyCell.svelte
+++ frontend/src/lib/components/staking/ProjectApyCell.svelte
@@ -2,11 +2,7 @@
   import ApyDisplay from "$lib/components/ic/ApyDisplay.svelte";
   import type { TableProject } from "$lib/types/staking";
 
-  interface Props {
-    rowData: TableProject;
-  }
-
-  let { rowData }: Props = $props();
+  export let rowData: TableProject;
 </script>
 
 <div data-tid="project-apy-cell-component">
