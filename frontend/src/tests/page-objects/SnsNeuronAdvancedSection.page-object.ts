73e094d057bc5c660576c2340bb36e42a89f418e
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-07-29 18:14:03 +0200

    fix relative imports

--- frontend/src/tests/page-objects/SnsNeuronAdvancedSection.page-object.ts
+++ frontend/src/tests/page-objects/SnsNeuronAdvancedSection.page-object.ts
@@ -1,8 +1,8 @@
+import { ApyDisplayPo } from "$tests/page-objects/ApyDisplay.page-object";
 import { SnsNeuronAgePo } from "$tests/page-objects/SnsNeuronAge.page-object";
 import { SnsNeuronVestingPeriodRemainingPo } from "$tests/page-objects/SnsNeuronVestingPeriodRemaining.page-object";
 import { BasePageObject } from "$tests/page-objects/base.page-object";
 import type { PageObjectElement } from "$tests/types/page-object.types";
-import { ApyDisplayPo } from "./ApyDisplay.page-object";
 
 export class SnsNeuronAdvancedSectionPo extends BasePageObject {
   private static readonly TID = "sns-neuron-advanced-section-component";
