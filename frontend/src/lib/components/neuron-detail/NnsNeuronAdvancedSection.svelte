ff244c918eefbfd5a2c31c1cb99f9d4aa2deaef3
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-07-31 11:44:55 +0200

    Restore APY

--- frontend/src/lib/components/neuron-detail/NnsNeuronAdvancedSection.svelte
+++ frontend/src/lib/components/neuron-detail/NnsNeuronAdvancedSection.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
+  import ApyDisplay from "$lib/components/ic/ApyDisplay.svelte";
   import NnsNeuronPublicVisibilityAction from "$lib/components/neuron-detail/NnsNeuronPublicVisibilityAction.svelte";
   import JoinCommunityFundCheckbox from "$lib/components/neuron-detail/actions/JoinCommunityFundCheckbox.svelte";
   import NnsAutoStakeMaturity from "$lib/components/neuron-detail/actions/NnsAutoStakeMaturity.svelte";
   import SplitNnsNeuronButton from "$lib/components/neuron-detail/actions/SplitNnsNeuronButton.svelte";
   import NnsNeuronAge from "$lib/components/neurons/NnsNeuronAge.svelte";
   import Hash from "$lib/components/ui/Hash.svelte";
+  import { OWN_CANISTER_ID_TEXT } from "$lib/constants/canister-ids.constants";
   import { icpAccountsStore } from "$lib/derived/icp-accounts.derived";
   import { authStore } from "$lib/stores/auth.store";
+  import { ENABLE_APY_PORTFOLIO } from "$lib/stores/feature-flags.store";
   import { i18n } from "$lib/stores/i18n";
   import { nnsLatestRewardEventStore } from "$lib/stores/nns-latest-reward-event.store";
+  import { stakingRewardsStore } from "$lib/stores/staking-rewards.store";
+  import type { ApyAmount } from "$lib/types/staking";
   import { secondsToDate, secondsToDateTime } from "$lib/utils/date.utils";
   import {
     canUserManageNeuronFundParticipation,
@@ -16,6 +21,7 @@
     isNeuronControllable,
     maturityLastDistribution,
   } from "$lib/utils/neuron.utils";
+  import { isStakingRewardDataReady } from "$lib/utils/staking-rewards.utils";
   import {
     Html,
     KeyValuePair,
@@ -46,7 +52,9 @@
 
   let apy: ApyAmount | undefined;
   $: apy =
-    nonNullish(neuron) && isStakingRewardDataReady($stakingRewardsStore)
+    $ENABLE_APY_PORTFOLIO &&
+    nonNullish(neuron) &&
+    isStakingRewardDataReady($stakingRewardsStore)
       ? $stakingRewardsStore.apy
           .get(OWN_CANISTER_ID_TEXT)
           ?.neurons?.get(neuron.neuronId.toString())
