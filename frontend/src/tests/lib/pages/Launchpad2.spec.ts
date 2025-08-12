a8d0b05a4e20e5616562f198117ca07dfccdd3b2
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-08-12 11:18:41 +0200

    Update tests

--- frontend/src/tests/lib/pages/Launchpad2.spec.ts
+++ frontend/src/tests/lib/pages/Launchpad2.spec.ts
@@ -1,3 +1,4 @@
+import { FEATURED_SNS_PROJECTS } from "$lib/constants/sns.constants";
 import type { SnsFullProject } from "$lib/derived/sns/sns-projects.derived";
 import Launchpad2 from "$lib/pages/Launchpad2.svelte";
 import {
@@ -12,6 +13,7 @@ import {
 import { JestPageObjectElement } from "$tests/page-objects/jest.page-object";
 import { Launchpad2Po } from "$tests/page-objects/Launchpad2.page-object";
 import { type ProposalInfo } from "@dfinity/nns";
+import { Principal } from "@dfinity/principal";
 import { SnsSwapLifecycle } from "@dfinity/sns";
 import { render } from "@testing-library/svelte";
 
@@ -38,7 +40,7 @@ describe("Launchpad2", () => {
     });
 
     expect(await po.getUpcomingLaunchesCardListPo().isPresent()).toBe(false);
-    expect(await po.getLaunchedProjectsCardListPo().isPresent()).toBe(false);
+    expect(await po.getFeaturedProjectsCardListPo().isPresent()).toBe(false);
     expect(await po.getSkeletonProjectsCardListPo().isPresent()).toBe(true);
   });
 
@@ -87,6 +89,125 @@ describe("Launchpad2", () => {
     expect(await upcomingLaunchesCardsEntryPos[2].getCardTitle()).toEqual(
       "Project 2"
     );
+  });
+
+  it("displays featured projects cards in correct order", async () => {
+    const project0 = createMockSnsFullProject({
+      rootCanisterId: Principal.fromText(FEATURED_SNS_PROJECTS[0]),
+      summaryParams: {
+        lifecycle: SnsSwapLifecycle.Committed,
+        swapOpenTimestampSeconds: BigInt(168_000_000),
+        projectName: "Outdated Featured Project",
+      },
+      metrics: undefined,
+    });
+    const project1 = createMockSnsFullProject({
+      rootCanisterId: Principal.fromText(FEATURED_SNS_PROJECTS[1]),
+      summaryParams: {
+        lifecycle: SnsSwapLifecycle.Committed,
+        swapOpenTimestampSeconds: BigInt(168_000_000),
+        projectName: "Not Committed, Not Active Featured Project",
+      },
+      metrics: {
+        ...mockSnsMetrics,
+        num_recently_executed_proposals: 0,
+      },
+    });
+    const project2 = createMockSnsFullProject({
+      rootCanisterId: Principal.fromText(FEATURED_SNS_PROJECTS[2]),
+      summaryParams: {
+        lifecycle: SnsSwapLifecycle.Committed,
+        swapOpenTimestampSeconds: BigInt(168_000_000),
+        projectName: "Not Committed, Very Active Featured Project",
+      },
+      metrics: {
+        ...mockSnsMetrics,
+        num_recently_executed_proposals: 22,
+      },
+    });
+    const project3 = createMockSnsFullProject({
+      rootCanisterId: Principal.fromText(FEATURED_SNS_PROJECTS[3]),
+      summaryParams: {
+        lifecycle: SnsSwapLifecycle.Committed,
+        swapOpenTimestampSeconds: BigInt(168_000_000),
+        projectName: "Committed, Not Active Featured Project",
+      },
+      icpCommitment: 10_000_000n,
+      metrics: {
+        ...mockSnsMetrics,
+        num_recently_executed_proposals: 0,
+      },
+    });
+    const project4 = createMockSnsFullProject({
+      rootCanisterId: Principal.fromText(FEATURED_SNS_PROJECTS[4]),
+      summaryParams: {
+        lifecycle: SnsSwapLifecycle.Committed,
+        swapOpenTimestampSeconds: BigInt(168_000_000),
+        projectName: "Committed, Very Active Featured Project",
+      },
+      icpCommitment: 10_000_000n,
+      metrics: {
+        ...mockSnsMetrics,
+        num_recently_executed_proposals: 22,
+      },
+    });
+    const project5 = createMockSnsFullProject({
+      rootCanisterId: Principal.fromText(FEATURED_SNS_PROJECTS[5]),
+      summaryParams: {
+        lifecycle: SnsSwapLifecycle.Committed,
+        swapOpenTimestampSeconds: BigInt(168_000_000),
+        projectName: "Committed, Extremely Active Featured Project",
+      },
+      icpCommitment: 10_000_000n,
+      metrics: {
+        ...mockSnsMetrics,
+        num_recently_executed_proposals: 100,
+      },
+    });
+    const notFeaturedProject = createMockSnsFullProject({
+      rootCanisterId: principal(0),
+      summaryParams: {
+        lifecycle: SnsSwapLifecycle.Committed,
+        swapOpenTimestampSeconds: BigInt(168_000_000),
+        projectName: "Not Featured Project",
+      },
+      icpCommitment: 10_000_000n,
+      metrics: {
+        ...mockSnsMetrics,
+        num_recently_executed_proposals: 100,
+      },
+    });
+
+    const po = renderComponent({
+      snsProjects: [
+        project0,
+        project1,
+        project2,
+        project3,
+        project4,
+        project5,
+        notFeaturedProject,
+      ],
+      openSnsProposals: [],
+    });
+
+    const featuredProjectCards = po.getFeaturedProjectsCardListPo();
+    const cardsEntryPos = await featuredProjectCards.getCardEntries();
+
+    expect(await featuredProjectCards.isPresent()).toBe(true);
+    expect(cardsEntryPos.length).toBe(6);
+    const launchedProjectsCardsTitles = await Promise.all(
+      cardsEntryPos.map((card) => card.getCardTitle())
+    );
+    expect(launchedProjectsCardsTitles).not.toContain("Not Featured Project");
+    expect(launchedProjectsCardsTitles).toEqual([
+      "Committed, Extremely Active Featured Project",
+      "Not Committed, Very Active Featured Project",
+      "Committed, Very Active Featured Project",
+      "Not Committed, Not Active Featured Project",
+      "Committed, Not Active Featured Project",
+      "Outdated Featured Project",
+    ]);
   });
 
   it("displays launched cards in correct order", async () => {
@@ -149,27 +270,50 @@ describe("Launchpad2", () => {
         num_recently_executed_proposals: 22,
       },
     });
+    const featuredProject = createMockSnsFullProject({
+      rootCanisterId: Principal.fromText(FEATURED_SNS_PROJECTS[0]),
+      summaryParams: {
+        lifecycle: SnsSwapLifecycle.Committed,
+        swapOpenTimestampSeconds: BigInt(168_000_000),
+        projectName: "Featured Project",
+      },
+      icpCommitment: 10_000_000n,
+      metrics: {
+        ...mockSnsMetrics,
+        num_recently_executed_proposals: 22,
+      },
+    });
 
     const po = renderComponent({
-      snsProjects: [project0, project1, project12, project3, project4],
+      snsProjects: [
+        project0,
+        project1,
+        project12,
+        project3,
+        project4,
+        featuredProject,
+      ],
       openSnsProposals: [],
     });
 
-    const launchedProjectsCards = po.getLaunchedProjectsCardListPo();
-    const launchedProjectsCardsEntryPos =
-      await launchedProjectsCards.getCardEntries();
+    const featuredProjectCards = po.getFeaturedProjectsCardListPo();
+    expect(await featuredProjectCards.getCardEntries()).toHaveLength(1);
 
-    expect(await launchedProjectsCards.isPresent()).toBe(true);
-    expect(launchedProjectsCardsEntryPos.length).toBe(5);
+    const restProjectCards = po.getRestProjectsCardListPo();
+    const cardsEntryPos = await restProjectCards.getCardEntries();
+
+    expect(await restProjectCards.isPresent()).toBe(true);
+    expect(cardsEntryPos.length).toBe(5);
     const launchedProjectsCardsTitles = await Promise.all(
-      launchedProjectsCardsEntryPos.map((card) => card.getCardTitle())
+      cardsEntryPos.map((card) => card.getCardTitle())
     );
 
+    expect(launchedProjectsCardsTitles).not.toContain("Featured Project");
     expect(launchedProjectsCardsTitles).toEqual([
-      "Committed, Very Active Project",
-      "Committed, Not Active Project",
       "Not Committed, Very Active Project",
+      "Committed, Very Active Project",
       "Not Committed, Not Active Project",
+      "Committed, Not Active Project",
       "Outdated Project",
     ]);
   });
