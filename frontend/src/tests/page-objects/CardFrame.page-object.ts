639cfdcd40ab553ff6394873b04da49ded83264a
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-08-19 16:33:17 +0200

    Simplify CardFramePo inheritance

--- frontend/src/tests/page-objects/CardFrame.page-object.ts
+++ frontend/src/tests/page-objects/CardFrame.page-object.ts
@@ -1,18 +1,12 @@
-import { BasePortfolioCardPo } from "$tests/page-objects/BasePortfolioCard.page-object";
-
-// TODO(launchpad2): Replace with extends BasePageObject
+import { BasePageObject } from "$tests/page-objects/base.page-object";
 
 // CardFramePo should not be used directly but rather as a base class for launchpad cards.
-export abstract class CardFramePo extends BasePortfolioCardPo {
+export abstract class CardFramePo extends BasePageObject {
   abstract getTitle(): Promise<string>;
 
   async hasClass(className: string): Promise<boolean> {
     const classNames = await this.root.getClasses();
     return classNames.includes(className);
-  }
-
-  async isHighlighted(): Promise<boolean> {
-    return this.hasClass("highlighted");
   }
 
   async hasBackgroundIcon(): Promise<boolean> {
