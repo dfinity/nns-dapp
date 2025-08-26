fbb0c899f32b937780e634d71ade8ed5b13bcc78
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-08-26 12:50:07 +0200

    refactor: canister names

--- frontend/src/lib/constants/ck-canister-ids.constants.ts
+++ frontend/src/lib/constants/ck-canister-ids.constants.ts
@@ -2,84 +2,84 @@ import type { DefaultIcrcCanisters } from "$lib/stores/default-icrc-canisters.st
 import { Principal } from "@dfinity/principal";
 
 // ckEURC
-export const CKEURC_LEDGER_CANISTER_ID = "pe5t5-diaaa-aaaar-qahwa-cai";
-export const CKEURC_INDEX_CANISTER_ID = "pd4vj-oqaaa-aaaar-qahwq-cai";
+const CKEURC_LEDGER_CANISTER_ID_TEXT = "pe5t5-diaaa-aaaar-qahwa-cai";
+const CKEURC_INDEX_CANISTER_ID_TEXT = "pd4vj-oqaaa-aaaar-qahwq-cai";
 
 // ckUNI
-export const CKUNI_LEDGER_CANISTER_ID = "ilzky-ayaaa-aaaar-qahha-cai";
-export const CKUNI_INDEX_CANISTER_ID = "imymm-naaaa-aaaar-qahhq-cai";
+const CKUNI_LEDGER_CANISTER_ID_TEXT = "ilzky-ayaaa-aaaar-qahha-cai";
+const CKUNI_INDEX_CANISTER_ID_TEXT = "imymm-naaaa-aaaar-qahhq-cai";
 
 // ckWBTC
-export const CKWBTC_LEDGER_CANISTER_ID = "bptq2-faaaa-aaaar-qagxq-cai";
-export const CKWBTC_INDEX_CANISTER_ID = "dso6s-wiaaa-aaaar-qagya-cai";
+const CKWBTC_LEDGER_CANISTER_ID_TEXT = "bptq2-faaaa-aaaar-qagxq-cai";
+const CKWBTC_INDEX_CANISTER_ID_TEXT = "dso6s-wiaaa-aaaar-qagya-cai";
 
 // ckLINK
-export const CKLINK_LEDGER_CANISTER_ID = "g4tto-rqaaa-aaaar-qageq-cai";
-export const CKLINK_INDEX_CANISTER_ID = "gvqys-hyaaa-aaaar-qagfa-cai";
+const CKLINK_LEDGER_CANISTER_ID_TEXT = "g4tto-rqaaa-aaaar-qageq-cai";
+const CKLINK_INDEX_CANISTER_ID_TEXT = "gvqys-hyaaa-aaaar-qagfa-cai";
 
 // ckXAUT
-export const CKXAUT_LEDGER_CANISTER_ID = "nza5v-qaaaa-aaaar-qahzq-cai";
-export const CKXAUT_INDEX_CANISTER_ID = "nmhmy-riaaa-aaaar-qah2a-cai";
+const CKXAUT_LEDGER_CANISTER_ID_TEXT = "nza5v-qaaaa-aaaar-qahzq-cai";
+const CKXAUT_INDEX_CANISTER_ID_TEXT = "nmhmy-riaaa-aaaar-qah2a-cai";
 
 // ckPEPE
-export const CKPEPE_LEDGER_CANISTER_ID = "etik7-oiaaa-aaaar-qagia-cai";
-export const CKPEPE_INDEX_CANISTER_ID = "eujml-dqaaa-aaaar-qagiq-cai";
+const CKPEPE_LEDGER_CANISTER_ID_TEXT = "etik7-oiaaa-aaaar-qagia-cai";
+const CKPEPE_INDEX_CANISTER_ID_TEXT = "eujml-dqaaa-aaaar-qagiq-cai";
 
 // ckWSTETH
-export const CKWSTETH_LEDGER_CANISTER_ID = "j2tuh-yqaaa-aaaar-qahcq-cai";
-export const CKWSTETH_INDEX_CANISTER_ID = "jtq73-oyaaa-aaaar-qahda-cai";
+const CKWSTETH_LEDGER_CANISTER_ID_TEXT = "j2tuh-yqaaa-aaaar-qahcq-cai";
+const CKWSTETH_INDEX_CANISTER_ID_TEXT = "jtq73-oyaaa-aaaar-qahda-cai";
 
 // ckSHIB
-export const CKSHIB_LEDGER_CANISTER_ID = "fxffn-xiaaa-aaaar-qagoa-cai";
-export const CKSHIB_INDEX_CANISTER_ID = "fqedz-2qaaa-aaaar-qagoq-cai";
+const CKSHIB_LEDGER_CANISTER_ID_TEXT = "fxffn-xiaaa-aaaar-qagoa-cai";
+const CKSHIB_INDEX_CANISTER_ID_TEXT = "fqedz-2qaaa-aaaar-qagoq-cai";
 
 // ckUSDT
-export const CKUSDT_LEDGER_CANISTER_ID = "cngnf-vqaaa-aaaar-qag4q-cai";
-export const CKUSDT_INDEX_CANISTER_ID = "cefgz-dyaaa-aaaar-qag5a-cai";
+const CKUSDT_LEDGER_CANISTER_ID_TEXT = "cngnf-vqaaa-aaaar-qag4q-cai";
+const CKUSDT_INDEX_CANISTER_ID_TEXT = "cefgz-dyaaa-aaaar-qag5a-cai";
 
 // ckOCT
-export const CKOCT_LEDGER_CANISTER_ID = "ebo5g-cyaaa-aaaar-qagla-cai";
-export const CKOCT_INDEX_CANISTER_ID = "egp3s-paaaa-aaaar-qaglq-cai";
+const CKOCT_LEDGER_CANISTER_ID_TEXT = "ebo5g-cyaaa-aaaar-qagla-cai";
+const CKOCT_INDEX_CANISTER_ID_TEXT = "egp3s-paaaa-aaaar-qaglq-cai";
 
 export const allCkTokens: DefaultIcrcCanisters[] = [
   {
-    ledgerCanisterId: Principal.fromText(CKEURC_LEDGER_CANISTER_ID),
-    indexCanisterId: Principal.fromText(CKEURC_INDEX_CANISTER_ID),
+    ledgerCanisterId: Principal.fromText(CKEURC_LEDGER_CANISTER_ID_TEXT),
+    indexCanisterId: Principal.fromText(CKEURC_INDEX_CANISTER_ID_TEXT),
   },
   {
-    ledgerCanisterId: Principal.fromText(CKUNI_LEDGER_CANISTER_ID),
-    indexCanisterId: Principal.fromText(CKUNI_INDEX_CANISTER_ID),
+    ledgerCanisterId: Principal.fromText(CKUNI_LEDGER_CANISTER_ID_TEXT),
+    indexCanisterId: Principal.fromText(CKUNI_INDEX_CANISTER_ID_TEXT),
   },
   {
-    ledgerCanisterId: Principal.fromText(CKWBTC_LEDGER_CANISTER_ID),
-    indexCanisterId: Principal.fromText(CKWBTC_INDEX_CANISTER_ID),
+    ledgerCanisterId: Principal.fromText(CKWBTC_LEDGER_CANISTER_ID_TEXT),
+    indexCanisterId: Principal.fromText(CKWBTC_INDEX_CANISTER_ID_TEXT),
   },
   {
-    ledgerCanisterId: Principal.fromText(CKLINK_LEDGER_CANISTER_ID),
-    indexCanisterId: Principal.fromText(CKLINK_INDEX_CANISTER_ID),
+    ledgerCanisterId: Principal.fromText(CKLINK_LEDGER_CANISTER_ID_TEXT),
+    indexCanisterId: Principal.fromText(CKLINK_INDEX_CANISTER_ID_TEXT),
   },
   {
-    ledgerCanisterId: Principal.fromText(CKXAUT_LEDGER_CANISTER_ID),
-    indexCanisterId: Principal.fromText(CKXAUT_INDEX_CANISTER_ID),
+    ledgerCanisterId: Principal.fromText(CKXAUT_LEDGER_CANISTER_ID_TEXT),
+    indexCanisterId: Principal.fromText(CKXAUT_INDEX_CANISTER_ID_TEXT),
   },
   {
-    ledgerCanisterId: Principal.fromText(CKPEPE_LEDGER_CANISTER_ID),
-    indexCanisterId: Principal.fromText(CKPEPE_INDEX_CANISTER_ID),
+    ledgerCanisterId: Principal.fromText(CKPEPE_LEDGER_CANISTER_ID_TEXT),
+    indexCanisterId: Principal.fromText(CKPEPE_INDEX_CANISTER_ID_TEXT),
   },
   {
-    ledgerCanisterId: Principal.fromText(CKWSTETH_LEDGER_CANISTER_ID),
-    indexCanisterId: Principal.fromText(CKWSTETH_INDEX_CANISTER_ID),
+    ledgerCanisterId: Principal.fromText(CKWSTETH_LEDGER_CANISTER_ID_TEXT),
+    indexCanisterId: Principal.fromText(CKWSTETH_INDEX_CANISTER_ID_TEXT),
   },
   {
-    ledgerCanisterId: Principal.fromText(CKSHIB_LEDGER_CANISTER_ID),
-    indexCanisterId: Principal.fromText(CKSHIB_INDEX_CANISTER_ID),
+    ledgerCanisterId: Principal.fromText(CKSHIB_LEDGER_CANISTER_ID_TEXT),
+    indexCanisterId: Principal.fromText(CKSHIB_INDEX_CANISTER_ID_TEXT),
   },
   {
-    ledgerCanisterId: Principal.fromText(CKUSDT_LEDGER_CANISTER_ID),
-    indexCanisterId: Principal.fromText(CKUSDT_INDEX_CANISTER_ID),
+    ledgerCanisterId: Principal.fromText(CKUSDT_LEDGER_CANISTER_ID_TEXT),
+    indexCanisterId: Principal.fromText(CKUSDT_INDEX_CANISTER_ID_TEXT),
   },
   {
-    ledgerCanisterId: Principal.fromText(CKOCT_LEDGER_CANISTER_ID),
-    indexCanisterId: Principal.fromText(CKOCT_INDEX_CANISTER_ID),
+    ledgerCanisterId: Principal.fromText(CKOCT_LEDGER_CANISTER_ID_TEXT),
+    indexCanisterId: Principal.fromText(CKOCT_INDEX_CANISTER_ID_TEXT),
   },
 ];
