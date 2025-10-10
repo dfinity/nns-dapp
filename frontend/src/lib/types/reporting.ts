c6da7985b13ddb3617ebbc7f32c6b69d3a8b7383
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-10-10 06:04:45 +0200

    remove all option

--- frontend/src/lib/types/reporting.ts
+++ frontend/src/lib/types/reporting.ts
@@ -2,7 +2,7 @@ import type { Account } from "$lib/types/account";
 import type { TransactionWithId } from "@dfinity/ledger-icp";
 import type { NeuronInfo } from "@dfinity/nns";
 
-export type ReportingPeriod = "all" | "last-year" | "year-to-date" | "custom";
+export type ReportingPeriod = "year-to-date" | "last-year" | "custom";
 
 export type TransactionsDateRange = {
   /** Start of the date range (inclusive) - timestamp in nanoseconds */
