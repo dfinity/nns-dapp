5d4d73d13a4c454ea08f1193c11699e83e4d4738
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-10-17 14:28:40 +0200

    clean up

--- frontend/src/lib/api/sns-aggregator.api.ts
+++ frontend/src/lib/api/sns-aggregator.api.ts
@@ -9,9 +9,10 @@ const aggregatorPageUrl = (page: number) =>
 // Aggregator has a page size of 10 elements.
 // We currently have 51 projects so 6 pages
 // https://github.com/dfinity/nns-dapp/blob/b056a5dd42ffc8f198ce8eb92688645389293ef6/rs/sns_aggregator/src/state.rs#L140
-export const queryParallelSnsAggregator = async (): Promise<CachedSnsDto[]> => {
-  const pages = [0, 1, 2, 3, 4, 5].map(aggregatorPageUrl);
+const PAGE_COUNT = 6;
+const pages = [...Array(PAGE_COUNT).keys()].map(aggregatorPageUrl);
 
+export const queryParallelSnsAggregator = async (): Promise<CachedSnsDto[]> => {
   const pagePromises = pages.map(async (page) => {
     try {
       const response = await fetch(page);
