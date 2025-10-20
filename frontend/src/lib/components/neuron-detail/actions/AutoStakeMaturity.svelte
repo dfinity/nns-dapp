54741a2bf6bd3b366011db8c40ab180d9594dd01
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-10-20 16:01:21 +0200

    display tooltip if checkbox is disabled

--- frontend/src/lib/components/neuron-detail/actions/AutoStakeMaturity.svelte
+++ frontend/src/lib/components/neuron-detail/actions/AutoStakeMaturity.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { i18n } from "$lib/stores/i18n";
-  import { Checkbox } from "@dfinity/gix-components";
+  import { Checkbox, Tooltip } from "@dfinity/gix-components";
 
   type Props = {
     hasAutoStakeOn: boolean;
@@ -10,14 +10,28 @@
   let { hasAutoStakeOn = $bindable(), disabled = false }: Props = $props();
 </script>
 
-<Checkbox
-  preventDefault
-  testId="auto-stake-maturity-checkbox-component"
-  inputId="auto-stake-maturity-checkbox"
-  checked={hasAutoStakeOn}
-  on:nnsChange
-  {disabled}
-  --checkbox-label-order="1"
->
-  <span>{$i18n.neuron_detail.auto_stake_maturity}</span>
-</Checkbox>
+{#snippet checkbox()}
+  <Checkbox
+    preventDefault
+    testId="auto-stake-maturity-checkbox-component"
+    inputId="auto-stake-maturity-checkbox"
+    checked={hasAutoStakeOn}
+    on:nnsChange
+    {disabled}
+    --checkbox-label-order="1"
+  >
+    <span>{$i18n.neuron_detail.auto_stake_maturity}</span>
+  </Checkbox>
+{/snippet}
+
+{#if disabled}
+  <Tooltip
+    testId="auto-stake-maturity-tooltip"
+    idPrefix="auto-stake-maturity-tooltip"
+    text={$i18n.neuron_detail.auto_stake_maturity_tooltip}
+  >
+    {@render checkbox()}
+  </Tooltip>
+{:else}
+  {@render checkbox()}
+{/if}
