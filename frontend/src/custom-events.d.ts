df4a291abcf5fbd2029f6766f7038de7f07c51ea
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-10-29 13:44:04 +0100

    disable eslint for event definitions

--- frontend/src/custom-events.d.ts
+++ frontend/src/custom-events.d.ts
@@ -11,13 +11,21 @@ declare module "svelte/elements" {
     "on:nnsIcrcTokenModal"?: CompositionEventHandler;
 
     // svelte5
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     nnsIntersecting?: (event: CustomEvent<any>) => void;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     nnsCanisterDetailModal?: (event: CustomEvent<any>) => void;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     nnsNeuronDetailModal?: (event: CustomEvent<any>) => void;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     snsNeuronDetailModal?: (event: CustomEvent<any>) => void;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     nnsWalletModal?: (event: CustomEvent<any>) => void;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     nnsCkBTCAccountsModal?: (event: CustomEvent<any>) => void;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     nnsAccountsModal?: (event: CustomEvent<any>) => void;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     nnsIcrcTokenModal?: (event: CustomEvent<any>) => void;
   }
 }
