0f343579f46ae395cd9ecc1b0b8b432ebba63139
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-11-12 18:57:39 +0100

    test env

--- frontend/vitest.setup.ts
+++ frontend/vitest.setup.ts
@@ -176,6 +176,7 @@ vi.mock("./src/lib/utils/env-vars.utils.ts", () => ({
     governanceCanisterId: "rrkah-fqaaa-aaaaa-aaaaq-cai",
     identityServiceUrl: "http://localhost:8000/",
     icpSwapUrl: "http://mrfq3-7eaaa-aaaaa-qabja-cai.localhost:8080",
+    kongSwapUrl: "http://xvemo-ap777-77774-qaalq-cai.localhost:8080",
     ledgerCanisterId: "ryjl3-tyaaa-aaaaa-aaaba-cai",
     indexCanisterId: "ryjl3-tyaaa-aaaaa-aaaba-cai",
     ownCanisterId: "qhbym-qaaaa-aaaaa-aaafq-cai",
