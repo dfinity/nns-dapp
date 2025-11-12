445b157cec27cd28e38b4dd7d32688ce939eaab2
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-11-12 19:03:14 +0100

    fmt

--- frontend/src/lib/types/kong-swap.ts
+++ frontend/src/lib/types/kong-swap.ts
@@ -1,12 +1,10 @@
 export interface KongSwapTicker {
   ticker_id: string;
-  base_currency: string; // ledger canister id
-  target_currency: string; // ledger canister id
+  base_currency: string;
+  target_currency: string;
   pool_id: string;
   last_price: number;
   base_volume?: number;
   target_volume?: number;
   liquidity_in_usd?: number;
 }
-
-
--- frontend/src/tests/lib/api/kong-swap.api.spec.ts
+++ frontend/src/tests/lib/api/kong-swap.api.spec.ts
@@ -2,39 +2,37 @@ import { queryKongSwapTickers } from "$lib/api/kong-swap.api";
 import { KONG_SWAP_URL } from "$lib/constants/environment.constants";
 
 describe("kong-swap.api", () => {
-  describe("queryKongSwapTickers", () => {
-    it("should fetch KongSwap tickers", async () => {
-      const tickersResponse = [
-        {
-          ticker_id: "mxzaz-hqaaa-aaaar-qaada-cai_cngnf-vqaaa-aaaar-qag4q-cai",
-          base_currency: "mxzaz-hqaaa-aaaar-qaada-cai",
-          target_currency: "cngnf-vqaaa-aaaar-qag4q-cai",
-          pool_id: "2",
-          last_price: 111324.134685011,
-        },
-      ];
+  it("should fetch KongSwap tickers", async () => {
+    const tickersResponse = [
+      {
+        ticker_id: "mxzaz-hqaaa-aaaar-qaada-cai_cngnf-vqaaa-aaaar-qag4q-cai",
+        base_currency: "mxzaz-hqaaa-aaaar-qaada-cai",
+        target_currency: "cngnf-vqaaa-aaaar-qag4q-cai",
+        pool_id: "2",
+        last_price: 111324.134685011,
+      },
+    ];
 
-      vi.spyOn(global, "fetch").mockResolvedValue({
-        ok: true,
-        json: async () => tickersResponse,
-      } as Response);
+    vi.spyOn(global, "fetch").mockResolvedValue({
+      ok: true,
+      json: async () => tickersResponse,
+    } as Response);
 
-      expect(await queryKongSwapTickers()).toEqual(tickersResponse);
+    expect(await queryKongSwapTickers()).toEqual(tickersResponse);
 
-      expect(global.fetch).toBeCalledWith(new URL(`${KONG_SWAP_URL}/api/coingecko/tickers`));
-      expect(global.fetch).toBeCalledTimes(1);
-    });
+    expect(global.fetch).toBeCalledWith(
+      new URL(`${KONG_SWAP_URL}/api/coingecko/tickers`)
+    );
+    expect(global.fetch).toBeCalledTimes(1);
+  });
 
-    it("should throw if fetch fails", async () => {
-      vi.spyOn(global, "fetch").mockResolvedValue({
-        ok: false,
-      } as Response);
+  it("should throw if fetch fails", async () => {
+    vi.spyOn(global, "fetch").mockResolvedValue({
+      ok: false,
+    } as Response);
 
-      await expect(queryKongSwapTickers()).rejects.toThrow(
-        new Error("Failed to fetch ticker information from KongSwap")
-      );
-    });
+    await expect(queryKongSwapTickers()).rejects.toThrow(
+      new Error("Failed to fetch ticker information from KongSwap")
+    );
   });
 });
-
-
