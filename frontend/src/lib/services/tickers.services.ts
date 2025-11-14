8bfdee04af6a135df331861567ff54915a33a95c
Yusef Habib <yusef.fernandez@dfinity.org>
2025-11-14 18:14:32 +0100

    feat(pricing): add KongSwap as fallback ticker provider (#7558)
    # Motivation
    
    The nns-dapp currently has only one provider for ICP tickers. If there
    is an issue with this provider, the dapp cannot display fiat values. To
    prevent such situations, a secondary provider will be added. This PR
    makes use of this new provider as a fallback mechanism.
    
    Prev. PR: #7557
    
    [NNS1-4238](https://dfinity.atlassian.net/browse/NNS1-4238)
    
    # Changes
    
    - Added `kong-swap` as a provider to the tickers service.
    
    # Tests
    
    - Added unit tests.
    
    # Todos
    
    - [x] Accessibility (a11y) – any impact?
    - [x] Changelog – is it needed?
    
    [NNS1-4238]:
    https://dfinity.atlassian.net/browse/NNS1-4238?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- frontend/src/lib/services/tickers.services.ts
+++ frontend/src/lib/services/tickers.services.ts
@@ -1,12 +1,14 @@
 import { icpSwapTickerProvider } from "$lib/services/icp-swap.provider";
+import { kongSwapTickerProvider } from "$lib/services/kong-swap.provider";
 import { tickersStore } from "$lib/stores/tickers.store";
 import type { ProviderLoader, TickersProviders } from "$lib/types/tickers";
 import { isNullish } from "@dfinity/utils";
 import { get } from "svelte/store";
 
-const providers: TickersProviders[] = ["icp-swap"];
+export const providers: TickersProviders[] = ["icp-swap", "kong-swap"];
 const providersLoaders = new Map<TickersProviders, ProviderLoader>([
   ["icp-swap", icpSwapTickerProvider],
+  ["kong-swap", kongSwapTickerProvider],
 ]);
 
 export const loadTickers = async (): Promise<void> => {
