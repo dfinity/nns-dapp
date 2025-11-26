7d30d48dbfa01aaade6fb75bb010c3802dde829d
Francesco <francesco.rizzi@dfinity.org>
2025-11-26 15:51:12 +0100

    WIP - Work in progress!

--- frontend/src/tests/mocks/i18n.mock.ts
+++ frontend/src/tests/mocks/i18n.mock.ts
@@ -6,7 +6,6 @@ import * as enGovernance from "$lib/i18n/en.governance.json";
 import * as enCore from "$lib/i18n/en.json";
 
 const en = {
-  lang: "en",
   ...enCore,
   ...enGovernance,
 } as I18n;
