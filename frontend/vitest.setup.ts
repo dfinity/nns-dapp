7e5e43920f0b10451260f1aa09dda7d009e83763
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-12-12 16:45:52 +0100

    remove enable_disburse_maturity

--- frontend/vitest.setup.ts
+++ frontend/vitest.setup.ts
@@ -172,7 +172,6 @@ vi.mock("./src/lib/utils/env-vars.utils.ts", () => ({
     featureFlags: JSON.stringify({
       ENABLE_CKTESTBTC: true,
       DISABLE_CKTOKENS: false,
-      ENABLE_DISBURSE_MATURITY: false,
       DISABLE_IMPORT_TOKEN_VALIDATION_FOR_TESTING: false,
       TEST_FLAG_EDITABLE: true,
       TEST_FLAG_NOT_EDITABLE: true,
